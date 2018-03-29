4381e68ca523a474b97bf75a4e616ccb27aa847f
신봉석 <bsshin@willbes.com>
2018-03-29 13:23:13 +0900

    LMS > 상품 > 학습형태 관리 소스 파일 삭제

--- application/third_party/PHPStorm_CI_CC.php
+++ application/third_party/PHPStorm_CI_CC.php
@@ -104,7 +104,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @property CategoryModel $categoryModel                   Category Model Class
  * @property CourseModel $courseModel                         Product Course Model Class
  * @property SubjectModel $subjectModel                       Product Subject Model Class
- * @property PatternModel $patternModel                       Product Pattern Model Class
  * @property BoardMasterModel $boardMasterModel          BoardMaster Model Class
  * @property BoardModel $boardModel                            Board Model Class
  */

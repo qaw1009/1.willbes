708e0a889936aef9657cea6ff0532441c75a39f2
조규호 <benoo@willbes.com>
2018-04-25 08:48:58 +0900

    모델추가 수정

--- application/third_party/PHPStorm_CI_CC.php
+++ application/third_party/PHPStorm_CI_CC.php
@@ -114,14 +114,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @property SortMappingModel $sortMappingModel                               Product Sort Mapping Model Class
  * @property BoardMasterModel $boardMasterModel                               BoardMaster Model Class
  * @property BoardModel $boardModel                                                 Board Model Class
- *
  * @property WCpModel $wcpModel                                                     WCp Model Class
-
  * @property SearchWMasterLectureModel $searchWMasterLectureModel   SearchWMasterLecture Model Class
- *
- *
- *
- *
  *
  *
  */

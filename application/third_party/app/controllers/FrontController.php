eb904566283bf796ec46d1dac1947952082f18b6
신봉석 <bsshin@willbes.com>
2018-05-08 13:26:02 +0900

    프런트 컨트롤러 로그인 체크 기능 추가

--- application/third_party/app/controllers/FrontController.php
+++ application/third_party/app/controllers/FrontController.php
@@ -5,17 +5,45 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 abstract class FrontController extends BaseController
 {
+    protected $auth_controller = false;
+    protected $auth_methods = array();
+    
     use InitController;
 
     public function __construct()
     {
         parent::__construct();
 
+        // 로그인 체크
+        $this->_checkLogin();
+        
+        // 초기화
         $this->_init();
 
         // 프로파일러 실행
         if (config_item('enable_profiler') === true) {
             $this->output->enable_profiler(true);
         }
+    }
+
+    /**
+     * 컨트롤러 클래스, 메소드별 로그인 체크
+     */
+    private function _checkLogin()
+    {
+        if ($this->auth_controller === true || in_array($this->router->method, $this->auth_methods) === true) {
+            if ($this->_isLogin() !== true) {
+                show_error('접근 권한이 없습니다.', _HTTP_UNAUTHORIZED, '접근 권한 없음');
+            }
+        }
+    }
+
+    /**
+     * 로그인 여부 리턴
+     * @return bool
+     */
+    private function _isLogin()
+    {
+        return $this->session->userdata('is_login');
     }
 }

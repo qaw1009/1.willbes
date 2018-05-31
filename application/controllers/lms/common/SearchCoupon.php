7766a010bc4c2ba284018ee727f9ff5286beae21
조규호 <benoo@willbes.com>
2018-05-31 15:48:52 +0900

    강좌지급쿠폰 컨트롤러

---
+++ application/controllers/lms/common/SearchCoupon.php
@@ -0,0 +1,51 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class SearchCoupon extends \app\controllers\BaseController
+{
+    protected $models = array('service/couponRegist');
+    protected $helpers = array();
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $this->load->view('common/search_coupon',[
+            'site_code' => $this->_req('site_code')
+            ,'ProdCode' => $this->_req('ProdCode')
+        ]);
+    }
+
+    /**
+     * 쿠폰 목록 조회
+     * @return CI_Output
+     */
+    public function listAjax()
+    {
+        $arr_condition = [
+            'EQ' => ['A.SiteCode' => $this->_reqP('site_code')],
+            'ORG' =>[
+                'LKB' => [
+                    'A.CouponIdx' => $this->_reqP('search_value'),
+                    'A.CouponName' => $this->_reqP('search_value')
+                ]
+            ]
+        ];
+
+        $list = [];
+        $count = $this->couponRegistModel->listCoupon(true,$arr_condition);
+
+        if($count > 0) {
+            $list = $this->couponRegistModel->listCoupon(false, $arr_condition, $this->_reqP('length'), $this->_reqP('start'), ['A.CouponIdx' => 'desc']);
+        }
+
+        return $this->response([
+            'recordsTotal' => $count,
+            'recordsFiltered' => $count,
+            'data' => $list
+        ]);
+    }
+}

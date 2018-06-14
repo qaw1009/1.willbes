95c495453cbf30e9ec70373920442fae13e6f710
최현탁 <cht@willbes.com>
2018-06-14 11:17:41 +0900

    PHPStorm_CI_CC : LiveManagerModel 추가

--- application/third_party/PHPStorm_CI_CC.php
+++ application/third_party/PHPStorm_CI_CC.php
@@ -128,6 +128,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @property PackageUserModel $packageuserModel                               Lecture user package Model Class
  * @property PackageAdminModel $packageadminModel                           Lecture admin package Model Class
  * @property PackagePeriodModel $packageperiodModel                          Lecture period package Model Class
+ * @property LiveManagerModel $liveManagerModel                             Live Video Manager For Lecture Model Class
  ***************** API ***********************
  * @property BaseProductModel $baseProductModel                               Product Base Model Class
  */

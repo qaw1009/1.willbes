7cc4017700890a08e27f800a1c8646f7be435f3f
김건우 <tempus8731@willbes.com>
2018-07-13 13:19:33 +0900

    테스트

--- views/html/list.blade.php
+++ views/html/list.blade.php
@@ -55,7 +55,7 @@
     </div>
     <div class="Depth">
         <img src="{{ img_url('sub/icon_home.gif') }}"> 
-        <span class="1depth"><span class="depth-Arrow">></span><strong>단강좌</strong></span>
+        <span class="1depth"><span class="depth-Arrow">></span><strong>단강좌000</strong></span>
     </div>
     <div class="Content p_re">
         

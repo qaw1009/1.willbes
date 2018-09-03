26ff52fe3b060880f0f1dc89a9ac3cce0b8e4e1a
김건우 <tempus8731@willbes.com>
2018-09-03 09:36:36 +0900

    모바일웹 > js 파일 분리 업로드

---
+++ public/js/willbes/mobile.js
@@ -0,0 +1,141 @@
+// Menu Script
+$(function() {
+    $('.Menu_open').click(function() {
+        $('.dim').show().css('display','block');
+        $('#aside').addClass('on');
+    });
+});
+$(function() {
+    $('.Menu_close').click(function() {
+        $('.dim').hide().css('display','none');
+        $('#aside').removeClass('on');
+    });
+});
+
+// Aside 아코디언 메뉴 Script
+$(function() {
+    $('.ListBox .List').click(function() {
+
+        $('.ListBox .List').removeClass('on')
+
+        if ($(this).next().is(':visible')) {
+            $(this).next().slideUp('normal');
+            $(this).removeClass('on');
+
+        } else {
+            $('.ListBox .List-Depth').slideUp('normal');
+            $(this).next().slideDown('normal');
+            $(this).addClass('on');
+
+        }   
+    });
+});
+
+// 열기, 닫기 버튼 Script
+$(function() {
+    $('.willbes-Txt .MoreBtn a').click(function() {
+        var $txt_info = $(this).parents('.willbes-Txt');
+
+        if ($txt_info.hasClass('on')) {
+            $txt_info.removeClass('on');
+            $(this).text('닫기 ▲');
+
+        } else {
+            $txt_info.addClass('on');
+            $(this).text('열기 ▼');
+        }
+    });
+});
+
+// 내강의실 온라인강좌 버튼 Script
+$(function() {
+    $('.willbes-Open-Table .MoreBtn a').click(function() {
+        var $lec_info_table = $(this).parents('.willbes-Open-Table').find('.openTable');
+
+        if ($lec_info_table.is(':hidden')) {
+            $lec_info_table.show().css('visibility','visible');
+            $('.willbes-Open-Table .MoreBtn a img').attr('src','/public/img/willbes/m/mypage/icon_arrow_off.png');
+        } else {
+            $lec_info_table.hide().css('visibility','hidden');
+            $('.willbes-Open-Table .MoreBtn a img').attr('src','/public/img/willbes/m/mypage/icon_arrow_on.png');
+        }
+    });
+});
+
+// Swiper Script
+$(function() {
+    var swiper = new Swiper('.swiper-container-page', {
+        spaceBetween: 0,
+        centeredSlides: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+    var swiper = new Swiper('.swiper-container-arrow', {
+        spaceBetween: 0,
+        centeredSlides: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        loop: true,
+        loopFillGroupWithBlank: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+    var swiper = new Swiper('.swiper-container-campus', {
+        slidesPerView: 'auto',
+        spaceBetween: 0,
+    });
+    var swiper = new Swiper('.swiper-container-campus-list', {
+        slidesPerView: 'auto',
+        spaceBetween: 0,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
+
+// Slider Swipe Script
+$(function() {
+    $('.swiper-container-campus .swiper-slide a').click(function() {
+        $('.swiper-container-campus .swiper-slide a').removeClass('on');
+        if ($(this).hasClass('on')) {
+            $(this).removeClass('on');
+        } else {
+            $(this).addClass('on');
+        } 
+    });
+    $('.swiper-container-campus-list .swiper-slide a').click(function() {
+        $('.swiper-container-campus-list .swiper-slide a').removeClass('on');
+        if ($(this).hasClass('on')) {
+            $(this).removeClass('on');
+        } else {
+            $(this).addClass('on');
+        } 
+    });
+});
+
+// Top fixed Script
+$(function() {
+    $('*[id*=Fixbtn]:visible').each(function() {
+        $(".goTopbtn").addClass('on');
+    });
+});
+
+// Top Script
+function link_go() {
+    $('html, body').animate({
+        scrollTop: $("#goTop").offset().top
+    }, 200);
+}

cbc2f8e1d246efd14d1ce0aa0311356e3afb079e
조규호 <benoo@willbes.com>
2018-09-03 16:01:20 +0900

    외부접속 (광고,외부배너) 처리 프로세스

---
+++ views/willbes/pc/access/access_process.blade.php
@@ -0,0 +1,16 @@
+@include('willbes.pc.layouts.header')
+<!-- 광고 스크립트 삽입 //-->
+<!-- 광고 스크립트 삽입 //-->
+<body>
+</body>
+<script type="text/javascript">
+    //{{sess_data('gw_idx')}}//
+    $(document).ready(function(){
+        @if(empty($result_msg) !== true)
+        alert('{{$result_msg}}');
+        @endif
+        document.location.replace('{{ empty($return_url) ? site_url('/') : $return_url}}');
+    })
+</script>
+</html>
+

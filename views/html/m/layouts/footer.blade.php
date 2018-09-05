cfe96beb5f98dd1be9a74ab7383f0427baee3961
김건우 <tempus8731@willbes.com>
2018-09-05 10:52:13 +0900

    푸터 개인정보관리책임자 변경 업로드

--- views/html/m/layouts/footer.blade.php
+++ views/html/m/layouts/footer.blade.php
@@ -12,7 +12,7 @@
         원격평생교육시설 신고 제56호 <span class="row-line">|</span> TEL : 1544-5006<br/>
         통신판매업신고 : 제2008-서울관악-0180호 <strong>[정보확인]</strong><br/>
         신고기관명 : 서울특별시 관악구<br/>
-        개인정보관리책임자 : 홍종훈(help@willbes.com)<br/>
+        개인정보관리책임자 : 배성안(help@willbes.com)<br/>
         주소 : 서울시 관악구 호암로 26길 13 세정빌딩 2층 (관악구 대학동 1514-6 ㈜윌비스)<br/>
         Copyright © (주)윌비스. All Right Reserved.
     </address>

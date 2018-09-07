5fd62363b3e128a431a7b809b0f4fa56eb518474
최현탁 cht@willbes.com <cht@willbes.com>
2018-09-07 17:24:33 +0900

    수강후기 파일명 수정

--- application/controllers/willbes/site/StudyComment.php
+++ application/controllers/willbes/site/StudyComment.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class StudyComment extends \app\controllers\FrontController
 {
-    protected $models = array('support/supportBoardTwoWayF');
+    protected $models = array('product/baseProductF', 'support/supportBoardTwoWayF');
     protected $helpers = array();
     protected $auth_controller = false;
     protected $auth_methods = array();
@@ -21,10 +21,22 @@ class StudyComment extends \app\controllers\FrontController
 
     public function index()
     {
+        echo 'a = '.config_app('IsPassSite').'<br>';
+
         $arr_input = array_merge($this->_reqG(null));
 
-        $this->load->view('site/professor/study_comment_list', [
-            'arr_input' => $arr_input
+        if ($this->_site_code == '2004') {
+            // 공무원일 경우 카테고별 직렬, 직렬별 과목 조회
+            $arr_base['series'] = $this->baseProductFModel->listSeriesCategoryMapping($this->_site_code, $this->_cate_code);
+            $arr_base['subject'] = $this->baseProductFModel->listSubjectSeriesMapping($this->_site_code, $this->_cate_code, element('series_ccd', $arr_input));
+        } else {
+            // 카테고리별 과목 조회
+            $arr_base['subject'] = $this->baseProductFModel->listSubjectCategoryMapping($this->_site_code, $this->_cate_code);
+        }
+
+        $this->load->view('site/professor/study_comment_index', [
+            'arr_input' => $arr_input,
+            'arr_base' => $arr_base
         ]);
     }
 }

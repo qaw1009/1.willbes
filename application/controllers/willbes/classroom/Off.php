405aa1a14121819f806a1a6d3d622efe0dcf4c91
하준걸 <hereisme@willbes.com>
2018-09-11 11:17:18 +0900

    대소문자

--- application/controllers/willbes/classroom/Off.php
+++ application/controllers/willbes/classroom/Off.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Offline extends \app\controllers\FrontController
+class Off extends \app\controllers\FrontController
 {
     protected $models = array();
     protected $helpers = array();
--- application/controllers/willbes/classroom/On.php
+++ application/controllers/willbes/classroom/On.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Online extends \app\controllers\FrontController
+class On extends \app\controllers\FrontController
 {
     protected $models = array();
     protected $helpers = array();

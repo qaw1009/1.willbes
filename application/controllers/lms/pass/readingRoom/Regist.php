0bb4a040209dbd6851107e0d32e000fa776193d5
최현탁 cht@willbes.com <cht@willbes.com>
2018-09-14 18:46:52 +0900

    독서실 등록 개발

--- application/controllers/lms/pass/readingRoom/Regist.php
+++ application/controllers/lms/pass/readingRoom/Regist.php
@@ -3,8 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Regist extends \app\controllers\BaseController
 {
-    protected $models = array('sys/site');
+    protected $models = array('sys/site', 'pass/readingRoom');
     protected $helpers = array();
+    protected $mang_type;
 
     public function __construct()
     {
@@ -12,20 +13,24 @@ class Regist extends \app\controllers\BaseController
     }
 
     /**
-     * 독서실등록/좌석현황 인덱스
+     * 독서실,사물함 등록/좌석현황 인덱스
      */
     public function index()
     {
+        $this->mang_type = $this->_req('mang_type');
+
         //캠퍼스 조회
         $arr_campus = $this->siteModel->getSiteCampusArray('');
 
         $this->load->view("pass/reading_room/regist/index", [
+            'mang_title' => $this->readingRoomModel->arr_mang_title[$this->mang_type],
+            'default_query_string' => '&mang_type='.$this->mang_type,
             'arr_campus' => $arr_campus
         ]);
     }
 
     /**
-     * 독서실등록/좌석현황 목록 조회
+     * 독서실,사물함 등록/좌석현황 목록 조회
      * @return CI_Output
      */
     public function listAjax()
@@ -59,11 +64,13 @@ class Regist extends \app\controllers\BaseController
     }
 
     /**
-     * 독서실등록/좌석현황 등록/수정 폼
+     * 독서실,사물함 등록/좌석현황 등록/수정 폼
      * @param array $params
      */
     public function create($params = [])
     {
+        $this->mang_type = $this->_req('mang_type');
+
         $method = 'POST';
         $data = null;
 
@@ -79,6 +86,9 @@ class Regist extends \app\controllers\BaseController
 
         $this->load->view("pass/reading_room/regist/create", [
             'method' => $method,
+            'mang_type' => $this->mang_type,
+            'mang_title' => $this->readingRoomModel->arr_mang_title[$this->mang_type],
+            'default_query_string' => '&mang_type='.$this->mang_type,
             'arr_campus' => $arr_campus,
             'site_csTel' => $site_csTel,
             'data' => $data
@@ -86,11 +96,43 @@ class Regist extends \app\controllers\BaseController
     }
 
     /**
-     * 독서실등록/좌석현황 등록/수정
+     * 독서실,사물함 등록/좌석현황 등록/수정
      */
     public function store()
     {
-        
+        $method = 'add';
+        $idx = '';
+        $this->mang_type = $this->_req('mang_type');
+
+        $rules = [
+            ['field' => 'mang_type', 'label' => $this->readingRoomModel->arr_mang_title[$this->mang_type].'타입', 'rules' => 'trim|required|in_list[R,L]'],
+            ['field' => 'site_code', 'label' => '운영사이트', 'rules' => 'trim|required'],
+            ['field' => 'is_use', 'label' => '사용여부', 'rules' => 'trim|required|in_list[Y,N]'],
+            ['field' => 'rd_name', 'label' => $this->readingRoomModel->arr_mang_title[$this->mang_type].'명', 'rules' => 'trim|required|max_length[50]'],
+            ['field' => 'lake_layer', 'label' => '강의실', 'rules' => 'trim|required|integer'],
+            ['field' => 'use_start_date', 'label' => '사용기간시작일자', 'rules' => 'trim|required'],
+            ['field' => 'use_end_date', 'label' => '사용기간종료일자', 'rules' => 'trim|required'],
+            ['field' => 'use_qty', 'label' => $this->readingRoomModel->arr_mang_title[$this->mang_type].'총좌석수', 'rules' => 'trim|required|integer'],
+            ['field' => 'transverse_num', 'label' => '가로수', 'rules' => 'trim|required|integer'],
+            ['field' => 'start_no', 'label' => '좌석시작번호', 'rules' => 'trim|required|integer'],
+            ['field' => 'end_no', 'label' => '좌석종료번호', 'rules' => 'trim|required|integer'],
+            ['field' => 'sale_price', 'label' => '정상가', 'rules' => 'trim|required|integer'],
+            ['field' => 'sale_rate', 'label' => '할인율', 'rules' => 'trim|required|integer'],
+            ['field' => 'real_sale_price', 'label' => '판매가', 'rules' => 'trim|required|integer']
+        ];
+
+        if ($this->validate($rules) === false) {
+            return;
+        }
+
+        if (empty($this->_reqP('idx')) === false) {
+            $method = 'modify';
+            $idx = $this->_reqP('idx');
+        }
+
+        //_addBoard, _modifyBoard
+        $result = $this->readingRoomModel->{$method . 'ReadingRoom'}($this->_reqP(null, false), $idx);
+        $this->json_result($result, '저장 되었습니다.', $result);
     }
 
     /**

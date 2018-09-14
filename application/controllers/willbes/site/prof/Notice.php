198f2aa0a854b0da527c092b3c0aa4fcaaafde72
최현탁 cht@willbes.com <cht@willbes.com>
2018-09-14 17:18:22 +0900

    대소문자 수정

--- application/controllers/willbes/site/prof/Notice.php
+++ application/controllers/willbes/site/prof/Notice.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-require_once APPPATH . 'controllers/willbes/share/support/supportnotice.php';
+require_once APPPATH . 'controllers/willbes/share/support/SupportNotice.php';
 
 class Notice extends SupportNotice
 {
--- application/controllers/willbes/site/prof/Qna.php
+++ application/controllers/willbes/site/prof/Qna.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-require_once APPPATH . 'controllers/willbes/share/support/supportqna.php';
+require_once APPPATH . 'controllers/willbes/share/support/SupportQna.php';
 
 class Qna extends SupportQna
 {

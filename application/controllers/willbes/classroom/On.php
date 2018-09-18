6eab37efb365df66a2bb2a40241dfd0b64d2b493
하준걸 <hereisme@willbes.com>
2018-09-18 09:31:05 +0900

    인증메일 경로 수정

--- application/controllers/willbes/classroom/On.php
+++ application/controllers/willbes/classroom/On.php
@@ -154,7 +154,7 @@ class On extends \app\controllers\FrontController
             ],
             'ORG' => [
                 'LKB' => [
-                    'ProdName' => $this->_req('search_text'), // 강의명 검색 (사용자패키지일때 패키지명)
+                    'ProdName' => $this->_req('search_text'), // 강의명 검색 (패키지명)
                     'subProdName' => $this->_req('search_text') // 강의명 검색 (실제 강좌명)
                 ]
             ]
@@ -162,36 +162,47 @@ class On extends \app\controllers\FrontController
 
         // 학습형태 : 단광좌, 사용자패키지
         // 결제방식 : 온라인결제, 학원방문결제
-        $leclist = $this->classroomFModel->getLecture(array_merge($cond_arr, ['IN' => [
-            'LearnPatternCcd' => ['615001','615002'], // 단과, 사용자
-            'PayRouteCcd' => ['670001','670002'] // 온, 방
-        ]]));
+        $leclist = $this->classroomFModel->getLecture(array_merge($cond_arr, [
+            'IN' => [
+                'LearnPatternCcd' => ['615001','615002'], // 단과, 사용자
+                'PayRouteCcd' => ['670001','670002'] // 온, 방
+            ]
+        ]));
 
         // 학습형태 : 관리자패키지
         // 결제방식 : 온라인결제, 학원방문결제
-        $pkglist = $this->classroomFModel->getPackage(array_merge($cond_arr, ['IN' => [
-            'LearnPatternCcd' => ['615003'], // 패키지
-            'PayRouteCcd' => ['670001','670002'] // 온, 방
-        ]]));
+        $pkglist = $this->classroomFModel->getPackage(array_merge($cond_arr, [
+            'IN' => [
+                'LearnPatternCcd' => ['615003'], // 패키지
+                'PayRouteCcd' => ['670001','670002'] // 온, 방
+            ]
+        ]));
 
         // 학습형태 : 무료강좌
         // 결제방식 : 모두
-        $freelist = $this->classroomFModel->getLecture(array_merge($cond_arr, ['IN' => [
-            'LearnPatternCcd' => ['615005'], // 무료
-        ]]));
+        $freelist = $this->classroomFModel->getLecture(array_merge($cond_arr, [
+            'IN' => [
+                'LearnPatternCcd' => ['615005'] // 무료
+            ]
+        ]));
 
         // 학습형태 : 단과반
         // 결제방식 : 0월결제, 무료결제\, 제휴사 결제
-        $adminlistLec = $this->classroomFModel->getLecture(array_merge($cond_arr, ['IN' => [
-            'LearnPatternCcd' => ['615001','615002'], // 단과, 사용자
-            'PayRouteCcd' => ['670003','670004','670005'] // 0원, 무료, 제휴사
-        ]]));
+        $adminlistLec = $this->classroomFModel->getLecture(array_merge($cond_arr, [
+            'IN' => [
+                'LearnPatternCcd' => ['615001','615002'], // 단과, 사용자
+                'PayRouteCcd' => ['670003','670004','670005'] // 0원, 무료, 제휴사
+            ]
+        ]));
+
         // 학습형태 : 관리자패키지
         // 결제방식 : 0월결제, 무료결제\, 제휴사 결제
-        $adminlistPkg = $this->classroomFModel->getPackage(array_merge($cond_arr, ['IN' => [
-            'LearnPatternCcd' => ['615002'], // 패키지
-            'PayRouteCcd' => ['670003','670004','670005'] // 0원, 무료, 제휴사
-        ]]));
+        $adminlistPkg = $this->classroomFModel->getPackage(array_merge($cond_arr, [
+            'IN' => [
+                'LearnPatternCcd' => ['615002'], // 패키지
+                'PayRouteCcd' => ['670003','670004','670005'] // 0원, 무료, 제휴사
+            ]
+        ]));
 
         // 관리자부여 : lec + pkg
         $adminlist = [ 'lec' => $adminlistLec, 'pkg' => $adminlistPkg ];
@@ -268,11 +279,7 @@ class On extends \app\controllers\FrontController
             'PayRouteCcd' => ['670001','670002'] // 온, 방
         ]]));
 
-
-
-
-
-        $this->load->view('/classroom/on_pause', [
+        return $this->load->view('/classroom/on_pause', [
             'course_arr' => $course_arr,
             'subject_arr' => $subject_arr,
             'prof_arr' => $prof_arr,
@@ -291,8 +298,8 @@ class On extends \app\controllers\FrontController
 
         // 기본 검색옵션 시작일이 오늘 보다 크면 수강대기강의
         $cond_arr = [
-            'GT' => [
-                'LecStartDate' => date("Y-m-d", time())
+            'LT' => [
+                'RealLecEndDate' => date("Y-m-d", time()) // 종료날짜 < 오늘
             ],
             'IN' => [
                 'LearnPatternCcd' => ['615001','615002','615003','615005'],
@@ -305,28 +312,56 @@ class On extends \app\controllers\FrontController
         $subject_arr = $this->classroomFModel->getSubjectList( $cond_arr);
         $prof_arr = $this->classroomFModel->getProfList($cond_arr);
 
+        // 실제 리스트용
+        $cond_arr = [
+            'EQ' => [
+                'MemIdx' => $this->session->userdata('mem_idx'), // 사용자번호
+                'SubjectIdx' => $this->_req('subject_ccd'), // 검색 : 과목
+                'wProfIdx' => $this->_req('prof_ccd'), // 검색 : 강사
+                'CourseIdx' => $this->_req('course_ccd') // 검색 : 과정
+            ],
+            'LT' => [
+                'RealLecEndDate' => date("Y-m-d", time()), // 종료일 < 오늘
+            ],
+            'ORG' => [
+                'LKB' => [
+                    'ProdName' => $this->_req('search_text'), // 강의명 검색 (사용자패키지일때 패키지명)
+                    'subProdName' => $this->_req('search_text') // 강의명 검색 (실제 강좌명)
+                ]
+            ]
+        ];
 
+        // 학습형태 : 단광좌, 사용자패키지
+        // 결제방식 : 온라인결제, 학원방문결제
+        $leclist = $this->classroomFModel->getLecture(array_merge($cond_arr, ['IN' => [
+            'LearnPatternCcd' => ['615001','615002'], // 단과, 사용자
+            'PayRouteCcd' => ['670001','670002'] // 온, 방
+        ]]));
 
-
-
-
-        $this->load->view('/classroom/on_end', [
+        return $this->load->view('/classroom/on_end', [
             'course_arr' => $course_arr,
             'subject_arr' => $subject_arr,
             'prof_arr' => $prof_arr,
             'input_arr' => $input_arr,
-            'lecList' => [],
+            'lecList' => $leclist,
             'pkgList' => []
         ]);
     }
 
     /**
-     * 실세 강의 상세페이지
-     * @param array $params
+     * 강의 상세페이지
+     * @return object|string
      */
     public function view()
     {
-        $this->load->view('/classroom/on_view');
+        // 강좌정보 읽어오기
+
+        // 커리큘럼 읽어오기
+
+        return $this->load->view('/classroom/on_view', [
+            'lecinfo' => [],
+            'curriculum' => []
+        ]);
     }
 
 }
--- views/willbes/pc/auth/_certMail_template.blade.php
+++ views/willbes/pc/auth/_certMail_template.blade.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 </head>
 <body>
-<a href="{{ app_url('/member/mailAuth', 'www') }}/{{$CertKey}}/{{$CertMail}}/Auth">여기를 눌러서 인증하세요</a>
+<a href="{{ app_url('/member/mailAuth/index', 'www') }}/{{$CertKey}}/{{$CertMail}}/Auth">여기를 눌러서 인증하세요</a>
 위버튼이 작동안하면 아래 경로를 복사해서 주소 창에 입력해주십시요
-경로 : {{ app_url('/member/mailAuth', 'www') }}/{{$CertKey}}/{{$CertMail}}/Auth
+경로 : {{ app_url('/member/mailAuth/index', 'www') }}/{{$CertKey}}/{{$CertMail}}/Auth
 </body>
 </html>

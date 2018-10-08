e5bf1835b14bcdc285ac4d82d6860e7eb1e25d0c
최현탁 cht@willbes.com <cht@willbes.com>
2018-10-08 17:47:22 +0900

    프론트 수험정보 메뉴 include 경로 수정

--- views/willbes/pc/support/examAnnouncement.blade.php
+++ views/willbes/pc/support/examAnnouncement.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <!-- Container -->
 <div id="Container" class="subContainer widthAuto c_both">
-    @include('willbes.pc.layouts.partial.site_tab_menu')
+    @include('willbes.pc.layouts.partial.site_menu')
     <div class="Depth">
         @include('willbes.pc.layouts.partial.site_route_path')
     </div>

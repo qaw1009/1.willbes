d7ef1b6cc45cd2b3dbdc844f63f03bd6a5996e7e
신봉석 <bsshin@willbes.com>
2018-10-17 13:15:54 +0900

    파일업로드 기본 디렉토리 지정 관련 수정

--- application/config/lms_config.php
+++ application/config/lms_config.php
@@ -1,6 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+// 업로드 하위 디렉토리
+$config['upload_prefix_dir'] = 'willbes';
+
 $config['lms'] = array(
     'site_name' => '학습 콘텐츠 통합관리 시스템 (LMS)',
     'head_title' => '미래를 준비는 사람들! : willbes.net',

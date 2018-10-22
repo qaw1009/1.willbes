a9ba2a5e2e92734f2e0929dfed05d90f2743967c
하준걸 <hereisme@willbes.com>
2018-10-22 15:40:39 +0900

    강의창 함수 util.js 로 이동

--- public/js/util.js
+++ public/js/util.js
@@ -1,4 +1,37 @@
 /**
+ * 샘플강좌 플레이어
+ * @param $prodCode
+ * @param $unitIdx
+ * @param $quility
+ */
+function fnPlayerSample($prodCode, $unitIdx, $quility){
+    popupOpen(app_url('/Player/Sample/'+$prodCode+'/'+$unitIdx+'/'+$quility, 'www'), 'samplePlayer', '1000', '600', null, null);
+}
+
+/**
+ * 강사 소개 플레이어
+ * @param $ProfessorCode
+ * @param $viewType ( OT | WS | S1 | S2 | S3 )
+ */
+function fnPlayerProf($ProfessorCode, $viewType){
+    popupOpen(app_url('/Player/Professor/'+$ProfessorCode+'/'+$viewType+"/_", 'www'), 'profPlayer', '1000', '600', null, null);
+}
+
+/**
+ * 일반강좌 플레이어
+ */
+function fnPlayer($OrderIdx, $ProdCode, $subProdCode, $lecIdx, $unitIdx, $quility){
+    popupOpen(app_url('/Player/?o='+$OrderIdx+'&p='+$ProdCode+'&sp='+$subProdCode+'&l='+$lecIdx+'&u='+$unitIdx+'&q='+$quility, 'www'), 'samplePlayer', '1000', '600', null, null);
+}
+
+/**
+ * 모바일웹 모바일 플레이어
+ */
+function mobilePlayer(){
+
+}
+
+/**
  * 만나이
  * @param birth YYYYMMDD || YYYY-MM-DD
  * @returns {number}

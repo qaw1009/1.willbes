9f6f95e5e688a117bf001be35a05608d17c2a73b
최현탁 cht@willbes.com <cht@willbes.com>
2018-11-07 16:37:53 +0900

    프론트 교수소개페이지 수정 (게시판 사용 유무 조건 추가)

--- views/willbes/pc/site/professor/show.blade.php
+++ views/willbes/pc/site/professor/show.blade.php
@@ -149,9 +149,9 @@
                 <a name="tabLink"></a>
                 <ul class="tabWrap tabDepthProf">
                     <li><a href="#none" id="hover_open_lecture" onclick="goUrl('tab', 'open_lecture');">개설강좌</a></li>
-                    <li><a href="#none" id="hover_notice" onclick="goUrl('tab', 'notice');">공지사항</a></li>
-                    <li><a href="#none" id="hover_qna" onclick="goUrl('tab', 'qna');">학습Q&A</a></li>
-                    <li><a href="#none" id="hover_material" onclick="goUrl('tab', 'material');">학습자료실</a></li>
+                    @if($data['IsNoticeBoard'] == 'Y')<li><a href="#none" id="hover_notice" onclick="goUrl('tab', 'notice');">공지사항</a></li>@endif
+                    @if($data['IsQnaBoard'] == 'Y')<li><a href="#none" id="hover_qna" onclick="goUrl('tab', 'qna');">학습Q&A</a></li>@endif
+                    @if($data['IsDataBoard'] == 'Y')<li><a href="#none" id="hover_material" onclick="goUrl('tab', 'material');">학습자료실</a></li>@endif
                     <li><a href="#none" id="hover_free_lecture" onclick="goUrl('tab', 'free_lecture');">무료강좌</a></li>
                 </ul>
                 <div class="tabBox">

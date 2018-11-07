6d09910652d635adac57fd2396304179b31493ee
김건우 <tempus8731@willbes.com>
2018-11-07 09:15:01 +0900

    모바일웹 이미지 선명하게 처리3

--- views/html/m/layouts/header.blade.php
+++ views/html/m/layouts/header.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="target-densitydpi=device-dpi, user-scalable=0, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, width=device-width" />
-    ]
+    
     <title>모바일 윌비스 통합 사이트</title>
 
     <!-- CSS -->

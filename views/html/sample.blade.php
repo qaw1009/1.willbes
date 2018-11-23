383ad296917ebfbfe315216cc841c696d8c89891
김건우 <tempus8731@willbes.com>
2018-11-23 17:46:37 +0900

    프론트 t-pass 첨삭게시판 작업페이지 업로드2

--- views/html/sample.blade.php
+++ views/html/sample.blade.php
@@ -23,6 +23,8 @@
 
     <a href="{{ site_url('/home/html/profsub') }}" target="_blank">교수진소개 > 교수진 상세 page 이동</a><br/><br/>
 
+    <a href="{{ site_url('/home/html/profsub_tpass') }}" target="_blank">교수진소개 > 교수진 상세 T-pass page 이동</a><br/><br/>
+
     <a href="{{ site_url('/home/html/package1') }}" target="_blank">패키지 > 추천패키지 수강신청 page 이동</a><br/><br/>
 
     <a href="{{ site_url('/home/html/packagesub1') }}" target="_blank">패키지 > 추천패키지 수강신청 상세 page 이동</a><br/><br/>
@@ -130,6 +132,8 @@
     <a href="{{ site_url('/home/html/mypage_pass1') }}" target="_blank">내강의실 > 무한PASS존 page 이동</a><br/><br/>
 
     <a href="{{ site_url('/home/html/mypage_pass2') }}" target="_blank">내강의실 > 무한PASS존 > 수강중인 강좌 page 이동</a><br/><br/>
+
+    <a href="{{ site_url('/home/html/mypage_edit1') }}" target="_blank">내강의실 > 온라인강좌 > 첨삭강좌 page 이동</a><br/><br/>
 
     <a href="{{ site_url('/home/html/mypage_online1') }}" target="_blank">내강의실 > 온라인강좌 > 수강대기강좌 page 이동</a><br/><br/>
 

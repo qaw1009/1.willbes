90b0b1f8e45b9643f328a5f1f88cf9ea5f31bed7
최현탁 cht@willbes.com <cht@willbes.com>
2018-12-07 16:44:29 +0900

    프론트 첨삭게시판 테스트소스  제거

--- views/willbes/pc/classroom/assignment/create.blade.php
+++ views/willbes/pc/classroom/assignment/create.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 <div class="willbes-Layer-PassBox willbes-Layer-PassBox1100 h920 fix" style="display: block">
 <form id="regi_form" name="regi_form" method="POST" enctype="multipart/form-data" onsubmit="return false;" novalidate>
-{{--<form id="regi_form" name="regi_form" method="POST" enctype="multipart/form-data" action="{{front_url('/classroom/assignment/store')}}" novalidate>--}}
     {!! csrf_field() !!}
     {!! method_field($method) !!}
     <input type="hidden" name="idx" value="{{ $board_idx }}"/>
@@ -110,10 +109,10 @@
                     </table>
                 </div>
                 <div class="search-Btn mt20 h36 p_re">
-                    <button type="button" {{--onclick="javascript:save('{{$arr_save_type_ccd[0]}}');"--}} class="btnAuto90 h36 mem-Btn bg-white bd-dark-gray f_left btn_save" data-save-type="{{$arr_save_type_ccd[0]}}">
+                    <button type="button" class="btnAuto90 h36 mem-Btn bg-white bd-dark-gray f_left btn_save" data-save-type="{{$arr_save_type_ccd[0]}}">
                         <span class="tx-purple-gray">임시저장</span>
                     </button>
-                    <button type="button" {{--onclick="javascript:save('{{$arr_save_type_ccd[1]}}');"--}} class="btnAuto90 h36 mem-Btn bg-blue bd-dark-blue center btn_save" data-save-type="{{$arr_save_type_ccd[1]}}">
+                    <button type="button" class="btnAuto90 h36 mem-Btn bg-blue bd-dark-blue center btn_save" data-save-type="{{$arr_save_type_ccd[1]}}">
                         <span>제출하기</span>
                     </button>
                 </div>

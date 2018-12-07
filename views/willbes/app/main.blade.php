dab36da00244e4284e9d2a1d89351eded1877267
하준걸 <hereisme@willbes.com>
2018-12-07 16:40:19 +0900

    브라우저확인

--- views/willbes/app/main.blade.php
+++ views/willbes/app/main.blade.php
@@ -4,7 +4,8 @@
     <!-- Container -->
     <div id="Container" class="Container NG c_both">
         윌비스 하이브리드앱 메인페이지입니다.
-
+        <br>
+        {{$_SERVER['HTTP_USER_AGENT']}}
     </div>
     <!-- End Container -->
 @stop

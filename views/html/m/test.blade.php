24b8aa6cbfc9d58b28385868f271b2586ad4e39f
김건우 <tempus8731@willbes.com>
2018-12-13 10:21:13 +0900

    모바일웹 이미지 테스트3

--- views/html/m/test.blade.php
+++ views/html/m/test.blade.php
@@ -3,6 +3,18 @@
 @section('content')
 <!-- Container -->
 <div id="Container" class="Container NG c_both mb20">
+
+<style>
+    ul li {float:left; width:100px;}
+</style>
+<ul>
+    <li><img style="width: 100%" src="{{ img_url('gosi/guide/logo_gif.gif') }}"><div>gif</div></li>
+    <li><img style="width: 100%" src="{{ img_url('gosi/guide/logo_png.png') }}"><div>png</div></li>
+    <li><img style="width: 100%" src="{{ img_url('gosi/guide/logo_100.jpg') }}"><div>100</div></li>
+    <li><img style="width: 100%" src="{{ img_url('gosi/guide/logo_80.jpg') }}"><div>80</div></li>
+    <li><img style="width: 100%" src="{{ img_url('gosi/guide/logo_70.jpg') }}"><div>70</div></li>
+</ul>
+
     <img style="width: 100%" src="{{ img_url('gosi/guide/guide_public_c3.jpg') }}"><br/><br/>
 
     <img style="width: 100%" src="{{ img_url('gosi/guide/guide_public_c3_100.jpg') }}"><br/><br/>

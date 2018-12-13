9764a4740ecb4b550d87ad261428d9c24aa53293
김건우 <tempus8731@willbes.com>
2018-12-13 16:56:20 +0900

    모바일웹 이미지 테스트5

--- views/html/m/test.blade.php
+++ views/html/m/test.blade.php
@@ -3,34 +3,26 @@
 @section('content')
 <!-- Container -->
 <div id="Container" class="Container NG c_both mb20">
+    <style>
+        ul li {float:left;}
+    </style>
+    <ul>
+        <li><img style="width: 100%" src="{{ img_url('gosi/guide/logo_gif.gif') }}"><div>gif</div></li>
+        <li><img style="width: 100%" src="{{ img_url('gosi/guide/logo_png.png') }}"><div>png</div></li>
+        <li><img style="width: 100%" src="{{ img_url('gosi/guide/logo_100.jpg') }}"><div>100</div></li>
+        <li><img style="width: 100%" src="{{ img_url('gosi/guide/logo_80.jpg') }}"><div>80</div></li>
+        <li><img style="width: 100%" src="{{ img_url('gosi/guide/logo_70.jpg') }}"><div>70</div></li>
+    </ul>
 
-<style>
-    ul li {float:left;}
-</style>
-<ul>
-    <li><img style="width: 100%" src="{{ img_url('gosi/guide/logo_gif.gif') }}"><div>gif</div></li>
-    <li><img style="width: 100%" src="{{ img_url('gosi/guide/logo_png.png') }}"><div>png</div></li>
-    <li><img style="width: 100%" src="{{ img_url('gosi/guide/logo_100.jpg') }}"><div>100</div></li>
-    <li><img style="width: 100%" src="{{ img_url('gosi/guide/logo_80.jpg') }}"><div>80</div></li>
-    <li><img style="width: 100%" src="{{ img_url('gosi/guide/logo_70.jpg') }}"><div>70</div></li>
-</ul>
-<br/><br/><br/><br/>
-<ul>
-    <li><img style="width: 100%" src="{{ img_url('gosi/guide/icon_myacad_gif.gif') }}"><div>gif</div></li>
-    <li><img style="width: 100%" src="{{ img_url('gosi/guide/icon_myacad_png.png') }}"><div>png</div></li>
-    <li><img style="width: 100%" src="{{ img_url('gosi/guide/icon_myacad_100.jpg') }}"><div>100</div></li>
-    <li><img style="width: 100%" src="{{ img_url('gosi/guide/icon_myacad_80.jpg') }}"><div>80</div></li>
-    <li><img style="width: 100%" src="{{ img_url('gosi/guide/icon_myacad_70.jpg') }}"><div>70</div></li>
-    <li><img style="width: 100%" src="{{ img_url('gosi/guide/icon_myacad_png_big.png') }}"><div>png_big</div></li>
-</ul>
+    <br/><br/><div style="clear:both"></div><br/><br/>
 
-    <img style="width: 100%" src="{{ img_url('gosi/guide/guide_public_c3.jpg') }}"><br/><br/>
-
-    <img style="width: 100%" src="{{ img_url('gosi/guide/guide_public_c3_100.jpg') }}"><br/><br/>
-
-    <img style="width: 100%" src="{{ img_url('gosi/guide/guide_public_c3_gif.gif') }}"><br/><br/>
-
-    <img style="width: 100%" src="{{ img_url('gosi/guide/guide_public_c3_png.png') }}">
+    <ul>
+        <li style="width: 230px"><img style="width: 100%" src="{{ img_url('gosi/guide/icon_menu.png') }}"><div>png230</div></li>
+        <li style="width: 150px"><img style="width: 100%" src="{{ img_url('gosi/guide/icon_menu.png') }}"><div>png150</div></li>
+        <li style="width: 100px"><img style="width: 100%" src="{{ img_url('gosi/guide/icon_menu.png') }}"><div>png100</div></li>
+        <li style="width: 50px"><img style="width: 100%" src="{{ img_url('gosi/guide/icon_menu.png') }}"><div>png50</div></li>
+        <li style="width: 25px"><img style="width: 100%" src="{{ img_url('gosi/guide/icon_menu.png') }}"><div>png25</div></li>
+    </ul>
 </div>
 <!-- End Container -->
 @stop

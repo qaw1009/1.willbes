f3725fb7575ded0435bcd73b61b3585a9cd55849
하준걸 <hereisme@willbes.com>
2018-12-13 10:29:53 +0900

    앱테스트

--- views/willbes/app/member/login/app.blade.php
+++ views/willbes/app/member/login/app.blade.php
@@ -1,14 +1,43 @@
 @extends('willbes.app.layouts.master')
 
 @section('content')
+    <div id="Container" class="memContainer widthAuto c_both">
+
+        <div class="Member mem-Login widthAuto460">
+
+            <button type="button" onclick="getDeviceInfo()" class="mem-Btn bg-blue bd-dark-blue">
+                <span>디바이스정보</span>
+            </button>
+            <br><br>
+            <button type="button" onclick="getToken()" class="mem-Btn bg-blue bd-dark-blue">
+                <span>겟토큰</span>
+            </button>
+        </div>
+    </div>
     <script type="text/javascript">
         var app = null;
-        $(document).ready(function() {
+
             app = new StarPlayerBridge();
+            alert('OK');
             app.login('{{$token}}');
+            alert('setlogin');
             location.replace('{{(empty($rtnUrl) == true) ? '/app/classroom/on/list/ongogin/' : $rtnUrl }}');
-        });
+
+
+        function getDeviceInfo() {
+            app.getDeviceInfo(function(id, name, model){
+                alert("id : "+id+" name : "+name+" model : "+model);
+            });
+        }
+
+        function getToken() {
+            app.getToken(function(token) {
+                alert(token);
+            });
+        }
+
     </script>
-    로그인 완료페이지...
+
+    로그인 완료페이지...<br>
     {{$token}}
 @stop

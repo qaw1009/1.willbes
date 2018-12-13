6262c4a11f3a7914daae44ca4ea9f571ef11753a
김건우 <tempus8731@willbes.com>
2018-12-13 10:32:36 +0900

    모바일웹 이미지 테스트4

--- views/html/m/test.blade.php
+++ views/html/m/test.blade.php
@@ -5,7 +5,7 @@
 <div id="Container" class="Container NG c_both mb20">
 
 <style>
-    ul li {float:left; width:100px;}
+    ul li {float:left;}
 </style>
 <ul>
     <li><img style="width: 100%" src="{{ img_url('gosi/guide/logo_gif.gif') }}"><div>gif</div></li>
@@ -13,6 +13,15 @@
     <li><img style="width: 100%" src="{{ img_url('gosi/guide/logo_100.jpg') }}"><div>100</div></li>
     <li><img style="width: 100%" src="{{ img_url('gosi/guide/logo_80.jpg') }}"><div>80</div></li>
     <li><img style="width: 100%" src="{{ img_url('gosi/guide/logo_70.jpg') }}"><div>70</div></li>
+</ul>
+<br/><br/><br/><br/>
+<ul>
+    <li><img style="width: 100%" src="{{ img_url('gosi/guide/icon_myacad_gif.gif') }}"><div>gif</div></li>
+    <li><img style="width: 100%" src="{{ img_url('gosi/guide/icon_myacad_png.png') }}"><div>png</div></li>
+    <li><img style="width: 100%" src="{{ img_url('gosi/guide/icon_myacad_100.jpg') }}"><div>100</div></li>
+    <li><img style="width: 100%" src="{{ img_url('gosi/guide/icon_myacad_80.jpg') }}"><div>80</div></li>
+    <li><img style="width: 100%" src="{{ img_url('gosi/guide/icon_myacad_70.jpg') }}"><div>70</div></li>
+    <li><img style="width: 100%" src="{{ img_url('gosi/guide/icon_myacad_png_big.png') }}"><div>png_big</div></li>
 </ul>
 
     <img style="width: 100%" src="{{ img_url('gosi/guide/guide_public_c3.jpg') }}"><br/><br/>

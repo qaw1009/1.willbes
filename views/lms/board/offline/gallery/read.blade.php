29e1c00138bc2607f9e08dab5f7feeae59cb8c6e
최현탁 cht@willbes.com <cht@willbes.com>
2018-12-19 09:21:30 +0900

    lms 갤러리게시판 수정

--- views/lms/board/offline/gallery/read.blade.php
+++ views/lms/board/offline/gallery/read.blade.php
@@ -48,14 +48,12 @@
 
             <div class="form-group">
                 <label class="control-label col-md-1-1" for="">첨부</label>
-                <div class="col-md-4">
+                <div class="col-md-4 form-control-static">
                     @for($i = 0; $i < $attach_file_cnt; $i++)
                         @if(empty($data['arr_attach_file_path'][$i]) === false)
-                            <p class="form-control-static">
-                                [ <a href="javascript:void(0);" class="file-download" data-file-path="{{ urlencode($data['arr_attach_file_path'][$i].$data['arr_attach_file_name'][$i])}}" data-file-name="{{ urlencode($data['arr_attach_file_real_name'][$i]) }}" target="_blank">
-                                    {{ $data['arr_attach_file_real_name'][$i] }}
-                                </a> ]
-                            </p>
+                            <a href="javascript:void(0);" class="file-download ml-5" data-file-path="{{ urlencode($data['arr_attach_file_path'][$i].$data['arr_attach_file_name'][$i])}}" data-file-name="{{ urlencode($data['arr_attach_file_real_name'][$i]) }}" target="_blank">
+                                [{{ $data['arr_attach_file_real_name'][$i] }}]
+                            </a>
                         @endif
                     @endfor
                 </div>

be60c196654f27f4931c1992db9ea0d381e587e4
eddie <eddie@willbes.com>
2019-01-08 10:43:16 +0900

    lms_Mock_Log 테이블 RemainSec(남은시간초단위) 컬럼추가

---
+++ application/controllers/lms/mocktest/StatisticsPrivate.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 윌비스
+ * Date: 2018-12-31
+ * Time: 오후 3:51
+ */
+
+class StatisticsPrivate
+{
+
+}
---
+++ application/controllers/willbes/classroom/MockExam.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 윌비스
+ * Date: 2019-01-04
+ * Time: 오후 4:54
+ */

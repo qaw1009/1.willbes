8742e9d475d878068187781caf264fc89d546220
신봉석 <bsshin@willbes.com>
2019-01-10 20:37:23 +0900

    lms > 과목, 과정 정렬변경 > 파라미터 체크 추가

--- views/lms/product/base/professor/index.blade.php
+++ views/lms/product/base/professor/index.blade.php
@@ -78,7 +78,7 @@
             $datatable = $list_table.DataTable({
                 serverSide: true,
                 buttons: [
-                    { text: '<i class="fa fa-sort-numeric-asc mr-5"></i> 정렬변경', className: 'btn-sm btn-success border-radius-reset mr-15 btn-reorder' },
+                    /*{ text: '<i class="fa fa-sort-numeric-asc mr-5"></i> 정렬변경', className: 'btn-sm btn-success border-radius-reset mr-15 btn-reorder' },*/
                     { text: '<i class="fa fa-pencil mr-5"></i> 교수 등록', className: 'btn-sm btn-primary border-radius-reset', action: function(e, dt, node, config) {
                         location.href = '{{ site_url('/product/base/professor/create') }}' + dtParamsToQueryString($datatable);
                     }}

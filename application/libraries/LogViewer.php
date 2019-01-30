a9e7d6c079fa1a696e861d229427b3ad29468837
신봉석 <bsshin@willbes.com>
2019-01-30 11:10:24 +0900

    로그뷰어 파일라인 읽기 수정

--- application/libraries/LogViewer.php
+++ application/libraries/LogViewer.php
@@ -45,7 +45,8 @@ class LogViewer
             $prev_line = '';
 
             while (! feof($handle)) {
-                $line = trim(fgets($handle));
+                //$line = trim(fgets($handle, 4096));
+                $line = trim(stream_get_line($handle, 1024 * 1024, '\n'));
 
                 if (starts_with($line, ['ERROR', 'DEBUG', 'INFO']) === true) {
                     $content = $prev_line . $sub_line;
@@ -103,7 +104,8 @@ class LogViewer
             $prev_line = '';
 
             while (! feof($handle)) {
-                $line = trim(fgets($handle, 4096));
+                //$line = trim(fgets($handle, 4096));
+                $line = trim(stream_get_line($handle, 1024 * 1024, '\n'));
 
                 if (starts_with($line, '/*=') === true) {
                     continue;

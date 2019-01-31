c23e64f57e40691e7d33e8990a0853a8928317a7
김건우 <tempus8731@willbes.com>
2019-01-31 11:34:54 +0900

    모바일웹 인트로 작업 페이지 업로드

---
+++ views/html/m/intro.blade.php
@@ -0,0 +1,20 @@
+@extends('html.m.layouts.master')
+
+@section('content')
+<!-- Container -->
+<div id="Container" class="Container c_both mb20">
+    <div class="introBox">
+        <a href="#none">
+            <img src="{{ img_url('m/intro/intro1.jpg') }}">
+        </a>
+    </div>
+    <div class="introBox">
+        <a href="#none">
+            <div class="subTit NSK-Thin">9급 / 7급 / 법원 / 소방 / 기술 / 군무원</div>
+            <div class="Tit NSK-Black"><span class="gosi-color">공무원</span> 온라인</div>
+            <div class="btnGo NSK"><span>바로가기 ></span></div>
+        </div>
+    </div>
+</div>
+<!-- End Container -->
+@stop

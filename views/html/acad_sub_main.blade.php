2574672929b436d6c04bc8122769d34650c41e28
김창용 <winmir@willbes.com>
2019-02-11 16:47:50 +0900

    학원 캠퍼스 메인 오시는 길 수정

--- views/html/acad_sub_main.blade.php
+++ views/html/acad_sub_main.blade.php
@@ -731,28 +731,7 @@
                         <!-- 신림 -->
                         <div class="mt40">
                             <div class="f_left">
-                                <table cellpadding="0" cellspacing="0"  style="width:536px">
-                                    <tr>
-                                        <td style="border:1px solid #cecece;">
-                                            <a href="http://map.naver.com/?searchCoord=37137823b61f7eed35964c6307d5817855f785353e6ee4129a77e5fe47eaebee&query=7Iug66a864%2BZIOycjOu5hOyKpOqyveywsO2VmeybkA%3D%3D&tab=1&lng=8ffb927ed67b0f3219d0d61d27ee1a59&mapMode=0&mpx=681039aecaf7f40c63cb6d0d3f3939492bfef483638eac8c5245c17f6e05851775fe3c76c78fc68e3a1a10e9d06d016aae6091272fe5223b59ea7cb6f93a916f&lat=41f3386316de60023a2987bec7e6eda7&dlevel=12&enc=b64&menu=location" target="_blank"><img src="http://prt.map.naver.com/mashupmap/print?key=p1549868701724_-567339848" width="534" height="418" alt="지도 크게 보기" title="지도 크게 보기" border="0" style="vertical-align:top;"/></a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <table cellpadding="0" cellspacing="0">
-                                                <tr>
-                                                    <td height="30" bgcolor="#f9f9f9" align="left" style="padding-left:9px; border-left:1px solid #cecece; border-bottom:1px solid #cecece;">
-                                                        <span style="font-family: tahoma; font-size: 11px; color:#666;">2019.2.11</span>&nbsp;<span style="font-size: 11px; color:#e5e5e5;">|</span>&nbsp;
-                                                        <a style="font-family: dotum,sans-serif; font-size: 11px; color:#666; text-decoration: none; letter-spacing: -1px;" href="http://map.naver.com/?searchCoord=37137823b61f7eed35964c6307d5817855f785353e6ee4129a77e5fe47eaebee&query=7Iug66a864%2BZIOycjOu5hOyKpOqyveywsO2VmeybkA%3D%3D&tab=1&lng=8ffb927ed67b0f3219d0d61d27ee1a59&mapMode=0&mpx=681039aecaf7f40c63cb6d0d3f3939492bfef483638eac8c5245c17f6e05851775fe3c76c78fc68e3a1a10e9d06d016aae6091272fe5223b59ea7cb6f93a916f&lat=41f3386316de60023a2987bec7e6eda7&dlevel=12&enc=b64&menu=location" target="_blank">지도 크게 보기</a>
-                                                    </td>
-                                                    <td width="98" bgcolor="#f9f9f9" align="right" style="text-align:right; padding-right:9px; border-right:1px solid #cecece; border-bottom:1px solid #cecece;">
-                                                        <span style="float:right;"><span style="font-size:9px; font-family:Verdana, sans-serif; color:#444;">&copy;&nbsp;</span>&nbsp;<a style="font-family:tahoma; font-size:9px; font-weight:bold; color:#2db400; text-decoration:none;" href="http://www.nhncorp.com" target="_blank">NAVER Corp.</a></span>
-                                                    </td>
-                                                </tr>
-                                            </table>
-                                        </td>
-                                    </tr>
-                                </table>
+                                <table cellpadding="0" cellspacing="0" width="536"> <tr> <td style="border:1px solid #cecece;"><a href="http://map.naver.com/?searchCoord=37137823b61f7eed35964c6307d5817855f785353e6ee4129a77e5fe47eaebee&query=7Iug66a864%2BZIOycjOu5hOyKpOqyveywsO2VmeybkA%3D%3D&tab=1&lng=8ffb927ed67b0f3219d0d61d27ee1a59&mapMode=0&mpx=681039aecaf7f40c63cb6d0d3f3939492bfef483638eac8c5245c17f6e05851775fe3c76c78fc68e3a1a10e9d06d016aae6091272fe5223b59ea7cb6f93a916f&lat=41f3386316de60023a2987bec7e6eda7&dlevel=12&enc=b64&menu=location" target="_blank"><img src="http://prt.map.naver.com/mashupmap/print?key=p1549871165699_755736770" width="534" height="250" alt="지도 크게 보기" title="지도 크게 보기" border="0" style="vertical-align:top;"/></a></td> </tr> <tr> <td> <table cellpadding="0" cellspacing="0" width="100%"> <tr> <td height="30" bgcolor="#f9f9f9" align="left" style="padding-left:9px; border-left:1px solid #cecece; border-bottom:1px solid #cecece;"> <span style="font-family: tahoma; font-size: 11px; color:#666;">2019.2.11</span>&nbsp;<span style="font-size: 11px; color:#e5e5e5;">|</span>&nbsp;<a style="font-family: dotum,sans-serif; font-size: 11px; color:#666; text-decoration: none; letter-spacing: -1px;" href="http://map.naver.com/?searchCoord=37137823b61f7eed35964c6307d5817855f785353e6ee4129a77e5fe47eaebee&query=7Iug66a864%2BZIOycjOu5hOyKpOqyveywsO2VmeybkA%3D%3D&tab=1&lng=8ffb927ed67b0f3219d0d61d27ee1a59&mapMode=0&mpx=681039aecaf7f40c63cb6d0d3f3939492bfef483638eac8c5245c17f6e05851775fe3c76c78fc68e3a1a10e9d06d016aae6091272fe5223b59ea7cb6f93a916f&lat=41f3386316de60023a2987bec7e6eda7&dlevel=12&enc=b64&menu=location" target="_blank">지도 크게 보기</a> </td> <td width="98" bgcolor="#f9f9f9" align="right" style="text-align:right; padding-right:9px; border-right:1px solid #cecece; border-bottom:1px solid #cecece;"> <span style="float:right;"><span style="font-size:9px; font-family:Verdana, sans-serif; color:#444;">&copy;&nbsp;</span>&nbsp;<a style="font-family:tahoma; font-size:9px; font-weight:bold; color:#2db400; text-decoration:none;" href="http://www.nhncorp.com" target="_blank">NAVER Corp.</a></span> </td> </tr> </table> </td> </tr> </table>
                             </div>
                             <div class="add">
                                 <ul>

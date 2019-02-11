3a8a5a96b89eab77b3f61b792d4d87b6e5a5997c
김창용 <winmir@willbes.com>
2019-02-11 17:13:10 +0900

    학원 캠퍼스 오시는 길 수정

--- views/html/acad_sub_main.blade.php
+++ views/html/acad_sub_main.blade.php
@@ -728,6 +728,7 @@
                     <!-- notice2//-->
 
                     <div id="notice3" class="tabContent p_re">
+                        <!-- 지도 534x250 -->
                         <!-- 신림 -->
                         <div class="mt40">
                             <div class="f_left">
@@ -740,7 +741,87 @@
                                 </ul>
                             </div>
                         </div>
+
+                        <!-- 인천 -->
+                        <div class="mt40">
+                            <div class="f_left">
+                                <table cellpadding="0" cellspacing="0" width="536"> <tr> <td style="border:1px solid #cecece;"><a href="http://map.naver.com/?searchCoord=548870a990ecf318c7ca984cf21b8e77d8b86c823d74cdb4579b6b36b6385347&query=7J247LKc6rSR7Jet7IucIOu2gO2Pieq1rCDrtoDtj4nrj5kgNTM0LTI4IOykkeuztOu5jOuUqQ%3D%3D&tab=1&lng=256fb22eeee2344678885954ed42a3bf&mapMode=0&mpx=55c4c2141071798f2d20805fcee6ecb9b5968e0bfb89170dd26109f7a9524c2def9b1198b4df5e220daab6a53f96c8795b482be9d1fb8d6e1fc559be9fbe1d87&lat=a81cbafdad9f8ba56dc1fd0b47c5a2b0&dlevel=12&enc=b64&menu=location" target="_blank"><img src="http://prt.map.naver.com/mashupmap/print?key=p1549871564023_1931329295" width="534" height="250" alt="지도 크게 보기" title="지도 크게 보기" border="0" style="vertical-align:top;"/></a></td> </tr> <tr> <td> <table cellpadding="0" cellspacing="0" width="100%"> <tr> <td height="30" bgcolor="#f9f9f9" align="left" style="padding-left:9px; border-left:1px solid #cecece; border-bottom:1px solid #cecece;"> <span style="font-family: tahoma; font-size: 11px; color:#666;">2019.2.11</span>&nbsp;<span style="font-size: 11px; color:#e5e5e5;">|</span>&nbsp;<a style="font-family: dotum,sans-serif; font-size: 11px; color:#666; text-decoration: none; letter-spacing: -1px;" href="http://map.naver.com/?searchCoord=548870a990ecf318c7ca984cf21b8e77d8b86c823d74cdb4579b6b36b6385347&query=7J247LKc6rSR7Jet7IucIOu2gO2Pieq1rCDrtoDtj4nrj5kgNTM0LTI4IOykkeuztOu5jOuUqQ%3D%3D&tab=1&lng=256fb22eeee2344678885954ed42a3bf&mapMode=0&mpx=55c4c2141071798f2d20805fcee6ecb9b5968e0bfb89170dd26109f7a9524c2def9b1198b4df5e220daab6a53f96c8795b482be9d1fb8d6e1fc559be9fbe1d87&lat=a81cbafdad9f8ba56dc1fd0b47c5a2b0&dlevel=12&enc=b64&menu=location" target="_blank">지도 크게 보기</a> </td> <td width="98" bgcolor="#f9f9f9" align="right" style="text-align:right; padding-right:9px; border-right:1px solid #cecece; border-bottom:1px solid #cecece;"> <span style="float:right;"><span style="font-size:9px; font-family:Verdana, sans-serif; color:#444;">&copy;&nbsp;</span>&nbsp;<a style="font-family:tahoma; font-size:9px; font-weight:bold; color:#2db400; text-decoration:none;" href="http://www.nhncorp.com" target="_blank">NAVER Corp.</a></span> </td> </tr> </table> </td> </tr> </table>
+                            </div>
+                            <div class="add">
+                                <ul>
+                                    <li><span>주소</span>인천 부평구 부평동 534-28 중보빌딩 10층</li>
+                                    <li><span>연락처</span>1544-1661</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <!-- 대구 -->
+                        <div class="mt40">
+                            <div class="f_left">
+                                <table cellpadding="0" cellspacing="0" width="536"> <tr> <td style="border:1px solid #cecece;"><a href="http://map.naver.com/?searchCoord=2966d6fd510e2815eeb05d14c7761f0459d09ff648de82e7e6fa4fdb565e2504&query=64yA6rWsIOycjOu5hOyKpOqyveywsO2VmeybkA%3D%3D&menu=location&tab=1&lng=a75075027346f16ab192a150a47954d0&mapMode=0&mpx=55c4c2141071798f2d20805fcee6ecb96c58b8634e9ea9c62c93bc96f9f4d635e4754ba8a412e14ab22cbc787c45e8e970d7009752968db691dd58f6aea6d7ca&lat=34a6b99f5cfe34e7c836e944c4ab1299&dlevel=12&enc=b64" target="_blank"><img src="http://prt.map.naver.com/mashupmap/print?key=p1549871903923_-1198915242" width="534" height="250" alt="지도 크게 보기" title="지도 크게 보기" border="0" style="vertical-align:top;"/></a></td> </tr> <tr> <td> <table cellpadding="0" cellspacing="0" width="100%"> <tr> <td height="30" bgcolor="#f9f9f9" align="left" style="padding-left:9px; border-left:1px solid #cecece; border-bottom:1px solid #cecece;"> <span style="font-family: tahoma; font-size: 11px; color:#666;">2019.2.11</span>&nbsp;<span style="font-size: 11px; color:#e5e5e5;">|</span>&nbsp;<a style="font-family: dotum,sans-serif; font-size: 11px; color:#666; text-decoration: none; letter-spacing: -1px;" href="http://map.naver.com/?searchCoord=2966d6fd510e2815eeb05d14c7761f0459d09ff648de82e7e6fa4fdb565e2504&query=64yA6rWsIOycjOu5hOyKpOqyveywsO2VmeybkA%3D%3D&menu=location&tab=1&lng=a75075027346f16ab192a150a47954d0&mapMode=0&mpx=55c4c2141071798f2d20805fcee6ecb96c58b8634e9ea9c62c93bc96f9f4d635e4754ba8a412e14ab22cbc787c45e8e970d7009752968db691dd58f6aea6d7ca&lat=34a6b99f5cfe34e7c836e944c4ab1299&dlevel=12&enc=b64" target="_blank">지도 크게 보기</a> </td> <td width="98" bgcolor="#f9f9f9" align="right" style="text-align:right; padding-right:9px; border-right:1px solid #cecece; border-bottom:1px solid #cecece;"> <span style="float:right;"><span style="font-size:9px; font-family:Verdana, sans-serif; color:#444;">&copy;&nbsp;</span>&nbsp;<a style="font-family:tahoma; font-size:9px; font-weight:bold; color:#2db400; text-decoration:none;" href="http://www.nhncorp.com" target="_blank">NAVER Corp.</a></span> </td> </tr> </table> </td> </tr> </table>
+                            </div>
+                            <div class="add">
+                                <ul>
+                                    <li><span>주소</span>대구 중구 중앙대로 412(남일동) CGV 2층</li>
+                                    <li><span>연락처</span>1522-6112</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <!-- 부산 -->
+                        <div class="mt40">
+                            <div class="f_left">
+                                <table cellpadding="0" cellspacing="0" width="536"> <tr> <td style="border:1px solid #cecece;"><a href="http://map.naver.com/?searchCoord=434c194eff4846098d98c231cedbd183691835a7eb699691f49cd030b1ef02ee&query=67aA7IKwIOycjOu5hOyKpOqyveywsO2VmeybkA%3D%3D&tab=1&lng=2c7b29c8495563f33b1888c743b1cf6d&mapMode=0&mpx=574b59b99ab9b8f10dd3d56152a98d94b1434e308f49e849f7f3a2959b2bdb0b171337b12433a983315d4959aeacf955363bc336bce5c21857d69874aa0cc83d&lat=8a946a423a722e11b3ee617ab6b05511&dlevel=12&enc=b64&menu=location" target="_blank"><img src="http://prt.map.naver.com/mashupmap/print?key=p1549872064284_1319919800" width="534" height="250" alt="지도 크게 보기" title="지도 크게 보기" border="0" style="vertical-align:top;"/></a></td> </tr> <tr> <td> <table cellpadding="0" cellspacing="0" width="100%"> <tr> <td height="30" bgcolor="#f9f9f9" align="left" style="padding-left:9px; border-left:1px solid #cecece; border-bottom:1px solid #cecece;"> <span style="font-family: tahoma; font-size: 11px; color:#666;">2019.2.11</span>&nbsp;<span style="font-size: 11px; color:#e5e5e5;">|</span>&nbsp;<a style="font-family: dotum,sans-serif; font-size: 11px; color:#666; text-decoration: none; letter-spacing: -1px;" href="http://map.naver.com/?searchCoord=434c194eff4846098d98c231cedbd183691835a7eb699691f49cd030b1ef02ee&query=67aA7IKwIOycjOu5hOyKpOqyveywsO2VmeybkA%3D%3D&tab=1&lng=2c7b29c8495563f33b1888c743b1cf6d&mapMode=0&mpx=574b59b99ab9b8f10dd3d56152a98d94b1434e308f49e849f7f3a2959b2bdb0b171337b12433a983315d4959aeacf955363bc336bce5c21857d69874aa0cc83d&lat=8a946a423a722e11b3ee617ab6b05511&dlevel=12&enc=b64&menu=location" target="_blank">지도 크게 보기</a> </td> <td width="98" bgcolor="#f9f9f9" align="right" style="text-align:right; padding-right:9px; border-right:1px solid #cecece; border-bottom:1px solid #cecece;"> <span style="float:right;"><span style="font-size:9px; font-family:Verdana, sans-serif; color:#444;">&copy;&nbsp;</span>&nbsp;<a style="font-family:tahoma; font-size:9px; font-weight:bold; color:#2db400; text-decoration:none;" href="http://www.nhncorp.com" target="_blank">NAVER Corp.</a></span> </td> </tr> </table> </td> </tr> </table>
+                            </div>
+                            <div class="add">
+                                <ul>
+                                    <li><span>주소</span>부산 진구 부정동 223-8</li>
+                                    <li><span>연락처</span>1522-8112</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <!-- 광주 -->
+                        <div class="mt40">
+                            <div class="f_left">
+                                <table cellpadding="0" cellspacing="0" width="536"> <tr> <td style="border:1px solid #cecece;"><a href="http://map.naver.com/?searchCoord=63dd3aca87f6b903abf7c794d3db6f8f6ff0f42efdd09b18fd7390c44a53864e&query=6rSR7KO8IOu2geq1rCDtmLjrj5nroZwgNi0xMQ%3D%3D&tab=1&lng=ac3f41916f4ce7826c1d17754b745b75&mapMode=0&mpx=1dbc5449bedcd7258544dfebb50269c7c08edfc61d5ba4bb481d2791afb7b1c53a6baca5a98d0e82496cbbb3aecbac00fe57b1ce88f8e09ff50a7f5a9fe56dcc&lat=c78f939baca7d2824c32e9ec86614aeb&dlevel=12&enc=b64&menu=location" target="_blank"><img src="http://prt.map.naver.com/mashupmap/print?key=p1549872306015_1310470280" width="534" height="250" alt="지도 크게 보기" title="지도 크게 보기" border="0" style="vertical-align:top;"/></a></td> </tr> <tr> <td> <table cellpadding="0" cellspacing="0" width="100%"> <tr> <td height="30" bgcolor="#f9f9f9" align="left" style="padding-left:9px; border-left:1px solid #cecece; border-bottom:1px solid #cecece;"> <span style="font-family: tahoma; font-size: 11px; color:#666;">2019.2.11</span>&nbsp;<span style="font-size: 11px; color:#e5e5e5;">|</span>&nbsp;<a style="font-family: dotum,sans-serif; font-size: 11px; color:#666; text-decoration: none; letter-spacing: -1px;" href="http://map.naver.com/?searchCoord=63dd3aca87f6b903abf7c794d3db6f8f6ff0f42efdd09b18fd7390c44a53864e&query=6rSR7KO8IOu2geq1rCDtmLjrj5nroZwgNi0xMQ%3D%3D&tab=1&lng=ac3f41916f4ce7826c1d17754b745b75&mapMode=0&mpx=1dbc5449bedcd7258544dfebb50269c7c08edfc61d5ba4bb481d2791afb7b1c53a6baca5a98d0e82496cbbb3aecbac00fe57b1ce88f8e09ff50a7f5a9fe56dcc&lat=c78f939baca7d2824c32e9ec86614aeb&dlevel=12&enc=b64&menu=location" target="_blank">지도 크게 보기</a> </td> <td width="98" bgcolor="#f9f9f9" align="right" style="text-align:right; padding-right:9px; border-right:1px solid #cecece; border-bottom:1px solid #cecece;"> <span style="float:right;"><span style="font-size:9px; font-family:Verdana, sans-serif; color:#444;">&copy;&nbsp;</span>&nbsp;<a style="font-family:tahoma; font-size:9px; font-weight:bold; color:#2db400; text-decoration:none;" href="http://www.nhncorp.com" target="_blank">NAVER Corp.</a></span> </td> </tr> </table> </td> </tr> </table>
+                            </div>
+                            <div class="add">
+                                <ul>
+                                    <li><span>주소</span>광주 북구 호동로 6-11</li>
+                                    <li><span>연락처</span>062-722-8140</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <!-- 제주 -->
+                        <div class="mt40">
+                            <div class="f_left">
+                                <table cellpadding="0" cellspacing="0" width="536"> <tr> <td style="border:1px solid #cecece;"><a href="http://map.naver.com/?searchCoord=63dd3aca87f6b903abf7c794d3db6f8f6ff0f42efdd09b18fd7390c44a53864e&query=7KCc7KO864%2BEIOyLoOq0keydgOqyveywsO2VmeybkA%3D%3D&tab=1&lng=4862e0dcec7108409c3b4fd8ae61f256&mapMode=0&mpx=1dbc5449bedcd7258544dfebb50269c7c08edfc61d5ba4bb481d2791afb7b1c53a6baca5a98d0e82496cbbb3aecbac00fe57b1ce88f8e09ff50a7f5a9fe56dcc&lat=4cb9a0719d8f73f51f340b67d12e91a0&dlevel=12&enc=b64&menu=location" target="_blank"><img src="http://prt.map.naver.com/mashupmap/print?key=p1549872556210_-64065344" width="534" height="250" alt="지도 크게 보기" title="지도 크게 보기" border="0" style="vertical-align:top;"/></a></td> </tr> <tr> <td> <table cellpadding="0" cellspacing="0" width="100%"> <tr> <td height="30" bgcolor="#f9f9f9" align="left" style="padding-left:9px; border-left:1px solid #cecece; border-bottom:1px solid #cecece;"> <span style="font-family: tahoma; font-size: 11px; color:#666;">2019.2.11</span>&nbsp;<span style="font-size: 11px; color:#e5e5e5;">|</span>&nbsp;<a style="font-family: dotum,sans-serif; font-size: 11px; color:#666; text-decoration: none; letter-spacing: -1px;" href="http://map.naver.com/?searchCoord=63dd3aca87f6b903abf7c794d3db6f8f6ff0f42efdd09b18fd7390c44a53864e&query=7KCc7KO864%2BEIOyLoOq0keydgOqyveywsO2VmeybkA%3D%3D&tab=1&lng=4862e0dcec7108409c3b4fd8ae61f256&mapMode=0&mpx=1dbc5449bedcd7258544dfebb50269c7c08edfc61d5ba4bb481d2791afb7b1c53a6baca5a98d0e82496cbbb3aecbac00fe57b1ce88f8e09ff50a7f5a9fe56dcc&lat=4cb9a0719d8f73f51f340b67d12e91a0&dlevel=12&enc=b64&menu=location" target="_blank">지도 크게 보기</a> </td> <td width="98" bgcolor="#f9f9f9" align="right" style="text-align:right; padding-right:9px; border-right:1px solid #cecece; border-bottom:1px solid #cecece;"> <span style="float:right;"><span style="font-size:9px; font-family:Verdana, sans-serif; color:#444;">&copy;&nbsp;</span>&nbsp;<a style="font-family:tahoma; font-size:9px; font-weight:bold; color:#2db400; text-decoration:none;" href="http://www.nhncorp.com" target="_blank">NAVER Corp.</a></span> </td> </tr> </table> </td> </tr> </table>
+                            </div>
+                            <div class="add">
+                                <ul>
+                                    <li><span>주소</span>제주도 제주시 동광로 56 3층</li>
+                                    <li><span>연락처</span>064-722-8140</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <!-- 진주 -->
+                        <div class="mt40">
+                            <div class="f_left">
+                                <table cellpadding="0" cellspacing="0" width="536"> <tr> <td style="border:1px solid #cecece;"><a href="http://map.naver.com/?searchCoord=a6d06bfb6855937bd7f4eb8cf1742f7b630ba9b1e3a90c1280ab523f1cbf00ce&query=7KeE7KO8IOycjOu5hOyKpOyLoOq0keydgOqyveywsO2VmeybkA%3D%3D&menu=location&tab=1&lng=63480df7bb5b6c344bd81f1943587b00&mapMode=0&mpx=f31bb06e57d7c02d39e001f61fd153554d93ee289f5015ec58d19b06303d75a49e2956161bf202391645f69b9234ddfc454d6808ca0544cc16254a9c6fd7941a&lat=a93919e466f89bb4da62fb08a6de53f9&dlevel=12&enc=b64" target="_blank"><img src="http://prt.map.naver.com/mashupmap/print?key=p1549872734360_-1886588831" width="534" height="250" alt="지도 크게 보기" title="지도 크게 보기" border="0" style="vertical-align:top;"/></a></td> </tr> <tr> <td> <table cellpadding="0" cellspacing="0" width="100%"> <tr> <td height="30" bgcolor="#f9f9f9" align="left" style="padding-left:9px; border-left:1px solid #cecece; border-bottom:1px solid #cecece;"> <span style="font-family: tahoma; font-size: 11px; color:#666;">2019.2.11</span>&nbsp;<span style="font-size: 11px; color:#e5e5e5;">|</span>&nbsp;<a style="font-family: dotum,sans-serif; font-size: 11px; color:#666; text-decoration: none; letter-spacing: -1px;" href="http://map.naver.com/?searchCoord=a6d06bfb6855937bd7f4eb8cf1742f7b630ba9b1e3a90c1280ab523f1cbf00ce&query=7KeE7KO8IOycjOu5hOyKpOyLoOq0keydgOqyveywsO2VmeybkA%3D%3D&menu=location&tab=1&lng=63480df7bb5b6c344bd81f1943587b00&mapMode=0&mpx=f31bb06e57d7c02d39e001f61fd153554d93ee289f5015ec58d19b06303d75a49e2956161bf202391645f69b9234ddfc454d6808ca0544cc16254a9c6fd7941a&lat=a93919e466f89bb4da62fb08a6de53f9&dlevel=12&enc=b64" target="_blank">지도 크게 보기</a> </td> <td width="98" bgcolor="#f9f9f9" align="right" style="text-align:right; padding-right:9px; border-right:1px solid #cecece; border-bottom:1px solid #cecece;"> <span style="float:right;"><span style="font-size:9px; font-family:Verdana, sans-serif; color:#444;">&copy;&nbsp;</span>&nbsp;<a style="font-family:tahoma; font-size:9px; font-weight:bold; color:#2db400; text-decoration:none;" href="http://www.nhncorp.com" target="_blank">NAVER Corp.</a></span> </td> </tr> </table> </td> </tr> </table>
+                            </div>
+                            <div class="add">
+                                <ul>
+                                    <li><span>주소</span>경남 진주시 칠암동 490-8 엠코아빌딩 4층</li>
+                                    <li><span>연락처</span>055-755-7771</li>
+                                </ul>
+                            </div>
+                        </div>
+
                     </div>
+                    <!-- notice3// -->
                 </div>
             </div>
         

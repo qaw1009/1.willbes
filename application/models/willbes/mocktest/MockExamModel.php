47a82a2db87ffc3dbc6a982d29f2e95e5bb5ff7b
eddie <eddie@willbes.com>
2019-02-12 14:36:48 +0900

    오류수정

--- application/models/willbes/mocktest/MockExamModel.php
+++ application/models/willbes/mocktest/MockExamModel.php
@@ -497,9 +497,10 @@ class MockExamModel extends WB_Model
         $obder_by = " ORDER BY RemainSec";
 
         $where = " WHERE MrIdx = ".$mridx;
-        echo "<pre>".'select '. $column . $from . $where . $obder_by."</pre>";
+        //echo "<pre>".'select '. $column . $from . $where . $obder_by."</pre>";
         $query = $this->_conn->query('select ' . $column . $from . $where . $obder_by);
-        return $query->row_array()['RemainSec'];
+        $data = $query->row_array();
+        return $data['RemainSec'];
     }
 
     /**

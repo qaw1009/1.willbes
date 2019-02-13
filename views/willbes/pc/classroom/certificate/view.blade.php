aac8512c0cf09c0db9702fa28eda7899aba11fed
하준걸 <hereisme@willbes.com>
2019-02-13 13:42:12 +0900

    수강증

--- views/willbes/pc/classroom/certificate/view.blade.php
+++ views/willbes/pc/classroom/certificate/view.blade.php
@@ -6,46 +6,6 @@
     <meta charset="utf-8">
     <link href="/public/css/willbes/basic.css?ver={{time()}}" rel="stylesheet">
     <link href="/public/css/willbes/style.css?ver={{time()}}" rel="stylesheet">
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-        }
-        * {
-            box-sizing: border-box;
-            -moz-box-sizing: border-box;
-        }
-        .page {
-            width: 21cm;
-            min-height: 29.7cm;
-            padding: 2cm;
-            margin: 0 auto;
-            background:#eee;
-            align:center;
-        }
-
-        @@page {
-            size: A4;
-            margin: 0;
-        }
-
-        @@media print {
-            html, body {
-                width: 210mm;
-                height: 297mm;
-            }
-
-            .page {
-                margin: 0;
-                border: initial;
-                width: initial;
-                min-height: initial;
-                box-shadow: initial;
-                background: initial;
-                page-break-after: always;
-            }
-        }
-    </style>
 </head>
 <body>
 <div class=".page willbes-Layer-PassBox willbes-Layer-PassBox740 abs mb20" style="display:block !important; top:20px">
@@ -170,7 +130,6 @@
 <script type="text/javascript">
     function fnPrint(obj)
     {
-        $(obj).hide();
         window.print();
     }
 </script>

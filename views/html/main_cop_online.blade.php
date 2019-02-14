085bb51d34db4e01aed210a4bb235e283937861e
김창용 <winmir@willbes.com>
2019-02-14 16:43:57 +0900

    경찰 온라인 메인

--- views/html/main_cop_online.blade.php
+++ views/html/main_cop_online.blade.php
@@ -93,11 +93,9 @@
     </div>
 
     <div class="Section youtubeWrap">
-        <div class="widthAuto">          
-            <img src="{{ img_url('cop/visual/visual_tit01.jpg') }}" alt="수험생에게 도움이 되는 특별한 영상!" usemap="#Mapvisual_tit01" border="0">
-            <map name="Mapvisual_tit01" id="Mapvisual_tit01">
-                <area shape="rect" coords="1097,0,1120,23" href="https://www.youtube.com/channel/UCQ-jvqaobw6E9EvnFO88vwQ" target="_blank"/>
-            </map>    
+        <div class="widthAuto smallTit">          
+            <p><span>수험생에게 도움이 되는 <strong>특별한 영상!</strong></span></p>
+            <a href="https://www.youtube.com/channel/UCQ-jvqaobw6E9EvnFO88vwQ" target="_blank"><img src="{{ img_url('cop/icon_add_big.png') }}" alt="동영상 더보기"></a>             
         </div>
 
         <div class="widthAuto mt80">
@@ -143,8 +141,6 @@
             </div>
         </div>
     </div>
-    <!-- youtubeWrap //-->
-
     <script type="text/javascript">        
 			var tab1_url = "https://www.youtube.com/embed/sVr6LYsbzek?rel=0&modestbranding=1&showinfo=0";
 			var tab2_url = "https://www.youtube.com/embed/SHbzPnFtkxk?rel=0&modestbranding=1&showinfo=0";
@@ -174,44 +170,47 @@
 					$(activeTab).fadeIn(); 
 					return false; 
 					});
-				});
-		
+				});		
     </script>
+    <!-- youtubeWrap //-->
 
     <div class="Section Flipped">
         <div class="widthAuto">
             <ul class="inner">
                 <li class="l1">
                     <a href="http://www.willbescop.net/movie/event.html?event_cd=On_180327_yp" target="_blank">
-                        <div class="f-tit tit1 NSK"># 평생0원PASS</div>
+                        <img src="{{ img_url('cop/flipped/flipped01.jpg') }}" alt="평생0원PASS">    
+                        <!--div class="f-tit tit1 NSK"># 평생0원PASS</div>
                         <span class="wrap_ban front">
                             <img src="{{ img_url('cop/flipped/flipped_front_190101.png') }}">
                         </span>
                         <span class="wrap_ban back">
                             <img src="{{ img_url('cop/flipped/flipped_back_190101.png') }}">
-                        </span>
+                        </span-->
                     </a>
                 </li>
                 <li class="l2">
                     <a href="http://www.willbescop.net/event/movie/event.html?event_cd=On_170831_p&topMenuType=O#main" target="_blank">
-                        <div class="f-tit tit2 NSK"># 2019 대비 기본이론</div>
+                        <img src="{{ img_url('cop/flipped/flipped02.jpg') }}" alt="2019 대비 기본이론<">
+                        <!--div class="f-tit tit2 NSK"># 2019 대비 기본이론</div>
                         <span class="wrap_ban front">
                             <img src="{{ img_url('cop/flipped/flipped_front_190102.png') }}">
                         </span>
                         <span class="wrap_ban back">
                             <img src="{{ img_url('cop/flipped/flipped_back_190102.png') }}">
-                        </span>
+                        </span-->
                     </a>
                 </li>
                 <li class="l3">
                     <a href="http://www.willbescop.net/movie/event.html?event_cd=On_premium&topMenuType=O" target="_blank">
-                        <div class="f-tit tit3 NSK"># 2019 대비 심화이론/기출</div>
+                        <img src="{{ img_url('cop/flipped/flipped03.jpg') }}" alt="2019 대비 심화이론/기출">
+                        <!--div class="f-tit tit3 NSK"># 2019 대비 심화이론/기출</div>
                         <span class="wrap_ban front">
                             <img src="{{ img_url('cop/flipped/flipped_front_190103.png') }}">
                         </span>
                         <span class="wrap_ban back">
                             <img src="{{ img_url('cop/flipped/flipped_back_190103.png') }}">
-                        </span>
+                        </span-->
                     </a>
                 </li>
             </ul>
@@ -222,8 +221,10 @@
             </div>
         </div>
     </div>
-    <div class="Section Section1 mt80 mb100">
-        <div class="widthAuto">
+
+    <div class="Section Section2 pb110">
+        <!--
+        <div class="widthAuto">            
             <div class="will-big-Tit">
                 <div class="small NSK-Thin">더욱 더 완벽하고 강력하게 무장한 신광은경찰팀</div>
                 <div class="big NSK-Black"><span class="cop-color">빈틈없는</span> 커리큘럼<span class="small NSK-Thin">으로 합격을 약속합니다.</span></div>
@@ -315,50 +316,66 @@
                             </a>
                         </li>
                     </ul>
-                </div>
+                </div>                
+            </div>
+            -->
+            
+            <div class="widthAuto curriculumTit">                
+                <img src="{{ img_url('cop/visual/visual_tit02.jpg') }}" alt="신의법칙 퍼펙트 커리큘럼">
+                <a href="#none">신의법칙 자세히 보기 &gt;</a>                
             </div>
 
-            <div class="curriculumTxt">
-                이 <span class="cop-color">모든 강의</span>를 평생 0원 PASS 하나로 <span class="cop-color">평생 수강</span>하실 수 있습니다.                    
-                <span class="btn"><a href="http://www.willbescop.net/movie/event.html?event_cd=On_180327_yp" target="_blank">평생 0원 PASS 구매하기</a></span>
-            </div>
-        </div>
-    </div>
-    <div class="Section Section2 pt100 pb90">
-        <div class="widthAuto">
-            <div class="will-big-Tit">
-                <div class="small NSK-Thin">출제경향이 매번 반복되는 경찰공무원 시험.</div>
-                <div class="big NSK-Black"><span class="cop-color">날카롭게 분석된</span> 기출강의<span class="small NSK-Thin">로 마무리해야합니다.</span></div>
-            </div>
-            <div class="SpecialLecBox mt60">
-                <dl>
-                    <dt class="nLec p_re">
-                        <div class="infoBox">
-                            <div class="infoTit">
-                                <div class="small NSK-Thin">일반 / 경행 / 101단</div>
-                                <div class="big NSK-Black">
-                                    최근 5개년<br/>
-                                    기출문제
-                                </div>
-                                <div class="btn">
-                                    <div class="btn-sbj"><a href="http://www.willbescop.net/movie/event.html?event_cd=On_181126_y&topMenuType=O" target="_blank">+ &nbsp; 문제 더 보기</a></div>
-                                    <div class="btn-lec mt5"><a href="https://cop.dev.willbes.net/lecture/index/cate/3001/pattern/free?course_idx=20032" target="_blank">+ &nbsp; 강의 더 보기</a></div>
-                                </div>
+            <div class="widthAuto CurriStepBox">
+                <ul class="CurriStep">
+                    <li class="active">
+                        <a href="#none">
+                            <div class="curriculumBox">
+                                <span><img src="{{ img_url('cop/icon_bubble.gif') }}" alt="2019대비 진행중"> </span>
+                                <div class="Tit">기본과정</div>
+                                <div class="subTit">집중연강식 진행</div>
+                                <ul class="info">
+                                    <li>영어 매달 초 개강</li>
+                                    <li>2주마다  형소법  →  경찰학<br/>→  한국사  →  형법 진행</li>
+                                </ul> 
                             </div>
-                            <div class="infoList">
-                                <ul class="List-Table">
-                                    <li><a href="#none"><span>[2018년 2차]</span>경찰공무원(일반/101단/경행) 채용시험 기출</a><span class="btn-more"><a href="https://cop.dev.willbes.net/support/examQuestion/show?board_idx=1286&" target="_blank">바로가기 ></a></span></li>
-                                    <li><a href="#none"><span>[2018년 1차]</span>경찰공무원(일반/101단/전의경) 채용시험 기출</a><span class="btn-more"><a href="https://cop.dev.willbes.net/support/examQuestion/show?board_idx=1286&" target="_blank">바로가기 ></a></span></li>
-                                    <li><a href="#none"><span>[2018년 2차]</span>경찰공무원(일반/101단/경행) 채용시험 기출</a><span class="btn-more"><a href="https://cop.dev.willbes.net/support/examQuestion/show?board_idx=1286&" target="_blank">바로가기 ></a></span></li>
-                                    <li><a href="#none"><span>[2018년 1차]</span>경찰공무원(일반/101단/경행) 채용시험 기출</a><span class="btn-more"><a href="https://cop.dev.willbes.net/support/examQuestion/show?board_idx=1286&" target="_blank">바로가기 ></a></span></li>
+                        </a>
+                    </li>
+                    <li>&nbsp;</li>
+                    <li>
+                        <a href="#none">
+                            <div class="curriculumBox">
+                                <div class="Tit">심화과정</div>
+                                <div class="subTit">프리미엄 심화과정</div>
+                                <ul class="info">
+                                    <li>이론 재정리</li>
+                                    <li>10주 과정</li>
                                 </ul>
                             </div>
-                        </div>
-                    </dt>
-                </dl>
+                        </a>
+                    </li>
+                    <li>&nbsp;</li>
+                    <li>
+                        <a href="#none">
+                            <div class="curriculumBox">
+                                <div class="Tit">3개월 필합 풀패키지</div>
+                                <div class="subTit">핵심요약/진도별 정리</div>
+                                <ul class="info">
+                                    <li>5주 단권화</li>
+                                    <li>5주 동형모의고사</li>
+                                    <li>FINAL 실전 모의고사</li>
+                                </ul>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+                <div class="curriculumTxt">
+                    <span class="cop-color">모든 강의</span>를 평생 0원 PASS 하나로 <span class="cop-color">평생 수강</span>하실 수 있습니다.                    
+                    <span class="btn"><a href="http://www.willbescop.net/movie/event.html?event_cd=On_180327_yp" target="_blank">평생 0원 PASS 구매하기</a></span>
+                </div>
             </div>
-        </div>
+            <!-- CurriStepBox //-->        
     </div>
+
     <div class="Section Section3 mt95">
         <div class="widthAuto">
             <div class="will-big-Tit">
@@ -393,9 +410,13 @@
             </ul>
         </div>
     </div>
+
     <div class="Section Section4 pt100 pb90">
-        <div class="widthAuto">
-            <div class="will-big-Tit big-Tit-line pb50">
+        <div class="widthAuto">  
+            <div class="widthAuto smallTit smallTit2">          
+                <p><span>진짜 살아있는 정보를 알 수 있는 <strong>합격수기!</strong></span></p>            
+            </div>          
+            <div class="will-big-Tit big-Tit-line pt50 pb50">
                 <div class="big NSK-Black"><span class="cop-color">1,881명*</span> 최종합격</div>
                 <div class="small NSK-Thin mt10">합격의 기쁨을 나눌 다음 차례, 바로 당신입니다.</div>
                 <div class="will-subTit sm mt30">* 2018.경찰공무원 1차최종, 2차 팔가합격인원 기준</div>
@@ -550,9 +571,72 @@
             </div>
         </div>
     </div>
+
+    <div class="Section Section3 mt100 pb90">        
+        <div class="widthAuto">
+            <div class="widthAuto smallTit">          
+                <p><span>기출문제와 강의를 한 곳에 <strong>기출강의!</strong></span></p>            
+            </div>
+            <div class="will-big-Tit pt100">
+                <div class="small NSK-Thin">출제경향이 매번 반복되는 경찰공무원 시험.</div>
+                <div class="big NSK-Black"><span class="cop-color">날카롭게 분석된</span> 기출강의<span class="small NSK-Thin">로 마무리해야합니다.</span></div>
+            </div>
+            <div class="SpecialLecBox mt60">
+                <dl>
+                    <dt class="nLec p_re">
+                        <div class="infoBox">
+                            <div class="infoTit">
+                                <div class="small NSK-Thin">일반 / 경행 / 101단</div>
+                                <div class="big NSK-Black">
+                                    최근 5개년<br/>
+                                    기출문제
+                                </div>
+                                <div class="btn">
+                                    <div class="btn-sbj"><a href="http://www.willbescop.net/movie/event.html?event_cd=On_181126_y&topMenuType=O" target="_blank">+ &nbsp; 문제 더 보기</a></div>
+                                    <div class="btn-lec mt5"><a href="https://cop.dev.willbes.net/lecture/index/cate/3001/pattern/free?course_idx=20032" target="_blank">+ &nbsp; 강의 더 보기</a></div>
+                                </div>
+                            </div>
+                            <div class="infoList">
+                                <ul class="List-Table">
+                                    <li><a href="#none"><span>[2018년 2차]</span>경찰공무원(일반/101단/경행) 채용시험 기출</a><span class="btn-more"><a href="https://cop.dev.willbes.net/support/examQuestion/show?board_idx=1286&" target="_blank">바로가기 ></a></span></li>
+                                    <li><a href="#none"><span>[2018년 1차]</span>경찰공무원(일반/101단/전의경) 채용시험 기출</a><span class="btn-more"><a href="https://cop.dev.willbes.net/support/examQuestion/show?board_idx=1286&" target="_blank">바로가기 ></a></span></li>
+                                    <li><a href="#none"><span>[2018년 2차]</span>경찰공무원(일반/101단/경행) 채용시험 기출</a><span class="btn-more"><a href="https://cop.dev.willbes.net/support/examQuestion/show?board_idx=1286&" target="_blank">바로가기 ></a></span></li>
+                                    <li><a href="#none"><span>[2018년 1차]</span>경찰공무원(일반/101단/경행) 채용시험 기출</a><span class="btn-more"><a href="https://cop.dev.willbes.net/support/examQuestion/show?board_idx=1286&" target="_blank">바로가기 ></a></span></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </dt>
+                </dl>
+            </div>
+        </div>
+    </div>
+    <!-- 기출강의// -->
+    
+    <div class="Section HotIssue pt50 pb50">
+        <div class="widthAuto">  
+            <div class="widthAuto smallTit">          
+                <p><span>학원 최신 소식을 한 눈에! <strong>학원 Hot Issue</strong></span></p>            
+            </div>
+        </div>
+        <ul class="widthAuto mt60">
+            <li><a href="#none"><img src="{{ img_url('cop/banner/bnr_onHiA01.jpg') }}" alt="배너명"></a></li>
+            <li><a href="#none"><img src="{{ img_url('cop/banner/bnr_onHiB01.jpg') }}" alt="배너명"></a></li>
+            <li class="sliderHotIssue nSlider pick">
+                <div class="sliderNum">
+                    <div><img src="{{ img_url('cop/banner/bnr_onHiC01.jpg') }}"></div>
+                    <div><img src="{{ img_url('cop/banner/bnr_onHiC01.jpg') }}"></div>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <!-- HotIssue //-->    
+
     <div class="Section Bnr mt40 mb70">
         <div class="widthAuto">
-            <div class="willbes-Bnr">
+            <div class="widthAuto smallTit">          
+                <p><span>신광은경찰 Hot Pick! <strong>온라인특강/이벤트</strong></span></p>            
+            </div>
+            <div class="willbes-Bnr mt60">
                 <ul>
                     <li><a href="http://www.willbescop.net/movie/event.html?event_cd=On_180604_p&topMenuType=O" target="_blank"><img src="{{ img_url('cop/banner/bnr_190109.jpg') }}"></a></li>
                 </ul>
@@ -841,7 +925,7 @@
                                     공휴일/일요일휴무<br/>
                                 </div>
                             </li>
-                            <li>
+                            <!--li>
                                 <div class="nTit">학원 고객센터</div>
                                 <div class="nNumber tx-color">1544-0336</div>
                                 <div class="nTxt">
@@ -849,7 +933,7 @@
                                     월-토: 09시~ 22시<br/>
                                     일요일: 09시~ 20시<br/>
                                 </div>
-                            </li>
+                            </li-->
                         </ul>
                     </dt>
                 </dl>
@@ -869,13 +953,16 @@
             <li>   
                 <div class="QuickSlider">      
                     <div class="sliderNum">
-                        <div><a href="http://www.willbescop.net/event/movie/event.html?event_cd=On_170911_popup" target="_blank"><img src="{{ img_url('cop/quick/quick_190108.jpg') }}"></a></div>
-                        <div><a href="http://www.willbescop.net/event/arm_event.html?event_cd=On_leaveArmy02_2018&topMenuType=O&EVENT_NO=53" target="_blank"><img src="{{ img_url('cop/quick/quick_190109.jpg') }}"></a></div>
+                        <div><a href="http://www.willbescop.net/event/movie/event.html?event_cd=On_170911_popup" target="_blank"><img src="{{ img_url('cop/quick/quick_190108.jpg') }}" alt="배너명"></a></div>
+                        <div><a href="http://www.willbescop.net/event/arm_event.html?event_cd=On_leaveArmy02_2018&topMenuType=O&EVENT_NO=53" target="_blank"><img src="{{ img_url('cop/quick/quick_190109.jpg') }}" alt="배너명"></a></div>
                     </div>
                 </div>
             </li>
             <li>
-                <a href="http://www.willbescop.net/movie/event.html?event_cd=Off_181129_p&topMenuType=F" target="_blank"><img src="{{ img_url('cop/quick/quick_190110.jpg') }}"></a>
+                <a href="http://www.willbescop.net/movie/event.html?event_cd=Off_181129_p&topMenuType=F" target="_blank"><img src="{{ img_url('cop/quick/quick_190110.jpg') }}" alt="배너명"></a>
+            </li>
+            <li>
+                <a href="http://www.willbescop.net/movie/event.html?event_cd=Off_181129_p&topMenuType=F" target="_blank"><img src="{{ img_url('cop/quick/quick_talk.jpg') }}" alt="배너명"></a>
             </li>
         </ul>
     </div>

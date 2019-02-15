d6f4808fc3edc10734a30e069e84151fa704e82e
eddie <eddie@willbes.com>
2019-02-15 16:16:10 +0900

    다운로드오류수정

--- views/wbs/cms/lecture/create.blade.php
+++ views/wbs/cms/lecture/create.blade.php
@@ -257,7 +257,8 @@
                                     @if(empty($row['wUnitAttachFile']) === false)
                                         <br>
                                         <p class="form-control-static ml-10 mr-10">
-                                            [ <a href="{{site_url('/cms/lecture/download/').urlencode($data['wAttachPath'].$row['wUnitAttachFile']).'/'.urlencode($row['wUnitAttachFileReal'])}}" target="_blank">{{ $row['wUnitAttachFileReal'] }}</a> ]
+                                            [ <a href="{{site_url('/cms/lecture/download/').'?filename='.urlencode($data['wAttachPath'].$row['wUnitAttachFile']).'&filename_ori='.urlencode($row['wUnitAttachFileReal'])}}" target="_blank">{{ $row['wUnitAttachFileReal'] }}</a> ]
+                                            
                                         </p>
                                     @endif
                                 </td>

b1b88b131f035ed72758f923875e112cdc2cb54d
신봉석 <bsshin@willbes.com>
2019-02-20 14:53:59 +0900

    관리자 > SMS 발송 수정

--- application/controllers/lcms/auth/Login.php
+++ application/controllers/lcms/auth/Login.php
@@ -265,7 +265,7 @@ class Login extends \app\controllers\BaseController
     private function _sendSmsAuthNumber($to, $auth_number)
     {
         try {
-            $this->load->library('sendsms');
+            $this->load->library('sendSms');
             // 070-4006-7122 최진영차장 직통 번호
             if($this->sendsms->send($to, '윌비스 본인 인증 번호입니다. ['.$auth_number.']를 입력해 주십시요.', '070-4006-7122') === false){
                 throw new \Exception('메세지 발송에 실패했습니다.\n다시 한번 시도해 주십시요.');

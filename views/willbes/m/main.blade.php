98cb0504fb8b896402298ad7fe6bb6b73e0ee113
신봉석 <bsshin@willbes.com>
2019-03-06 18:28:12 +0900

    프런트 > 도메인 변경 적용

--- views/willbes/m/main.blade.php
+++ views/willbes/m/main.blade.php
@@ -5,12 +5,12 @@
     <div id="Container" class="Container c_both mb20">
         <div class="introBox">
             <div class="Tit NSK">내 인생의 모든 시험, 윌비스로 합격하다!</div>
-            <a href="{{ front_app_url('/home/index', 'cop') }}">
+            <a href="{{ front_app_url('/home/index', 'police') }}">
                 <img src="{{ img_url('m/intro/m_main_hub01.gif') }}" alt="신광은경찰 온라인">
             </a>
         </div>
         <div class="introBox">
-            <a href="{{ front_app_url('/home/index', 'gosi') }}">
+            <a href="{{ front_app_url('/home/index', 'pass') }}">
                 <img src="{{ img_url('m/intro/m_main_hub02.gif') }}" alt="공무원 온라인">
             </a>
         </div>

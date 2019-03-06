4abd71fe439f292cea6fe065784a6616fb0ac005
김창용 <winmir@willbes.com>
2019-03-06 13:52:19 +0900

    강사모집 페이지

---
+++ views/html/profRecruit.blade.php
@@ -0,0 +1,83 @@
+@extends('willbes.pc.layouts.master')
+
+@section('content')
+<!-- Container -->
+<div id="Container" class="subContainer widthAuto c_both">
+    <div class="Menu NSK c_both">
+        <h3>
+            <ul class="menu-Tit">
+                <li class="Tit">경찰<span class="row-line">|</span></li>
+                <li class="subTit">일반경찰</li>
+            </ul>
+            <ul class="menu-List">
+                <li>
+                    <a href="{{ site_url('/home/html/prof') }}">교수진소개</a>
+                </li>
+                <li>
+                    <a href="#none">PASS</a>
+                </li>
+                <li class="dropdown">
+                    <a href="{{ site_url('/home/html/package1') }}">패키지</a>
+                    <div class="drop-Box list-drop-Box">
+                        <ul>
+                        <li class="Tit">패키지</li>
+                            <li><a href="{{ site_url('/home/html/package1') }}">추천 패키지</a></li>
+                            <li><a href="{{ site_url('/home/html/package2') }}">선택 패키지</a></li>
+                            <li><a href="{{ site_url('/home/html/diypackage') }}">DIY 패키지</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <a href="{{ site_url('/home/html/list') }}">단강좌</a>
+                </li>
+                <li>
+                    <a href="#none">무료강좌</a>
+                </li>
+                <li class="dropdown">
+                    <a href="{{ site_url('/home/html/mocktest1') }}">수험정보</a>
+                    <div class="drop-Box list-drop-Box">
+                        <ul>
+                            <li class="Tit">수험정보</li>
+                            <li><a href="{{ site_url('/home/html/mocktest1') }}">시험공고</a></li>
+                            <li><a href="{{ site_url('/home/html/mocktest2') }}">수험뉴스</a></li>
+                            <li><a href="{{ site_url('/home/html/mocktest3') }}">기출문제</a></li>
+                            <li><a href="#none">공무원가이드</a></li>
+                            <li><a href="#none">초보합격전략</a></li>
+                            <li><a href="{{ site_url('/home/html/mocktest6_1') }}">모의고사</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="dropdown">
+                    <a href="{{ site_url('/home/html/event_ing') }}">이벤트</a>
+                    <div class="drop-Box list-drop-Box">
+                        <ul>
+                            <li class="Tit">이벤트</li>
+                            <li><a href="{{ site_url('/home/html/event_ing') }}">진행중인 이벤트</a></li>
+                            <li><a href="{{ site_url('/home/html/event_end') }}">마감된 이벤트</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="Acad">
+                    <a href="#none">경찰학원 <span class="arrow-Btn">></span></a>
+                </li>
+            </ul>
+        </h3>
+    </div>
+    <div class="Depth">
+        <img src="{{ img_url('sub/icon_home.gif') }}"> 
+        <span class="1depth">
+            <span class="depth-Arrow">></span><strong>수험정보</strong>
+            <span class="depth-Arrow">></span><strong>모의고사</strong>
+        </span>
+    </div>
+    <div class="Content p_re">
+        <div>
+            <img src="{{ img_url('sub/profRecruit.jpg') }}" alt="강사모집">
+        </div>
+    </div>
+    <div class="Quick-Bnr ml20">
+        <img src="{{ img_url('sample/banner_180605.jpg') }}">     
+    </div>
+</div>
+<!-- End Container -->
+@stop
--- views/html/sample.blade.php
+++ views/html/sample.blade.php
@@ -286,6 +286,8 @@
                 <li><a href="{{ site_url('/home/html/cscenter6_2') }}" target="_blank">고객센터 > 학습 프로그램설치 page 이동</a></li>
                 <li><a href="{{ site_url('/home/html/cscenter7') }}" target="_blank">고객센터 > 부정사용자 규제 page 이동</a></li>
                 <li class="c_both"></li>
+                <li><a href="{{ site_url('/home/html/profRecruit') }}" target="_blank">강사모집</a></li>
+                <li class="c_both"></li>
                 <li><a href="{{ site_url('/home/html/test_popup') }}" target="_blank">*** test popup page 이동</a></li>
                 <li><a href="{{ site_url('/home/html/test_popup2') }}" target="_blank">*** test 인증하기 page 이동</a></li>
                 <li><a href="{{ site_url('/home/html/test_popup3') }}" target="_blank">*** 전국모의고사 결제완료 popup page 이동</a></li>

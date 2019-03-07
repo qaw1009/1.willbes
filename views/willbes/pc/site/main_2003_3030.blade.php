9c294a73da2e98a0fb802f7a7d341197c2f3b4ce
신봉석 <bsshin@willbes.com>
2019-03-07 18:14:37 +0900

    프런트 > 공무원 군무원, 부사관 메인 추가

--- views/willbes/pc/site/main_2003_3030.blade.php
+++ views/willbes/pc/site/main_2003_3030.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <!-- Container -->
-    <div id="Container" class="Container gp c_both">
+    <div id="Container" class="Container noncom c_both">
         <!-- site nav -->
         @include('willbes.pc.layouts.partial.site_menu')
 
@@ -27,39 +27,26 @@
         </div>
 
         <div class="Section mt30">
-            <div class="widthAuto">
+            <div class="widthAuto bSlider">
                 @if(empty($data['arr_main_banner']['메인_띠배너']) === false)
-                    @php $link_url = '#none'; $last_banner = end($data['arr_main_banner']['메인_띠배너']); @endphp
-                    @if(empty($last_banner['LinkUrl']) === false)
-                        @php $link_url = front_app_url('/banner/click?banner_idx=' . $last_banner['BIdx'] . '&return_url=' . urlencode($last_banner['LinkUrl']) . '&link_url_type=' . urlencode($last_banner['LinkUrlType']), 'www'); @endphp
-                    @endif
-                    <a href="{{ $link_url }}" target="_{{ $last_banner['LinkType'] }}"><img src="{{ $last_banner['BannerFullPath'] . $last_banner['BannerImgName'] }}" alt="{{ $last_banner['BannerName'] }}"></a>
+                <ul class="sliderPlay">
+                    @php $link_url = '#none'; @endphp
+                    @foreach($data['arr_main_banner']['메인_띠배너'] as $row)
+                        @if(empty($row['LinkUrl']) === false)
+                            @php $link_url = front_app_url('/banner/click?banner_idx=' . $row['BIdx'] . '&return_url=' . urlencode($row['LinkUrl']) . '&link_url_type=' . urlencode($row['LinkUrlType']), 'www'); @endphp
+                        @endif
+                        <li><a href="{{ $link_url }}" target="_{{ $row['LinkType'] }}"><img src="{{ $row['BannerFullPath'] . $row['BannerImgName'] }}" alt="{{ $row['BannerName'] }}"></a></li>
+                    @endforeach
+                </ul>
                 @endif
             </div>
         </div>
 
         <div class="Section">
             <div class="widthAuto">
-                <img src="{{ img_url('gpgosi/visual/visual_tit01.jpg') }}" alt="빠른 합격을 위한 윌비스 군무원 추천강좌">
+                <img src="{{ img_url('gosi_noncom/visual/visual_tit01.jpg') }}" alt="오랜 경험과 노하우를 가진 전문 교수진">
                 <ul class="PBcts">
                     @for($i=1; $i<=4; $i++)
-                        @if(empty($data['arr_main_banner']['메인_미들'.$i]) === false)
-                            @php $link_url = '#none'; $last_banner = end($data['arr_main_banner']['메인_미들'.$i]); @endphp
-                            @if(empty($last_banner['LinkUrl']) === false)
-                                @php $link_url = front_app_url('/banner/click?banner_idx=' . $last_banner['BIdx'] . '&return_url=' . urlencode($last_banner['LinkUrl']) . '&link_url_type=' . urlencode($last_banner['LinkUrlType']), 'www'); @endphp
-                            @endif
-                            <li><a href="{{ $link_url }}" target="_{{ $last_banner['LinkType'] }}"><img src="{{ $last_banner['BannerFullPath'] . $last_banner['BannerImgName'] }}" alt="{{ $last_banner['BannerName'] }}"></a></li>
-                        @endif
-                    @endfor
-                </ul>
-            </div>
-        </div>
-
-        <div class="Section">
-            <div class="widthAuto">
-                <img src="{{ img_url('gpgosi/visual/visual_tit02.jpg') }}" alt="빠른 합격을 위한 윌비스 군무원 추천강좌">
-                <ul class="PBctsB">
-                    @for($i=1; $i<=8; $i++)
                         @if(empty($data['arr_main_banner']['메인_교수진'.$i]) === false)
                             @php $link_url = '#none'; $last_banner = end($data['arr_main_banner']['메인_교수진'.$i]); @endphp
                             @if(empty($last_banner['LinkUrl']) === false)
@@ -92,5 +79,22 @@
     </div>
     <!-- End Container -->
 
+    <script type="text/javascript">
+        $(function() {
+            $('.sliderPlay').bxSlider({
+                auto: true,
+                controls: false,
+                pause: 4000,
+                moveSlides:2,
+                minSlides:2,
+                maxSlides:2,
+                slideWidth:1120,
+                autoHover: true,
+                onSliderLoad: function(){
+                    $(".bSlider").css("visibility", "visible").animate({opacity:1});
+                }
+            });
+        });
+    </script>
     {!! popup('657001', $__cfg['SiteCode'], $__cfg['CateCode']) !!}
 @stop

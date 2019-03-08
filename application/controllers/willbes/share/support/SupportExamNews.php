ac4942ea573ded9f1b641ff10fa671cc2021b6fc
최현탁 cht@willbes.com <cht@willbes.com>
2019-03-08 20:02:31 +0900

    프론트 수험정보 게시판 수정

--- application/controllers/willbes/share/support/SupportExamNews.php
+++ application/controllers/willbes/share/support/SupportExamNews.php
@@ -46,13 +46,13 @@ class SupportExamNews extends BaseSupport
             ]
         ];
 
-        /*if ($this->_site_code != config_item('app_intg_site_code')) {
+        if ($this->_site_code != config_item('app_intg_site_code')) {
             $arr_condition = array_merge_recursive($arr_condition, [
                 'LKB' => [
                     'Category_String' => $this->_cate_code
                 ]
             ]);
-        }*/
+        }
 
         $column = 'b.BoardIdx,b.CampusCcd,b.TypeCcd,b.IsBest,b.AreaCcd
                        ,b.Title,b.Content, (b.ReadCnt + b.SettingReadCnt) as TotalReadCnt

0fc0dd543ebb17bfa889a96711f3859057bbfbbe
김창용 <winmir@willbes.com>
2019-03-14 14:38:47 +0900

    프로모션 페이지

--- views/willbes/pc/promotion/2003/1112.blade.php
+++ views/willbes/pc/promotion/2003/1112.blade.php
@@ -110,11 +110,11 @@
         <div class="rLnb">
             <ul class="typeA">
                 <li class="NSK-Black">서비스 바로가기</li>
-                <li><a href="/promotion/index/cate/3001/code/1111" class="menu1" target="_blank">인증센터</a></li>
-                <li><a href="/promotion/index/cate/3001/code/1116" class="menu2" target="_blank">서울 노량진 교육과정</a></li>
-                <li><a href="/promotion/index/cate/3001/code/1115" class="menu3" target="_blank">인천 부평 교육과정</a></li>
-                <li><a href="/promotion/index/cate/3001/code/1117" class="menu4" target="_blank">부산 서면 교육과정</a></li>
-                <li><a href="/promotion/index/cate/3001/code/1113" class="menu5">윌비스 PASS</a></li>
+                <li><a href="{{ site_url('/promotion/index/cate/3001/code/1111') }}" class="menu1" target="_blank">인증센터</a></li>
+                <li><a href="{{ site_url('/promotion/index/cate/3001/code/1116') }}" class="menu2" target="_blank">서울 노량진 교육과정</a></li>
+                <li><a href="{{ site_url('/promotion/index/cate/3001/code/1115') }}" class="menu3" target="_blank">인천 부평 교육과정</a></li>
+                <li><a href="{{ site_url('/promotion/index/cate/3001/code/1117') }}" class="menu4" target="_blank">부산 서면 교육과정</a></li>
+                <li><a href="{{ site_url('/promotion/index/cate/3001/code/1113') }}" class="menu5">윌비스 PASS</a></li>
             </ul>            <ul class="typeB">
                 <li class="NSK-Black">전역(예정)간부 가입/인증</li>
                 @if(empty($cert_apply))

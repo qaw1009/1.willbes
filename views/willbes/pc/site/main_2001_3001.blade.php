f648c55dae030315f4a03798c15a3aa0dd4a7a87
김창용 <winmir@willbes.com>
2019-03-15 17:42:31 +0900

    경찰 온라인 메인

--- views/willbes/pc/site/main_2001_3001.blade.php
+++ views/willbes/pc/site/main_2001_3001.blade.php
@@ -8,8 +8,10 @@
 
         <div class="Section MainVisual">
             <div class="widthAuto">
-                <iframe src="https://www.youtube.com/embed/lrZxQV21DE8?rel=0&modestbranding=1&showinfo=1&controls=2" frameborder="0" allowfullscreen=""></iframe>
-                <img src="{{ img_url('cop/visual/visual_190213.jpg') }}" alt="전국 4,000명 동시수강">
+                <!--iframe src="https://www.youtube.com/embed/lrZxQV21DE8?rel=0&modestbranding=1&showinfo=1&controls=2" frameborder="0" allowfullscreen=""></iframe-->
+                <a href="{{ site_url('/promotion/index/cate/3001/code/1019') }}" target="_blank">
+                    <img src="{{ img_url('cop/visual/visual_190213.jpg') }}" alt="전국 4,000명 동시수강">
+                </a>
             </div>
         </div>
 

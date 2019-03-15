082f3dd63b43e01ce94d3ebaa16e58c3d3d42f25
김창용 <winmir@willbes.com>
2019-03-15 17:47:53 +0900

    경찰승진 메인

--- views/willbes/pc/site/main_2001_3006.blade.php
+++ views/willbes/pc/site/main_2001_3006.blade.php
@@ -7,10 +7,10 @@
         @include('willbes.pc.layouts.partial.site_menu')
 
         <div class="Section MainVisual">
-            <div class="widthAuto">
-                <iframe src="https://www.youtube.com/embed/lrZxQV21DE8?rel=0&modestbranding=1&showinfo=0&&wmode=transparent" frameborder="0" allowfullscreen=""></iframe>
+            <!--iframe src="https://www.youtube.com/embed/lrZxQV21DE8?rel=0&modestbranding=1&showinfo=1&controls=2" frameborder="0" allowfullscreen=""></iframe-->
+            <a href="{{ site_url('/promotion/index/cate/3001/code/1019') }}" target="_blank">
                 <img src="{{ img_url('cop/visual/visual_190213.jpg') }}" alt="전국 4,000명 동시수강">
-            </div>
+            </a>
         </div>
 
         <div class="Section">

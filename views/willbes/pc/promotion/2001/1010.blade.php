9a59a156f162c226e919357f76f905c1ee04d5d2
최현탁 cht@willbes.com <cht@willbes.com>
2019-03-15 13:08:22 +0900

    프로모션 댓글 기능 적용

--- views/willbes/pc/promotion/2001/1010.blade.php
+++ views/willbes/pc/promotion/2001/1010.blade.php
@@ -268,7 +268,11 @@
         </div>
 
         <!--  이모티콘 댓글 -->
-        @include('html.event_incReplyEmoticon')
+        {{--@include('html.event_incReplyEmoticon')--}}
+        <?php if( empty($data['data_option_ccd']) === false && array_key_exists($arr_base['option_ccd']['comment_list'], $data['data_option_ccd']) === true && array_key_exists($arr_base['comment_use_area']['event'], $data['data_comment_use_area']) === true): ?>
+            <?php echo $this->runChild('willbes.pc.promotion.show_comment_list_partial'); ?>
+        <?php endif; ?>
+
     </div>
     <!-- End Container -->
 

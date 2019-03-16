8f02bdfa21da5f4dfa7c59edd904c4e8f9a1d442
김창용 <winmir@willbes.com>
2019-03-16 18:34:34 +0900

    경찰학원 퀵배너

--- views/willbes/pc/site/main_partial/quick_menu_2002.blade.php
+++ views/willbes/pc/site/main_partial/quick_menu_2002.blade.php
@@ -1,6 +1,6 @@
 <ul>
     <li><a href="{{ front_url('/offinfo/boardInfo/index/78') }}">이달의 개강안내</a></li>
-    <li><a href="{{ front_url('/pass/support/notice/index') }}">공지사항</a></li>
+    <li><a href="{{ front_url('/support/notice/index') }}">공지사항</a></li>
     <li><a href="{{ front_url('/offinfo/boardInfo/index/80') }}">강의 시간표</a></li>
     <li><a href="{{ front_url('/offinfo/boardInfo/index/82') }}">강의실 배정표</a></li>
     <!--li><a href="#map_campus">학원 오시는 길</a></li-->

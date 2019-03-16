5595b1c88bc7daf389ec64d7163f475a3ee007a8
김창용 <winmir@willbes.com>
2019-03-16 18:13:30 +0900

    경학학원 메인 퀵배너

--- views/html/main_cop_acad.blade.php
+++ views/html/main_cop_acad.blade.php
@@ -749,12 +749,14 @@
 
     <div id="QuickMenu" class="MainQuickMenu">
         <ul>
-            <li><a href="#none">이달의 개강안내</a></li>
-            <li><a href="#none">강의 시간표</a></li>
-            <li><a href="#none">강의실 배정표</a></li>
-            <li><a href="#none">학원 오시는 길</a></li>
-            <li><a href="#none">1:1 방문상담</a></li>
-            <li><a href="#none">학원 갤러리</a></li>
+            <li><a href="{{ front_url('/offinfo/boardInfo/index/78') }}">이달의 개강안내</a></li>
+            <li><a href="{{ front_url('/pass/support/notice/index') }}">공지사항</a></li>
+            <li><a href="{{ front_url('/offinfo/boardInfo/index/80') }}">강의 시간표</a></li>
+            <li><a href="{{ front_url('/offinfo/boardInfo/index/82') }}">강의실 배정표</a></li>
+            <!--li><a href="#map_campus">학원 오시는 길</a></li-->
+            <li><a href="{{ front_url('/consultManagement/index') }}">1:1 방문상담</a></li>
+            <li><a href="{{ front_url('/offinfo/gallery/index') }}">학원 갤러리</a></li>
+            <li><a href="{{ front_url('/lecture/index/cate/3001/pattern/free?course_idx=1077') }}" target="_blank">보강동영상</a></li>
         </ul>
     </div>
 </div>

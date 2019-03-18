7baf36d60354ad4a09b3707a98f11610b65eeea4
김창용 <winmir@willbes.com>
2019-03-18 10:44:52 +0900

    경행경채 메인 기출문제 수정

--- views/willbes/pc/site/main_2001_3002.blade.php
+++ views/willbes/pc/site/main_2001_3002.blade.php
@@ -26,16 +26,16 @@
                     <ul class="youtubetab">
                         <li>
                             <a href="#tab1" class="active">
-                                2018년 2차 최종합격생 인터뷰
+                                신의 법칙! 2주만에 1회독?
                                 <span>신광은경찰TV</span>
                             </a>
                         </li>
                         <li>
                             <a href="#tab2">
-                                신의 법칙! 2주만에 1회독?
+                                2018년 2차 최종합격생 인터뷰
                                 <span>신광은경찰TV</span>
                             </a>
-                        </li>
+                        </li>                        
                         <li>
                             <a href="#tab3">
                                 스타강사가 들려주는 시험 꿀팁!
@@ -51,11 +51,11 @@
                     </ul>
                 </div>
                 <div id="tab1" class="youtubeBox">
-                    <iframe src="https://www.youtube.com/embed/GlE9EGMDF98?rel=0&modestbranding=1&showinfo=0" frameborder="0" allowfullscreen=""></iframe>
-                </div>
-                <div id="tab2" class="youtubeBox">
                     <iframe src="https://www.youtube.com/embed/re8w_IFAPS4?rel=0&modestbranding=1&showinfo=0" frameborder="0" allowfullscreen=""></iframe>
                 </div>
+                <div id="tab2" class="youtubeBox">
+                    <iframe src="https://www.youtube.com/embed/GlE9EGMDF98?rel=0&modestbranding=1&showinfo=0" frameborder="0" allowfullscreen=""></iframe>
+                </div>                
                 <div id="tab3" class="youtubeBox">
                     <iframe src="https://www.youtube.com/embed/VEmBnYu8tcc?rel=0&modestbranding=1&showinfo=0" frameborder="0" allowfullscreen=""></iframe>
                 </div>
@@ -207,10 +207,10 @@
                                 </div>
                                 <div class="infoList">
                                     <ul class="List-Table">
-                                        <li><a href="#none"><span>[2018년 2차]</span>경찰공무원(일반/101단/경행) 채용시험 기출</a><span class="btn-more"><a href="{{ site_url('/promotion/index/cate/' . $__cfg['CateCode'] . '/code/1010') }}'" target="_blank">바로가기 ></a></span></li>
-                                        <li><a href="#none"><span>[2018년 1차]</span>경찰공무원(일반/101단/전의경) 채용시험 기출</a><span class="btn-more"><a href="{{ site_url('/promotion/index/cate/' . $__cfg['CateCode'] . '/code/1010') }}'" target="_blank">바로가기 ></a></span></li>
-                                        <li><a href="#none"><span>[2018년 2차]</span>경찰공무원(일반/101단/경행) 채용시험 기출</a><span class="btn-more"><a href="{{ site_url('/promotion/index/cate/' . $__cfg['CateCode'] . '/code/1010') }}'" target="_blank">바로가기 ></a></span></li>
-                                        <li><a href="#none"><span>[2018년 1차]</span>경찰공무원(일반/101단/경행) 채용시험 기출</a><span class="btn-more"><a href="{{ site_url('/promotion/index/cate/' . $__cfg['CateCode'] . '/code/1010') }}'" target="_blank">바로가기 ></a></span></li>
+                                        <li><a href="#none"><span>[2018년 3차]</span>경찰공무원(일반/경행) 채용시험 기출</a><span class="btn-more"><a href="{{ site_url('/promotion/index/cate/' . $__cfg['CateCode'] . '/code/1010') }}" target="_blank">바로가기 ></a></span></li>
+                                        <li><a href="#none"><span>[2018년 2차]</span>경찰공무원(일반/101단/경행) 채용시험 기출</a><span class="btn-more"><a href="{{ site_url('/promotion/index/cate/' . $__cfg['CateCode'] . '/code/1010') }}" target="_blank">바로가기 ></a></span></li>
+                                        <li><a href="#none"><span>[2018년 1차]</span>경찰공무원(일반/101단/전의경) 채용시험 기출</a><span class="btn-more"><a href="{{ site_url('/promotion/index/cate/' . $__cfg['CateCode'] . '/code/1010') }}" target="_blank">바로가기 ></a></span></li>
+                                        <li><a href="#none"><span>[2017년 2차]</span>경찰공무원(일반/101단/경행) 채용시험 기출</a><span class="btn-more"><a href="{{ site_url('/promotion/index/cate/' . $__cfg['CateCode'] . '/code/1010') }}" target="_blank">바로가기 ></a></span></li>
                                     </ul>
                                 </div>
                             </div>
@@ -254,8 +254,8 @@
     <!-- End Container -->
 
     <script type="text/javascript">
-        var tab1_url = "https://www.youtube.com/embed/GlE9EGMDF98?rel=0&modestbranding=1&showinfo=0";
-        var tab2_url = "https://www.youtube.com/embed/re8w_IFAPS4?rel=0&modestbranding=1&showinfo=0";
+        var tab1_url = "https://www.youtube.com/embed/re8w_IFAPS4?rel=0&modestbranding=1&showinfo=0";
+        var tab2_url = "https://www.youtube.com/embed/GlE9EGMDF98?rel=0&modestbranding=1&showinfo=0";        
         var tab3_url = "https://www.youtube.com/embed/VEmBnYu8tcc?rel=0&modestbranding=1&showinfo=0";
         var tab4_url = "https://www.youtube.com/embed/lrZxQV21DE8?rel=0&modestbranding=1&showinfo=0";
 

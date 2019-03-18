fc3b7d2e1a3f12aaf2b2a5eb77cc82b393dfa969
김창용 <winmir@willbes.com>
2019-03-18 18:16:16 +0900

    경찰학원 메인 공지사항 / 시험공고 / 수험뉴스 수정

--- views/willbes/pc/site/main_partial/board_2002.blade.php
+++ views/willbes/pc/site/main_partial/board_2002.blade.php
@@ -1,7 +1,8 @@
 <div class="noticeTabs acad">
     <ul class="tabWrap noticeWrap_acad two">
         <li><a href="#notice1" class="on">공지사항</a></li>
-        <li><a href="#notice2" class="">수험뉴스</a></li>
+        <li><a href="#notice2" class="">시험공고</a></li>
+        <li><a href="#notice3" class="">수험뉴스</a></li>
     </ul>
     <div class="tabBox noticeBox_acad">
         <div id="notice1" class="tabContent p_re">
@@ -22,6 +23,23 @@
             </ul>
         </div>
         <div id="notice2" class="tabContent p_re">
+            <a href="#none" class="f_right btn-add"><img src="{{ img_url('cop/icon_add_big.png') }}" alt="더보기"></a>
+            <ul class="List-Table">
+                @if(empty($data['exam_news']) === true)
+                    <li>등록된 내용이 없습니다.</li>
+                @else
+                    @foreach($data['exam_news'] as $row)
+                        <li>
+                            <a href="{{front_url('/support/examNews/show/cate/'.$__cfg['CateCode'].'?board_idx='.$row['BoardIdx'])}}">
+                                @if($row['IsBest'] == '1') <img src="{{ img_url('cop/icon_hot.png') }}" alt="HOT"> @endif
+                                {{$row['Title']}}
+                            </a>
+                        </li>
+                    @endforeach
+                @endif
+            </ul>
+        </div>
+        <div id="notice3" class="tabContent p_re">
             <a href="#none" class="f_right btn-add"><img src="{{ img_url('cop/icon_add_big.png') }}" alt="더보기"></a>
             <ul class="List-Table">
                 @if(empty($data['exam_news']) === true)

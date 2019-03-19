ef5fdde1d46a84771ff0370e2bf07cd83c834177
김창용 <winmir@willbes.com>
2019-03-19 19:38:07 +0900

    공무원의 A to Z

--- views/willbes/pc/promotion/2003/1084.blade.php
+++ views/willbes/pc/promotion/2003/1084.blade.php
@@ -40,13 +40,12 @@
         .PeMenu li a img.on {display:none}
 
         .skybanner {
-            position:absolute;
-            top:20px;
+            position:fixed;
+            bottom:50px;
             right:10px;
             width:112px;
             z-index:1;
         }
-        .skybanner_sectionFixed {position:fixed; top:20px}
 
     </style>
 
@@ -70,7 +69,7 @@
                 <img src="http://file3.willbes.net/new_gosi/2018/01/EV180110_c1_2.jpg" usemap="#Map171218_c2" border="0"  >
                 <map name="Map171218_c2" >
                     <area shape="rect" coords="158,18,585,103"  href="#none" onfocus="this.blur();" alt="초시생을 위한 합격비법"/>
-                    <area shape="rect" coords="677,22,1055,93" href="event_onGosiPassNote" target="_blank" onfocus="this.blur();" alt="N수생의 합격노트" />
+                    <area shape="rect" coords="677,22,1055,93" href="{{ site_url('/promotion/index/cate/3019/code/1085') }}"" target="_blank" onfocus="this.blur();" alt="N수생의 합격노트" />
                 </map>
                 <div class="why"><img src="http://file3.willbes.net/new_gosi/2018/01/EV180110_top.gif" alt=""/></div>
                 <img src="http://file3.willbes.net/new_gosi/2018/01/EV180110_c2.jpg"  >
@@ -83,13 +82,13 @@
                 <li id="Q1">
                     <img src="http://file3.willbes.net/new_gosi/2018/01/EV180110_c4.jpg" alt="공무원에는 어떤 직렬이 있나요?" usemap="#Map180110_c2" border="0" />
                     <map name="Map180110_c2">
-                        <area shape="rect" coords="111,433,241,484" href="/boardExamInfoOn/board_list.html?topMenuType=O&topMenuGnb=OM_005&topMenu=001&menuID=OM_005_001&BOARDTYPE=1&INCTYPE=list&BOARD_MNG_SEQ=NOTICE_008" target="_blank"   onfocus="this.blur();"/>
+                        <area shape="rect" coords="111,433,241,484" href="{{ site_url('/support/examAnnouncement/index/cate/3019') }}" target="_blank"   onfocus="this.blur();"/>
                     </map>
-                <li id="Q2"><img src="http://file3.willbes.net/new_gosi/2018/01/EV180110_c5.jpg" alt="Q2. 시험은 어떤 식으로 치러지나요?" />
-                <li id="Q3"><img src="http://file3.willbes.net/new_gosi/2018/01/EV180110_c6.jpg" alt="Q3. 올해 시험 일정은 언제인가요?" />
-                <li id="Q4"><img src="http://file3.willbes.net/new_gosi/2018/01/EV180110_c7.jpg" alt="Q4. 시험 공부 계획은 어떻게 세워야 하나요?" />
-                <li id="Q5"><img src="http://file3.willbes.net/new_gosi/2018/01/EV180110_c8.jpg" alt="Q5. 공무원, 경찰, 소방 단기간 합격비법, 영어에 달려있다! " />
                 </li>
+                <li id="Q2"><img src="http://file3.willbes.net/new_gosi/2018/01/EV180110_c5.jpg" alt="Q2. 시험은 어떤 식으로 치러지나요?" /></li>
+                <li id="Q3"><img src="http://file3.willbes.net/new_gosi/2018/01/EV180110_c6.jpg" alt="Q3. 올해 시험 일정은 언제인가요?" /></li>
+                <li id="Q4"><img src="http://file3.willbes.net/new_gosi/2018/01/EV180110_c7.jpg" alt="Q4. 시험 공부 계획은 어떻게 세워야 하나요?" /></li>
+                <li id="Q5"><img src="http://file3.willbes.net/new_gosi/2018/01/EV180110_c8.jpg" alt="Q5. 공무원, 경찰, 소방 단기간 합격비법, 영어에 달려있다! " /></li>
             </ul>
         </div><!--wb_cts02//-->
 
@@ -99,42 +98,42 @@
                 <div class="PeMenu">
                     <ul>
                         <li>
-                            <a href="http://www.willbesgosi.net/001/index.html?topMenuType=O&topMenu=001&topMenuName=9급공무원&topMenuGnb=OM_001" target="_blank" onFocus="this.blur();" >
+                            <a href="{{ site_url('/home/index/cate/3019') }}" target="_blank" onFocus="this.blur();" >
                                 <img src="http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_1.jpg" onmouseover="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_1on.jpg'" onMouseOut="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_1.jpg'" onMouseDown="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_1.jpg'" alt=""  />
                             </a>
                         </li>
                         <li>
-                            <a href="http://www.willbescop.net/main/index.html" target="_blank" onFocus="this.blur();" >
+                            <a href="{{ site_url('/home/index/cate/3020') }}" target="_blank" onFocus="this.blur();" >
                                 <img src="http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_2.jpg" onmouseover="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_2on.jpg'" onMouseOut="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_2.jpg'" onMouseDown="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_2.jpg'" alt=""  />
                             </a>
                         </li>
                         <li>
-                            <a href="http://www.willbesgosi.net/005/index.html?topMenuType=O&topMenu=005&topMenuName=세무/관세&topMenuGnb=OM_001" target="_blank" onFocus="this.blur();" >
+                            <a href="{{ site_url('/home/index/cate/3022') }}" target="_blank" onFocus="this.blur();" >
                                 <img src="http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_3.jpg" onmouseover="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_3on.jpg'" onMouseOut="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_3.jpg'" onMouseDown="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_3.jpg'" alt=""  />
                             </a>
                         </li>
                         <li>
-                            <a href="http://www.willbesgosi.net/018/index.html?topMenuType=O&topMenu=018&topMenuName=법원직 김동진팀&topMenuGnb=OM_001&topMenuGnb=OM_001" target="_blank" onFocus="this.blur();" >
+                            <a href="{{ site_url('/home/index/cate/3035') }}" target="_blank" onFocus="this.blur();" >
                                 <img src="http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_4.jpg" onmouseover="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_4on.jpg'" onMouseOut="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_4.jpg'" onMouseDown="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_4.jpg'" alt=""  />
                             </a>
                         </li>
                         <li class="2line">
-                            <a href="http://www.willbesgosi.net/006/index.html?topMenuType=O&topMenu=006&topMenuName=소방공무원&topMenuGnb=OM_001" target="_blank" onFocus="this.blur();" >
+                            <a href="{{ site_url('/home/index/cate/3023') }}" target="_blank" onFocus="this.blur();" >
                                 <img src="http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_5.jpg" onmouseover="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_5on.jpg'" onMouseOut="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_5.jpg'" onMouseDown="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_5.jpg'" alt=""  />
                             </a>
                         </li>
                         <li>
-                            <a href="/011/index.html?topMenuType=O&topMenu=011&topMenuName=기술직&topMenuGnb=OM_001" target="_blank" onFocus="this.blur();" >
+                            <a href="{{ site_url('/home/index/cate/3028') }}" target="_blank" onFocus="this.blur();" >
                                 <img src="http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_6.jpg"  onmouseover="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_6on.jpg'" onMouseOut="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_6.jpg'" onMouseDown="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_6.jpg'" alt=""  />
                             </a>
                         </li>
                         <li>
-                            <a href="http://www.willbesgosi.net/007/index.html??topMenuType=O&topMenu=007&topMenuName=군무원&topMenuGnb=OM_001" target="_blank" onFocus="this.blur();" >
+                            <a href="{{ site_url('/home/index/cate/3024') }}" target="_blank" onFocus="this.blur();" >
                                 <img src="http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_7.jpg" onmouseover="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_7on.jpg'" onMouseOut="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_7.jpg'" onMouseDown="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_4.jpg'" alt=""  />
                             </a>
                         </li>
                         <li>
-                            <a href="http://www.willbesgosi.net/main/index.html" target="_blank" onFocus="this.blur();" >
+                            <a href="{{ site_url('/home/index/cate/3019') }}" target="_blank" onFocus="this.blur();" >
                                 <img src="http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_8.jpg" onmouseover="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_8on.jpg'" onMouseOut="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_8.jpg'" onMouseDown="this.src='http://file3.willbes.net/new_gosi/2018/01/EV180110_c10_8.jpg'" alt=""  />
                             </a>
                         </li>

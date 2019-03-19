ba2d4bc8715f832c704ad9e37e721bdd63d67c18
김창용 <winmir@willbes.com>
2019-03-19 15:58:03 +0900

    모의고사 버튼 텍스트 수정

--- views/willbes/pc/classroom/mock/exam/winpopup_step3.blade.php
+++ views/willbes/pc/classroom/mock/exam/winpopup_step3.blade.php
@@ -99,7 +99,7 @@
                     </span>
                         <span>
                         <button type="submit" onclick="goStep2()" class="btnAuto130 h36 mem-Btn bg-blue bd-dark-blue strong">
-                            <span class="strong">답안다시확인</span>
+                            <span class="strong">모의고사 메인 이동</span>
                         </button>
                     </span>
                     </div>

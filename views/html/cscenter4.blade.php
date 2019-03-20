df26fc0142cd5d924135c3dbf883307bfe5091ff
김창용 <winmir@willbes.com>
2019-03-20 09:33:31 +0900

    고객센터 > 사이트 이용가이드

--- views/html/cscenter4.blade.php
+++ views/html/cscenter4.blade.php
@@ -57,26 +57,25 @@
                         <ul class="tabWrap tabcsDepth2 tab_Guide p_re NGEB">
                             <li class="w-guide1">
                                 <a class="qBox on" href="#guide1"><span>주요메뉴 안내</span></a>
-                                <div class="subBox on">
-                                    <!--dl>
+                                <!--div class="subBox on">
+                                    <dl>
                                         <dt><button type="submit" onclick="">네비게이션</button><span class="row-line">|</span></dt>
                                         <dt><button type="submit" onclick="">강좌검색</button><span class="row-line">|</span></dt>
                                         <dt><button type="submit" onclick="">HOT 클릭</button><span class="row-line">|</span></dt>
                                         <dt><button type="submit" onclick="">수험정보</button><span class="row-line">|</span></dt>
                                         <dt><button type="submit" onclick="">이벤트</button></dt>
-                                    </dl-->
-                                    
-                                </div>
+                                    </dl>                                    
+                                </div-->
                             </li>
                             <li class="w-guide2">
                                 <a class="qBox" href="#guide2"><span>회원가입</span></a>
-                                <div class="subBox">
-                                    <!--dl>
+                                <!--div class="subBox">
+                                    <dl>
                                         <dt><button type="submit" onclick="">전체</button><span class="row-line">|</span></dt>
                                         <dt><button type="submit" onclick="">결제</button><span class="row-line">|</span></dt>
                                         <dt><button type="submit" onclick="">환불</button></dt>
-                                    </dl-->                                    
-                                </div>
+                                    </dl>                                    
+                                </div-->
                             </li>
                             <li class="w-guide3">
                                 <a class="qBox" href="#guide3"><span>수강신청</span></a>

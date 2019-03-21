14f1154df17d1370d6a6c4cba91e79c04290c4d2
김창용 <winmir@willbes.com>
2019-03-21 14:44:07 +0900

    통합회원가입 완료 > 쿠폰지급

--- views/willbes/pc/member/join/step3.blade.php
+++ views/willbes/pc/member/join/step3.blade.php
@@ -18,7 +18,15 @@
                 <span class="tx-blue">{{$MemName}}</span>님, <strong>윌비스 통합 회원 가입을 환영합니다.</strong></br>
                 아이디<span class="tx-blue"> {{$MemId}}</span>로 모든 윌비스 서비스를 이용하실 수 있습니다.
             </div>
-            <img class="mt70" src="{{ img_url('login/willbes_welcome.jpg') }}">
+            <div class="tx-center"><img class="mt70" src="{{ img_url('login/willbes_welcome.jpg') }}"></div>
+            <div class="info-Txt info-Txt-Wrap tx-black mt60">
+                현재 웰컴팩 이벤트 중입니다.<br>
+                특별한 혜택을 받으시려면 아래 서비스를 선택해주세요.
+                <div class="mt10">
+                    <input name="cp1" type="radio" value="" id="cp1" /><label for="cp1"> 경찰</label>
+                    <input name="cp1" type="radio" value="" id="cp2" /><label for="cp2"> 공무원</label>
+                </div>            
+            </div>
             @if($ismobile == false)
                 <div class="info-Txt info-Txt-Wrap tx-black mt60">
                     <strong class="tx-gray">시작할 서비스를 선택해 주세요</strong>

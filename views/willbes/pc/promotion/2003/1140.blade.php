e3d44d6a8c9f986680e8bc725b373ad61f0b7a7c
김창용 <winmir@willbes.com>
2019-03-22 14:07:22 +0900

    공무원 온라인 풀캐어 서비스

--- views/willbes/pc/promotion/2003/1140.blade.php
+++ views/willbes/pc/promotion/2003/1140.blade.php
@@ -23,30 +23,43 @@
             background:url(http://file3.willbes.net/new_gosi/2019/03/EV190320Y_top_bg.jpg) no-repeat center top
         }
         
-        .evtMenu {width:100%; background:#36374d;}
-        .evtMenu ul {width:1120px; margin:0 auto}
+        .evtMenu {width:100%; background:#36374d; border-bottom:5px solid #e2be43}
+        .evtMenu ul {width:1120px; margin:0 auto; border-left:1px solid #686063}
         .evtMenu li {display:inline; float:left; width:25%}
-        .evtMenu li a {display:block; text-align:center; padding:30px 0; color:#868791; font-size:150%; font-weight:900; background:#36374d; margin-right:1px}
-        .evtMenu li:last-child a {margin:0}
+        .evtMenu li a {
+            display:block; text-align:center; padding:30px 0; color:#868791; font-size:150%; font-weight:900; 
+            background:#36374d; border-right:1px solid #686063;
+        }  
         .evtMenu li a span {padding:3px 10px; border-radius:15px; background:#868791; color:#36374d; font-weight:normal; font-size:70%}
-        .evtMenu li a div {margin-top:5px}
+        .evtMenu li a div {margin-top:8px}
         .evtMenu li:hover a,
         .evtMenu li.active a {background:#e2be43; color:#fff}
         .evtMenu li:hover a span,
         .evtMenu li.active a span {background:#fff; color:#e2be43}
         .evtMenu ul:after {content:""; display:block; clear:both}
-        
+
+        .tabCts {
+            position:relative; width:1120px; margin:0 auto; 
+        }
+        .download span {position:absolute; display:block; width:72px; height:24px; line-height:24px; text-align:center; z-index:1}
+        .download span:nth-child(1) {left:128px; top:711px}
+        .download span:nth-child(2) {left:326px; top:711px}
+        .download span:nth-child(3) {left:534px; top:711px}
+        .download span:nth-child(4) {left:720px; top:711px}
+        .download span:nth-child(5) {left:900px; top:711px}
+        .download span a {display:block; color:#fff; background:#d18f04; border-radius:14px;}
+        .download span a:hover {background:#e50001}
         
     </style>
 
 
     <div class="evtContent" id="evtContainer">      
 
-        <div class="evtTop" >
+        <div class="evtCtnsBox evtTop" >
             <img src="http://file3.willbes.net/new_gosi/2019/03/EV190320Y_top.jpg" alt="2019 국가직 9급 풀캐어 서비스" />
         </div>
 
-        <div class="evtMenu" id="evtMenu">                
+        <div class="evtCtnsBox evtMenu NG" id="evtMenu">                
             <ul>
                 <li class="active">
                     <a href="#tab1">
@@ -76,27 +89,51 @@
             </ul>
         </div>
 
-        <!--이벤트설명-->
-        <div class="skybanner2">
-            <img src="http://file3.willbes.net/new_cop/On_Transfer/EV190320_p_sky2.png" alt="타 학원 수강 인증 시, PASS 수강료 지원">
-        </div>
 
-        <div class="evtCtnsBox wb_top" id="transfer">
-            <img src="http://file3.willbes.net/new_cop/On_Transfer/EV190320_p1.png" alt="합격환승"  />
-        </div>
+        <!--최종 마무리 전략-->
+        <div id="tab1" class="tabCts">
+            <div class="download">		
+                <!--국어-->
+                <span>
+                    <!--a href="javascript:alert('로그인을 해주세요.');">다운로드</a-->
+                    <a href="{{ site_url('#none') }}" title="다운로드">다운로드</a>
+                </span>
 
-        <div class="evtCtnsBox wb_cts01" id="sky1">
-            <img src="http://file3.willbes.net/new_cop/On_Transfer/EV190320_p2.png"  alt="합격입니다 " />
+                <!--영어-->
+                <span>
+                    <a href="javascript:alert('로그인을 해주세요.');">다운로드</a>
+                    <!--a href="{{ site_url('#none') }}" title="다운로드">다운로드</a-->
+                </span>
+                    
+                <!--한국사-->
+                <span>
+                    <a href="javascript:alert('로그인을 해주세요.');">다운로드</a>
+                    <!--a href="{{ site_url('#none') }}" title="다운로드">다운로드</a-->
+                </span>
+    
+                <!--행정법-->
+                <span>
+                    <a href="javascript:alert('로그인을 해주세요.');">다운로드</a>
+                    <!--a href="{{ site_url('#none') }}" title="다운로드">다운로드</a-->
+                </span>
+                    
+                <!--행정학-->
+                <span>
+                    <a href="javascript:alert('로그인을 해주세요.');">다운로드</a>
+                    <!--a href="{{ site_url('#none') }}" title="다운로드">다운로드</a-->
+                </span>
+                <img src="http://file3.willbes.net/new_gosi/2019/03/EV190320Y_01_1.jpg" alt="풀캐어 강사진" />            
+            </div>
+               
+            <img src="http://file3.willbes.net/new_gosi/2019/03/EV190320Y_01_2.jpg" alt="점수 10점 상승을 위한 시험 전 유의사항/최상의 컨디션을 위한 시험 당일 유의사항" /><br>
+            <img src="http://file3.willbes.net/new_gosi/2019/03/EV190320Y_01_3.jpg" alt="응시자 준수사항 및 필기장소 안내" />
+            <div class="youtube">
+                <iframe class="youtubePlayer" src="https://www.youtube.com/embed/gpppIN1ISaw?rel=0" frameborder="0" allowfullscreen></iframe>
+            </div>
+            <div><a href="http://gosi.go.kr/cop/bbs/selectBoardList.do?bbsId=BBSMSTR_000000000131" target="_blank"><img src="http://file3.willbes.net/new_gosi/2019/03/EV190320Y_btn01.png" alt="필기시험 장소 안내 바로가기" /></a> </div>  
         </div>
+    </div>
 
-        <div class="evtCtnsBox wb_cts02" >
-            <img src="http://file3.willbes.net/new_cop/On_Transfer/EV190320_p3_1.png" alt="수강료지원" id="sky2"/>
-            <img src="http://file3.willbes.net/new_cop/On_Transfer/EV190320_p3_2.png" alt="타학원 수강 인증" usemap="#rebound1" />
-            <map name="rebound1">
-                <area shape="rect" coords="273,428,698,507" href="javascript:doEvent()" onfocus='this.blur()' alt="타학원 수강 인증">
-            </map>  
-            <img src="http://file3.willbes.net/new_cop/On_Transfer/EV190320_p3_3.png" alt="이벤트참여방법" id="sky3"/>
-        </div>
               
     </div>
     <!-- End Container --> 

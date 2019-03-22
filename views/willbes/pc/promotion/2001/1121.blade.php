da805a1df9f5c4e0c8d4136259634e3a52faa350
김창용 <winmir@willbes.com>
2019-03-22 16:55:29 +0900

    경찰 전역간부

--- views/willbes/pc/promotion/2001/1121.blade.php
+++ views/willbes/pc/promotion/2001/1121.blade.php
@@ -158,11 +158,11 @@
         <div class="rLnb">
             <ul class="typeA">
                 <li class="NSK-Black">서비스 바로가기</li>
-                <li><a href="https://pass.willbes.net/promotion/index/cate/3019/code/1111" class="menu1" target="_blank">인증센터</a></li>
-                <li><a href="https://pass.stage.willbes.net/promotion/index/cate/3019/code/1116" class="menu2" target="_blank">서울 노량진 교육과정</a></li>
-                <li><a href="https://pass.stage.willbes.net/promotion/index/cate/3019/code/1115" class="menu3" target="_blank">인천 부평 교육과정</a></li>
-                <li><a href="https://pass.stage.willbes.net/promotion/index/cate/3019/code/1117" class="menu4" target="_blank">부산 서면 교육과정</a></li>
-                <li><a href="https://pass.stage.willbes.net/promotion/index/cate/3019/code/1113" class="menu5">윌비스 PASS</a></li>
+                <li><a href="{{ app_url('/promotion/index/cate/3019/code/1111', 'pass') }}" class="menu1" target="_blank">인증센터</a></li>
+                <li><a href="{{ app_url('/promotion/index/cate/3019/code/1116', 'pass') }}" class="menu2" target="_blank">서울 노량진 교육과정</a></li>
+                <li><a href="{{ app_url('/promotion/index/cate/3019/code/1115', 'pass') }}" class="menu3" target="_blank">인천 부평 교육과정</a></li>
+                <li><a href="{{ app_url('/promotion/index/cate/3019/code/1117', 'pass') }}" class="menu4" target="_blank">부산 서면 교육과정</a></li>
+                <li><a href="{{ app_url('/promotion/index/cate/3019/code/1113', 'pass') }}" class="menu5">윌비스 PASS</a></li>
             </ul>
 
             <ul class="typeB">

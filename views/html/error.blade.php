57a1ac1972cb11a2124e05c1168ade20d2322543
김창용 <winmir@willbes.com>
2019-03-23 20:11:35 +0900

    에러페이지

--- views/html/error.blade.php
+++ views/html/error.blade.php
@@ -14,54 +14,45 @@ body {
 	background-color: #fff;
 	margin: 40px;
 	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
-
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
-
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
-
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
 }
 
 #container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
+	margin: 100px;
+	/*border: 1px solid #D0D0D0;
+	box-shadow: 0 0 8px #D0D0D0;*/
+	text-align:center
+	
 }
-
-p {
-	margin: 12px 15px 12px 15px;
-}
+#container div {font-size:14px; margin-top:20px; color:#000; line-height:1.5}
+#container div strong {font-size:20px; font-weight:bold; color:#1087ef; font-family: Consolas, Monaco, Courier New, Courier, monospace}
+#container div span { font-weight:bold}
+#container div p {margin-top:20px}
+#container div a {
+	display:inline-block;
+	height:30px; line-height:30px;
+	border:1px solid #363636;
+	color:#363636;
+	padding:0 10px;
+	font-size:12px;
+	text-decoration:none;
+	}
+#container div a:hover {
+	background:#363636;
+	color:#fff;
+	}
 </style>
 </head>
 <body>
 	<div id="container">
         <?php #요기에 작업하면 됩니다. ?>
-        <img src="{{ img_url('../public/img/willbes/error_404.png') }}" alt="404 error">   
+        <img src="../../public/img/error_404.png" alt="404 error">   
         <div>
         고객센터 <strong>1544-5006</strong><Br>
-        평일 09:00 ~ 18:00 / 토요일 09:00 ~ 13:00
+        <span>평일 09:00 ~ 18:00 / 토요일 09:00 ~ 13:00</span>
+        <p>
+        	<a href="#none">메인으로 &gt;</a>
+            <a href="#none">이전으로 &gt;</a>
+        </p>
         </div>     
 	</div>
 </body>

8f65899f231474a0b31ac05deced111dfe50cd77
김창용 <winmir@willbes.com>
2019-03-25 01:55:34 +0900

    모바일

--- views/html/m/layouts/header.blade.php
+++ views/html/m/layouts/header.blade.php
@@ -8,23 +8,25 @@
 
     <title>모바일 윌비스 통합 사이트</title>
 
-    <!-- CSS -->
-    <!-- Slider jQuery -->
-    <!--link rel="stylesheet" href="/public/vendor/jquery/swiper/swiper.css"-->
-    <link rel="stylesheet" href="/public/vendor/jquery/swiper/swiper.min.css">
-    <!--script src="/public/vendor/jquery/swiper/swiper.js"></script-->
-    <script src="/public/vendor/jquery/swiper/swiper.min.js"></script>
+    <!-- CSS -->    
     <!-- bootstrap-datepicker -->
     <link rel="stylesheet" href="/public/vendor/bootstrap/datepicker/css/bootstrap-datepicker.standalone.min.css">
     <!-- Custom Theme Style -->
     <link href="/public/css/willbes/basic.css" rel="stylesheet">
     <link href="/public/css/willbes/m/style.css" rel="stylesheet">
-    <!--// CSS -->
+    
+    <!-- Slider jQuery -->
+    <link rel="stylesheet" href="/public/vendor/jquery/swiper/swiper.css">
+    <link rel="stylesheet" href="/public/vendor/jquery/swiper/swiper.min.css">
+    <script src="/public/vendor/jquery/swiper/swiper.js"></script>
+    <script src="/public/vendor/jquery/swiper/swiper.min.js"></script>
     <!-- JAVASCRIPT -->
+    <!--// CSS -->
     <!-- jQuery -->
     <script src="/public/vendor/jquery/v.2.2.3/jquery.min.js"></script>
     <script src="/public/vendor/jquery/form/jquery.form.js"></script>
     <!--// JAVASCRIPT -->
     <!-- Custom Script -->
     <script src="/public/js/willbes/mobile.js?ver={{time()}}"></script>
+    
 </head>

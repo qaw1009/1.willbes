4e5c8fb6e095e4be4762dec0ab294e65b2be3f2a
김창용 <winmir@willbes.com>
2019-03-24 15:57:55 +0900

    경학학원 메인, 공무원 메인 퀵배너

--- views/html/main_gosi_online.blade.php
+++ views/html/main_gosi_online.blade.php
@@ -432,6 +432,41 @@
     </div>
     <!-- CS센터 //-->
 
+    <div id="QuickMenu" class="MainQuickMenu">
+        <ul>
+            <li>
+                <div class="QuickDdayBox QuickSlider">
+                    <div class="sliderNum">
+                        <div>
+                            <div class="q_tit">3차 필기시험</div>
+                            <div class="q_day">2018.12.12</div>
+                            <div class="q_dday NSK-Blac">D-5</div>
+                        </div>
+                        <div>
+                            <div class="q_tit">1차 공무원</div>
+                            <div class="q_day">2019.04.05</div>
+                            <div class="q_dday NSK-Blac">D-10</div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>   
+                <div class="QuickSlider">      
+                    <div class="sliderNum">
+                        <div><a href="http://www.willbescop.net/event/movie/event.html?event_cd=On_170911_popup" target="_blank"><img src="{{ img_url('cop/quick/quick_190108.jpg') }}" title="배너명"></a></div>
+                        <div><a href="http://www.willbescop.net/event/arm_event.html?event_cd=On_leaveArmy02_2018&topMenuType=O&EVENT_NO=53" target="_blank"><img src="{{ img_url('cop/quick/quick_190109.jpg') }}" title="배너명"></a></div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="http://www.willbescop.net/movie/event.html?event_cd=Off_181129_p&topMenuType=F" target="_blank"><img src="{{ img_url('cop/quick/quick_190110.jpg') }}" title="배너명"></a>
+            </li>
+            <li>
+                <a href="http://www.willbescop.net/movie/event.html?event_cd=Off_181129_p&topMenuType=F" target="_blank"><img src="{{ img_url('cop/quick/quick_talk.jpg') }}" title="배너명"></a>
+            </li>
+        </ul>
+    </div>
+
 </div>
 <!-- End Container -->
 

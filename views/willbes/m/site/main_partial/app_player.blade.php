fee9f07999f7a96b2ba05419897c06efff0d28c9
하준걸 <hereisme@willbes.com>
2019-03-24 18:26:17 +0900

    마켓 테스트

--- views/willbes/m/site/main_partial/app_player.blade.php
+++ views/willbes/m/site/main_partial/app_player.blade.php
@@ -1,3 +1,3 @@
-<a href="https://play.google.com/store/apps/details?id=net.willbes.willbes" target="_blank">
+<a href="market://details?id=net.willbes.portal" target="_blank">
     <img src="{{ img_url('m/main/icon_app_player.gif') }}">
 </a>

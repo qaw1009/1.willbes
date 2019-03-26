f23e9bca91f17548e4b2c61a791324b40e944581
김창용 <winmir@willbes.com>
2019-03-26 18:24:57 +0900

    공무원 풀캐어

--- views/html/1140_popup.blade.php
+++ views/html/1140_popup.blade.php
@@ -1,10 +1,10 @@
 @extends('willbes.pc.layouts.master_no_sitdbar')
-
-@section('content')
-<!-- Container -->
-
+<link href="/public/css/willbes/basic.css" rel="stylesheet">
+<link href="/public/css/willbes/style.css" rel="stylesheet">
 <style type="text/css">
-	/* 팝업*/	
+    /* 팝업*/
+    .popcontent {padding:20px}
+    .popcontent h3 { font-size:18px; border:2px solid #353348; color:#d39004}	
 	.Layerpop {background:#FFF; border:#4582cd solid 3px; padding:30px;}
 	.Layerpop h1 {text-align:left; font-weight:bold; letter-spacing:-1px; font-size:20px; color:#000; margin-bottom:20px}
 	.Layerpop .tit {font-size:16px; color:#4582cd; font-weight:bold; letter-spacing:1px; text-align:left; padding:0; margin-bottom:10px}
@@ -25,19 +25,19 @@
 	.Layerpop .termsBx01 .st  {margin-top:15px}
 	.Layerpop .termsBx01 ul li p {padding-left:6px}
 	.Layerpop .termsBx01 .span { height:60px; text-align:right}	
-	#gridContainer * {font-family:'Noto Sans KR', Arial, Sans-serif, "나눔고딕", "돋움"; font-weight:normal}
 
 	.popupCts {padding:0 30px}
 	.popupCts span {display:inline-block; color:#000; font-weight:bold; vertical-align:middle}	
 	.popupCts span.red {color:#F00; text-decoration:underline}
 	.popupCts input,
 	.popupCts select {vertical-align:middle; height:26px; line-height:26px}
-	.popupCts input[type=text] {padding:0 2px; height:24px; line-height:24px}
-	
+	.popupCts input[type=text] {padding:0 2px; height:24px; line-height:24px}	
 </style>
 
-<div id="popup" class="Layerpop" >
-    <div class="popcontent">
+@section('content')
+<!-- Container -->
+
+    <div class="popcontent NGR">
         <h3>시험 난이도 설문조사</h3>                 
         <div class="question">
             <p>Q1. 전체적인 시험 체감 난이도 </p>
@@ -53,37 +53,76 @@
         </div> 
                             
         <div class="question">
-            <p>Q2. 과목 시험 체감 난이도 </p>
+            <p>Q2. 공통 과목 시험 체감 난이도 </p>
+            <div class="qBox">
+                <strong>국어</strong>
+                <ul>
+                    <li><label><input type="radio" name=" " id=" " value="1" /> 매우 쉬움</label></li>
+                    <li><label><input type="radio" name=" " id=" " value="2" /> 쉬움</label></li>
+                    <li><label><input type="radio" name=" " id=" " value="3" /> 보통</label></li>
+                    <li><label><input type="radio" name=" " id=" " value="4" /> 어려움</label></li>
+                    <li><label><input type="radio" name=" " id=" " value="5" /> 매우 어려움</label></li>
+                </ul>
+            </div>
+            <div class="qBox">
+                <strong>영어</strong>
+                <ul>
+                    <li><label><input type="radio" name=" " id=" " value="1" /> 매우 쉬움</label></li>
+                    <li><label><input type="radio" name=" " id=" " value="2" /> 쉬움</label></li>
+                    <li><label><input type="radio" name=" " id=" " value="3" /> 보통</label></li>
+                    <li><label><input type="radio" name=" " id=" " value="4" /> 어려움</label></li>
+                    <li><label><input type="radio" name=" " id=" " value="5" /> 매우 어려움</label></li>
+                </ul>
+            </div>
+            <div class="qBox">
+                <strong>한국사</strong>
+                <ul>
+                    <li><label><input type="radio" name=" " id=" " value="1" /> 매우 쉬움</label></li>
+                    <li><label><input type="radio" name=" " id=" " value="2" /> 쉬움</label></li>
+                    <li><label><input type="radio" name=" " id=" " value="3" /> 보통</label></li>
+                    <li><label><input type="radio" name=" " id=" " value="4" /> 어려움</label></li>
+                    <li><label><input type="radio" name=" " id=" " value="5" /> 매우 어려움</label></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="question">
+            <p>Q3. 선택 과목 시험 체감 난이도 </p>
             <div class="qBox">
                 <div>1) 응시한 과목을 선택해 주세요.</div>
                 <ul>
-                    <li><label><input type="checkbox" name="chk_sbj"  id="chk_sbj" value="31" onclick="vwItem();"/>국어</label></li>
-                    <li><label><input type="checkbox" name="chk_sbj"  id="chk_sbj" value="33" onclick="vwItem();"/>한국사</label></li>
-                    <li><label><input type="checkbox" name="chk_sbj"  id="chk_sbj" value="108" onclick="vwItem();"/>헌법</label></li>
-                    <li><label><input type="checkbox" name="chk_sbj"  id="chk_sbj" value="77" onclick="vwItem();"/>행정법</label></li>
-                    <li><label><input type="checkbox" name="chk_sbj"  id="chk_sbj" value="116" onclick="vwItem();"/>행정학</label></li>
-                    <li><label><input type="checkbox" name="chk_sbj"  id="chk_sbj" value="109" onclick="vwItem();"/>경제학</label></li>
-                    <li><label><input type="checkbox" name="chk_sbj"  id="chk_sbj" value="41" onclick="vwItem();"/>회계학</label></li>
-                    <li><label><input type="checkbox" name="chk_sbj"  id="chk_sbj" value="90" onclick="vwItem();"/>세법</label></li>
-                    <li><label><input type="checkbox" name="chk_sbj"  id="chk_sbj" value="117" onclick="vwItem();"/>국제법</label></li>
-                    <li><label><input type="checkbox" name="chk_sbj"  id="chk_sbj" value="115" onclick="vwItem();"/>국제정치학</label></li>               
+                    <li><label><input type="checkbox" name=" "  id=" " value=" " />과목1</label></li>
+                    <li><label><input type="checkbox" name=" "  id=" " value=" " />과목2</label></li>
+                    <li><label><input type="checkbox" name=" "  id=" " value=" " />과목3</label></li>
+                    <li><label><input type="checkbox" name=" "  id=" " value=" " />과목4</label></li>
+                    <li><label><input type="checkbox" name=" "  id=" " value=" " />과목5</label></li>               
                 </ul>                                     
-                <div>2) 응시한 과목의 체감난이도는 어떠셨나요?</div>
-                <div id="vItem" >  
+                <div id="vItem" > 
+                    <strong>과목1</strong>   
                     <ul>
-                        <li>국어 <label><input type="radio" name="POLL_30" id="POLL_30" value="1" /> 매우 쉬움</label></li>
-                        <li>한국사 <label><input type="radio" name="POLL_30" id="POLL_30" value="1" /> 매우 쉬움</label></li>
-                    </ul>                   
+                        <li><label><input type="radio" name=" " id=" " value="1" /> 매우 쉬움</label></li>
+                        <li><label><input type="radio" name=" " id=" " value="2" /> 쉬움</label></li>
+                        <li><label><input type="radio" name=" " id=" " value="3" /> 보통</label></li>
+                        <li><label><input type="radio" name=" " id=" " value="4" /> 어려움</label></li>
+                        <li><label><input type="radio" name=" " id=" " value="5" /> 매우 어려움</label></li>
+                    </ul>
+                    <strong>과목2</strong>   
+                    <ul>
+                        <li><label><input type="radio" name=" " id=" " value="1" /> 매우 쉬움</label></li>
+                        <li><label><input type="radio" name=" " id=" " value="2" /> 쉬움</label></li>
+                        <li><label><input type="radio" name=" " id=" " value="3" /> 보통</label></li>
+                        <li><label><input type="radio" name=" " id=" " value="4" /> 어려움</label></li>
+                        <li><label><input type="radio" name=" " id=" " value="5" /> 매우 어려움</label></li>
+                    </ul>              
                 </div>
             </div>
         </div>
+
                             
         <div class="btnsSt3">
             <a href="javascript:fn_submit();">설문 완료</a>
         </div>    
 
-    </div> <!--popcontent//-->
+    </div> 
 
-</div>
-<!--willbes-Layer-PassBox//-->
 @stop

e7bd5af4ae0c81dd940031cab0079d2903d3bc58
신봉석 <bsshin@willbes.com>
2019-03-26 09:43:45 +0900

    lms > 수강증 수강기간 관련 수정 (상품정보 조회)

--- application/models/lms/pay/BaseOrderModel.php
+++ application/models/lms/pay/BaseOrderModel.php
@@ -25,6 +25,7 @@ class BaseOrderModel extends WB_Model
         'product_sale' => 'lms_product_sale',
         'product_division' => 'lms_product_division',
         'product_r_product' => 'lms_product_r_product',
+        'product_r_sublecture' => 'lms_product_r_sublecture',
         'product_r_autocoupon' => 'lms_product_r_autocoupon',
         'product_professor_concat' => 'vw_product_r_professor_concat',
         'product_professor_concat_repr' => 'vw_product_r_professor_concat_repr',

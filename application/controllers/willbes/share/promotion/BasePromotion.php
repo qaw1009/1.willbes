96861ffcd6390e50642baccab2109d3a0d9c95c6
조규호 <benoo@willbes.com>
2019-03-28 19:34:06 +0900

    쿠폰

--- application/controllers/willbes/share/promotion/BasePromotion.php
+++ application/controllers/willbes/share/promotion/BasePromotion.php
@@ -309,6 +309,7 @@ class BasePromotion extends \app\controllers\FrontController
             }else{
                 //발급여부 확인
                 $check = $this->couponFModel->checkIssueCoupon($give_idx);
+                echo $check;
 
                 if((int)$check > $limit_count) {
                     return $this->json_error("이미 발급받은 쿠폰이 존재합니다.");

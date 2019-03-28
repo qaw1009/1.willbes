4ead69479b9d62c413b831f466074ef35c3e1949
최현탁 cht@willbes.com <cht@willbes.com>
2019-03-28 19:09:20 +0900

    1141 프로모션 수정

--- views/willbes/pc/promotion/2003/1141.blade.php
+++ views/willbes/pc/promotion/2003/1141.blade.php
@@ -376,9 +376,9 @@
     <script type="text/javascript">
         function fn_live(p_type) {
             if(p_type == "hd"){
-                location.href = "http://willbes.flive.skcdn.com/willbeslive/livestreamcop5011/Playlist.m3u8";
+                location.href = "http://willbes.flive.skcdn.com/willbeslive/livestream11011/Playlist.m3u8";
             }else{
-                location.href = "http://willbes.flive.skcdn.com/willbeslive/livestreamcop5012/Playlist.m3u8";
+                location.href = "http://willbes.flive.skcdn.com/willbeslive/livestream11011/Playlist.m3u8";
             }
         }
 

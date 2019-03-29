74e19e8bd4260a4c1e4dbbd480867d6dbed53bbb
최현탁 cht@willbes.com <cht@willbes.com>
2019-03-29 11:52:14 +0900

    프로모션 1141 수정

--- views/willbes/pc/promotion/2003/1141.blade.php
+++ views/willbes/pc/promotion/2003/1141.blade.php
@@ -205,11 +205,11 @@
             <img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_5.png" alt="윌비스가 준비한 수험생 합격 응원 선물 적중 50선 LIVE">
             <div class="tabContaier">
                 <ul>
-                    <li><a href="#tab1" id="tab_css1" class="tabDate {!! (date('YmdHis') > '20190328000000' && date('YmdHis') < '20190328240000') ? 'active' : '' !!}"><img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_t1.png" class="off" alt="3/28 기미진" /><img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_t1_on.png" class="on"  alt="#"/></a></li>
-                    <li><a href="#tab2" id="tab_css2" class="tabDate {!! (date('YmdHis') > '20190329000000' && date('YmdHis') < '20190329240000') ? 'active' : '' !!}"><img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_t2.png" class="off" alt="3/29 한경준" /><img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_t2_on.png"  class="on" alt="#"/></a></li>
-                    <li><a href="#tab3" id="tab_css3" class="tabDate {!! (date('YmdHis') > '20190331000000' && date('YmdHis') < '20180331240000') ? 'active' : '' !!}"><img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_t3.png" class="off" alt="3/31 김덕관" /><img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_t3_on.png"  class="on" alt="#"/></a></li>
-                    <li><a href="#tab4" id="tab_css4" class="tabDate {!! (date('YmdHis') > '20190401000000' && date('YmdHis') < '20180401240000') ? 'active' : '' !!}"><img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_t4.png" class="off" alt="4/1  한덕현" /><img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_t4_on.png" class="on"  alt="#"/></a></li>
-                    <li><a href="#tab5" id="tab_css5" class="tabDate {!! (date('YmdHis') > '20190402000000' && date('YmdHis') < '20180402240000') ? 'active' : '' !!}"><img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_t5.png" class="off" alt="4/2  한세훈" /><img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_t5_on.png" class="on"  alt="#"/></a></li>
+                    <li><a href="#tab1" id="tab_css1" class="tabDate {!! (date('YmdHis') > '20190328000000' && date('YmdHis') < '20190328240000') ? 'active' : '' !!}"><img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_t1.png" class="off" alt="3/28 기미진" /><img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_t1_on.png" class="on" alt="#"/></a></li>
+                    <li><a href="#tab2" id="tab_css2" class="tabDate {!! (date('YmdHis') > '20190329000000' && date('YmdHis') < '20190329240000') ? 'active' : '' !!}"><img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_t2.png" class="off" alt="3/29 한경준" /><img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_t2_on.png" class="on" alt="#"/></a></li>
+                    <li><a href="#tab3" id="tab_css3" class="tabDate {!! (date('YmdHis') > '20190331000000' && date('YmdHis') < '20180331240000') ? 'active' : '' !!}"><img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_t3.png" class="off" alt="3/31 김덕관" /><img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_t3_on.png" class="on" alt="#"/></a></li>
+                    <li><a href="#tab4" id="tab_css4" class="tabDate {!! (date('YmdHis') > '20190401000000' && date('YmdHis') < '20180401240000') ? 'active' : '' !!}"><img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_t4.png" class="off" alt="4/1  한덕현" /><img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_t4_on.png" class="on" alt="#"/></a></li>
+                    <li><a href="#tab5" id="tab_css5" class="tabDate {!! (date('YmdHis') > '20190402000000' && date('YmdHis') < '20180402240000') ? 'active' : '' !!}"><img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_t5.png" class="off" alt="4/2  한세훈" /><img src="http://file3.willbes.net/new_gosi/2019/03/EV190319_t5_on.png" class="on" alt="#"/></a></li>
                 </ul>
                 <div class="tabContents" id="tab1" style="display:none;">
                     <p>
@@ -432,13 +432,11 @@
         $(document).ready(function(){
             $(".tabContents").hide();
             if ($(".tabDate").hasClass('active') == true) {
-                var temp_id = $('.active').attr('href');
-                var target_id = temp_id.replace('#','');
-                $('#'+target_id).css('display','block');
+                var target_id = $('.active').attr('href');
+                $(target_id).css('display','block');
             } else {
                 $(".tabContents:first").show();
             }
-
 
             $(".tabContaier ul li a").click(function(){
                 var activeTab = $(this).attr("href");

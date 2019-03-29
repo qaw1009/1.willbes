2803a3808d92f8b2f0fd2646fd85f2cdaf33fc1b
김창용 <winmir@willbes.com>
2019-03-29 16:39:48 +0900

    경찰 오태진 한국사

--- views/willbes/pc/promotion/2002/1183.blade.php
+++ views/willbes/pc/promotion/2002/1183.blade.php
@@ -20,15 +20,15 @@
 
         /************************************************************/
 
-        .evtTop {background:url(https://static.willbes.net/public/images/promotion/2019/03/1181_top_bg.jpg) no-repeat center top;}
-        .evt01 {background:#fce428 url(https://static.willbes.net/public/images/promotion/2019/03/1181_top_bg.jpg) no-repeat center top; padding-bottom:120px;}
+        .evtTop {background:url(https://static.willbes.net/public/images/promotiont2019/03/1183_top_bg.jpg) no-repeat center top;}
+        .evt01 {background:#fce428 url(https://static.willbes.net/public/images/promotiont2019/03/1183_top_bg.jpg) no-repeat center top; padding-bottom:120px;}
 
     </style>
 
     <div class="p_re evtContent NSK" id="evtContainer">
 
-     <div class="evtCtnsBox evtTop"><img src="https://static.willbes.net/public/images/promotion/2019/03/1181_top.jpg" title="오태진 한국사 3개년기출특강"></div>
-	 <div class="evtCtnsBox evt01"><img src="https://static.willbes.net/public/images/promotion/2019/03/1181_01.jpg" usemap="#Map1181A" title="시험전,수강생공부방법" border="0">
+     <div class="evtCtnsBox evtTop"><img src="https://static.willbes.net/public/images/promotiont2019/03/1183_top.jpg" title="오태진 한국사 3개년기출특강"></div>
+	 <div class="evtCtnsBox evt01"><img src="https://static.willbes.net/public/images/promotiont2019/03/1183_01.jpg" usemap="#Map1181A" title="시험전,수강생공부방법" border="0">
 		<map name="Map1181A">
 		  <area shape="rect" coords="203,909,928,1002" href="#" target="_blank" alt="신청하기">
 		</map>

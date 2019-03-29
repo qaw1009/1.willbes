7580ade877e9b89f0b19ef45b5e3b32811270ebe
김창용 <winmir@willbes.com>
2019-03-29 16:47:14 +0900

    적중 50선

--- views/willbes/pc/promotion/2003/1141.blade.php
+++ views/willbes/pc/promotion/2003/1141.blade.php
@@ -52,7 +52,7 @@
         .wb_cts04 {background:#8f755c;}	
         .wb_cts04 #movieFrame {position:relative; width:980px; height:500px; margin:0 auto; background:url(http://file3.willbes.net/new_gosi/2019/03/EV190319_live_vod_off.png) no-repeat center top;}
         .wb_cts04 .embedWrap {padding-top:13px; width:980px; margin:0 auto}
-        .wb_cts04 .embed-container {position:absolute; padding-bottom:46.25%; height:0; overflow:hidden; width:980px; height:auto; margin:0 auto}        
+        .wb_cts04 .embed-container {position:absolute; /*padding-bottom:46.25%;*/ height:0; overflow:hidden; width:100%; height:auto; margin:0 auto}        
         .wb_cts04 .mobileCh {position:absolute; bottom:0; }
         .wb_cts04 .mobileCh li {width:50%; display:inline; float:left;}
         .wb_cts04 .mobileCh li a {display:block; text-align:center; font-size:150%; font-weight:bold; color:#FFF; background:#1e162b; padding:30px 0}

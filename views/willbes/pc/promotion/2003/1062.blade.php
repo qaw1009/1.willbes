257dd0bd3acaeeb15c27a91fd11996bc18dcea3d
김창용 <winmir@willbes.com>
2019-04-01 13:57:28 +0900

    공무원 7급 패스  마감일 변경

--- views/willbes/pc/promotion/2003/1062.blade.php
+++ views/willbes/pc/promotion/2003/1062.blade.php
@@ -159,7 +159,7 @@
 
         function daycountDown() {
             // 한달 전 날짜로 셋팅
-            event_day = new Date(2019,1,28,23,59,59);
+            event_day = new Date(2019,3,10,23,59,59);
             now = new Date();
             var timeGap = new Date(0, 0, 0, 0, 0, 0, (event_day - now));
 

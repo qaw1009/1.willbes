c23ace117952b7c370ad5fb5cf9c609c83cad830
김창용 <winmir@willbes.com>
2019-04-01 17:16:17 +0900

    하승민 영어 독해 특강

--- views/willbes/pc/promotion/2002/1182.blade.php
+++ views/willbes/pc/promotion/2002/1182.blade.php
@@ -22,21 +22,21 @@
         /************************************************************/
 
         .evtTop {background:url(https://static.willbes.net/public/images/promotion/2019/04/1182_top_bg.jpg) no-repeat center top;}
-        .evt01 {background:#fce428 url(https://static.willbes.net/public/images/promotion/2019/04/1182_01_bg.jpg) no-repeat center top}
-        .evt02 {background:#fce428 url(https://static.willbes.net/public/images/promotion/2019/04/1182_02_bg.jpg) repeat-x }
+        .evt01 {background:url(https://static.willbes.net/public/images/promotion/2019/04/1182_01_bg.jpg) no-repeat center top}
+        .evt02 {background:url(https://static.willbes.net/public/images/promotion/2019/04/1182_02_bg.jpg) repeat-x }
 
     </style>
 
     <div class="p_re evtContent NSK" id="evtContainer">
 
         <div class="evtCtnsBox evtTop">
-            <img src="https://static.willbes.net/public/images/promotion/2019/04/1182_top.jpg" title="오태진 한국사 3개년기출특강">
+            <img src="https://static.willbes.net/public/images/promotion/2019/04/1182_top.jpg" title="하승민 영어 독해특강">
         </div>
         <div class="evtCtnsBox evt01">
-            <img src="https://static.willbes.net/public/images/promotion/2019/04/1182_01.jpg"  title="시험전,수강생공부방법" >
+            <img src="https://static.willbes.net/public/images/promotion/2019/04/1182_01.jpg"  title="합격생이 전한느 수업 후기" >
         </div>
         <div class="evtCtnsBox evt02">
-            <img src="https://static.willbes.net/public/images/promotion/2019/04/1182_02.jpg" usemap="#Map1182A" title="시험전,수강생공부방법" border="0">
+            <img src="https://static.willbes.net/public/images/promotion/2019/04/1182_02.jpg" usemap="#Map1182A" title="독해특강 신청하기" border="0">
             <map name="Map1182A">
             <area shape="rect" coords="363,877,756,974" href="{{ site_url('/pass/professor/show/prof-idx/50136/?cate_code=3010&subject_idx=1054&subject_name=%EC%98%81%EC%96%B4&tab=open_lecture') }}" target="_blank" alt="신청하기">
             </map>

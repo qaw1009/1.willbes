7d72e90da61a7dac587ddd5c635d1790cf52278d
김창용 <winmir@willbes.com>
2019-04-02 21:20:44 +0900

    공무원 풀캐어

--- views/willbes/pc/promotion/2003/1140.blade.php
+++ views/willbes/pc/promotion/2003/1140.blade.php
@@ -1127,7 +1127,9 @@
 
             <div><img src="https://static.willbes.net/public/images/promotion/2019/03/1140_03_2.jpg" title="시험후기 댓글 이벤트" /></div>
 
-            {{--@include('html.event_replyEvaluate')--}}
+            <?php if( empty($data['data_option_ccd']) === false && array_key_exists($arr_base['option_ccd']['comment_list'], $data['data_option_ccd']) === true && array_key_exists($arr_base['comment_use_area']['event'], $data['data_comment_use_area']) === true): ?>
+                <?php echo $this->runChild('willbes.pc.promotion.show_comment_list_emoticon_partial'); ?>
+            <?php endif; ?>
 
             <div class="mt100"><img src="https://static.willbes.net/public/images/promotion/2019/03/1140_03_3.jpg" title="소름돋는 적중" /> </div>
 

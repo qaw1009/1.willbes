25f7c5b779079d1e03f02c39d23edc8b5c0572c1
김창용 <winmir@willbes.com>
2019-04-02 11:35:20 +0900

    공무원 풀캐어 서비스

--- views/html/1140.blade.php
+++ views/html/1140.blade.php
@@ -69,27 +69,20 @@
         
         .Cts03 h3 {font-size:16px;}
         .Cts03 h3 span {color:#fa7738}
-        .Cts03 .graphWrap {width:100%}
-        .Cts03 .graphWrap {
-            margin-top:50px; font-size:14px; line-height:1.5; 
-        }
-        .Cts03 .graphWrap li {
-            display:inline; float:left; width:48%; text-align:left; margin:0 0.5%; position:relative;
-        }
-        .Cts03 .graphWrap select {position:absolute; top:5px; right:0}
-        .Cts03 .graphWrap:after {content:""; display:block; clear:both}
-                
-        .Cts03 .graphbox {width:90%; margin:20px auto; border:1px solid #000;}
-        .Cts03 .graphbox .subTit {font-size:120%; color:#F30}
-        .Cts03 .graph {width:20%; float:left; text-align:center; background:url(http://file.willbes.net/new_image/2015/04/graphBg.png) repeat;}
-        .Cts03 .graph p {padding:10px 0; background:#fff}
-        .Cts03 .graph p:last-child {border-top:1px solid #333}
-        .Cts03 .graph div {position:relative; width:45px; height:250px; margin:0 auto; }
-        .Cts03 .graph div img {position:absolute; bottom:0; width:100%; background:#e2be43 url(https://static.willbes.net/public/images/promotion/common/graphA.png) repeat;}
-        .Cts03 .graph2 div img {background:#bdbdcc url(https://static.willbes.net/public/images/promotion/common/graphA.png) repeat;}
-        .Cts03 .graphbox:after {content:""; display:block; clear:both}
-        .Cts03 .graphWrap:after {content:""; display:block; clear:both}
-
+            .graphWrap {width:100%; margin-top:50px; font-size:14px; line-height:1.5;}
+            .graphWrap li {position:relative; display:inline; float:left; width:48%; text-align:left; margin:0 0.5%;}
+            .graphWrap select {position:absolute; top:5px; right:0}
+            .graphWrap:after {content:""; display:block; clear:both}                
+            .graphbox {width:90%; margin:20px auto; border:1px solid #000;}
+            .graphbox .subTit {font-size:120%; color:#F30}
+            .graph {width:20%; float:left; text-align:center; background:url(http://file.willbes.net/new_image/2015/04/graphBg.png) repeat;}
+            .graph p {padding:10px 0; background:#fff}
+            .graph p:last-child {border-top:1px solid #333}
+            .graph div {position:relative; width:45px; height:250px; margin:0 auto; }
+            .graph div img {position:absolute; bottom:0; width:100%; background:#e2be43 url(https://static.willbes.net/public/images/promotion/common/graphA.png) repeat;}
+            .graph2 div img {background:#bdbdcc url(https://static.willbes.net/public/images/promotion/common/graphA.png) repeat;}
+            .graphbox:after {content:""; display:block; clear:both}
+            .graphWrap:after {content:""; display:block; clear:both}
         .Cts03_01 {width:1120px; text-align:left; margin:80px auto 0}
         .Cts03_01 div {border:1px solid #e4e4e4; padding:20px; margin-top:20px}
         
@@ -1162,7 +1155,6 @@
                         </div>
                         <!--graphbox//-->
                     </div>
-
                 </li>
                 <li>
                     <div>

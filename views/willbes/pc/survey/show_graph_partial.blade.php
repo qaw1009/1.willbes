5828f32f0001ce88e6bb74fc8ff5d98900d37816
eddie <eddie@willbes.com>
2019-04-03 15:43:55 +0900

    오류수정

--- views/willbes/pc/survey/show_graph_partial.blade.php
+++ views/willbes/pc/survey/show_graph_partial.blade.php
@@ -5,16 +5,23 @@
     var spidx = '{{ $arr_base['spidx'] }}';
 
     function resizeIframeSurvey(iframe) {
-        var agent = navigator.userAgent.toLowerCase();
-        if (agent.indexOf("chrome") != -1) {
-            iframe.height = (iframe.contentDocument.documentElement.scrollHeight + 15) + "px";
+        var newheight;
+        var newwidth;
+
+        if(iframe.contentDocument){
+            newheight = iframe.contentDocument.documentElement.scrollHeight+30;
+            newwidth = iframe.contentDocument.documentElement.scrollWidth+30;
         } else {
-            iframe.height = (iframe.contentWindow.document.body.scrollHeight + 15) + "px";
+            newheight=iframe.contentWindow.document.body.scrollHeight+30;
+            newwidth=iframe.contentWindow.document.body.scrollWidth+30;
         }
+
+        iframe.height= newheight + "px";
+        iframe.width= newwidth + "px";
 
         // https://pass.local.willbes.net/promotion/index/cate/3019/code/1140/spidx/2 에서 스크립트가 안먹어서 넣음
         if(spidx == 2){
-            iframe.height = '505px';
+            //iframe.height = '505px';
         }
     }
 

359e6948cce6f5be61f6ce1966e2de3d76b7ef48
최현탁 cht@willbes.com <cht@willbes.com>
2019-04-03 16:04:46 +0900

    프로모션 그레프 프레임 호출 부분 스크립트 수정

--- views/willbes/pc/survey/show_graph_partial.blade.php
+++ views/willbes/pc/survey/show_graph_partial.blade.php
@@ -2,15 +2,17 @@
 <iframe id='frm' frameborder="0" scrolling="no" width="100%" onload="resizeIframeSurvey(this)" src="{{front_url('/survey/graph/'.$arr_base['spidx'])}}"></iframe>
 @endif
 <script>
-    var spidx = '{{ $arr_base['spidx'] }}';
-
     function resizeIframeSurvey(iframe) {
         var newheight;
-
+        var newwidth;
         if(iframe.contentDocument){
             newheight = iframe.contentDocument.documentElement.scrollHeight+30;
+            newwidth = iframe.contentDocument.documentElement.scrollWidth+30;
         } else {
             newheight=iframe.contentWindow.document.body.scrollHeight+30;
+            newwidth=iframe.contentWindow.document.body.scrollWidth+30;
         }
+        iframe.height= newheight + "px";
+        iframe.width= newwidth + "px";
     }
 </script>

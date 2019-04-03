765e91af396e9bca2a0f3941152bf4c8f2853bbb
최현탁 cht@willbes.com <cht@willbes.com>
2019-04-03 15:58:33 +0900

    주석 제거

--- views/willbes/pc/survey/show_graph_partial.blade.php
+++ views/willbes/pc/survey/show_graph_partial.blade.php
@@ -12,14 +12,5 @@
         } else {
             newheight=iframe.contentWindow.document.body.scrollHeight+30;
         }
-
-        iframe.height= newheight + "px";
-        // /promotion/index/cate/3019/code/1140/spidx/2 그래프가 3번째 탭에 있어서 크롬에서 세로사이즈를 제대로 감지못하는 오류가 있어서 예외처리
-        if(spidx == 2){
-            iframe.height = '505px';
-        }
     }
-
 </script>
-
-

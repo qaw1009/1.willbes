a44d202cc5f916c1632d3c958fbc6478111c0358
김창용 <winmir@willbes.com>
2019-04-04 18:32:24 +0900

    경찰 모달 팝업

--- views/willbes/pc/site/main_partial/modal_popup_2001.blade.php
+++ views/willbes/pc/site/main_partial/modal_popup_2001.blade.php
@@ -13,10 +13,10 @@
             <div class="pop-container">
                 <div class="pop-conts">
                     <!--content //-->
-                    <img src="https://static.willbes.net/public/images/promotion/2019/04/LayPop190401_p.jpg" alt="2단계 동형모의고사/숨은 필합자를 찾아라" usemap="#Map190325" border="0"/>
-                    <map name="Map190325" id="Map190325">
+                    <img src="https://static.willbes.net/public/images/promotion/2019/04/LayPop190404_p.jpg" alt="2단계 동형모의고사/숨은 필합자를 찾아라" usemap="#MaplayerPopup" border="0" href="#none"/>
+                    <map name="MaplayerPopup" id="MaplayerPopup">
                         <area shape="rect" coords="6,4,367,313" href="{{ site_url('/promotion/index/cate/3001/code/1127') }}" alt="2단계 동형모의고사" />
-                        <area shape="rect" coords="372,4,754,312" href="{{ site_url('/pass/promotion/index/cate/3010/code/1174') }}" alt="합격자의 밤" />
+                        <area shape="rect" coords="372,4,754,312" href="#none" alt="합격생 중경 입교 버스 든든이벤트" />
                     </map>
                     <div class="btn-r">
                         <a class="cbtn" href="javascript:void(0)" onclick="closeLayerPop2002('passpopup', 'blackpopup');" >하루 보지않기</a>

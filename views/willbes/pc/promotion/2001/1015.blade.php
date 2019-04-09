ad9a505a63734ea0903d221f5082e8241cb8d000
김창용 <winmir@willbes.com>
2019-04-09 16:55:26 +0900

    경찰 기본이론 1015

--- views/willbes/pc/promotion/2001/1015.blade.php
+++ views/willbes/pc/promotion/2001/1015.blade.php
@@ -60,8 +60,8 @@
                 <area shape="rect" coords="444,505,516,543" href="{{ site_url('/lecture/show/cate/3001/pattern/only/prod-code/132285') }}" onfocus='this.blur()' title="김원욱 형법 기본이론(19년 3월)" target="_blank"/>
                 <area shape="rect" coords="444,553,516,593" href="{{ site_url('/lecture/show/cate/3001/pattern/only/prod-code/132290') }}" onfocus='this.blur()' title="하승민 영어 기본이론(19년 3월)" target="_blank"/>
 
-                <area shape="rect" coords="868,406,944,441" href="{{ site_url('/lecture/show/cate/3001/pattern/only/prod-code/132261') }}" onfocus='this.blur()' title="오태진 한국사 기본이론(19년 3월)" target="_blank"/>
-                <area shape="rect" coords="868,455,944,491" href="{{ site_url('/lecture/show/cate/3001/pattern/only/prod-code/132259') }}" onfocus='this.blur()' title="원유철 한국사 기본이론(19년 3월)" target="_blank"/>
+                <area shape="rect" coords="868,406,944,441" href="{{ site_url('/lecture/show/cate/3001/pattern/only/prod-code/152386') }}" onfocus='this.blur()' title="원유철 한국사 기본이론(19년 3월)" target="_blank"/>
+                <area shape="rect" coords="868,455,944,491" href="{{ site_url('/lecture/show/cate/3001/pattern/only/prod-code/152384') }}" onfocus='this.blur()' title="오태진 한국사 기본이론(19년 3월)" target="_blank"/>
                 <area shape="rect" coords="868,505,944,542" href="{{ site_url('/lecture/show/cate/3002/pattern/only/prod-code/152609') }}" onfocus='this.blur()' title="신광은 수사 이론" target="_blank"/>
                 <area shape="rect" coords="868,553,944,593" href="{{ site_url('/lecture/show/cate/3002/pattern/only/prod-code/151726') }}" onfocus='this.blur()' title="장정훈 행정법 이론" target="_blank"/>
 

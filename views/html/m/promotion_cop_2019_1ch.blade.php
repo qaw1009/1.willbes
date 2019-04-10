e566dddbdcd35b013e2e361e610f2bc4d98611a1
김창용 <winmir@willbes.com>
2019-04-10 15:42:58 +0900

    경찰 마무리 특강, 합격예측 모바일

--- views/html/m/promotion_cop_2019_1ch.blade.php
+++ views/html/m/promotion_cop_2019_1ch.blade.php
@@ -7,7 +7,7 @@
         <div class="willbes-Tit"> 
             합격예측 풀서비스 <span class="NGEB">사전예약</span>
         </div>
-
+        {{--26일 시험일전까지 사전예약 이벤트 노출--}}
         <div class="predictMain">
             <div class="mainImg">
                 <img src="https://static.willbes.net/public/images/promotion/2019/04/1187M_01.jpg" title="2019년 경찰 1차 합격예측 풀서비스 사전예약">
@@ -21,6 +21,26 @@
                 <div><span>일반<br>공채</span></div>
                 <div><span>101<br>경비단</span></div>
                 <div><span>전의경<br>경채</span></div>
+            </div>
+        </div>
+
+        {{--27일 시험일부터 채점입력 노출--}}
+        <div class="predictMain">
+            <div class="mainImg">
+                <img src="https://static.willbes.net/public/images/promotion/2019/04/1187M_01.jpg" title="2019년 경찰 1차 합격예측 풀서비스 사전예약">
+            </div>
+            <div class="mainBtn">
+                {{--로그인 전--}}
+                <a href="#none">                    
+                    로그인 후 서비스 이용이 가능합니다.
+                </a>
+                {{--로그인 후--}}
+                <a href="#none" class="btn2">                    
+                   채점 서비스 시작하기
+                </a>
+            </div>
+            <div class="marktxt1">
+                OMR 정답지에 답을 체크 하는 [일반채점] 서비스 및 합격예측 분석 데이터는 PC버전을 이용해 주세요
             </div>
         </div>
     </div>   

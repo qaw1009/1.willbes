00bd877dc525a06af04aca89a4425a8f59dad0c9
김창용 <winmir@willbes.com>
2019-04-10 10:02:47 +0900

    경찰 든든이벤트

--- views/willbes/pc/promotion/2002/1188.blade.php
+++ views/willbes/pc/promotion/2002/1188.blade.php
@@ -26,24 +26,27 @@
         .evt02 {background:#a3c4ef url(https://static.willbes.net/public/images/promotion/2019/04/1188_02_bg.jpg) no-repeat center top;}
         .evt02 .evt02_02 {position:relative; width:1120px; margin:0 auto}
         .evt02 .evt02_02 span {position:absolute; left:495px; display:block; width:98px; height:30px; line-height:30px; z-index:10}
+        .evt02 .evt02_02 span:nth-child(2) {top:207px;}
+        .evt02 .evt02_02 span:nth-child(3) {top:256px}
+        .evt02 .evt02_02 span:nth-child(4) {top:305px}
+        .evt02 .evt02_02 span:nth-child(5) {top:354px}
+        .evt02 .evt02_02 span:nth-child(6) {top:403px}
+        .evt02 .evt02_02 span:last-child {top:452px}
         .evt02 .evt02_02 span a {display:block; text-align:center; color:#010100; background:#ffff01; font-size:14px; font-weight:600; border:1px solid #010100;}
         .evt02 .evt02_02 span a:hover {color:#fff; background:#000}
-        .evt02 .evt02_02 span:nth-child(1) {top:207px;}
-        .evt02 .evt02_02 span:nth-child(2) {top:256px}
-        .evt02 .evt02_02 span:nth-child(3) {top:305px}
-        .evt02 .evt02_02 span:nth-child(4) {top:354px}
-        .evt02 .evt02_02 span:nth-child(5) {top:403px}
-        .evt02 .evt02_02 span:nth-child(6) {top:452px}
+        .evt02 .evt02_02 span a.end {color:#fff; background:#86aee8; border:1px solid #86aee8;}
+        
         .evt02 .evt02_03 {position:relative; width:1120px; margin:0 auto}
         .evt02 .evt02_03 span {position:absolute; top:577px; display:block; width:98px; height:30px; line-height:30px; z-index:10}
+        .evt02 .evt02_03 span:nth-child(2) {left:250px;}
+        .evt02 .evt02_03 span:nth-child(3) {left:392px}
+        .evt02 .evt02_03 span:nth-child(4) {left:534px}
+        .evt02 .evt02_03 span:nth-child(5) {left:672px}
+        .evt02 .evt02_03 span:nth-child(6) {left:810px}
+        .evt02 .evt02_03 span:last-child  {left:953px}        
         .evt02 .evt02_03 span a {display:block; text-align:center; color:#fff; background:#000; font-size:14px; font-weight:600; border:1px solid #010100;}
         .evt02 .evt02_03 span a:hover {color:#fff; background:#0c95d3}
-        .evt02 .evt02_03 span:nth-child(1) {left:250px;}
-        .evt02 .evt02_03 span:nth-child(2) {left:392px}
-        .evt02 .evt02_03 span:nth-child(3) {left:534px}
-        .evt02 .evt02_03 span:nth-child(4) {left:672px}
-        .evt02 .evt02_03 span:nth-child(5) {left:810px}
-        .evt02 .evt02_03 span:nth-child(6) {left:953px}
+        .evt02 .evt02_03 span a.end {color:#fff; background:#86aee8; border:1px solid #86aee8;}
         
         .evt03 {background:#fff;}
 
@@ -66,21 +69,57 @@
             <div><img src="https://static.willbes.net/public/images/promotion/2019/04/1188_02_01.jpg" title="신청 및 출발안내"></div>
             <div class="evt02_02">
                 <img src="https://static.willbes.net/public/images/promotion/2019/04/1188_02_02.jpg" title="입교버스 지역별 신청">
-                <span><a href="javascript:requestOpen();">신청하기 > </a></span>
-                <span><a href="javascript:requestOpen();">신청하기 > </a></span>
-                <span><a href="javascript:requestOpen();">신청하기 > </a></span>
-                <span><a href="javascript:requestOpen();">신청하기 > </a></span>
-                <span><a href="javascript:requestOpen();">신청하기 > </a></span>
-                <span><a href="javascript:requestOpen();">신청하기 > </a></span>
+                <span>
+                    <a href="javascript:requestOpen();">신청하기 > </a>
+                    {{--<a href="#none">신청마감 </a>--}}
+                </span>
+                <span>
+                    <a href="javascript:requestOpen();">신청하기 > </a>
+                    {{--<a href="#none">신청마감 </a>--}}
+                </span>
+                <span>
+                {{--<a href="javascript:requestOpen();">신청하기 > </a>--}}
+                    <a href="#none" class="end">신청마감 </a>
+                </span>
+                <span>
+                    <a href="javascript:requestOpen();">신청하기 > </a>
+                    {{--<a href="#none">신청마감 </a>--}}
+                </span>
+                <span>
+                    <a href="javascript:requestOpen();">신청하기 > </a>
+                    {{--<a href="#none">신청마감 </a>--}}
+                </span>
+                <span>
+                    <a href="javascript:requestOpen();">신청하기 > </a>
+                    {{--<a href="#none">신청마감 </a>--}}
+                </span>
             </div>
             <div id="event" class="evt02_03">
                 <img src="https://static.willbes.net/public/images/promotion/2019/04/1188_02_03.jpg" title="입교버스 지역별 신청지역 안내">
-                <span><a href="javascript:requestOpen();">신청하기 > </a></span>
-                <span><a href="javascript:requestOpen();">신청하기 > </a></span>
-                <span><a href="javascript:requestOpen();">신청하기 > </a></span>
-                <span><a href="javascript:requestOpen();">신청하기 > </a></span>
-                <span><a href="javascript:requestOpen();">신청하기 > </a></span>
-                <span><a href="javascript:requestOpen();">신청하기 > </a></span>
+                <span>
+                    <a href="javascript:requestOpen();">신청하기 > </a>
+                    {{--<a href="#none">신청마감 </a>--}}
+                </span>
+                <span>
+                    <a href="javascript:requestOpen();">신청하기 > </a>
+                    {{--<a href="#none">신청마감 </a>--}}
+                </span>
+                <span>
+                {{--<a href="javascript:requestOpen();">신청하기 > </a>--}}
+                    <a href="#none" class="end">신청마감 </a>
+                </span>
+                <span>
+                    <a href="javascript:requestOpen();">신청하기 > </a>
+                    {{--<a href="#none">신청마감 </a>--}}
+                </span>
+                <span>
+                    <a href="javascript:requestOpen();">신청하기 > </a>
+                    {{--<a href="#none">신청마감 </a>--}}
+                </span>
+                <span>
+                    <a href="javascript:requestOpen();">신청하기 > </a>
+                    {{--<a href="#none">신청마감 </a>--}}
+                </span>
             </div>
         </div>
 	</div>

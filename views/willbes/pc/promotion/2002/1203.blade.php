e587b59509c45af2bd4d703a48e12ede6e0c89d4
김창용 <winmir@willbes.com>
2019-04-15 17:59:03 +0900

    경찰학원 3단계 파이널 실전모의고사

--- views/willbes/pc/promotion/2002/1203.blade.php
+++ views/willbes/pc/promotion/2002/1203.blade.php
@@ -37,9 +37,9 @@
         } 
         .evt01 {background:#0c4a6f}
         .evt02 {background:#4e4e4e}
-        .evt02 div {position:relative; width:1120px; margin:0 auto}
-        .evt02 div span {position:absolute; top:256px; left:720px; width:103px; z-index:1}
-        .evt02 div span:last-child {left:822px}
+        .evt02 > div {position:relative; width:1120px; margin:0 auto}
+        .evt02 div div span {position:absolute; top:256px; left:720px; width:103px; z-index:1}
+        .evt02 div div span:last-child {left:822px}
         .evt03 {background:#363636 url(https://static.willbes.net/public/images/promotion/2019/04/1203_03_bg.jpg) no-repeat center top;}        
         .evt04 {background:#231f20}
         .evt05 {background:#fff; position:relative;}
@@ -64,8 +64,10 @@
 	<div class="evtCtnsBox evt02">
         <div> 
             <img src="https://static.willbes.net/public/images/promotion/2019/04/1203_02.jpg" title="1차 필기시험 남은 시간">
-            <span><img src="https://static.willbes.net/public/images/promotion/2019/04/1203_num9.png" title="숫자"></span>
-            <span><img src="https://static.willbes.net/public/images/promotion/2019/04/1203_num4.png" title="숫자"></span>
+            <div id="ddaytime">
+                <span><img src="https://static.willbes.net/public/images/promotion/2019/04/1203_num0.png" title="숫자" id="dd1"></span>
+                <span><img src="https://static.willbes.net/public/images/promotion/2019/04/1203_num0.png" title="숫자" id="dd2"></span>
+            </div>
         </div>
 	</div>
 
@@ -88,4 +90,68 @@
 	</div>
 </div>
 <!-- End Container -->
+<script type="text/javascript">
+        /*타이머*/
+        var DdayDiff = { //타이머를 설정합니다.
+            inDays: function(dd1, dd2) {
+                var tt2 = dd2.getTime();
+                var tt1 = dd1.getTime();
+
+                return Math.floor((tt2-tt1) / (1000 * 60 * 60 * 24));
+            },
+
+            inWeeks: function(dd1, dd2) {
+                var tt2 = dd2.getTime();
+                var tt1 = dd1.getTime();
+
+                return parseInt((tt2-tt1)/(24*3600*1000*7));
+            },
+
+            inMonths: function(dd1, dd2) {
+                var dd1Y = dd1.getFullYear();
+                var dd2Y = dd2.getFullYear();
+                var dd1M = dd1.getMonth();
+                var dd2M = dd2.getMonth();
+
+                return (dd2M+12*dd2Y)-(dd1M+12*dd1Y);
+            },
+
+            inYears: function(dd1, dd2) {
+                return dd2.getFullYear()-dd1.getFullYear();
+            }
+        };
+
+        function daycountDown() {
+            // 한달 전 날짜로 셋팅
+            var event_day = new Date(2019,3,27,23,59,59);
+            var now = new Date();
+            var timeGap = new Date(0, 0, 0, 0, 0, 0, (event_day - now));
+
+            var Monthleft = event_day.getMonth() - now.getMonth();
+            var Dateleft = DdayDiff.inDays(now, event_day);
+            var Hourleft = timeGap.getHours();
+            var Minuteleft = timeGap.getMinutes();
+            var Secondleft = timeGap.getSeconds();
+
+            if((event_day.getTime() - now.getTime()) > 0) {
+                $("#dd1").attr("src", "https://static.willbes.net/public/images/promotion/2019/04/" + "1203_num" + parseInt(Dateleft/10) + ".png");
+                $("#dd2").attr("src", "https://static.willbes.net/public/images/promotion/2019/04/" + "1203_num" + parseInt(Dateleft%10) + ".png");
+
+                $("#hh1").attr("src", "https://static.willbes.net/public/images/promotion/2019/04/" + "1203_num" + parseInt(Hourleft/10) + ".png");
+                $("#hh2").attr("src", "https://static.willbes.net/public/images/promotion/2019/04/" + "1203_num" + parseInt(Hourleft%10) + ".png");
+
+                $("#mm1").attr("src", "https://static.willbes.net/public/images/promotion/2019/04/" + "1203_num" + parseInt(Minuteleft/10) + ".png");
+                $("#mm2").attr("src", "https://static.willbes.net/public/images/promotion/2019/04/" + "1203_num" + parseInt(Minuteleft%10) + ".png");
+
+                $("#ss1").attr("src", "https://static.willbes.net/public/images/promotion/2019/04/" + "1203_num" + parseInt(Secondleft/10) + ".png");
+                $("#ss2").attr("src", "https://static.willbes.net/public/images/promotion/2019/04/" + "1203_num" + parseInt(Secondleft%10) + ".png");
+                setTimeout(daycountDown, 1000);
+            }
+            else{
+                $("#newTopDday").hide();
+            }
+
+        }
+        daycountDown();
+    </script>
 @stop

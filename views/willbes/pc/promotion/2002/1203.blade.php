1451acc518490772a5d39bf9842f0569f730687f
김창용 <winmir@willbes.com>
2019-04-18 12:46:58 +0900

    경찰 마무리 특강

--- views/willbes/pc/promotion/2002/1203.blade.php
+++ views/willbes/pc/promotion/2002/1203.blade.php
@@ -46,10 +46,17 @@
         .evt05 a {position:absolute; display:block; top:685px; left:50%; width:200px; margin-left:-100px; height:50px; line-height:50px; background:#000; color:#fff; border-radius:26px; font-size:20px; z-index:1} 
         .evt05 a:hover {background:#20bdb2;}
         .evt06 {background:#ececec;}
-        
+        .evt07 {width:1000px; margin:0 auto; background:#20bdb2; margin-bottom:100px; padding:100px; font-size:14px; text-align:left}
+        .evt07 h3 {border:1px solid #fff; color:#fff; font-size:40px; padding:20px 0; text-align:center}
+        .evt07 p {margin-top:30px; font-size:18px; color:#fff}
+        .evt07 ul {margin-top:20px; background:#fff; padding:20px}
+        .evt07 ul li {height:50px; line-height:50px; border-bottom:1px solid #e4e4e4;}
+        .evt07 ul li:last-child {border:0}
+        .evt07 ul li a {display:inline-block; width:140px; color:#fff; background:#20bdb2; text-align:center; float:right; height:30px; line-height:30px; margin-top:10px }
+        .evt07 ul li a:hover {background:#000}
     </style>
 
-<div class="p_re evtContent NSK" id="evtContainer">
+<div class="p_re evtContent NGR" id="evtContainer">
     <div class="evtCtnsBox evtTop">
         <div class="evtInmg">    
             <img src="https://static.willbes.net/public/images/promotion/2019/04/1203_top.png" title="3단계 파이널 실전모의고사">
@@ -81,12 +88,53 @@
     
     <div class="evtCtnsBox evt05">
         <img src="https://static.willbes.net/public/images/promotion/2019/04/1203_05.jpg" title="3단계 파이널 실전모의고사 수강신청">
-        <a href="https://police.willbes.net/pass/offPackage/index?cate_code=3010&campus_ccd=605001&course_idx=1045">신청하기 ></a>
+        <a href="https://police.willbes.net/pass/offPackage/index?cate_code=3010&campus_ccd=605001&course_idx=1045">신청하기 ></a>        
+    </div>
+
+    <div class="evtCtnsBox evt07">
+        <h3 class="NSK-Black">동영상 수강신청</h3>
+        <p>[단과]</p>
+        <ul>
+            <li>
+                2019년 1차대비 신광은 형사소송법 파이널 실전모의고사
+                <a href="https://police.willbes.net/lecture/show/cate/3001/pattern/only/prod-code/152914" target="_blank">신청하기 ></a>
+            </li>
+            <li>
+                2019년 1차대비 장정훈 경찰학개론 파이널 실전모의고사
+                <a href="https://police.willbes.net/lecture/show/cate/3001/pattern/only/prod-code/152913" target="_blank">신청하기 ></a>
+            </li>
+            <li>
+                2019년 1차대비 김원욱 형법 파이널 실전모의고사
+                <a href="https://police.willbes.net/lecture/show/cate/3001/pattern/only/prod-code/152912" target="_blank">신청하기 ></a>
+            </li>
+            <li>
+                2019년 1차대비 하승민 영어 파이널 실전모의고사
+                <a href="https://police.willbes.net/lecture/show/cate/3001/pattern/only/prod-code/152911" target="_blank">신청하기 ></a>
+            </li>
+            <li>
+                2019년 1차대비 원유철 한국사 파이널 실전모의고사
+                <a href="https://police.willbes.net/lecture/show/cate/3001/pattern/only/prod-code/152887" target="_blank">신청하기 ></a>
+            </li>
+            <li>
+                2019년 1차대비 오태진 한국사 파이널 실전모의고사
+                <a href="https://police.willbes.net/lecture/show/cate/3001/pattern/only/prod-code/152886" target="_blank">신청하기 ></a>
+            </li>
+        </ul>
+        <p>[종합반]</p>
+        <ul>
+            <li>
+                2019년 1차대비 일반경찰 파이널 실전모의고사 패키지(원유철史) 
+                <a href="https://police.willbes.net/package/show/cate/3001/pack/648001/prod-code/152929" target="_blank">신청하기 ></a>
+            </li>
+            <li>
+                2019년 1차대비 일반경찰 파이널 실전모의고사 패키지(오태진史) 
+                <a href="https://police.willbes.net/package/show/cate/3001/pack/648001/prod-code/152928" target="_blank">신청하기 ></a>
+            </li>
+        </ul>
     </div>
     
     <div class="evtCtnsBox evt06">
-        <img src="https://static.willbes.net/public/images/promotion/2019/04/1203_06.jpg" title="코퀄리티강의로 수험생 여러분의 합격을 책임지겠습니다.">
-        
+        <img src="https://static.willbes.net/public/images/promotion/2019/04/1203_06.jpg" title="코퀄리티강의로 수험생 여러분의 합격을 책임지겠습니다.">        
 	</div>
 </div>
 <!-- End Container -->

ab63c7fe6df1852ac3d3373de0ece55a6da1cc80
조규호 <benoo@willbes.com>
2019-04-18 12:17:25 +0900

    TM결제.환불 내역 엑셀 추출 수정

--- application/controllers/lms/crm/tm/TmOrderRefund.php
+++ application/controllers/lms/crm/tm/TmOrderRefund.php
@@ -122,11 +122,10 @@ class TmOrderRefund extends \app\controllers\BaseController
 
         $list = $this->tmModel->listOrderRefundExcel($arr_condition, $order_by);
 
-        $headers = ['NO', '회원명', '회원아이디', '주문번호', '사이트', '결제완료일', '상품명', '결제금액', '환불금액','환불완료일', 'TM담당자', '배정일', '최종상담일'];
+        $headers = ['회원명', '회원아이디', '주문번호', '사이트', '결제완료일', '상품명', '결제금액', '환불금액','환불완료일', 'TM담당자', '배정일', '최종상담일'];
         // export excel
         $this->load->library('excel');
-        $this->excel->exportExcel('TM환불내역('.date("Y-m-d").')', $list, $headers);
+        $this->excel->exportExcel('TM환불내역_'.$this->session->userdata('admin_idx').'_'.date("Y-m-d"), $list, $headers);
     }
-
 
 }

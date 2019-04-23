33d89e2c87bb5814a2bc1b8f6bf4ef23aa0cde00
조규호 <benoo@willbes.com>
2019-04-23 14:43:00 +0900

    tm 배정 정책 안내 수정 반영

--- views/lms/crm/tm/tm_policy_partial.blade.php
+++ views/lms/crm/tm/tm_policy_partial.blade.php
@@ -6,7 +6,7 @@
                 <button type="button" class="close" onclick="closeWin('in_pop_modal')" data-dismiss="modal"><span aria-hidden="true">×</span></button>
                 <h4 class="modal-title">TM 운영정책</h4>
             </div>
-            <div class="modal-body">
+            <!--기존 권선희 대리 정책 정리 : 수정된 부분이 많아 주석처리 div class="modal-body">
                 <div class="form-group form-group-sm">
                     <div class="x_title no-border text-left">• TM 대상</div>
                     <div class="x_txt">- 활성화된 회원(휴면, 탈퇴회원 제외) 중 ‘블랙컨슈머’ 회원 제외 and 수신동의 ‘Y’인 회원</div>
@@ -29,6 +29,38 @@
                     <div class="x_txt">
                         - 결제 : TM분류값이 ‘통화’이며, 최종통화일 (상담등록일)로 부터 21일 이내 매출 발생 시<br/>
                         (대상 상품 : 온라인강좌 상품 전체 (교재, 학원강좌 제외)  : 단강좌, 사용자패키지, 운영자패키지, 기간제패키지)<br/>
+                        - 환불 : 결제일 기준 30일 이내 환불 시 제외
+                    </div>
+                </div>
+            </div//-->
+            <div class="modal-body">
+                <div class="form-group form-group-sm">
+                    <div class="x_title no-border text-left">• TM 대상</div>
+                    <div class="x_txt">- 활성화된 회원(휴면, 탈퇴회원 제외) 중 ‘블랙컨슈머’ 회원 제외 and SMS 수신동의 ‘Y’인 회원</div>
+
+                    <div class="x_title no-border text-left">• TM 배정 조건</div>
+                    <div class="x_txt">- 신규/장바구/회수의 경우 1회 배정 시 이후 30일간 배정되지 않음</div>
+                    <div class="x_txt">- 재수강의 경우 1회만 배정됨</div>
+
+                    <div class="x_title no-border text-left">• TM 배정 분류값별 정책</div>
+                    <div class="x_txt">
+                        - <B>신규</B> : 검색일이 가입일이며, 온라인 상품에 관한 유료결제 내역이 없고 장바구니 내역이 없는 회원<br/>
+                        - <B>장바구니</B> : 장바구니에 온라인 상품 내역이 존재하는 회원<br/>
+                        - <B>재수강</B> : 한번도 배정이 안되었으며, 검색일 기준 -30~+30일 사이 강의 종료이면서 검색일 기준 -30~+30일 사이 유료 결제가 없는 회원<BR>
+                        - <B>회수(부재중)</B> : TM상담관리>상담분류값이 ‘부재중’으로 등록된 회원<br/>
+                        ※ 신규 > 재수강 > 회수(부재중) > 장바구니
+                    </div>
+
+                    <div class="x_title no-border text-left">• 배정 회원 조회</div>
+                    <div class="x_txt">- 접근일로부터 3개월 이내만 배정된 회원 검색/조회 가능</div>
+
+                    <div class="x_title no-border text-left">• 결제/환불 집계 조건</div>
+                    <div class="x_txt">
+                        - 결제 : TM분류값이 ‘통화’이며, 최종통화일 (상담등록일)로 부터 21일 이내 매출 발생 시<br/>
+                        - 결제 대상 상품 : 온라인강좌 상품 전체 (교재, 학원강좌 제외)<BR>
+                          : 경찰 - 단강좌<BR>
+                          : 공무원 -  단강좌, 사용자패키지, 운영자패키지, 기간제패키지<BR>
+                        <br/>
                         - 환불 : 결제일 기준 30일 이내 환불 시 제외
                     </div>
                 </div>

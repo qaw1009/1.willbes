fab8a50c213fc506f6a365fcd250273f4f91a2bb
최현탁 cht@willbes.com <cht@willbes.com>
2019-04-27 11:50:35 +0900

    프로모션 카운트 기능 수정

--- application/controllers/willbes/share/passpredict/BasePassPredict.php
+++ application/controllers/willbes/share/passpredict/BasePassPredict.php
@@ -870,13 +870,6 @@ class BasePassPredict extends \app\controllers\FrontController
                 $cnt3 = $this->predictFModel->getCntScore('');              //채점
                 $cnt4 = $this->predictFModel->getCntEventComment('1187');     //소문내기
                 $cnt5 = $this->predictFModel->getCntEventComment('1199');     //적중
-
-                $cnt6_1 = $this->predictFModel->getCntMyLecture('152583');   //최신판례특강
-                $cnt6_2 = $this->predictFModel->getCntMyLecture('152582');   //최신판례특강
-                $cnt6 = $cnt6_1 + $cnt6_2;
-
-                $cnt7 = $this->predictFModel->getCntMyLecture('152580');     //봉투모의고사
-                $cnt8 = $this->predictFModel->getCntMyLecture('152581');     //시크릿다운
                 $random = mt_rand(1, 10);
                 break;
         }
@@ -900,7 +893,18 @@ class BasePassPredict extends \app\controllers\FrontController
      */
     private function _promotion_cnt_type_2($data, $reg_cnt)
     {
-        $event_pv = $this->predictFModel->getCntEventPV($data['event_idx']);
+        switch ($data['type']) {
+            case "1":
+            case "2":
+                $event_pv = $this->predictFModel->getCntEventPV($data['event_idx']);
+                break;
+            case "3":
+                $event_pv = 0;
+                break;
+            default:
+                $event_pv = 0;
+                break;
+        }
 
         $random = mt_rand(1, 10);
         $ins_cnt = $reg_cnt + $random; //DB 저장 데이터

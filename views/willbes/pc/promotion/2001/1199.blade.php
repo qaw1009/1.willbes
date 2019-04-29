e2fa090918faf743d21006489c262fa73eb7e27c
김창용 <winmir@willbes.com>
2019-04-29 15:11:11 +0900

    1199 이벤트 댓글 재수정

--- views/willbes/pc/promotion/2001/1199.blade.php
+++ views/willbes/pc/promotion/2001/1199.blade.php
@@ -101,7 +101,7 @@
 
         <!--  이모티콘 댓글 -->
         @if( empty($data['data_option_ccd']) === false && array_key_exists($arr_base['option_ccd']['comment_list'], $data['data_option_ccd']) === true && array_key_exists($arr_base['comment_use_area']['event'], $data['data_comment_use_area']) === true)
-            @include('willbes.pc.promotion.show_comment_list_emoticon_url_partial')
+            @include('willbes.pc.promotion.show_comment_list_emoticon_partial')
         @endif
 
 	</div>

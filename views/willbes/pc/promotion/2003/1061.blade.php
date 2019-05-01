96b7ac97803b5ebcb36fdb5c504770a79ca41503
김창용 <winmir@willbes.com>
2019-05-01 09:37:47 +0900

    공무원 직렬별 패스 수정

--- views/willbes/pc/promotion/2003/1061.blade.php
+++ views/willbes/pc/promotion/2003/1061.blade.php
@@ -65,7 +65,7 @@
             <div>
                 <table>
                     <tr>
-                    <td class="time_txt NGEB"><span>4/30(화) 마감!</span></td>
+                    <td class="time_txt NGEB"><span>5/8(수) 마감!</span></td>
                     <td class="time_txt">마감까지<br><span>남은 시간은</span></td>
                     <td><img id="dd1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></td>
                     <td><img id="dd2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></td>
@@ -181,7 +181,7 @@
 
         function daycountDown() {
             // 한달 전 날짜로 셋팅
-            var event_day = new Date(2019,3,30,23,59,59);
+            var event_day = new Date(2019,4,8,23,59,59);
             var now = new Date();
             var timeGap = new Date(0, 0, 0, 0, 0, 0, (event_day - now));
 

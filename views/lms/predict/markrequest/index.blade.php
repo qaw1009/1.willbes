f809f89db96631410033821575d309c78fc190c3
eddie <eddie@willbes.com>
2019-05-02 18:14:31 +0900

    채점서비스현황

---
+++ views/lms/predict/markrequest/index.blade.php
@@ -0,0 +1,155 @@
+@extends('lcms.layouts.master')
+
+@section('content')
+    <h5 class="mt-20">- 주문번호 기준으로 모의고사 결제 및 응시여부를 확인하고 응시표를 출력하는 메뉴입니다.</h5>
+    <form class="form-horizontal" id="search_form" name="search_form" method="POST" onsubmit="return false;">
+        {{--!!  html_def_site_tabs($siteCodeDef, 'tabs_site_code', 'tab', false, $arrtab , true, $arrsite) !!--}}
+        {!! html_def_site_tabs($siteCodeDef, 'tabs_site_code', 'tab', false, [] , true, []) !!}
+        {!! csrf_field() !!}
+        <input type="hidden" id="search_site_code" name="search_site_code" value="{{$siteCodeDef}}">
+
+        <div class="x_panel">
+            <div class="x_content">
+                <div class="form-group form-inline">
+                    <label class="col-md-1 control-label">조건</label>
+                    <div class="col-md-11">
+                        <select class="form-control mr-5" id="search_ApplyType" name="search_ApplyType">
+                            <option value="">구분</option>
+                            <option value="합격예측">합격예측</option>
+                            <option value="사전특강">사전특강</option>
+                        </select>
+                        <select class="form-control mr-5" id="search_TakeMockPart" name="search_TakeMockPart">
+                            <option value="">응시직렬</option>
+                            @foreach($serial as $k => $v)
+                                <option value="{{$v['Ccd']}}">{{$v['CcdName']}}</option>
+                            @endforeach
+                        </select>
+                        <select class="form-control mr-5" id="search_TakeArea" name="search_TakeArea">
+                            <option value="">응시지역</option>
+                            @foreach($area as $k => $v)
+                                @if($v['Ccd'] != '712018')
+                                    <option value="{{$v['Ccd']}}">{{$v['CcdName']}}</option>
+                                @endif
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group form-inline">
+                    <label class="col-md-1 control-label">회원검색</label>
+                    <div class="col-md-6">
+                        <input type="text" class="form-control" style="width:300px;" id="search_fi" name="search_fi" value="{{ $search_fi }}"> 회원명, 아이디, 응시변호 검색가능
+                    </div>
+                </div>
+                <div class="pt-10">
+                    <div class="col-md-6">
+                        <button type="button" class="btn btn-primary mr-50 btn-excel" id="btn-excel">엑셀다운로드</button>
+                    </div>
+                    <div class="col-md-6 text-right">
+                        <button type="submit" class="btn btn-primary" id="btn_search">검색</button>
+                        <button type="button" class="btn btn-default" id="act-searchInit">초기화</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+
+    <div class="x_panel mt-10" style="overflow-x: auto; overflow-y: hidden;">
+        <div class="x_content">
+            <form class="form-horizontal" id="list_form" name="list_form" method="POST" onsubmit="return false;">
+                {!! csrf_field() !!}
+                <table id="list_table" class="table table-bordered table-striped table-head-row2 form-table">
+                    <thead class="bg-white-gray">
+                    <tr>
+                        <th class="text-center"></th>
+                        <th class="text-center">NO</th>
+                        <th class="text-center">구분</th>
+                        <th class="text-center">이름</th>
+                        <th class="text-center">아이디</th>
+                        <th class="text-center">휴대폰번호</th>
+                        <th class="text-center">직렬</th>
+                        <th class="text-center">지역</th>
+                        <th class="text-center">성적정보</th>
+                        <th class="text-center">응시번호</th>
+                        <th class="text-center">수강여부</th>
+                        <th class="text-center">시험준비기간</th>
+                        <th class="text-center">신청일</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </form>
+        </div>
+    </div>
+    <script type="text/javascript">
+        var $datatable;
+        var $search_form = $('#search_form');
+        var $list_form = $('#list_form');
+        var $list_table = $('#list_table');
+
+        $(document).ready(function() {
+
+
+            // 검색 초기화
+            $('#act-searchInit, #tabs_site_code > li').on('click', function () {
+                $search_form.find('[name^=search_]:not(#search_site_code)').each(function () {
+                    $(this).val('');
+                });
+                $search_form.find('#search_cateD1').trigger('change');
+
+                var eTarget = (event.target) ? event.target : event.srcElement;
+                if($(eTarget).attr('id') == 'searchInit') $datatable.draw();
+            });
+
+            // DataTables
+            $datatable = $list_table.DataTable({
+                info: true,
+                language: {
+                    "info": "[ 총 _MAX_건 ]"
+                },
+                dom: "<<'pull-left mb-5'i><'pull-right mb-5'B>>tp",
+                buttons: [
+                    { text: '<i class="fa fa-mobile mr-5"></i> SMS발송', className: 'btn btn-sm btn-primary mr-15 btn-sms' },
+
+                ],
+                serverSide: true,
+                ajax: {
+                    'url' : '{{ site_url('/predict/markrequest/list') }}',
+                    'type' : 'POST',
+                    'data' : function(data) {
+                        return $.extend(arrToJson($search_form.serializeArray()), {'start' : data.start, 'length' : data.length});
+                    }
+                },
+                columns: [
+                    {'data' : null, 'class': 'text-center', 'render' : function(data, type, row, meta) {
+                            return '<input type="checkbox" id="checkIdx" name="checkIdx[]" class="flat target-crm-member" value="" data-mem-idx="'+data.MemIdx+'" />';
+                        }},
+                    {'data' : null, 'class': 'text-center', 'render' : function(data, type, row, meta) {
+                            return $datatable.page.info().recordsTotal - (meta.row + meta.settings._iDisplayStart);
+                        }},
+                    {'data' : 'ApplyType', 'class': 'text-center'},
+                    {'data' : 'MemName', 'class': 'text-center'},
+                    {'data' : 'MemId', 'class': 'text-center'},
+                    {'data' : 'Phone', 'class': 'text-center'},
+                    {'data' : 'TakeMockPart', 'class': 'text-center'},
+                    {'data' : 'TaKeArea', 'class': 'text-center'},
+                    {'data' : 'OPOINT', 'class': 'text-center'},
+                    {'data' : 'TaKeNumber', 'class': 'text-center'},
+                    {'data' : 'LectureType', 'class': 'text-center'},
+                    {'data' : 'Period', 'class': 'text-center'},
+                    {'data' : 'RegDatm', 'class': 'text-center'}
+                ]
+            });
+
+            // 엑셀다운로드
+            $('.btn-excel').on('click', function(event) {
+                event.preventDefault();
+                if (confirm('엑셀다운로드 하시겠습니까?')) {
+                    formCreateSubmit('{{ site_url('/predict/markrequest/list/Y') }}', $search_form.serializeArray(), 'POST');
+                }
+            });
+
+
+        });
+    </script>
+@stop

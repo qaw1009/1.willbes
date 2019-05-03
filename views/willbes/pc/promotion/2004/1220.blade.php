55337c042284a014e0fb1cd026b8c8c6742740b3
이용준 <alkadia@willbes.com>
2019-05-03 17:48:45 +0900

    no message

--- views/willbes/pc/promotion/2004/1220.blade.php
+++ views/willbes/pc/promotion/2004/1220.blade.php
@@ -215,7 +215,7 @@
             </div><br>
 			<img src="https://static.willbes.net/public/images/promotion/2019/04/1220_05_01.jpg" usemap="#Map1220A" title="소방직 설명회" border="0"  />
             <map name="Map1220A" id="Map1220A">
-                <area shape="rect" coords="339,876,786,963" href="https://pass.willbes.net/pass/event/show/ongoing?event_idx=220&amp;" target="_blank" alt="설명회신청하기" />
+                <area shape="rect" coords="326,1098,791,1197" href="https://pass.willbes.net/pass/event/show/ongoing?event_idx=220&amp;" target="_blank" alt="설명회신청하기" />
             </map>
         </div>
         <div class="evt06">

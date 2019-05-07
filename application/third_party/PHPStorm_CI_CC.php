5b2dc2d51b57aca1b91bdd00e71914dbf6673694
최현탁 cht@willbes.com <cht@willbes.com>
2019-05-07 11:24:11 +0900

    룰렛 모델 추가 EXERD : 룰렛 테이블 수정

--- application/third_party/PHPStorm_CI_CC.php
+++ application/third_party/PHPStorm_CI_CC.php
@@ -176,6 +176,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @property ManageBlackConsumerModel $manageBlackConsumerModel     Member Manage BlackConsumer Model Class
  * @property CsModel $csModel                                                           Manage Cs Model Class
  * @property ExcelDownLogModel $excelDownLogModel                                 Excel Download Log Model Class
+ * @property RouletteModel $rouletteModel                                         Roulette Model Class
  ***************** MockTest ****************************************************************************
  * @property MockCommonModel $mockCommonModel                              MockTest MockCommon Model Class
  * @property BaseCodeModel $baseCodeModel                                        MockTest BaseCod Model Class

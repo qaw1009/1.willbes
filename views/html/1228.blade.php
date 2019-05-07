cf161a3e21c1b46171eeda6e18a2834df0cb9d9e
김창용 <winmir@willbes.com>
2019-05-07 14:03:08 +0900

    경찰 광은서포터즈 1기 1228

--- views/html/1228.blade.php
+++ views/html/1228.blade.php
@@ -21,26 +21,22 @@
 
         /************************************************************/
 
-        .skyBanner {position:fixed; bottom:20px; right:20px; width:138px; border:1px solid #000; z-index:10;
-        -webkit-box-shadow: 5px 5px 20px 0px rgba(0,0,0,0.3);
-        -moz-box-shadow: 5px 5px 20px 0px rgba(0,0,0,0.3);
-        box-shadow: 5px 5px 20px 0px rgba(0,0,0,0.3);}
-        .skyBanner li a {display:block; padding:15px 0; text-align:center; background:#fff; color:#000; font-size:14px; font-weight:600; border-bottom:1px solid #000; line-height:1.4}
-        .skyBanner li:last-child a {height:27px; line-height:27px; padding:0; background:#000; color:#fff}
-        .skyBanner li a:hover {background:#000; color:#fff;}
-        .evtTop {background:#404ae0; position:re}
-        .btnSet {margin-top:-150px}
+        .skyBanner {position:fixed; bottom:20px; right:20px; width:138px; border:1px solid #000; z-index:10;}
+        
+        .evtTop {background:#404ae0; position:relative; }
+        .btnSet {position:absolute; bottom:80px; width:596px; left:50%; margin-left:-298px; z-index:1}
+        .btnSet a {display:block; float:left}
+        .btnSet:after {content:''; display:block; clear:both}
+
         .evt01 {background:#484c57;}
         .evt02 {background:#e65261;}
-
-
     </style>
 
     <div class="p_re evtContent NGR" id="evtContainer"> 
         <div class="evtCtnsBox evtTop">
             <img src="https://static.willbes.net/public/images/promotion/2019/05/1228_top.jpg" title="광은 서포터즈 1기">
             <div class="btnSet">
-                <a href="#none"><img src="https://static.willbes.net/public/images/promotion/2019/05/1228_01_btn01.png" title="서포터즈 지원서 다운로드"></a>
+                <a href="#none"><img src="https://static.willbes.net/public/images/promotion/2019/05/1228_01_btn01.png" title="서포터즈 지원서 다운로드"></a>                
                 <a href="#none"><img src="https://static.willbes.net/public/images/promotion/2019/05/1228_01_btn02.png" title="합격수기 양식 다운로드"></a>
             </div>
 		</div>

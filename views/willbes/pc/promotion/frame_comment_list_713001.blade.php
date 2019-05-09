9d6b1d2ab30cf9bf374e20a5a2ebbca041e3fab5
김창용 <winmir@willbes.com>
2019-05-09 11:55:18 +0900

    공무원 패스, 기본댓글 수정

--- views/willbes/pc/promotion/frame_comment_list_713001.blade.php
+++ views/willbes/pc/promotion/frame_comment_list_713001.blade.php
@@ -1,6 +1,117 @@
 @extends('willbes.pc.layouts.master_no_sitdbar')
-
 @section('content')
+<style type="text/css">
+.replySection { 
+    width:1000px; 
+    margin:30px auto 100px; 
+    font-size:12px; 
+    line-height: 1.4;
+}
+.replyWrite {
+    position:relative; border:1px solid #ababab;
+}	
+.replyWrite .textarBx {
+    border-bottom:1px solid #eaeaea; padding:10px;
+}		
+.replyWrite .textarBx textarea {
+    float:left;
+    width:100%;
+    height:80px;
+    overflow-y: auto;
+    border:0;  
+}
+
+.replyWrite .textarBx:after {
+    content: '';
+    display: block;
+    clear:both;
+}
+.replyWrite p {
+    margin:0 0 0 20px; color:#999; text-align:left; height:42px; line-height:42px; 
+}
+.replyWrite .btnrwt {    
+    position: absolute; bottom:0; right:0;
+    width:70px; border-left:1px solid #eaeaea; display:block; height:42px; line-height:42px; color:#333; background:#fff;
+}
+.replyNoticeWrap .btnRt {
+    margin-top:20px;
+    text-align: right;
+}
+
+.replyNoticeWrap .btnRt a {
+    display: inline-block;
+    background: #000;
+    color:#fff;
+    padding:0 20px;
+    height: 30px;
+    line-height: 30px;
+    border:1px solid #000;
+    margin-left:5px;
+}
+.replyNoticeWrap .btnRt a:hover {
+    background: #fff;
+    color:#000;
+}
+.replyNotice {
+    margin-top: 20px;
+    border:1px solid #000;
+}
+.replyNotice .ry_info {
+    padding: 10px;
+    border-bottom: 1px solid #e0e0e0;
+    color:#555;
+}
+.replyNotice .ry_info .notice {
+    display: inline-block;
+    color:#fff;
+    background: #ff0033;
+    height: 20px;
+    line-height: 20px;
+    vertical-align: middle;
+    font-size: 11px;
+    padding:0 5px;
+    border-radius: 4px;
+    margin-right:10px;
+}
+.replyNotice .ry_info .rnBtns {
+    float:right;
+}
+.replySection .rnEditBtn {
+    color:#666;
+}
+.replySection .rnDelBtn {
+    color:#ff0033;
+}
+
+.replyNotice .ry_info .date {
+    margin-right:10px;
+}
+.replyNotice .ry_cont {
+    padding: 10px;
+    background: #fafafa;
+}
+.replyNotice .ry_cont a {
+    color:#3366ff;
+}
+
+.replyList {
+    margin-top:30px;
+}
+.replyList .ryw_info {
+    padding: 10px;
+    border-bottom: 1px solid #e0e0e0;
+    color:#555;
+}
+.replyList .ryw_info strong {
+    margin-right: 10px
+}
+.replyList .ryw_cont {
+    padding: 10px;
+    color:#555;
+    background: #fafafa;
+    border-bottom:1px solid #999;
+}
+</style>
 <!--댓글-->
 <div class="replySection" id="link">
     <form id="regi_form_comment" name="regi_form_comment" method="POST" onsubmit="return false;" novalidate>
@@ -12,10 +123,10 @@
         <div class="reply" id="reply">
             <div class="replyWrite">
                 <div class="textarBx">
-                    <textarea id="event_comment" name="event_comment" cols="30" rows="3"></textarea>
-                    <button type="button" class="NSK" id="btn_submit_comment">글쓰기</button>
+                    <textarea id="event_comment" name="event_comment" cols="30" rows="3"></textarea>               
                 </div>
                 <p> * 지나친 도배, 욕설, 주제와 상관없는 글은 예고 없이 관리자에 의해 삭제될 수 있습니다. </p>
+                <button type="button" class="btnrwt" id="btn_submit_comment">글쓰기</button>
             </div>
 
             <!--댓글공지-관리자만 등록,수정,삭제 가능-->

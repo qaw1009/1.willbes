ae7a0ccf1c9342a99b74ed909a55fadd70699b66
김창용 <winmir@willbes.com>
2019-05-21 09:27:40 +0900

    경찰 온라인 기본이론

--- views/willbes/pc/promotion/2001/1015.blade.php
+++ views/willbes/pc/promotion/2001/1015.blade.php
@@ -55,20 +55,20 @@
             <img src="https://static.willbes.net/public/images/promotion/2019/04/1015_04.jpg"  title="기본이론강좌소개" usemap="#link2" />
             <map name="link2" >
                 <!--단과-->
-                <area shape="rect" coords="444,406,516,440" href="{{ site_url('/lecture/show/cate/3001/pattern/only/prod-code/151727') }}" onfocus='this.blur()' title="형사소송법 기본이론(19년 3월)" target="_blank"/>
-                <area shape="rect" coords="444,457,516,491" href="{{ site_url('/lecture/show/cate/3001/pattern/only/prod-code/152354') }}" onfocus='this.blur()' title="경찰학개론 기본이론(19년 4월)" target="_blank"/>
-                <area shape="rect" coords="444,505,516,543" href="{{ site_url('/lecture/show/cate/3001/pattern/only/prod-code/132285') }}" onfocus='this.blur()' title="김원욱 형법 기본이론(19년 3월)" target="_blank"/>
-                <area shape="rect" coords="444,553,516,593" href="{{ site_url('/lecture/show/cate/3001/pattern/only/prod-code/132290') }}" onfocus='this.blur()' title="하승민 영어 기본이론(19년 3월)" target="_blank"/>
+                <area shape="rect" coords="444,406,516,440" href="{{ site_url('/lecture/show/cate/3001/pattern/only/prod-code/153645') }}" onfocus='this.blur()' title="형사소송법 기본이론(19년 3월)" target="_blank"/>
+                <area shape="rect" coords="444,457,516,491" href="{{ site_url('/lecture/show/cate/3001/pattern/only/prod-code/153644') }}" onfocus='this.blur()' title="경찰학개론 기본이론(19년 4월)" target="_blank"/>
+                <area shape="rect" coords="444,505,516,543" href="{{ site_url('/lecture/show/cate/3001/pattern/only/prod-code/153546') }}" onfocus='this.blur()' title="김원욱 형법 기본이론(19년 3월)" target="_blank"/>
+                <area shape="rect" coords="444,553,516,593" href="{{ site_url('/lecture/show/cate/3001/pattern/only/prod-code/153531') }}" onfocus='this.blur()' title="하승민 영어 기본이론(19년 3월)" target="_blank"/>
 
-                <area shape="rect" coords="868,406,944,441" href="{{ site_url('/lecture/show/cate/3001/pattern/only/prod-code/152386') }}" onfocus='this.blur()' title="원유철 한국사 기본이론(19년 3월)" target="_blank"/>
-                <area shape="rect" coords="868,455,944,491" href="{{ site_url('/lecture/show/cate/3001/pattern/only/prod-code/152384') }}" onfocus='this.blur()' title="오태진 한국사 기본이론(19년 3월)" target="_blank"/>
+                <area shape="rect" coords="868,406,944,441" href="{{ site_url('/lecture/show/cate/3001/pattern/only/prod-code/153627') }}" onfocus='this.blur()' title="원유철 한국사 기본이론(19년 3월)" target="_blank"/>
+                <area shape="rect" coords="868,455,944,491" href="{{ site_url('/lecture/show/cate/3001/pattern/only/prod-code/153626') }}" onfocus='this.blur()' title="오태진 한국사 기본이론(19년 3월)" target="_blank"/>
                 <area shape="rect" coords="868,505,944,542" href="{{ site_url('/lecture/show/cate/3002/pattern/only/prod-code/153345') }}" onfocus='this.blur()' title="신광은 수사 이론" target="_blank"/>
                 <area shape="rect" coords="868,553,944,593" href="{{ site_url('/lecture/show/cate/3002/pattern/only/prod-code/151726') }}" onfocus='this.blur()' title="장정훈 행정법 이론" target="_blank"/>
 
                 <!--패키지-->
-                <area shape="rect" coords="808,835,938,879" href="{{ site_url('/package/show/cate/3001/pack/648001/prod-code/152607') }}" onfocus='this.blur()' title="2019대비 일반경찰 기본이론 동영상 종합반(원유철史)" target="_blank"/>
-                <area shape="rect" coords="808,1056,938,1102" href="{{ site_url('/package/show/cate/3001/pack/648001/prod-code/152606') }}" onfocus='this.blur()' title="2019대비 일반경찰 기본이론 동영상 종합반(오태진史)" target="_blank"/>
-                <area shape="rect" coords="808,1274,938,1324" href="{{ site_url('/package/show/cate/3002/pack/648001/prod-code/153361') }}" onfocus='this.blur()' title="2019대비 경행경채 기본이론 동영상 종합반" target="_blank"/>
+                <area shape="rect" coords="808,835,938,879" href="{{ site_url('/package/show/cate/3001/pack/648001/prod-code/153649') }}" onfocus='this.blur()' title="2019대비 일반경찰 기본이론 동영상 종합반(원유철史)" target="_blank"/>
+                <area shape="rect" coords="808,1056,938,1102" href="{{ site_url('/package/show/cate/3001/pack/648001/prod-code/153648') }}" onfocus='this.blur()' title="2019대비 일반경찰 기본이론 동영상 종합반(오태진史)" target="_blank"/>
+                <area shape="rect" coords="808,1274,938,1324" href="{{ site_url('/package/show/cate/3002/pack/648001/prod-code/153650') }}" onfocus='this.blur()' title="2019대비 경행경채 기본이론 동영상 종합반" target="_blank"/>
             </map>
         </div><!--//wb_cts04-->
 

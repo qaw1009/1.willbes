48aae998e2b42826c4210758164f62f46bc9f3e4
최현탁 cht@willbes.com <cht@willbes.com>
2019-05-24 16:30:15 +0900

    프론트 경찰 학원 메인 퀵 매뉴 수정

--- views/willbes/pc/site/main_partial/quick_menu_2002.blade.php
+++ views/willbes/pc/site/main_partial/quick_menu_2002.blade.php
@@ -10,14 +10,3 @@
 </ul>
 {!! banner_html(element('메인_우측퀵_01', $data['arr_main_banner'])) !!}
 {!! banner_html(element('메인_우측퀵_02', $data['arr_main_banner'])) !!}
-{{--
-<li>
-    <div class="QuickSlider">
-        {!! banner_html(element('메인_우측퀵_01', $data['arr_main_banner'])) !!}
-    </div>
-</li>
-<li>
-    <div class="QuickSlider">
-        {!! banner_html(element('메인_우측퀵_02', $data['arr_main_banner'])) !!}
-    </div>
-</li>--}}

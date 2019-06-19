f57e4ad6294d17739b99b4e19309c03b570f2af2
김창용 <winmir@willbes.com>
2019-06-19 16:05:18 +0900

    자격증 프로모션

--- views/html/190619_certB.blade.php
+++ views/html/190619_certB.blade.php
@@ -11,30 +11,28 @@
         .evtContent {
             width:100% !important;
             min-width:1120px !important;
-            background:#ccc;
             margin-top:20px !important;
             padding:0 !important;
             background:#fff;
         }
         .evtContent span {vertical-align:auto}
-        .evtCtnsBox {width:100%; text-align:center; min-width:1120px;}
+		.evtCtnsBox {width:100%; text-align:center; min-width:1120px;}
+		
+		/************************************************************/  
 
 		.cert_top{background:url(https://static.willbes.net/public/images/promotion/2019/06/190619_certB_top_bg.jpg) no-repeat center top;}	
 		.cert_01{background:url(https://static.willbes.net/public/images/promotion/2019/06/190619_certB_01_bg.jpg) no-repeat center top;}	
 		.cert02{background:#fff;}
 		.cert03{background:#f5f5f5;}
-		.cert04 {position:relative;width:1120px; margin:0 auto} 
-		.cert04 label {position:absolute; z-index:1;width:20px;height:20px;                        }
-		.cert04 label.check1 {top:378px; left:679px;}
+		.cert04 {position:relative; width:1120px; margin:0 auto} 
+		.cert04 label {position:absolute; z-index:1; width:20px; height:20px;}
+		input[type="checkbox"] + label {width:30px;height:30px; outline:5px solid #15365d; background:#fff}
+		.cert04 label.check1 {top:380px; left:679px;}
 		.cert04 label.check2 {top:518px; left:930px;}
-		.cert04 label.check3 {top:647px; left:174px;}
-		.cert04 .check1{border:5px solid #15365d;width:30px;height:30px;background-color:#fff;}
-		.cert04 .check2{border:5px solid #15365d;width:30px;height:30px;background-color:#fff;}
-		.cert04 .check3{background-color: #fff;}
-		
-
-        /************************************************************/      
+		.cert04 label.check3 {top:645px; left:174px;}		
+		input[type="checkbox"] + label.check3 {background-color: #fff; outline:2px solid #15365d; width:20px;height:20px;}            
     </style>
+
 	<div class="evtContent">
 		<div class="evtCtnsBox cert_top">
 			<img src="https://static.willbes.net/public/images/promotion/2019/06/190619_certB_top.jpg" alt="전기/소방분야 올패스" />
@@ -55,9 +53,9 @@
 				<area shape="rect" coords="742,638,901,673" href="#;" target="_blank"  />
 				<area shape="rect" coords="271,771,857,858" href="#;" target="_blank"  />
 			</map>
-			<label class="check1"><input name="ischk1" type="checkbox" value="Y"></label>
-			<label class="check2"><input name="ischk2" type="checkbox" value="Y"></label>
-			<label class="check3"><input name="ischk3" type="checkbox" value="Y"></label>            
+			<input name="ischk1" type="checkbox" value="Y"><label class="check1"></label>
+			<input name="ischk2" type="checkbox" value="Y"><label class="check2"></label>
+			<input name="ischk3" type="checkbox" value="Y"><label class="check3"></label>            
 		</div>
 		<div class="evtCtnsBox">
 			<img src="https://static.willbes.net/public/images/promotion/2019/06/190619_certB_05.jpg" alt="이용안내" />
@@ -66,6 +64,5 @@
     <!-- End Container -->
 
     <script type="text/javascript">
-
     </script>
 @stop

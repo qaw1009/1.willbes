9b6e6d05ed67f6226564e2fc35e0ee773d8f8f12
이윤구 <yoongukk@willbes.com>
2019-06-26 13:09:27 +0900

    전기프로모션 페이지 버튼 링크 _blank 로 수정

--- views/willbes/pc/promotion/2006/1295.blade.php
+++ views/willbes/pc/promotion/2006/1295.blade.php
@@ -88,8 +88,7 @@
 			<div class="evtCtnsBox cert01">
 				<img src="https://static.willbes.net/public/images/promotion/2019/06/190618_certA_02.jpg" alt="윌비스와 함께" usemap="#Map1293A"/>
 				<map name="Map1293A" id="Map1293A">
-					<area shape="rect" coords="860,1441,1073,1504" href="https://job.willbes.net/book/index/cate/308902" alt="교재 구매하기" />
-					<area shape="rect" coords="843,1443,1056,1506" href="https://job.willbes.net/book/index/cate/308901" alt="교재 구매하기" />
+					<area shape="rect" coords="860,1441,1073,1504" href="https://job.willbes.net/book/index/cate/308902" target="_blank" alt="교재 구매하기" />
 				</map>
 			</div>
 			<div class="evtCtnsBox cert02">

d93438da4deaef2009b8c48b652e1fa1eb1476b3
김창용 <winmir@willbes.com>
2019-06-28 15:51:37 +0900

    제로백

--- views/willbes/pc/promotion/2003/1286.blade.php
+++ views/willbes/pc/promotion/2003/1286.blade.php
@@ -77,7 +77,7 @@
         </div>
         {{--홍보url--}}
         @if( empty($data['data_option_ccd']) === false && array_key_exists($arr_base['option_ccd']['comment_list'], $data['data_option_ccd']) === true && array_key_exists($arr_base['comment_use_area']['event'], $data['data_comment_use_area']) === true)
-            @include('willbes.pc.promotion.show_comment_list_emoticon_url_partial')
+            @include('willbes.pc.promotion.show_comment_list_url_partial')
         @endif
 
         {{--기본댓글--}}

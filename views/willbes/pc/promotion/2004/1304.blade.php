cb836531d9e45f37a04952ed642f9e9e2a0abc82
김창용 <winmir@willbes.com>
2019-07-03 16:57:03 +0900

    군무원 면접반

--- views/willbes/pc/promotion/2004/1304.blade.php
+++ views/willbes/pc/promotion/2004/1304.blade.php
@@ -30,14 +30,14 @@
         .WB_con01 {background:#65a285 url('https://static.willbes.net/public/images/promotion/2019/07/1304_top_bg.jpg') no-repeat center;}
         .WB_con02 {background:#fef6ef;}
         .WB_con03 {background:#f7e7d0 url('https://static.willbes.net/public/images/promotion/2019/07/1304_02_bg.jpg') no-repeat center;}
-		.WB_con04 {background:#ea592a;}
+		    .WB_con04 {background:#ea592a;}
 
     </style>
 
   <div class="evtContent NSK" id="evtContainer">
   
   <div class="skybanner"> 
-	 <a href="#1304_go"><img src="https://static.willbes.net/public/images/promotion/2019/07/1304_sky.png" alt="신청하기" /></a>
+	 <a href="https://pass.willbes.net/pass/event/show/ongoing?event_idx=316&" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2019/07/1304_sky.png" alt="신청하기" /></a>
   </div>
   <!-- skybnr //--> 
   
@@ -62,7 +62,7 @@
  <div class="evtCtnsBox WB_con04" id="1304_go">
 	 <img src="https://static.willbes.net/public/images/promotion/2019/07/1304_03.jpg" alt="#" usemap="#Map_down" border="0" />
 	<map name="Map_down">
-	  <area shape="rect" coords="148,673,987,786" href="javascript:alert('교육일정 준비중');" target="_blank">
+	  <area shape="rect" coords="148,673,987,786" href="@if(empty($file_yn) === false && $file_yn[1] == 'Y') {{ front_url($file_link[1]) }} @else {{ $file_link[1] }} @endif" target="_blank">
 	</map>
   </div>
  <!-- WB_con03//-->

702340206cb6553ba1a87679ae6b046715cecd24
최현탁 cht@willbes.com <cht@willbes.com>
2019-07-03 16:58:23 +0900

    서포터즈 검수 수정

--- views/willbes/pc/classroom/index.blade.php
+++ views/willbes/pc/classroom/index.blade.php
@@ -88,6 +88,12 @@
                 </div>
                 <div class="Quick-Top">
                     {!! banner('내강의실_우측퀵', 'Quick-Bnr', $__cfg['SiteCode'], '0') !!}
+                    {{-- 서포터즈 회원인 경우 배너 노출 --}}
+                    @if (empty($data['supporters']) === false)
+                        <a href="{{ ($data['supporters']['SiteCode'] == '2001') ? 'https://police.local.willbes.net' : 'https://pass.local.willbes.net' }}/supporters/home/index">
+                            <img class="mt10" src="https://static.willbes.net/public/images/willbes/mypage/banner_supporters.jpg">
+                        </a>
+                    @endif
                 </div>
             </div>
         </div>

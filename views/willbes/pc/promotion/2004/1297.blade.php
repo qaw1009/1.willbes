16b24486fbd896144d6e2eb5f0d247d88cb36a43
김창용 <winmir@willbes.com>
2019-07-08 16:09:02 +0900

    공무원 아침실전모의고사

--- views/willbes/pc/promotion/2004/1297.blade.php
+++ views/willbes/pc/promotion/2004/1297.blade.php
@@ -10,100 +10,61 @@
         }
         .evtContent {
             width:100% !important;
-            min-width:1210px !important;
+            min-width:1160px !important;
             background:#ccc;
             margin-top:20px !important;
             padding:0 !important;
             background:#fff;
         }
-        .evtCtnsBox {width:100%; text-align:center; min-width:1210px;}
+        .evtContent span {vertical-align:auto}
+        .evtCtnsBox {width:100%; text-align:center; min-width:1160px;}
 
         /************************************************************/
-
-        .wb_skybn {position:fixed; bottom:80px; right:10px; width:100px; z-index:10}
-        .wb_cts01 {position:relative;overflow:hidden; min-width:1210px; text-align:center; background:#5f4b99 url("http://file3.willbes.net/new_gosi/2019/01/EV190118_1_bg.gif") center top  no-repeat; margin-top:5px;}
+        .wb_cts01 {background:#5f4b99 url("https://static.willbes.net/public/images/promotion/2019/07/1297_top_bg.jpg") center top  no-repeat}
         .wb_cts02 {background:#fff;}
-        .wb_cts03 {background:#f8f9fb;}
-        .wb_cts04 {background:#fff;}
-        .wb_cts05 {background:#f8f9fb;}
-        .wb_cts06 {position:relative; overflow:hidden; background:#21375e;}
+        .wb_cts03 {background:#fff; padding-bottom:150px}
+        .wb_last {background:#233758;}
 
-        /*업다운 애니메이션(퀵배너)*/
-        #event_sky .quick {position:fixed; right:10px; top:200px; z-index:10; animation:upDown 1s infinite;-webkit-animation:upDown 1s infinite}
-        #event_sky .quick li{padding-bottom:5px;}
-        @@keyframes upDown{
-             from{margin-top:0}
-             60%{margin-top:-30px}
-             to{margin-top:0}
-         }
-        @@-webkit-keyframes upDown{
-             from{margin-top:0}
-             60%{margin-top:-30px}
-             to{margin-top:0}
-         }
 
+        .quick {position:fixed; right:10px; top:200px; z-index:10;}
+        .quick li{padding-bottom:5px;}
     </style>
 
-    <div class="p_re evtContent NSK" id="evtContainer">
-        <div id="event_sky">
-            <div class="quick">
-                <ul>
-                    <li><img src="http://file3.willbes.net/new_gosi/2019/01/EV190118_sky1.png" alt="학원문의" ></li>
-                    <li><a href="https://pass.willbes.net/pass/event/show/ongoing?event_idx=231&" target="_blank" ><img src="https://static.willbes.net/public/images/promotion/2019/04/1098_sky.jpg" alt="국어기미진개강" ></a></li>
-                    <li><a href="https://pass.willbes.net/pass/event/show/ongoing?event_idx=230&" target="_blank" ><img src="https://static.willbes.net/public/images/promotion/2019/04/1098_sky2.jpg" alt="영어한덕현개강" ></a></li>
-                </ul>
-            </div>
-        </div>
+    <div class="p_re evtContent NGR" id="evtContainer">
+        <ul class="quick">
+            <li><img src="https://static.willbes.net/public/images/promotion/2019/07/1297_q01.jpg" alt="학원문의" ></li>
+            <li><a href="#none"><img src="https://static.willbes.net/public/images/promotion/2019/07/1297_q02.jpg" alt="국어 기미진 개강" ></a></li>
+            <li><a href="#none"><img src="https://static.willbes.net/public/images/promotion/2019/07/1297_q03.jpg" alt="영어 한덕현 개강" ></a></li>
+        </ul>
         <!--//Quick-->
 
-        <div class="evtCtnsBox wb_cts01"><img src="http://file3.willbes.net/new_gosi/2019/01/EV190118_1.png" alt="아침실전모고" /></div>
+        <div class="evtCtnsBox wb_cts01">
+            <img src="https://static.willbes.net/public/images/promotion/2019/07/1297_top.jpg" alt="아침실전모고" />
+        </div>
         <!--wb_cts01//-->
 
         <div class="evtCtnsBox wb_cts02">
-            <img src="http://file3.willbes.net/new_gosi/2019/01/EV190118_2.png" alt="기미진국어" usemap="#Map_1123_lec1" border="0" />
-            <map name="Map_1123_lec1">
-                <area shape="rect" coords="318,642,894,715" href="{{ site_url('/pass/professor/show/prof-idx/50242/?cate_code=3043&subject_idx=1253&subject_name=%EA%B5%AD%EC%96%B4&tab=open_lecture') }}" target="_blank">
+            <img src="https://static.willbes.net/public/images/promotion/2019/07/1297_01.jpg" alt="기미진국어" usemap="#Map1297A" border="0" />
+            <map name="Map1297A" id="Map1297A">
+                <area shape="rect" coords="275,607,851,680" href="https://pass.willbes.net/pass/professor/show/prof-idx/50242/?cate_code=3043&amp;subject_idx=1253&amp;subject_name=국어&amp;tab=open_lecture" target="_blank" alt="수강신청">
             </map>
         </div>
         <!--wb_cts02//-->
 
         <div class="evtCtnsBox wb_cts03">
-            <img src="http://file3.willbes.net/new_gosi/2019/01/EV190118_3.png" alt="한덕현영어" usemap="#Map_1123_lec2" border="0" />
-            <map name="Map_1123_lec2">
-                <area shape="rect" coords="314,676,895,754" href="{{ site_url('/pass/professor/show/prof-idx/50500/?cate_code=3043&subject_idx=1254&subject_name=%EC%98%81%EC%96%B4&tab=open_lecture') }}" target="_blank">
+            <img src="https://static.willbes.net/public/images/promotion/2019/07/1297_02.jpg" alt="한덕현영어" usemap="#Map1297B" border="0" />
+            <map name="Map1297B" id="Map1297B">
+                <area shape="rect" coords="281,747,843,819" href="https://pass.willbes.net/pass/professor/show/prof-idx/50500/?cate_code=3043&amp;subject_idx=1254&amp;subject_name=영어&amp;tab=open_lecture" target="_blank" alt="수강신청하기">
             </map>
         </div>
         <!--wb_cts03//-->
 
-        <div class="evtCtnsBox wb_cts04">
-            <img src="http://file3.willbes.net/new_gosi/2019/01/EV190118_4.png" alt="김영영어" usemap="#Map_1123_lec3" border="0" />
-            <map name="Map_1123_lec3">
-                <area shape="rect" coords="288,695,928,759" href="{{ site_url('/pass/professor/show/prof-idx/50384/?cate_code=3043&subject_idx=1254&subject_name=%EC%98%81%EC%96%B4&tab=open_lecture') }}" target="_blank">
-            </map>
-        </div>
-        <!--wb_cts04//-->
 
-        <div class="evtCtnsBox wb_cts05">
-            <img src="http://file3.willbes.net/new_gosi/2019/01/EV190118_5.png" alt="성기건영어" usemap="#Map_1123_lec4" border="0" />
-            <map name="Map_1123_lec4">
-                <area shape="rect" coords="282,694,909,761" href="{{ site_url('/pass/professor/show/prof-idx/50346/?cate_code=3043&subject_idx=1254&subject_name=%EC%98%81%EC%96%B4&tab=open_lecture') }}" target="_blank">
-            </map>
-        </div>
-        <!--wb_cts05//-->
-
-        <div class="evtCtnsBox wb_cts06">
-            <img src="http://file3.willbes.net/new_gosi/2019/01/EV190118_6.gif" alt="#" />
+        <div class="evtCtnsBox wb_last">
+            <img src="https://static.willbes.net/public/images/promotion/2019/07/1297_last.gif" alt="#" />
         </div>
         <!--wb_cts06//-->
 
     </div>
     <!-- End Container -->
-
-    <script>
-        $(function(e){
-            var targetOffset= $("#evtContainer").offset().top;
-            $('html, body').animate({scrollTop: targetOffset}, 700);
-            /*e.preventDefault(); */
-        });
-    </script>
 @stop

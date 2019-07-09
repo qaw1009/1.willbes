6a4ea06c06474152570f8b9b66005b03f59b7784
이윤구 <yoongukk@willbes.com>
2019-07-09 11:39:15 +0900

    공무원 test

--- views/willbes/pc/promotion/2003/1313.blade.php
+++ views/willbes/pc/promotion/2003/1313.blade.php
@@ -22,7 +22,20 @@
         /************************************************************/
 
 .wb_cts01{background:#1c1c1c url("https://static.willbes.net/public/images/promotion/2019/07/1313_top_bg.jpg") center top  no-repeat}
-        
+
+.wb_cts02{background:#1c1c1c url("https://static.willbes.net/public/images/promotion/2019/07/1313_mid_bg.jpg") center top  no-repeat}
+
+.tabContaier{width:1120px;margin:0 auto;}
+.tabContaier li{display:inline-block;width:280px;height:84px;line-height:84px;background:#fff;color:#000;float:left;font-size:18px;font-weight:bold;}
+.tabContaier:after {content:""; display:block; clear:both}
+.tabContaier li a{display:block;}
+.tabContaier li a:hover,
+.tabContaier li a.active {background:#b6061b;color:#fff;}
+
+
+
+
+
 
  </style>
 
@@ -52,42 +65,52 @@
   <!-- 타이머 //-->
   
   <div class="evtCtnsBox wb_cts01">
-    <img src="https://static.willbes.net/public/images/promotion/2019/07/1313_top.jpg"  title="윌비스 T-PASS" />
+    <img src="https://static.willbes.net/public/images/promotion/2019/07/1313_top.jpg"  title="군무원 0원 패스" />
   </div>  
   <!--wb_cts01//-->  
   <div class="evtCtnsBox wb_cts02">
-    <img src="https://static.willbes.net/public/images/promotion/2019/07/1313_mid.jpg"  title="윌비스 T-PASS" />
-    <ul class="tab">
-        <li><a href="#tab1">국어</a></li>
-        <li><a href="#tab2">행정학</a></li>
-        <li><a href="#tab3">행정법</a></li>
-        <li><a href="#tab4">한국사능력검정시험</a></li>
-    </ul>
-    <div id="tab1">       
-        <img src="https://static.willbes.net/public/images/promotion/2019/07/1313_tab_01.jpg" title="기미진" />      
+    <img src="https://static.willbes.net/public/images/promotion/2019/07/1313_mid.jpg"  title="윌비스 교수진과 함께" />
+    <div class="tabContaier">    
+        <ul>    
+            <li>
+                <a href="#tab1" class="">국어</a>
+            </li>
+            <li>
+                <a href="#tab2">행정학</a>
+            </li>
+            <li>
+                <a href="#tab3">행정법</a>
+            </li>
+            <li>
+                <a href="#tab4">한국사능력검정시험</a>
+            </li>
+        </ul>
+    </div> 
+    <div id="tab1" class="tabContents">       
+        <img src="https://static.willbes.net/public/images/promotion/2019/07/1313_tab_01.jpg" title="국어탭" />      
     </div>
-    <div id="tab2">       
-        <img src="https://static.willbes.net/public/images/promotion/2019/07/1313_tab_02.jpg" title="기미진" />      
+    <div id="tab2" class="tabContents">       
+        <img src="https://static.willbes.net/public/images/promotion/2019/07/1313_tab_02.jpg"  title="행정학탭" />      
     </div>
-    <div id="tab3">       
-        <img src="https://static.willbes.net/public/images/promotion/2019/07/1313_tab_03.jpg" title="기미진" />      
+    <div id="tab3" class="tabContents">       
+        <img src="https://static.willbes.net/public/images/promotion/2019/07/1313_tab_03.jpg"  title="행정법탭" />      
     </div>
-    <div id="tab4">       
-        <img src="https://static.willbes.net/public/images/promotion/2019/07/1313_tab_04.jpg" title="기미진" />      
+    <div id="tab4" class="tabContents">       
+        <img src="https://static.willbes.net/public/images/promotion/2019/07/1313_tab_04.jpg" title="한극사능력검정시험탭" />      
     </div>   
     <div>
-        <img src="https://static.willbes.net/public/images/promotion/2019/07/1313_mid_curri.jpg" title="기미진" />
+        <img src="https://static.willbes.net/public/images/promotion/2019/07/1313_mid_curri.jpg" title="커리큘럼" />
     </div>
     </div> 
   </div>  
   </div>
   <!--wb_cts02//-->  
   <div class="evtCtnsBox wb_cts03">
-     <img src="https://static.willbes.net/public/images/promotion/2019/07/1313_pass.jpg" title="기미진" />
+     <img src="https://static.willbes.net/public/images/promotion/2019/07/1313_pass.jpg" title="수강신청" />
   </div>
   <!--wb_cts03//-->  
   <div class="evtCtnsBox wb_cts04">
-     <img src="https://static.willbes.net/public/images/promotion/2019/07/1313_bot.jpg" title="기미진" />  
+     <img src="https://static.willbes.net/public/images/promotion/2019/07/1313_bot.jpg" title="이용안내" />  
   </div>
   <!--wb_cts04//-->
   
@@ -101,29 +124,20 @@
 
 <script type="text/javascript">
 $(document).ready(function(){
-        $('.tab').each(function(){
-            var $active, $content, $links = $(this).find('a');
-            $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
-            $active.addClass('active');
-        
-            $content = $($active[0].hash);
-        
-            $links.not($active).each(function () {
-            $(this.hash).hide()});
-        
-            // Bind the click event handler
-            $(this).on('click', 'a', function(e){
-            $active.removeClass('active');
-            $content.hide();
-        
-            $active = $(this);
-            $content = $(this.hash);
-        
-            $active.addClass('active');
-            $content.show();
-        
-            e.preventDefault()})})}
-        );
+			$(".tabContents").hide(); 
+			$(".tabContents:first").show();
+
+			$(".tabContaier ul li a").click(function(){ 
+
+			var activeTab = $(this).attr("href"); 
+			$(".tabContaier ul li a").removeClass("active"); 
+			$(this).addClass("active"); 
+			$(".tabContents").hide(); 
+			$(activeTab).fadeIn(); 
+
+			return false; 
+			});
+		});						
  /*디데이카운트다운*/
  $(document).ready(function() {
             dDayCountDown('{{$arr_promotion_params['edate']}}');

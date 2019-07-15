262ccb1398f8c01fa4bb36b8469c6eb459f2a963
이윤구 <yoongukk@willbes.com>
2019-07-15 10:42:34 +0900

    하승민 영어 T-PASS 신규 프로모션

--- views/willbes/pc/promotion/2001/1316.blade.php
+++ views/willbes/pc/promotion/2001/1316.blade.php
@@ -102,7 +102,7 @@
             }
             var lUrl;
             if(no == 1){
-                lUrl = "#";
+                lUrl = "https://police.willbes.net/periodPackage/show/cate/3008/pack/648001/prod-code/155451";
             }
             location.href = lUrl;
         }
@@ -111,107 +111,6 @@
             location.href = '#careful';
         }
     </script>
-
-        <script type="text/javascript">
-        function goCartNDirectPay(ele_id, field_name, cart_type, learn_pattern, is_direct_pay)
-    {
-        alert("로그인 후 이용하여 주십시오.");
-return;
-
-        var $regi_form = $('#' + ele_id);
-        var $prod_code = $regi_form.find('input[name="' + field_name + '"]:checked');   // 상품코드
-        var $is_chk = $regi_form.find('input[name="is_chk"]');  // 동의여부
-        var params;
-
-        if ($is_chk.length > 0) {
-            if ($is_chk.is(':checked') === false) {
-                alert('이용안내에 동의하셔야 합니다.');
-                return;
-            }
-        }
-
-        if ($prod_code.length < 1) {
-            alert('강좌를 선택해 주세요.');
-            return;
-        }
-
-                params = [
-            { 'name' : '_csrf_token', 'value' : 'd74589844f72a04cf7b4e31d64296e43' },
-            { 'name' : '_method', 'value' : 'POST' },
-            { 'name' : 'cart_type', 'value' : cart_type },
-            { 'name' : 'learn_pattern', 'value' : learn_pattern },
-            { 'name' : 'is_direct_pay', 'value' : is_direct_pay }
-        ];
-
-                $prod_code.each(function() {
-            params.push({ 'name' : 'prod_code[]', 'value' : $(this).val() + ':613001:' + $(this).val() });
-        });
-
-                formCreateSubmit('//police.willbes.net/cart/store', params, 'POST');
-    }
-
-    // 날짜차이 계산
-    var dDayDateDiff = {
-        inDays: function(dd1, dd2) {
-            var tt2 = dd2.getTime();
-            var tt1 = dd1.getTime();
-
-            return Math.floor((tt2-tt1) / (1000 * 60 * 60 * 24));
-        },
-        inWeeks: function(dd1, dd2) {
-            var tt2 = dd2.getTime();
-            var tt1 = dd1.getTime();
-
-            return parseInt((tt2-tt1)/(24*3600*1000*7));
-        },
-        inMonths: function(dd1, dd2) {
-            var dd1Y = dd1.getFullYear();
-            var dd2Y = dd2.getFullYear();
-            var dd1M = dd1.getMonth();
-            var dd2M = dd2.getMonth();
-
-            return (dd2M+12*dd2Y)-(dd1M+12*dd1Y);
-        },
-        inYears: function(dd1, dd2) {
-            return dd2.getFullYear()-dd1.getFullYear();
-        }
-    };
-
-        function dDayCountDown(end_date) {
-        // 마감일 1개월전 날짜 (사용안함)
-        //var arr_start_date = moment(end_date).add(-1, 'months').format('YYYY-M-D').split('-');
-        var arr_end_date = end_date.split('-');
-        var event_day = new Date(arr_end_date[0], parseInt(arr_end_date[1]) - 1, arr_end_date[2], 23, 59, 59);
-        var now = new Date();
-        var timeGap = new Date(0, 0, 0, 0, 0, 0, (event_day - now));
-
-        var Monthleft = event_day.getMonth() - now.getMonth();
-        var Dateleft = dDayDateDiff.inDays(now, event_day);
-        var Hourleft = timeGap.getHours();
-        var Minuteleft = timeGap.getMinutes();
-        var Secondleft = timeGap.getSeconds();
-
-        if ((event_day.getTime() - now.getTime()) > 0) {
-            $('#dd1').attr('src', 'https://static.willbes.net/public/images/promotion/common/' + parseInt(Dateleft/10) + '.png');
-            $('#dd2').attr('src', 'https://static.willbes.net/public/images/promotion/common/' + parseInt(Dateleft%10) + '.png');
-
-            $('#hh1').attr('src', 'https://static.willbes.net/public/images/promotion/common/' + parseInt(Hourleft/10) + '.png');
-            $('#hh2').attr('src', 'https://static.willbes.net/public/images/promotion/common/' + parseInt(Hourleft%10) + '.png');
-
-            $('#mm1').attr('src', 'https://static.willbes.net/public/images/promotion/common/' + parseInt(Minuteleft/10) + '.png');
-            $('#mm2').attr('src', 'https://static.willbes.net/public/images/promotion/common/' + parseInt(Minuteleft%10) + '.png');
-
-            $('#ss1').attr('src', 'https://static.willbes.net/public/images/promotion/common/' + parseInt(Secondleft/10) + '.png');
-            $('#ss2').attr('src', 'https://static.willbes.net/public/images/promotion/common/' + parseInt(Secondleft%10) + '.png');
-
-            setTimeout(function() {
-                dDayCountDown(end_date);
-            }, 1000);
-        } else {
-            $('#newTopDday').hide();
-        }
-    }
-</script>
 
 
 @stop

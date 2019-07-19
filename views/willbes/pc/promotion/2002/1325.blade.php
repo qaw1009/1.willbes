440654130a53674e0265d8ee68a673564f6ecb9a
이윤구 <yoongukk@willbes.com>
2019-07-19 15:13:17 +0900

    6개월 12 슈퍼패스 페이지

---
+++ views/willbes/pc/promotion/2002/1325.blade.php
@@ -0,0 +1,187 @@
+@extends('willbes.pc.layouts.master')
+
+@section('content')
+    @include('willbes.pc.layouts.partial.site_menu')
+    <!-- Container -->
+    <style type="text/css">
+        .subContainer {
+            min-height: auto !important;
+            margin-bottom:0 !important;
+        }
+        .evtContent {
+            position:relative;
+            width:100% !important;
+            min-width:1210px !important;
+            background:#ccc;
+            margin-top:20px !important;
+            padding:0 !important;
+            background:#fff;
+        }
+        .evtContent span {vertical-align:auto}
+        .evtCtnsBox {width:100%; text-align:center; min-width:1120px;}
+
+        /************************************************************/
+
+        .skybanner {
+            position:fixed;
+            top:250px;
+            right:0;
+            z-index:1;
+        }
+
+        .wb_mp4 {background:#000;}
+
+        .wb_cts00 {background:#404040}
+        .wb_cts01 {background:#191c22 url(https://static.willbes.net/public/images/promotion/2019/07/1325_top_bg.jpg) no-repeat center top; }   
+
+        .wb_cts02 {background:#fff}
+        .wb_cts03 {background:#abb1b9}
+        .wb_cts04 {background:#696d73}
+		.wb_cts05 {background:#fff}
+        .wb_cts06 {background:#f8f9fa}
+        .wb_cts07 {background:#292b31}
+        
+        /*타이머*/
+        .time {width:100%; text-align:center; background:#e1e1e1; padding:20px 0}
+        .time ul {width:1000px; margin:0 auto}     
+        .time li {display:inline; float:left; font-size:30px; line-height:70px; letter-spacing:-1px; color:#000;}   
+        .time li img {width:70%}
+        .time li span {color:#d63e4d; font-size:30px; animation:upDown 2s infinite;-webkit-animation:upDown 2s infinite;}
+        .time li:first-child {text-align:left; line-height:1; font-size:20px; padding:10px 20px 0}
+        .time li:last-child {text-align:right: padding:0 20px}
+        .time ul:after { content:""; display:block; clear:both}
+        @@keyframes upDown{
+        from{color:#000}
+        50%{color:#424ac7}
+        to{color:#000}
+        }
+        @@-webkit-keyframes upDown{
+        from{color:#000}
+        50%{color:#424ac7}
+        to{color:#000}
+        } 
+
+        .tabContaier{width:100%; text-align:center; padding-bottom:20px;}
+        .tabContaier ul {width:1120px;margin:0 auto;}		
+        .tabContaier li {display:inline; float:left;}	
+        .tabContaier a img.off {display:block}
+        .tabContaier a img.on {display:none}
+        .tabContaier a.active img.off {display:none}
+        .tabContaier a.active img.on {display:block}
+        .tabContaier ul:after {content:""; display:block; clear:both}
+
+
+    </style>
+
+    <div class="p_re evtContent NGR" id="evtContainer">
+        <!-- 타이머 -->
+        <div class="evtCtnsBox time NGEB"  id="newTopDday">
+            <ul>
+                <li>마감까지<br><span>남은 시간은</span></li>
+                <li><img id="dd1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                <li><img id="dd2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                <li>일</li>
+                <li><img id="hh1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                <li><img id="hh2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                <li>:</li>
+                <li><img id="mm1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                <li><img id="mm2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                <li>:</li>
+                <li><img id="ss1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                <li><img id="ss2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                <li><span>{{ kw_date('n/j(%)', $arr_promotion_params['edate']) }} </span>마감!</li>
+            </ul>
+        </div>
+        <!-- 타이머 //-->
+        <div class="skybanner" >
+            <a href="#lect"><img src="https://static.willbes.net/public/images/promotion/2019/07/1325_skybanner.png" alt="스카이베너" usemap="#Map1325c" border="0" ></a>
+        </div>
+
+		<div class="evtCtnsBox wb_cts00">
+            <img src="https://static.willbes.net/public/images/promotion/2019/06/1283_00.jpg" alt="슈퍼pass"/>            
+        </div>
+
+        <div class="evtCtnsBox wb_cts01" >
+            <img src="https://static.willbes.net/public/images/promotion/2019/07/1325_top.jpg" alt="슈퍼pass"  />
+        </div>
+
+        <div class="evtCtnsBox wb_cts02" >
+            <img src="https://static.willbes.net/public/images/promotion/2019/07/1325_01.jpg" alt="슈퍼pass"  />
+        </div>
+
+        <div class="evtCtnsBox wb_cts03">
+            <img src="https://static.willbes.net/public/images/promotion/2019/07/1325_02.jpg" alt="슈퍼pass"  />
+        </div>
+
+        <div class="evtCtnsBox wb_cts04" >
+            <img src="https://static.willbes.net/public/images/promotion/2019/06/1283_03.jpg" alt="슈퍼pass"  />
+        </div>
+
+        <div class="evtCtnsBox wb_cts05" id="lect">
+            <img src="https://static.willbes.net/public/images/promotion/2019/07/1325_04.jpg" alt="슈퍼pass"  />   
+            <div class="tabContaier">
+                <ul>
+                    <li>
+                        <a class="active" href="#tab1"><img src="https://static.willbes.net/public/images/promotion/2019/07/1325_04_tab1_off.jpg" class="off" />
+                        <img src="https://static.willbes.net/public/images/promotion/2019/07/1325_04_tab1_on.jpg" class="on"  /></a></li>
+                    <li>
+                        <a href="#tab2"><img src="https://static.willbes.net/public/images/promotion/2019/07/1325_04_tab2_off.jpg"  class="off"  />
+                        <img src="https://static.willbes.net/public/images/promotion/2019/07/1325_04_tab2_on.jpg"  class="on" /></a>
+                    </li>                  
+                </ul>
+                <div class="tabContents" id="tab1">
+                <img src="https://static.willbes.net/public/images/promotion/2019/07/1325_04_tab1_c1.jpg" alt="6개월" usemap="#1325a" border="0" />
+                    <map name="1325a" id="1325a">
+                        <area shape="rect" coords="377,1018,743,1129" 
+                        href="https://police.willbes.net/pass/OffVisitPackage?cate_code=3010&campus_ccd=605001&course_idx=1085" target="_blank" alt="6개월"/>
+                    </map>    
+                </div>
+                <div class="tabContents" id="tab2">
+                <img src="https://static.willbes.net/public/images/promotion/2019/07/1325_04_tab2_c2.jpg" alt="12개월" usemap="#1325b" border="0" />
+                    <map name="1325b" id="1325b"> 
+                        <area shape="rect" coords="365,667,751,787"
+                         href="https://police.willbes.net/pass/OffVisitPackage?cate_code=3010&campus_ccd=605001&course_idx=1085" target="_blank" alt="12개월"/>
+                    </map>
+                </div>
+			</div> 
+        </div>
+		
+		<div class="evtCtnsBox wb_cts06" >
+            <img src="https://static.willbes.net/public/images/promotion/2019/06/1283_05.jpg" alt="슈퍼pass"  />
+        </div>
+
+        <div class="evtCtnsBox wb_cts07" >
+            <img src="https://static.willbes.net/public/images/promotion/2019/07/1325_06.jpg" alt="슈퍼pass"  />
+        </div>
+    </div>
+    <!-- End Container -->
+
+    <script type="text/javascript">
+        /*디데이카운트다운*/
+        $(document).ready(function() {
+            dDayCountDown('{{$arr_promotion_params['edate']}}');
+        });
+
+        /*tab*/
+		$(document).ready(function(){
+			$(".tabContents").hide(); 
+			$(".tabContents:first").show();
+
+			$(".tabContaier ul li a").click(function(){ 
+
+			var activeTab = $(this).attr("href"); 
+			$(".tabContaier ul li a").removeClass("active"); 
+			$(this).addClass("active"); 
+			$(".tabContents").hide(); 
+			$(activeTab).fadeIn(); 
+
+			return false; 
+			});
+		});						
+
+    </script>
+
+    {{-- 프로모션용 스크립트 include --}}
+    @include('willbes.pc.promotion.promotion_script')
+
+@stop

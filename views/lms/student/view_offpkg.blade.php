a1f8a0ad28771c2c1288c392f8066d3af8b76440
하준걸 <hereisme@willbes.net>
2019-07-19 12:06:10 +0900

    수강생관리 수정

--- views/lms/student/view_offpkg.blade.php
+++ views/lms/student/view_offpkg.blade.php
@@ -1,7 +1,7 @@
 @extends('lcms.layouts.master')
 
 @section('content')
-    <h5>- 온라인 단강좌 수강생을 관리하는 메뉴입니다.</h5>
+    <h5>- 종합반 수강생(결제완료자) 현황을 확인할 수 있습니다.(종합반에 포함된 단과반 수강생 현황도 확인 가능)</h5>
     <div class="x_panel mt-10">
         <div class="x_content">
             <table class="table table-striped table-bordered">
@@ -46,7 +46,7 @@
             </table>
         </div>
     </div>
-    <form class="form-horizontal" id="search_form" name="search_form" method="POST" onsubmit="return false;">
+    <form class="form-horizontal" id="search_form_view" name="search_form_view" method="POST" onsubmit="return false;">
         {!! csrf_field() !!}
         <input type="hidden" name="ProdCode" value="{{$lec['ProdCode']}}" />
         <div class="x_panel">
@@ -119,6 +119,7 @@
             <div class="col-xs-12 text-center">
                 <button type="submit" class="btn btn-primary btn-search" id="btn_search"><i class="fa fa-spin fa-refresh"></i>&nbsp; 검 색</button>
                 <button type="button" class="btn btn-default btn-search" id="btn_reset_in_set_search_date">초기화</button>
+                <button class="btn btn-primary" type="button" id="btn_list">목록으로</button>
             </div>
         </div>
     </form>
@@ -149,7 +150,7 @@
     </div>
     <script type="text/javascript">
         var $datatable;
-        var $search_form = $('#search_form');
+        var $search_form = $('#search_form_view');
         var $list_table = $('#list_ajax_table');
 
         $(document).ready(function() {
@@ -207,6 +208,10 @@
                 if (confirm('엑셀다운로드 하시겠습니까?')) {
                     formCreateSubmit('{{ site_url('/student/'.$lecType.'/excel/') }}', $search_form.serializeArray(), 'POST');
                 }
+            });
+
+            $('#btn_list').click(function() {
+                location.replace('{{ site_url('/student/offpkg/') }}' + getQueryString());
             });
         });
     </script>

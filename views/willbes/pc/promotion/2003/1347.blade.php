c8227a8da6cf8c6826ba1052087dd206bfddc976
이윤구 <yoongukk@willbes.com>
2019-08-01 12:27:43 +0900

    no message

--- views/willbes/pc/promotion/2003/1347.blade.php
+++ views/willbes/pc/promotion/2003/1347.blade.php
@@ -27,14 +27,27 @@
         .wb_cts01 li {display:inline; float:left; margin-right:5px; width:277px;}
         .wb_cts01 li:last-child {margin:0; background:#c0c0c0; height:177px; line-height:177px; text-align:center}
         .wb_cts01 ul:after {content:""; display:block; clear:both}
-        .wb_cts02 {background:url(https://static.willbes.net/public/images/promotion/2019/08/1347_02_bg.jpg) no-repeat center top;}
+        .wb_cts02 {background:url(https://static.willbes.net/public/images/promotion/2019/08/1347_02x_bg.jpg) no-repeat center top;position:relative;}
+        .wb_cts02 span.img01 {position:absolute; top:285px; left:16%; margin-left:125px; z-index:10; animation:rubberBand2 2s infinite 1s;}
+        .wb_cts02 span.img02 {position:absolute; top:285px; left:27%; margin-left:125px; z-index:10; animation:rubberBand2 4s infinite 1s;}
+        .wb_cts02 span.img03 {position:absolute; top:285px; left:38%; margin-left:125px; z-index:10; animation:rubberBand2 3s infinite 1s;}
+        .wb_cts02 span.img04 {position:absolute; top:285px; left:49%; margin-left:125px; z-index:10; animation:rubberBand2 2s infinite 1s;}
+        .wb_cts02 span.img05 {position:absolute; top:285px; left:60%; margin-left:125px; z-index:10; animation:rubberBand2 3s infinite 1s;}
+        @@keyframes rubberBand2{
+        0%{-webkit-transform:scale3d(1,1,1);transform:scale3d(1,1,1)}
+        50%{-webkit-transform:scale3d(.85,.85,1);transform:scale3d(.85,.85,1)} 
+        100%{-webkit-transform:scale3d(1,1,1);transform:scale3d(1,1,1)}
+        }
         .wb_cts03 {background:#FFF; padding-bottom:150px}         
- 
+
+
 
     </style>
 
 
     <div class="p_re evtContent NSK" id="evtContainer">
+
+
         <div class="evtCtnsBox wb_top" >            
             <img src="https://static.willbes.net/public/images/promotion/2019/08/1347_top.jpg" alt="윌비스 제로백 수강후기 한줄평 이벤트" />            
         </div>
@@ -43,15 +56,15 @@
             <img src="https://static.willbes.net/public/images/promotion/2019/08/1347_01.jpg" alt="윌비스 제로백"/>
             <ul>
                 <li><img src="https://static.willbes.net/public/images/promotion/2019/08/1347_01_t01.gif" alt="임재진 국어"/></li>
-                <li><img src="https://static.willbes.net/public/images/promotion/2019/08/1347_01_t02.gif" alt="박초롱 영어"/></li>
-                <li><img src="https://static.willbes.net/public/images/promotion/2019/08/1347_01_t03.gif" alt="한경준 한국사"/></li>
-                <li><img src="https://static.willbes.net/public/images/promotion/2019/08/1347_01_t04.gif" alt="김헌 행정학"/></li>
+                <li><img src="https://static.willbes.net/public/images/promotion/2019/08/1347_01_t02.gif" alt="임재진 국어"/></li>
+                <li><img src="https://static.willbes.net/public/images/promotion/2019/08/1347_01_t03.gif" alt="임재진 국어"/></li>
+                <li><img src="https://static.willbes.net/public/images/promotion/2019/08/1347_01_t04.gif" alt="임재진 국어"/></li>
             </ul>
             <img src="https://static.willbes.net/public/images/promotion/2019/08/1347_01_1.jpg" alt="윌비스 제로백"/>
             <ul>               
-                <li><img src="https://static.willbes.net/public/images/promotion/2019/08/1347_01_t05.gif" alt="윤세훈 행정학"/></li>
-                <li><img src="https://static.willbes.net/public/images/promotion/2019/08/1347_01_t06.gif" alt="이석준 행정법"/></li>
-                <li><img src="https://static.willbes.net/public/images/promotion/2019/08/1347_01_t07.gif" alt="양승우 행정법"/></li>
+                <li><img src="https://static.willbes.net/public/images/promotion/2019/08/1347_01_t05.gif" alt="임재진 국어"/></li>
+                <li><img src="https://static.willbes.net/public/images/promotion/2019/08/1347_01_t06.gif" alt="임재진 국어"/></li>
+                <li><img src="https://static.willbes.net/public/images/promotion/2019/08/1347_01_t07.gif" alt="임재진 국어"/></li>
                 <li><img src="https://static.willbes.net/public/images/promotion/2019/08/1347_01_t.png" alt="coming soon"/></li>
             </ul>
             <a href="https://pass.willbes.net/promotion/index/cate/3092/code/1312" target="_blank">
@@ -60,7 +73,14 @@
         </div>
 
         <div class="evtCtnsBox wb_cts02" >            
-            <img src="https://static.willbes.net/public/images/promotion/2019/08/1347_02.jpg" alt="등불"> 
+            <img src="https://static.willbes.net/public/images/promotion/2019/08/1347_02x.jpg" alt="등불">   
+            <div class="comments">
+                <span class="img01"><img src="https://static.willbes.net/public/images/promotion/2019/08/1347_02_01.png" alt="후기1"></span>   
+                <span class="img02"><img src="https://static.willbes.net/public/images/promotion/2019/08/1347_02_02.png" alt="후기2"></span>          
+                <span class="img03"><img src="https://static.willbes.net/public/images/promotion/2019/08/1347_02_03.png" alt="후기3"></span>  
+                <span class="img04"><img src="https://static.willbes.net/public/images/promotion/2019/08/1347_02_04.png" alt="후기4"></span>  
+                <span class="img05"><img src="https://static.willbes.net/public/images/promotion/2019/08/1347_02_05.png" alt="후기5"></span>  
+            </div>    
         </div>
 
         <div class="evtCtnsBox wb_cts03" id="careful" >            

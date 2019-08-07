700b129ac29c34c8df75ae9e2d7653da49ae4205
최현탁 cht@willbes.com <cht@willbes.com>
2019-08-07 17:29:33 +0900

    프로모션 라이브 송출 로그인 기능 수정

--- views/willbes/pc/promotion/live_video_partial.blade.php
+++ views/willbes/pc/promotion/live_video_partial.blade.php
@@ -74,39 +74,35 @@
         {{--7/2 ~ 7.15 오전 9시 30분 방송 중--}}
         <script src="/public/vendor/jwplayer/jwplayer.js"></script>
         <div class="movieplayer">
-            {{--@if (sess_data('is_login') !== true)
-                <li class="liveVideoNoLogin">로그인이 필요합니다.</li>
-            @else--}}
-                <div class="embedWrap">
-                    @if ($ismobile == false)
-                        <!--PC-->
-                        @if (sess_data('is_login') !== true)
-                            <div onclick="javascript:alert('로그인 후 이용해 주세요.');" style="cursor: pointer;"><img src="https://static.willbes.net/public/images/promotion/live/liveIng.jpg" title=""></div>
-                        @else
-                            <div class="embed-container" id="myElement">
-                                <script type="text/javascript">jwplayer.key="kl6lOhGqjWCTpx6EmOgcEVnVykhoGWmf4CXllubWP5JwYq6K34m5XnpF0KGiCbQN";</script>
-                                <script type="text/javascript">
-                                    jwplayer("myElement").setup({
-                                        width: '100%',
-                                        logo: {file: 'https://static.willbes.net/public/images/promotion/live/liveBi.png'},
-                                        image: "https://static.willbes.net/public/images/promotion/live/liveIng.jpg",
-                                        aspectratio: "{{ $live_ratio }}",
-                                        autostart: "{{ $live_onoff_auto }}",
-                                        file: "{{ $live_path }}"
-                                    });
-                                </script>
-                            </div>
-                        @endif
+            <div class="embedWrap">
+                @if ($ismobile == false)
+                    <!--PC-->
+                    @if (sess_data('is_login') !== true)
+                        <div onclick="javascript:alert('로그인 후 이용해 주세요.');" style="cursor: pointer;"><img src="https://static.willbes.net/public/images/promotion/live/liveIng.jpg" title=""></div>
                     @else
-                        <!--모바일용-->
-                        <div class="embed-container-mobile" id="myElement"></div>
-                        <ul class="mobileCh">
-                            <li><a href="javascript:fn_live('hd')"><img src="https://static.willbes.net/public/images/promotion/live/livePlaybtnH.png" title="고화질 보기"></a></li>
-                            <li><a href="javascript:fn_live('low')"><img src="https://static.willbes.net/public/images/promotion/live/livePlaybtnN.png" title="일반화질 보기"></a></li>
-                        </ul>
+                        <div class="embed-container" id="myElement">
+                            <script type="text/javascript">jwplayer.key="kl6lOhGqjWCTpx6EmOgcEVnVykhoGWmf4CXllubWP5JwYq6K34m5XnpF0KGiCbQN";</script>
+                            <script type="text/javascript">
+                                jwplayer("myElement").setup({
+                                    width: '100%',
+                                    logo: {file: 'https://static.willbes.net/public/images/promotion/live/liveBi.png'},
+                                    image: "https://static.willbes.net/public/images/promotion/live/liveIng.jpg",
+                                    aspectratio: "{{ $live_ratio }}",
+                                    autostart: "{{ $live_onoff_auto }}",
+                                    file: "{{ $live_path }}"
+                                });
+                            </script>
+                        </div>
                     @endif
-                </div>
-            {{--@endif--}}
+                @else
+                    <!--모바일용-->
+                    <div class="embed-container-mobile" id="myElement"></div>
+                    <ul class="mobileCh">
+                        <li><a href="javascript:fn_live('hd')"><img src="https://static.willbes.net/public/images/promotion/live/livePlaybtnH.png" title="고화질 보기"></a></li>
+                        <li><a href="javascript:fn_live('low')"><img src="https://static.willbes.net/public/images/promotion/live/livePlaybtnN.png" title="일반화질 보기"></a></li>
+                    </ul>
+                @endif
+            </div>
         </div>
 
     @elseif ($live_type == 'on' && $live_video_type == 'off')

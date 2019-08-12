e540dd695a285ef115f0b538f1731d3cad103c34
김창용 <winmir@willbes.com>
2019-08-12 12:30:25 +0900

    모바일 수강신청

--- views/html/m/sample.blade.php
+++ views/html/m/sample.blade.php
@@ -61,7 +61,9 @@
             <br><br>
             <a href="{{ site_url('/home/html/m/basket_01') }}" target="_blank">장바구니</a>
             <a href="{{ site_url('/home/html/m/payment_01') }}" target="_blank">결제하기</a>
-            <a href="{{ site_url('/home/html/m/payment_02') }}" target="_blank">결제완료(작업중)</a>
+            <a href="{{ site_url('/home/html/m/payment_02') }}" target="_blank">결제완료</a>
+            <br><br>
+            <a href="{{ site_url('/home/html/m/lecture_online_free1') }}" target="_blank">무료강좌(작업중)</a>
         </div>
     </li>
     <li>

ec6aef3566bc505613cf19db6f4bca4c7df0c409
최형진 <letsboo@willbes.com>
2019-08-13 16:10:29 +0900

    직렬체크 다시 제거

--- views/willbes/pc/promotion/2002/1363.blade.php
+++ views/willbes/pc/promotion/2002/1363.blade.php
@@ -60,8 +60,10 @@
             <input type="hidden" name="target_param_names[]" value="참여캠퍼스"/> {{-- 체크 항목 전송 --}}
             <input type="hidden" name="target_param_names[]" value="직렬"/> {{-- 체크 항목 전송 --}}
             <input type="hidden" name="register_type" value="promotion"/>
+            <!--
             <input type="hidden" name="register_chk_col[]" value="EtcValue"/>
             <input type="hidden" name="register_chk_val[]" value=""/>
+            -->
 
             <div class="skyBanner">
                 <ul>
@@ -201,7 +203,7 @@
             }
 
             if (!confirm('저장하시겠습니까?')) { return true; }
-            $regi_form_register.find('input[name="register_chk_val[]"]').val($regi_form_register.find('input:radio[name="register_data2"]:checked').val());//신청자 조건 추가
+            // $regi_form_register.find('input[name="register_chk_val[]"]').val($regi_form_register.find('input:radio[name="register_data2"]:checked').val());//신청자 조건 추가
             ajaxSubmit($regi_form_register, _url, function(ret) {
                 if(ret.ret_cd) {
                     alert(ret.ret_msg);

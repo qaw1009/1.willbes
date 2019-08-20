65b881316759941152bfd19f245f57041cc45b8c
김창용 <winmir@willbes.com>
2019-08-20 17:20:01 +0900

    코딩 페이지

--- views/html/layouts/header.blade.php
+++ views/html/layouts/header.blade.php
@@ -5,7 +5,8 @@
     <!-- Meta, title, CSS, favicons, etc. -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--meta name="viewport" content="width=device-width, initial-scale=1"-->
+    <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, width=device-width">
 
     <title>윌비스 통합 사이트</title>
 

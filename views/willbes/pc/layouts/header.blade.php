e51df8f9ce550b4e38654b340e7a0ae4893cc0cd
김창용 <winmir@willbes.com>
2019-08-20 17:34:38 +0900

    뷰포트

--- views/willbes/pc/layouts/header.blade.php
+++ views/willbes/pc/layouts/header.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!--meta name="viewport" content="width=device-width, initial-scale=1.0"-->
-    <meta name="viewport" content="initial-scale=1.0, minimum-scale=1.0, width=device-width">
+    <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, width=device-width">
     <title>{{ $__cfg['HeadTitle'] }}</title>
     <meta name="keywords" content="{{ $__cfg['MetaKeyword'] }}">
     <meta name="description" content="{{ $__cfg['MetaDesc'] }}">

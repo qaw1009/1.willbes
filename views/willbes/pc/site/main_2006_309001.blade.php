781722e70b71628dd4cf9fe74dcfff839298d098
최형진 <letsboo@willbes.com>
2019-08-29 11:14:57 +0900

    스포츠 지도사 -메인 우측퀵 배너 추가

--- views/willbes/pc/site/main_2006_309001.blade.php
+++ views/willbes/pc/site/main_2006_309001.blade.php
@@ -74,6 +74,11 @@
             </div>
         </div>
         <!-- CS센터 //-->
+
+        <div id="QuickMenu" class="MainQuickMenu">
+            {{-- quick menu --}}
+            @include('willbes.pc.site.main_partial.quick_menu_' . $__cfg['SiteCode'])
+        </div>
     </div>
     <!-- End Container -->
 

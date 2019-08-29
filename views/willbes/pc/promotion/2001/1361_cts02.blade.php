a78da8a5fc0f180a40a108e7c547e37a59a09614
김창용 <winmir@willbes.com>
2019-08-29 11:06:09 +0900

    경찰 합격 예측 풀서비스

---
+++ views/willbes/pc/promotion/2001/1361_cts02.blade.php
@@ -0,0 +1,32 @@
+    <style type="text/css">  
+        .cts02_top {background:#182b43 url(https://static.willbes.net/public/images/promotion/2019/08/1361_cts02_top_bg.jpg) repeat-x left top;}
+        .cts02_01 {background:#fff; padding:100px 0} 
+        .cts02_02 {background:#eaeaea}
+        .cts02_03 {background:#fff} 
+    </style>
+    <div>
+        <div class="evtCtnsBox cts02_top">             
+            <img src="https://static.willbes.net/public/images/promotion/2019/08/1361_cts02_top.jpg" alt="합격예측 풀서비스">
+        </div>
+
+        <div class="evtCtnsBox cts02_01">
+            댓글영역
+            <div class="mt100">
+                <a href="#none"><img src="https://static.willbes.net/public/images/promotion/2019/08/1361_cts02_btn01.png" alt="합격예측 풀서비스 참여하기"></a>
+            </div>
+        </div>
+
+        <div class="evtCtnsBox cts02_02" id="cts02_02">
+            <img src="https://static.willbes.net/public/images/promotion/2019/08/1361_cts02_02.jpg" alt="라이브 토크쇼" usemap="#Map1361_cts02_02" border="0">
+            <map name="Map1361_cts02_02" id="Map1361_cts02_02">
+                <area shape="rect" coords="355,1910,763,2016" href="https://police.willbes.net/promotion/index/cate/3001/code/1376" target="_blank" alt="라이브 토크쇼 신청하기" />
+            </map>
+        </div>
+
+        <div class="evtCtnsBox cts02_03">
+            <img src="https://static.willbes.net/public/images/promotion/2019/08/1361_cts02_03.jpg" alt="신광은 경찰팀" usemap="#Map1361_cts02_03" border="0">
+            <map name="Map1361_cts02_03" id="Map1361_cts02_03">
+                <area shape="rect" coords="350,1177,767,1291" href="https://police.willbes.net/promotion/index/cate/3002/code/1362" target="_blank" alt="적중이벤트 바로가기" />
+            </map>
+        </div>
+	</div>

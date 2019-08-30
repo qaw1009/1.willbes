8b73b7b6adba58ecadca7415557115a899e09703
최현탁 cht@willbes.com <cht@willbes.com>
2019-08-30 10:34:06 +0900

    합격예측 수정

--- views/willbes/pc/promotion/2001/1352_top.blade.php
+++ views/willbes/pc/promotion/2001/1352_top.blade.php
@@ -128,12 +128,12 @@
 
     /*서비스이용현황 */
     $(document).ready(function() {
-        if('{{ENVIRONMENT}}' !== 'local' && '{{ENVIRONMENT}}' !== 'development') {
+        /*if('{{ENVIRONMENT}}' !== 'local' && '{{ENVIRONMENT}}' !== 'development') {
             @if(date('YmdHi') >= '201905011600')
             alert('서비스가 종료되었습니다.');
             location.href = '{{ site_url('/home/index/cate/3001') }}';
             @endif
-        }
+        }*/
 
         // 상단 합격안정권 예측 데이터 조회
         areaAvrAjax('{{ $arr_promotion_params['PredictIdx'] }}');

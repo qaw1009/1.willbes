1fe0e836707761fb1f8dbfa34d02740d0b80ffbf
신봉석 <bsshin@willbes.com>
2019-09-18 10:40:48 +0900

    프런트 > 모바일 리다이렉트 수정 (학원사이트 추가)

--- application/third_party/app/controllers/FrontController.php
+++ application/third_party/app/controllers/FrontController.php
@@ -462,7 +462,11 @@ abstract class FrontController extends BaseController
                 } else { // 파람이 없으면
                     $viewPC = $this->session->userdata('viewPC');
                     if ($viewPC != 1) { // 세션이 있으면
-                        redirect(front_url('/m/home/index'));
+                        if ($this->_is_pass_site === true) {
+                            redirect(site_url('/m/pass/home/index'));
+                        } else {
+                            redirect(site_url('/m/home/index'));
+                        }
                     }
                 }
             }

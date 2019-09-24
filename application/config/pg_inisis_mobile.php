c71e0bb0ea00186fda2c406550ed53c05f9b74dd
신봉석 <bsshin@willbes.com>
2019-09-24 16:54:57 +0900

    모바일 > 이니시스 모바일 설정 원복2

--- application/config/pg_inisis_mobile.php
+++ application/config/pg_inisis_mobile.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config = [
-    'mode' => 'real',
+    'mode' => 'test',
     'test' => [
         'mid' => 'INIpayTest',
         'type' => 'web',

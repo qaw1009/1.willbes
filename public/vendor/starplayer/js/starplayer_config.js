edaa36e916b68cf74d1b97d3e1ccb2d5c57e98aa
하준걸 <hereisme@willbes.net>
2019-09-25 10:40:18 +0900

    스타플레이어 버전 업데이트

--- public/vendor/starplayer/js/starplayer_config.js
+++ public/vendor/starplayer/js/starplayer_config.js
@@ -1,51 +1,22 @@
 //x86
-var STARPLAYER_URL = "https://static.willbes.net/public/uploads/starplayer/starplayer-1.5.17.45.cab";
-var STARPLAYER_SETUP_URL = "https://static.willbes.net/public/uploads/starplayer/starplayer-1.5.17.45.exe";
-var STARPLAYER_VERSION = "1,5,17,45";
+var STARPLAYER_URL = "https://cabstarplayer.myskcdn.com/starplayer/starplayer-1.5.21.52.cab";
+var STARPLAYER_SETUP_URL = "https://cabstarplayer.myskcdn.com/starplayer/starplayer-1.5.21.52.exe";
+var STARPLAYER_VERSION = "1,5,21,52";
 
 //x64
-var STARPLAYER64_URL = "https://static.willbes.net/public/uploads/starplayer/starplayer64-1.5.16.45.cab";
-var STARPLAYER64_SETUP_URL = "https://static.willbes.net/public/uploads/starplayer/starplayer64-1.5.16.45.exe";
-var STARPLAYER64_VERSION = "1,5,16,45";
+var STARPLAYER64_URL = "https://cabstarplayer.myskcdn.com/starplayer/starplayer64-1.5.21.52.cab";
+var STARPLAYER64_SETUP_URL = "https://cabstarplayer.myskcdn.com/starplayer/starplayer64-1.5.21.52.exe";
+var STARPLAYER64_VERSION = "1,5,21,52";
 
 //Agent
-var STARPLAYER_AGENT_SETUP_URL = "https://static.willbes.net/public/uploads/starplayer/starplayer_agent_1.1.3.41.exe";
-var STARPLAYER_AGENT_VERSION = "1,1,3,41";
+var STARPLAYER_AGENT_SETUP_URL = "https://cabstarplayer.myskcdn.com/starplayer/starplayer_agent_1.1.5.49.exe";
+var STARPLAYER_AGENT_VERSION = "1,1,5,49";
 
 //OSX
-var STARPLAYER_OSX_SETUP_URL = "https://static.willbes.net/public/uploads/starplayer/StarPlayerAgent_1.0.0.8.pkg";
-var STARPLAYER_OSX_VERSION = "1,0,0,8";
+var STARPLAYER_OSX_SETUP_URL = "https://cabstarplayer.myskcdn.com/starplayer/StarPlayerAgent_1.0.2.9.pkg";
+var STARPLAYER_OSX_VERSION = "1,0,2,9";
 
 //CONFIG
 var STARPLAYER_CONFIG_URL = "https://static.willbes.net/public/uploads/starplayer/starplayer.txt";
-
 var STARPLAYER_UPDATED_DATE = "2016.02.25";
-
 var STARPLAYER_AGENT_UPDATED_DATE = "2016.09.09";
-
-/*
-//x86
-var STARPLAYER_URL = "http://cab.starplayer.skcdn.com/starplayer/starplayer-1.5.17.45.cab";
-var STARPLAYER_SETUP_URL = "http://cab.starplayer.skcdn.com/starplayer/starplayer-1.5.17.45.exe";
-var STARPLAYER_VERSION = "1,5,17,45";
-
-//x64
-var STARPLAYER64_URL = "http://cab.starplayer.skcdn.com/starplayer/starplayer64-1.5.16.45.cab";
-var STARPLAYER64_SETUP_URL = "http://cab.starplayer.skcdn.com/starplayer/starplayer64-1.5.16.45.exe";
-var STARPLAYER64_VERSION = "1,5,16,45";
-
-//Agent
-var STARPLAYER_AGENT_SETUP_URL = "http://cab.starplayer.skcdn.com/starplayer/starplayer_agent_1.1.3.41.exe";
-var STARPLAYER_AGENT_VERSION = "1,1,3,41";
-
-//OSX
-var STARPLAYER_OSX_SETUP_URL = "http://cab.starplayer.skcdn.com/starplayer/StarPlayerAgent_1.0.0.8.pkg";
-var STARPLAYER_OSX_VERSION = "1,0,0,8";
-
-//CONFIG
-var STARPLAYER_CONFIG_URL = "http://mgt.hd.willbes.gscdn.com/config/starplayer.txt";
-
-var STARPLAYER_UPDATED_DATE = "2016.02.25";
-
-var STARPLAYER_AGENT_UPDATED_DATE = "2016.09.09";
-*/

6090f4a3fc340246987c0fe0595237084d035740
이윤구 <yoongukk@willbes.com>
2019-09-26 09:37:35 +0900

    경찰 장정훈 적중

--- views/willbes/pc/promotion/2001/1404.blade.php
+++ views/willbes/pc/promotion/2001/1404.blade.php
@@ -22,17 +22,34 @@
         .evtCtnsBox {width:100%; text-align:center; min-width:1120px}
 
         /************************************************************/
-
+       
         .skybanner {
             position:fixed;
             top:250px;
-            right:10px;
+            right:0;
             z-index:1;
         }
+        .skybanner2{
+            position:fixed;
+            top:400px;
+            right:0;
+            z-index:1;
+        }
+
+        .wb_cts03 {background:#fff; width:1200px; margin:0 auto; padding-bottom:100px}
+        .wb_cts03 ul {margin-bottom:30px}
+        .wb_cts03 li {display:inline; float:left; width:50%}
+        .wb_cts03 li a {display:block; text-align:center; font-size:24px; padding:24px 0; font-weight:bold; color:#888787; background:#f4f4f4; border:1px solid #e5e5e5; border-bottom:1px solid #fff; margin-right:10px}
+        .wb_cts03 li a:hover,
+        .wb_cts03 li a.active {color:#de3b1f; background:#fff; padding:20px 0; border:5px solid #de3b1f; border-bottom:5px solid #fff}
+        .wb_cts03 ul:after {content:""; display:block; clear:both}
+
+        /*
         .skybanner ul {border:1px solid #000; border-bottom:none}
         .skybanner a {height:40px; line-height:40px; display:block; text-align:center; background:#009ef5; color:#fff; font-size:14px !important; font-weight:600 !important; border-bottom:1px solid #000}
         .skybanner a:hover {background:#fff; color:#000}
         .skybanner li:last-child a {background:#000; color:#fff;}
+        */
 
         .wb_cts01 {background:url(https://static.willbes.net/public/images/promotion/2019/05/1024_top_bg.jpg) no-repeat center top; position:relative}
         .wb_cts01 span {position:absolute; width:520px; top:724px; left:50%; margin-left:-469px; background:#000; z-index:10}
@@ -53,6 +70,7 @@
     </style>
 
     <div class="evtContent NSK" id="evtContainer">
+        {{--
         <div class="skybanner">
             <img src="https://static.willbes.net/public/images/promotion/2019/05/1024_skybanner.png" alt="장정훈 경찰학" />
             <ul>
@@ -64,23 +82,37 @@
                 <li><a href="#evtContainer">TOP ▲</a></li>
             </ul>
         </div>
+        --}}
+        <div class="skybanner" >
+            <a href="https://police.willbes.net/support/notice/show?board_idx=238714&" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2019/09/1404_sb1.png" alt="경찰학 베너"></a>            
+        </div>
+        <div class="skybanner2" >
+            <a href="https://police.willbes.net/support/notice/show/cate/3001?board_idx=239117&" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2019/09/1404_sb2.png" alt="행정학 베너" ></a>
+        </div>
 
+
+      
         <div class="evtCtnsBox wb_cts01" id="wb_cts01">
             <img src="https://static.willbes.net/public/images/promotion/2019/05/1024_top.jpg" alt="장정훈 경찰학" />
             <span><iframe src="https://www.youtube.com/embed/xw_BTo5soJI" frameborder="0" allowfullscreen></iframe></span>
         </div>       
 
         <div class="evtCtnsBox wb_cts02" id="wb_cts02">
-            <img src="https://static.willbes.net/public/images/promotion/2019/05/1024_01.jpg" alt="수강후기" />
+            <img src="https://static.willbes.net/public/images/promotion/2019/05/1024_01.jpg" alt="수강후기" />            
         </div>
 
         <div class="evtCtnsBox wb_cts03" id="wb_cts03">
             <img src="https://static.willbes.net/public/images/promotion/2019/09/1404_02.jpg" usemap="#Map1404a" border="0" />
             <map name="Map1404a" id="Map1404a">
-                <area shape="rect" coords="360,433,758,521" href="https://police.willbes.net/support/notice/show/cate/3001?board_idx=238714&" target="_blank" />
-            </map>        
-            <img src="https://static.willbes.net/public/images/promotion/2019/09/1404_05.jpg"/>
-  
+                <area shape="rect" coords="155,356,564,450" href="https://police.willbes.net/support/notice/show?board_idx=238714&" target="_blank" />
+                <area shape="rect" coords="576,355,988,452" href="https://police.willbes.net/support/notice/show/cate/3001?board_idx=239117&" target="_blank" />
+            </map>
+            <ul class="tabs">   
+                <li><a href="#tab01">경찰학개론</a></li>
+                <li><a href="#tab02">행정법</a></li>
+            </ul>
+            <img src="https://static.willbes.net/public/images/promotion/2019/09/1404_05.jpg" title="경찰학개론" id="tab01"/>
+            <img src="https://static.willbes.net/public/images/promotion/2019/09/1404_06.jpg" title="행정법" id="tab02"/> 
         </div><!--wb_cts03//-->
 
         <div class="evtCtnsBox wb_cts04" id="wb_cts04">
@@ -124,10 +156,30 @@
 
 
     <script>
-        $(function(e) {
-            var targetOffset = $("#evtContainer").offset().top;
-            $('html, body').animate({scrollTop: targetOffset}, 700);
-            /*e.preventDefault(); */
-        });
+        /*tab*/
+        $(document).ready(function(){
+            $('.tabs').each(function(){
+                var $active, $content, $links = $(this).find('a');
+                $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+                $active.addClass('active');
+
+                $content = $($active[0].hash);
+
+                $links.not($active).each(function () {
+                    $(this.hash).hide()});
+
+                // Bind the click event handler
+                $(this).on('click', 'a', function(e){
+                    $active.removeClass('active');
+                    $content.hide();
+
+                    $active = $(this);
+                    $content = $(this.hash);
+
+                    $active.addClass('active');
+                    $content.show();
+
+                    e.preventDefault()})})}
+        ) 
     </script>
 @stop

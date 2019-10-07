615dd1801c7a9203c8a8d07bf746507ba3100fd2
김창용 <winmir@willbes.com>
2019-10-07 15:22:17 +0900

    경찰 통합관리 및 4개월 패스

--- views/willbes/pc/promotion/2002/1053.blade.php
+++ views/willbes/pc/promotion/2002/1053.blade.php
@@ -23,7 +23,7 @@
 
         .skyBanner {position:fixed; bottom:250px;right:0;z-index:10;}
 
-        .wb_top {background:#222 url(https://static.willbes.net/public/images/promotion/2019/08/1053_top_bg.png) no-repeat center top}
+        .wb_top {background:#222 url(https://static.willbes.net/public/images/promotion/2019/10/1053_top_bg.jpg) no-repeat center top}
         .wb_cts01 {background:#191919;}
         .btnLec {padding-top:100px; position:relative;}
         .btnLec span {position:absolute; top:70px; left:50%; margin-left:-360px; z-index:10; animation:ani 1s infinite;}
@@ -53,35 +53,24 @@
             <img src="https://static.willbes.net/public/images/promotion/2019/08/1053_skybanner.png" title="경찰통합생활관리반">
         </div>
         <div class="evtCtnsBox wb_top" >
-            <img src="https://static.willbes.net/public/images/promotion/2019/08/1053_top.png"alt="경찰통합생활관리반">
+            <img src="https://static.willbes.net/public/images/promotion/2019/10/1053_top.jpg"alt="경찰통합생활관리반">
         </div>
 
         <div class="evtCtnsBox wb_cts01" >
-            <div class="pt100">
-                {{--<img src="https://static.willbes.net/public/images/promotion/2019/08/1053_190828_01.jpg"alt="통합생활관리반 기본이론 과정"/>--}}
-            </div>
-            <img src="https://static.willbes.net/public/images/promotion/2019/08/1053_01_1A.png"alt="최적의학습환경"/><br>  
-            <img src="https://static.willbes.net/public/images/promotion/2019/08/1053_01_1C.png"alt="최적의학습환경"/><br>           
-            <img src="https://static.willbes.net/public/images/promotion/2019/08/1053_01_1B.png"alt="최적의학습환경"/>
-            <div class="btnLec">
-                <img src="https://static.willbes.net/public/images/promotion/2019/09/1053_02.jpg"alt="통합생활관리반 신청하기" usemap="#Map1053A" border="0"/>
-                <map name="Map1053A" id="Map1053A">
-                    <area shape="rect" coords="357,419,624,529" href="https://police.willbes.net/pass/offLecture/index?cate_code=3010&amp;course_idx=1093&amp;subject_idx=1473" target="_blank" alt="신청하기" />
-                </map>
-                {{--
-                <span>
-                    <img src="https://static.willbes.net/public/images/promotion/2019/08/1053_190828_02_popup.png"alt="이벤트 ~9.2 마감"/>   
-                <span>
-                --}}
-            </div>            
+            <img src="https://static.willbes.net/public/images/promotion/2019/10/1053_01.jpg"alt="월 87만원"/><br>
+            <img src="https://static.willbes.net/public/images/promotion/2019/10/1053_02.jpg"alt="최적의학습환경"/><br> 
+            <img src="https://static.willbes.net/public/images/promotion/2019/10/1053_03.jpg"alt="통합생활관리반 신청하기" usemap="#Map1053A" border="0"/>
+            <map name="Map1053A" id="Map1053A">
+                <area shape="rect" coords="83,936,1039,1060" href="https://police.willbes.net/pass/offLecture/index?cate_code=3010&amp;course_idx=1093&amp;subject_idx=1473" target="_blank" alt="신청하기" />
+            </map>           
         </div>
 
         <div class="evtCtnsBox wb_cts01" >
-            <img src="https://static.willbes.net/public/images/promotion/2019/08/1053_01_2.png"alt="막강한할인혜택3가지" usemap="#Map_lec_go" border="0"/>
+            <img src="https://static.willbes.net/public/images/promotion/2019/10/1053_04.jpg"alt="막강한할인혜택3가지" usemap="#Map_lec_go" border="0"/>
             <map name="Map_lec_go">
-                <area shape="rect" coords="172,583,951,678" href="https://police.willbes.net/pass/promotion/index/code/1051" target="_blank" alt="관리반보러가기">
-                <area shape="rect" coords="177,1105,956,1200" href="https://police.willbes.net/pass/promotion/index/code/1128" target="_blank" alt="체력학원보러가기">
-            </map>            
+                <area shape="rect" coords="173,466,952,561" href="https://police.willbes.net/pass/promotion/index/code/1051" target="_blank" alt="관리반보러가기">
+                <area shape="rect" coords="181,984,960,1079" href="https://police.willbes.net/pass/promotion/index/code/1128" target="_blank" alt="체력학원보러가기">
+            </map>              
         </div>
 
         <div class="evtCtnsBox wb_cts02" >
@@ -94,6 +83,7 @@
                     <li><img src="https://static.willbes.net/public/images/promotion/2019/08/1053_02_s4.png" alt="6"/></li>
                     <li><img src="https://static.willbes.net/public/images/promotion/2019/08/1053_02_s5.png" alt="7"/></li>
                     <li><img src="https://static.willbes.net/public/images/promotion/2019/08/1053_02_s6.png" alt="8"/></li>
+                    <li><img src="https://static.willbes.net/public/images/promotion/2019/10/1053_02_s7.png" alt="8"/></li>
                 </ul>
                 <p class="left_arr"><a id="imgBannerLeft"><img src="https://static.willbes.net/public/images/promotion/2019/08/1053_arr_l.png"></a></p>
                 <p class="right_arr"><a id="imgBannerRight"><img src="https://static.willbes.net/public/images/promotion/2019/08/1053_arr_r.png"></a></p>

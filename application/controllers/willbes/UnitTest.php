e213f57b9b3f90e8e3b7976e42aeffb4aeeefb21
신봉석 <bsshin@willbes.com>
2019-10-08 09:28:34 +0900

    프런트 > 모바일 테스트 소스 제거

--- application/controllers/willbes/UnitTest.php
+++ application/controllers/willbes/UnitTest.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class UnitTest extends \app\controllers\FrontController
 {
-    protected $models = array('order/orderF');
+    protected $models = array();
     protected $helpers = array();
     protected $auth_controller = false;
     protected $auth_methods = array();
@@ -13,80 +13,8 @@ class UnitTest extends \app\controllers\FrontController
         parent::__construct();
     }
 
-    /**
-     * PG사 모바일 결제 테스트 [https://www.local.willbes.net/unitTest/requestMobile/card]
-     * @param array $params
-     */
-    public function requestMobile($params = [])
+    public function index()
     {
-        $this->load->driver('pg', ['driver' => 'inisis_mobile'], 'pg_mobile');
-
-        // 결제수단
-        $pay_method = element('0', $params, 'vbank');
-        $arr_pay_method_ccd = ['card' => '604001', 'bank' => '604002', 'vbank' => '604003'];
-        $pay_method_ccd = array_get($arr_pay_method_ccd, $pay_method, '604003');
-
-        // PG사 결제요청 폼 생성
-        $order_no = $this->orderFModel->makeOrderNo();
-        $data = [
-            'mid' => 'INIpayTest',
-            'order_no' => $order_no,
-            'repr_prod_name' => '테스트상품 (' . $order_no . ')',
-            'req_pay_price' => '1000',
-            'order_name' => '홍길동',
-            'order_phone' => '',
-            'order_mail' => 'bsshin@willbes.com',
-            'pay_method_ccd' => $pay_method_ccd,
-            'is_escrow' => 'N',
-            'return_prefix_url' => front_url('/unitTest/'),
-            'return_data' => ''
-        ];
-
-        $form = $this->pg_mobile->requestForm($data);
-
-        $this->output->_display($form);
-    }
-
-    /**
-     * PG사 모바일 결제 테스트
-     * @param array $params
-     */
-    public function returnsMobile($params = [])
-    {
-        $this->load->driver('pg', ['driver' => 'inisis_mobile'], 'pg_mobile');
-
-        $result = $this->pg_mobile->returnResult();
-
-        var_dump($result);
-    }
-
-    /**
-     * PG사 모바일 결제 테스트
-     * @param array $params
-     * @return mixed
-     */
-    public function notiMobile($params = [])
-    {
-        $this->load->driver('pg', ['driver' => 'inisis_mobile'], 'pg_mobile');
-
-        $result = $this->pg_mobile->depositResult();
-        var_dump($result);
-
-        if ($result['result'] === true && empty($deposit_results['next_method']) === false) {
-            return $this->pg_mobile->depositReturn(true);
-        }
-
-        return null;
-    }
-
-    /**
-     * PG사 모바일 결제 테스트
-     * @param array $params
-     */
-    public function nothingMobile($params = [])
-    {
-        $result = array_merge($this->input->get(null, false), $this->input->post(null, false));
-
-        var_dump($result);
+        var_dump('unit test controller method');
     }
 }

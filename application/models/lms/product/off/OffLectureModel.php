b52fe75b8e4af478aef69c90fc4bc6fc7e6b7c1a
조규호 <benoo@willbes.com>
2019-10-15 15:16:07 +0900

    단과반 교재 접목

--- application/models/lms/product/off/OffLectureModel.php
+++ application/models/lms/product/off/OffLectureModel.php
@@ -172,6 +172,12 @@ class OffLectureModel extends CommonLectureModel
             }
             /*----------------          SMS등록        ---------------*/
 
+            /*----------------          교재등록        ---------------*/
+            if($this->_setSubProduct($input,$prodcode,'ProdCode_book','636003','교재') !== true) {
+                throw new \Exception('교재 등록에 실패했습니다.');
+            }
+            /*----------------          교재등록        ---------------*/
+
             /*----------------          자동지급단강좌 등록        ---------------*/
             if($this->_setSubProduct($input,$prodcode,'ProdCode_lecture','636001','자동지급단강좌') !== true) {
                 throw new \Exception('자동지급단강좌 등록에 실패했습니다.');
@@ -296,6 +302,12 @@ class OffLectureModel extends CommonLectureModel
                 throw new \Exception('SMS 등록에 실패했습니다.');
             }
             /*----------------          SMS등록        ---------------*/
+
+            /*----------------          교재등록        ---------------*/
+            if($this->_setSubProduct($input,$prodcode,'ProdCode_book','636003','교재') !== true) {
+                throw new \Exception('교재 등록에 실패했습니다.');
+            }
+            /*----------------          교재등록        ---------------*/
 
             /*----------------          자동지급단강좌 등록        ---------------*/
             if($this->_setSubProduct($input,$prodcode,'ProdCode_lecture','636001','자동지급단강좌') !== true) {

92aee4e0d13a7735d0996d01d5f8c444949ae44a
김창용 <winmir@willbes.com>
2019-10-15 11:09:19 +0900

    공무원 제로백

--- views/willbes/pc/promotion/2003/1286.blade.php
+++ views/willbes/pc/promotion/2003/1286.blade.php
@@ -43,7 +43,7 @@
         }        
         .zero100_03 {background:#fbfa00}
         .zero100_04 {}
-        .zero100_05 {}
+        .zero100_05 {padding-top:100px}
         .zero100_06 {background:#e9eaee}
 
         .m-margin {margin-top:-100px}
@@ -79,21 +79,25 @@
             </map>
         </div>
 
+        {{--
         <div class="evtCtnsBox zero100_04">
-            <img src="https://static.willbes.net/public/images/promotion/2019/06/zero100_04.jpg" alt="제로100 소문내기"/>
-   
+            <img src="https://static.willbes.net/public/images/promotion/2019/06/zero100_04.jpg" alt="제로100 소문내기"/>   
         </div>
-        {{--홍보url--}}
+        --}}
+
+        {{--홍보url
         @if( empty($data['data_option_ccd']) === false && array_key_exists($arr_base['option_ccd']['comment_list'], $data['data_option_ccd']) === true && array_key_exists($arr_base['comment_use_area']['event'], $data['data_comment_use_area']) === true)
             @include('willbes.pc.promotion.show_comment_list_url_partial')
         @endif
+        --}}
 
-        <div class="m-margin">
-        {{--기본댓글--}}
+        {{--기본댓글
+        <div class="m-margin">        
         @if( empty($data['data_option_ccd']) === false && array_key_exists($arr_base['option_ccd']['comment_list'], $data['data_option_ccd']) === true && array_key_exists($arr_base['comment_use_area']['event'], $data['data_comment_use_area']) === true)
             @include('willbes.pc.promotion.show_comment_list_normal_partial')
         @endif
         </div>
+        --}}
 
         <div class="evtCtnsBox zero100_05">
             <img src="https://static.willbes.net/public/images/promotion/2019/06/zero100_05.jpg" alt="이벤트 유의사항"  />    

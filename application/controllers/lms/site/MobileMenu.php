96831fbdabf9d69bb3212342e310f0a1709a65a8
신봉석 <bsshin@willbes.com>
2019-10-28 11:08:50 +0900

    lms > 사이트 메뉴 > 모바일 메뉴 관리 추가

--- application/controllers/lms/site/MobileMenu.php
+++ application/controllers/lms/site/MobileMenu.php
@@ -3,10 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 require_once APPPATH . 'controllers/lms/site/menu/BaseSiteMenu.php';
 
-class SiteMenu extends BaseSiteMenu
+class MobileMenu extends BaseSiteMenu
 {
     public function __construct()
     {
-        parent::__construct('site', '사이트');
+        parent::__construct('mobile', '모바일');
     }
 }

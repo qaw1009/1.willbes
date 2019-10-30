3f47298e7e7ddb5d544757ec1906345cfa79fbd1
이윤구 <yoongukk@willbes.com>
2019-10-30 14:00:41 +0900

    오태진 한국사 근현대사 학원강의 페이지 및 팝업 요청

--- views/willbes/pc/promotion/2002/1435.blade.php
+++ views/willbes/pc/promotion/2002/1435.blade.php
@@ -26,6 +26,21 @@
         .evt03 {background:#ececec;}
 
         .evt04 {background:#fff;}
+
+        .evt02s{background:#333;padding-bottom:75px;}
+
+        /* 슬라이드배너 */
+        .slide_con {position:relative; width:980px; margin:0 auto;padding-left:100px;}
+        .slide_con p {position:absolute; top:50%; margin-top:-30px; width:67px; height:67px; z-index:10}
+        .slide_con p a {cursor:pointer}
+        .slide_con p.leftBtn {left:0}
+        .slide_con p.rightBtn {right:0}
+    
+        .evt01 {padding-bottom:100px}
+        .evt01 .slide_con {position:relative; width:950px; margin:0 auto}
+        .evt01 .slide_con p {position:absolute; top:50%; margin-top:-22px; width:44px; height:45px; z-index:10}
+        .evt01 .slide_con p.leftBtn {left:-80px}
+        .evt01 .slide_con p.rightBtn {right:-80px}         
     </style>
     <div class="p_re evtContent NGR" id="evtContainer">
 
@@ -35,19 +50,62 @@
         <div class="evtCtnsBox evt02" id="evt02">
             <img src="https://static.willbes.net/public/images/promotion/2019/10/1435_top.jpg" title="오태진 근현대사 이번에 끝내기">
         </div>    
+        <div class="evtCtnsBox evt02s" id="evt02s">
+            <img src="https://static.willbes.net/public/images/promotion/2019/10/1435_slide.jpg" title="후기">
+            <div class="slide_con">
+                <ul id="slidesImg4">
+                    <li><img src="https://static.willbes.net/public/images/promotion/2019/10/1435_slide1.png" /></li>
+                    <li><img src="https://static.willbes.net/public/images/promotion/2019/10/1435_slide2.png" /></li>
+                    <li><img src="https://static.willbes.net/public/images/promotion/2019/10/1435_slide3.png" /></li>   
+                    <li><img src="https://static.willbes.net/public/images/promotion/2019/10/1435_slide4.png" /></li>
+                    <li><img src="https://static.willbes.net/public/images/promotion/2019/10/1435_slide5.png" /></li>
+                    <li><img src="https://static.willbes.net/public/images/promotion/2019/10/1435_slide6.png" /></li>        
+                    <li><img src="https://static.willbes.net/public/images/promotion/2019/10/1435_slide7.png" /></li>                         
+                </ul>
+                <p class="leftBtn"><a id="imgBannerLeft4"><img src="https://static.willbes.net/public/images/promotion/2019/09/1009_01_arrowL.png"></a></p>
+                <p class="rightBtn"><a id="imgBannerRight4"><img src="https://static.willbes.net/public/images/promotion/2019/09//1009_01_arrowR.png"></a></p>
+            </div>
+        </div>   
         <div class="evtCtnsBox evt03" id="evt03">
             <img src="https://static.willbes.net/public/images/promotion/2019/10/1435_01.jpg" title="100점에 도전">
         </div>      
         <div class="evtCtnsBox evt04" id="evt04">
-            <img src="https://static.willbes.net/public/images/promotion/2019/10/1435_02.jpg" usemap="#Map1435a" title="수강 신청하기" border="0">
+            <img src="https://static.willbes.net/public/images/promotion/2019/10/1435_02.jpg" usemap="#Map1435a" border="0" />
             <map name="Map1435a" id="Map1435a">
-                <area shape="rect" coords="676,992,917,1066" href="https://police.willbes.net/pass/offLecture/index/type/all?cate_code=3010&campus_ccd=605001&course_idx=1041&subject_idx=1055#none" target="_blank" />
-            </map>
+                <area shape="rect" coords="875,1018,984,1057" href="https://police.willbes.net/lecture/show/cate/3001/pattern/only/prod-code/157521" target="_blank" />
+                <area shape="rect" coords="875,1068,986,1106" href="https://police.willbes.net/lecture/show/cate/3001/pattern/only/prod-code/157519" target="_blank" />
+                <area shape="rect" coords="873,1115,987,1156" href="https://police.willbes.net/lecture/show/cate/3001/pattern/only/prod-code/156657" target="_blank"/>
+            </map>        
         </div> 
     </div>
     <!-- End Container -->
 
-    <script type="text/javascript">
-      
+    <script type="text/javascript">      
+
+        $(document).ready(function() {
+            var slidesImg4 = $("#slidesImg4").bxSlider({
+                mode:'horizontal', //option : 'horizontal', 'vertical', 'fade'
+                auto:true,
+                speed:350,
+                pause:4000,
+                pager:true,
+                controls:false,
+                minSlides:1,
+                maxSlides:1,
+                slideMargin:0,
+                autoHover: true,
+                moveSlides:1,
+                pager:false,
+            });
+
+            $("#imgBannerLeft4").click(function (){
+                slidesImg4.goToPrevSlide();
+            });
+
+            $("#imgBannerRight4").click(function (){
+                slidesImg4.goToNextSlide();
+            });
+        });
+       
     </script>
 @stop

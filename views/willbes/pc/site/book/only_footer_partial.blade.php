eb391e7b85c93d144f14562384f2a77d6790b633
조규호 <benoo@willbes.com>
2019-10-31 15:49:12 +0900

    교재 구매버튼 수정건

--- views/willbes/pc/site/book/only_footer_partial.blade.php
+++ views/willbes/pc/site/book/only_footer_partial.blade.php
@@ -1,58 +1,47 @@
-<div id="buy_layer" class="willbes-Lec-buyBtn-sm NG">
-    <div>
-        <button type="button" name="btn_cart" data-direct-pay="N" data-is-redirect="N" class="bg-deep-gray">
-            <span>장바구니</span>
-        </button>
-    </div>
-    <div>
-        <button type="button" name="btn_direct_pay" data-direct-pay="Y" data-is-redirect="Y" class="bg-dark-blue">
-            <span>바로결제</span>
-        </button>
-    </div>
-    <div id="pocketBox" class="pocketBox" style="display: none;">
+<div id="buy_continue_layer" class="willbes-Lec-buyBtn-sm NG common_buy_layer">
+    <div class="pocketBox" style="display:block">
+        <a class="closeBtn" href="#none">
+            <img src="{{ img_url('cart/close.png') }}">
+        </a>
         해당 상품이 장바구니에 담겼습니다.<br/>
         장바구니로 이동하시겠습니까?
         <ul class="NSK mt20">
             <li class="aBox answerBox_block"><a href="#none">예</a></li>
             <li class="aBox waitBox_block"><a href="#none">계속구매</a></li>
+            <li class="aBox closeBox_block"><a href="#none">닫기</a></li>
         </ul>
     </div>
 </div>
+
 <!-- willbes-Lec-buyBtn-sm -->
 <script src="/public/js/willbes/product_util.js"></script>
 <script type="text/javascript">
     var $regi_form = $('#regi_book_form');
-    var $buy_layer = $('#buy_layer');
+    var $buy_continue_layer = $('#buy_continue_layer');
 
     $(document).ready(function() {
-        // 목록 페이지
-        // 상품 선택/해제
-        $regi_form.on('change', '.chk_books', function() {
-            showBuyLayer('book', $(this), 'buy_layer');
-        });
-
-        // 장바구니 이동 버튼 클릭
-        $buy_layer.on('click', '.answerBox_block', function() {
-            goCartPage('book');
-        });
-
-        // 계속구매 버튼 클릭
-        $buy_layer.on('click', '.waitBox_block', function() {
-            $buy_layer.find('.pocketBox').css('display','none').hide();
-            $buy_layer.removeClass('active');
-        });
-
-        // 장바구니, 바로결제 버튼 클릭
-        $('button[name="btn_cart"], button[name="btn_direct_pay"]').on('click', function() {
+        {{--장바구니, 바로결제 버튼 클릭--}}
+        $('a[name="btn_book_cart"], a[name="btn_book_direct_pay"]').on('click', function () {
             {!! login_check_inner_script('로그인 후 이용하여 주십시오.','Y') !!}
+            var $prod_code = $(this).data('prod-code');
+            $regi_form.find('input:checkbox[name="prod_code[]"]').prop('checked', false);
+            $regi_form.find('input:checkbox[name="prod_code[]"]:input[data-prod-code="'+$prod_code+'"]').prop('checked', true);
             var $is_direct_pay = $(this).data('direct-pay');
             var $is_redirect = $(this).data('is-redirect');
-
             var $result = cartNDirectPay($regi_form, $is_direct_pay, $is_redirect);
-
             if ($is_redirect === 'N' && $result === true) {
-                openWin('pocketBox');
+                showContinueLayer('book', '', $(this), 'buy_continue_layer');
             }
+        });
+
+        {{--장바구니 이동 버튼 클릭--}}
+        $('.common_buy_layer').on('click', '.answerBox_block', function() {
+            goCartPage(getCartType($regi_form));
+        });
+
+        {{--계속구매 버튼 클릭--}}
+        $('.common_buy_layer').on('click', '.closeBtn, .waitBox_block, .closeBox_block', function() {
+            $('.common_buy_layer').removeClass('active');
         });
 
         // 교재로 진행중인 강의 버튼 클릭

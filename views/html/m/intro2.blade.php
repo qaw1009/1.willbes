886065e510cfb459f271d04dcc01e210646ad3a4
김창용 <winmir@willbes.com>
2019-11-05 15:27:17 +0900

    통합 모바일, gnb 아이콘

--- views/html/m/intro2.blade.php
+++ views/html/m/intro2.blade.php
@@ -11,7 +11,7 @@
                     <a href="https://police.willbes.net/m/home/index">  
                         <p class="subTit">일반경찰/경행/승진/해경</p>
                         <p class="siteTit">
-                            <img src="{{ img_url('m/intro/icon_cop.gif') }}" alt="신광은경찰">
+                            <img src="{{ img_url('m/intro/icon_cop.png') }}" alt="신광은경찰">
                             신광은 경찰
                         </p>
                     </a>
@@ -22,7 +22,7 @@
                     <a href="https://pass.willbes.net/m/home/index">   
                         <p class="subTit">9급/7급/법원/소방/기술직/군무원</p>
                         <p class="siteTit">
-                            <img src="{{ img_url('m/intro/icon_gosi.gif') }}" alt="공무원">
+                            <img src="{{ img_url('m/intro/icon_gosi.png') }}" alt="공무원">
                             공무원
                         </p>
                     </a>
@@ -33,7 +33,7 @@
                     <a href="https://job.willbes.net/m/home/index">   
                         <p class="subTit">국가기술자격/국가전문자격</p>
                         <p class="siteTit">
-                            <img src="{{ img_url('m/intro/icon_job.gif') }}" alt="자격증">
+                            <img src="{{ img_url('m/intro/icon_job.png') }}" alt="자격증">
                             자격증
                         </p>
                     </a>
@@ -44,7 +44,7 @@
                     <a href="https://lang.willbes.net/m/home/index"> 
                         <p class="subTit">G-TELP</p>
                         <p class="siteTit">
-                            <img src="{{ img_url('m/intro/icon_lang.gif') }}" alt="어학">
+                            <img src="{{ img_url('m/intro/icon_lang.png') }}" alt="어학">
                             어학
                         </p>
                     </a>

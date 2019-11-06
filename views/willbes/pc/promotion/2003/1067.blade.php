429e4dc2b3fd5e6b050b16887fc4144b74f718df
김창용 <winmir@willbes.com>
2019-11-06 17:33:22 +0900

    공무원 한덕현 첨삭지도반

--- views/willbes/pc/promotion/2003/1067.blade.php
+++ views/willbes/pc/promotion/2003/1067.blade.php
@@ -44,7 +44,7 @@
      }
 </style>
 @php
-    $goSubmit = (date('YmdHis') >= '20191010140000') ? 'doEvent(); return false;' : 'javascript:alert("10.10(목) 14시부터 접수합니다.");';
+    $goSubmit = (date('YmdHis') >= '20191107140000') ? 'doEvent(); return false;' : 'javascript:alert("11.7(목) 14시부터 접수합니다.");';
 @endphp
 
 <div class="p_re evtContent NGR" id="evtContainer">

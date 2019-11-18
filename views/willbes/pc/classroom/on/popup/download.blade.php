e0c2851371c443304e07225f8282263822453018
하준걸 <hereisme@willbes.net>
2019-11-18 11:27:55 +0900

    인쇄 스크립트 딜레이 적용

--- views/willbes/pc/classroom/on/popup/download.blade.php
+++ views/willbes/pc/classroom/on/popup/download.blade.php
@@ -11,42 +11,49 @@
     <script type="text/javascript">
         var ezpdf = new ezPDFWFLauncher(ezPDFWSPrinter, ezPDFData, 1);
 
-        function Pr()
-        {
-@if($type == 'SAMPLE')
-            ezpdf._debug = 0;
-            var urlPdf = encodeURIComponent('https://static.willbes.net/public/uploads/ezpdf/sample.pdf');
-            ezpdf.SetPDF(urlPdf, false);
-            var eventURL = "";
-            eventURL = encodeURIComponent(eventURL);
-            ezpdf.SetEventURL(eventURL);
-            ezpdf.SetSecData(2);
-            ezpdf.Launch(false);
-            setTimeout(function(){self.close();},3000);
-@else
-            var url = '{{front_url('classroom/on/getDownload/'.$orderidx.'/'.$prodcode.'/'.$prodcodesub.'/'.$lecidx.'/'.$unitidx)}}';
-            var data = '';
-            sendAjax(url,
-                data,
-                function (d) {
-                    var eventURL = '{{front_url('classroom/on/setPrintLog/'.$orderidx.'/'.$prodcode.'/'.$prodcodesub.'/'.$lecidx.'/'.$unitidx.'/'.sess_data('mem_idx').'/t/')}}';
+        $(document).ready(function() {
+            setTimeout(function() {
+                Pr();
+            }, 1000);
+
+            function Pr() {
+                @if($type == 'SAMPLE')
                     ezpdf._debug = 0;
-                    ezpdf.SetPDF(d.ret_data.pdfUrl, false);
-                    ezpdf.SetEventURL(eventURL);
-                    ezpdf.SetSecData(2);
-                    ezpdf.Launch(false);
-                    setTimeout(function () {
-                        self.close();
-                    }, 3000);
-                },
-                function (ret, status) {
-                    alert(ret.ret_msg);
+                var urlPdf = encodeURIComponent('https://static.willbes.net/public/uploads/ezpdf/sample.pdf');
+                ezpdf.SetPDF(urlPdf, false);
+                var eventURL = "";
+                eventURL = encodeURIComponent(eventURL);
+                ezpdf.SetEventURL(eventURL);
+                ezpdf.SetSecData(2);
+                ezpdf.Launch(false);
+                setTimeout(function () {
                     self.close();
-                }, false, 'GET', 'json');
-@endif
-        }
+                }, 3000);
+                        @else
+                var url = '{{front_url('classroom/on/getDownload/'.$orderidx.'/'.$prodcode.'/'.$prodcodesub.'/'.$lecidx.'/'.$unitidx)}}';
+                var data = '';
+                sendAjax(url,
+                    data,
+                    function (d) {
+                        var eventURL = '{{front_url('classroom/on/setPrintLog/'.$orderidx.'/'.$prodcode.'/'.$prodcodesub.'/'.$lecidx.'/'.$unitidx.'/'.sess_data('mem_idx').'/t/')}}';
+                        ezpdf._debug = 0;
+                        ezpdf.SetPDF(d.ret_data.pdfUrl, false);
+                        ezpdf.SetEventURL(eventURL);
+                        ezpdf.SetSecData(2);
+                        ezpdf.Launch(false);
+                        setTimeout(function () {
+                            self.close();
+                        }, 3000);
+                    },
+                    function (ret, status) {
+                        alert(ret.ret_msg);
+                        self.close();
+                    }, false, 'GET', 'json');
+                @endif
+            }
+        });
     </script>
 </head>
-<body onload='javascript:Pr();'>
+<body>
 </body>
 </html>

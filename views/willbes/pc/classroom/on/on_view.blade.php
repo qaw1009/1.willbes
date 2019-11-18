2965b972ccc9476b2175c45c4d8570293def9ecd
하준걸 <hereisme@willbes.net>
2019-11-18 09:28:23 +0900

    테스트코드 삭제

--- views/willbes/pc/classroom/on/on_view.blade.php
+++ views/willbes/pc/classroom/on/on_view.blade.php
@@ -94,7 +94,7 @@
                 <div id="WrapReply"></div>
             </div>
             <!-- willbes-Mypage-PASSZONE -->
-            @if(true) <!--$lec['wControlCountUse'] > 0)-->
+            @if($lec['wControlCountUse'] > 0)-->
             <div class="willbes-Leclist-txt c_both mt40">
                 <p>※ 해당 강좌는 회차별 자료 인쇄 제한이 있는 강좌 입니다.<br>
                     샘플파일로 프린터 연결상태, 설정(용지크기)등을 확인 후 출력하시기 바랍니다.</p>

451bdb7876ffbd3e07705c6bfb45397cd1362c46
이윤구 <yoongukk@willbes.com>
2019-11-22 13:35:10 +0900

    기미진 라이브 댓글

--- views/willbes/pc/promotion/2003/1442.blade.php
+++ views/willbes/pc/promotion/2003/1442.blade.php
@@ -86,7 +86,7 @@
         {{--홍보url--}}
         @if( empty($data['data_option_ccd']) === false && array_key_exists($arr_base['option_ccd']['comment_list'], $data['data_option_ccd']) === true && array_key_exists($arr_base['comment_use_area']['event'], $data['data_comment_use_area']) === true)
             @include('willbes.pc.promotion.show_comment_list_url_partial')
-        @endif y
+        @endif 
 
         
 	</div>

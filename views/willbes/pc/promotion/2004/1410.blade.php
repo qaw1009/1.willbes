99b697fe7f538985f04fa27c19c0dcc11ceb5f65
이윤구 <yoongukk@willbes.com>
2019-11-27 11:55:28 +0900

    (공무원_학원) 소방 학원 1월 개강 페이지 수정

--- views/willbes/pc/promotion/2004/1410.blade.php
+++ views/willbes/pc/promotion/2004/1410.blade.php
@@ -30,7 +30,7 @@
         .evt03 {background:url("https://static.willbes.net/public/images/promotion/2019/10/1410_03_bg.jpg") no-repeat center top}
         .evt04 {background:#dfdfdf; padding-bottom:150px}
         .evt04 .tabs {width:920px; margin:0 auto 20px}
-        .evt04 li {display:inline; float:left; width:20%; text-align:center}
+        .evt04 li {display:inline; float:left; width:16.6%; text-align:center}
         .evt04 li a {display:block; height:70px; line-height:70px; color:#757474; background:#dfdfdf; font-size:16px; border:1px solid #8d8d8d; margin-right:2px}
         .evt04 li a:hover,
         .evt04 li a.active {color:#fff; background:#ac3433;border:1px solid #981e1d; }
@@ -55,7 +55,7 @@
         </ul> 
 
         <div class="evtCtnsBox evtTop">
-            <img src="https://static.willbes.net/public/images/promotion/2019/10/1410_top.jpg" title="불꽃소방 1만 문풀의 법칙"/>
+            <img src="https://static.willbes.net/public/images/promotion/2019/11/1410_top.jpg" title="불꽃소방 1만 문풀의 법칙"/>
         </div>
       
         <div class="evtCtnsBox evt01">
@@ -63,7 +63,7 @@
         </div>
 
         <div class="evtCtnsBox evt02" id="to_go">
-            <img src="https://static.willbes.net/public/images/promotion/2019/10/1410_02.jpg" usemap="#Map1410C" title="1만 문풀의 법칙" border="0" />
+            <img src="https://static.willbes.net/public/images/promotion/2019/11/1410_02.jpg" usemap="#Map1410C" title="1만 문풀의 법칙" border="0" />
             <map name="Map1410C" id="Map1410C">
                 <area shape="rect" coords="162,772,331,829" href="https://pass.willbes.net/pass/offPackage/show/prod-code/157366" target="_blank" alt="2개월 공채" />
                 <area shape="rect" coords="355,774,523,827" href="https://pass.willbes.net/pass/offPackage/show/prod-code/157365" target="_blank" alt="2개월 특채" />
@@ -78,37 +78,40 @@
         --}}
 
         <div class="evtCtnsBox evt04">
-            <img src="https://static.willbes.net/public/images/promotion/2019/10/1410_04.jpg" title="10월 테스트 일정" />
+            <img src="https://static.willbes.net/public/images/promotion/2019/11/1410_04.jpg" title="10월 테스트 일정" />
             <ul class="tabs">
                 <li><a href="#tab01" class="active">소방학개론</a></li>
                 <li><a href="#tab02">소방관계법규</a></li>
                 <li><a href="#tab03">소방국어</a></li>
                 <li><a href="#tab04">소방한국사</a></li>
-                <li><a href="#tab05">소방영어</a></li>
+                <li><a href="#tab05">소방영어[공채]</a></li>
+                <li><a href="#tab06">소방영어[특채]</a></li>
             </ul>
-            <div id="tab01"><img src="https://static.willbes.net/public/images/promotion/2019/10/1410_t1_c.jpg" title="소방학개론" /></div>
-            <div id="tab02"><img src="https://static.willbes.net/public/images/promotion/2019/10/1410_t2_c.jpg" title="소방관계법규" /></div>
-            <div id="tab03"><img src="https://static.willbes.net/public/images/promotion/2019/10/1410_t3_c.jpg" title="소방국어" /></div>
-            <div id="tab04"><img src="https://static.willbes.net/public/images/promotion/2019/10/1410_t4_c.jpg" title="소방한국사" /></div>
-            <div id="tab05"><img src="https://static.willbes.net/public/images/promotion/2019/10/1410_t5_c.jpg" title="소방영어" /></div>
+            <div id="tab01"><img src="https://static.willbes.net/public/images/promotion/2019/11/1410_t1_c.jpg" title="소방학개론" /></div>
+            <div id="tab02"><img src="https://static.willbes.net/public/images/promotion/2019/11/1410_t2_c.jpg" title="소방관계법규" /></div>
+            <div id="tab03"><img src="https://static.willbes.net/public/images/promotion/2019/11/1410_t3_c.jpg" title="소방국어" /></div>
+            <div id="tab04"><img src="https://static.willbes.net/public/images/promotion/2019/11/1410_t4_c.jpg" title="소방한국사" /></div>
+            <div id="tab05"><img src="https://static.willbes.net/public/images/promotion/2019/11/1410_t5_c.jpg" title="소방영어" /></div>
+            <div id="tab06"><img src="https://static.willbes.net/public/images/promotion/2019/11/1410_t6_c.jpg" title="소방영어" /></div>
         </div>
 
         <div class="evtCtnsBox evt05">
-            <img src="https://static.willbes.net/public/images/promotion/2019/10/1410_05.jpg" usemap="#Map1410A" title="불꽃소방팀 교수진" border="0"  />
-            <map name="Map1410A" id="Map1410A">
-                <area shape="rect" coords="155,703,266,734" href="https://pass.willbes.net/pass/professor/show/prof-idx/50466/?cate_code=3050&amp;subject_idx=1259&amp;subject_name=소방학개론" target="_blank" alt="김종상" />
-                <area shape="rect" coords="387,745,496,773" href="https://pass.willbes.net/pass/professor/show/prof-idx/50662/?cate_code=3050&amp;subject_idx=1253&amp;subject_name=국어" target="_blank" alt="김세령" />
-                <area shape="rect" coords="619,745,726,775" href="https://pass.willbes.net/pass/professor/show/prof-idx/50306/?cate_code=3050&amp;subject_idx=1255&amp;subject_name=한국사" target="_blank" alt="한경준" />
-                <area shape="rect" coords="838,693,944,723" href="https://pass.willbes.net/pass/professor/show/prof-idx/50310/?cate_code=3050&amp;subject_idx=1254&amp;subject_name=영어" target="_blank" alt="이아림" />
-            </map>
+            <img src="https://static.willbes.net/public/images/promotion/2019/11/1410_05.jpg" usemap="#Map1410z" border="0" />
+            <map name="Map1410z" id="Map1410z">
+                <area shape="rect" coords="91,755,194,784" href="https://pass.willbes.net/pass/professor/show/prof-idx/50466/?cate_code=3050&subject_idx=1259&subject_name=%EC%86%8C%EB%B0%A9%ED%95%99%EA%B0%9C%EB%A1%A0" target="_blank" />
+                <area shape="rect" coords="295,745,400,776" href="https://pass.willbes.net/pass/professor/show/prof-idx/50662/?cate_code=3050&subject_idx=1253&subject_name=%EA%B5%AD%EC%96%B4" target="_blank" />
+                <area shape="rect" coords="501,745,606,775" href="https://pass.willbes.net/pass/professor/show/prof-idx/50306/?cate_code=3050&subject_idx=1255&subject_name=%ED%95%9C%EA%B5%AD%EC%82%AC" target="_blank" />
+                <area shape="rect" coords="703,744,816,777" href="https://pass.willbes.net/pass/professor/show/prof-idx/50310/?cate_code=3050&subject_idx=1254&subject_name=%EC%98%81%EC%96%B4" target="_blank" />
+                <area shape="rect" coords="908,747,1016,779" href="https://pass.willbes.net/pass/professor/show/prof-idx/50072/?cate_code=3043&subject_idx=1254&subject_name=%EC%98%81%EC%96%B4" target="_blank" />
+            </map>           
             <ul>
                 <li>
                     <img src="https://static.willbes.net/public/images/promotion/2019/10/1410_a1.jpg" title="복습 동영상 제공" class="off"/>
                     <img src="https://static.willbes.net/public/images/promotion/2019/10/1410_a1_over.jpg" title="복습 동영상 제공" class="on"/>
                 </li>
                 <li>
-                    <img src="https://static.willbes.net/public/images/promotion/2019/10/1410_a2.jpg" title="사물함 무료 제공" class="off"/>
-                    <img src="https://static.willbes.net/public/images/promotion/2019/10/1410_a2_over.jpg" title="사물함 무료 제공" class="on"/>
+                    <img src="https://static.willbes.net/public/images/promotion/2019/11/1410_a2.jpg" title="사물함 무료 제공" class="off"/>
+                    <img src="https://static.willbes.net/public/images/promotion/2019/11/1410_a2_over.jpg" title="사물함 무료 제공" class="on"/>
                 </li>
                 <li>
                     <img src="https://static.willbes.net/public/images/promotion/2019/10/1410_a3.jpg" title="체력학원 연계 할인" class="off"/>
@@ -118,7 +121,7 @@
         </div>
 
         <div class="evtCtnsBox evt06">
-            <img src="https://static.willbes.net/public/images/promotion/2019/10/1410_06.jpg" title=""  />
+            <img src="https://static.willbes.net/public/images/promotion/2019/11/1410_06.jpg" title=""  />
         </div>
     </div>
     <!-- End Container -->

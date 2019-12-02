3e04972e1d9b99d4154047ae38d5fbde5f61a9d9
김창용 <winmir@willbes.com>
2019-12-02 15:50:48 +0900

    취업 - 공기업 메인

---
+++ views/html/main_willpass.blade.php
@@ -0,0 +1,190 @@
+@extends('willbes.pc.layouts.master')
+
+@section('content')
+<!-- Container -->
+
+<div id="Container" class="Container willpass NGR c_both">
+    <div class="Menu widthAuto NGR c_both">
+        <h3>
+            <ul class="menu-Tit">
+                <li class="Tit">취업<span class="row-line">|</span></li>
+                <li class="subTit">공기업</li>
+            </ul>
+            <ul class="menu-List">
+                <li class="dropdown">
+                    <a href="#none">교수진소개</a>
+                    <div class="drop-Box list-drop-Box">
+                        <ul>
+                            <li class="Tit">교수진소개 메인</li>
+                            <li><a href="#none">신규강좌게시판</li>
+                            <li><a href="#none">민사법</li>
+                            <li><a href="#none">형사법</a></li>
+                            <li><a href="#none">공법(헌법)</a></li>
+                            <li><a href="#none">공법(행정법)</li>
+                            <li><a href="#none">국제거래법</a></li>
+                            <li><a href="#none">경제법</a></li>
+                            <li><a href="#none">환경법</a></li>
+                            <li><a href="#none">노동법</a></li>
+                            <li class="Tit">교수님 홈</li>
+                            <li><a href="#none">개설강좌</a></li>
+                            <li><a href="#none">무료강좌</a></li>
+                            <li><a href="#none">공지사항</a></li>
+                            <li><a href="#none">학습자료실</a></li>
+                            <li><a href="#none">수강후기</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <a href="#none">수강신청</a>
+                </li>
+                <li>
+                    <a href="#none">수험정보</a>
+                </li>
+                <li>
+                    <a href="#none">교재구매</a>
+                </li>
+            </ul>
+        </h3>
+    </div>
+
+    <div class="Section MainVisual">        
+        <div class="widthAuto NSK mt30">
+            <div class="VisualsubBox">
+                <div class="bSlider">
+                    <div class="sliderStopAutoPager">
+                        <div><a href="#none"><img src="https://static.willbes.net/public/images/promotion/main/2009_1120x380.jpg" alt="배너명"></a></div>
+                        <div><a href="#none"><img src="https://static.willbes.net/public/images/promotion/main/2009_1120x380.jpg" alt="배너명"></a></div>
+                        <div><a href="#none"><img src="https://static.willbes.net/public/images/promotion/main/2009_1120x380.jpg" alt="배너명"></a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="Section">
+        <div class="widthAuto bnrSec">   
+            <ul>
+                <li><a href="#"><img src="https://static.willbes.net/public/images/promotion/main/2009_556x120.jpg" alt="배너명"></a></li>
+                <li><a href="#"><img src="https://static.willbes.net/public/images/promotion/main/2009_556x120.jpg" alt="배너명"></a></li>
+            </ul>  
+        </div>
+    </div>
+
+    <div class="Section NSK mt90">
+        <div class="widthAuto"> 
+            <div class="noticeTabs">
+                <div class="will-listTit">공지사항</div>
+                <div class="tabBox noticeBox">
+                    <div class="tabContent p_re">
+                        <a href="#none" class="f_right btn-add"><img src="{{ img_url('gosi_acad/icon_add_big.png') }}" alt="더보기"></a>
+                        <ul class="List-Table">
+                            <li><a href="#none"><span>EVENT</span>2019년도 국가공무원 공개경쟁채용시험 등 계획 공고</a><span class="date">2019-01-25 </span></li>
+                            <li><a href="#none"><span>EVENT</span>2019 제1회 서울시 지방공무원(7,9급 등) 임용시험 시행계획 변경 공고</a><span class="date">2019-01-25 </span></li>
+                            <li><a href="#none">[공지] 2019년도 제주교육청 지방공무원 임용시험 일정안내</a><span class="date">2019-01-25 </span></li>
+                            <li><a href="#none">[공지]2019년도 광주교육청 지방공무원 임용시험 일정안내</a><span class="date">2019-01-25 </span></li>
+                            <li><a href="#none">2019년도 부산교육청 지방공무원 임용시험 일정안내</a><span class="date">2019-01-25 </span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="noticeTabs">
+                <div class="will-listTit">채용공고</div>
+                <div class="tabBox noticeBox">
+                    <div class="tabContent p_re">
+                        <a href="#none" class="f_right btn-add"><img src="{{ img_url('gosi_acad/icon_add_big.png') }}" alt="더보기"></a>
+                        <ul class="List-Table">
+                            <li><a href="#none"><span>EVENT</span>2019년도 국가공무원 공개경쟁채용시험 등 계획 공고</a><span class="date">2019-01-25 </span></li>
+                            <li><a href="#none"><span>EVENT</span>2019 제1회 서울시 지방공무원(7,9급 등) 임용시험 시행계획 변경 공고</a><span class="date">2019-01-25 </span></li>
+                            <li><a href="#none">[공지] 2019년도 제주교육청 지방공무원 임용시험 일정안내</a><span class="date">2019-01-25 </span></li>
+                            <li><a href="#none">[공지]2019년도 광주교육청 지방공무원 임용시험 일정안내</a><span class="date">2019-01-25 </span></li>
+                            <li><a href="#none">2019년도 부산교육청 지방공무원 임용시험 일정안내</a><span class="date">2019-01-25 </span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="noticeTabs mr-zero">
+                <div class="will-listTit">수험뉴스</div>
+                <div class="tabBox noticeBox">
+                    <div class="tabContent p_re">
+                        <a href="#none" class="f_right btn-add"><img src="{{ img_url('gosi_acad/icon_add_big.png') }}" alt="더보기"></a>
+                        <ul class="List-Table">
+                            <li><a href="#none"><span>EVENT</span>2019년도 국가공무원 공개경쟁채용시험 등 계획 공고</a><span class="date">2019-01-25 </span></li>
+                            <li><a href="#none"><span>EVENT</span>2019 제1회 서울시 지방공무원(7,9급 등) 임용시험 시행계획 변경 공고</a><span class="date">2019-01-25 </span></li>
+                            <li><a href="#none">[공지] 2019년도 제주교육청 지방공무원 임용시험 일정안내</a><span class="date">2019-01-25 </span></li>
+                            <li><a href="#none">[공지]2019년도 광주교육청 지방공무원 임용시험 일정안내</a><span class="date">2019-01-25 </span></li>
+                            <li><a href="#none">2019년도 부산교육청 지방공무원 임용시험 일정안내</a><span class="date">2019-01-25 </span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="Section NSK mt90 mb90">
+        <div class="widthAuto">
+            <div class="CScenterBox">
+                <dl>
+                    <dt class="willbesNumber">
+                        <ul>
+                            <li>
+                                <div class="nTit">온라인 수강문의</div>
+                                <div class="nNumber tx-color">1544-5006 <span>▶</span> 2</div>
+                                <div class="nTxt">
+                                    [운영시간]<br/>
+                                    평일: 09시~ 18시 (점심시간12시~13시)<br/>
+                                    공휴일/일요일휴무<br/>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="nTit">교재문의</div>
+                                <div class="nNumber tx-color">1544-4944</div>
+                                <div class="nTxt">
+                                    [운영시간]<br/>
+                                    평일: 09시~ 17시 (점심시간12시~13시)<br/>
+                                    공휴일/일요일휴무<br/>
+                                </div>
+                            </li>
+                        </ul>
+                    </dt>    
+                    <dt class="willbesCenter">
+                        <div class="centerTit">윌비스 취업 사이트에 물어보세요!</div>
+                        <ul>
+                            <li>
+                                <a href="#none">
+                                    <img src="{{ img_url('cop/icon_cecenter1.png') }}">
+                                    <div class="nTxt">자주하는<br/>질문</div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#none">
+                                    <img src="{{ img_url('cop/icon_cecenter2.png') }}">
+                                    <div class="nTxt">모바일<br/>서비스</div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#none">
+                                    <img src="{{ img_url('cop/icon_cecenter3.png') }}">
+                                    <div class="nTxt">동영상<br/>상담실</div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#none">
+                                    <img src="{{ img_url('cop/icon_cecenter4.png') }}">
+                                    <div class="nTxt">1:1<br/>고객지원</div>
+                                </a>
+                            </li>
+                        </ul>
+                    </dt>
+                    
+                </dl>
+            </div>            
+        </div>
+    </div>
+    <!-- CS센터 //-->
+</div>
+
+<!-- End Container -->
+
+@stop

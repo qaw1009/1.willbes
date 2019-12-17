4b8c6f491b6b082f306a3ec8c21f962058a9ae33
최형진 <letsboo@willbes.com>
2019-12-17 15:12:51 +0900

    프론트 고등고시 메인 - 학원 공지사항, 동영상 공지사항, 수험정보 조회

---
+++ application/controllers/willbes/site/support/GosiNotice.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require_once APPPATH . 'controllers/willbes/share/support/SupportNotice.php';
+
+class GosiNotice extends SupportNotice
+{
+    protected $_bm_idx = '108';       //bmidx : GosiNotice 게시판
+    protected $_default_path = '/support/notice';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+}

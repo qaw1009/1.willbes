3181e64d1a19c4a230c21e60807efd2a522def12
최형진 <letsboo@willbes.com>
2019-12-26 11:33:57 +0900

    프론트 고등고시, 경찰간부, 취업 - 모바일 메인 개발

--- views/willbes/m/main.blade.php
+++ views/willbes/m/main.blade.php
@@ -2,29 +2,6 @@
 
 @section('content')
     <!-- Container -->
-    {{--
-    <div id="Container" class="Container c_both mb20">
-        <div class="introBox NSK">
-            <div class="Tit">내 인생의 모든 시험, 윌비스로 합격하다!</div>
-            <ul>
-                <li>
-                    <div>
-                        <p class="subTit">일반/경행/승진/간부/해경</p>
-                        <p class="siteTit"><span>신광은 경찰</span><br><span> 온라인</span></p>
-                        <a href="{{ front_app_url('/home/index', 'police') }}" class="btnGo">바로가기</a>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <p class="subTit">9급/7급/법원/소방/기술/군무원</p>
-                        <p class="siteTit"><span>공무원</span><br><span> 온라인</span></p>
-                        <a href="{{ front_app_url('/home/index', 'pass') }}" class="btnGo">바로가기</a>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </div>
-    --}}
     <div id="Container" class="Container NGR c_both">
         <div class="Tit">내 인생의 모든 시험, 윌비스로 합격하다!</div>
         <div class="introBox2">
@@ -53,11 +30,44 @@
                 </li>
                 <li>
                     <div>
+                        <a href="{{ front_app_url('/home/index', 'gosi') }}">
+                            <p class="subTit">5급행정/외교원/PSAT/법행/변호사</p>
+                            <p class="siteTit">
+                                <img src="{{ img_url('m/intro/icon_gosi.png') }}" alt="고등고시">
+                                고등고시
+                            </p>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div>
                         <a href="{{ front_app_url('/home/index', 'job') }}">
-                            <p class="subTit">국가기술자격/국가전문자격</p>
+                            <p class="subTit">국가기술/국가전문/국가실무/국사공인</p>
                             <p class="siteTit">
                                 <img src="{{ img_url('m/intro/icon_job.png') }}" alt="자격증">
                                 자격증
+                            </p>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <a href="{{ front_app_url('/home/index', 'spo') }}">
+                            <p class="subTit">간보후보생</p>
+                            <p class="siteTit">
+                                <img src="{{ img_url('m/intro/icon_spo.png') }}" alt="경찰간부">
+                                경찰간부
+                            </p>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <a href="{{ front_app_url('/home/index', 'work') }}">
+                            <p class="subTit">공기업</p>
+                            <p class="siteTit">
+                                <img src="{{ img_url('m/intro/icon_willpass.png') }}" alt="취업">
+                                취업
                             </p>
                         </a>
                     </div>
@@ -76,6 +86,5 @@
             </ul>
         </div>
     </div>
-
     <!-- End Container -->
 @stop
--- views/willbes/m/site/main_partial/board_off.blade.php
+++ views/willbes/m/site/main_partial/board_off.blade.php
@@ -1,17 +1,20 @@
 <ul class="tabWrap buttonWrap noticeWrap three">
-    <li><a href="#notice1" class="on">공지사항</a></li>
-    <li><a href="#notice2">@if(SUB_DOMAIN == 'work') 채용공고 @else 시험공고 @endif</a></li>
-    <li><a href="#notice3">수험뉴스</a></li>
+    <li><a href="#notice1" class="on">학원 공지사항</a></li>
+    <li><a href="#notice2">동영상 공지사항</a></li>
+    <li><a href="#notice3">수험정보</a></li>
 </ul>
 <div class="tabBox buttonBox noticeBox">
     <div id="notice1" class="tabContent">
         <ul class="List-Table">
-            @if(empty($data['notice']) === true)
+            @if(empty($data['off_notice']) === true)
                 <li><span>등록된 내용이 없습니다.</span></li>
             @else
-                @foreach($data['notice'] as $row)
+                @foreach($data['off_notice'] as $row)
                     <li>
-                        <a href="{{front_url('/support/notice/show?board_idx='.$row['BoardIdx'])}}">{{$row['Title']}}</a>
+                        <a href="{{front_url('/support/gosiNotice/show/cate/'.$__cfg['CateCode'].'?board_idx='.$row['BoardIdx'])}}">
+                            {{-- <span>EVENT</span> --}}
+                            {{$row['Title']}}
+                        </a>
                         <span class="date">{{$row['RegDatm']}}</span>
                     </li>
                 @endforeach
@@ -20,12 +23,15 @@
     </div>
     <div id="notice2" class="tabContent" style="display: none;">
         <ul class="List-Table">
-            @if(empty($data['exam_announcement']) === true)
+            @if(empty($data['notice']) === true)
                 <li><span>등록된 내용이 없습니다.</span></li>
             @else
-                @foreach($data['exam_announcement'] as $row)
+                @foreach($data['notice'] as $row)
                     <li>
-                        <a href="{{front_url('/support/examAnnouncement/show/cate/'.$__cfg['CateCode'].'?board_idx='.$row['BoardIdx'])}}">{{$row['Title']}}</a>
+                        <a href="{{front_url('/support/notice/show/cate/'.$__cfg['CateCode'].'?board_idx='.$row['BoardIdx'])}}">
+                            {{-- <span>EVENT</span> --}}
+                            {{$row['Title']}}
+                        </a>
                         <span class="date">{{$row['RegDatm']}}</span>
                     </li>
                 @endforeach
@@ -39,7 +45,10 @@
             @else
                 @foreach($data['exam_news'] as $row)
                     <li>
-                        <a href="{{front_url('/support/examNews/show/cate/'.$__cfg['CateCode'].'?board_idx='.$row['BoardIdx'])}}">{{$row['Title']}}</a>
+                        <a href="{{front_url('/support/examNews/show/cate/'.$__cfg['CateCode'].'?board_idx='.$row['BoardIdx'])}}">
+                            {{-- <span>EVENT</span> --}}
+                            {{$row['Title']}}
+                        </a>
                         <span class="date">{{$row['RegDatm']}}</span>
                     </li>
                 @endforeach

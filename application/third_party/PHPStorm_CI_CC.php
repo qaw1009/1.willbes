95279c355054b460b7dd8a6316d114a39b46226f
신봉석 <bsshin@willbes.com>
2020-01-02 14:52:17 +0900

    자동완성 클래스 추가 (단과할인율)

--- application/third_party/PHPStorm_CI_CC.php
+++ application/third_party/PHPStorm_CI_CC.php
@@ -196,6 +196,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @property GatewayModel $gatewayModel                                     GatewayModel Model Class
  * @property GatewayStatModel $gatewayStatModel                                     GatewayStatModel Model Class
  * @property CommonLectureModel $commonLectureModel                        Common Lecture Model Class
+ * @property LectureDiscModel $lectureDiscModel                            Lecture Discount Model Class
  ***************** MockTest ****************************************************************************
  * @property MockCommonModel $mockCommonModel                              MockTest MockCommon Model Class
  * @property BaseCodeModel $baseCodeModel                                        MockTest BaseCod Model Class

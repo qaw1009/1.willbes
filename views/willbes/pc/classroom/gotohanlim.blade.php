28b66ee68da06e7669848ab8afcb3e8592e76e84
하준걸 <hereisme@willbes.net>
2020-01-07 17:11:45 +0900

    한림 이관 회원 이전 강의보기

--- views/willbes/pc/classroom/gotohanlim.blade.php
+++ views/willbes/pc/classroom/gotohanlim.blade.php
@@ -1,5 +1,5 @@
 <form name="form" id="form" method="post" action="http://www.hanlimgosi.co.kr/api/willbes.asp">
-<input type="hidden" name="enc_data" value="{{$enc_data}}" />
+    <input type="hidden" name="enc_data" value="{{$enc_data}}" />
 </form>
 <script>
     document.form.submit();

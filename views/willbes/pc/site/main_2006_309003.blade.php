6d6f569cd44c01053f1101d208f7bf956231cf0f
김창용 <winmir@willbes.com>
2020-01-09 15:57:58 +0900

    고등고시, 자격증 우측 퀵메뉴

--- views/willbes/pc/site/main_2006_309003.blade.php
+++ views/willbes/pc/site/main_2006_309003.blade.php
@@ -56,17 +56,10 @@
         </div>
         <!-- CS센터 //-->
 
-        {{--
         <div id="QuickMenu" class="MainQuickMenu">
-            <div><a href="#none"><img src="https://static.willbes.net/public/images/promotion/main/2005_sky01.jpg"></a></div>
-            <ul>
-                <li><a href="#none">강의 계획서</a></li>
-                <li><a href="#none">강의 시간표</a></li>
-                <li><a href="#none">강의실 배정표</a></li>
-                <li><a href="#none">학원 할인정책</a></li>
-            </ul>
-        </div>
-        --}}
+            {{-- quick menu --}}
+            @include('willbes.pc.site.main_partial.quick_menu_' . $__cfg['SiteCode'])
+        </div> 
 
     </div>
     <!-- End Container -->
--- views/willbes/pc/site/main_partial/quick_menu_2005.blade.php
+++ views/willbes/pc/site/main_partial/quick_menu_2005.blade.php
@@ -1,4 +1,5 @@
-<div><a href="{{ site_url('/lecture/index/cate/' . $__cfg['CateCode'] . '/pattern/free') }}"><img src="https://static.willbes.net/public/images/promotion/main/2005_sky01.jpg"></a></div>
+<div><a href="{{ site_url('/lecture/index/cate/' . $__cfg['CateCode'] . '/pattern/free') }}"><img src="https://static.willbes.net/public/images/promotion/main/2000_sky01.jpg"></a></div>
+<div class="mt5"><a href="{{ site_url('/pass/support/qna/index') }}"><img src="https://static.willbes.net/public/images/promotion/main/2000_sky02.jpg" alt="학원 1:1상담"></a></div>
 <ul>
     <li><a href="{{ site_url('/pass/offinfo/boardInfo/index/109') }}">강의 계획서</a></li>
     <li><a href="{{ site_url('/pass/offinfo/boardInfo/index/80') }}">강의 시간표</a></li>

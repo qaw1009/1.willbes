11b70b8f23a8e5630c3d2a5c591293f85f3bcbde
김창용 <winmir@willbes.com>
2020-01-14 14:55:52 +0900

    모바일 게이트 페이지

--- views/willbes/m/main.blade.php
+++ views/willbes/m/main.blade.php
@@ -11,8 +11,8 @@
                         <a href="{{ front_app_url('/home/index', 'police') }}">
                             <p class="subTit">일반경찰/경행/승진/해경</p>
                             <p class="siteTit">
-                                <img src="{{ img_url('m/intro/icon_cop.png') }}" alt="신광은경찰">
-                                신광은 경찰
+                            <img src="{{ img_url('m/intro/icon_cop.png') }}" alt="신광은경찰">
+                             신광은 경찰
                             </p>
                         </a>
                     </div>
@@ -22,8 +22,8 @@
                         <a href="{{ front_app_url('/home/index', 'pass') }}">
                             <p class="subTit">9급/7급PSAT/7급/법원/소방/기술직/군무원</p>
                             <p class="siteTit">
-                                <img src="{{ img_url('m/intro/icon_gosi.png') }}" alt="공무원">
-                                공무원
+                            <img src="{{ img_url('m/intro/icon_gosi.png') }}" alt="공무원">
+                             공무원
                             </p>
                         </a>
                     </div>
@@ -34,7 +34,7 @@
                             <p class="subTit">5급행정/외교원/PSAT/법행/변호사</p>
                             <p class="siteTit">
                                 <img src="{{ img_url('m/intro/icon_gosi.png') }}" alt="고등고시">
-                                    고등고시
+                                 고등고시
                             </p>
                         </a>
                     </div>
@@ -42,10 +42,10 @@
                 <li>
                     <div>
                         <a href="{{ front_app_url('/home/index', 'job') }}">
-                            <p class="subTit">국가기술/국가전문/국가실무/국가공인</p>
+                            <p class="subTit">노무/감평/변리/관세/스포츠</p>
                             <p class="siteTit">
                                 <img src="{{ img_url('m/intro/icon_job.png') }}" alt="자격증">
-                                자격증
+                                 자격증
                             </p>
                         </a>
                     </div>
@@ -56,7 +56,7 @@
                             <p class="subTit">간보후보생</p>
                             <p class="siteTit">
                                 <img src="{{ img_url('m/intro/icon_spo.png') }}" alt="경찰간부">
-                                    경찰간부
+                                 경찰간부
                             </p>
                         </a>
                     </div>
@@ -67,7 +67,7 @@
                             <p class="subTit">공기업</p>
                             <p class="siteTit">
                                 <img src="{{ img_url('m/intro/icon_willpass.png') }}" alt="취업">
-                                    취업
+                                 취업
                             </p>
                         </a>
                     </div>

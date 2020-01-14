d1c8d4e630a851d0ff6c6ddcd7418074cb819e8e
김창용 <winmir@willbes.com>
2020-01-14 16:30:02 +0900

    고등고시, 자격증 메인 퀵메뉴

--- views/willbes/pc/site/main_partial/quick_menu_2005.blade.php
+++ views/willbes/pc/site/main_partial/quick_menu_2005.blade.php
@@ -1,5 +1,18 @@
-<div><a href="{{ site_url('/lecture/index/cate/' . $__cfg['CateCode'] . '/pattern/free') }}"><img src="https://static.willbes.net/public/images/promotion/main/2000_sky01.jpg"></a></div>
-<div class="mt5"><a href="{{ site_url('/pass/support/qna/index') }}"><img src="https://static.willbes.net/public/images/promotion/main/2000_sky02.jpg" alt="학원 1:1상담"></a></div>
+<div>
+    <a href="{{ site_url('/lecture/index/cate/' . $__cfg['CateCode'] . '/pattern/free') }}">
+        <img src="https://static.willbes.net/public/images/promotion/main/2000_sky01.jpg">
+    </a>
+</div>
+<div class="mt5">
+    <a href="{{ site_url('/support/qna/index') }}">
+        <img src="https://static.willbes.net/public/images/promotion/main/2000_sky03.jpg" alt="동영상 1:1상담">
+    </a>
+</div>
+<div class="mt5">
+    <a href="{{ site_url('/pass/support/qna/index') }}">
+        <img src="https://static.willbes.net/public/images/promotion/main/2000_sky02.jpg" alt="학원 1:1상담">
+    </a>
+</div>
 <ul>
     <li><a href="{{ site_url('/pass/offinfo/boardInfo/index/109') }}">강의 계획서</a></li>
     <li><a href="{{ site_url('/pass/offinfo/boardInfo/index/80') }}">강의 시간표</a></li>

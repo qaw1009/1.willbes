abeb1432da87dc4fe948ba0e9b50699119601210
김창용 <winmir@willbes.com>
2020-01-17 13:18:57 +0900

    송광호 해양경찰학

---
+++ views/willbes/pc/promotion/2002/1523.blade.php
@@ -0,0 +1,115 @@
+@extends('willbes.pc.layouts.master')
+
+@section('content')
+    @include('willbes.pc.layouts.partial.site_menu')
+    <!-- Container -->
+    <style type="text/css">
+        .subContainer {
+            min-height: auto !important;
+            margin-bottom:0 !important;
+        }
+        .evtContent {
+            width:100% !important;
+            min-width:1120px !important;
+            margin-top:20px !important;
+            padding:0 !important;
+            background:#fff;
+        }
+        .evtContent span {vertical-align:auto}
+        .evtCtnsBox {width:100%; text-align:center; min-width:1120px; position:relative}
+
+
+        /************************************************************/
+        .evt_police{background:#404040}
+        .evt_top{background:url("https://static.willbes.net/public/images/promotion/2020/01/1523_top_bg.jpg")}
+        .evt_top span {position:absolute; top:330px; left:50%; margin-left:-100px; -webkit-animation:swing 2s linear infinite;animation:swing 2s linear infinite}
+        @@keyframes swing{
+            0%{-webkit-transform:rotate3d(0,0,1,0deg);transform:rotate3d(0,0,1,0deg)}
+            25%{-webkit-transform:rotate3d(0,0,1,10deg);transform:rotate3d(0,0,1,10deg)}
+            50%{-webkit-transform:rotate3d(0,0,1,0deg);transform:rotate3d(0,0,1,0deg)}
+            75%{-webkit-transform:rotate3d(0,0,1,-10deg);transform:rotate3d(0,0,1,-10deg)}
+            100%{-webkit-transform:rotate3d(0,0,1,0deg);transform:rotate3d(0,0,1,0deg)}
+        }
+
+        .evt_01 {background:#fff;}
+        .evt_02 {background:#e9e9e9; padding-bottom:100px} 
+        .evt_02 ul {width:800px; margin:50px auto 0}
+        .evt_02 li {display:inline; float:left; width:50%;}
+        .evt_02 li a {display:block; text-align:center; background:#363636; color:#fff; padding:15px 0; border-radius:30px; margin:0 10px; font-size:24px}
+        .evt_02 li a:hover {background:#806443; color:#f9dd74;
+            -webkit-box-shadow: 10px 10px 30px 1px rgba(0,0,0,0.31);
+            -moz-box-shadow: 10px 10px 30px 1px rgba(0,0,0,0.31);
+            box-shadow: 10px 10px 30px 1px rgba(0,0,0,0.31);
+        }
+        .evt_02 ul:after {content:""; display:block; clear:both}
+
+        /*타이머*/
+        .newTopDday {clear:both;background:#f5f5f5; width:100%; padding:20px 0; font-size:26px;}
+        .newTopDday ul {width:1120px; margin:0 auto}
+        .newTopDday ul li {display:inline; float:left; margin-right:5px; text-align:center; font-weight:600; color:#000}
+        .newTopDday ul li strong {line-height:70px}
+        .newTopDday ul li img {width:50px}
+        .newTopDday ul li:first-child {line-height:none; text-align:right; padding-right:10px; padding-top:10px; width:28%}
+        .newTopDday ul li:last-child {line-height:none; text-align:left; padding-left:10px; padding-top:5px; width:24%; line-height:70px}
+        .newTopDday ul:after {content:""; display:block; clear:both}
+    </style>
+
+    <div class="p_re evtContent NGR" id="evtContainer">
+        <!-- 타이머 -->
+        <div id="newTopDday" class="newTopDday">
+            <div>
+                <ul>
+                    <li>
+                        이벤트 할인<br>마감까지
+                    </li>
+                    <li><img id="dd1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="dd2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><strong>일</strong></li>
+                    <li><img id="hh1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="hh2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><strong>:</strong></li>
+                    <li><img id="mm1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="mm2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><strong>:</strong></li>
+                    <li><img id="ss1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="ss2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li>
+                        남았습니다.
+                    </li>
+                </ul>
+            </div>
+        </div> 
+
+        <div class="evtCtnsBox evt_police">
+            <img src="https://static.willbes.net/public/images/promotion/common/police_promotion_top.jpg" title="대학민국 경찰학원 1위 윌비스 신광은경찰팀">            
+        </div>
+
+        <div class="evtCtnsBox evt_top">
+            <img src="https://static.willbes.net/public/images/promotion/2020/01/1523_top.jpg" title="경찰학 장정훈 무료특강">
+            <span><img src="https://static.willbes.net/public/images/promotion/2020/01/1523_top_img.png" alt="꼭 들어보세요"></span>
+        </div>
+
+        <div class="evtCtnsBox evt_01">
+            <img src="https://static.willbes.net/public/images/promotion/2020/01/1523_01.jpg" title="경찰공무원 승진시험 분석">
+        </div>
+
+        <div class="evtCtnsBox evt_02">
+            <img src="https://static.willbes.net/public/images/promotion/2020/01/1523_02.jpg" title="완벽분석,개정법령">
+            <ul class="NSK-Black">
+                <li><a href="#none">온라인강의 Coming soon</a></li>
+                <li><a href="#none">학원강의 Coming soon</a></li>
+            </ul>
+        </div>    
+    </div>
+    <!-- End evtContainer --> 
+
+    <script type="text/javascript">
+        /*디데이카운트다운*/
+        $(document).ready(function() {
+            dDayCountDown('@if(empty($arr_promotion_params['edate'])===false) {{$arr_promotion_params['edate']}} @endif');
+        });
+    </script>
+
+    {{-- 프로모션용 스크립트 include --}}
+    @include('willbes.pc.promotion.promotion_script')
+@stop   

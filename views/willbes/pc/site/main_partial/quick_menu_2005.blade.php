1bf4f2f6aa3cf156218b5f0ed4808c2dc59099e5
최형진 <letsboo@willbes.com>
2020-01-30 10:23:02 +0900

    [프론트] 고시 메인 - 퀵배너 추가

--- views/willbes/pc/site/main_partial/quick_menu_2005.blade.php
+++ views/willbes/pc/site/main_partial/quick_menu_2005.blade.php
@@ -14,9 +14,12 @@
     </a>
 </div>
 <div class="mt5">
+    {!! banner_html(element('메인_우측퀵_01', $data['arr_main_banner'])) !!}
+    {{--
     <a href="{{ site_url('/support/notice/show/cate/' . $__cfg['CateCode'] . '?board_idx=253193') }}">
         <img src="https://static.willbes.net/public/images/promotion/main/2005_sky01.jpg" alt="신규강의">
     </a>
+    --}}
 </div>
 <ul>
     <li><a href="{{ site_url('/pass/offinfo/boardInfo/index/109?on_off_link_cate_code=' . $__cfg['CateCode'] . '&s_cate_code_disabled=Y') }}">강의 계획서</a></li>

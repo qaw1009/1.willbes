7fda60e1cbd21841960ae32bd893ed5b5bf3ed63
김창용 <winmir@willbes.com>
2020-02-12 09:13:26 +0900

    경찰 끝장 패스

--- views/willbes/pc/promotion/2002/1489.blade.php
+++ views/willbes/pc/promotion/2002/1489.blade.php
@@ -167,18 +167,18 @@
                     <li><img src="https://static.willbes.net/public/images/promotion/2019/12/1489_03_benefit02.jpg" alt=""/></li>
                     <li><img src="https://static.willbes.net/public/images/promotion/2019/12/1489_03_benefit03.jpg" alt=""/></li>
                     <li><img src="https://static.willbes.net/public/images/promotion/2019/12/1489_03_benefit04.jpg" alt=""/></li>
-                    {{--<li><img src="https://static.willbes.net/public/images/promotion/2019/12/1489_03_benefit05.jpg" alt=""/></li>--}}
                     <li><img src="https://static.willbes.net/public/images/promotion/2019/12/1489_03_benefit06.jpg" alt=""/></li>
                     <li><img src="https://static.willbes.net/public/images/promotion/2019/12/1489_03_benefit07.jpg" alt=""/></li>
                     <li><img src="https://static.willbes.net/public/images/promotion/2019/12/1489_03_benefit08.jpg" alt=""/></li>
+                    <li><img src="https://static.willbes.net/public/images/promotion/2019/12/1489_03_benefit09.jpg" alt=""/></li>
                     <li><img src="https://static.willbes.net/public/images/promotion/2019/12/1489_03_benefit01.jpg" alt=""/></li>
                     <li><img src="https://static.willbes.net/public/images/promotion/2019/12/1489_03_benefit02.jpg" alt=""/></li>
                     <li><img src="https://static.willbes.net/public/images/promotion/2019/12/1489_03_benefit03.jpg" alt=""/></li>
                     <li><img src="https://static.willbes.net/public/images/promotion/2019/12/1489_03_benefit04.jpg" alt=""/></li>
-                    {{--<li><img src="https://static.willbes.net/public/images/promotion/2019/12/1489_03_benefit05.jpg" alt=""/></li>--}}
                     <li><img src="https://static.willbes.net/public/images/promotion/2019/12/1489_03_benefit06.jpg" alt=""/></li>
                     <li><img src="https://static.willbes.net/public/images/promotion/2019/12/1489_03_benefit07.jpg" alt=""/></li>
                     <li><img src="https://static.willbes.net/public/images/promotion/2019/12/1489_03_benefit08.jpg" alt=""/></li> 
+                    <li><img src="https://static.willbes.net/public/images/promotion/2019/12/1489_03_benefit09.jpg" alt=""/></li> 
                 </ul>
             </div> 
         </div>

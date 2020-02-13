8b8f249bee3924b85c22e5bd0b50d848c30af43b
최형진 <letsboo@willbes.com>
2020-02-13 16:44:17 +0900

    [프론트] 강의 수강신청 - 강의시간표 링크 수정

--- views/willbes/pc/site/off_pack_lecture/index.blade.php
+++ views/willbes/pc/site/off_pack_lecture/index.blade.php
@@ -99,8 +99,14 @@
                         <span>검색</span>
                     </button>
                 </div>
-                
-                <div class="InfoBtnOff"><a href="/pass/offinfo/boardInfo/index">강의시간표 안내 <span>▶</span></a></div>
+
+                <div class="InfoBtnOff">
+                    @if(($__cfg['SiteCode'] == '2010' || $__cfg['SiteCode'] == '2011') && empty(element('cate_code', $arr_input)) === false)
+                        <a href="{{ front_url('/offinfo/boardInfo/index/80?s_cate_code='.element('cate_code', $arr_input).'&s_cate_code_disabled=Y') }}">강의시간표 안내 <span>▶</span></a>
+                    @else
+                        <a href="{{ front_url('/offinfo/boardInfo/index') }}">강의시간표 안내 <span>▶</span></a>
+                    @endif
+                </div>
                 <div class="InfoBtn mr10"><a href="#none" onclick="openWin('requestInfo')">학원수강 안내 <span>▶</span></a></div>
 
                 <div id="requestInfo" class="willbes-Layer-requestInfo">

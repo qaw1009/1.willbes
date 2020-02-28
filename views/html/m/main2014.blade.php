931dbc1600096e2c38da5f79008f822fc186e9f5
이윤구 <yoongukk@willbes.com>
2020-02-28 15:17:13 +0900

    [마케팅] 창업교육 N잡 티저(안) - 모바일

---
+++ views/html/m/main2014.blade.php
@@ -0,0 +1,58 @@
+<style type="text/css">        
+        .subContainer {
+            min-height: auto !important;
+            margin-bottom:0 !important;
+        }        
+        .evtContent {
+            width:100% !important;
+            min-width:720px !important;
+            margin-top:20px !important;
+            padding:0 !important;
+            background:#fff;
+        }
+        .evtContent span {vertical-align:auto}
+        .evtCtnsBox {width:100%; text-align:center; min-width:720px;}
+ </style>       
+
+@extends('html.m.layouts.master')
+
+@section('content')
+<!-- Container -->
+    <div id="Container" class="Container cus NGR c_both">
+        <!-- site nav -->
+
+        <div class="evtCtnsBox evtTop">
+            <img src="https://static.willbes.net/public/images/promotion/main/3114_mtop.png" title="">
+        </div>
+
+        <div class="evtCtnsBox evt01">
+            <img src="https://static.willbes.net/public/images/promotion/main/3114_m01.png" title="">
+        </div>    
+
+        <div class="evtCtnsBox evt02">
+            <img src="https://static.willbes.net/public/images/promotion/main/3114_m02.png" title="">
+        </div>    
+
+        <div class="evtCtnsBox evt03">
+            <img src="https://static.willbes.net/public/images/promotion/main/3114_m03.png" title="">
+        </div>    
+
+        <div class="evtCtnsBox evt04">
+            <img src="https://static.willbes.net/public/images/promotion/main/3114_m04.png" title="">
+        </div>    
+
+        <div class="evtCtnsBox evt05">
+            <img src="https://static.willbes.net/public/images/promotion/main/3114_m05.png" title="">
+        </div>    
+
+        <div class="evtCtnsBox evt06">
+            <img src="https://static.willbes.net/public/images/promotion/main/3114_m06.png" title="">
+        </div>   
+
+        <div class="evtCtnsBox evt07">
+            <img src="https://static.willbes.net/public/images/promotion/main/3114_m07.png" title="">
+        </div>                    
+       
+    </div>
+<!-- End Container -->
+@stop

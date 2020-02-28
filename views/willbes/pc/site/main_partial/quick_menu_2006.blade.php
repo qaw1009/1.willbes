a2d19e59d4c31b1874eacb72460aefa0c973498e
김창용 <winmir@willbes.com>
2020-02-28 18:33:07 +0900

    자격증 퀵배너

--- views/willbes/pc/site/main_partial/quick_menu_2006.blade.php
+++ views/willbes/pc/site/main_partial/quick_menu_2006.blade.php
@@ -47,6 +47,11 @@
         <img src="https://static.willbes.net/public/images/promotion/main/2000_sky02.jpg" alt="학원 1:1상담">
     </a>
 </div>
+<div class="mt5">
+    <a href="{{ site_url('/pass/support/qna/index?on_off_link_cate_code=' . $__cfg['CateCode'] . '&s_cate_code_disabled=Y') }}">
+        <img src="https://static.willbes.net/public/images/promotion/main/2000_sky04.jpg" alt="학원 공지사항">
+    </a>
+</div>
 <ul>
     <li><a href="{{ site_url('/pass/offinfo/boardInfo/index/109?on_off_link_cate_code=' . $__cfg['CateCode'] . '&s_cate_code_disabled=Y') }}">강의 계획서</a></li>
     <li><a href="{{ site_url('/pass/offinfo/boardInfo/index/80?on_off_link_cate_code=' . $__cfg['CateCode'] . '&s_cate_code_disabled=Y') }}">강의 시간표</a></li>

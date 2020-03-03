ba088ec2780aa7096dde014dad9610dd618096e1
하준걸 <hereisme@willbes.net>
2020-03-03 15:06:52 +0900

    이전사이트가기 5번 추가

--- views/willbes/pc/classroom/gotohanlim.blade.php
+++ views/willbes/pc/classroom/gotohanlim.blade.php
@@ -1,5 +1,6 @@
 <form name="form" id="form" method="post" action="{{$url}}/api/willbes.asp">
     <input type="hidden" name="enc_data" value="{{$enc_data}}" />
+    <input type="hidden" name="param" value="{{$param}}" />
 </form>
 <script>
     document.form.submit();

69114040e7786f2b5c81ede106ba50a0af26f7c3
최형진 <letsboo@willbes.com>
2020-03-03 16:23:30 +0900

    VideoJs 테스트

---
+++ views/html/development_videojs_49.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="/public/vendor/videojs/4.9/video-js.css" rel="stylesheet" />
+    <script src="/public/vendor/videojs/4.9/video.js"></script>
+</head>
+
+<body>
+    <h1>VideoJs(4.9) Sample</h1>
+    <video
+        id="my-video"
+        class="video-js vjs-default-skin vjs-big-play-centered"
+        controls
+        preload="none"
+        width="640"
+        height="264"
+        poster="/public/img/willbes/willbes_video_test.jpg"
+        data-setup="{}"
+    >
+        <source src="http://edge.v.iask.com.sinastorage.com/126385755.mp4" type="video/mp4" />
+    </video>
+</body>
+
+</html>

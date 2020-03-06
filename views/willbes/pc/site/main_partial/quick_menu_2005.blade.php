346ffcd4373c5b3d6b302727c1fc9d6faaaeb1c9
김창용 <winmir@willbes.com>
2020-03-06 15:16:51 +0900

    고등고시 퀵배너

--- views/willbes/pc/site/main_partial/quick_menu_2005.blade.php
+++ views/willbes/pc/site/main_partial/quick_menu_2005.blade.php
@@ -13,13 +13,15 @@
         <img src="https://static.willbes.net/public/images/promotion/main/2000_sky02.jpg" alt="학원 1:1상담">
     </a>
 </div>
+@if(empty($__cfg['CateCode']) === false && ($__cfg['CateCode'] == '3098' || $__cfg['CateCode'] == '3099'))
+<div class="mt5">
+    <a href="{{ site_url('/support/notice/show/cate/' . $__cfg['CateCode'] . '?board_idx=262920') }}">
+        <img src="https://static.willbes.net/public/images/promotion/main/2005_sky_200306.jpg" alt="불법공유">
+    </a>
+</div>
+@endif
 <div class="mt5">
     {!! banner_html(element('메인_우측퀵_01', $data['arr_main_banner'])) !!}
-    {{--
-    <a href="{{ site_url('/support/notice/show/cate/' . $__cfg['CateCode'] . '?board_idx=253193') }}">
-        <img src="https://static.willbes.net/public/images/promotion/main/2005_sky01.jpg" alt="신규강의">
-    </a>
-    --}}
 </div>
 <ul>
     <li><a href="{{ site_url('/pass/offinfo/boardInfo/index/109?on_off_link_cate_code=' . $__cfg['CateCode'] . '&s_cate_code_disabled=Y') }}">강의 계획서</a></li>

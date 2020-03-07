42e20e6f8dcabf8b24e2187931e20ced5fd518b8
김창용 <winmir@willbes.com>
2020-03-07 16:35:06 +0900

    엔잡 모바일

--- views/html/m/main2014.blade.php
+++ views/html/m/main2014.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <!-- Container -->
 <style type="text/css">     
-    .evtCtnsBox {width:100%; text-align:center; margin:0 auto;}
+    .evtCtnsBox {width:100%; text-align:center; max-width:720px; margin:0 auto;}
     .evtCtnsBox img {width:100%; max-width:720px}
 
     .skybanner {
@@ -24,8 +24,8 @@
     .evtFooter {}
     .swiper-button-next,
     .swiper-button-prev {background-color:#fff !important; border-radius:30px; width:30px !important; height:30px !important; margin:0 10px; }
-    .btnbuy {position:fixed; width:calc(100% - 20px); bottom:10px; left:50%; margin-left:calc(-50% + 10px); z-index:100; text-align:center; }    
-    .btnbuy a {display:block; font-size:1rem; background:#000; color:#fff; background:#000; border-radius:50px; color:#fff; line-height:1.4; padding:20px 0; font-size:1rem;}
+    .btnbuy {position:fixed; width:100%; bottom:10px; left:0; z-index:100; text-align:center; }    
+    .btnbuy a {display:block; font-size:1rem; color:#fff; background:#000; border-radius:50px; line-height:1.4; padding:20px 0; width:720px; margin:0 auto; font-size:1rem;}
     .btnbuy span {color:#3a99f0;}
     .btnbuy p {font-size:1.2rem;}
     .btnbuy a:hover {background:#fff; color:#3a99f0;

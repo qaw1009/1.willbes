75183bb8ac633b1d7de7a9c772d2735c83d9f1b2
김창용 <winmir@willbes.com>
2020-03-13 16:50:24 +0900

    경찰간부 퀵배너

--- views/willbes/pc/site/main_partial/quick_menu_2013.blade.php
+++ views/willbes/pc/site/main_partial/quick_menu_2013.blade.php
@@ -1,1 +1,10 @@
-경찰간부[학원] - 퀵메뉴
+<div class="mt5">
+    <a href="{{ site_url('/pass/board/qna/index')}}">
+        <img src="https://static.willbes.net/public/images/promotion/main/2000_sky02.jpg" alt="학원 1:1상담">
+    </a>
+</div>
+<div class="mt5">
+    <a href="{{ site_url('/pass/support/notice/index/cate/') }}">
+        <img src="https://static.willbes.net/public/images/promotion/main/2000_sky04.jpg" alt="학원 공지사항">
+    </a>
+</div>

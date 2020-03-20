6b22346e6c637c4e93e259000674432bbb71ec05
김창용 <winmir@willbes.com>
2020-03-20 10:05:58 +0900

    모바일 인트로

---
+++ views/html/m/intro3.blade.php
@@ -0,0 +1,92 @@
+@extends('html.m.layouts.master')
+
+@section('content')
+<!-- Container -->
+<div id="Container" class="Container NGR c_both"> 
+    <div class="introBox3 NSK"> 
+        <div>
+            <h4><img src="{{ img_url('m/intro/icon_cop.png') }}" alt="신광은경찰"> 신광은 경찰</h4>
+            <ul>
+                <li><a href="#none">일반경찰</a></li>
+                <li><a href="#none">경행경채</a></li>
+                <li><a href="#none">경찰승진</a></li>
+                <li><a href="#none">해양경찰</a></li>
+                <li><a href="#none">해양경찰특채</a></li>
+            </ul>
+        </div>
+        <div>
+            <h4><img src="{{ img_url('m/intro/icon_gosi.png') }}" alt="공무원"> 공무원</h4>
+            <ul>
+                <li><a href="#none">무료인강</a></li>
+                <li><a href="#none">9급</a></li>
+                <li><a href="#none">7급 PSAT</a></li>
+                <li><a href="#none">7급</a></li>
+                <li><a href="#none">세무직</a></li>
+                <li><a href="#none">법원직</a></li>
+                <li><a href="#none">소방직</a></li>
+                <li><a href="#none">기술직</a></li>
+                <li><a href="#none">군무원</a></li>
+                <li><a href="#none">부사관</a></li>
+            </ul>
+        </div>
+        <div>
+            <h4><img src="{{ img_url('m/intro/icon_gosi.png') }}" alt="고등고시"> 고등고시</h4>
+            <ul>
+                <li><a href="#none">5급행정</a></li>
+                <li><a href="#none">국립외교원</a></li>
+                <li><a href="#none">PSAT</a></li>
+                <li><a href="#none">5급헌법</a></li>
+                <li><a href="#none">법원행시</a></li>
+                <li><a href="#none">변호사시험</a></li>
+            </ul>
+        </div>
+        <div>
+            <h4><img src="{{ img_url('m/intro/icon_job.png') }}" alt="자격증"> 자격증</h4>
+            <ul>
+                <li><a href="#none">공인노무사</a></li>
+                <li><a href="#none">감정평가사</a></li>
+                <li><a href="#none">변리사</a></li>
+                <li><a href="#none">세무사</a></li>
+                <li><a href="#none">관세사</a></li>
+                <li><a href="#none">스포츠지도사</a></li>
+            </ul>
+        </div>
+        <div>
+            <h4><img src="{{ img_url('m/intro/icon_job.png') }}" alt="기타 자격증"> 기타 자격증</h4>
+            <ul>
+                <li><a href="#none">소프트웨어자산관리사</a></li>
+                <li><a href="#none">전기(산업)기사</a></li>
+                <li><a href="#none">소방(산업)기사</a></li>
+                <li><a href="#none">한국사능력시험</a></li>
+                <li><a href="#none">경제교육지도사</a></li>
+                <li><a href="#none">진로직업체험지도사</a></li>
+            </ul>
+        </div>
+        <div class="w50">
+            <h4><img src="{{ img_url('m/intro/icon_spo.png') }}" alt="경찰간부"> 경찰간부</h4>
+            <ul>
+                <li><a href="#none">경찰간부온라인</a></li>
+            </ul>
+        </div>
+        <div class="w50">
+            <h4><img src="{{ img_url('m/intro/icon_willpass.png') }}" alt="취업"> 취업</h4>
+            <ul>
+                <li><a href="#none">공기업</a></li>
+            </ul>
+        </div>
+        <div class="w50">
+            <h4><img src="{{ img_url('m/intro/icon_lang.png') }}" alt="어학"> 어학</h4>
+            <ul>
+                <li><a href="#none">G-TELP</a></li>
+            </ul>
+        </div>
+        <div class="w50">
+            <h4><img src="{{ img_url('m/intro/icon_njob.gif') }}" alt="N잡"> N잡</h4>
+            <ul>
+                <li><a href="#none">e커머스</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- End Container -->
+@stop

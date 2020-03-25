bc014e6cd0ec9d20ce62deb40d9c2391e7e0941a
조규호 <benoo@willbes.com>
2020-03-25 14:17:34 +0900

    모델 추가

--- application/third_party/PHPStorm_CI_CC.php
+++ application/third_party/PHPStorm_CI_CC.php
@@ -206,6 +206,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @property BaseStatsModel $baseStatsModel                            BaseStats Model Class
  * @property StatsMemberModel $statsMemberModel                            StatsMember  Model Class
  * @property StatsOrderModel $statsOrderModel                            StatsOrder  Model Class
+ * @property StatsBannerModel $statsBannerModel                            StatsBanner  Model Class
+ * @property StatsSearchModel $statsSearchModel                            StatsBanner  Model Class
  ***************** MockTest ****************************************************************************
  * @property MockCommonModel $mockCommonModel                              MockTest MockCommon Model Class
  * @property BaseCodeModel $baseCodeModel                                        MockTest BaseCod Model Class

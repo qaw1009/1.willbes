14de46c979e0b397a860e011d78f8d291843cc4c
최형진 <letsboo@willbes.com>
2020-04-01 11:19:54 +0900

    테스트

--- application/helpers/WB_download_helper.php
+++ application/helpers/WB_download_helper.php
@@ -107,7 +107,7 @@ if (!function_exists('rename_download')) {
 //        header('Content-Disposition: attachment; filename="'.iconv('UTF-8','EUC-KR', $filename).'"');
 //        header('Content-Disposition: attachment; filename="'. iconv('UTF-8', 'EUC-KR', $filename) .'"; filename*=utf-8\'\''. rawurlencode($filename) .';');
 //        header('Content-Disposition: attachment; filename="'. iconv('UTF-8', 'EUC-KR', $filename) .'"' . $add_disposition);
-        header('Content-Disposition: attachment; filename="'.rawurlencode($filename).'"');
+        header('Content-Disposition: attachment; filename="'.$filename.'"');
         header('Expires: 0');
         header('Content-Transfer-Encoding: binary');
         header('Content-Length: '.$filesize);

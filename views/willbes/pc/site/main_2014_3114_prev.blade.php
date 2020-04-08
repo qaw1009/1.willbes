ed64537e69f7b3470806ef61b03dda92438f4945
최형진 <letsboo@willbes.com>
2020-04-08 11:32:17 +0900

    [프론트] 엔잡 메인 - 메인 변경

---
+++ views/willbes/pc/site/main_2014_3114_prev.blade.php
@@ -0,0 +1,67 @@
+@extends('willbes.pc.layouts.master')
+
+@section('content')
+    <!-- Container -->
+    <link href="/public/css/willbes/style_2014_3114.css?ver={{time()}}" rel="stylesheet">
+
+    <div id="Container" class="Container njob NGR c_both">
+        <!-- site nav -->
+        @include('willbes.pc.layouts.partial.site_menu')
+
+        <div class="skybanner">
+            <span><img src="https://static.willbes.net/public/images/promotion/main/3114_sky00.png" alt="시계" ></span>
+            <img src="https://static.willbes.net/public/images/promotion/main/3114_sky01_0316.png" alt="사전예약 신청하기" usemap="#Map3114" border="0">
+            <map name="Map3114" id="Map3114">
+                <area shape="rect" coords="18,313,88,422" href="https://njob.willbes.net/promotion/index/cate/3114/code/1564" target="_blank" alt="김정환" />
+                <area shape="rect" coords="93,312,162,424" href="https://njob.willbes.net/promotion/index/cate/3114/code/1566" target="_blank" alt="김경은" />
+                <area shape="rect" coords="20,428,87,540" href="https://njob.willbes.net/promotion/index/cate/3114/code/1567" target="_blank" alt="정문진" />
+                <area shape="rect" coords="93,427,161,539" href="https://njob.willbes.net/promotion/index/cate/3114/code/1565" target="_blank" alt="황채영" />
+            </map>
+        </div>
+
+        <div class="evtCtnsBox evtTop">
+            <img src="https://static.willbes.net/public/images/promotion/main/3114_standby_top.jpg" alt="1억뷰 N잡">
+        </div>
+
+        <div class="evtCtnsBox evt01">
+            <img src="https://static.willbes.net/public/images/promotion/main/3114_standby_01_0316.jpg" alt="사전수강신청" usemap="#Map3114B" border="0">
+            <map name="Map3114B" id="Map3114B">
+                <area shape="rect" coords="175,1178,362,1350" href="#evt04" alt="김정환" />
+                <area shape="rect" coords="371,1176,557,1348" href="#evt05" alt="김경은" />
+                <area shape="rect" coords="562,1176,747,1348" href="#evt06" alt="황채영" />
+                <area shape="rect" coords="757,1176,947,1348" href="#evt07" alt="정문진" />
+            </map>
+        </div>
+
+        <div class="evtCtnsBox evt02">
+            <img src="https://static.willbes.net/public/images/promotion/main/3114_standby_02.jpg" alt="1억뷰 n잡에서 그 꿈을 응원합니다.">
+        </div>
+
+        <div class="evtCtnsBox evt03">
+            <img src="https://static.willbes.net/public/images/promotion/main/3114_standby_03.jpg" alt="인플루언서 막강라이업">
+        </div>
+
+        <div class="evtCtnsBox evt04" id="evt04">
+            <a href="/promotion/index/cate/3114/code/1564" target="_blank" ><img src="https://static.willbes.net/public/images/promotion/main/3114_standby_04.jpg" alt="김정환"></a>
+        </div>
+
+        <div class="evtCtnsBox evt05" id="evt05">
+            <a href="/promotion/index/cate/3114/code/1566" target="_blank"><img src="https://static.willbes.net/public/images/promotion/main/3114_standby_05.jpg" alt="김경은"></a>
+        </div>
+
+        <div class="evtCtnsBox evt06" id="evt06">
+            <a href="/promotion/index/cate/3114/code/1565" target="_blank"><img src="https://static.willbes.net/public/images/promotion/main/3114_standby_06.jpg" alt="황채영"></a>
+        </div>
+
+        <div class="evtCtnsBox evt07" id="evt07">
+            <a href="/promotion/index/cate/3114/code/1567" target="_blank"><img src="https://static.willbes.net/public/images/promotion/main/3114_standby_07.jpg" alt="정문진"></a>
+        </div>
+
+        <div class="evtCtnsBox evt08">
+            <img src="https://static.willbes.net/public/images/promotion/main/3114_standby_08.jpg" alt="사전 예약시 파격혜택 제공">
+        </div>
+    </div>
+    <!-- End Container -->
+
+    {!! popup('657001', $__cfg['SiteCode'], $__cfg['CateCode']) !!}
+@stop

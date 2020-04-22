197f471c0ff914ff7ec32319b1d0ffd53240b3c0
이윤구 <yoongukk@willbes.com>
2020-04-22 17:25:30 +0900

    5급헌법 + PSAT 1차 이벤트 웹페이지 신규

---
+++ views/willbes/pc/promotion/2005/1617.blade.php
@@ -0,0 +1,138 @@
+@extends('willbes.pc.layouts.master')
+
+@section('content')
+    @include('willbes.pc.layouts.partial.site_menu')
+<!-- content -->
+<!-- Container -->
+<style type="text/css">
+    .subContainer {
+        min-height: auto !important;
+        margin-bottom:0 !important;
+    }
+    .evtContent {
+        position:relative;
+        width:100% !important;
+        min-width:1210px !important;
+        background:#ccc;
+        margin-top:20px !important;
+        padding:0 !important;
+        background:#fff;
+    }
+    .evtCtnsBox {width:100%; text-align:center; min-width:1120px;}
+
+    /************************************************************/
+
+    .wb_top {background:#2C3C51 url(https://static.willbes.net/public/images/promotion/2020/04/1617_top_bg.jpg) no-repeat center top;}
+
+    .menuWarp {position:relative; width:1210px; height:1138px; margin:0 auto;position:relative;}
+    .PeMenu {width:1210px;position:absolute;bottom:0px; left:0px;}
+    .PeMenu li {display:inline; float:left; margin-right:10px}
+    .PeMenu li:last-child {margin:0}
+    .PeMenu li a{display:block; text-align:center}
+    .PeMenu li img:hover {
+                        -webkit-box-shadow: 5px 5px 10px 1px rgba(0,0,0,0.5);
+                        -moz-box-shadow: 5px 5px 10px 1px rgba(0,0,0,0.5);
+                        box-shadow: 5px 5px 10px 1px rgba(0,0,0,0.5);
+                         }
+    .PeMenu ul:hover a:not(:hover){    
+                                   opacity: 0.4; 
+                                   }
+    .PeMenu ul:after {content:""; display:block; clear:both}
+
+    .wb_evt01 {background:#eee;}
+
+</style>
+
+<div class="evtContent NGR" id="evtContainer">
+
+    <div class="evtCtnsBox wb_top" >  
+ 
+        <div class="menuWarp">
+            <div class="PeMenu">
+                <ul>
+                    <li>
+                        <a href="https://gosi.willbes.net/lecture/show/cate/3097/pattern/only/prod-code/160275" target="_blank">
+                            <img src="https://static.willbes.net/public/images/promotion/2020/04/1617_top_01.png" class="off" alt="김유향" />                        
+                        </a>
+                    </li>                  
+                    <li>
+                        <a href="https://gosi.willbes.net/lecture/show/cate/3097/pattern/only/prod-code/160277" target="_blank">
+                            <img src="https://static.willbes.net/public/images/promotion/2020/04/1617_top_02.png" class="off" alt="선동주" />                           
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://gosi.willbes.net/search/result/?cate=3096&=&searchfull_text=%ED%95%A9%EA%B2%A9%EC%9D%B4%EB%B2%A4%ED%8A%B8%EC%9D%B4%EB%82%98%EC%9A%B0" target="_blank">                
+                            <img src="https://static.willbes.net/public/images/promotion/2020/04/1617_top_03.png" class="off" alt="이나우" />                            
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://gosi.willbes.net/search/result/?cate=3096&=&searchfull_text=%ED%95%A9%EA%B2%A9%EC%9D%B4%EB%B2%A4%ED%8A%B8%ED%95%9C%EC%8A%B9%EC%95%84&searchfull_order=" target="_blank">
+                            <img src="https://static.willbes.net/public/images/promotion/2020/04/1617_top_04.png" class="off" alt="한승아" />                            
+                        </a>
+                    </li>                   
+                    <li>
+                        <a href="https://gosi.willbes.net/search/result/?cate=3096&=&searchfull_text=%ED%95%A9%EA%B2%A9%EC%9D%B4%EB%B2%A4%ED%8A%B8%EC%84%9D%EC%B9%98%EC%88%98&searchfull_order=" target="_blank">
+                            <img src="https://static.willbes.net/public/images/promotion/2020/04/1617_top_05.png" class="off" alt="석치수" />                           
+                        </a>
+                    </li>                  
+                    <li>
+                        <a href="https://gosi.willbes.net/search/result/?cate=3096&=&searchfull_text=%ED%95%A9%EA%B2%A9%EC%9D%B4%EB%B2%A4%ED%8A%B8%EB%B0%95%EC%A4%80%EB%B2%94&searchfull_order=" target="_blank">
+                            <img src="https://static.willbes.net/public/images/promotion/2020/04/1617_top_06.png" class="off" alt="박준범" />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="evtCtnsBox wb_evt01" >  
+        <img src="https://static.willbes.net/public/images/promotion/2020/04/1617_01.jpg" alt="" usemap="#Map1617a" border="0"  />
+        <map name="Map1617a" id="Map1617a">
+            <area shape="rect" coords="898,100,1065,144" href="https://gosi.willbes.net/m/lecture/index/cate/3097/pattern/only?search_order=course&course_idx=1132" target="_blank" />
+            <area shape="rect" coords="898,149,1066,194" href="https://gosi.willbes.net/lecture/index/cate/3097/pattern/only?search_order=course&course_idx=1132" target="_blank" />
+            <area shape="rect" coords="898,279,1066,323" href="https://gosi.willbes.net/m/lecture/index/cate/3096/pattern/only?search_order=course&course_idx=1287" target="_blank" />
+            <area shape="rect" coords="898,331,1065,373" href="https://gosi.willbes.net/lecture/index/cate/3096/pattern/only?search_order=course&course_idx=1287" target="_blank" />
+            <area shape="rect" coords="898,460,1065,504" href="https://gosi.willbes.net/m/lecture/index/cate/3096/pattern/only?search_order=course&course_idx=1286" target="_blank" />
+            <area shape="rect" coords="898,510,1065,553" href="https://gosi.willbes.net/lecture/index/cate/3096/pattern/only?search_order=course&course_idx=1286" target="_blank" />
+            <area shape="rect" coords="899,640,1065,683" href="https://gosi.willbes.net/m/lecture/index/cate/3096/pattern/only?search_order=course&course_idx=1289" target="_blank" />
+            <area shape="rect" coords="897,690,1065,734" href="https://gosi.willbes.net/lecture/index/cate/3096/pattern/only?search_order=course&course_idx=1289" target="_blank" />
+            <area shape="rect" coords="899,820,1063,864" href="https://gosi.willbes.net/m/lecture/index/cate/3096/pattern/only?search_order=course&course_idx=1286" target="_blank" />
+            <area shape="rect" coords="898,870,1064,912" href="https://gosi.willbes.net/lecture/index/cate/3096/pattern/only?search_order=course&course_idx=1286" target="_blank" />
+            <area shape="rect" coords="898,1000,1065,1044" href="https://gosi.willbes.net/m/lecture/index/cate/3096/pattern/only?search_order=course&course_idx=1204" target="_blank" />
+            <area shape="rect" coords="898,1051,1065,1094" href="https://gosi.willbes.net/lecture/index/cate/3096/pattern/only?search_order=course&course_idx=1204" target="_blank" />
+            <area shape="rect" coords="898,1180,1065,1223" href="https://gosi.willbes.net/m/lecture/index/cate/3096/pattern/only?search_order=course&course_idx=1132&school_year=2020" target="_blank" />
+            <area shape="rect" coords="898,1230,1064,1273" href="https://gosi.willbes.net/lecture/index/cate/3096/pattern/only?search_order=course&course_idx=1132&school_year=2020" target="_blank" />
+        </map>  
+    </div>
+ 
+
+</div>
+<!-- End Container -->
+
+   <script type="text/javascript">
+        $(document).ready(function(){
+            $('.tabs').each(function(){
+                var $active, $content, $links = $(this).find('a');
+                $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+                $active.addClass('active');
+            
+                $content = $($active[0].hash);
+            
+                $links.not($active).each(function () {
+                $(this.hash).hide()});
+            
+                // Bind the click event handler
+                $(this).on('click', 'a', function(e){
+                $active.removeClass('active');
+                $content.hide();
+            
+                $active = $(this);
+                $content = $(this.hash);
+            
+                $active.addClass('active');
+                $content.show();
+            
+                e.preventDefault()})})}
+        );
+    </script>
+@stop

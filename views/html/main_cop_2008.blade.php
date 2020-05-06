2c7c00d64d3e0d09da180b63da0923b64e9c0a72
김창용 <winmir@willbes.com>
2020-05-06 10:07:32 +0900

    경찰간부온오프메인

--- views/html/main_cop_2008.blade.php
+++ views/html/main_cop_2008.blade.php
@@ -296,33 +296,34 @@
         </div>
     </div>
 
-    {{--
+    
     <div id="QuickMenu" class="MainQuickMenu">
         <ul>
             <li>
-                <div class="QuickDdayBox">
-                    <div class="q_tit">3차 필기시험</div>
-                    <div class="q_day">2018.12.12</div>
-                    <div class="q_dday NSK-Blac">D-5</div>
-                </div>
-            </li>
-            <li>   
-                <div class="QuickSlider">      
+                <div class="QuickSlider ">
                     <div class="sliderNum">
-                        <div><a href="http://www.willbescop.net/event/movie/event.html?event_cd=On_170911_popup" target="_blank"><img src="{{ img_url('cop/quick/quick_190108.jpg') }}" alt="배너명"></a></div>
-                        <div><a href="http://www.willbescop.net/event/arm_event.html?event_cd=On_leaveArmy02_2018&topMenuType=O&EVENT_NO=53" target="_blank"><img src="{{ img_url('cop/quick/quick_190109.jpg') }}" alt="배너명"></a></div>
+                        <div class="QuickDdayBox">
+                            <div class="q_tit">3차 필기시험</div>
+                            <div class="q_day">2018.12.12</div>
+                            <div class="q_dday NSK-Blac">D-5</div>
+                        </div>
+                        <div class="QuickDdayBox">
+                            <div class="q_tit">1차 공무원</div>
+                            <div class="q_day">2019.04.05</div>
+                            <div class="q_dday NSK-Blac">D-10</div>
+                        </div>
                     </div>
                 </div>
             </li>
             <li>
-                <a href="http://www.willbescop.net/movie/event.html?event_cd=Off_181129_p&topMenuType=F" target="_blank"><img src="{{ img_url('cop/quick/quick_190110.jpg') }}" alt="배너명"></a>
+                <a href="https://spo.willbes.net/support/qna/index?s_cate_code=3100" target="_blank"><img src="https://static.willbes.net/public/images/promotion/main/2000_sky03.jpg" alt="배너명"></a>
             </li>
             <li>
-                <a href="http://www.willbescop.net/movie/event.html?event_cd=Off_181129_p&topMenuType=F" target="_blank"><img src="{{ img_url('cop/quick/quick_talk.jpg') }}" alt="배너명"></a>
+                <a href="https://spo.willbes.net/support/notice/index/cate/3100" target="_blank"><img src="https://static.willbes.net/public/images/promotion/main/2000_sky04.jpg" alt="배너명"></a>
             </li>
         </ul>
     </div>
-    --}}
+    
 </div>
 <!-- End Container -->
 

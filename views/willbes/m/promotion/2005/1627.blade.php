cbac312706900bdfe4270fec5fc821c30bfe2a16
김창용 <winmir@willbes.com>
2020-05-13 17:47:27 +0900

    고등고시 합격에측 서비스

--- views/willbes/m/promotion/2005/1627.blade.php
+++ views/willbes/m/promotion/2005/1627.blade.php
@@ -47,69 +47,12 @@
     .btns a.btn2:hover {background:#fff; color:#464646 !important}
 
 
-    .sectin1_box {
-        padding:180px 0 100px; width:100%; text-align:center;
-        -webkit-animation: color-change-5x 8s linear infinite alternate both;
-	        animation: color-change-5x 8s linear infinite alternate both;
-    }
-    @@-webkit-keyframes color-change-5x {
-            0% {
-            background: #0c74ae;
-            }
-            50% {
-            background: #83cff9;
-            }
-            100% {
-            background: #0c74ae;
-            }
-        }
-        @@keyframes color-change-5x {
-            0% {
-            background: #0c74ae;
-            }
-            50% {
-            background: #83cff9;
-            }
-            100% {
-            background: #0c74ae;
-            }
-        }
-    .sectin1_box div {font-size:100px; font-weight:bold; color:#fff; 
-            -webkit-animation: text-pop-up-top 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
-            animation: text-pop-up-top 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;}
-    .sectin1_box div span {font-size:50px; display:block; margin-bottom:10px}
-    @@-webkit-keyframes text-pop-up-top {
-        0% {
-            -webkit-transform: translateY(0);
-                    transform: translateY(0);
-            -webkit-transform-origin: 50% 50%;
-                    transform-origin: 50% 50%;
-            text-shadow: none;
-        }
-        100% {
-            -webkit-transform: translateY(-50px);
-                    transform: translateY(-50px);
-            -webkit-transform-origin: 50% 50%;
-                    transform-origin: 50% 50%;
-            text-shadow: 0 1px 0 #cccccc, 0 2px 0 #cccccc, 0 3px 0 #cccccc, 0 4px 0 #cccccc, 0 5px 0 #cccccc, 0 6px 0 #cccccc, 0 7px 0 #cccccc, 0 8px 0 #cccccc, 0 9px 0 #cccccc, 0 50px 30px rgba(0, 0, 0, 0.3);
-        }
-    }
-    @@keyframes text-pop-up-top {
-        0% {
-            -webkit-transform: translateY(0);
-                    transform: translateY(0);
-            -webkit-transform-origin: 50% 50%;
-                    transform-origin: 50% 50%;
-            text-shadow: none;
-        }
-        100% {
-            -webkit-transform: translateY(-50px);
-                    transform: translateY(-50px);
-            -webkit-transform-origin: 50% 50%;
-                    transform-origin: 50% 50%;
-            text-shadow: 0 1px 0 #cccccc, 0 2px 0 #cccccc, 0 3px 0 #cccccc, 0 4px 0 #cccccc, 0 5px 0 #cccccc, 0 6px 0 #cccccc, 0 7px 0 #cccccc, 0 8px 0 #cccccc, 0 9px 0 #cccccc, 0 50px 30px rgba(0, 0, 0, 0.3);
-        }
-    }
+    .sectin1_box {}
+    .sectin2_box {background:#000; color:#fff; padding:50px 0;}
+    .sectin2_box div {font-size:20px; margin: 0 20px 30px}
+    .sectin2_box ul {margin: 0 20px 30px}
+    .sectin2_box li {margin-bottom:5px; font-size:14px}
+    .sectin2_box li:last-child {margin-top:15px}
 
     .txtinfo {border:1px solid #464646; padding:20px; height:150px; overflow-y:scroll}
     .txtinfo li {margin-left:20px; list-style-type: decimal;}
@@ -158,9 +101,12 @@
 
 <div id="Container" class="Container c_both">            
     <div class="evtContent NSK">
-        <div class="sectin1_box NSK-Black">
-            <div><span>2020년 1차 고등고시 시험</span>
-            합격예측 인증</div>
+        <div class="sectin1_box">
+            {{--16일 20시 까지 보여지는 이미지--}}
+            <img src="https://static.willbes.net/public/images/promotion/2020/05/1627m_top.jpg" alt="#">
+            <!--16일 20시 이후 보여지는 이미지
+            <img src="https://static.willbes.net/public/images/promotion/2020/05/1627m_top_01.jpg" alt="#">
+            -->
         </div>
         <!--sectin1_box//-->
         @include('willbes.pc.predict2.1627_promotion_partial')
@@ -606,6 +552,23 @@
 
             </div> 
         </div>--}}
+
+        <div class="sectin2_box">
+            <div class="NSK-Black">이벤트 유의사항</div>
+            <ul>
+                <li>- 본 이벤트는 5급공채(일반행정·재경), 국립외교원 시험 응시자만 참여 가능합니다.</li>
+                <li>- 설문조사 + 정답 또는 점수까지 입력이 완료된 이용자에 한하여 지급 및 추첨합니다.</li>
+                <li>- 이벤트 2의 경우 중복당첨되지 않습니다.</li>
+                <li>- 개인정보 수집 및 이용에 동의하지 않으신 분은 이벤트 참여가 불가능합니다.</li>
+                <li>- 경품은 모바일 쿠폰(기프티콘) 형태로 발송되며, 발송은 1회로 제한합니다.</li>
+                <li>- 기프티콘은 이벤트 참여시 기재된 전화번호로 발송됩니다.</li>
+                <li>- 휴대폰번호 오류 시 기프티콘은 재발송 되지 않습니다.</li>
+                <li>- 휴대전화 단말기의 MMS 수신상태가 양호하지 않은 경우, 기프티콘 수신이 불가할 수 있습니다.</li>
+                <li>- 이벤트 참여 전 회원정보(휴대폰 번호)를 정확히 수정해주시기 바랍니다. </li>
+                <li>- 기프티콘을 수신한 이후 개인사정에 의해 유효기간이 지나 사용하지 못한 경우 사용하지 않은 혜택에 대해서는 별도로 보상하지 않습니다.</li>
+                <li>※ 유의사항을 읽지 않고 발생한 모든 상황에 대해서 한림법학원은 책임지지 않습니다.</li>
+            </ul>
+        </div>
     </div>
 
 </div>

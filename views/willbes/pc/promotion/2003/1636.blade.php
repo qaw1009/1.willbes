9bc11989a227e2e921f1bde188fb6c80d99ca6cf
김창용 <winmir@willbes.com>
2020-05-13 13:45:08 +0900

    공무원 법원직 합격스토리

--- views/willbes/pc/promotion/2003/1636.blade.php
+++ views/willbes/pc/promotion/2003/1636.blade.php
@@ -21,24 +21,29 @@
 
         .sky_banner {position:fixed;top:250px;right:10px;}
 
-        .evt_top {background:#111016 url(https://static.willbes.net/public/images/promotion/2020/03/1553_top_bg.jpg) no-repeat center top;}
+        .evt_top {background:#111016 url(https://static.willbes.net/public/images/promotion/2020/05/1636_top_bg.jpg) no-repeat center top;}
 
-        .evt_01 {background:#001751 url(https://static.willbes.net/public/images/promotion/2020/03/1553_01_bg.jpg) no-repeat center top;}
+        .evt_01 {background:url(https://static.willbes.net/public/images/promotion/2020/05/1636_01_bg.jpg) no-repeat center top;}
 
-        .evt_02 {background:#fff url(https://static.willbes.net/public/images/promotion/2020/03/1553_02_bg.jpg) no-repeat center top;}
+        .evt_02 {background:#fff url(https://static.willbes.net/public/images/promotion/2020/05/1636_02_bg.jpg) no-repeat center top;}
 
         .evt_03 {background:#e8ebf2}
 
-        .evt_04 {background:#fff}
-        .evt_04_hover {width:980px; height:630px; margin:0 auto}  
+        .evt_04 {background:#fff; padding-bottom:150px}
+        .evt_04_list {width:980px; margin:0 auto;}  
         .evt_04_list li {float:left;padding-right:35px;}
+        .evt_04_list li .on {display:none}
+        .evt_04_list li .off {display:block}
+        .evt_04_list li:hover .on {display:block}
+        .evt_04_list li:hover .off {display:none}
         .evt_04_list li:last-child{padding-right:0;}
+        .evt_04_list:after {content:""; display:block; clear:both}
 
         .evt_05 {background:#002b97}
 
-        .evt_06 {background:#e2e2e2 url(https://static.willbes.net/public/images/promotion/2020/03/1553_06_bg.jpg) no-repeat center top;}
+        .evt_06 {background:#e2e2e2 url(https://static.willbes.net/public/images/promotion/2020/05/1636_06_bg.jpg) no-repeat center top;}
 
-        .evt_07 {background:#000 url(https://static.willbes.net/public/images/promotion/2020/03/1553_07_bg.jpg) no-repeat center top;}   
+        .evt_07 {background:#000 url(https://static.willbes.net/public/images/promotion/2020/05/1636_07_bg.jpg) no-repeat center top;}   
 
         </style>
 
@@ -46,7 +51,7 @@
     <div class="p_re evtContent NSK" id="evtContainer">   
 
         <div class="sky_banner">
-            <img src="https://static.willbes.net/public/images/promotion/2020/03/1553_sky.png" alt="스카이 베너" usemap="#Map" border="0" />
+            <img src="https://static.willbes.net/public/images/promotion/2020/05/1636_sky.png" alt="스카이 베너" usemap="#Map" border="0" />
             <map name="Map" id="Map">
                 <area shape="rect" coords="13,112,112,191" href="#tab01" />
                 <area shape="rect" coords="11,203,112,276" href="#tab02" />
@@ -55,54 +60,44 @@
         </div>
 
         <div class="evtCtnsBox evt_top" >
-            <img src="https://static.willbes.net/public/images/promotion/2020/03/1553_top.jpg" alt="2020 국가직 파이널" />
+            <img src="https://static.willbes.net/public/images/promotion/2020/05/1636_top.jpg" alt="2020 국가직 파이널" />
         </div>     
 
         <div class="evtCtnsBox evt_01" >
-            <img src="https://static.willbes.net/public/images/promotion/2020/03/1553_01.jpg" alt="마지막 전략" />
+            <img src="https://static.willbes.net/public/images/promotion/2020/05/1636_01.jpg" alt="마지막 전략" />
         </div>
 
         <div class="evtCtnsBox evt_02" id="tab01" >
-            <img src="https://static.willbes.net/public/images/promotion/2020/03/1553_02.jpg" alt="실전동형모의고사" />
+            <img src="https://static.willbes.net/public/images/promotion/2020/05/1636_02.jpg" alt="실전동형모의고사" />
         </div>
 
         <div class="evtCtnsBox evt_03" >
-            <img src="https://static.willbes.net/public/images/promotion/2020/03/1553_03.jpg" alt="선택형 패키지 신청하기" usemap="#Map1553b" border="0" />
+            <img src="https://static.willbes.net/public/images/promotion/2020/05/1636_03.jpg" alt="선택형 패키지 신청하기" usemap="#Map1553b" border="0" />
             <map name="Map1553b" id="Map1553b">
                 <area shape="rect" coords="125,699,995,863" href="https://pass.willbes.net/package/show/cate/3019/pack/648002/prod-code/162499" target="_blank" />
             </map>
         </div>
 
         <div class="evtCtnsBox evt_04" id="tab02" >
-            <img src="https://static.willbes.net/public/images/promotion/2020/03/1553_04.jpg" alt="국.영.사 특강" />
-            <div class="evt_04_hover">
-                <div class="evt_04_list">
-                    <ul>
-                        <li>
-                            <img src="https://static.willbes.net/public/images/promotion/2020/03/1553_04_01.png"
-                              onmouseover="this.src='https://static.willbes.net/public/images/promotion/2020/03/1553_04_01_on.png'"
-                              onMouseOut="this.src='https://static.willbes.net/public/images/promotion/2020/03/1553_04_01.png'" 
-                              onMouseDown="this.src='https://static.willbes.net/public/images/promotion/2020/03/1553_04_01_on.png'" alt=""  />                         
-                        </li>            
-                        <li>
-                            <img src="https://static.willbes.net/public/images/promotion/2020/03/1553_04_02.png"
-                              onmouseover="this.src='https://static.willbes.net/public/images/promotion/2020/03/1553_04_02_on.png'"
-                              onMouseOut="this.src='https://static.willbes.net/public/images/promotion/2020/03/1553_04_02.png'" 
-                              onMouseDown="this.src='https://static.willbes.net/public/images/promotion/2020/03/1553_04_02_on.png'" alt=""  />                         
-                        </li>            
-                        <li>
-                            <img src="https://static.willbes.net/public/images/promotion/2020/03/1553_04_03.png"
-                              onmouseover="this.src='https://static.willbes.net/public/images/promotion/2020/03/1553_04_03_on.png'"
-                              onMouseOut="this.src='https://static.willbes.net/public/images/promotion/2020/03/1553_04_03.png'" 
-                              onMouseDown="this.src='https://static.willbes.net/public/images/promotion/2020/03/1553_04_03_on.png'" alt=""  />                         
-                        </li>                     
-                    </ul>
-                </div>
-            </div>
+            <img src="https://static.willbes.net/public/images/promotion/2020/05/1636_04.jpg" alt="국.영.사 특강" />
+            <ul class="evt_04_list">
+                <li>
+                    <img src="https://static.willbes.net/public/images/promotion/2020/05/1636_04_01.png" title="국어" class="off"/>
+                    <img src="https://static.willbes.net/public/images/promotion/2020/05/1636_04_01_on.png" title="국어" class="on"/>                      
+                </li>            
+                <li>
+                    <img src="https://static.willbes.net/public/images/promotion/2020/05/1636_04_02.png" title="영어" class="off"/>
+                    <img src="https://static.willbes.net/public/images/promotion/2020/05/1636_04_02_on.png" title="영어" class="on"/>                       
+                </li>            
+                <li>
+                    <img src="https://static.willbes.net/public/images/promotion/2020/05/1636_04_03.png" title="한국사" class="off"/>
+                    <img src="https://static.willbes.net/public/images/promotion/2020/05/1636_04_03_on.png" title="한국사" class="on"/>                       
+                </li>                     
+            </ul>
         </div>
 
         <div class="evtCtnsBox evt_05" >
-            <img src="https://static.willbes.net/public/images/promotion/2020/03/1553_05.jpg" alt="수강 신청" usemap="#Map1553c" border="0" />
+            <img src="https://static.willbes.net/public/images/promotion/2020/05/1636_05.jpg" alt="수강 신청" usemap="#Map1553c" border="0" />
             <map name="Map1553c" id="Map1553c">
                 <area shape="rect" coords="783,241,996,452" href="https://pass.willbes.net/package/show/cate/3019/pack/648001/prod-code/162256" target="_blank" />
                 <area shape="rect" coords="194,788,324,822" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/161947" target="_blank" />
@@ -112,11 +107,11 @@
         </div>
 
         <div class="evtCtnsBox evt_06" id="tab03" >
-            <img src="https://static.willbes.net/public/images/promotion/2020/03/1553_06.jpg" alt="한덕현 영어 커밍쑨" />
+            <img src="https://static.willbes.net/public/images/promotion/2020/05/1636_06.jpg" alt="한덕현 영어 커밍쑨" />
         </div>
 
         <div class="evtCtnsBox evt_07" >
-            <img src="https://static.willbes.net/public/images/promotion/2020/03/1553_07.jpg" alt="합격을 확신" />
+            <img src="https://static.willbes.net/public/images/promotion/2020/05/1636_07.jpg" alt="합격을 확신" />
         </div>        
 
     </div>

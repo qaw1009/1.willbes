04f76650cd4df3f0ba2618351683d54e561a0ea7
이윤구 <yoongukk@willbes.com>
2020-05-14 16:46:22 +0900

    PC_자격증_메인 퀵배너 수정

--- views/willbes/pc/site/main_partial/quick_menu_2006.blade.php
+++ views/willbes/pc/site/main_partial/quick_menu_2006.blade.php
@@ -71,5 +71,6 @@
 <ul>
     <li><a href="{{ site_url('/pass/offinfo/boardInfo/index/109?on_off_link_cate_code=' . $__cfg['CateCode'] . '&s_cate_code_disabled=Y') }}">강의 계획서</a></li>
     <li><a href="{{ site_url('/pass/offinfo/boardInfo/index/80?on_off_link_cate_code=' . $__cfg['CateCode'] . '&s_cate_code_disabled=Y') }}">강의 시간표</a></li>
+    <li><a href="{{ site_url('/pass/offinfo/boardInfo/index/82?on_off_link_cate_code=' . $__cfg['CateCode'] . '&s_cate_code_disabled=Y') }}">강의실 배정표</a></li>
     <li><a href="{{ site_url('/pass/offinfo/boardInfo/index/110?on_off_link_cate_code=' . $__cfg['CateCode'] . '&s_cate_code_disabled=Y') }}">강의자료실</a></li>
 </ul>

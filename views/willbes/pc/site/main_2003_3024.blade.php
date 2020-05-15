d5b6244abda2f128ef31ebf3e0ef7e854f8cc24a
김창용 <winmir@willbes.com>
2020-05-15 13:03:56 +0900

    군무원 css

--- views/willbes/pc/site/main_2003_3024.blade.php
+++ views/willbes/pc/site/main_2003_3024.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <!-- Container -->
-    <div id="Container" class="Container gp NGR c_both">
+    <div id="Container" class="Container gp3024 NGR c_both">
         <!-- site nav -->
         @include('willbes.pc.layouts.partial.site_menu')
 

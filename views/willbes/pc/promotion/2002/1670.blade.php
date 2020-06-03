445928250f9399bfa506bb5ba4154ba5873e6147
이윤구 <yoongukk@willbes.com>
2020-06-03 16:22:49 +0900

    형사법 url 남기기 영역 수정

--- views/willbes/pc/promotion/2002/1670.blade.php
+++ views/willbes/pc/promotion/2002/1670.blade.php
@@ -107,22 +107,10 @@
             </div>
 
             <!-- sns 및 url 남기기 영역 -->
-            {{-- 하단 카페 링크 사용여부 --}}
-            <iframe frameborder="0" scrolling="no" width="100%" onload="resizeIframe(this)" src="{{front_url('/promotion/frameCommentList/713002?bottom_cafe_type='.(empty($bottom_cafe_type) === true ? 'Y' : $bottom_cafe_type).'&'.$arr_base['frame_params']).'&give_timing='.(empty($arr_promotion_params['give_timing']) === false ? $arr_promotion_params['give_timing'] : '' )}}"></iframe>
-            <div id="NOTICEPASS" class="willbes-Layer-Black"></div>
-
-            <script type="text/javascript">
-                var first_height = '';
-                function resizeIframe(iframe) {
-                    if (first_height == '') {
-                        iframe.height = (iframe.contentWindow.document.body.scrollHeight + 15) + "px";
-                        if (iframe.contentWindow.document.body.scrollHeight <= 100) {
-                            iframe.height = '633px';    //강제초기화
-                        }
-                    }
-                    first_height = iframe.height;
-                }
-            </script>
+            {{--홍보url--}}
+            @if( empty($data['data_option_ccd']) === false && array_key_exists($arr_base['option_ccd']['comment_list'], $data['data_option_ccd']) === true && array_key_exists($arr_base['comment_use_area']['event'], $data['data_comment_use_area']) === true)
+                @include('willbes.pc.promotion.show_comment_list_url_partial')
+            @endif
 
             <div class="evtCtnsBox evt03" id="apply">
                 <img src="https://static.willbes.net/public/images/promotion/2020/06/1670_03.jpg" title="신광은 형사법 무료특강">

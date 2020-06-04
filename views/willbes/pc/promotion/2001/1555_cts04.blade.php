7ba5604bff594a644e02b4064b991cfc9d2d5508
김창용 <winmir@willbes.com>
2020-06-04 13:38:47 +0900

    경찰 합격예측 4번째탭, 한덕현 반반 6월

---
+++ views/willbes/pc/promotion/2001/1555_cts04.blade.php
@@ -0,0 +1,94 @@
+    <style type="text/css">  
+        .cts04_top {background:url(https://static.willbes.net/public/images/promotion/2019/10/1361_cts04_top_bg.jpg) no-repeat center top;}
+        .cts04_top ul {
+            position:absolute; top:915px; left:50%; width:890px; margin-left:-445px;
+        }
+        .cts04_top li {display:inline; float:left; width:25%;}
+        .cts04_top li a {
+            display:block; text-align:center; padding:15px 0; color:#fff; font-size:20px; line-height:1.3;
+            border:1px solid #fdda80; border-radius:15px; margin-right:10px;
+        }
+        .cts04_top li:last-child a {margin:0} 
+        .cts04_top li a span {color:#fdda80}
+        .cts04_top li a:hover span {color:#0f0704}
+        .cts04_top li a:hover {background:#fdda80; color:#0f0704}
+
+        .cts04_01 {background:#ffe040;} 
+        .cts04_02 {background:#555}
+        .cts04_03 {background:#484c57 url(https://static.willbes.net/public/images/promotion/2019/08/1361_cts04_03_bg.jpg) no-repeat center top;} 
+        .cts04_04 {background:#fffcdf}
+        .cts04_05 {background:#e8e8e8}
+        .cts04_06 {background:#373737}
+        .cts04_07 {background:#fff}
+        .cts04_08 {background:#e9e7e8}
+
+        .cts04_talkshow{background:url(https://static.willbes.net/public/images/promotion/2019/10/1361_cts04_talkshow_bg.jpg) no-repeat center top;}
+    </style>
+    <div>
+        <div class="evtCtnsBox cts04_talkshow">
+            <img src="https://static.willbes.net/public/images/promotion/2019/10/1361_cts04_talkshow.jpg" alt="신광은 토크쇼">
+        </div>
+        <div class="evtCtnsBox cts04_top">
+            <img src="https://static.willbes.net/public/images/promotion/2019/10/1361_cts04_top.jpg" alt="3법 면접 무료특강">
+        </div>
+        <div class="evtCtnsBox cts04_01">
+            <img src="https://static.willbes.net/public/images/promotion/2019/10/1361_cts04_01.jpg" alt="면접합격 스테이션">
+        </div>
+        <div class="evtCtnsBox cts04_02">
+            <img src="https://static.willbes.net/public/images/promotion/2019/10/1361_cts04_02.jpg" alt="유의사항">
+        </div>
+        {{--
+        <div class="evtCtnsBox cts04_top">             
+            <img src="https://static.willbes.net/public/images/promotion/2019/08/1361_cts04_top.jpg" alt="면접혁명">
+            <ul class="NGEB">
+                <li><a href="#cts04_02"><span>설명회&무료특강</span><br>신청하기</a></li>
+                <li><a href="#cts04_05"><span>인.적성검사</span><br>신청하기</a></li>
+                <li><a href="#cts04_06"><span>사전조사서</span><br>신청하기</a></li>
+                <li><a href="#cts04_07"><span>면접캠프</span><br>신청하기</a></li>
+            </ul>
+        </div>
+
+        <div class="evtCtnsBox cts04_01">
+            <img src="https://static.willbes.net/public/images/promotion/2019/08/1361_cts04_01.jpg" alt="이제 여러분이 주인공입니다.">
+        </div>
+
+        <div class="evtCtnsBox cts04_02" id="cts04_02">
+            <img src="https://static.willbes.net/public/images/promotion/2019/08/1361_cts04_02A.jpg" alt="사전특강 및 설명회 신청" usemap="#Map1361_cts04_02" border="0">
+            <map name="Map1361_cts04_02" id="Map1361_cts04_02">
+              <area shape="rect" coords="339,779,783,854" href="https://police.willbes.net/pass/event/show/ongoing?event_idx=423" target="_blank" alt="사전특강&amp;설명회신청하기" />
+              <area shape="rect" coords="639,1625,733,1659" href="https://police.willbes.net/pass/campus/show/code/605005" target="_blank" alt="인천캠퍼스" />
+              <area shape="rect" coords="639,1686,735,1719" href="https://police.willbes.net/pass/support/notice/show?board_idx=236556&amp;s_cate_code=&amp;s_campus=&amp;s_keyword=&amp;prof_idx=&amp;subject_idx=&amp;view_type=&amp;page=1" target="_blank" alt="광주캠퍼스" />
+              <area shape="rect" coords="639,1746,733,1779" href="https://police.willbes.net/pass/support/notice/show?board_idx=236040" target="_blank" alt="부산캠퍼스" />
+              <area shape="rect" coords="640,1804,732,1841" href="https://police.willbes.net/pass/frame/notice/show?board_idx=236522" target="_blank" alt="대구캠퍼스" />
+              <area shape="rect" coords="639,1863,734,1902" href="https://police.willbes.net/pass/frame/notice/show?board_idx=236579" target="_blank" alt="전북캠퍼스" />              
+            </map>
+        </div>
+
+        <div class="evtCtnsBox cts04_03">
+            <img src="https://static.willbes.net/public/images/promotion/2019/08/1361_cts04_03.jpg" alt="면접캠프 교수진">
+        </div>
+
+        <div class="evtCtnsBox cts04_04">
+            <img src="https://static.willbes.net/public/images/promotion/2019/08/1361_cts04_04.jpg" alt="면접캠프 교수진 소개">
+        </div>
+
+        <div class="evtCtnsBox cts04_05" id="cts04_05">
+            <img src="https://static.willbes.net/public/images/promotion/2019/08/1361_cts04_05.jpg" alt="인적성 검사" usemap="#Map1361_cts04_05" border="0">
+        </div>
+
+        <div class="evtCtnsBox cts04_06" id="cts04_06">
+            <img src="https://static.willbes.net/public/images/promotion/2019/08/1361_cts04_06.jpg" alt="사전조사서 특강 및 첨삭">
+        </div>
+
+        <div class="evtCtnsBox cts04_07" id="cts04_07">
+            <img src="https://static.willbes.net/public/images/promotion/2019/08/1361_cts04_07.gif" alt="면접캠프 일정 안내" usemap="#Map1361_cts04_07" border="0">
+            <map name="Map1361_cts04_07" id="Map1361_cts04_07">
+                <area shape="rect" coords="861,406,1054,507" href="https://police.willbes.net/pass/offLecture/index/type/interview?cate_code=3010&amp;campus_ccd=605001&amp;course_idx=1047&amp;subject_idx=1064" target="_blank" alt="면접수강신청" />
+            </map>
+        </div>
+
+        <div class="evtCtnsBox cts04_08">
+            <img src="https://static.willbes.net/public/images/promotion/2019/08/1361_cts04_08.jpg" alt="최종합격을 위한 차별화전략">
+        </div>
+        --}}
+	</div>

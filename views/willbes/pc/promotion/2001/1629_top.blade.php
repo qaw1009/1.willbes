3de171955e18969a9525985ff364c1dbaaad4b17
최현탁 <cht@willbes.com>
2020-06-04 12:49:45 +0900

    프론트 합격예측 본서비스 관련 조건 추가

--- views/willbes/pc/promotion/2001/1629_top.blade.php
+++ views/willbes/pc/promotion/2001/1629_top.blade.php
@@ -128,9 +128,9 @@
 
     /*서비스이용현황 */
     $(document).ready(function() {
-        @if(date('YmdHi') >= '202009061500' && ENVIRONMENT == 'production')
+        @if(date('YmdHi') >= '202006050000')
             alert('2020년 1차 합격예측 풀서비스는 종료 되었습니다.');
-            location.href = '{{ site_url('/') }}';
+            location.href = '{{ front_url('/') }}';
         @endif
 
         // 상단 합격안정권 예측 데이터 조회

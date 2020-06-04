457eaa4b4db1ddbbac67bbfa31006f2f69d8e3f4
김창용 <winmir@willbes.com>
2020-06-04 13:05:28 +0900

    경찰 합격예측

--- views/willbes/pc/promotion/2001/1555.blade.php
+++ views/willbes/pc/promotion/2001/1555.blade.php
@@ -342,10 +342,16 @@
                 @include('willbes.pc.promotion.2001.1555_cts02')
             </div>
         @endif
-        <div id="tab03" class="comingsoon">
-            <img src="https://static.willbes.net/public/images/promotion/2020/03/1555_comingsoon.jpg" alt="coming soon">
-        </div>
 
+        @if(time() < strtotime('202006041140'))
+            <div id="tab03" class="comingsoon">
+                <img src="https://static.willbes.net/public/images/promotion/2020/03/1555_comingsoon.jpg" alt="coming soon">
+            </div>
+        @else
+            <div id="tab03">
+                @include('willbes.pc.promotion.2001.1555_cts03')
+            </div>
+        @endif
         <div id="tab04" class="comingsoon">
             <img src="https://static.willbes.net/public/images/promotion/2020/03/1555_comingsoon.jpg" alt="coming soon">
         </div>

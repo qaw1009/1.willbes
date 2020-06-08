e20cd8d95c9f56794bcaffcb38b4185a18d3d614
이윤구 <yoongukk@willbes.com>
2020-06-08 10:53:32 +0900

    온라인 공무원 법원직 수정

--- views/willbes/pc/site/main_2003_3035.blade.php
+++ views/willbes/pc/site/main_2003_3035.blade.php
@@ -75,7 +75,7 @@
                 <ul class="tipGo NSK">
                     <li><a href="{{ site_url('/promotion/index/cate/3035/code/1485') }}" target="_blank">강좌 바로가기</a></li>
                     <li><a href="{{ site_url('/promotion/index/cate/3035/code/1585') }}" target="_blank">강좌 바로가기</a></li>
-                    <li><a class="prepare" href="#none">준비중</a></li>
+                    <li><a href="{{ site_url('/promotion/index/cate/3035/code/1653') }}" target="_blank">강좌 바로가기</a></li>
                     <li><a class="prepare" href="#none">준비중</a></li>
                     <li><a class="prepare" href="#none">준비중</a></li>
                     <li><a class="prepare" href="#none">준비중</a></li>

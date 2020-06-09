351a6281a724fa0105a10debdccc2e02938fce77
김창용 <winmir@willbes.com>
2020-06-09 11:32:13 +0900

    경찰 최상단 배너

--- views/willbes/pc/layouts/topbnr.blade.php
+++ views/willbes/pc/layouts/topbnr.blade.php
@@ -1,6 +1,5 @@
 @if($__cfg['SiteCode'] == '2001')
-    {{-- 경찰온라인 사이트일 경우만 적용 --}}
-    {{--
+    {{-- 경찰온라인 사이트일 경우만 적용
     <div id="topBannerLayer">
         <div class="topBanner">
             <!--a id="show_topBanner" href="javascript:showOrHidefigure(0);"-->
@@ -17,7 +16,6 @@
             </div>
         </div>
     </div>
-    --}}
 
     <script type='text/javascript'>
         function showOrHidefigure(id_dummy) {
@@ -30,12 +28,13 @@
             }
         }
     </script>
+     --}}
 @endif
 @if($__cfg['SiteCode'] == '2002')
     {{-- 경찰학원--}}
     <div id="topBannerLayer">
         <div class="topBanner">
-            <a href="{{ site_url('/promotion/index/cate/3001/code/1022') }}">
+            <a href="{{site_url('/promotion/index/cate/3001/code/1022')}}">
                 <img src="https://static.willbes.net/public/images/promotion/2019/09/1022_top_ban.jpg" alt="적중! 적중! 또 다시 적중!">
             </a>
         </div>

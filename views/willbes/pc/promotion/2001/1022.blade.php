ba19c96cb02e4ad16b965dd5a2d3eb446de55d3c
김창용 <winmir@willbes.com>
2020-06-16 14:57:46 +0900

    경찰 신광은 적중

--- views/willbes/pc/promotion/2001/1022.blade.php
+++ views/willbes/pc/promotion/2001/1022.blade.php
@@ -72,7 +72,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="javascript:alert('20년 1차 적중, 곧 공개됩니다!');" >         
+                        <a href="/promotion/index/cate/3001/code/1694" target="_blank">        
                             <img src="https://static.willbes.net/public/images/promotion/2019/05/1022_01_1.jpg" class="off" alt="신광은 형소법"  />
                             <img src="https://static.willbes.net/public/images/promotion/2019/05/1022_01_1on.jpg" class="on" alt="신광은 형소법"  />
                         </a>
@@ -84,13 +84,13 @@
                         </a>
                     </li>                   
                     <li>
-                        <a href="https://police.willbes.net/promotion/index/cate/3001/code/1690" target="_blank">
+                        <a href="/promotion/index/cate/3001/code/1690" target="_blank">
                             <img src="https://static.willbes.net/public/images/promotion/2019/05/1022_01_4.jpg" class="off" alt="하승민 경찰영어"  />
                             <img src="https://static.willbes.net/public/images/promotion/2019/05/1022_01_4on.jpg" class="on" alt="하승민 경찰영어"  />
                         </a>
                     </li>                  
                     <li>
-                        <a href="https://police.willbes.net/promotion/index/cate/3001/code/1691" target="_blank">
+                        <a href="/promotion/index/cate/3001/code/1691" target="_blank">
                             <img src="https://static.willbes.net/public/images/promotion/2019/05/1022_01_7.jpg" class="off" alt="김현정 경찰영어"  />
                             <img src="https://static.willbes.net/public/images/promotion/2019/05/1022_01_7on.jpg" class="on" alt="김현정 경찰영어"  />
                         </a>

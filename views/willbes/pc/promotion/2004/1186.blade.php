8ee6ca7c0a0eb08dcdd8d6b8dbf509a18c0b5dce
이윤구 <yoongukk@willbes.com>
2020-06-24 17:26:36 +0900

    소방직 공무원 시험 해설강의 수정 자료다운받기

--- views/willbes/pc/promotion/2004/1186.blade.php
+++ views/willbes/pc/promotion/2004/1186.blade.php
@@ -22,167 +22,124 @@
 
         /************************************************************/
 
-        .evtTop {background:url(https://static.willbes.net/public/images/promotion/2019/04/1186_top_bg.png) no-repeat center top;}
-        .evt01 {background:#eee; padding:50px 0 100px}
-        
-        .evt01 .lecture {
-            width:1000px; margin:0 auto; padding:50px 0;
-        }
-        .evt01 .lecture li {
-            display:inline; float:left; width:25%; text-align:center; margin-bottom:20px; padding-top:20px;
-        }
-        .evt01 .lecture li:hover {background:url(https://static.willbes.net/public/images/promotion/common/patternB.png) repeat}
-        .evt01 .lecture li img.prof {
-            width:200px !important; border:1px solid #ccc; background:#fff;
-        }
-        .evt01 .t_tilte {
-           line-height:1.5; padding:10px 0; color:#666; width:200px; margin:0 auto
-        }
-        .evt01 .t_tilte p {border-top:1px solid #e4e4e4; padding-top:10px; margin-top:10px}
-        .evt01 .t_tilte span {
-            color:#36374d; font-size:14px; ;
-        }
+        .evtTop {background:#B7130F url(https://static.willbes.net/public/images/promotion/2020/06/1186_top_bg.jpg) no-repeat center top;}
 
-        .evt01 .lecture ul:after {
-            content:""; display:block; clear:both;
-        }
+         /* 슬라이드배너 */
+        .slide_con {position:relative; width:1120px; margin:0 auto}
+        .slide_con p {position:absolute; top:50%; margin-top:-30px; width:67px; height:67px; z-index:10}
+        .slide_con p a {cursor:pointer}
+        .slide_con p.leftBtn {left:0}
+        .slide_con p.rightBtn {right:0}
+
+        .evt01s {background:#e4e4e4;padding-bottom:50px}
+        .evt01s .slide_con {position:relative; width:950px; margin:0 auto}
+        .evt01s .slide_con p {position:absolute; top:50%; margin-top:-22px; width:44px; height:45px; z-index:10}
+        .evt01s .slide_con p.leftBtn {left:-80px}
+        .evt01s .slide_con p.rightBtn {right:-80px}         
         
+        .evt01 {background:#eee; padding:50px 0 100px}        
         .evt01 div {position:relative; width:1120px; margin:0 auto}
-        .evt01 div span {position:absolute; left:463px; display:block; width:433px; height:67px; line-height:67px; z-index:10}
-        .evt01 div span:nth-child(1) {top:220px}
+        .evt01 div span {position:absolute;  display:block; width:433px; height:67px; line-height:67px; z-index:10}
+        .evt01 div span:nth-child(1) {top:250px;left:460px;}
 
-        .evt01 div span:nth-child(2) {top:620px}
-        .evt01 div span:nth-child(3) {top:700px}
-        .evt01 div span:nth-child(4) {top:780px}
+        .evt01 div span:nth-child(2) {top:700px}
+        .evt01 div span:nth-child(3) {top:780px}
+        .evt01 div span:nth-child(4) {top:860px}
 
-        .evt01 div span:nth-child(5) {top:1170px}
-        .evt01 div span:nth-child(6) {top:1250px}
+        .evt01 div span:nth-child(5) {top:1200px;left:460px;}
 
-        .evt01 div span:nth-child(7) {top:1710px}
-        .evt01 div span:nth-child(11) {top:1790px}
+        .evt01 div span:nth-child(6) {top:1740px}
+
+        .evt01 div span:nth-child(7) {top:2300px;left:460px;}
+
         .evt01 div span a {display:block; text-align:center; color:#333; background:#fff; font-size:18px; font-weight:600; border:1px solid #ff5d7a !important;}
         .evt01 div span a:hover {color:#fff; background:#ff5d7a}
         
-		    .evt02 {background:#fff;}
+		.evt02 {background:#fff;}
 
     </style>
 
     <div class="p_re evtContent NGR" id="evtContainer">
 
       <div class="evtCtnsBox evtTop">
-        <img src="https://static.willbes.net/public/images/promotion/2019/04/1186_top.png" title="소방전담 교수님의 완벽분석 해설강의">
+        <img src="https://static.willbes.net/public/images/promotion/2020/06/1186_top.gif" title="소방전담 교수님의 완벽분석 해설강의">
       </div>
-      <div class="evtCtnsBox evt01">
-        {{--
-        <div class="lecture">
-          <ul>
-              <li>
-                  <img src="https://pass.willbes.net/public/uploads/willbes/professor/50661/lec_detail_50661_1553238216.png" title="교수명" class="prof">
-                  <div class="t_tilte">
-                      국어 김세령 교수<br>
-                      <span>고득점에 최적화된<br>신개념 국어</span>                               
-                      <p>추후 제공 예정입니다.</p>                            
-                  </div>
-              </li>
-              <li>
-                  <img src="https://pass.willbes.net/public/uploads/willbes/professor/50465/lec_detail_50465.png" title="교수명" class="prof">
-                  <div class="t_tilte">
-                      소방학개론 김종상 교수<br>
-                      <span>소방 전문<br>스페셜리스트</span> 
-                      <p>
-                          <a href="#none"><img src="https://static.willbes.net/public/images/promotion/2019/03/1140_btn03.png" title="해설강의"></a>
-                          <a href="#none"><img src="https://static.willbes.net/public/images/promotion/2019/03/1140_btn04.png" title="해설자료"></a>
-                      </p>
-                  </div>
-              </li>
-              <li>
-                  <img src="https://pass.willbes.net/public/uploads/willbes/professor/50465/lec_detail_50465.png" title="교수명" class="prof">
-                  <div class="t_tilte">
-                      공채 소방관계법규 김종상 교수<br>
-                      <span>소방 전문<br>스페셜리스트</span> 
-                      <p>
-                          <a href="#none"><img src="https://static.willbes.net/public/images/promotion/2019/03/1140_btn03.png" title="해설강의"></a>
-                          <a href="#none"><img src="https://static.willbes.net/public/images/promotion/2019/03/1140_btn04.png" title="해설자료"></a>
-                      </p>
-                  </div>
-              </li>
-              <li>
-                  <img src="https://pass.willbes.net/public/uploads/willbes/professor/50465/lec_detail_50465.png" title="교수명" class="prof">
-                  <div class="t_tilte">
-                      경채 소방관계법규 김종상 교수<br>
-                      <span>소방 전문<br>스페셜리스트</span> 
-                      <p>
-                          <a href="#none"><img src="https://static.willbes.net/public/images/promotion/2019/03/1140_btn03.png" title="해설강의"></a>
-                          <a href="#none"><img src="https://static.willbes.net/public/images/promotion/2019/03/1140_btn04.png" title="해설자료"></a>
-                      </p>
-                  </div>
-              </li>
-              <li>
-                  <img src="https://pass.willbes.net/public/uploads/willbes/professor/50479/lec_detail_50479.png" title="교수명" class="prof">
-                  <div class="t_tilte">
-                      공채 영어 이현정 교수<br>
-                      <span>막힘없이 풀어가는<br>수험 영어 전문가!</span> 
-                      <p>
-                          <a href="#none"><img src="https://static.willbes.net/public/images/promotion/2019/03/1140_btn03.png" title="해설강의"></a>
-                          <a href="#none"><img src="https://static.willbes.net/public/images/promotion/2019/03/1140_btn04.png" title="해설자료"></a>
-                      </p>
-                  </div>
-              </li>
-              <li>
-                  <img src="https://pass.willbes.net/public/uploads/willbes/professor/50479/lec_detail_50479.png" title="교수명" class="prof">
-                  <div class="t_tilte">
-                      경채 영어 이현정 교수<br>
-                      <span>막힘없이 풀어가는<br>수험 영어 전문가!</span> 
-                      <p>
-                          <a href="#none"><img src="https://static.willbes.net/public/images/promotion/2019/03/1140_btn03.png" title="해설강의"></a>
-                          <a href="#none"><img src="https://static.willbes.net/public/images/promotion/2019/03/1140_btn04.png" title="해설자료"></a>
-                      </p>
-                  </div>
-              </li>
-              <li>
-                  <img src="https://pass.willbes.net/public/uploads/willbes/professor/50305/lec_detail_50305.png" title="교수명" class="prof">
-                  <div class="t_tilte">
-                      한국사 한경준 교수<br>
-                      <span>한국사에<br>미치게 만들다</span> 
-                      <p>
-                          <a href="#none"><img src="https://static.willbes.net/public/images/promotion/2019/03/1140_btn03.png" title="해설강의"></a>
-                          <a href="#none"><img src="https://static.willbes.net/public/images/promotion/2019/03/1140_btn04.png" title="해설자료"></a>
-                      </p>
-                  </div>
-              </li>
-          </ul>
+
+      <div class="evtCtnsBox evt01s">
+        <img src="https://static.willbes.net/public/images/promotion/2020/06/thank.jpg"  alt="소방직 합격"/>
+        <div class="slide_con">
+            <ul id="slidesImg4">
+                <li><img src="https://static.willbes.net/public/images/promotion/2020/06/thanks01.jpg" /></li>
+                <li><img src="https://static.willbes.net/public/images/promotion/2020/06/thanks02.jpg" /></li>     
+            </ul>
+            <p class="leftBtn"><a id="imgBannerLeft4"><img src="https://static.willbes.net/public/images/promotion/2020/06/1699_arrowL.png"></a></p>
+            <p class="rightBtn"><a id="imgBannerRight4"><img src="https://static.willbes.net/public/images/promotion/2020/06/1699_arrowR.png"></a></p>
         </div>
-        --}}
+        <img src="https://static.willbes.net/public/images/promotion/2020/06/thanks_emo.jpg"  alt="이모티콘"/>
+    </div>
+
+      <div class="evtCtnsBox evt01">
           
         <div>          
           {{--국어 김새령--}}
-          <span><a href="{{ site_url('/lecture/show/cate/3023/pattern/free/prod-code/152486') }}" target="_blank">2019 소방직 국어 해설강의 및 총평&해설자료 ></a></span>
+          <span><a href="@if(empty($file_yn) === false && $file_yn[0] == 'Y') {{ front_url($file_link[0]) }} @else {{ $file_link[0] }} @endif" >2020 소방직 국어 해설강의 및 총평&해설자료 ></a></span>
 
           {{--소방학개론/소방관계법규 김종상--}}
-          <span><a href="{{ site_url('/lecture/show/cate/3023/pattern/free/prod-code/152494') }}" target="_blank">2019 소방직 소방학개론 및 총평&해설자료 ></a></span>
-          <span><a href="{{ site_url('/lecture/show/cate/3023/pattern/free/prod-code/152495') }}" target="_blank">2019 소방직 공채 소방관계법규 및 총평&해설자료 ></a></span>
-          <span><a href="{{ site_url('/lecture/show/cate/3023/pattern/free/prod-code/152496') }}" target="_blank">2019 소방직 경채 소방관계법규 및 총평&해설자료 ></a></span>
+          <span><a href="@if(empty($file_yn) === false && $file_yn[1] == 'Y') {{ front_url($file_link[1]) }} @else {{ $file_link[1] }} @endif" >2020 소방직 소방학개론 및 총평&해설자료 ></a></span>
+          <span><a href="@if(empty($file_yn) === false && $file_yn[2] == 'Y') {{ front_url($file_link[2]) }} @else {{ $file_link[2] }} @endif" >2020 소방직 공채 소방관계법규 및 총평&해설자료 ></a></span>
+          <span><a href="@if(empty($file_yn) === false && $file_yn[3] == 'Y') {{ front_url($file_link[3]) }} @else {{ $file_link[3] }} @endif" >2020 소방직 경채 소방관계법규 및 총평&해설자료 ></a></span>
 
-          {{--영어 이현정--}}
-          <span><a href="{{ site_url('/lecture/show/cate/3023/pattern/free/prod-code/152497') }}" target="_blank">2019 소방직 공채 영어 및 총평&해설자료 ></a></span>
-          <span><a href="{{ site_url('/lecture/show/cate/3023/pattern/free/prod-code/152498') }}" target="_blank">2019 소방직 경채 영어 및 총평&해설자료 ></a></span>
+          {{--영어 이아림--}}
+          <span><a href="javascript:alert('준비중입니다.')">2020 소방직 공채 영어 및 총평&해설자료 ></a></span>
+
+          {{--영어 양익--}}
+          <span><a href="@if(empty($file_yn) === false && $file_yn[5] == 'Y') {{ front_url($file_link[5]) }} @else {{ $file_link[5] }} @endif" >2020 소방직 공채 영어 및 총평&해설자료 ></a></span>
 
           {{--한국사 한경준--}}
-          <span><a href="{{ site_url('/lecture/show/cate/3023/pattern/free/prod-code/152491') }}" target="_blank">2019 소방직 한국사 및 총평&해설자료 ></a></span>
+          <span><a href="@if(empty($file_yn) === false && $file_yn[6] == 'Y') {{ front_url($file_link[6]) }} @else {{ $file_link[6] }} @endif" >2020 소방직 한국사 및 총평&해설자료 ></a></span>
 
-          <img src="https://static.willbes.net/public/images/promotion/2019/04/1186_01.png" usemap="#1186_01" title="소방직 해설강의" border="0">
+          <img src="https://static.willbes.net/public/images/promotion/2020/06/1186_01.png" title="소방직 해설강의" border="0">
         </div>
       
       </div>
 	  
 	  <div class="evtCtnsBox evt02">
-        <img src="https://static.willbes.net/public/images/promotion/2019/04/1186_02.png" usemap="#1186_02" title="불꽃소방팀의 합격전담반" border="0">
-        <map name="1186_02">
-          <area shape="rect" coords="381,1084,740,1147" href="javascript:alert('준비중입니다');">
-        </map>
+        <img src="https://static.willbes.net/public/images/promotion/2020/06/1186_02.jpg" usemap="#Map1186a" title="불꽃소방팀의 합격전담반" border="0" />
+        <map name="Map1186a" id="Map1186a">
+            <area shape="rect" coords="380,1071,741,1142" href="javascript:alert('준비중입니다.');">
+        </map>     
 	  </div>
 
 	</div>
     <!-- End Container -->
 
+    <script type="text/javascript">
+             $(document).ready(function() {
+            var slidesImg4 = $("#slidesImg4").bxSlider({
+                mode:'horizontal', //option : 'horizontal', 'vertical', 'fade'
+                auto:true,
+                speed:350,
+                pause:4000,
+                pager:true,
+                controls:false,
+                minSlides:1,
+                maxSlides:1,
+                slideMargin:0,
+                autoHover: true,
+                moveSlides:1,
+                pager:false,
+            });
+
+            $("#imgBannerLeft4").click(function (){
+                slidesImg4.goToPrevSlide();
+            });
+
+            $("#imgBannerRight4").click(function (){
+                slidesImg4.goToNextSlide();
+            });
+        });
+    </script>
+
+    {{-- 프로모션용 스크립트 include --}}
+    @include('willbes.pc.promotion.promotion_script')
 @stop

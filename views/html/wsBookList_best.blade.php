ce97c1574196ed183d591c15ba27813d17f3627d
김창용 <winmir@willbes.com>
2020-06-26 11:21:55 +0900

    윌스토리 코딩 페이지

--- views/html/wsBookList_best.blade.php
+++ views/html/wsBookList_best.blade.php
@@ -3,6 +3,37 @@
 @section('content')
 <!-- Container -->
 <div id="Container" class="subContainer widthAuto c_both">
+    <div class="Section widthAuto">
+        <div class="wsSearchWrap">
+            <div class="wsSearch NSK">
+                <form id="unifiedSearch_form" name="unifiedSearch_form" method="GET">                
+                    <input type="hidden" name="cate" id="unifiedSearch_cate" value="{{$__cfg['CateCode']}}">
+                    <select name="search_type">
+                        <option value="0">통합검색</option>
+                        <option value="1">도서명</option>
+                        <option value="2">저자명</option>                            
+                        <option value="3">출판사</option>
+                    </select>
+                    <input type="text" name="" class="d_none">                
+                    <input type="search" class='unifiedSearch' data-form="unifiedSearch_form" id="unifiedSearch_text" name="searchfull_text" value="" placeholder="검색어를 입력하세요." maxlength="100"/>
+                    <label for="onsearch" class="NSK-Black"><button title="검색" type="button" id="btn_unifiedSearch" class='btn_unifiedSearch' data-form="unifiedSearch_form">Search</button></label>
+                </form>
+            </div>
+            <div class="keyword">
+                <strong>윌스토리 인기검색어</strong>										
+                <a href="#none">김정일</a>                 
+                <a href="#none">김유향</a>                 
+                <a href="#none">PSAT</a>                 
+                <a href="#none">김동진</a>                
+                <a href="#none">NCS</a>                 
+                <a href="#none">로스쿨</a>                 
+                <a href="#none">나무경영아카데미</a>                 
+                <a href="#none">노무사</a>                 
+                <a href="#none">황종휴</a> 
+            </div>
+        </div>
+    </div>
+
     <div class="Menu NG c_both">
         <h3>
             <ul class="menu-Tit">
@@ -705,8 +736,8 @@
                                 <label><input type="checkbox" name="" id=""></label>
                                 <a href="#none">정채영 국어 마무리시리즈 70테마로 끝내주마!</a>
                             </li>
-                            <li><strong>[저자]</strong> 윌비스</li>
-                            <li><strong>[출판사]</strong> 윌비스</li>
+                            <li><strong>[저자]</strong> <a href="#none">윌비스</a></li>
+                            <li><strong>[출판사]</strong> <a href="#none">윌비스</a></li>
                             <li><strong>[출간일]</strong> 2020.05.20</li>
                             <li><strong class="tx-red">[품절]</strong> <span class="line-through">20,000원</span> → <span class="tx-blue strong">10,800원</span></li>
                             <li>
@@ -736,8 +767,8 @@
                                 <label><input type="checkbox" name="" id=""></label>
                                 <a href="#none">2019 기태국어 실전문제 1340(전2권) </a>
                             </li>
-                            <li><strong>[저자]</strong> 윌비스</li>
-                            <li><strong>[출판사]</strong> 윌비스</li>
+                            <li><strong>[저자]</strong> <a href="#none">윌비스</a></li>
+                            <li><strong>[출판사]</strong> <a href="#none">윌비스</a></li>
                             <li><strong>[출간일]</strong> 2020.05.20</li>
                             <li><strong>[판매중]</strong> <span class="line-through">20,000원</span> → <span class="tx-blue strong">10,800원</span></li>
                             <li>
@@ -767,8 +798,8 @@
                                 <label><input type="checkbox" name="" id=""></label>
                                 <a href="#none">2019 기태국어 기본이론(어법, 독해)(세트전2권) </a>
                             </li>
-                            <li><strong>[저자]</strong> 윌비스</li>
-                            <li><strong>[출판사]</strong> 윌비스</li>
+                            <li><strong>[저자]</strong> <a href="#none">윌비스</a></li>
+                            <li><strong>[출판사]</strong> <a href="#none">윌비스</a></li>
                             <li><strong>[출간일]</strong> 2020.05.20</li>
                             <li><strong>[판매중]</strong> <span class="line-through">20,000원</span> → <span class="tx-blue strong">10,800원</span></li>
                             <li>
@@ -798,8 +829,8 @@
                                 <label><input type="checkbox" name="" id=""></label>
                                 <a href="#none">2019 기특한 국어(전3권) </a>
                             </li>
-                            <li><strong>[저자]</strong> 윌비스</li>
-                            <li><strong>[출판사]</strong> 윌비스</li>
+                            <li><strong>[저자]</strong> <a href="#none">윌비스</a></li>
+                            <li><strong>[출판사]</strong> <a href="#none">윌비스</a></li>
                             <li><strong>[출간일]</strong> 2020.05.20</li>
                             <li><strong>[판매중]</strong> <span class="line-through">20,000원</span> → <span class="tx-blue strong">10,800원</span></li>
                             <li>
@@ -829,8 +860,8 @@
                                 <label><input type="checkbox" name="" id=""></label>
                                 <a href="#none">풀면 이론이 정리되는 문제집(풀이정) - 햇살국어 Level 2 </a>
                             </li>
-                            <li><strong>[저자]</strong> 윌비스</li>
-                            <li><strong>[출판사]</strong> 윌비스</li>
+                            <li><strong>[저자]</strong> <a href="#none">윌비스</a></li>
+                            <li><strong>[출판사]</strong> <a href="#none">윌비스</a></li>
                             <li><strong>[출간일]</strong> 2020.05.20</li>
                             <li><strong>[판매중]</strong> <span class="line-through">20,000원</span> → <span class="tx-blue strong">10,800원</span></li>
                             <li>
@@ -860,8 +891,8 @@
                                 <label><input type="checkbox" name="" id=""></label>
                                 <a href="#none">정채영 국어 마무리시리즈 70테마로 끝내주마!</a>
                             </li>
-                            <li><strong>[저자]</strong> 윌비스</li>
-                            <li><strong>[출판사]</strong> 윌비스</li>
+                            <li><strong>[저자]</strong> <a href="#none">윌비스</a></li>
+                            <li><strong>[출판사]</strong> <a href="#none">윌비스</a></li>
                             <li><strong>[출간일]</strong> 2020.05.20</li>
                             <li><strong>[판매중]</strong> <span class="line-through">20,000원</span> → <span class="tx-blue strong">10,800원</span></li>
                             <li>
@@ -891,8 +922,8 @@
                                 <label><input type="checkbox" name="" id=""></label>
                                 <a href="#none">2019 기태국어 실전문제 1340(전2권) </a>
                             </li>
-                            <li><strong>[저자]</strong> 윌비스</li>
-                            <li><strong>[출판사]</strong> 윌비스</li>
+                            <li><strong>[저자]</strong> <a href="#none">윌비스</a></li>
+                            <li><strong>[출판사]</strong> <a href="#none">윌비스</a></li>
                             <li><strong>[출간일]</strong> 2020.05.20</li>
                             <li><strong>[판매중]</strong> <span class="line-through">20,000원</span> → <span class="tx-blue strong">10,800원</span></li>
                             <li>
@@ -922,8 +953,8 @@
                                 <label><input type="checkbox" name="" id=""></label>
                                 <a href="#none">2019 기태국어 기본이론(어법, 독해)(세트전2권) </a>
                             </li>
-                            <li><strong>[저자]</strong> 윌비스</li>
-                            <li><strong>[출판사]</strong> 윌비스</li>
+                            <li><strong>[저자]</strong> <a href="#none">윌비스</a></li>
+                            <li><strong>[출판사]</strong> <a href="#none">윌비스</a></li>
                             <li><strong>[출간일]</strong> 2020.05.20</li>
                             <li><strong>[판매중]</strong> <span class="line-through">20,000원</span> → <span class="tx-blue strong">10,800원</span></li>
                             <li>
@@ -953,8 +984,8 @@
                                 <label><input type="checkbox" name="" id=""></label>
                                 <a href="#none">2019 기특한 국어(전3권) </a>
                             </li>
-                            <li><strong>[저자]</strong> 윌비스</li>
-                            <li><strong>[출판사]</strong> 윌비스</li>
+                            <li><strong>[저자]</strong> <a href="#none">윌비스</a></li>
+                            <li><strong>[출판사]</strong> <a href="#none">윌비스</a></li>
                             <li><strong>[출간일]</strong> 2020.05.20</li>
                             <li><strong>[판매중]</strong> <span class="line-through">20,000원</span> → <span class="tx-blue strong">10,800원</span></li>
                             <li>
@@ -984,8 +1015,8 @@
                                 <label><input type="checkbox" name="" id=""></label>
                                 <a href="#none">풀면 이론이 정리되는 문제집(풀이정) - 햇살국어 Level 2 </a>
                             </li>
-                            <li><strong>[저자]</strong> 윌비스</li>
-                            <li><strong>[출판사]</strong> 윌비스</li>
+                            <li><strong>[저자]</strong> <a href="#none">윌비스</a></li>
+                            <li><strong>[출판사]</strong> <a href="#none">윌비스</a></li>
                             <li><strong>[출간일]</strong> 2020.05.20</li>
                             <li><strong>[판매중]</strong> <span class="line-through">20,000원</span> → <span class="tx-blue strong">10,800원</span></li>
                             <li>
--- views/html/wsBookView.blade.php
+++ views/html/wsBookView.blade.php
@@ -3,6 +3,37 @@
 @section('content')
 <!-- Container -->
 <div id="Container" class="subContainer widthAuto c_both">
+    <div class="Section widthAuto">
+        <div class="wsSearchWrap">
+            <div class="wsSearch NSK">
+                <form id="unifiedSearch_form" name="unifiedSearch_form" method="GET">                
+                    <input type="hidden" name="cate" id="unifiedSearch_cate" value="{{$__cfg['CateCode']}}">
+                    <select name="search_type">
+                        <option value="0">통합검색</option>
+                        <option value="1">도서명</option>
+                        <option value="2">저자명</option>                            
+                        <option value="3">출판사</option>
+                    </select>
+                    <input type="text" name="" class="d_none">                
+                    <input type="search" class='unifiedSearch' data-form="unifiedSearch_form" id="unifiedSearch_text" name="searchfull_text" value="" placeholder="검색어를 입력하세요." maxlength="100"/>
+                    <label for="onsearch" class="NSK-Black"><button title="검색" type="button" id="btn_unifiedSearch" class='btn_unifiedSearch' data-form="unifiedSearch_form">Search</button></label>
+                </form>
+            </div>
+            <div class="keyword">
+                <strong>윌스토리 인기검색어</strong>										
+                <a href="#none">김정일</a>                 
+                <a href="#none">김유향</a>                 
+                <a href="#none">PSAT</a>                 
+                <a href="#none">김동진</a>                
+                <a href="#none">NCS</a>                 
+                <a href="#none">로스쿨</a>                 
+                <a href="#none">나무경영아카데미</a>                 
+                <a href="#none">노무사</a>                 
+                <a href="#none">황종휴</a> 
+            </div>
+        </div>
+    </div>
+
     <div class="Menu NG c_both">
         <h3>
             <ul class="menu-Tit">

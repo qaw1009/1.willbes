9e2dcd4b932257e2ce42a2c94539b042feb5bc8c
최현탁 <cht@willbes.com>
2020-07-07 17:01:16 +0900

    LMS 인천학원용 수강증 출력 기능 추가

---
+++ views/lcms/layouts/master_no_header.blade.php
@@ -0,0 +1,26 @@
+{{-- 팝업기본화면 --}}
+@include('lcms.layouts.header_no_css')
+<body class="single">
+
+@hasSection('popup_title')
+    @yield('popup_header')
+    <div class="modal-header bg-green">
+        <h4 class="modal-title">@yield('popup_title')</h4>
+    </div>
+@endif
+
+<div class="modal-body">
+    @yield('popup_content')
+</div>
+
+@hasSection('add_buttons')
+    <div class="modal-footer">
+        @yield('add_buttons')
+        <button type="button" class="btn btn-default" onclick="self.close();" >닫기</button>
+    </div>
+@endif
+
+@yield('popup_footer')
+{{-- Main Scripts --}}
+</body>
+</html>

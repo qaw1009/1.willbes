c19460b56915e60fbd51f6506e0d25ce5eba5e82
김창용 <winmir@willbes.com>
2020-07-07 11:17:32 +0900

    모바일 경찰 메인 코딩페이지

--- views/html/m/main5.blade.php
+++ views/html/m/main5.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <!-- Container -->
-<div id="Container" class="Container NG c_both mb20">
+<div id="Container" class="Container NG c_both mb20 cop">
     <div class="gosibtns bdb-none mt0 pb10">
         <ul>
             <li><a href="https://www.willbes.net/m/classroom/on/list/ongoing">내강의실</a></li>
@@ -11,11 +11,12 @@
         </ul>
     </div>
     
-    <div class="MainSlider swiper-container swiper-container-page c_both">
+    <div class="MainSlider swiper-container swiper-container-page">
         <div class="swiper-wrapper">
-            <div class="swiper-slide"><a href="#none"><img src="https://police.willbes.net/public/uploads/willbes/banner/2019/0910/banner_20191028180337.jpg"></a></div>
-            <div class="swiper-slide"><a href="#none"><img src="https://police.willbes.net/public/uploads/willbes/banner/2019/0510/banner_20190510170355.jpg"></a></div>
-            <div class="swiper-slide"><a href="#none"><img src="https://police.willbes.net/public/uploads/willbes/banner/2020/0304/banner_20200304162845.jpg"></a></div>
+            <div class="swiper-slide"><img src="https://static.willbes.net/public/images/promotion/m/3094_720x400_01.jpg"></div>
+            <div class="swiper-slide"><img src="https://static.willbes.net/public/images/promotion/m/3094_720x400_02.jpg"></div>
+            <div class="swiper-slide"><img src="https://static.willbes.net/public/images/promotion/m/3094_720x400_03.jpg"></div>
+            <div class="swiper-slide"><img src="https://static.willbes.net/public/images/promotion/m/3094_720x400_04.jpg"></div>
         </div>
         <!-- Add Pagination -->
         <div class="swiper-pagination"></div>
@@ -23,23 +24,160 @@
     
     <div class="MainSlider swiper-container swiper-container-page c_both mt20">
         <div class="swiper-wrapper">
-            <div class="swiper-slide"><a href="#none"><img src="https://static.willbes.net/public/images/promotion/m/2002_734_120_20032301.jpg" alt="샘플"></a></div>
-            
-            {{--<div class="swiper-slide"><a href="#none"><img src="https://static.willbes.net/public/images/promotion/m/2002_734_120_20032302.jpg" alt="샘플"></a></div>
-            <div class="swiper-slide"><a href="#none"><img src="https://static.willbes.net/public/images/promotion/m/2002_734_120_20032303.jpg" alt="샘플"></a></div>--}}
+            <div class="swiper-slide"><a href="#none"><img src="https://static.willbes.net/public/images/promotion/m/2002_734_120_20032301.jpg" alt="샘플"></a></div>            
+            <div class="swiper-slide"><a href="#none"><img src="https://static.willbes.net/public/images/promotion/m/2002_734_120_20032302.jpg" alt="샘플"></a></div>
+            <div class="swiper-slide"><a href="#none"><img src="https://static.willbes.net/public/images/promotion/m/2002_734_120_20032303.jpg" alt="샘플"></a></div>
         </div>
         <!-- Add Pagination -->
         <div class="swiper-pagination"></div>
     </div> 
 
-    <div class="buttonTabs noticeTabs c_both">
-        <ul class="tabWrap buttonWrap noticeWrap three">
+    <div class="mSubTit NSK-Black mt40 tx-left">윌비스 <span class="tx-blue">경찰 케스트</span></div>
+    <div class="cast">
+        <div class="swiper-container-lec">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <a href="#none">
+                        <img src="https://static.willbes.net/public/images/promotion/m//2001_bn_226x152.jpg" alt="배너명">
+                    </a>
+                </div>            
+                <div class="swiper-slide">
+                    <a href="#none">
+                        <img src="https://static.willbes.net/public/images/promotion/m//2001_bn_226x152.jpg" alt="배너명">  
+                    </a>
+                </div>
+                <div class="swiper-slide">
+                    <a href="#none">
+                        <img src="https://static.willbes.net/public/images/promotion/m//2001_bn_226x152.jpg" alt="배너명">
+                    </a>
+                </div>
+                <div class="swiper-slide">
+                    <a href="#none">
+                        <img src="https://static.willbes.net/public/images/promotion/m//2001_bn_226x152.jpg" alt="배너명">
+                    </a>
+                </div>
+                <div class="swiper-slide">
+                    <a href="#none">
+                        <img src="https://static.willbes.net/public/images/promotion/m//2001_bn_226x152.jpg" alt="배너명">
+                    </a>
+                </div>
+                <div class="swiper-slide">
+                    <a href="#none">
+                        <img src="https://static.willbes.net/public/images/promotion/m//2001_bn_226x152.jpg" alt="배너명">
+                    </a>
+                </div>
+                <div class="swiper-slide">
+                    <a href="#none">
+                        <img src="https://static.willbes.net/public/images/promotion/m//2001_bn_226x152.jpg" alt="배너명">
+                    </a>
+                </div>
+            </div>
+            <!-- Add Pagination -->
+            <div class="swiper-pagination"></div>
+        </div>
+    </div> 
+
+    <div class="mSubTit NSK-Black mt40 tx-left" >신광은 경찰 <span class="tx-blue">베스트 강좌</span></div>
+    <div class="bestView">
+        <div class="overhidden">
+            <div class="swiper-container-view">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <a href="#none">
+                            <img src="https://police.willbes.net/public/uploads/willbes/professor/50547/lec_list_505471.png" alt="강사명">
+                            <div>
+                                문풀 1단계 핵심요약 / 진도별 
+                                <p>2020년 2차 대비 신광은 형사소송법 핵심이론 요약정리</p>
+                                <span>신청하기 ></span>
+                            </div>                            
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a href="#none">
+                            <img src="https://police.willbes.net/public/uploads/willbes/professor/50031/lec_list_50031.png" alt="강사명">
+                            <div>
+                                문풀 1단계 핵심요약 / 진도별 
+                                <p>2020년 2차 대비 신광은 형사소송법 핵심이론 요약정리</p>
+                                <span>신청하기 ></span>
+                            </div>                            
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a href="#none">
+                            <img src="https://police.willbes.net/public/uploads/willbes/professor/50297/lec_list_50297.png" alt="강사명">
+                            <div>
+                                문풀 1단계 핵심요약 / 진도별 
+                                <p>2020년 2차 대비 신광은 형사소송법 핵심이론 요약정리</p>
+                                <span>신청하기 ></span>
+                            </div>                            
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a href="#none">
+                            <img src="https://police.willbes.net/public/uploads/willbes/professor/50641/lec_list_50641.png" alt="강사명">
+                            <div>
+                                문풀 1단계 핵심요약 / 진도별 
+                                <p>2020년 2차 대비 신광은 형사소송법 핵심이론 요약정리</p>
+                                <span>신청하기 ></span>
+                            </div>                            
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a href="#none">
+                            <img src="https://police.willbes.net/public/uploads/willbes/professor/50131/lec_list_50131_1567240397.png" alt="강사명">
+                            <div>
+                                문풀 1단계 핵심요약 / 진도별 
+                                <p>2020년 2차 대비 신광은 형사소송법 핵심이론 요약정리</p>
+                                <span>신청하기 ></span>
+                            </div>                            
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a href="#none">
+                            <img src="https://police.willbes.net/public/uploads/willbes/professor/50135/lec_list_50135.png" alt="강사명">
+                            <div>
+                                문풀 1단계 핵심요약 / 진도별 
+                                <p>2020년 2차 대비 신광은 형사소송법 핵심이론 요약정리</p>
+                                <span>신청하기 ></span>
+                            </div>                            
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a href="#none">
+                            <img src="https://police.willbes.net/public/uploads/willbes/professor/50748/lec_list_50748.png" alt="강사명">
+                            <div>
+                                문풀 1단계 핵심요약 / 진도별 
+                                <p>2020년 2차 대비 신광은 형사소송법 핵심이론 요약정리</p>
+                                <span>신청하기 ></span>
+                            </div>                            
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a href="#none">
+                            <img src="https://police.willbes.net/public/uploads/willbes/professor/50749/lec_list_50749.png" alt="강사명">
+                            <div>
+                                문풀 1단계 핵심요약 / 진도별 
+                                <p>2020년 2차 대비 신광은 형사소송법 핵심이론 요약정리</p>
+                                <span>신청하기 ></span>
+                            </div>                            
+                        </a>
+                    </div>
+                </div>
+                <!-- Add Pagination -->
+                <div class="swiper-pagination"></div>
+            </div>
+        </div>
+    </div>    
+
+    <div class="noticeTabs c_both">
+        <ul class="tabWrap mainTab">
             <li><a href="#notice1" class="on">공지사항</a></li>
-            <li><a href="#notice2">채용공고</a></li>
+            <li><a href="#notice2">시험공고</a></li>
             <li><a href="#notice3">수험뉴스</a></li>
         </ul>
         <div class="tabBox buttonBox noticeBox">
-            <div id="notice1" class="tabContent">
+            <div id="notice1" class="tabContent pd20">
+                <div class="moreBtn"><a href="#none">+ 더보기</a></div>
                 <ul class="List-Table">
                     <li><a href="#none">공지사항 제목이 출력됩니다.</a><span class="date">2018-04-01</span></li>
                     <li><a href="#none">3월 31일(금) 새벽시스템점검안내 안내안내안내</a><span class="date">2018-04-01</span></li>
@@ -48,7 +186,8 @@
                     <li><a href="#none">추석교재배송및고객센터휴무안내</a><span class="date">2018-03-06</span></li>
                 </ul>
             </div>
-            <div id="notice2" class="tabContent" style="display: none;">
+            <div id="notice2" class="tabContent pd20">
+                <div class="moreBtn"><a href="#none">+ 더보기</a></div>
                 <ul class="List-Table">
                     <li><a href="#none">공지사항 제목이 출력됩니다.</a><span class="date">2018-04-01</span></li>
                     <li><a href="#none">3월 31일(금) 새벽시스템점검안내222</a><span class="date">2018-04-01</span></li>
@@ -57,7 +196,8 @@
                     <li><a href="#none">추석교재배송및고객센터휴무안내22</a><span class="date">2018-03-06</span></li>
                 </ul>
             </div>
-            <div id="notice3" class="tabContent" style="display: none;">
+            <div id="notice3" class="tabContent pd20">
+                <div class="moreBtn"><a href="#none">+ 더보기</a></div>
                 <ul class="List-Table">
                     <li><a href="#none">공지사항 제목이 출력됩니다.333</a><span class="date">2018-04-01</span></li>
                     <li><a href="#none">3월 31일(금) 새벽시스템점검안내33</a><span class="date">2018-04-01</span></li>
@@ -75,11 +215,52 @@
         </a>
     </div>
 
-    <div class="appPlayer c_both">
+    <div class="MainSlider swiper-container swiper-container-page c_both mt20">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide"><a href="#none"><img src="https://static.willbes.net/public/images/promotion/m/2002_734_120_20032301.jpg" alt="샘플"></a></div>            
+            <div class="swiper-slide"><a href="#none"><img src="https://static.willbes.net/public/images/promotion/m/2002_734_120_20032302.jpg" alt="샘플"></a></div>
+            <div class="swiper-slide"><a href="#none"><img src="https://static.willbes.net/public/images/promotion/m/2002_734_120_20032303.jpg" alt="샘플"></a></div>
+        </div>
+        <!-- Add Pagination -->
+        <div class="swiper-pagination"></div>
+    </div> 
+
+    <div class="appPlayer mt20">
         <a href="#none">
             <img src="{{ img_url('m/main/icon_app_player.gif') }}">
         </a>
-    </div>
+    </div>    
 </div>
 <!-- End Container -->
+
+<script>  
+    //경찰케스트
+    var swiper = new Swiper('.swiper-container-Lec', {
+      slidesPerView: 'auto',
+      spaceBetween: 2,
+      autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        }, //3초에 한번씩 자동 넘김
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+
+    //맛보기강의
+    var swiper = new Swiper('.swiper-container-view', {
+        slidesPerView: 1,
+        slidesPerColumn: 4,
+        spaceBetween: 10,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        }, //3초에 한번씩 자동 넘김
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });     
+</script> 
 @stop

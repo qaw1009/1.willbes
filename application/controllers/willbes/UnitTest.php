240dc9d9dac2ca84e76ce25c88ba637a03038407
신봉석 <bsshin@willbes.com>
2020-07-09 14:25:35 +0900

    [테스트제외] curl 테스트

--- application/controllers/willbes/UnitTest.php
+++ application/controllers/willbes/UnitTest.php
@@ -18,9 +18,35 @@ class UnitTest extends \app\controllers\FrontController
         var_dump('unit test controller method');
     }
 
-    public function npayResult()
+    public function curlSend()
     {
-        logger('npay register result', '', 'ERROR');
+        $url = 'https://www.dev.willbes.net/unitTest/npayResult';
+        $send_xml = '<?xml version="1.0" encoding="utf-8"?>';
+        $send_xml .= '<order>';
+        $send_xml .= '<item>1000</item>';
+        $send_xml .= '</order>';
+
+        $this->load->library('curl');
+        $this->curl->setOpt(CURLOPT_SSL_VERIFYPEER, false);
+        $this->curl->setOpt(CURLOPT_SSL_VERIFYHOST, false);
+        $this->curl->setOpt(CURLOPT_RETURNTRANSFER, true);
+        $this->curl->setOpt(CURLOPT_HTTPHEADER, ['Content-Type: application/xml; charset=utf-8']);
+        $this->curl->setOpt(CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+        $this->curl->setOpt(CURLOPT_TIMEOUT, 10);
+        $this->curl->setOpt(CURLOPT_FAILONERROR, false);
+        $this->curl->post($url, $send_xml);
+
+        if ($this->curl->error === true) {
+            echo $this->curl->errorMessage . ' (' . $this->curl->errorCode . ')';
+        } else {
+            echo $this->curl->rawResponse;
+        }
+
+        $this->curl->close();
+    }
+
+    public function curlResult()
+    {
         echo 'SUCCESS:1:2';
     }
 }

c16d155e326d94f28e9762b985d8835a72392fab
신봉석 <bsshin@willbes.com>
2020-07-10 18:42:29 +0900

    프런트 > 공무원 메인 링크 삭제

--- views/willbes/pc/site/main_2003_3019.blade.php
+++ views/willbes/pc/site/main_2003_3019.blade.php
@@ -38,9 +38,9 @@
 
         <div class="Section">
             <div class="widthAuto">
-                <a href="https://pass.willbes.net/promotion/index/cate/3019/code/1636" target="_blank">
+                {{--<a href="https://pass.willbes.net/promotion/index/cate/3019/code/1636" target="_blank">--}}
                     <img src="https://static.willbes.net/public/images/promotion/main/3019_graph.gif" alt="파이널 수강생 성적향상 효과 증명">
-                </a>
+                {{--</a>--}}
             </div>
         </div>
 

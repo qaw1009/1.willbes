61fbc5ac9b54089636d15d01e78a0c8017029014
신봉석 <bsshin@willbes.com>
2020-07-15 17:39:06 +0900

    프런트 > 윌스토리 서비스 연동 주소 적용

--- views/willbes/pc/site/book_store/show.blade.php
+++ views/willbes/pc/site/book_store/show.blade.php
@@ -109,7 +109,7 @@
                                             return false;
                                         }
                                     </script>
-                                    <script type="text/javascript" src="https://test-pay.naver.com/customer/js/naverPayButton.js" charset="utf-8"></script>
+                                    <script type="text/javascript" src="https://pay.naver.com/customer/js/naverPayButton.js" charset="utf-8"></script>
                                     <script type="text/javascript" >//<![CDATA[
                                         naver.NaverPayButton.apply({
                                             BUTTON_KEY: '{{ config_app('npay_btn_cert_key') }}', // 페이에서 제공받은 버튼 인증 키 입력

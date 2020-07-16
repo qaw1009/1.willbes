3b853c8466fd22d92b429c6e399ca09990b6402c
김창용 <winmir@willbes.com>
2020-07-16 13:29:20 +0900

    엔잡 양원근 모바일

--- views/willbes/m/promotion/2014/1722.blade.php
+++ views/willbes/m/promotion/2014/1722.blade.php
@@ -13,97 +13,28 @@
     .evt02 .dday {font-size:22px;padding:20px 0;}
     .evt02 .dday span {color:#a0774e; box-shadow:inset 0 -25px 0 rgba(0,0,0,0.1);}
 
-    .evt03 {background:#252525; padding-bottom:80px}   
-    .evt03 .btn {width:90%; margin:30px auto 0}
-    .evt03 .btn a {display:block; color:#282f4c; background:#fff; border-radius:30px; font-size:20px; padding:15px 0;
-        box-shadow: 0 1px 0 #cccccc, 0 2px 0 #cccccc, 0 3px 0 #cccccc, 0 4px 0 #cccccc, 0 5px 0 #cccccc, 0 6px 0 #cccccc, 0 7px 0 #cccccc, 0 8px 0 #cccccc, 0 9px 0 #cccccc, 0 50px 30px rgba(0, 0, 0, 0.3)
-    }
-    .evt03 .btn a:hover {color:#fff; background:#282f4c;}
+    .evt03 {background:#252525; padding-bottom:80px}      
 
-    .evt04 {}
-    .evt04 div {color:#01c73c; font-size:24px; font-weight:600; margin-top:20px}
-
-    .evt05 {text-align:left; padding:50px 20px}
-    .evt05 h5 {color:#01c73c; font-size:2.2rem; line-height:1; margin-bottom:20px; letter-spacing:-1px;}
-    .evt05 .evt05Txt01 {font-size:1.6rem;}
-    .evt05 dl {margin-top:30px;}
-    .evt05 dl:first-child {margin:0}
-    .evt05 dt {font-size:16px; font-weight:bold; color:#01c73c; margin-top:30px}
-    .evt05 dt:first-child {margin:0}
-    .evt05 .curriculum {margin:30px 0}
-    .evt05 .sample {margin:0 auto}
-    .evt05 .sample li {display:inline; float:left; width:49%; padding:10px 0; margin-right:1%; border-radius:10px; 
-        background:#01c73c; color:#fff; font-size:16px; font-weight:600; text-align:center}
-    .evt05 .sample li p {margin-bottom:15px;}
-    .evt05 .sample li a {display:inline-block; padding:5px 10px; font-size:14px; margin-right:5px; border-radius:4px}
-    .evt05 .sample li a.btnst01 {border:1px solid #ccc;}
-    .evt05 .sample li a.btnst02 {border:1px solid #000; color:#fff; background:#333}
-    .evt05 .sample li a.btnst03 {border:1px solid #ccc; color:#000; background:#ccc}
-    .evt05 .sample li a:hover {background:#000; color:#fff}
-    .evt05 .sample li:last-child {margin:0}
-    .evt05 .sample:after {content:""; display:block; clear:both}
-
-    .evt05 .evt05Txt02 {font-size:14px; line-height:1.4; letter-spacing:-1px; color:#333; margin-top:20px; text-align:left}   
- 
-    .evt06 {background:#ececec; padding-top:20px}
-    .evt06 .columns {padding:20px;
-        column-count: 1;
-        column-gap:20px;
-    }
-    .evt06 .columns div {
-        text-align:justify; font-size:14px; line-height:1.4;
-        display:inline-block;
-        padding:20px; border:1px solid #eee; border-radius:10px;
-        margin-bottom:20px; color:#888; background:#fff;
-        width:100%;
-    }
-    .evt06 .columns div p {border-bottom:1px solid #eee; margin-bottom:10px; padding-bottom:10px; color:#01c73c}
-    .evt06 .columns div:hover {box-shadow:0 10px 10px rgba(102,102,102,0.2); color:#000}
-    .evt06 .columns div strong {font-size:bold; color:#333}
-
-    .evt07 {background:#01c73c; padding:20px 0}
-    /*.evt07 li {display:inline; float:left; width:50%}*/
-    .evt07 li a {display:block; font-size:1rem; color:#fff; padding:20px 0; text-align:center; background:#000; line-height:1.5; border-radius:10px; margin:0 1.5%;}
-    .evt07 li a:hover {background:#fff; color:#000;
-        -webkit-animation: shadow-drop-2-center 0.4s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
-        animation: shadow-drop-2-center 0.4s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
-    }
-    .evt07 li span {display:block; font-size:1.25rem}
-    .evt07 ul:after {content:""; display:block; clear:both}
+    .evt04 {padding-bottom:80px}
+    .evt04 div {font-size:16px; font-weight:600; margin-top:20px}
 
     .video-container {position:relative; padding-bottom:56.25%; padding-top:30px; height:0; overflow: hidden;}
     .video-container iframe,
     .video-container object,
     .video-container embed {position:absolute; top:0; left:50%; width:100%; margin-left:-50%; height:100%;}
 
-    .btnbuyBox {width:100%; position:fixed; bottom:0; text-align:center; background:rgba(255,255,255,0.5); padding-top:10px}
-    .btnbuy a {display:block; width:100%; max-width:720px; margin:0 auto; font-size:1.5rem; background:#000; color:#fff; padding:15px 0; text-align:center; border-radius:50px; line-height:1.4}
-    .btnbuy a span {font-size:1.2rem;}
-    .btnbuy a:hover {background:#01c73c;
-        -webkit-animation: shadow-drop-2-center 0.4s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
-        animation: shadow-drop-2-center 0.4s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+    .evtCtnsBox .btn {width:90%; margin:30px auto 0}
+    .evtCtnsBox .btn a {display:block; color:#282f4c; background:#fff; border-radius:30px; font-size:20px; padding:15px 0; border:1px solid #333;
+        box-shadow: 0 1px 0 #cccccc, 0 2px 0 #cccccc, 0 3px 0 #cccccc, 0 4px 0 #cccccc, 0 5px 0 #cccccc, 0 6px 0 #cccccc, 0 7px 0 #cccccc, 0 8px 0 #cccccc, 0 9px 0 #cccccc, 0 50px 30px rgba(0, 0, 0, 0.3)
     }
-    .infoCheck {width:100%; max-width:720px; margin:10px auto; font-size:12px;}
-    .infoCheck label {margin-right:30px; cursor: pointer; }
-    .infoCheck input[type=checkbox] {width:20px; height:20px; margin-right:10px}
-    .infoCheck input[type=checkbox]:checked + label {border-bottom:1px dashed #0099ff; font-weight:bold; color:#0099ff} 
-    .infoCheck a {display:inline-block; background:#333; color:#fff; height:30px; line-height:30px; text-align:center; padding:0 20px; border-radius:20px}
-    .infoCheck a:hover {background:#01c73c;}
-
-    .evtFooter {margin:0 auto; padding:30px 20px; text-align:left; color:#666; font-size:0.875rem; line-height:1.4 }
-    .evtFooter h3 {font-size:1.5rem; margin-bottom:30px; color:#000}
-    .evtFooter p {margin-bottom:10px; color:#333; font-size:1.2rem;}
-    .evtFooter div,
-    .evtFooter ul {margin-bottom:30px; padding-left:10px}
-    .evtFooter li {margin-left:20px; list-style-type: decimal;}
+    .evtCtnsBox .btn a:hover {color:#fff; background:#282f4c;}
 
 
     /* 폰 가로, 태블릿 세로*/
     @@media only all and (min-width: 408px)  {        
         .evt02 .dday strong {font-size:32px}
         .evt02 .dday br {display:none} 
-        .evt03 .btn a {font-size:30px;}
-        
+        .evt03 .btn a {font-size:30px;}        
     }
 
     /* 태블릿 세로 */
@@ -140,7 +71,6 @@
             </div>
         </div>
         <div class="btn NSK-Black"><a href="javascript:goLecture();">지금, 사전 예약하고<br> 끝장 혜택받기 ></a></div>
-
     </div> 
 
     <div class="evtCtnsBox evt04">
@@ -148,7 +78,9 @@
         <img src="https://static.willbes.net/public/images/promotion/2020/07/1722_05.jpg" alt="" ><br>
         <img src="https://static.willbes.net/public/images/promotion/2020/07/1722_06.jpg" alt="" ><br>
         <img src="https://static.willbes.net/public/images/promotion/2020/07/1722_07.jpg" alt="" ><br>  
-        <img src="https://static.willbes.net/public/images/promotion/2020/07/1722_08.jpg" alt="" >        
+        <img src="https://static.willbes.net/public/images/promotion/2020/07/1722m_08.jpg" alt="" > 
+        <div>소문내기 이벤트는 PC버전에서 참여 가능합니다.</div>
+        <div class="btn NSK-Black"><a href="https://njob.willbes.net/promotion/index/cate/3114/code/1722" target="_blank">소문내기 이벤트 참여하기 ></a></div>       
     </div>  
 
     <div class="evtCtnsBox evt04">

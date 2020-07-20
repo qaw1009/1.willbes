91b8813ffed01917f4f4793682325387e7ae7db7
김창용 <winmir@willbes.com>
2020-07-20 13:45:01 +0900

    공무원 게이트 코딩 페이지 수정

--- views/html/main_gosi_gate.blade.php
+++ views/html/main_gosi_gate.blade.php
@@ -312,6 +312,7 @@
                         <li><a href="#none"><span>하승민 영어 2018년 3차 시험 적중!</span></a><span class="date">2018-09-01</span></li>
                         <li><a href="#none"><span>[공지] 2018년 제3차 경찰공무원(순경)채용 공고 입니다.</span></a><span class="date">2018-08-24</span></li>
                         <li><a href="#none"><span>[신규강의 안내] 해양경찰특채 11~12월 동영상 업데이트 안내</span></a><span class="date">2018-08-13</span></li>
+                        <li><a href="#none"><span>[신규강의 안내] 해양경찰특채 11~12월 동영상 업데이트 안내</span></a><span class="date">2018-08-13</span></li>
                     </ul>
                 </div>
                 <div class="noticeList widthAuto350 f_left ml35">
@@ -321,6 +322,7 @@
                         <li><a href="#none"><span>[공지] 2018년 제3차 경찰공무원 채용 필기시험 문제 및 가답안</span></a><span class="date">2018-09-01</span></li>
                         <li><a href="#none"><span>2018년 제3차 경찰공무원 채용시험 원서접수일정 안내입니다.</span></a><span class="date">2018-08-24</span></li>
                         <li><a href="#none"><span>[공지] 2018년 제2차 경찰공무원 채용시험 일정 안내입니다.</span></a><span class="date">2018-08-13</span></li>
+                        <li><a href="#none"><span>[공지] 2018년 제2차 경찰공무원 채용시험 일정 안내입니다.</span></a><span class="date">2018-08-13</span></li>
                     </ul>
                 </div>
                 <div class="noticeList widthAuto350 f_left ml35">
@@ -329,6 +331,7 @@
                         <li><a href="#none"><span>경찰청, 경찰간부후보생 68기 최종합격자 명단 입니다. 확인 바랍니다.</span><img src="{{ img_url('cop/icon_new.png') }}"></a><span class="date">2018-09-06</span></li>
                         <li><a href="#none"><span>12월 22일, 경찰공무원 합격의 기회가 다가옵니다.</span></a><span class="date">2018-09-01</span></li>
                         <li><a href="#none"><span>제주자치경찰 확대 시험운영 추진</span></a><span class="date">2018-08-24</span></li>
+                        <li><a href="#none"><span>순경 3차 '필기시험 대비, 기출 분석으로 철저하게'</span></a><span class="date">2018-08-13</span></li>
                         <li><a href="#none"><span>순경 3차 '필기시험 대비, 기출 분석으로 철저하게'</span></a><span class="date">2018-08-13</span></li>
                     </ul>
                 </div>
@@ -362,7 +365,7 @@
                             </li>
                             <li>
                                 <div class="nTit">학원 고객센터</div>
-                                <div class="nNumber tx-color">1544-0336</div>
+                                <div class="nNumber tx-color">1544-0330</div>
                                 <div class="nTxt">
                                     [전화/방문상담 운영시간]<br/>
                                     평일/주말: 09시~ 22시<br/>

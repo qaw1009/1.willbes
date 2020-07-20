db48ccdd24ea60781ba92a71cf42e06b2cb351df
신봉석 <bsshin@willbes.com>
2020-07-20 17:58:00 +0900

    프런트 > 공무원 인트로 퀵메뉴 추가

--- views/willbes/pc/site/intro/intro_2003.blade.php
+++ views/willbes/pc/site/intro/intro_2003.blade.php
@@ -311,6 +311,42 @@
             </div>
         </div>
         <!-- CS센터 //-->
+
+        <div id="QuickMenu" class="MainQuickMenu">
+            <ul>
+                @if(empty($data['dday']) === false)
+                    <li>
+                        <div class="QuickSlider">
+                            <div class="sliderNum">
+                                @foreach($data['dday'] as $row)
+                                    <div class="QuickDdayBox">
+                                        <div class="q_tit">{{$row['DayTitle']}}</div>
+                                        <div class="q_day">{{$row['DayDatm']}}</div>
+                                        <div class="q_dday NSK-Black">{{($row['DDay'] == 0) ? 'D-'.$row['DDay'] : 'D'.$row['DDay']}}</div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </li>
+                @endif
+                <li>
+                    <div class="QuickSlider">
+                        {!! banner_html(element('인트로_우측퀵_01', $data['banner']), 'sliderNum') !!}
+                    </div>
+                </li>
+                <li>
+                    <div class="QuickSlider">
+                        {!! banner_html(element('인트로_우측퀵_02', $data['banner']), 'sliderNum') !!}
+                    </div>
+                </li>
+                <li>
+                    <div class="QuickSlider">
+                        {!! banner_html(element('인트로_우측퀵_03', $data['banner']), 'sliderNum') !!}
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <!-- 퀵메뉴 //-->
     </div>
     <!-- End Container -->
     {!! popup('657005', $__cfg['SiteCode'], '0', '') !!}

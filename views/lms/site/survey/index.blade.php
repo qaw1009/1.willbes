bf07c1fcb4d9fcbe45c8d3b5854abe1a901ae69e
김성일 <jxr880210@willbes.com>
2020-07-22 13:51:40 +0900

    LMS > 설문조사

--- views/lms/site/survey/index.blade.php
+++ views/lms/site/survey/index.blade.php
@@ -4,39 +4,29 @@
     <h5 class="mt-20">- 설문을 등록하는 메뉴입니다.</h5>
     <h5 class="mt-20 red">- 설문링크 와 프로모션페이지 내 그래프를 willbes/pc/survey/index+spidx 와 willbes/pc/survey/graph+spidx 경로에 생성해주세요.</h5>
     <h5 class="mt-20 red">- 프로모션 블레이드에 &#64;include('willbes.pc.survey.show_graph_partial') </h5>
+
     <form class="form-horizontal" id="search_form" name="search_form" method="POST" onsubmit="return false;">
         {!! csrf_field() !!}
-
-        <div class="x_panel">
-            <div class="x_content">
-                <div class="form-group form-inline">
-
-                </div>
-            </div>
     </form>
+
     <form id="regi_form" name="regi_form" method="POST" onsubmit="return false;" novalidate>
         {!! csrf_field() !!}
         <input type="hidden" id="MgIdx" name="MgIdx" value="">
     </form>
 
-    <div style="text-align:right;">
-        <a class="btn btn-default btn-sm btn-primary border-radius-reset" tabindex="0" aria-controls="list_ajax_table" href="{{ site_url('/site/survey/surveyCreate') }}">
-            <span><i class="fa fa-pencil mr-5"></i> 등록</span>
-        </a>
-    </div>
     <div class="x_panel mt-10" style="overflow-x: auto; overflow-y: hidden;">
         <div class="x_content">
             <form class="form-horizontal" id="list_form" name="list_form" method="POST" onsubmit="return false;">
                 {!! csrf_field() !!}
                 <table id="list_table" class="table table-bordered table-striped table-head-row2 form-table">
                     <colgroup>
-                        <col style="width:5%">
+                        <col style="width:4%">
                         <col style="">
-                        <col style="width:18%">
-                        <col style="width:18%">
-                        <col style="width:18%">
-                        <col style="width:8%">
-                        <col style="width:8%">
+                        <col style="width:20%">
+                        <col style="width:17%">
+                        <col style="width:17%">
+                        <col style="width:7%">
+                        <col style="width:7%">
                     </colgroup>
                     <thead class="bg-white-gray">
                         <tr>
@@ -50,22 +40,82 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>[2020 국가직 9급 풀케어] 시험 난이도 설문조사</td>
-                            <td>https://www.local.willbes.net/survey/index/11</td>
-                            <td>프로모션 페이지 URL + /spidx/11</td>
-                            <td>2020-07-01 00:00:00 / 2020-07-15 00:00:00</td>
-                            <td>5</td>
-                            <td>사용</td>
-                        </tr>
                     </tbody>
                 </table>
             </form>
         </div>
     </div>
     <script type="text/javascript">
+        var $datatable;
+        var $search_form = $('#search_form');
+        var $list_form = $('#list_form');
+        var $list_table = $('#list_table');
+        var $regi_form = $('#regi_form');
 
+        $(document).ready(function() {
 
+            // DataTables
+            $datatable = $list_table.DataTable({
+                info: true,
+                language: {
+                    "info": "[ 총 _END_ / _MAX_건 ]",
+                },
+                pageLength: 10,
+                serverSide: true,
+                buttons: [
+                    { text: '<i class="fa fa-pencil mr-5"></i> 등록', className: 'btn-sm btn-primary border-radius-reset', action: function(e, dt, node, config) {
+                            location.href = '{{ site_url('/site/survey/eventSurveyCreate') }}' + dtParamsToQueryString($datatable);
+                        }}
+                ],
+                ajax: {
+                    'url' : '{{ site_url('/site/survey/eventSurveyList') }}',
+                    'type' : 'POST',
+                    'data' : function(data) {
+                        return $.extend(arrToJson($search_form.serializeArray()), {'start' : data.start, 'length' : data.length});
+                    }
+                },
+                columns: [
+                    {'data' : null, 'class': 'text-center', 'render' : function(data, type, row, meta) {
+                            return $datatable.page.info().recordsTotal - (meta.row + meta.settings._iDisplayStart);
+                        }},
+                    {'data' : 'SpTitle', 'class': 'text-center', 'render' : function(data, type, row, meta) {
+                            return '<a href="javascript:void(0);" class="btn-modify" data-idx="' + row.SpIdx + '"><u class="blue">' + row.SpTitle + '</u></a>';
+                        }},
+                    {'data' : 'link', 'class': 'text-center'},
+                    {'data' : 'include', 'class': 'text-center'},
+                    {'data' : null, 'class': 'text-center', 'render' : function(data, type, row, meta) {
+                            var date = row.StartDate + ' / ' + row.EndDate;
+                            return date;
+                        }},
+                    {'data' : null, 'class': 'text-center', 'render' : function(data, type, row, meta) {
+                            var date = row.CNT > 0 ? '<button class="btn btn-sm btn-primary act-move" onClick="popResult(' + row.SpIdx + ')">' + row.CNT + '</button>' : 0;
+                            return date;
+                        }},
+                    {'data' : 'SpIsUse', 'class': 'text-center', 'render' : function(data, type, row, meta) {
+                            var SpUseYn = (row.SpIsUse === 'Y') ? '<span>사용</span>' : '<span class="red">미사용</span>';
+                            return SpUseYn;
+                        }},
+                ]
+            });
+
+            // 데이터 수정 폼
+            $list_table.on('click', '.btn-modify', function() {
+                location.href='{{ site_url('/site/survey/eventSurveyCreate/') }}' + $(this).data('idx') + dtParamsToQueryString($datatable);
+            });
+
+        });
+
+        function popResult(idx){
+
+            var uri_param;
+
+            uri_param = 'spidx=' + idx;
+
+            var _url = '{{ site_url('/predict/survey/surveyResult') }}' + '?' + uri_param;
+
+            win = window.open(_url, 'surveyPopup', 'width=1100, height=845, scrollbars=yes, resizable=yes');
+            win.focus();
+
+        }
     </script>
 @stop

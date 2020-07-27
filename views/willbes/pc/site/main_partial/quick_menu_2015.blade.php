1944060d3d328398de37c16ecec13f587774464e
최형진 <letsboo@willbes.com>
2020-07-27 14:57:54 +0900

    프론트 > 인천학원 메인 - 퀵메뉴 배너 수정

--- views/willbes/pc/site/main_partial/quick_menu_2015.blade.php
+++ views/willbes/pc/site/main_partial/quick_menu_2015.blade.php
@@ -1,4 +1,8 @@
-{!! banner_html(element('메인_퀵배너1', $data['arr_main_banner'])) !!}
+<li>
+    <div class="QuickSlider">
+        {!! banner_html(element('메인_퀵배너1', $data['arr_main_banner']), 'sliderNum') !!}
+    </div>
+</li>
 <ul class="mb5 gobtn">
     <li><a href="{{ front_url('/offinfo/boardInfo/index/78') }}">이달의 개강안내</a></li>
     <li><a href="{{ front_url('/offinfo/gallery/index') }}">학원 갤러리</a></li>
@@ -6,5 +10,13 @@
     <li><a href="{{ front_url('/offinfo/boardInfo/index/89') }}">모의고사</a></li>
     <li><a href="https://pf.kakao.com/_xapHUxb" target="_blank">실시간 소통실</a></li>
 </ul>
-{!! banner_html(element('메인_퀵배너2', $data['arr_main_banner'])) !!}
-{!! banner_html(element('메인_퀵배너3', $data['arr_main_banner'])) !!}
+<li>
+    <div class="QuickSlider">
+        {!! banner_html(element('메인_퀵배너2', $data['arr_main_banner']), 'sliderNum') !!}
+    </div>
+</li>
+<li>
+    <div class="QuickSlider">
+        {!! banner_html(element('메인_퀵배너3', $data['arr_main_banner']), 'sliderNum') !!}
+    </div>
+</li>

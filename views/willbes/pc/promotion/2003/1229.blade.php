44e32c74c19072c4ff2b328fc4e46dc081071f62
이윤구 <yoongukk@willbes.com>
2020-08-14 15:24:01 +0900

    (어학) 서민지 G-TELP 페이지 링크 직렬별 수정

--- views/willbes/pc/promotion/2003/1229.blade.php
+++ views/willbes/pc/promotion/2003/1229.blade.php
@@ -112,9 +112,15 @@
 		@endif
 
 		<div class="evtCtnsBox wb_cts07">
-			<a href="https://lang.willbes.net/package/show/cate/3024/pack/648001/prod-code/170880" target="_blank">
+			@if(empty($__cfg['CateCode']) === false && $__cfg['CateCode'] == '3024')	
+			<a href="{{ site_url('/package/show/cate/' . $__cfg['CateCode'] . '/pack/648001/prod-code/170880') }}" target="_blank">
 				<img src="https://static.willbes.net/public/images/promotion/2020/07/1425_05.png" title="수강신청">
 			</a>
+			@elseif(empty($__cfg['CateCode']) === false && $__cfg['CateCode'] == '3020')
+			<a href="{{ site_url('/package/show/cate/' . $__cfg['CateCode'] . '/pack/648001/prod-code/170880') }}" target="_blank">
+				<img src="https://static.willbes.net/public/images/promotion/2020/07/1425_05.png" title="수강신청">
+			</a>
+			@endif      
 		</div>
 	  
 	</div>

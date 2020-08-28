21d28604b62dbd5630e78b47c155021ab74f5148
최형진 <letsboo@willbes.com>
2020-08-28 17:42:32 +0900

    프론트 > 1773 프로모션 - 쿠폰발급 개발

--- views/willbes/pc/promotion/2002/1773.blade.php
+++ views/willbes/pc/promotion/2002/1773.blade.php
@@ -96,6 +96,11 @@
 
     </style>
 
+    <form id="regi_form" name="regi_form" method="POST" onsubmit="return false;" novalidate>
+        {!! csrf_field() !!}
+        {!! method_field('POST') !!}
+    </form>
+
     <div class="p_re evtContent NGR" id="evtContainer">        
 
         <ul class="sky">
@@ -281,11 +286,27 @@
     <!-- End Container -->
 
     <script type="text/javascript">
-       
         /*디데이카운트다운*/
         $(document).ready(function() {
-        dDayCountDown('{{$arr_promotion_params['edate']}}');
+            dDayCountDown('{{$arr_promotion_params['edate']}}');
         });
+
+        {{--쿠폰발급--}}
+        var $regi_form = $('#regi_form');
+        function giveCheck() {
+            {!! login_check_inner_script('로그인 후 이용하여 주십시오.','') !!}
+            @if(empty($arr_promotion_params) === false)
+                var _check_url = '{!! front_url('/promotion/promotionEventCheck/') !!}?give_type={{$arr_promotion_params["give_type"]}}&give_idx={{$arr_promotion_params["give_idx"]}}&comment_chk_yn={{$arr_promotion_params["comment_chk_yn"]}}&event_code={{$data['ElIdx']}}';
+                ajaxSubmit($regi_form, _check_url, function (ret) {
+                    if (ret.ret_cd) {
+                        alert('쿠폰이 발급되었습니다. \n\n내강의실에서 확인해 주세요.');
+                        {{--location.href = '{{ app_url('/classroom/coupon/index', 'www') }}';--}}
+                    }
+                }, showValidateError, null, false, 'alert');
+            @else
+                alert('프로모션 추가 파라미터가 지정되지 않았습니다.');
+            @endif
+        }
     </script>
 
     {{-- 프로모션용 스크립트 include --}}

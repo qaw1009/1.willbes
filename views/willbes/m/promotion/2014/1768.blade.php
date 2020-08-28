7cc07537cc3482581c09526d8f4fa6658305143f
김창용 <winmir@willbes.com>
2020-08-28 11:35:12 +0900

    모바일 엔잡 소개

---
+++ views/willbes/m/promotion/2014/1768.blade.php
@@ -0,0 +1,35 @@
+@extends('willbes.m.layouts.master')
+
+@section('content')
+    <!-- Container -->
+    <style type="text/css">
+        .evtCtnsBox {width:100%; max-width:720px; margin:0 auto; text-align:center; position:relative; font-size:14px; line-height:1.5}
+        .evtCtnsBox img {width:100%; max-width:720px;}
+
+        /************************************************************/
+    </style>
+
+
+    <div class="p_re evtContent NSK" id="evtContainer">        
+        <div class="evtCtnsBox">
+            <img src="https://static.willbes.net/public/images/promotion/2020/08/1768m_01.jpg" alt="" > 
+        </div> 
+        
+        <div class="evtCtnsBox">
+            <img src="https://static.willbes.net/public/images/promotion/2020/08/1768m_02.gif" alt="" >            
+        </div>
+
+        <div class="evtCtnsBox">
+            <img src="https://static.willbes.net/public/images/promotion/2020/08/1768m_03.jpg" alt="" > 
+        </div>
+
+        <div class="evtCtnsBox">
+            <img src="https://static.willbes.net/public/images/promotion/2020/08/1768m_04.gif" alt="" >            
+        </div>
+
+        <div class="evtCtnsBox">
+            <img src="https://static.willbes.net/public/images/promotion/2020/08/1768m_05.jpg" alt="" > 
+        </div>
+    </div>
+    <!-- End Container -->
+@stop

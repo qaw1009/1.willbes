2023f93bb2d00efab585d5d5bba888c45f5bc5fa
김창용 <winmir@willbes.com>
2020-09-02 16:47:45 +0900

    공무원 농업직, 전기통신

--- views/willbes/pc/promotion/2001/1794.blade.php
+++ views/willbes/pc/promotion/2001/1794.blade.php
@@ -14,10 +14,18 @@
     .evtContent span {vertical-align:auto}
     .evtCtnsBox {width:100%; text-align:center; min-width:1120px; position:relative;}
     /*****************************************************************/ 
-    .evt_top {background:url(https://static.willbes.net/public/images/promotion/2020/08/1772_top_bg.jpg) no-repeat center top;}
+    .evt_top {background:url(https://static.willbes.net/public/images/promotion/2020/09/1794_top_bg.jpg) no-repeat center top;}
     .evt00 {background:#0a0a0a}
     .evt01 {background:#f1f1f1;}
     .evt02 {background:#fff}
+    .evt03 {background:#fff}
+    /* 슬라이드배너 */
+    .slide_con {position:relative; width:1120px; margin:0 auto}	
+    .slide_con p {position:absolute; top:50%; width:56px; height:56px; z-index:100}
+    .slide_con p a {cursor:pointer}
+    .slide_con p.leftBtn {left:0; top:46%; width:67px; height:67px;}
+    .slide_con p.rightBtn {right:0;top:46%; width:67px; height:67px;}
+    .evt03 {background:#fff;}
     </style>
 
     <form id="regi_form" name="regi_form" method="POST" onsubmit="return false;" novalidate>
@@ -27,19 +35,39 @@
 
     <div class="evtContent NGR" id="evtContainer">  
         <div class="evtCtnsBox evt00">
-            <img src="https://static.willbes.net/public/images/promotion/2020/08/1772_00.jpg" alt="대한민국 경찰학원 1위">
+            <img src="https://static.willbes.net/public/images/promotion/2020/09/1794_00.jpg" alt="대한민국 경찰학원 1위">
         </div> 
 
         <div class="evtCtnsBox evt_top">  
-            <div><img src="https://static.willbes.net/public/images/promotion/2020/08/1772_top.jpg" alt="코로나 19 극복 긴급지원프로젝트"></div>
+            <div><img src="https://static.willbes.net/public/images/promotion/2020/09/1794_top.jpg" alt="코로나 19 극복 긴급지원프로젝트"></div>
         </div>
 
         <div class="evtCtnsBox evt01">
-            <img src="https://static.willbes.net/public/images/promotion/2020/08/1772_01.jpg" alt="강사진">
+            <img src="https://static.willbes.net/public/images/promotion/2020/09/1794_01.jpg" alt="강사진">
         </div>  
 
         <div class="evtCtnsBox evt02">
-            <img src="https://static.willbes.net/public/images/promotion/2020/08/1772_02.jpg" alt="코로나19 극복 이벤트" usemap="#Map1772" border="0">
+            <img src="https://static.willbes.net/public/images/promotion/2020/09/1794_02.jpg" alt="강사진">
+        </div>
+
+        <div class="evtCtnsBox evt03">
+            <div class="slide_con">
+                <ul id="slidesImg3">
+                    <li><img src="https://static.willbes.net/public/images/promotion/2020/05/1628_02_01.jpg" alt="신광은" /></li>
+                    <li><img src="https://static.willbes.net/public/images/promotion/2020/05/1628_02_02.jpg" alt="장정훈" /></li>
+                    <li><img src="https://static.willbes.net/public/images/promotion/2020/05/1628_02_03.jpg" alt="김원욱" /></li>
+                    <li><img src="https://static.willbes.net/public/images/promotion/2020/05/1628_02_04.jpg" alt="하승민" /></li>
+                    <li><img src="https://static.willbes.net/public/images/promotion/2020/05/1628_02_05.jpg" alt="오태진" /></li>
+                    <li><img src="https://static.willbes.net/public/images/promotion/2020/05/1628_02_06.jpg" alt="원유철" /></li>
+                    <li><img src="https://static.willbes.net/public/images/promotion/2020/05/1628_02_07.jpg" alt="김현정" /></li>
+                </ul>
+                <p class="leftBtn"><a id="imgBannerLeft3"><img src="https://static.willbes.net/public/images/promotion/2019/04/1199_arrow_prev.png"></a></p>
+                <p class="rightBtn"><a id="imgBannerRight3"><img src="https://static.willbes.net/public/images/promotion/2019/04/1199_arrow_next.png"></a></p>
+            </div>
+        </div>
+
+        <div class="evtCtnsBox evt04">
+            <img src="https://static.willbes.net/public/images/promotion/2020/09/1794_04.jpg" alt="코로나19 극복 이벤트" usemap="#Map1772" border="0">
             <map name="Map1772">
                 <area shape="rect" coords="256,720,865,815" href="javascript:;" onclick="giveCheck()" alt="할인쿠폰 받기">
                 <area shape="rect" coords="364,1174,750,1222" href="@if($file_yn[0] == 'Y') {{ front_url($file_link[0]) }} @else {{ $file_link[0] }} @endif" alt="이벤트이미지받운받기">
@@ -48,7 +76,7 @@
         
         {{--홍보url--}}
         @if( empty($data['data_option_ccd']) === false && array_key_exists($arr_base['option_ccd']['comment_list'], $data['data_option_ccd']) === true && array_key_exists($arr_base['comment_use_area']['event'], $data['data_comment_use_area']) === true)
-                    @include('willbes.pc.promotion.show_comment_list_url_partial')
+            @include('willbes.pc.promotion.show_comment_list_url_partial')
         @endif
     </div>
     <!-- End Container -->
@@ -82,6 +110,31 @@
             }, showValidateError, null, false, 'alert');
             @endif
         }
+
+        $(document).ready(function() {
+            var slidesImg3 = $("#slidesImg3").bxSlider({
+                mode:'horizontal', //option : 'horizontal', 'vertical', 'fade'
+                auto:true,
+                speed:350,
+                pause:4000,
+                pager:false,
+                controls:false,
+                minSlides:1,
+                maxSlides:1,
+                slideWidth:1120,
+                slideMargin:0,
+                autoHover: true,
+                moveSlides:1
+                });
+            
+                $("#imgBannerLeft3").click(function (){
+                    slidesImg3.goToPrevSlide();
+                });
+            
+                $("#imgBannerRight3").click(function (){
+                    slidesImg3.goToNextSlide();
+                });
+        });
     </script>
 
     {{-- 프로모션용 스크립트 include --}}
---
+++ views/willbes/pc/promotion/2003/1787.blade.php
@@ -0,0 +1,130 @@
+@extends('willbes.pc.layouts.master')
+
+@section('content')
+    @include('willbes.pc.layouts.partial.site_menu')
+    <!-- Container -->
+    <style type="text/css">
+        .evtContent {
+            width:100% !important;
+            min-width:1120px !important;
+            margin-top:20px !important;
+            padding:0 !important;
+            background:#fff;
+        }
+        .evtCtnsBox {width:100%; text-align:center; min-width:1120px; position:relative}
+
+        /************************************************************/
+
+        .evtTop {background:#154E3B url(https://static.willbes.net/public/images/promotion/2020/09/1787_top_bg.jpg) no-repeat center top;}
+
+        .evt01 {background:#272f4c; padding-bottom:150px}
+        /* TAB */
+        .evt01 .tab {position:absolute; width:1064px; top:322px; left:50%; margin-left:-532px}		
+        .evt01 .tab li {display:inline; float:left; width:50%}
+        .evt01 .tab a {display:block}
+        .evt01 .tab li:frist-chlid a {text-align:right}
+        .evt01 .tab li:last-chlid a {text-align:left}        
+        .evt01 .tab a img.off {display:block}
+        .evt01 .tab a img.on {display:none}
+        .evt01 .tab a.active img.off {display:none}
+        .evt01 .tab a.active img.on {display:block}
+        .evt01 .tab:after {content:""; display:block; clear:both}
+        .evt01 div {margin-top:68px}
+
+        .youtube {position:absolute; top:297px; left:50%; width:607px; z-index:1}
+        .youtube iframe {width:607px; height:342px}
+
+
+        .evt02 {background:url(https://static.willbes.net/public/images/promotion/2020/09/1787_02_bg.jpg) no-repeat center top;}
+        .evt02 .youtube {margin-left:-140px}
+
+        .evt03 {background:url(https://static.willbes.net/public/images/promotion/2020/09/1787_03_bg.jpg) no-repeat center top;}
+        .evt03 .youtube {top:269px; margin-left:-469px;}
+
+        
+    </style>
+
+
+    <div class="p_re evtContent NSK" id="evtContainer">   
+        <div class="evtCtnsBox evtTop">
+            <img src="https://static.willbes.net/public/images/promotion/2020/09/1787_top.jpg" title="최우영 T-PASS" />          
+        </div>
+
+        <div class="evtCtnsBox evt01">
+            <img src="https://static.willbes.net/public/images/promotion/2020/09/1787_01.jpg" title="단기 성적형상에 효과적" />
+            <ul class="tab" id="apply">
+                <li>
+                    <a href="#tab1">
+                        <img src="https://static.willbes.net/public/images/promotion/2020/09/1787_01_tab01.png" class="off" alt=""/>
+                        <img src="https://static.willbes.net/public/images/promotion/2020/09/1787_01_tab01_on.png" class="on"  />
+                    </a>
+                </li>
+                <li>
+                    <a href="#tab2">
+                        <img src="https://static.willbes.net/public/images/promotion/2020/09/1787_01_tab02.png" class="off" alt=""/>
+                        <img src="https://static.willbes.net/public/images/promotion/2020/09/1787_01_tab02_on.png" class="on"  />
+                    </a>
+                </li>
+            </ul>
+            <div id="tab1">
+                <img src="https://static.willbes.net/public/images/promotion/2020/09/1787_01_01.jpg" title="농업직" />
+            </div>
+            <div id="tab2">
+                <img src="https://static.willbes.net/public/images/promotion/2020/09/1787_01_02.jpg" title="전기통신직"/>
+            </div> 
+        </div>
+
+        <div class="evtCtnsBox evt02">
+            <img src="https://static.willbes.net/public/images/promotion/2020/09/1787_02.jpg" usemap="#Map1787_01" title="카폐바로가기" border="0" />
+            <map name="Map1787_01" id="Map1728_cafe">
+                <area shape="rect" coords="104,577,329,627" href="https://pass.willbes.net/professor/show/cate/3028/prof-idx/50429/?subject_idx=1171&subject_name=%EC%9E%AC%EB%B0%B0%ED%95%99" target="_blank" />
+              <area shape="rect" coords="281,996,838,1094" href="https://pass.willbes.net/promotion/index/cate/3028/code/1068" target="_blank" alt="농업직패키지">
+            </map>
+            {{--
+            <div class="youtube">
+                <iframe src="https://www.youtube.com/embed/_RDnE7u4k8U?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            --}}
+        </div>
+
+        <div class="evtCtnsBox evt03">
+            <img src="https://static.willbes.net/public/images/promotion/2020/09/1787_03.jpg" usemap="#Map1787_02" title="최우영이 정답" border="0" />
+            <map name="Map1787_02">
+              <area shape="rect" coords="786,546,1017,604" href="https://pass.willbes.net/professor/show/cate/3028/prof-idx/50163/?subject_idx=1193&subject_name=%EC%A0%84%EC%9E%90%EA%B3%B5%ED%95%99" target="_blank" alt="전기통신">
+              <area shape="rect" coords="280,970,839,1063" href="https://pass.willbes.net/promotion/index/cate/3028/code/1071" target="_blank" alt="전기통신 패키지">
+            </map>
+            <div class="youtube">
+                <iframe src="https://www.youtube.com/embed/_RDnE7u4k8U?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div> 
+
+    </div>
+    <!-- End Container -->
+
+    <script type="text/javascript">
+        $(document).ready(function(){
+        $('.tab').each(function(){
+            var $active, $content, $links = $(this).find('a');
+            $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+            $active.addClass('active');
+        
+            $content = $($active[0].hash);
+        
+            $links.not($active).each(function () {
+            $(this.hash).hide()});
+        
+            // Bind the click event handler
+            $(this).on('click', 'a', function(e){
+            $active.removeClass('active');
+            $content.hide();
+        
+            $active = $(this);
+            $content = $(this.hash);
+        
+            $active.addClass('active');
+            $content.show();
+        
+            e.preventDefault()})})}
+        );      
+    </script>
+@stop

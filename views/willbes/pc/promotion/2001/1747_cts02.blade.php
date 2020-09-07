d5c99c5077c1c4d20c7fe60401be66ff558d9c14
이윤구 <yoongukk@willbes.com>
2020-09-07 15:38:51 +0900

    2차 합격풀케어 서비스 페이지 <합격예측 부분> 페이지 추가

--- views/willbes/pc/promotion/2001/1747_cts02.blade.php
+++ views/willbes/pc/promotion/2001/1747_cts02.blade.php
@@ -1,10 +1,27 @@
-<style type="text/css">
-    .cts02_top {background:url(https://static.willbes.net/public/images/promotion/2020/03/1555_comingsoon_bg.jpg) no-repeat center top;}
+<style type="text/css">  
+        .cts02_top {background:#203141 url(https://static.willbes.net/public/images/promotion/2020/09/1747_cts02_top_bg.jpg) repeat-x center top; position:relative; padding-bottom:100px;} 
+        .cts02_02 {background:#eaeaea}
+        .cts02_03 {background:#fff} 
 </style>
-<div>
-    <div class="evtCtnsBox cts02_top">
-        <img src="https://static.willbes.net/public/images/promotion/2020/03/1555_comingsoon.jpg" alt="커밍쑨" />
-        
-    </div>
-    
-</div>
+    <div>
+        <div class="evtCtnsBox cts02_top">             
+            <img src="https://static.willbes.net/public/images/promotion/2020/09/1747_cts02_top.jpg" alt="합격예측 풀서비스" usemap="#Map1747_cts02_top" border="0">
+            <map name="Map1747_cts02_top" id="Map1747_cts02_top">
+                <area shape="rect" coords="291,1278,833,1383" href="police.willbes.net/promotion/index/cate/3001/code/1775" target="_blank" />
+            </map>
+        </div>
+
+        <div class="evtCtnsBox cts02_02" id="cts02_02">
+            <img src="https://static.willbes.net/public/images/promotion/2020/09/1747_cts02_02.jpg" alt="라이브 토크쇼" usemap="#Map1747_cts02_02" border="0">
+            <map name="Map1747_cts02_02" id="Map1747_cts02_02">
+                <area shape="rect" coords="337,1832,769,1925" href="https://www.youtube.com/channel/UCQ-jvqaobw6E9EvnFO88vwQ" target="_blank" />
+            </map>
+        </div>
+
+        <div class="evtCtnsBox cts02_03">
+            <img src="https://static.willbes.net/public/images/promotion/2020/09/1747_cts02_03.jpg" alt="신광은 경찰팀" usemap="#Map1747_cts02_03" border="0">
+            <map name="Map1747_cts02_03" id="Map1747_cts02_03">
+                <area shape="rect" coords="361,1224,767,1312" href="https://police.willbes.net/promotion/index/cate/3002/code/1766" target="_blank" />
+            </map>
+        </div>
+	</div>

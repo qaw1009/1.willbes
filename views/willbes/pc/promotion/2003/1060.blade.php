e8d7edf4777ce8b6df90f1b65ba14340c78b7b0c
김창용 <winmir@willbes.com>
2020-09-09 09:12:20 +0900

    공무원 소방직 패스

--- views/willbes/pc/promotion/2003/1060.blade.php
+++ views/willbes/pc/promotion/2003/1060.blade.php
@@ -216,7 +216,7 @@
                     <input name="ischk"  type="checkbox" value="Y" />
                     페이지 하단 이용안내를 모두 확인하였고, 이에 동의합니다.
                 </label>
-                <a href="#tab1">이용안내확인하기 ↓</a>
+                <a href="#careful">이용안내확인하기 ↓</a>
             </div>    
         </div><!--evt04//-->
 

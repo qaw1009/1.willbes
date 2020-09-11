daf1e7737a538e4d24dc55988d29597c6a36d76e
김창용 <winmir@willbes.com>
2020-09-11 15:12:31 +0900

    경찰 신광은 형사법

--- views/willbes/pc/promotion/2001/1801.blade.php
+++ views/willbes/pc/promotion/2001/1801.blade.php
@@ -50,13 +50,13 @@
         </div>  
 
         <div class="evtCtnsBox evt02">
-            <img src="https://static.willbes.net/public/images/promotion/2020/09/1801_02.jpg" alt="형사법 무료특강" usemap="#Map1801_01" border="0">
+            <img src="https://static.willbes.net/public/images/promotion/2020/09/1801_02.jpg" alt="형사법 무료특강" usemap="#Map1801_01">
             <map name="Map1801_01">
               <area shape="rect" coords="130,403,245,443" href="https://police.willbes.net/lecture/show/cate/3001/pattern/free/prod-code/167023" target="_blank">
               <area shape="rect" coords="379,405,495,442" href="https://police.willbes.net/lecture/show/cate/3001/pattern/free/prod-code/167489" target="_blank">
               <area shape="rect" coords="632,404,741,442" href="https://police.willbes.net/lecture/show/cate/3001/pattern/free/prod-code/168022" target="_blank">
               <area shape="rect" coords="878,403,989,443" href="https://police.willbes.net/lecture/show/cate/3001/pattern/free/prod-code/168198" target="_blank">
-              <area shape="rect" coords="132,594,246,626" href="https://police.willbes.net/lecture/show/cate/3001/pattern/free/prod-code/168198" target="_blank">
+              <area shape="rect" coords="132,594,246,626" href="https://police.willbes.net/lecture/show/cate/3001/pattern/free/prod-code/169145" target="_blank">
               <area shape="rect" coords="381,594,493,627" href="https://police.willbes.net/lecture/show/cate/3001/pattern/free/prod-code/170566" target="_blank">
               <area shape="rect" coords="629,594,741,627" href="https://police.willbes.net/lecture/show/cate/3001/pattern/free/prod-code/172060" target="_blank">
             </map>            

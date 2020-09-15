b52c6f827787860567c0d5c89ea481f5977fbaee
김창용 <winmir@willbes.com>
2020-09-15 09:21:05 +0900

    임용 프로모션 html

--- views/html/promotion/ssam_200130_skybanner.blade.php
+++ views/html/promotion/ssam_200130_skybanner.blade.php
@@ -15,13 +15,13 @@
 </style>
 <ul class="skybanner">
     <li><a href='ssam_190924_wsammjs'>유아 <strong>민정선</strong></a></li>
-    <li><a href='#none'>초등 <strong>배재민</strong></a></li>
+    <li><a href='ssam_200130_wsambjm'>초등 <strong>배재민</strong></a></li>
     <li><a href='ssam_200130_wsamkcw'>교육학 <strong>김차웅</strong></a></li>
     <li><a href='ssam_200130_wsam14'>교육학 <strong>이인재</strong></a></li>
-    <li><a href='#none'>교육학 <strong>홍의일</strong></a></li>
-    <li><a href='#none'>전공국어 <strong>송원영</strong></a></li>
-    <li><a href='#none'>전공국어 <strong>이원근</strong></a></li>
-    <li><a href='#none'>전공국어 <strong>권보민</strong></a></li>
+    <li><a href='ssam_200130_wsamhei'>교육학 <strong>홍의일</strong></a></li>
+    <li><a href='ssam_191128_wsam32'>전공국어 <strong>송원영</strong></a></li>
+    <li><a href='ssam_190924_wsam22'>전공국어 <strong>이원근</strong></a></li>
+    <li><a href='ssam_191212_wsamkbm'>전공국어 <strong>권보민</strong></a></li>
     <li><a href='#none'>전공영어 <strong>김유석</strong></a></li>
     <li><a href='#none'>전공영어 <strong>김영문</strong></a></li>
     <li><a href='#none'>전공영어 <strong>공훈</strong></a></li>

d8837446d6db99d281874c37faa0671f3e25df61
김창용 <winmir@willbes.com>
2020-09-15 17:31:36 +0900

    임용 강사별 프로모션

---
+++ views/html/promotion/ssam_200130_wsamyhj.blade.php
@@ -0,0 +1,49 @@
+@extends('willbes.pc.layouts.master')
+@section('content')
+    @include('willbes.pc.layouts.partial.site_menu')
+    <!-- Container -->
+    <style type="text/css">
+        .evtContent {
+            position:relative;
+            width:100% !important;
+            min-width:1278px !important;
+            margin-top:20px !important;
+            padding:0 !important;
+            background:#fff;
+            color:#3a3a3a;
+        }
+        .evtContent span {vertical-align:auto}
+        .evtCtnsBox {width:100%; text-align:center; min-width:1278px; position:relative;}
+
+        /************************************************************/
+        .evtTop {background:url(https://static.willbes.net/public/images/promotion/2020/09/200130_wsamyhj_top_bg.jpg) no-repeat center top;}
+        .evt01 {background:url(https://static.willbes.net/public/images/promotion/2020/09/200130_wsamyhj_01_bg.jpg) no-repeat center top;}
+        .evt02 {background:url(https://static.willbes.net/public/images/promotion/2020/09/200130_wsamyhj_02_bg.jpg) no-repeat center top; height:1069px}
+        .evt02 iframe {margin-top:300px}
+    </style>
+
+    <div class="p_re evtContent NSK" id="evtContainer">
+        <div class="evtCtnsBox ev01">
+            @include('html.promotion.ssam_200130_skybanner')
+            <div class="evtTop">
+                <img src="https://static.willbes.net/public/images/promotion/2020/09/200130_wsamyhj_top.jpg" alt="전공영어 공훈" usemap="#Mapkcw01" border="0" />
+                <map name="Mapkcw01" id="Mapkcw01">
+                    <area shape="rect" coords="322,828,708,925" href="#none" alt="설명회보기" />
+                    <area shape="rect" coords="750,826,934,929" href="#none" alt="설명회자료다운" />
+                </map>
+            </div>
+
+            <div class="evt01">
+                <img src="https://static.willbes.net/public/images/promotion/2020/09/200130_wsamyhj_01.jpg" usemap="#Mapkcw02" border="0">
+                <map name="Mapkcw02" class="review_btn" id="wsamyhj">
+                    <area shape="rect" coords="425,1352,865,1428" href="#none" alt="합격수기확인" />
+                </map>
+            </div>
+
+            <div class="evt02">
+                <iframe width="853" height="480" src="https://www.youtube.com/embed/sUpPfLo3Z_4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+       	    </div>
+        </div>
+    </div>
+    <!-- End Container -->
+@stop

26b2f7e17f558d8132c16d7a8bdfb9dd5b95ecb5
김창용 <winmir@willbes.com>
2020-09-21 10:22:41 +0900

    통합 로그인 유패스 배너 추가

--- views/willbes/pc/member/login/form.blade.php
+++ views/willbes/pc/member/login/form.blade.php
@@ -10,56 +10,61 @@
                 <img src="{{ img_url('login/logo.gif') }}">
                 <span class="tx-blue">로그인</span>
             </div>
+
             <!-- 로그인 -->
-            <div class="Member mem-Login widthAuto460">
-                <div class="inputBox p_re">
-                    <input type="text" id="id" name="id" class="iptId" placeholder="아이디" maxlength="30" required="required" title="아이디" value="{{ $saved_member_id }}">
-                </div>
-                <div class="inputBox p_re">
-                    <input type="password" id="pwd" name="pwd" class="iptPwd" placeholder="비밀번호" maxlength="30" required="required" title="비밀번호">
-                </div>
-                <div class="tx-red item" style="display: block;" id="ret_msg"></div>
-                <div class="chkBox mt30">
-                    <ul>
-                        <li class="chkBox-Save">
-                            <input type="checkbox" id="save_id" name="save_id" class="iptSave" @if($saved_member_id != '') checked="checked" @endif value="Y">
-                            <label for="save_id" class="labelSave tx-gray">아이디 저장</label>
-                        </li>
-                        <li class="chkBox-Search tx-gray">
-                            <span><a class="tx-gray mr5" href="{{ front_app_url('/member/find/id/', 'www') }}">아이디찾기</a> | <a class="tx-gray ml5" href="{{ front_app_url('/member/find/pwd/', 'www') }}">비밀번호찾기</a></span>
-                        </li>
-                    </ul>
-                </div>
-                <button type="submit" onclick="" class="mem-Btn bg-blue bd-dark-blue">
-                    <span>로그인</span>
-                </button>
-                <table cellspacing="0" cellpadding="0" class="joinTable tx-gray mt40">
-                <colgroup>
-                <col/>
-                <col width="20%"/>
-            </colgroup>
-                    <tbody>
-                    <tr>
-                        <td>
-                            <span class="tx-blue">아직 윌비스 통합회원이 아니신가요?</span><br/>
-                            가입 즉시 패밀리 포인트 3,000P를 받으실 수 있습니다.
-                        </td>
-                        <td>
-                            <a class="bg-dark-blue" href="{{ front_app_url('/member/join/', 'www') }}">통합회원가입</a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="2"><span class="tx-blue">윌비스 통합회원이란?</span><br/>
-                            한번의 회원가입으로 윌비스 전체 서비스를 이용하실 수 있는 멤버십입니다.
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-                <div class="bSlider mt40">
-                    {!! banner('로그인하단', '', $__cfg['SiteCode'], '0') !!}
+            <div class="MemberBox">
+                <div class="Member mem-Login widthAuto460">
+                    <div class="inputBox p_re">
+                        <input type="text" id="id" name="id" class="iptId" placeholder="아이디" maxlength="30" required="required" title="아이디" value="{{ $saved_member_id }}">
+                    </div>
+                    <div class="inputBox p_re">
+                        <input type="password" id="pwd" name="pwd" class="iptPwd" placeholder="비밀번호" maxlength="30" required="required" title="비밀번호">
+                    </div>
+                    <div class="tx-red item" style="display: block;" id="ret_msg"></div>
+                    <div class="chkBox mt30">
+                        <ul>
+                            <li class="chkBox-Save">
+                                <input type="checkbox" id="save_id" name="save_id" class="iptSave" @if($saved_member_id != '') checked="checked" @endif value="Y">
+                                <label for="save_id" class="labelSave tx-gray">아이디 저장</label>
+                            </li>
+                            <li class="chkBox-Search tx-gray">
+                                <span><a class="tx-gray mr5" href="{{ front_app_url('/member/find/id/', 'www') }}">아이디찾기</a> | <a class="tx-gray ml5" href="{{ front_app_url('/member/find/pwd/', 'www') }}">비밀번호찾기</a></span>
+                            </li>
+                        </ul>
+                    </div>
+                    <button type="submit" onclick="" class="mem-Btn bg-blue bd-dark-blue">
+                        <span>로그인</span>
+                    </button>
+                    <table cellspacing="0" cellpadding="0" class="joinTable tx-gray mt40">
+                        <colgroup>
+                            <col/>
+                            <col width="20%"/>
+                        </colgroup>
+                        <tbody>
+                        <tr>
+                            <td>
+                                <span class="tx-blue">아직 윌비스 통합회원이 아니신가요?</span><br/>
+                                가입 즉시 패밀리 포인트 3,000P를 받으실 수 있습니다.
+                            </td>
+                            <td>
+                                <a class="bg-dark-blue" href="{{ front_app_url('/member/join/', 'www') }}">통합회원가입</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2"><span class="tx-blue">윌비스 통합회원이란?</span><br/>
+                                한번의 회원가입으로 윌비스 전체 서비스를 이용하실 수 있는 멤버십입니다.
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <div class="bSlider mt40">
+                        {!! banner('로그인하단', '', $__cfg['SiteCode'], '0') !!}
+                    </div>
+                </div>  
+                <div class="youpass">
+                    <a href="https://youpass.co.kr" target="_blank"><img src="https://static.willbes.net/public/images/promotion/main/youpass.jpg" title="유패스"></a>
                 </div>
             </div>
-           
             <!-- End 로그인 -->
             <br/><br/><br/><br/><br/><br/>
         </form>

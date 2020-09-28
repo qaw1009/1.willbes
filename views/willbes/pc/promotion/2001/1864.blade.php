e5896a4ccaed1b60d1a0edb6ce2ae2dd13d23dae
김성일 <jxr880210@willbes.com>
2020-09-28 11:30:06 +0900

    [프론트] > 온라인경찰 0원 패스 1864

--- views/willbes/pc/promotion/2001/1864.blade.php
+++ views/willbes/pc/promotion/2001/1864.blade.php
@@ -280,19 +280,19 @@
                         <li>
                             <div>
                                 <strong>99</strong>만원<br>
-                                <input type="radio" id="y_pkg2" name="y_pkg2" value="172949" onClick=""/> <label for="y_pkg2">무제한 0원 PASS</label>                           
+                                <input type="radio" id="y_pkg2" name="y_pkg" value="172949" onClick=""/> <label for="y_pkg2">무제한 0원 PASS</label>
                             </div>
                         </li>
                         <li>
                             <div>
                                 <strong>79</strong>만원<br>
-                                <input type="radio" id="y_pkg3" name="y_pkg3" value="172950" onClick=""/> <label for="y_pkg3">무제한 PASS</label>                           
+                                <input type="radio" id="y_pkg3" name="y_pkg" value="172950" onClick=""/> <label for="y_pkg3">무제한 PASS</label>
                             </div>
                         </li>
                         <li>
                             <div>
                                 <strong>60</strong>만원<br>
-                                <input type="radio" id="y_pkg4" name="y_pkg4" value="172951" onClick=""/> <label for="y_pkg4">6개월 PASS</label>                           
+                                <input type="radio" id="y_pkg4" name="y_pkg" value="172951" onClick=""/> <label for="y_pkg4">6개월 PASS</label>
                             </div>
                         </li>
                     </ul>

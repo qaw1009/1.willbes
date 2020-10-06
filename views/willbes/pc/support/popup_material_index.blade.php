9cce34a6d2840794395f0c1505467ae38db98062
김성일 <jxr880210@willbes.com>
2020-10-06 11:39:11 +0900

    [테스트제외] > 임용 교수진 소개 게시판

---
+++ views/willbes/pc/support/popup_material_index.blade.php
@@ -0,0 +1,170 @@
+<div id="profNotice" class="willbes-Layer-Board" style="display: block">
+    <a class="closeBtn" href="#none" onclick="closeWin('{{ $arr_input['ele_id'] }}');"><img src="{{ img_url('prof/close.png') }}"></a>
+    <div class="Layer-Tit NG tx-dark-black">학습자료실</div>
+
+    <form id="ajax_list_form" name="ajax_list_form" method="GET">
+        {!! csrf_field() !!}
+        <input type="hidden" id="temp_keyword" name="temp_keyword" value="">
+        <input type="hidden" id="page" name="page" value="{{$arr_input['page'] or 1}}">
+    </form>
+
+    <div class="Layer-Cont">
+        <div class="willbes-Prof-Subject pl-zero NG tx-dark-black">
+            <div class="willbes-Lec-Search GM f_right">
+                <div class="inputBox p_re">
+                    <input type="text" id="s_keyword" name="s_keyword" class="labelSearch" placeholder="제목 또는 내용을 입력해 주세요" value="{{ element('s_keyword', $arr_input) }}" maxlength="30">
+                    <button type="button" onclick="callAjax(1);" class="search-Btn">
+                        <span>검색</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <!-- List -->
+        <div class="willbes-Leclist c_both">
+            <div class="LeclistTable">
+                <table cellspacing="0" cellpadding="0" class="listTable qnaTable upper-gray upper-black bdb-gray tx-gray" id="ajax_table">
+                    <colgroup>
+                        <col style="width: 65px;">
+                        <col style="width: 80px;">
+                        <col>
+                        <col style="width: 90px;">
+                        <col style="width: 100px;">
+                        <col style="width: 90px;">
+                    </colgroup>
+                    <thead>
+                    <tr>
+                        <th>No<span class="row-line">|</span></th>
+                        <th>자료유형<span class="row-line">|</span></th>
+                        <th>제목<span class="row-line">|</span></th>
+                        <th>첨부파일<span class="row-line">|</span></th>
+                        <th>작성일<span class="row-line">|</span></th>
+                        <th>조회수</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+                <div class="row">
+                    <div class="col-xs-12 add-paging"></div>
+                </div>
+            </div>
+        </div>
+        <!-- willbes-Leclist -->
+    </div>
+</div>
+
+<div id="Layerprof" class="willbes-Layer-Black" style="display: block"></div>
+
+
+<script type="text/javascript">
+    var $ajax_list_form = $('#ajax_list_form');
+
+    $(document).ready(function(){
+        callAjax($("#page").val());
+    });
+
+    function callAjax(num) {
+        listAjax(num);    //list data load
+        applyPagination();
+    }
+
+    //list ajax
+    function listAjax(page) {
+        var add_table = '';
+        var _url = '{{ front_url($default_path."/material/listAjax") }}';
+        var data = {
+            '{{ csrf_token_name() }}' : $ajax_list_form.find('input[name="{{ csrf_token_name() }}"]').val(),
+            'prof_idx' : '{{$arr_input['prof_idx']}}',
+            's_keyword': $('#s_keyword').val(),
+            'page' : page
+        };
+
+        sendAjax(_url, data, function(ret) {
+            var rownum = ret.paging.rownum;
+
+            if (ret.ret_data.length < 1) {
+                add_table += '<tr>';
+                add_table += '<td colspan="5" class="w-list tx-center">검색 결과가 없습니다.</td>';
+                add_table += '</tr>';
+            } else {
+                $.each(ret.ret_data, function (i, item) {
+                    var file_icon = '';
+                    var num_html = '';
+                    var bg_class = '';
+                    var type_ccd_name = '';
+
+                    if(item.AttachData){
+                        file_icon = '<img src="{{ img_url('prof/icon_file.gif') }}">';
+                    }
+
+                    if (item.IsBest == 1) {
+                        num_html += '<img src="{{ img_url('prof/icon_notice.gif') }}">';
+                        bg_class = 'top';
+                    } else {
+                        num_html += rownum;
+                    }
+
+                    if(item.TypeCcd_Name){
+                        type_ccd_name = item.TypeCcd_Name;
+                    }
+
+                    add_table += '<tr class="'+ bg_class +'">';
+                    add_table += '<td class="w-no">'+ num_html +'</td>';
+                    add_table += '<td class="w-type"><div class="pBox p1 NSK">'+ type_ccd_name +'</div></td>';
+                    add_table += '<td class="w-list tx-left pl20"><a href="#none" data-board-idx="'+ item.BoardIdx +'" onclick="showAjax(this)">'+ item.Title +'</a></td>';
+                    add_table += '<td class="w-file">'+ file_icon +'</td>';
+                    add_table += '<td class="w-date">'+ item.RegDatm +'</td>';
+                    add_table += '<td class="w-click">'+ item.TotalReadCnt +'</td>';
+                    add_table += '</tr>';
+                    rownum = rownum - 1;
+                });
+
+                $('#page').val(page);
+                $('#temp_keyword').val($('#s_keyword').val());
+            }
+
+            $('#ajax_table > tbody').html(add_table);
+            $('.add-paging').html(ret.paging.pagination);
+
+        }, showError, false, 'GET');
+    }
+
+    //show ajax
+    function showAjax(obj) {
+        var ele_id = 'WrapReply';
+        var _url = '{{ front_url($default_path."/material/popupShow") }}';
+        var data = {
+            'ele_id' : ele_id,
+            'prof_idx' : '{{$arr_input['prof_idx']}}',
+            'board_idx' : $(obj).data('board-idx'),
+            's_keyword' : $('#temp_keyword').val(),
+            'page' : $('#page').val(),
+        };
+
+        sendAjax(_url, data, function(ret) {
+            $('#' + ele_id).html(ret).show().css('display', 'block').trigger('create');
+        }, showAlertError, false, 'GET', 'html');
+    }
+
+    //href 리턴 false, list,page ajax 호출
+    function applyPagination() {
+        $("div.Paging a").on("click", function() {
+            var link, temp_params, params, num;
+
+            link = $(this).attr('href');
+            if (link != undefined) {
+                temp_params = link.split('?');
+                params = temp_params[1].split('=');
+                num = params[1];
+            }
+
+            if (num != undefined) {
+                callAjax(num);
+            } else {
+                callAjax(1);
+            }
+            return false;
+        });
+    }
+</script>
---
+++ views/willbes/pc/support/popup_show_material.blade.php
@@ -0,0 +1,103 @@
+<div id="profNotice" class="willbes-Layer-Board" style="display: block">
+    <div class="Layer-Cont">
+        <div class="willbes-Leclist c_both">
+            <div class="LecViewTable">
+                <table cellspacing="0" cellpadding="0" class="listTable upper-gray upper-black bdb-gray tx-gray">
+                    <colgroup>
+                        <col>
+                        <col style="width: 150px;">
+                        <col style="width: 150px;">
+                    </colgroup>
+                    <thead>
+                    <tr>
+                        <td class="subTit tx-left pl20">
+                            @if($data['IsBest'] == '1')
+                                <img src="{{ img_url('prof/icon_notice.gif') }}" style="marign-right: 5px;">
+                            @endif
+                            <strong class="tx-light-blue" style="padding-right: 5px;">{{$data['Title']}}</strong><span class="row-line">|</span>
+                        </td>
+                        <td class="w-date">{{$data['RegDatm']}}<span class="row-line">|</span></td>
+                        <td class="w-click"><strong>조회수</strong> {{$data['TotalReadCnt']}}</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td class="w-file tx-left pl20" colspan="3">
+                            @if(empty($data['AttachData']) === false)
+                                @foreach($data['AttachData'] as $row)
+                                    <a href="{{front_url($default_path.'/notice/download?file_idx=').$row['FileIdx'].'&board_idx='.$board_idx }}" target="_blank">
+                                        <img src="{{ img_url('prof/icon_file.gif') }}"> {{$row['RealName']}}
+                                    </a>
+                                @endforeach
+                            @endif
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="w-txt tx-left" colspan="3">
+                            {!! $data['Content'] !!}
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <div class="search-Btn btnAuto90 h36 mt20 mb30 f_right">
+                    <button data-board-idx="" data-board-url="{{front_url($default_path.'/material/popupIndex?'.$get_params)}}" onclick="go_board_popup(this)" class="mem-Btn bg-purple-gray bd-dark-gray">
+                        <span>목록</span>
+                    </button>
+                </div>
+
+                @if($data['IsBest'] != '1')
+                    <table cellspacing="0" cellpadding="0" class="listTable prevnextTable upper-gray bdt-gray bdb-gray tx-gray">
+                        <colgroup>
+                            <col style="width: 150px;">
+                            <col style="width: 640px;">
+                            <col style="width: 150px;">
+                        </colgroup>
+                        <tbody>
+                        <tr>
+                            <td class="w-prev bg-light-gray"><strong>이전글</strong></td>
+                            <td class="tx-left pl20">
+                                @if(empty($pre_data) === false)
+                                    <a href="#none" data-board-idx="{{$pre_data['BoardIdx']}}" data-board-url="{{front_url($default_path.'/material/popupShow?'.$get_params)}}" onclick="go_board_popup(this)">{{$pre_data['Title']}}</a><span class="row-line">|</span>
+                                @else
+                                    이전글이 없습니다.
+                                @endif
+                            </td>
+                            <td class="w-date">@if(empty($pre_data) === false){{$pre_data['RegDatm']}}@endif</td>
+                        </tr>
+                        <tr>
+                            <td class="w-next bg-light-gray"><strong>다음글</strong></td>
+                            <td class="tx-left pl20">
+                                @if(empty($next_data) === false)
+                                    <a href="#none" data-board-idx="{{$next_data['BoardIdx']}}" data-board-url="{{front_url($default_path.'/material/popupShow?'.$get_params)}}" onclick="go_board_popup(this)">{{$next_data['Title']}}</a><span class="row-line">|</span>
+                                @else
+                                    다음글이 없습니다.
+                            @endif
+                            <td class="w-date">@if(empty($next_data) === false){{$next_data['RegDatm']}}@endif</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                @endif
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="Layerprof" class="willbes-Layer-Black" style="display: block"></div>
+
+<script type="text/javascript">
+    // 레이어팝업
+    function go_board_popup(obj){
+        var ele_id = 'WrapReply';
+        var _url = $(obj).data('board-url');
+        var data = {
+            'ele_id' : ele_id,
+            'board_idx' : $(obj).data('board-idx'),
+            'prof_idx' : '{{$prof_idx}}',
+        };
+
+        sendAjax(_url, data, function(ret) {
+            $('#' + ele_id).html(ret).show().css('display', 'block').trigger('create');
+        }, showAlertError, false, 'GET', 'html');
+    }
+</script>

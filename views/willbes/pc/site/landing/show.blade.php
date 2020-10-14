d198cb4293a474e0948f9696fab75fbaa8fb7af4
김성일 <jxr880210@willbes.com>
2020-10-14 17:58:19 +0900

    [테스트제외] > 임용 가이드

--- views/willbes/pc/site/landing/show.blade.php
+++ views/willbes/pc/site/landing/show.blade.php
@@ -8,6 +8,16 @@
             @include('willbes.pc.layouts.partial.site_route_path')
         </div>
         <div class="Content p_re">
+            @if($__cfg['SiteCode'] == '2017') {{-- 임용 --}}
+                @php
+                    if(strpos($data['Title'],'>') !== false){
+                       $arr_title = explode('>',$data['Title']);
+                    }else{
+                        $arr_title = array($data['Title']);
+                    }
+                @endphp
+                <h4 class="NG" style="margin-bottom: 10px;font-size: 16px; color: #000;line-height: 30px;">{{end($arr_title)}}</h4>
+            @endif
             {!! $data['Content'] !!}
         </div>
         {!! banner('수험정보_우측퀵', 'Quick-Bnr ml20', $__cfg['SiteCode'], $__cfg['CateCode']) !!}

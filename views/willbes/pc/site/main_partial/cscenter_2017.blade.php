0f9615153a622b59a6d53e8fff1df2f254adfcd8
김성일 <jxr880210@willbes.com>
2020-10-14 09:51:51 +0900

    [테스트제외] > 임용 pc 메인 수정

--- views/willbes/pc/site/main_partial/cscenter_2017.blade.php
+++ views/willbes/pc/site/main_partial/cscenter_2017.blade.php
@@ -26,18 +26,18 @@
                         <div class="nTxt">동영상 원격지원</div>
                     </a>
                 </li>
-{{--                <li>--}}
-{{--                    <a href="#none">--}}
-{{--                        <span><img src="https://static.willbes.net/public/images/promotion/main/2018/2018_main_csicon05.png"></span>--}}
-{{--                        <div class="nTxt">대학특강 문의</div>--}}
-{{--                    </a>--}}
-{{--                </li>--}}
-{{--                <li>--}}
-{{--                    <a href="#none">--}}
-{{--                        <span><img src="https://static.willbes.net/public/images/promotion/main/2018/2018_main_csicon06.png"></span>--}}
-{{--                        <div class="nTxt">교수채용</div>--}}
-{{--                    </a>--}}
-{{--                </li>--}}
+                <li>
+                    <a href="{{ front_url('/landing/show/lcode/1038/cate/3137/preview/Y') }}">
+                        <span><img src="https://static.willbes.net/public/images/promotion/main/2018/2018_main_csicon05.png"></span>
+                        <div class="nTxt">대학특강 문의</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ front_url('/landing/show/lcode/1039/cate/3137/preview/Y') }}">
+                        <span><img src="https://static.willbes.net/public/images/promotion/main/2018/2018_main_csicon06.png"></span>
+                        <div class="nTxt">교수채용</div>
+                    </a>
+                </li>
             </ul>
         </dt>
         <dt class="willbesNumber">

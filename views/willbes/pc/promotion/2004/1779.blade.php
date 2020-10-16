762c31d6ac03d20e4d89647bfa6f2bee336c5f49
이윤구 <yoongukk@willbes.com>
2020-10-16 14:17:09 +0900

    (공무원학원) 11월 개강 직렬별 랜딩페이지 수정

--- views/willbes/pc/promotion/2004/1779.blade.php
+++ views/willbes/pc/promotion/2004/1779.blade.php
@@ -25,7 +25,7 @@
         .skybanner2 {position:fixed; top:250px; left:50%; margin-left:-680px; width:120px; z-index:1;}
         .skybanner2 a {display:block; margin-bottom:10px;}
 
-        .evtTop {background:url(https://static.willbes.net/public/images/promotion/2020/08/1779_top_bg.jpg) center top no-repeat}    
+        .evtTop {background:#F4A0A0;}  
         .evt01 {background:#fff}
         .evt02 {background:url(https://static.willbes.net/public/images/promotion/2020/08/1779_02_bg.jpg) left top repeat-x}
         .evt03 {background:#a7d4ff}
@@ -58,28 +58,24 @@
 
     <div class="p_re evtContent NSK" id="evtContainer"> 
         <div class="skybanner">
-            <img src="https://static.willbes.net/public/images/promotion/2020/08/1779_sky01.jpg" alt="한덕현 T" usemap="#Map1779" border="0" />
-            <map name="Map1779">
-                <area shape="rect" coords="8,60,123,115" href="https://pass.willbes.net/pass/promotion/index/cate/3043/code/1297" target="_blank" alt="한덕현 새벽모의고사">
-                <area shape="rect" coords="8,117,122,172" href="https://pass.willbes.net/pass/support/notice/show?board_idx=294823&" target="_blank" alt="티패스">
-            </map>
-            <a href="https://pass.willbes.net/pass/support/notice/show?board_idx=294829&" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2020/08/1779_sky02.jpg" alt="기미진 T" /></a>
-            <a href="https://pass.willbes.net/pass/support/notice/show?board_idx=294815&" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2020/08/1779_sky03.jpg" alt="조민주 T" /></a>
+            <a href="https://pass.willbes.net/pass/support/notice/show?board_idx=298992&" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2020/10/1779_sky02.jpg" alt="기미진 T" /></a>
+            <a href="https://pass.willbes.net/pass/support/notice/show?board_idx=298988&" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2020/10/1779_sky01.jpg" alt="한덕현 T" /></a>       
+            <a href="https://pass.willbes.net/pass/support/notice/show?board_idx=298975&" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2020/10/1779_sky03.jpg" alt="조민주 T" /></a>
         </div>
         <div class="skybanner2">
-            {{--<a href="https://pass.willbes.net/pass/support/notice/show?board_idx=294996" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2020/09/1779_sky05.png" alt="한덕현" /></a>--}}
-            <a href="https://pass.willbes.net/pass/event/show/ongoing?event_idx=861" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2020/09/1779_sky04.png" alt="이석준" /></a>
-        </div>
+            <a href="https://pass.willbes.net/pass/event/show/ongoing?event_idx=896&" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2020/10/1779_sky04.png" alt="이석준" /></a>
+            {{--<a href="https://pass.willbes.net/pass/support/notice/show?board_idx=294996" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2020/09/1779_sky05.png" alt="한덕현" /></a>--}}           
+        </div>         
 
         <div class="evtCtnsBox evtTop" >            
-            <img src="https://static.willbes.net/public/images/promotion/2020/08/1779_top.gif" alt="9급 일반행정" />           
+            <img src="https://static.willbes.net/public/images/promotion/2020/10/1779_top_irona.jpg" alt="이로나" />                
         </div>    
 
         <div class="evtCtnsBox evt01">
-            <img src="https://static.willbes.net/public/images/promotion/2020/08/1779_01.jpg" alt="커리큘럼" usemap="#Map1779_01" border="0" />
+            <img src="https://static.willbes.net/public/images/promotion/2020/10/1779_01.gif" alt="커리큘럼" usemap="#Map1779_01" border="0" />
             <map name="Map1779_01">
               <area shape="rect" coords="938,1431,1058,1560" href="https://pass.willbes.net/pass/offLecture/index?cate_code=3043&campus_ccd=605001&subject_idx=1254&prof_idx=50500" target="_blank" alt="한덕현">
-              <area shape="rect" coords="940,1608,1060,1736" href="https://pass.willbes.net/pass/event/show/ongoing?event_idx=861" target="_blank" alt="이석준">
+              <area shape="rect" coords="940,1608,1060,1736" href="https://pass.willbes.net/pass/event/show/ongoing?event_idx=896&" target="_blank" alt="이석준">
             </map>
         </div>
 
@@ -92,11 +88,11 @@
         </div>
 
         <div class="evtCtnsBox evt04">
-            <img src="https://static.willbes.net/public/images/promotion/2020/08/1779_04.jpg" alt="수강신청" usemap="#Map1779_02" border="0" />
-            <map name="Map1779_02">
-              <area shape="rect" coords="190,1175,931,1292" href="https://pass.willbes.net/pass/offPackage/index?cate_code=3043&campus_ccd=605001" target="_blank" alt="파란불 수강신청">
-              <area shape="rect" coords="278,1422,515,1478" href="https://pass.willbes.net/pass/offLecture/index?cate_code=3043&campus_ccd=605001&subject_idx=1254&prof_idx=50500" target="_blank" alt="한덕현">
-              <area shape="rect" coords="692,1422,928,1475" href="https://pass.willbes.net/pass/event/show/ongoing?event_idx=861" target="_blank" alt="이석준">
+            <img src="https://static.willbes.net/public/images/promotion/2020/10/1779_04.jpg" alt="수강신청" usemap="#Map1779_02" border="0" />
+            <map name="Map1779_02" id="Map1779_02">
+                <area shape="rect" coords="192,1131,931,1325" href="https://pass.willbes.net/pass/offPackage/index?cate_code=3043&campus_ccd=605001" target="_blank" alt="파란불 수강신청">
+                <area shape="rect" coords="283,1420,507,1475" href="https://pass.willbes.net/pass/offLecture/index?cate_code=3043&campus_ccd=605001&subject_idx=1254&prof_idx=50500" target="_blank" alt="한덕현">
+                <area shape="rect" coords="698,1419,922,1473" href="https://pass.willbes.net/pass/event/show/ongoing?event_idx=896&" target="_blank" alt="이석준">
             </map>
         </div>
 

7c855db472c6bd40fdf40ae1f5e0c4e5642b6fed
김성일 <jxr880210@willbes.com>
2020-10-19 11:11:42 +0900

    [테스트제외] > 임용 오시는길 수정

---
+++ views/willbes/m/site/location/map_2017.blade.php
@@ -0,0 +1,29 @@
+@extends('willbes.m.layouts.master')
+
+@section('page_title', '학원 오시는 길')
+
+@section('content')
+    <!-- Container -->
+    <div id="Container" class="Container NG c_both">
+        <!-- PageTitle -->
+        @include('willbes.m.layouts.page_title')
+        <div class="mapArea">
+            <div id="location1" class="tabContent">
+                <div class="mapTitle">윌비스임용고시학원 노량진 임용 본관</div>
+                <ul>
+                    <li>[도로명] 서울시 동작구 노량진로 202 5층 <br>
+                        [지번] 서울시 동작구 노량진동 197 남강타워 5층</li>
+                    <li><a href="tel:1544-3169" target="_blank">1544-3169</a></li>
+                </ul>
+
+                <div class="location_map">
+                    <span><img src="https://static.willbes.net/public/images/promotion/m/icon_add.png" alt="학원"></span>
+                    <div class="mapimg"><a href="http://kko.to/NfJMuAxYo" target="_blank"><img src="https://static.willbes.net/public/images/promotion/m/2018/map_ssam.jpg" alt="오시는 길"></a></div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Topbtn -->
+        @include('willbes.m.layouts.topbtn')
+    </div>
+@stop

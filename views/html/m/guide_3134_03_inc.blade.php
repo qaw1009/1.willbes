3c70ec86b3ae9e68b571ac1d12f7e796a2f148ed
김창용 <winmir@willbes.com>
2020-10-21 13:42:31 +0900

    임용 모바일

---
+++ views/html/m/guide_3134_03_inc.blade.php
@@ -0,0 +1,98 @@
+        <div class="guidebox GM">                   
+            <table>
+                <tbody>
+                    <tr>
+                        <th rowspan="18">2020<BR>
+                        학년도 </th>
+                        <th>지역</th>
+                        <th>유아 · 초등</th>
+                        <th>중등</th>
+                    </tr>
+                    <tr>
+                        <td>서울특별시</td>
+                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                    </tr>
+                    <tr>
+                        <td>인천광역시</td>
+                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                    </tr>
+                    <tr>
+                        <td>대전광역시</td>
+                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                    </tr>
+                    <tr>
+                        <td>세종시</td>
+                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                    </tr>
+                    <tr>
+                        <td>광주광역시</td>
+                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                    </tr>
+                    <tr>
+                        <td>대구광역시</td>
+                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                    </tr>
+                    <tr>
+                        <td>울산광역시</td>
+                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                    </tr>
+                    <tr>
+                        <td>부산광역시</td>
+                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                    </tr>
+                    <tr>
+                      <td>경기도</td>
+                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                    </tr>
+                    <tr>
+                      <td>충청북도</td>
+                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                    </tr>
+                    <tr>
+                      <td>충청남도</td>
+                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                    </tr>
+                    <tr>
+                      <td>전라북도</td>
+                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                    </tr>
+                    <tr>
+                      <td>전라남도</td>
+                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                    </tr>
+                    <tr>
+                      <td>강원도</td>
+                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                    </tr>
+                    <tr>
+                      <td>경상북도</td>
+                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                    </tr>
+                    <tr>
+                      <td>경상남도</td>
+                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                    </tr>
+                    <tr>
+                        <td>제주도</td>
+                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>

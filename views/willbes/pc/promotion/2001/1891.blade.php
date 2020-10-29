2533495fa22cc6e8378af9189432b2e91f1ab4c8
이윤구 <yoongukk@willbes.com>
2020-10-29 10:54:41 +0900

    경찰_맞춤형 커리큘럼 가이드_프로모션 페이지 수정

--- views/willbes/pc/promotion/2001/1891.blade.php
+++ views/willbes/pc/promotion/2001/1891.blade.php
@@ -45,25 +45,45 @@
         </div>
 
         <div class="evtCtnsBox wb_04">
-            <img src="https://static.willbes.net/public/images/promotion/2020/10/1891_04.jpg" alt="셀프 진단하기" usemap="#Map1891a" border="0"/>
-            <map name="Map1891a" id="Map1891a">
+            <img src="https://static.willbes.net/public/images/promotion/2020/10/1891_04.jpg" alt="셀프 진단하기" usemap="#Map1891_start" border="0"/>
+            <map name="Map1891_start" id="Map1891_start">
                 <area shape="rect" coords="446,420,676,486" href="#start" />
             </map>            
         </div>
 
         <div class="evtCtnsBox wb_05">
-            <img src="https://static.willbes.net/public/images/promotion/2020/10/1891_05.jpg" alt="테스트 yes or no" usemap="#Map1891b" border="0"/>
-            <map name="Map1891b" id="Map1891b">
+            <img src="https://static.willbes.net/public/images/promotion/2020/10/1891_05.jpg" alt="테스트 yes or no" usemap="#Map1891_test" border="0"/>
+            <map name="Map1891_test" id="Map1891_test">
                 <area shape="rect" coords="257,546,551,625" href="#none" />
                 <area shape="rect" coords="563,546,863,626" href="#none" />
             </map>            
         </div>
 
         <div class="evtCtnsBox wb_06">
-            <img src="https://static.willbes.net/public/images/promotion/2020/10/1891_06.jpg" alt="나에게 맞는 커리큘럼은?" usemap="#Map1891c" border="0"/>
+            <img src="https://static.willbes.net/public/images/promotion/2020/10/1891_06a.jpg" alt="나에게 맞는 커리큘럼은?" usemap="#Map1891a" border="0"/>
+            <map name="Map1891a" id="Map1891a">
+                <area shape="rect" coords="393,717,728,807" href="https://police.willbes.net/lecture/index/cate/3001/pattern/only?search_order=course&course_idx=1004" target="_blank" />
+            </map>  
+            <img src="https://static.willbes.net/public/images/promotion/2020/10/1891_06a.jpg" alt="나에게 맞는 커리큘럼은?" usemap="#Map1891b" border="0"/>
+            <map name="Map1891b" id="Map1891b">
+                <area shape="rect" coords="393,717,728,807" href="https://police.willbes.net/lecture/index/cate/3001/pattern/only?search_order=course&course_idx=1005" target="_blank" />
+            </map> 
+            <img src="https://static.willbes.net/public/images/promotion/2020/10/1891_06a.jpg" alt="나에게 맞는 커리큘럼은?" usemap="#Map1891c" border="0"/>
             <map name="Map1891c" id="Map1891c">
-                <area shape="rect" coords="393,717,728,807" href="#none" />
-            </map>          
+                <area shape="rect" coords="393,717,728,807" href="https://police.willbes.net/lecture/index/cate/3001/pattern/only?search_order=course&course_idx=1006" target="_blank" />
+            </map> 
+            <img src="https://static.willbes.net/public/images/promotion/2020/10/1891_06a.jpg" alt="나에게 맞는 커리큘럼은?" usemap="#Map1891d" border="0"/>
+            <map name="Map1891d" id="Map1891d">
+                <area shape="rect" coords="393,717,728,807" href="https://police.willbes.net/lecture/index/cate/3001/pattern/only?search_order=course&course_idx=1007" target="_blank" />
+            </map> 
+            <img src="https://static.willbes.net/public/images/promotion/2020/10/1891_06a.jpg" alt="나에게 맞는 커리큘럼은?" usemap="#Map1891e" border="0"/>
+            <map name="Map1891e" id="Map1891e">
+                <area shape="rect" coords="393,717,728,807" href="https://police.willbes.net/lecture/index/cate/3001/pattern/only?search_order=course&course_idx=1008" target="_blank" />
+            </map> 
+            <img src="https://static.willbes.net/public/images/promotion/2020/10/1891_06a.jpg" alt="나에게 맞는 커리큘럼은?" usemap="#Map1891f" border="0"/>
+            <map name="Map1891f" id="Map1891f">
+                <area shape="rect" coords="393,717,728,807" href="https://police.willbes.net/lecture/index/cate/3001/pattern/only?search_order=course&course_idx=1009" target="_blank" />
+            </map>           
         </div>
 
     </div>

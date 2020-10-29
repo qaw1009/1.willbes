c98fb7ca58a8a669075c6361f13fc057c7b3c432
이윤구 <yoongukk@willbes.com>
2020-10-29 10:09:03 +0900

    최우영 연간 커리큘럼 페이지 스카이베너 링크 수정

--- views/willbes/pc/promotion/2004/1721.blade.php
+++ views/willbes/pc/promotion/2004/1721.blade.php
@@ -74,9 +74,9 @@
                 <li>     
                     <img src="https://static.willbes.net/public/images/promotion/2020/10/1721_sky.png" usemap="#Map1721sky" title="10월 개강" border="0"/>
                     <map name="Map1721sky">
-                        <area shape="rect" coords="12,128,109,248" href="https://pass.willbes.net/pass/offLecture/show/cate/3052/prod-code/173953" target="_blank" alt="전기기기">
-                        <area shape="rect" coords="15,276,107,400" href="https://pass.willbes.net/pass/offLecture/index?cate_code=3052&campus_ccd=605001&search_text=UHJvZE5hbWU67Ya17Iug" target="_blank" alt="무선공학">
-                        <area shape="rect" coords="12,430,108,554" href="https://pass.willbes.net/pass/offLecture/show/cate/3052/prod-code/173965" target="_blank" alt="전자공학">
+                        <area shape="rect" coords="12,128,109,248" href="https://pass.willbes.net/pass/offLecture/index?cate_code=3052&campus_ccd=605001&search_text=UHJvZE5hbWU67KCE6riw&subject_idx=" target="_blank" alt="전기기기">
+                        <area shape="rect" coords="15,276,107,400" href="https://pass.willbes.net/pass/offLecture/index?cate_code=3052&campus_ccd=605001&search_text=UHJvZE5hbWU67Ya17Iug&subject_idx=" target="_blank" alt="무선공학">
+                        <area shape="rect" coords="12,430,108,554" href="https://pass.willbes.net/pass/offLecture/index?cate_code=3052&campus_ccd=605001&search_text=UHJvZE5hbWU67KCE7J6Q&subject_idx=" target="_blank" alt="전자공학">
                     </map>
                 </li>
                 <li><a href="#t_pass_go"><img src="https://static.willbes.net/public/images/promotion/2020/10/1721_sky2.png" title="최우영 티패스" /></a></li>

e55b3f6644c11be5b47e064c8c81671de8718446
김성일 <jxr880210@willbes.com>
2020-11-03 09:37:42 +0900

    [프론트] > 7급 PSAT 메인 우측 퀵배너 추가

--- views/willbes/pc/site/main_2003_3103.blade.php
+++ views/willbes/pc/site/main_2003_3103.blade.php
@@ -391,6 +391,11 @@
                 </dl>
             </div>
         </div>
+
+        <div id="QuickMenu" class="MainQuickMenu">
+            {{-- quick menu --}}
+            @include('willbes.pc.site.main_partial.quick_menu_' . $__cfg['SiteCode'])
+        </div>
     </div>
 
     <!-- End Container -->

9d268db194c6cdd3bdc9fbf075efbf3a659d79e3
김창용 <winmir@willbes.com>
2020-11-17 17:13:11 +0900

    시스템 점검 html

---
+++ views/html/check.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>시스템점검</title>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@900&display=swap" rel="stylesheet">
+<style type="text/css">
+
+::selection { background-color: #E13300; color: white; }
+::-moz-selection { background-color: #E13300; color: white; }
+
+body {
+	background-color: #fff;
+	font-family: 'Noto Sans KR', sans-serif;
+}
+#container {
+	margin: 100px auto;
+	text-align:center	
+}
+#container .systemCheck {width:746px; margin:20px auto; 
+	background:url(https://static.willbes.net/public/images/temp/systemCheck.jpg) no-repeat center top; height:507px}
+#container .systemCheck div {padding-top:380px;  color:#000; line-height:1.5; font-size:26px; font-weight:900}
+</style>
+</head>
+<body>
+	<div id="container">
+		<div class="systemCheck">  
+			<div>
+				[점검일시]<br>
+				2020년 11월 30일 (월) 22:00 ~ 12월 1일 (화) 08:00</div>
+		</div>
+	</div>
+</body>
+</html>

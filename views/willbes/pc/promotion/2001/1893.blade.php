7fc135af65da484a065d6a5e32e810cb10cb75ca
김창용 <winmir@willbes.com>
2020-11-20 09:05:19 +0900

    경찰 한능컴 프로모션

--- views/willbes/pc/promotion/2001/1893.blade.php
+++ views/willbes/pc/promotion/2001/1893.blade.php
@@ -30,10 +30,12 @@
     </form>
 
     <div class="evtContent NGR" id="evtContainer">
+        {{--
         <div class="skybanner">
             <a href="#evt01"><img src="https://static.willbes.net/public/images/promotion/2020/10/1893_sky.png" alt="한능검&지텔프"/></a>
             <a href="#evt02"><img src="https://static.willbes.net/public/images/promotion/2020/10/1893_sky2.png" alt="한능검&지텔프"/></a>
         </div>
+        --}}
         <div class="evtCtnsBox wb_top">
             <img src="https://static.willbes.net/public/images/promotion/2020/10/1893_top.jpg" alt="개정과목시험"/>            
         </div>
@@ -42,6 +44,7 @@
             <img src="https://static.willbes.net/public/images/promotion/2020/10/1893_01.jpg" alt="한능검,지텔프"/>          
         </div>
 
+        {{--
         <div class="evtCtnsBox wb_02" id="evt02">
             <img src="https://static.willbes.net/public/images/promotion/2020/10/1893_02.jpg" alt="소문내기 이벤트" usemap="#Map1893_01" border="0"/><br>
             <map name="Map1893_01">
@@ -57,11 +60,13 @@
                 <area shape="rect" coords="852,45,1018,136" href="https://cafe.naver.com/specup" target="_blank" alt="스펙업">
             </map>        
         </div> 
+        --}}
 
-        {{--홍보url--}}
+        {{--홍보url
         @if( empty($data['data_option_ccd']) === false && array_key_exists($arr_base['option_ccd']['comment_list'], $data['data_option_ccd']) === true && array_key_exists($arr_base['comment_use_area']['event'], $data['data_comment_use_area']) === true)
             @include('willbes.pc.promotion.show_comment_list_url_partial',array('bottom_cafe_type'=>'N'))
-        @endif   
+        @endif  
+        --}} 
         
         <div class="evtCtnsBox wb_03">
             <img src="https://static.willbes.net/public/images/promotion/2020/10/1893_03.jpg" alt="강의신청" usemap="#Map1893_02" border="0"/>

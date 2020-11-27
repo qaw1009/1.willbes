26fc7cfb210a8ea9856a866c19e2f4b9945f2596
하준걸 <hereisme@willbes.net>
2020-11-27 14:14:52 +0900

    임용고시 이벤트코드 추가

--- views/willbes/pc/classroom/gotossam.blade.php
+++ views/willbes/pc/classroom/gotossam.blade.php
@@ -1,6 +1,6 @@
-<form name="form" id="form" method="post" action="http://pressam.willbes.net/login/login2.html">
+<form name="form" id="form" method="post" action="http://pressam.willbes.net/m/login/login2.html">
     <input type="hidden" name="enc_data" value="{{$enc_data}}" />
-    <input type="hidden" name="param" value="{{$param}}" />
+    <input type="hidden" name="event_cd" value="{{$param}}" />
 </form>
 <script>
     document.form.submit();

48bf11eecd1680746cabe403a87210db43e5bd95
김성일 <jxr880210@willbes.com>
2020-12-02 15:39:36 +0900

    [프론트] > 모바일 7급 PSAT 수정

--- views/willbes/m/site/main_partial/cscenter_2003.blade.php
+++ views/willbes/m/site/main_partial/cscenter_2003.blade.php
@@ -1,23 +1,33 @@
 {{-- 온라인공무원, 법원직 링크경로, 전화번호 설정 --}}
-@if($__cfg['CateCode'] == '3035')
-    @php
-        $_arr_cs = [
-            'on_qna_link' => front_url('/support/qna/index/cate/'.$__cfg['CateCode'].'?s_cate_code='.$__cfg['CateCode'].'&s_cate_code_disabled=Y'),
-            'off_qna_link' => front_url('/support/qna/index/?'.(empty($data['mapping_cate_data']['CateCode']) === false ? 's_cate_code='.$data['mapping_cate_data']['CateCode'] : '').'&on_off_link_cate_code='.$__cfg['CateCode'].'&s_cate_code_disabled=Y', true),
-            'off_map_link' => front_url('/location/map/cate/'.$__cfg['CateCode']),
-            'off_tel' => '1544-0330',
-        ];
-    @endphp
-@else
-    @php
-        $_arr_cs = [
-            'on_qna_link' => front_url('/support/qna/index'),
-            'off_qna_link' => front_url('/support/qna/index', true),
-            'off_map_link' => front_url('/home/index#map_campus', true),
-            'off_tel' => '1544-0330',
-        ];
-    @endphp
-@endif
+@php
+    $online_tel = '1544-5006';
+    $school_tel = '1544-0330';
+    $_arr_cs = [
+        'on_qna_link' => front_url('/support/qna/index'),
+        'off_qna_link' => front_url('/support/qna/index', true),
+        'off_map_link' => front_url('/home/index#map_campus', true),
+        'off_tel' => '1544-0330',
+    ];
+
+    switch ($__cfg['CateCode']){
+        case "3103":
+            $online_tel = '1566-4770';
+            $school_tel = '1544-1881';
+            break;
+
+        case "3035":
+            $_arr_cs = [
+                'on_qna_link' => front_url('/support/qna/index/cate/'.$__cfg['CateCode'].'?s_cate_code='.$__cfg['CateCode'].'&s_cate_code_disabled=Y'),
+                'off_qna_link' => front_url('/support/qna/index/?'.(empty($data['mapping_cate_data']['CateCode']) === false ? 's_cate_code='.$data['mapping_cate_data']['CateCode'] : '').'&on_off_link_cate_code='.$__cfg['CateCode'].'&s_cate_code_disabled=Y', true),
+                'off_map_link' => front_url('/location/map/cate/'.$__cfg['CateCode']),
+                'off_tel' => '1544-0330',
+            ];
+            break;
+
+        default:
+            break;
+    }
+@endphp
 
 <div class="csCenter">
     @if($__cfg['CateCode'] != '3103')
@@ -32,7 +42,7 @@
             <div class="goTel"><img src="{{ img_url('m/main/icon_tel.png') }}">
                 <div>
                     <strong>온라인문의</strong>
-                    <span>1544-5006</span>
+                    <span>{{ $online_tel }}</span>
                     평일 09시~18시<Br>
                     주말/공휴일 제외<Br>
                     <Br>
@@ -45,11 +55,7 @@
                     <strong>학원문의</strong>
                     {{--<span>{{ element('off_tel', $_arr_cs, '') }}</span>--}}
                     <span>
-                        @if($__cfg['CateCode'] == '3103')
-                            <a href="tel:1544-1881">1544-1881</a>
-                        @else
-                            <a href="tel:1544-0330">1544-0330</a>
-                        @endif
+                        <a href="tel:{{ $school_tel }}">{{ $school_tel }}</a>
                     </span>
                     <p>평일 09시~18시<br>
                     토요일 09시~14시</p>

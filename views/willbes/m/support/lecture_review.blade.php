f95ee3b8d3444d8869b97646d60e44843a115e5b
김성일 <jxr880210@willbes.com>
2020-12-10 15:13:23 +0900

    [프론트] > 임용 모바일 수강후기 게시판 검색방식 변경

--- views/willbes/m/support/lecture_review.blade.php
+++ views/willbes/m/support/lecture_review.blade.php
@@ -15,28 +15,41 @@
             </button>
             수강후기
         </div>
-        <div class="willbes-Lec-Selected NG tx-gray">
+{{--        <div class="willbes-Lec-Selected NG tx-gray">--}}
 
-            <select id="s_cate_code" name="s_cate_code" title="카테고리" class="width49p" onchange="change_url('s_cate_code',this.value)">
-                <option value="">카테고리</option>
-                @foreach($arr_base['category'] as $row)
-                    <option value="{{$row['CateCode']}}" class="{{$row['SiteCode']}}" @if(element('s_cate_code', $arr_input) == $row['CateCode'])selected="selected"@endif @if(empty($row['ChildCnt']) === false && $row['ChildCnt'] > 0) disabled @endif>{{$row['CateNameRoute']}}</option>
-                @endforeach
-            </select>
+{{--            <select id="s_cate_code" name="s_cate_code" title="카테고리" class="width49p" onchange="change_url('s_cate_code',this.value)">--}}
+{{--                <option value="">카테고리</option>--}}
+{{--                @foreach($arr_base['category'] as $row)--}}
+{{--                    <option value="{{$row['CateCode']}}" class="{{$row['SiteCode']}}" @if(element('s_cate_code', $arr_input) == $row['CateCode'])selected="selected"@endif @if(empty($row['ChildCnt']) === false && $row['ChildCnt'] > 0) disabled @endif>{{$row['CateNameRoute']}}</option>--}}
+{{--                @endforeach--}}
+{{--            </select>--}}
 
-            <select id="subject_idx" name="subject_idx" title="과목" class="width50p ml1p" onchange="change_url('subject_idx',this.value)" @if(empty(element('s_cate_code', $arr_input)) === true) disabled @endif>
-                <option value="">과목</option>
-                @foreach($arr_base['subject'] as $row)
-                    <option value="{{$row['SubjectIdx']}}" @if(element('subject_idx', $arr_input) == $row['SubjectIdx'])selected="selected"@endif>{{$row['SubjectName']}}</option>
-                @endforeach
-            </select>
+{{--            <select id="subject_idx" name="subject_idx" title="과목" class="width50p ml1p" onchange="change_url('subject_idx',this.value)" @if(empty(element('s_cate_code', $arr_input)) === true) disabled @endif>--}}
+{{--                <option value="">과목</option>--}}
+{{--                @foreach($arr_base['subject'] as $row)--}}
+{{--                    <option value="{{$row['SubjectIdx']}}" @if(element('subject_idx', $arr_input) == $row['SubjectIdx'])selected="selected"@endif>{{$row['SubjectName']}}</option>--}}
+{{--                @endforeach--}}
+{{--            </select>--}}
 
-            <select id="prof_idx" name="prof_idx" title="교수" class="width49p mt1p" onchange="change_url('prof_idx',this.value)" @if(empty(element('s_cate_code', $arr_input)) === true || empty(element('subject_idx', $arr_input)) === true) disabled @endif>
-                <option value="">교수</option>
-                @foreach($arr_base['professor'] as $key => $row)
-                    <option value="{{$row['ProfIdx']}}" @if(element('prof_idx', $arr_input) == $row['ProfIdx'])selected="selected"@endif>{{$row['wProfName']}}</option>
+{{--            <select id="prof_idx" name="prof_idx" title="교수" class="width49p mt1p" onchange="change_url('prof_idx',this.value)" @if(empty(element('s_cate_code', $arr_input)) === true || empty(element('subject_idx', $arr_input)) === true) disabled @endif>--}}
+{{--                <option value="">교수</option>--}}
+{{--                @foreach($arr_base['professor'] as $key => $row)--}}
+{{--                    <option value="{{$row['ProfIdx']}}" @if(element('prof_idx', $arr_input) == $row['ProfIdx'])selected="selected"@endif>{{$row['wProfName']}}</option>--}}
+{{--                @endforeach--}}
+{{--            </select>--}}
+{{--        </div>--}}
+
+        <div class="sortList">
+            <ul>
+                @foreach($arr_base['professor'] as $row)
+                    <li data-cate-code="{{ $row['CateCode'] }}" data-subject-idx="{{ $row['SubjectIdx'] }}" data-prof-idx="{{ $row['ProfIdx'] }}">
+                        <a href="#none" class="@if(element('s_cate_code', $arr_input) == $row['CateCode'] && element('subject_idx', $arr_input) == $row['SubjectIdx'] && element('prof_idx', $arr_input) == $row['ProfIdx']) on @endif">
+                            {{ $row['SubjectName'] }} {{ $row['ProfNickName'] }}
+                        </a>
+                    </li>
                 @endforeach
-            </select>
+                <li data-cate-code="" data-subject-idx="" data-prof-idx=""><a href="#none" class="@if(element('prof_idx', $arr_input) == '' && element('subject_idx', $arr_input) == '') on @endif">전체</a></li>
+            </ul>
         </div>
 
         <div class="ml10">※ 수강후기 등록은 PC버전에서만 가능합니다.</div>
@@ -103,6 +116,20 @@
 
     <script>
         var $url_form = $('#url_form');
+
+        // 교수별 수강후기 조회
+        $(".sortList ul li").click(function (){
+            var s_cate_code = $(this).data('cate-code');
+            var subject_idx = $(this).data('subject-idx');
+            var prof_idx = $(this).data('prof-idx');
+
+            $url_form.html('');
+            $url_form.append('<input type="hidden" name="s_cate_code" value="' + s_cate_code + '"/>');
+            $url_form.append('<input type="hidden" name="subject_idx" value="' + subject_idx + '"/>');
+            $url_form.append('<input type="hidden" name="prof_idx" value="' + prof_idx + '"/>');
+            $url_form.prop('action', location.protocol + '//' + location.host + location.pathname);
+            $url_form.submit();
+        });
 
         function change_url(s_type,s_val){
             switch (s_type){

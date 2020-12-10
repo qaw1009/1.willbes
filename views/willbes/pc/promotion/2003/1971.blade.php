c097ab341d041c4469b7139875eca5c6db3a1831
김창용 <winmir@willbes.com>
2020-12-10 09:31:16 +0900

    공무원 대방고시학원 제휴 프로모션

--- views/willbes/pc/promotion/2003/1971.blade.php
+++ views/willbes/pc/promotion/2003/1971.blade.php
@@ -44,9 +44,34 @@
                 <img src="https://static.willbes.net/public/images/promotion/2020/12/1971_tab01_01.jpg" alt="기술직" />
             </div>
             <div class="evtCtnsBox evt02">
-                <img src="https://static.willbes.net/public/images/promotion/2020/12/1971_tab01_02.jpg" alt="기술직" />
+                <img src="https://static.willbes.net/public/images/promotion/2020/12/1971_tab01_02.jpg" alt="기술직" usemap="#Map1971A" border="0" />
+                <map name="Map1971A">
+                    <area shape="rect" coords="243,585,332,627" href="https://pass.willbes.net/package/show/cate/3024/pack/648001/prod-code/176362" target="_blank" alt="전산직 기초">
+                    <area shape="rect" coords="243,653,329,693" href="https://pass.willbes.net/package/show/cate/3024/pack/648001/prod-code/176364" target="_blank" alt="전산직 기본">
+                    <area shape="rect" coords="243,726,333,765" href="https://pass.willbes.net/package/show/cate/3024/pack/648001/prod-code/176366" target="_blank" alt="전산직 심화">
+                    <area shape="rect" coords="91,1094,288,1154" href="https://pass.willbes.net/periodPackage/show/cate/3024/pack/648001/prod-code/176396" target="_blank" alt="곽후근 티패스">
+                    <area shape="rect" coords="613,602,700,642" href="https://pass.willbes.net/package/show/cate/3024/pack/648001/prod-code/176388" target="_blank" alt="환경직 기초">
+                    <area shape="rect" coords="613,688,700,730" href="https://pass.willbes.net/package/show/cate/3024/pack/648001/prod-code/176390" target="_blank" alt="환경직 기본">
+                    <area shape="rect" coords="613,772,700,812" href="https://pass.willbes.net/package/show/cate/3024/pack/648001/prod-code/176391" target="_blank" alt="환경직 심화">
+                    <area shape="rect" coords="614,840,700,883" href="https://pass.willbes.net/package/show/cate/3024/pack/648001/prod-code/176393" target="_blank" alt="환경직 특채">
+                    <area shape="rect" coords="464,1094,652,1155" href="https://pass.willbes.net/periodPackage/show/cate/3024/pack/648001/prod-code/176398" target="_blank" alt="환경직 티패스">
+                    <area shape="rect" coords="985,585,1071,627" href="https://pass.willbes.net/package/show/cate/3024/pack/648001/prod-code/176367" target="_blank" alt="산림자원직 기초">
+                    <area shape="rect" coords="983,649,1070,694" href="https://pass.willbes.net/package/show/cate/3024/pack/648001/prod-code/176370" target="_blank" alt="산림자원직 기본">                
+                    <area shape="rect" coords="830,1095,1028,1153" href="https://pass.willbes.net/periodPackage/show/cate/3024/pack/648001/prod-code/176397" target="_blank" alt="산림자원직 티패스">
+                </map>
                 <br>
-                <img src="https://static.willbes.net/public/images/promotion/2020/12/1971_tab01_03.jpg" alt="기술직" />
+                <img src="https://static.willbes.net/public/images/promotion/2020/12/1971_tab01_03.jpg" alt="기술직" usemap="#Map1971B" border="0" />
+                <map name="Map1971B">
+                    <area shape="rect" coords="226,378,314,424" href="https://pass.willbes.net/professor/show/cate/3028/prof-idx/51162?subject_idx=1169&subject_name=컴퓨터일반" target="_blank" alt="곽후근">
+                    <area shape="rect" coords="772,379,868,422" href="https://pass.willbes.net/professor/show/cate/3028/prof-idx/51163?subject_idx=2129&subject_name=환경공학" target="_blank" alt="신영조">
+                    <area shape="rect" coords="226,560,313,600" href="https://pass.willbes.net/professor/show/cate/3028/prof-idx/51165?subject_idx=1182&subject_name=화학" target="_blank" alt="송연욱">
+                    <area shape="rect" coords="775,559,868,600" href="https://pass.willbes.net/professor/show/cate/3028/prof-idx/51164?subject_idx=1182&subject_name=화학" target="_blank" alt="김병일">
+                    <area shape="rect" coords="227,739,314,783" href="https://pass.willbes.net/professor/show/cate/3028/prof-idx/50395?subject_idx=2130&subject_name=환경보건" target="_blank" alt="하재남">
+                    <area shape="rect" coords="776,740,865,782" href="https://pass.willbes.net/professor/show/cate/3028/prof-idx/50541?subject_idx=1223&subject_name=임업경영" target="_blank" alt="장재영">
+                    <area shape="rect" coords="109,1219,302,1277" href="#none" onclick="javascript:alert('준비중입니다.');" alt="전산직 패스">
+                    <area shape="rect" coords="461,1216,656,1279" href="#" onclick="javascript:alert('준비중입니다.');" alt="환경직 패스">
+                    <area shape="rect" coords="814,1219,1009,1278" href="#" onclick="javascript:alert('준비중입니다.');" alt="산림자원직 패스">
+                </map>
             </div>
         </div>
 
@@ -55,7 +80,14 @@
                 <img src="https://static.willbes.net/public/images/promotion/2020/12/1971_tab02_01.jpg" alt="세무직" />
             </div>
             <div class="evtCtnsBox evt02">
-                <img src="https://static.willbes.net/public/images/promotion/2020/12/1971_tab02_02.jpg" alt="세무직" />
+                <img src="https://static.willbes.net/public/images/promotion/2020/12/1971_tab02_02.jpg" alt="세무직" usemap="#Map1971C" border="0" />
+                <map name="Map1971C">
+                  <area shape="rect" coords="851,592,985,639" href="https://pass.willbes.net/lecture/show/cate/3022/pattern/only/prod-code/175999" target="_blank" alt="회계학 기본">
+                  <area shape="rect" coords="852,650,985,696" href="https://pass.willbes.net/lecture/show/cate/3022/pattern/only/prod-code/176001" target="_blank" alt="회계학 심화">
+                  <area shape="rect" coords="848,851,984,897" href="https://pass.willbes.net/lecture/show/cate/3022/pattern/only/prod-code/175992" target="_blank" alt="세법 기본">
+                  <area shape="rect" coords="852,910,985,953" href="https://pass.willbes.net/lecture/show/cate/3022/pattern/only/prod-code/175998" target="_blank" alt="세법 심화">
+                  <area shape="rect" coords="739,1308,932,1368" href="#" onclick="javascript:alert('준비중입니다.');" alt="세무직 패스">
+                </map>
             </div>
         </div>
 

93aac53f5ebe0da89d17ddf5a7c6d0502bb8b525
이윤구 <yoongukk@willbes.com>
2020-12-14 18:34:05 +0900

    NEW 2022 과목개편대비 입문기초 입문기본 단과 페이지 모바일 신규

---
+++ views/willbes/m/promotion/2001/1981.blade.php
@@ -0,0 +1,100 @@
+@extends('willbes.m.layouts.master')
+
+@section('content')
+<!-- Container -->
+<style type="text/css">
+    .evtCtnsBox {width:100%; max-width:720px; margin:0 auto; text-align:center; position:relative; font-size:14px; line-height:1.5; word-break: keep-all;}
+    .evtCtnsBox img {width:100%; max-width:720px;}
+    .title {font-size:30px;  margin:20px 10px; text-align:left; color:#65069b}
+
+    .video-container-box {padding:20px}
+    .video-container {position:relative; padding-bottom:56.25%; padding-top:30px; height:0; overflow: hidden;}
+    .video-container iframe,
+    .video-container object,
+    .video-container embed {position:absolute; top:0; left:50%; width:100%; margin-left:-50%; height:100%;}
+
+    .evtFooter {margin:0 auto; padding:30px 20px; text-align:left;background:#555; color:#fff; font-size:0.875rem; line-height:1.4 }
+    .evtFooter h3 {font-size:1.5rem; margin-bottom:30px; color:#fff}
+    .evtFooter p {margin-bottom:10px; color:#fff; font-size:1.2rem;}
+    .evtFooter div,
+    .evtFooter ul {margin-bottom:30px; padding-left:10px}
+    .evtFooter li {margin-left:20px; list-style-type: decimal;}
+
+    /* 폰 가로, 태블릿 세로*/
+    @@media only all and (min-width: 408px)  {        
+        
+    }
+
+    /* 태블릿 세로 */
+    @@media only all and (min-width: 768px) {
+
+    }
+    /* 태블릿 가로, PC */
+    @@media only all and (min-width: 1024px) {
+
+    }
+</style>
+
+<div id="Container" class="Container NSK c_both"> 
+    <div class="evtCtnsBox">
+        <img src="https://static.willbes.net/public/images/promotion/2020/12/1967_mpolice.jpg" alt="" >
+    </div> 
+
+    <div class="evtCtnsBox">
+        <img src="https://static.willbes.net/public/images/promotion/2020/12/1981_mtop.jpg" alt="" usemap="#Map1981_mtop" border="0" >
+        <map name="Map1981_mtop" id="Map1981_mtop">
+            <area shape="rect" coords="189,653,515,706" href="https://police.willbes.net/package/index/cate/3001/pack/648001?course_idx=1004&school_year=2022" target="_blank" />
+        </map>
+    </div> 
+    
+    <div class="evtCtnsBox">
+        <img src="https://static.willbes.net/public/images/promotion/2020/12/1981_01m.jpg" alt="" >
+        <div class="video-container-box">
+            <div class="video-container">
+                <iframe src="https://www.youtube.com/embed/qkIw507IPpM?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div> 
+
+    <div class="evtCtnsBox">
+        <img src="https://static.willbes.net/public/images/promotion/2020/12/1981_01sm.jpg" alt="" >
+    </div> 
+
+    <div class="evtCtnsBox">
+        <img src="https://static.willbes.net/public/images/promotion/2020/12/1981_02m.jpg" alt="" >
+    </div> 
+
+    <div class="evtCtnsBox">
+        <img src="https://static.willbes.net/public/images/promotion/2020/12/1981_03m.jpg" alt="" >
+    </div> 
+
+    <div class="evtCtnsBox">
+        <img src="https://static.willbes.net/public/images/promotion/2020/12/1981_04m.gif" alt="" >
+    </div> 
+
+    <div class="evtCtnsBox">
+        <img src="https://static.willbes.net/public/images/promotion/2020/12/1981_05m.jpg" alt="" >
+    </div> 
+
+    <div class="evtCtnsBox">
+        <img src="https://static.willbes.net/public/images/promotion/2020/12/1981_06m.jpg" alt="" >
+    </div> 
+
+    <div class="evtCtnsBox">
+        <img src="https://static.willbes.net/public/images/promotion/2020/12/1981_07m.jpg" alt="" >
+    </div>     
+    
+    @if(empty($arr_base['display_product_data']) === false)
+        @include('willbes.m.promotion.display_product_partial',array('group_num'=>1))
+    @endif
+
+    <div class="evtCtnsBox">
+        <img src="https://static.willbes.net/public/images/promotion/2020/12/1981_08m.jpg" alt="" >
+    </div> 
+
+    @if(empty($arr_base['display_product_data']) === false)
+        @include('willbes.m.promotion.display_product_partial',array('group_num'=>2))
+    @endif
+
+<!-- End Container -->
+@stop

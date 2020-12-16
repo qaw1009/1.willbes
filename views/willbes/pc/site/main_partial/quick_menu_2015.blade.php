ea4d1844e37361ff21cf4e454f0c8fc2f8922bad
김창용 <winmir@willbes.com>
2020-12-16 16:39:55 +0900

    메인 퀵배너 영역 수정

--- views/willbes/pc/site/main_partial/quick_menu_2015.blade.php
+++ views/willbes/pc/site/main_partial/quick_menu_2015.blade.php
@@ -1,25 +1,24 @@
-<style>
-    .lst-none {list-style-type: none !important;padding-bottom:5px;}
-</style>
-<li class="lst-none">
-    <div class="QuickSlider">
-        {!! banner_html(element('메인_퀵배너1', $data['arr_main_banner']), 'sliderNum') !!}
-    </div>
-</li>
-<ul class="mb5 gobtn">
-    <li><a href="{{ front_url('/offinfo/boardInfo/index/78') }}">이달의 개강안내</a></li>
-    <li><a href="{{ front_url('/offinfo/gallery/index') }}">학원 갤러리</a></li>
-    <li><a href="{{ front_url('/offinfo/boardInfo/index/80') }}">강의 시간표</a></li>
-    <li><a href="{{ front_url('/offinfo/boardInfo/index/89') }}">모의고사</a></li>
-    <li><a href="https://pf.kakao.com/_xapHUxb" target="_blank">실시간 소통실</a></li>
+<ul>
+    <li>
+        <div class="QuickSlider">
+            {!! banner_html(element('메인_퀵배너1', $data['arr_main_banner']), 'sliderNum') !!}
+        </div>
+    </li>
+    <ul class="mb5 gobtn">
+        <li><a href="{{ front_url('/offinfo/boardInfo/index/78') }}">이달의 개강안내</a></li>
+        <li><a href="{{ front_url('/offinfo/gallery/index') }}">학원 갤러리</a></li>
+        <li><a href="{{ front_url('/offinfo/boardInfo/index/80') }}">강의 시간표</a></li>
+        <li><a href="{{ front_url('/offinfo/boardInfo/index/89') }}">모의고사</a></li>
+        <li><a href="https://pf.kakao.com/_xapHUxb" target="_blank">실시간 소통실</a></li>
+    </ul>
+    <li>
+        <div class="QuickSlider">
+            {!! banner_html(element('메인_퀵배너2', $data['arr_main_banner']), 'sliderNum') !!}
+        </div>
+    </li>
+    <li>
+        <div class="QuickSlider">
+            {!! banner_html(element('메인_퀵배너3', $data['arr_main_banner']), 'sliderNum') !!}
+        </div>
+    </li>
 </ul>
-<li class="lst-none">
-    <div class="QuickSlider">
-        {!! banner_html(element('메인_퀵배너2', $data['arr_main_banner']), 'sliderNum') !!}
-    </div>
-</li>
-<li class="lst-none">
-    <div class="QuickSlider">
-        {!! banner_html(element('메인_퀵배너3', $data['arr_main_banner']), 'sliderNum') !!}
-    </div>
-</li>

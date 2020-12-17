44d6d656445b05c5dd00d8287e9447d7d0412968
김창용 <winmir@willbes.com>
2020-12-17 11:46:49 +0900

    임용 합격설명회

--- views/willbes/pc/promotion/2017/1973.blade.php
+++ views/willbes/pc/promotion/2017/1973.blade.php
@@ -136,15 +136,15 @@
                                             <tr>
                                                 <td>
                                                     <a href="#none">2021 학년도 대비 ></a>
-                                                    <a href="@if($file_yn[7] == 'Y') {{ front_url($file_link[4]) }} @else {{ $file_link[4] }} @endif" alt="설명회 자료">자료 ></a>
+                                                    <a href="@if($file_yn[4] == 'Y') {{ front_url($file_link[4]) }} @else {{ $file_link[4] }} @endif" alt="설명회 자료">자료 ></a>
                                                 </td>
                                                 <td>
                                                     <a href="#none">2022 학년도 대비 ></a>
-                                                    <a href="@if($file_yn[8] == 'Y') {{ front_url($file_link[5]) }} @else {{ $file_link[6] }} @endif" alt="설명회 자료">자료 ></a>
+                                                    <a href="@if($file_yn[5] == 'Y') {{ front_url($file_link[5]) }} @else {{ $file_link[6] }} @endif" alt="설명회 자료">자료 ></a>
                                                 </td>
                                                 <td>
                                                     <a href="#none">2021 학년도 기출 ></a>
-                                                    <a href="@if($file_yn[9] == 'Y') {{ front_url($file_link[6]) }} @else {{ $file_link[6] }} @endif" alt="기출 자료">자료 ></a>
+                                                    <a href="@if($file_yn[6] == 'Y') {{ front_url($file_link[6]) }} @else {{ $file_link[6] }} @endif" alt="기출 자료">자료 ></a>
                                                 </td>
                                             </tr>
                                         </tbody>
@@ -172,15 +172,15 @@
                                             <tr>
                                                 <td>
                                                     <a href="#none">2021 학년도 대비 ></a>
-                                                    <a href="@if($file_yn[10] == 'Y') {{ front_url($file_link[7]) }} @else {{ $file_link[7] }} @endif" alt="설명회 자료">자료 ></a>
+                                                    <a href="@if($file_yn[7] == 'Y') {{ front_url($file_link[7]) }} @else {{ $file_link[7] }} @endif" alt="설명회 자료">자료 ></a>
                                                 </td>
                                                 <td>
                                                     <a href="#none">2022 학년도 대비 ></a>
-                                                    <a href="@if($file_yn[11] == 'Y') {{ front_url($file_link[8]) }} @else {{ $file_link[8] }} @endif" alt="설명회 자료">자료 ></a>
+                                                    <a href="@if($file_yn[8] == 'Y') {{ front_url($file_link[8]) }} @else {{ $file_link[8] }} @endif" alt="설명회 자료">자료 ></a>
                                                 </td>
                                                 <td>
                                                     <a href="#none">2021 학년도 기출 ></a>
-                                                    <a href="@if($file_yn[12] == 'Y') {{ front_url($file_link[9]) }} @else {{ $file_link[9] }} @endif" alt="기출 자료">자료 ></a>
+                                                    <a href="@if($file_yn[9] == 'Y') {{ front_url($file_link[9]) }} @else {{ $file_link[9] }} @endif" alt="기출 자료">자료 ></a>
                                                 </td>
                                             </tr>
                                         </tbody>
@@ -207,11 +207,11 @@
                                             <tr>
                                                 <td>
                                                     <a href="#none">2021 학년도 대비 ></a>
-                                                    <a href="@if($file_yn[13] == 'Y') {{ front_url($file_link[10]) }} @else {{ $file_link[10] }} @endif" alt="설명회 자료">자료 ></a>
+                                                    <a href="@if($file_yn[10] == 'Y') {{ front_url($file_link[10]) }} @else {{ $file_link[10] }} @endif" alt="설명회 자료">자료 ></a>
                                                 </td>
                                                 <td>
                                                     <a href="#none">2022 학년도 대비 ></a>
-                                                    <a href="@if($file_yn[14] == 'Y') {{ front_url($file_link[11]) }} @else {{ $file_link[11] }} @endif" alt="기출 자료">자료 ></a>
+                                                    <a href="@if($file_yn[11] == 'Y') {{ front_url($file_link[11]) }} @else {{ $file_link[11] }} @endif" alt="기출 자료">자료 ></a>
                                                 </td>
                                             </tr>
                                         </tbody>
@@ -240,15 +240,15 @@
                                             <tr>
                                                 <td>
                                                     <a href="#none">2021 학년도 대비 ></a>
-                                                    <a href="@if($file_yn[15] == 'Y') {{ front_url($file_link[12]) }} @else {{ $file_link[12] }} @endif" alt="설명회 자료">자료 ></a>
+                                                    <a href="@if($file_yn[12] == 'Y') {{ front_url($file_link[12]) }} @else {{ $file_link[12] }} @endif" alt="설명회 자료">자료 ></a>
                                                 </td>
                                                 <td>
                                                     <a href="#none">2022 학년도 대비 ></a>
-                                                    <a href="@if($file_yn[16] == 'Y') {{ front_url($file_link[13]) }} @else {{ $file_link[13] }} @endif" alt="설명회 자료">자료 ></a>
+                                                    <a href="@if($file_yn[13] == 'Y') {{ front_url($file_link[13]) }} @else {{ $file_link[13] }} @endif" alt="설명회 자료">자료 ></a>
                                                 </td>
                                                 <td>
                                                     <a href="#none">2021 학년도 기출 ></a>
-                                                    <a href="@if($file_yn[17] == 'Y') {{ front_url($file_link[14]) }} @else {{ $file_link[14] }} @endif" alt="기출 자료">자료 ></a>
+                                                    <a href="@if($file_yn[14] == 'Y') {{ front_url($file_link[14]) }} @else {{ $file_link[14] }} @endif" alt="기출 자료">자료 ></a>
                                                 </td>
                                             </tr>
                                         </tbody>
@@ -276,15 +276,15 @@
                                             <tr>
                                                 <td>
                                                     <a href="#none">2021 학년도 대비 ></a>
-                                                    <a href="@if($file_yn[18] == 'Y') {{ front_url($file_link[15]) }} @else {{ $file_link[15] }} @endif" alt="설명회 자료">자료 ></a>
+                                                    <a href="@if($file_yn[15] == 'Y') {{ front_url($file_link[15]) }} @else {{ $file_link[15] }} @endif" alt="설명회 자료">자료 ></a>
                                                 </td>
                                                 <td>
                                                     <a href="#none">2022 학년도 대비 ></a>
-                                                    <a href="@if($file_yn[19] == 'Y') {{ front_url($file_link[16]) }} @else {{ $file_link[16] }} @endif" alt="설명회 자료">자료 ></a>
+                                                    <a href="@if($file_yn[16] == 'Y') {{ front_url($file_link[16]) }} @else {{ $file_link[16] }} @endif" alt="설명회 자료">자료 ></a>
                                                 </td>
                                                 <td>
                                                     <a href="#none">2021 학년도 기출 ></a>
-                                                    <a href="@if($file_yn[20] == 'Y') {{ front_url($file_link[17]) }} @else {{ $file_link[17] }} @endif" alt="기출 자료">자료 ></a>
+                                                    <a href="@if($file_yn[17] == 'Y') {{ front_url($file_link[17]) }} @else {{ $file_link[17] }} @endif" alt="기출 자료">자료 ></a>
                                                 </td>
                                             </tr>
                                         </tbody>
@@ -312,15 +312,15 @@
                                             <tr>
                                                 <td>
                                                     <a href="#none">2021 학년도 대비 ></a>
-                                                    <a href="@if($file_yn[21] == 'Y') {{ front_url($file_link[21]) }} @else {{ $file_link[21] }} @endif" alt="설명회 자료">자료 ></a>
+                                                    <a href="@if($file_yn[18] == 'Y') {{ front_url($file_link[18]) }} @else {{ $file_link[18] }} @endif" alt="설명회 자료">자료 ></a>
                                                 </td>
                                                 <td>
                                                     <a href="#none">2022 학년도 대비 ></a>
-                                                    <a href="@if($file_yn[22] == 'Y') {{ front_url($file_link[22]) }} @else {{ $file_link[22] }} @endif" alt="설명회 자료">자료 ></a>
+                                                    <a href="@if($file_yn[19] == 'Y') {{ front_url($file_link[19]) }} @else {{ $file_link[19] }} @endif" alt="설명회 자료">자료 ></a>
                                                 </td>
                                                 <td>
                                                     <a href="#none">2021 학년도 기출 ></a>
-                                                    <a href="@if($file_yn[23] == 'Y') {{ front_url($file_link[23]) }} @else {{ $file_link[23] }} @endif" alt="기출 자료">자료 ></a>
+                                                    <a href="@if($file_yn[20] == 'Y') {{ front_url($file_link[20]) }} @else {{ $file_link[20] }} @endif" alt="기출 자료">자료 ></a>
                                                 </td>
                                             </tr>
                                         </tbody>
@@ -348,16 +348,16 @@
                                             <tr>
                                                 <td>
                                                     <a href="#none">2021 학년도 대비 ></a>
-                                                    <a href="@if($file_yn[24] == 'Y') {{ front_url($file_link[24]) }} @else {{ $file_link[24] }} @endif" alt="설명회 자료">자료 ></a>
+                                                    <a href="@if($file_yn[21] == 'Y') {{ front_url($file_link[21]) }} @else {{ $file_link[21] }} @endif" alt="설명회 자료">자료 ></a>
                                                 </td>
                                                 <td>
                                                     <a href="#none">2022 학년도 대비 ></a>
-                                                    <a href="@if($file_yn[25] == 'Y') {{ front_url($file_link[25]) }} @else {{ $file_link[25] }} @endif" alt="설명회 자료">자료 ></a>
+                                                    <a href="@if($file_yn[22] == 'Y') {{ front_url($file_link[22]) }} @else {{ $file_link[22] }} @endif" alt="설명회 자료">자료 ></a>
                                                 </td>
                                                 <td>
                                                     <a href="#none">2021 학년도 기출 1강 ></a>
                                                     <a href="#none">2021 학년도 기출 2강 ></a>
-                                                    <a href="@if($file_yn[26] == 'Y') {{ front_url($file_link[26]) }} @else {{ $file_link[26] }} @endif" alt="기출 자료">자료 ></a>
+                                                    <a href="@if($file_yn[23] == 'Y') {{ front_url($file_link[23]) }} @else {{ $file_link[23] }} @endif" alt="기출 자료">자료 ></a>
                                                 </td>
                                             </tr>
                                         </tbody>
@@ -385,15 +385,15 @@
                                             <tr>
                                                 <td>
                                                     <a href="#none">2021 학년도 대비 ></a>
-                                                    <a href="@if($file_yn[27] == 'Y') {{ front_url($file_link[21]) }} @else {{ $file_link[21] }} @endif" alt="설명회 자료">자료 ></a>
+                                                    <a href="@if($file_yn[24] == 'Y') {{ front_url($file_link[24]) }} @else {{ $file_link[24] }} @endif" alt="설명회 자료">자료 ></a>
                                                 </td>
                                                 <td>
                                                     <a href="#none">2022 학년도 대비 ></a>
-                                                    <a href="@if($file_yn[28] == 'Y') {{ front_url($file_link[22]) }} @else {{ $file_link[22] }} @endif" alt="설명회 자료">자료 ></a>
+                                                    <a href="@if($file_yn[25] == 'Y') {{ front_url($file_link[25]) }} @else {{ $file_link[25] }} @endif" alt="설명회 자료">자료 ></a>
                                                 </td>
                                                 <td>
                                                     <a href="#none">2021 학년도 기출 ></a>
-                                                    <a href="@if($file_yn[29] == 'Y') {{ front_url($file_link[23]) }} @else {{ $file_link[23] }} @endif" alt="기출 자료">자료 ></a>
+                                                    <a href="@if($file_yn[26] == 'Y') {{ front_url($file_link[26]) }} @else {{ $file_link[26] }} @endif" alt="기출 자료">자료 ></a>
                                                 </td>
                                             </tr>
                                         </tbody>
@@ -421,15 +421,15 @@
                                             <tr>
                                                 <td>
                                                     <a href="#none">2021 학년도 대비 ></a>
-                                                    <a href="@if($file_yn[30] == 'Y') {{ front_url($file_link[24]) }} @else {{ $file_link[24] }} @endif" alt="설명회 자료">자료 ></a>
+                                                    <a href="@if($file_yn[27] == 'Y') {{ front_url($file_link[27]) }} @else {{ $file_link[27] }} @endif" alt="설명회 자료">자료 ></a>
                                                 </td>
                                                 <td>
                                                     <a href="#none">2022 학년도 대비 ></a>
-                                                    <a href="@if($file_yn[31] == 'Y') {{ front_url($file_link[25]) }} @else {{ $file_link[25] }} @endif" alt="설명회 자료">자료 ></a>
+                                                    <a href="@if($file_yn[28] == 'Y') {{ front_url($file_link[28]) }} @else {{ $file_link[28] }} @endif" alt="설명회 자료">자료 ></a>
                                                 </td>
                                                 <td>
                                                     <a href="#none">2021 학년도 기출 ></a>
-                                                    <a href="@if($file_yn[32] == 'Y') {{ front_url($file_link[26]) }} @else {{ $file_link[26] }} @endif" alt="기출 자료">자료 ></a>
+                                                    <a href="@if($file_yn[29] == 'Y') {{ front_url($file_link[29]) }} @else {{ $file_link[29] }} @endif" alt="기출 자료">자료 ></a>
                                                 </td>
                                             </tr>
                                         </tbody>
@@ -457,15 +457,15 @@
                                             <tr>
                                                 <td>
                                                     <a href="#none">2021 학년도 대비 ></a>
-                                                    <a href="@if($file_yn[33] == 'Y') {{ front_url($file_link[27]) }} @else {{ $file_link[27] }} @endif" alt="설명회 자료">자료 ></a>
+                                                    <a href="@if($file_yn[30] == 'Y') {{ front_url($file_link[30]) }} @else {{ $file_link[30] }} @endif" alt="설명회 자료">자료 ></a>
                                                 </td>
                                                 <td>
                                                     <a href="javascript:alert('준비중입니다.');">2022 학년도 대비 ></a>
-                                                    <a href="@if($file_yn[34] == 'Y') {{ front_url($file_link[28]) }} @else {{ $file_link[28] }} @endif" alt="설명회 자료">자료 ></a>
+                                                    <a href="@if($file_yn[31] == 'Y') {{ front_url($file_link[31]) }} @else {{ $file_link[31] }} @endif" alt="설명회 자료">자료 ></a>
                                                 </td>
                                                 <td>
                                                     <a href="#none">2021 학년도 기출 ></a>
-                                                    <a href="@if($file_yn[35] == 'Y') {{ front_url($file_link[29]) }} @else {{ $file_link[29] }} @endif" alt="기출 자료">자료 ></a>
+                                                    <a href="@if($file_yn[32] == 'Y') {{ front_url($file_link[32]) }} @else {{ $file_link[32] }} @endif" alt="기출 자료">자료 ></a>
                                                 </td>
                                             </tr>
                                         </tbody>
@@ -482,7 +482,7 @@
             <img src="https://static.willbes.net/public/images/promotion/2020/12/1973_03.jpg" alt="소식알리기" usemap="#Map1973" border="0"/>
             <map name="Map1973">
                 <area shape="rect" coords="221,1905,546,1989" href="#none" alt="주소복사">
-                <area shape="rect" coords="564,1906,886,1990" href="@if($file_yn[30] == 'Y') {{ front_url($file_link[30]) }} @else {{ $file_link[30] }} @endif"  alt="이미지 다운로드">
+                <area shape="rect" coords="564,1906,886,1990" href="@if($file_yn[33] == 'Y') {{ front_url($file_link[33]) }} @else {{ $file_link[33] }} @endif"  alt="이미지 다운로드">
             </map>
         </div>
 

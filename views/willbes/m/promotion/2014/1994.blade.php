21c57b2d77b1b8ffa1c45516e427fda8d3e83db6
이윤구 <yoongukk@willbes.com>
2020-12-22 13:45:23 +0900

    [N잡] 연말 1+1 이벤트 프로모션 페이지 신규

---
+++ views/willbes/m/promotion/2014/1994.blade.php
@@ -0,0 +1,86 @@
+@extends('willbes.m.layouts.master')
+
+@section('content')
+<!-- Container -->
+<style type="text/css">
+    .evtCtnsBox {width:100%; max-width:720px; margin:0 auto; text-align:center; position:relative; font-size:14px; line-height:1.5}
+    .evtCtnsBox img {width:100%; max-width:720px;}
+
+    .evt00 {text-align:center;}
+    .evt00 .dday {font-size:22px;padding:20px 0;}
+    .evt00 .dday span {color:#435d96; box-shadow:inset 0 -15px 0 rgba(0,0,0,0.1);}
+
+    .evtTop {position:relative}
+    
+    .evt01 {}
+
+    .evt02 {text-align:center; }
+    .evt02 .dday {font-size:22px;padding:20px 0; background:#fff}
+    .evt02 .dday span {color:#a0774e; box-shadow:inset 0 -25px 0 rgba(0,0,0,0.1);}
+    .evt03 {}   
+
+
+   /* 폰 가로, 태블릿 세로*/
+   @@media only all and (min-width: 408px)  {        
+    }
+
+    /* 태블릿 세로 */
+    @@media only all and (min-width: 768px) {
+        .evt00 .dday {font-size:30px;}
+        .evt00 .dday span {box-shadow:inset 0 -20px 0 rgba(0,0,0,0.1);}  
+    }
+
+    /* 태블릿 가로, PC */
+    @@media only all and (min-width: 1024px) {
+
+    }
+
+</style>
+
+<div id="Container" class="Container NSK c_both">            
+    <div class="evtCtnsBox evt00 ddayArea">
+        <div class="dday NSK-Thin">
+            <strong class="NSK-Black">따블로 할인 이벤트 <br><span id="ddayCountText"></span> 남았습니다.</strong>
+        </div>     
+    </div>  
+
+    <div class="evtCtnsBox evtTop">
+        <img src="https://static.willbes.net/public/images/promotion/2020/12/1994_top.jpg" alt="" >        
+    </div> 
+    
+    <div class="evtCtnsBox evt01">
+        <img src="https://static.willbes.net/public/images/promotion/2020/12/1994_01.jpg" alt="" >
+    </div> 
+    
+    <div class="evtCtnsBox evt02">
+        <img src="https://static.willbes.net/public/images/promotion/2020/12/1994_02.jpg" alt="" >  
+    </div>     
+
+    <div class="evtCtnsBox evt03">
+        <img src="https://static.willbes.net/public/images/promotion/2020/12/1994_03.jpg" alt="" usemap="#Map1994A" border="0" />
+        <map name="Map1994A" id="Map1994A">
+            <area shape="rect" coords="89,261,622,356" href="https://njob.willbes.net/userPackage/show/cate/3114/prod-code/177081" target="_blank" alt="시작하기" />
+            <area shape="rect" coords="247,362,461,407" href="https://njob.willbes.net/support/notice/show/cate/3114?board_idx=296682" target="_blank" alt="이용안내" />
+        </map>
+    </div> 
+
+</div>
+<!-- End Container -->
+
+    <script type="text/javascript">
+        /*디데이카운트다운*/
+        $(document).ready(function() {
+            dDayCountDownText('{{$arr_promotion_params['edate']}}');
+        });
+
+    </script>
+
+    <!-- AceCounter Log Gathering Script V.8.0.AMZ2019080601 -->
+    <script language='javascript'>
+        var _AceGID=(function(){var Inf=['gtp14.acecounter.com','8080','AH1A44052179653','AW','0','NaPm,Ncisy','ALL','0']; var _CI=(!_AceGID)?[]:_AceGID.val;var _N=0;var _T=new Image(0,0);if(_CI.join('.').indexOf(Inf[3])<0){ _T.src ="https://"+ Inf[0] +'/?cookie'; _CI.push(Inf);  _N=_CI.length; } return {o: _N,val:_CI}; })();
+        var _AceCounter=(function(){var G=_AceGID;var _sc=document.createElement('script');var _sm=document.getElementsByTagName('script')[0];if(G.o!=0){var _A=G.val[G.o-1];var _G=(_A[0]).substr(0,_A[0].indexOf('.'));var _C=(_A[7]!='0')?(_A[2]):_A[3];var _U=(_A[5]).replace(/\,/g,'_');_sc.src='https:'+'//cr.acecounter.com/Web/AceCounter_'+_C+'.js?gc='+_A[2]+'&py='+_A[4]+'&gd='+_G+'&gp='+_A[1]+'&up='+_U+'&rd='+(new Date().getTime());_sm.parentNode.insertBefore(_sc,_sm);return _sc.src;}})();
+    </script>
+    <noscript><img src='https://gtp14.acecounter.com:8080/?uid=AH1A44052179653&je=n&' border='0' width='0' height='0' alt=''></noscript>
+    <!-- AceCounter Log Gathering Script End -->
+
+@stop
---
+++ views/willbes/pc/promotion/2014/1994.blade.php
@@ -0,0 +1,104 @@
+@extends('willbes.pc.layouts.master')
+
+@section('content')
+    @include('willbes.pc.layouts.partial.site_menu')
+    <!-- content -->
+    <!-- Container -->
+    <style type="text/css">
+        .evtContent {
+            width:100% !important;
+            min-width:1120px !important;
+            margin-top:20px !important;
+            padding:0 !important;
+            background:#fff;
+        }
+        .evtContent span {vertical-align:auto}
+        .evtCtnsBox {width:100%; text-align:center; min-width:1120px; position:relative}
+
+        /************************************************************/
+
+        .sky {position:fixed; top:225px;right:10px;z-index:10;}
+
+        .wb_top {background:#e1c6b0}
+        .wb_01 {background:#e1c6b0}
+        .wb_02 {background:#e1c6b0}
+        .wb_03 {background:url(https://static.willbes.net/public/images/promotion/2020/12/1994_03_bg.jpg) no-repeat center top}
+         /*타이머*/
+        .newTopDday {background:#e4e4e4; width:100%; padding:15px 0 40px; font-size:24px}
+        .newTopDday ul {width:1120px; margin:0 auto;}
+        .newTopDday ul li {display:inline; float:left; margin-right:5px; text-align:center; font-size:28px; height:60px; line-height:60px; padding-top:10px !important; font-weight:bold; color:#000}
+        .newTopDday ul li strong {line-height:60px}
+        .newTopDday ul li img {width:50px}
+        .newTopDday ul li:first-child {text-align:right; padding-right:20px; width:28%; font-size:16px; color:#666; line-height:1.3; }
+        .newTopDday ul li:first-child span { font-size:28px; color:#000; }
+        .newTopDday ul li:last-child {text-align:left; padding-left:20px; width:24%; line-height:60px}
+        .newTopDday ul:after {content:""; display:block; clear:both}  
+    </style>
+
+
+    <div class="p_re evtContent NSK" id="evtContainer">
+        <!-- 타이머 -->
+        <div id="newTopDday" class="newTopDday NG">        
+            <div>
+                <ul>
+                    <li>
+                        따블로 할인 이벤트<br />
+                        <span class="NGEB">{{ kw_date('n.j(%)', $arr_promotion_params['edate']) }} 마감!</span>
+                    </li>
+                    <li><img id="dd1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="dd2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><strong>일</strong></li>
+                    <li><img id="hh1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="hh2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><strong>:</strong></li>
+                    <li><img id="mm1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="mm2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><strong>:</strong></li>
+                    <li><img id="ss1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="ss2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li>
+                        남았습니다
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="sky">
+            <a href="https://njob.willbes.net/userPackage/show/cate/3114/prod-code/177081" target="_blank" > 
+                <img src="https://static.willbes.net/public/images/promotion/2020/12/1994_sky.png" alt="1+1 특가" >
+            </a>             
+        </div>   
+
+        <div class="evtCtnsBox wb_top">
+            <img src="https://static.willbes.net/public/images/promotion/2020/12/1994_top.jpg" alt="할인 이벤트"  />
+        </div>
+
+        <div class="evtCtnsBox wb_01">
+            <img src="https://static.willbes.net/public/images/promotion/2020/12/1994_01.jpg" alt="첫번째 혜택" />
+        </div>
+
+        <div class="evtCtnsBox wb_02">
+            <img src="https://static.willbes.net/public/images/promotion/2020/12/1994_02.jpg" alt="두번재 혜택" />
+        </div>
+
+        <div class="evtCtnsBox wb_03">
+            <img src="https://static.willbes.net/public/images/promotion/2020/12/1994_03.jpg" alt="특가" usemap="#Map1994A" border="0" />
+            <map name="Map1994A" id="Map1994A">
+              <area shape="rect" coords="89,261,622,356" href="https://njob.willbes.net/userPackage/show/cate/3114/prod-code/177081" target="_blank" alt="시작하기" />
+              <area shape="rect" coords="247,362,461,407" href="https://njob.willbes.net/support/notice/show/cate/3114?board_idx=296682" target="_blank" alt="이용안내" />
+            </map>
+        </div>
+    </div>
+    <!-- End Container -->
+
+    <script>
+        /*디데이카운트다운*/
+        $(document).ready(function() {
+            dDayCountDown('{{$arr_promotion_params['edate']}}');
+        });
+    </script>
+
+    {{-- 프로모션용 스크립트 include --}}
+    @include('willbes.pc.promotion.promotion_script')
+
+@stop

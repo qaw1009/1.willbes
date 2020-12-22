64db0384f15db9966995d0b0e735c53a9ceaad46
이윤구 <yoongukk@willbes.com>
2020-12-22 13:51:18 +0900

    엔잡 연말 1+1 이벤트 모바일 타이머 추가

--- views/willbes/m/promotion/2014/1994.blade.php
+++ views/willbes/m/promotion/2014/1994.blade.php
@@ -67,20 +67,82 @@
 </div>
 <!-- End Container -->
 
-    <script type="text/javascript">
-        /*디데이카운트다운*/
-        $(document).ready(function() {
-            dDayCountDownText('{{$arr_promotion_params['edate']}}');
-        });
+<script src="/public/vendor/starplayer/js/starplayer_app.js"></script>
+<link rel="stylesheet" href="/public/vendor/jquery/bxslider/jquery.bxslider.min.css">
+<script src="/public/vendor/jquery/bxslider/jquery.bxslider.js"></script>
+<script type="text/javascript">
+    /*디데이카운트다운*/
+    $(document).ready(function() {
+        dDayCountDownText('{{$arr_promotion_params['edate']}}');
+    });
 
-    </script>
+    // 날짜차이 계산
+    var dDayDateDiff = {
+        inDays: function(dd1, dd2) {
+            var tt2 = dd2.getTime();
+            var tt1 = dd1.getTime();
 
-    <!-- AceCounter Log Gathering Script V.8.0.AMZ2019080601 -->
-    <script language='javascript'>
-        var _AceGID=(function(){var Inf=['gtp14.acecounter.com','8080','AH1A44052179653','AW','0','NaPm,Ncisy','ALL','0']; var _CI=(!_AceGID)?[]:_AceGID.val;var _N=0;var _T=new Image(0,0);if(_CI.join('.').indexOf(Inf[3])<0){ _T.src ="https://"+ Inf[0] +'/?cookie'; _CI.push(Inf);  _N=_CI.length; } return {o: _N,val:_CI}; })();
-        var _AceCounter=(function(){var G=_AceGID;var _sc=document.createElement('script');var _sm=document.getElementsByTagName('script')[0];if(G.o!=0){var _A=G.val[G.o-1];var _G=(_A[0]).substr(0,_A[0].indexOf('.'));var _C=(_A[7]!='0')?(_A[2]):_A[3];var _U=(_A[5]).replace(/\,/g,'_');_sc.src='https:'+'//cr.acecounter.com/Web/AceCounter_'+_C+'.js?gc='+_A[2]+'&py='+_A[4]+'&gd='+_G+'&gp='+_A[1]+'&up='+_U+'&rd='+(new Date().getTime());_sm.parentNode.insertBefore(_sc,_sm);return _sc.src;}})();
-    </script>
-    <noscript><img src='https://gtp14.acecounter.com:8080/?uid=AH1A44052179653&je=n&' border='0' width='0' height='0' alt=''></noscript>
-    <!-- AceCounter Log Gathering Script End -->
+            return Math.floor((tt2-tt1) / (1000 * 60 * 60 * 24));
+        },
+        inWeeks: function(dd1, dd2) {
+            var tt2 = dd2.getTime();
+            var tt1 = dd1.getTime();
+
+            return parseInt((tt2-tt1)/(24*3600*1000*7));
+        },
+        inMonths: function(dd1, dd2) {
+            var dd1Y = dd1.getFullYear();
+            var dd2Y = dd2.getFullYear();
+            var dd1M = dd1.getMonth();
+            var dd2M = dd2.getMonth();
+
+            return (dd2M+12*dd2Y)-(dd1M+12*dd1Y);
+        },
+        inYears: function(dd1, dd2) {
+            return dd2.getFullYear()-dd1.getFullYear();
+        }
+    };
+
+    {{--
+        * 프로모션용 디데이카운터 텍스트
+        * @@param end_date [마감일 (YYYY-MM-DD)]
+    --}}
+    function dDayCountDownText(end_date, ele_id) {
+        if(!ele_id) ele_id = 'ddayCountText';
+        var arr_end_date = end_date.split('-');
+        var event_day = new Date(arr_end_date[0], parseInt(arr_end_date[1]) - 1, arr_end_date[2], 23, 59, 59);
+        var now = new Date();
+        var timeGap = new Date(0, 0, 0, 0, 0, 0, (event_day - now));
+        var date_left = String( dDayDateDiff.inDays(now, event_day) );
+        var hour_left = String( timeGap.getHours() );
+        var minute_left = String( timeGap.getMinutes() );
+        var second_left = String(  timeGap.getSeconds() );
+
+        if(date_left.length == 1) date_left = '0' + date_left;
+        if(hour_left.length == 1) hour_left = '0' + hour_left;
+        if(minute_left.length == 1) minute_left = '0' + minute_left;
+        if(second_left.length == 1) second_left = '0' + second_left;
+
+        if ((event_day.getTime() - now.getTime()) > 0) {
+            $('#'+ele_id).html(date_left + '일 ' + hour_left + ':' + minute_left + ':' + second_left);
+
+            setTimeout(function() {
+                dDayCountDownText(end_date, ele_id);
+            }, 1000);
+        } else {
+            $('#'+ele_id).hide();
+            $('.ddayArea').hide();
+        }
+    }
+    
+</script>
+
+<!-- AceCounter Log Gathering Script V.8.0.AMZ2019080601 -->
+<script language='javascript'>
+    var _AceGID=(function(){var Inf=['gtp14.acecounter.com','8080','AH1A44052179653','AW','0','NaPm,Ncisy','ALL','0']; var _CI=(!_AceGID)?[]:_AceGID.val;var _N=0;var _T=new Image(0,0);if(_CI.join('.').indexOf(Inf[3])<0){ _T.src ="https://"+ Inf[0] +'/?cookie'; _CI.push(Inf);  _N=_CI.length; } return {o: _N,val:_CI}; })();
+    var _AceCounter=(function(){var G=_AceGID;var _sc=document.createElement('script');var _sm=document.getElementsByTagName('script')[0];if(G.o!=0){var _A=G.val[G.o-1];var _G=(_A[0]).substr(0,_A[0].indexOf('.'));var _C=(_A[7]!='0')?(_A[2]):_A[3];var _U=(_A[5]).replace(/\,/g,'_');_sc.src='https:'+'//cr.acecounter.com/Web/AceCounter_'+_C+'.js?gc='+_A[2]+'&py='+_A[4]+'&gd='+_G+'&gp='+_A[1]+'&up='+_U+'&rd='+(new Date().getTime());_sm.parentNode.insertBefore(_sc,_sm);return _sc.src;}})();
+</script>
+<noscript><img src='https://gtp14.acecounter.com:8080/?uid=AH1A44052179653&je=n&' border='0' width='0' height='0' alt=''></noscript>
+<!-- AceCounter Log Gathering Script End -->
 
 @stop

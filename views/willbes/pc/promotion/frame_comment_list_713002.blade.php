44b88c8a4a930635046638bef3e35067180ad08c
김성일 <jxr880210@willbes.com>
2020-12-24 13:24:16 +0900

    [프론트] > 프로모션 1973 수정 테스트

--- views/willbes/pc/promotion/frame_comment_list_713002.blade.php
+++ views/willbes/pc/promotion/frame_comment_list_713002.blade.php
@@ -175,7 +175,7 @@
             @elseif($arr_base['comment_create_type'] == '2')
                 alert('회원만 댓글을 등록할 수 있습니다.');
                 @if(empty($arr_base['login_url']) === false)
-                    window.open('{{$arr_base['login_url']}}', 'login', 'width='+ document.body.clientWidth +', height=1000, left=0, top=0' );
+                    parent.document.location.href = "{{$arr_base['login_url']}}";
                 @endif
             @elseif($arr_base['comment_create_type'] == '3')
             alert('만료된 이벤트 입니다.');

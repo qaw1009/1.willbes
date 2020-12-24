cae7ad8b5c1043236b4e4d311fb689adf1cc7e0b
김창용 <winmir@willbes.com>
2020-12-24 09:19:03 +0900

    경찰 새해다짐 룰렛

--- views/willbes/pc/promotion/2001/1993.blade.php
+++ views/willbes/pc/promotion/2001/1993.blade.php
@@ -21,12 +21,13 @@
  
         .wb_top {position:relative; overflow:hidden}
         .rulletBox {position:absolute; top:2000px; width:786px; left:50%; margin-left:-393px; z-index:5}
+        .rulletBox canvas {width:786px; height:786px}
         .rulletBox .btn-roulette {position:absolute; top:280px; width:255px; 
             height:255px; left:50%; padding:0; margin:0; margin-left:-127px; background:none; z-index:6}
         .rulletBox a {position:absolute; top:650px; left:650px; width:80px; height:80px; line-height:60px; color:#fff; background:#ff6600; font-size:15px;
             border-radius:40px; border:10px solid #000; z-index:20}
         .rulletBox a:hover {background:#a16136; color:#fff}
-        .rulletBox .tutCanvas {width:786px; height:786px}
+        
 
         .giftPopupWrap {
             position:absolute; 

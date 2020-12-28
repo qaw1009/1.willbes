610ccfd96f2d7b4af25ab286cfd8735c274cc199
최형진 <letsboo@willbes.com>
2020-12-28 11:39:25 +0900

    프론트 > 1968 프로모션 - 사파리 이슈 테스트

--- views/willbes/m/promotion/2014/1968.blade.php
+++ views/willbes/m/promotion/2014/1968.blade.php
@@ -88,10 +88,10 @@
             </div>
         </div>
         <div>
-            <img src="https://static.willbes.net/public/images/promotion/2020/12/1968_02_1.jpg" alt="" usemap="#Map1968A" border="0">
+            <img id="lecBuy" src="https://static.willbes.net/public/images/promotion/2020/12/1968_02_1.jpg" alt="" usemap="#Map1968A" border="0">
             <map name="Map1968A">
-                <area shape="rect" coords="78,752,311,846" href="https://njob.willbes.net/m/lecture/show/cate/3114/pattern/only/prod-code/175175" target="_blank" alt="강의+키트 사전예약">
-                <area shape="rect" coords="410,754,639,846" href="https://njob.willbes.net/m/lecture/show/cate/3114/pattern/only/prod-code/175310" target="_blank" alt="강의 사전예약">
+                <area shape="rect" coords="78,752,311,846" href="https://njob.willbes.net/m/lecture/show/cate/3114/pattern/only/prod-code/175175" target="_blank" alt="강의+키트 사전예약" style="display: block;">
+                <area shape="rect" coords="410,754,639,846" href="https://njob.willbes.net/m/lecture/show/cate/3114/pattern/only/prod-code/175310" target="_blank" alt="강의 사전예약" style="display: block;">
             </map>
         </div>
     </div> 

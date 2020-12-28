6863369efa30bea92c0e810e7cc343ec5b5f88d8
최형진 <letsboo@willbes.com>
2020-12-28 14:15:28 +0900

    아이폰 테스트

--- public/js/willbes/mobile.js
+++ public/js/willbes/mobile.js
@@ -258,7 +258,8 @@ function link_go() {
 $(function(){
     $(document).ready(function(e) {
         // IOS에서는 이미지맵을 스크립트로 조절하면 안먹히는 이슈 있음.
-        if (navigator.userAgent.match(/iPad/i) == null && navigator.userAgent.match(/iPhone|iPod/i) == null) {
+        //if (navigator.userAgent.match(/iPad/i) == null && navigator.userAgent.match(/iPhone|iPod/i) == null) {
+        if (navigator.userAgent.match(/iPad/i) == null) {
             $('img[usemap]').rwdImageMaps();
         }
     });

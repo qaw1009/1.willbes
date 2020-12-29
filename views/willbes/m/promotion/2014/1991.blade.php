4837ab26b176050a3802e27e4322bac0dba42be9
김창용 <winmir@willbes.com>
2020-12-29 17:06:58 +0900

    엔잡 요리 프로모션

--- views/willbes/m/promotion/2014/1991.blade.php
+++ views/willbes/m/promotion/2014/1991.blade.php
@@ -231,7 +231,7 @@
         <img src="https://static.willbes.net/public/images/promotion/2020/12/1991_10.jpg" alt="" >
     </div>   
 
-    <div class="evtCtnsBox evt07" id="infoText">
+    <div class="evtCtnsBox evt07" id="notice">
         <div class="evtFooter">
             <h3 class="NSK-Black">이용 안내</h3>
             <p>#사전예약 혜택</p>
@@ -279,24 +279,28 @@
         [온라인강의] 신청하기 >
         </a>
     </div>
-    <div id="pass" class="infoCheck">
-        <input type="checkbox" name="y_pkg" value="172160" style="display: none;" checked/>
-        <input type="checkbox" id="is_chk" name="is_chk"><label for="is_chk">페이지 하단 이용안내를 모두 확인하였고, 이에 동의합니다. </label>
-        <a href="#infoText">이용안내 확인하기 ↓</a>
+    <div class="infoCheck" id="chkInfo">   
+        <label>
+            <input name="ischk" type="checkbox" value="Y" />페이지 하단 이용안내를 모두 확인하였고, 이에 동의합니다.
+        </label>
+        <a href="#notice" class="infotxt">이용안내확인하기 ↓</a>
     </div>
 </div>
-
-<!-- AceCounter Log Gathering Script V.8.0.AMZ2019080601 -->
-<script language='javascript'>
-    var _AceGID=(function(){var Inf=['gtp14.acecounter.com','8080','AH1A44052179653','AW','0','NaPm,Ncisy','ALL','0']; var _CI=(!_AceGID)?[]:_AceGID.val;var _N=0;var _T=new Image(0,0);if(_CI.join('.').indexOf(Inf[3])<0){ _T.src ="https://"+ Inf[0] +'/?cookie'; _CI.push(Inf);  _N=_CI.length; } return {o: _N,val:_CI}; })();
-    var _AceCounter=(function(){var G=_AceGID;var _sc=document.createElement('script');var _sm=document.getElementsByTagName('script')[0];if(G.o!=0){var _A=G.val[G.o-1];var _G=(_A[0]).substr(0,_A[0].indexOf('.'));var _C=(_A[7]!='0')?(_A[2]):_A[3];var _U=(_A[5]).replace(/\,/g,'_');_sc.src='https:'+'//cr.acecounter.com/Web/AceCounter_'+_C+'.js?gc='+_A[2]+'&py='+_A[4]+'&gd='+_G+'&gp='+_A[1]+'&up='+_U+'&rd='+(new Date().getTime());_sm.parentNode.insertBefore(_sc,_sm);return _sc.src;}})();
-</script>
-<noscript><img src='https://gtp14.acecounter.com:8080/?uid=AH1A44052179653&je=n&' border='0' width='0' height='0' alt=''></noscript>
-<!-- AceCounter Log Gathering Script End -->
 
 <link rel="stylesheet" href="/public/vendor/jquery/bxslider/jquery.bxslider.min.css">
 <script src="/public/vendor/jquery/bxslider/jquery.bxslider.js"></script>
 <script type="text/javascript">
+    {{-- 수강신청 동의 --}}
+    function go_PassLecture(code){
+        if($("input[name='ischk']:checked").size() < 1){
+            alert("이용안내에 동의하셔야 합니다.");
+            return;
+        }
+
+        var url = '{{ site_url('/m/lecture/show/cate/3114/pattern/only/prod-code/') }}' + code;
+        location.href = url;
+    };
+
     $(document).ready(function() {
         var slidesImg1 = $("#slidesImg1").bxSlider({
             auto: true, 
@@ -317,18 +321,15 @@
             controls:false,
             pager:true,
         });
-    });
-
-    {{-- 수강신청 동의 --}}
-    function go_PassLecture(code){
-        if($("input[name='ischk']:checked").size() < 1){
-            alert("이용안내에 동의하셔야 합니다.");
-            return;
-        }
-
-        var url = '{{ site_url('/m/lecture/show/cate/3114/pattern/only/prod-code/') }}' + code;
-        location.href = url;
-    }
+    });    
 </script>
+
+<!-- AceCounter Log Gathering Script V.8.0.AMZ2019080601 -->
+<script language='javascript'>
+    var _AceGID=(function(){var Inf=['gtp14.acecounter.com','8080','AH1A44052179653','AW','0','NaPm,Ncisy','ALL','0']; var _CI=(!_AceGID)?[]:_AceGID.val;var _N=0;var _T=new Image(0,0);if(_CI.join('.').indexOf(Inf[3])<0){ _T.src ="https://"+ Inf[0] +'/?cookie'; _CI.push(Inf);  _N=_CI.length; } return {o: _N,val:_CI}; })();
+    var _AceCounter=(function(){var G=_AceGID;var _sc=document.createElement('script');var _sm=document.getElementsByTagName('script')[0];if(G.o!=0){var _A=G.val[G.o-1];var _G=(_A[0]).substr(0,_A[0].indexOf('.'));var _C=(_A[7]!='0')?(_A[2]):_A[3];var _U=(_A[5]).replace(/\,/g,'_');_sc.src='https:'+'//cr.acecounter.com/Web/AceCounter_'+_C+'.js?gc='+_A[2]+'&py='+_A[4]+'&gd='+_G+'&gp='+_A[1]+'&up='+_U+'&rd='+(new Date().getTime());_sm.parentNode.insertBefore(_sc,_sm);return _sc.src;}})();
+</script>
+<noscript><img src='https://gtp14.acecounter.com:8080/?uid=AH1A44052179653&je=n&' border='0' width='0' height='0' alt=''></noscript>
+<!-- AceCounter Log Gathering Script End -->
 
 @stop

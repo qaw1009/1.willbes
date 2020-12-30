7093d99c9bc20d3762a6a98dab7993b21a62df86
김창용 <winmir@willbes.com>
2020-12-30 11:49:57 +0900

    고등고시 새해이벤트

--- views/willbes/pc/promotion/2005/2002.blade.php
+++ views/willbes/pc/promotion/2005/2002.blade.php
@@ -91,7 +91,7 @@
 		.evtInfoBox { width:1000px; margin:0 auto; text-align:left; line-height:1.4}
 		.evtInfoBox h4 {font-size:30px; margin-bottom:40px}
 		.evtInfoBox .infoTit {font-size:18px; margin-bottom:20px}
-		.evtInfoBox .infoTit strong {padding:8px 20px; background:#000; border-radius:20px}
+		.evtInfoBox .infoTit strong {padding:8px 20px; background:#000; border-radius:20px; font-weight:normal !important}
 		.evtInfoBox ul {margin-bottom:30px}
         .evtInfoBox li {margin-bottom:8px; list-style:disc; margin-left:20px}
         .evtInfoBox span {vertical-align:bottom}        
@@ -174,7 +174,7 @@
         <div class="evtCtnsBox evtInfo NSK" id="notice">
             <div class="evtInfoBox">
                 <h4 class="NSK-Black">한림법학원 새해맞이 동영상 특별 이벤트안내</h4>
-                <div class="infoTit NSK-Black"><strong>이용안내</strong></div>
+                <div class="infoTit"><strong>이용안내</strong></div>
                 <ul>
                     <li>본 상품은 2021년 1월 3일 (일)까지 진행되는 이벤트 상품입니다.</li>
                     <li>이벤트 내용 (각 홈페이지 이벤트 페이지 <span class="tx-light-blue">2021년 새해맞이 이벤트!</span>에서 신청시 적용)</li>
@@ -188,21 +188,21 @@
                         3강좌 동시 결제 :  약 <span class="tx-red">30%할인</span></li>
                     <li>2강좌 이상 결제시 적용되는 할인은 이벤트 페이지에서 신청해주셔야 적용됩니다.</li>
                 </ul>
-                <div class="infoTit NSK-Black"><strong>수강시작일관련</strong></div>
+                <div class="infoTit"><strong>수강시작일관련</strong></div>
                 <ul>
                     <li>수강시작일은 30일 범위내에서 지정이 가능합니다.<br>
                         다만, <span class="tx-red">2강좌 이상 선택시 원하시는 수강시작일을 90일 범위내에서</span> 동영상게시판에 적어주시면 변경하여 드리겠습니다.</li>
                 </ul>
-                <div class="infoTit NSK-Black"><strong>교재</strong></div>
+                <div class="infoTit"><strong>교재</strong></div>
                 <ul>
                     <li>각 강의수강에 필요한 교재는 별도로 구매하셔야 하며, 각 강좌별 교재는 강좌소개 및 교재구매 메뉴에서 별도 구매 가능합니다.</li>
                 </ul>
-                <div class="infoTit NSK-Black"><strong>강의수강</strong></div>
+                <div class="infoTit"><strong>강의수강</strong></div>
                 <ul>
-                    <li>강의배수 제한 : 강의는 2배수 제한 규정이 적용됩니다.</li>
+                    <li>강의배수 제한 : 강의는 배수제한 규정이 적용됩니다.</li>
                     <li>동영상 강의는 컴퓨터 또는 스마트기기를 이용하여 수강하실 수 있습니다.</li>
                 </ul>
-                <div class="infoTit NSK-Black"><strong>환불</strong></div>
+                <div class="infoTit"><strong>환불</strong></div>
                 <ul>
                     <li>결제일로부터 7일 이내 전액 환불 가능합니다. 단, 맛보기 강좌를 제외하고 2강 이하 수강 시에만 전액 환불이 가능합니다.</li>
                     <li>강의 자료 및 모바일 강의 다운로드 서비스를 이용 시 수강한 것으로 간주 됩니다.</li>
@@ -210,11 +210,11 @@
                     -사용자패키지를 통한 <span class="tx-red">2과목 이상 수강시 일부과목만의 환불은 불가</span>합니다.(전체환불 진행)</li>
                     <li>아이디 공유 적발 시 회원 자격 박탈 및 환불이 불가하오니 유의 바랍니다.</li>
                 </ul>
-                <div class="infoTit NSK-Black"><strong>유의사항</strong></div>
+                <div class="infoTit"><strong>유의사항</strong></div>
                 <ul>
                     <li>본 상품은 특별할인기획 상품으로 쿠폰 할인, 다다익선 할인, 포인트 사용 등 혜택이 적용되지 않으니 양해 및 참고 부탁드립니다.</li>
                 </ul>
-                <div class="infoTit NSK-Black"><strong>※ 이용 문의 : 윌비스 고객만족센터 1566-4770</strong></div>
+                <div class="infoTit"><strong>※ 이용 문의 : 윌비스 고객만족센터 1566-4770</strong></div>
             </div>
         </div>
 

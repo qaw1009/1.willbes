dd58f735bf47abc659416bc00dedd02abb5398e6
김창용 <winmir@willbes.com>
2020-12-30 19:59:09 +0900

    공무원 학원 공무원학원 신규 개강 - 군무원

--- views/willbes/m/promotion/2004/1781.blade.php
+++ views/willbes/m/promotion/2004/1781.blade.php
@@ -6,6 +6,9 @@
     .evtCtnsBox {width:100%; max-width:720px; margin:0 auto; text-align:center; font-size:14px; line-height:1.5; position:relative;}
     .evtCtnsBox img {width:100%; max-width:720px;}  
 
+    .evt02 a {position: absolute; left: 9.44%; top: 78.72%; width: 81.81%; height: 8.24%; z-index: 2;}
+    .evt04 a {position: absolute; left: 21.11%; top: 73.02%; width: 58.06%; height: 11.69%; z-index: 2;}
+
     /* 폰 가로, 태블릿 세로*/
     @@media only all and (min-width: 408px)  {
     }
@@ -22,10 +25,8 @@
     </div> 
    
     <div class="evtCtnsBox evt02">
-        <img src="https://static.willbes.net/public/images/promotion/2020/12/1781m_02.jpg" alt="라이브모드" usemap="#Map1781A" border="0">
-        <map name="Map1781A">
-          <area shape="rect" coords="68,929,668,1044" href="#evt04" alt="군무원 라이브 모드">
-        </map>
+        <img src="https://static.willbes.net/public/images/promotion/2020/12/1781m_02.jpg" alt="라이브모드">
+        <a href="#evt04" title="군무원 라이브 모드"></a>
     </div>
 
     <div class="evtCtnsBox evt03" >
@@ -33,10 +34,8 @@
     </div>
 
     <div class="evtCtnsBox evt04" id="evt04">
-        <img src="https://static.willbes.net/public/images/promotion/2020/12/1781m_04.jpg" alt="군무원 행정직" usemap="#Map1981B" border="0">
-        <map name="Map1981B">
-          <area shape="rect" coords="143,402,581,476" href="https://pass.willbes.net/m/pass/offPackage/index?cate_code=3048&campus_ccd=605001" target="_blank" alt="군무원 행정직 수강신청">
-        </map>
+        <img src="https://static.willbes.net/public/images/promotion/2020/12/1781m_04.jpg" alt="군무원 행정직">
+        <a href="https://pass.willbes.net/m/pass/offPackage/index?cate_code=3048&campus_ccd=605001" title="군무원 행정직 수강신청" target="_blank"></a>
     </div>
 </div>
 <!-- End Container -->

c594e71f07df32ee6270a4913bfaf7dd5ba3de06
김창용 <winmir@willbes.com>
2021-01-06 11:44:51 +0900

    임용 메인 코딩파일

--- views/html/layouts/cop_topBnr.blade.php
+++ views/html/layouts/cop_topBnr.blade.php
@@ -1,5 +1,9 @@
-<div>
-    <div>
-        <img src="{{ img_url('cop/banner/onTopBnar_190123C_01.gif') }}" alt="배너명">  
+    <div id="topBannerLayer" class="ssam">
+        <div class="topBanner">
+            <img src="https://static.willbes.net/public/images/promotion/main/2018/2018_top_bn.jpg" alt="실전모의고사" usemap="#Map2017" border="0">
+            <map name="Map2017">
+              <area shape="rect" coords="641,10,826,63" href="#none" target="_blank" alt="2020대비실전모의고사다시보기">
+              <area shape="rect" coords="829,11,1020,62" href="#none" target="_blank" alt="2022대비실전모의고사다시보기">
+            </map>
+        </div>
     </div>
-</div>

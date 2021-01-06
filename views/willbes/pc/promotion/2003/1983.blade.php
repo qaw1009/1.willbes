f791cbfbc1b0449cd55c53e1079bf0f1763d0f08
이윤구 <yoongukk@willbes.com>
2021-01-06 14:55:24 +0900

    공무원 온라인 윌비스X대방고시 세무직PASS 페이지 수정

--- views/willbes/pc/promotion/2003/1983.blade.php
+++ views/willbes/pc/promotion/2003/1983.blade.php
@@ -19,7 +19,7 @@
 
         .sky {position:fixed; top:225px;right:10px;z-index:10;}
 
-        .wb_top {background:url(https://static.willbes.net/public/images/promotion/2020/12/1983_top_bg.jpg) no-repeat center top;}
+        .wb_top {background:url(https://static.willbes.net/public/images/promotion/2021/01/1983_top_bg.jpg) no-repeat center top;}
 
         .wb_cts01 {background:#EBEBEB;}
         .wb_cts01 .tImg img {margin:0 5px 10px;width:343px;height:188px;}
@@ -102,36 +102,40 @@
 
         <div class="sky">
             <a href="#apply"> 
-                <img src="https://static.willbes.net/public/images/promotion/2020/12/1983_sky01.png" alt="" >
+                <img src="https://static.willbes.net/public/images/promotion/2021/01/1983_sky01.png" alt="윌비스 패스 바로가기" >
             </a>             
         </div>   
 
         <div class="evtCtnsBox wb_top">
-            <img src="https://static.willbes.net/public/images/promotion/2020/12/1983_top.jpg" alt=""  />
+            <img src="https://static.willbes.net/public/images/promotion/2021/01/1983_top.jpg" alt="세무직 패스"  />
+        </div>
+
+        <div class="evtCtnsBox wb_tops">
+            <img src="https://static.willbes.net/public/images/promotion/2021/01/1983_01.jpg" alt="전격출시"  />
         </div>
 
         <div class="evtCtnsBox wb_cts01">
-            <img src="https://static.willbes.net/public/images/promotion/2020/12/1983_01.jpg" alt="" />
+            <img src="https://static.willbes.net/public/images/promotion/2021/01/1983_02.jpg" alt="라이브로 만나다" />
             <div class="tImg">
-                <img src="https://static.willbes.net/public/images/promotion/2020/12/1982_02_01.gif" alt="" />
-                <img src="https://static.willbes.net/public/images/promotion/2020/12/1982_02_02.gif" alt="" />
-                <img src="https://static.willbes.net/public/images/promotion/2020/12/1982_02_03.gif" alt="" />
+                <img src="https://static.willbes.net/public/images/promotion/2020/12/1982_02_01.gif" alt="강의1" />
+                <img src="https://static.willbes.net/public/images/promotion/2020/12/1982_02_02.gif" alt="강의2" />
+                <img src="https://static.willbes.net/public/images/promotion/2020/12/1982_02_03.gif" alt="강의3" />
             </div>
-            <img src="https://static.willbes.net/public/images/promotion/2020/12/1983_02.jpg" alt="" />
+            <img src="https://static.willbes.net/public/images/promotion/2021/01/1983_03.jpg" alt="합격이 앞당겨집니다" />
         </div>
 
         <div class="evtCtnsBox wb_cts03">
-            <img src="https://static.willbes.net/public/images/promotion/2020/12/1983_03.jpg" alt="" />
+            <img src="https://static.willbes.net/public/images/promotion/2021/01/1983_04.jpg" alt="교수진" />
         </div>
 
         <div class="evtCtnsBox wb_cts04">
-            <img src="https://static.willbes.net/public/images/promotion/2020/12/1983_04.jpg" alt="" />
+            <img src="https://static.willbes.net/public/images/promotion/2021/01/1983_05.jpg" alt="커리큘럼" />
         </div>
 
         <div class="evtCtnsBox wb_cts05">
-            <img src="https://static.willbes.net/public/images/promotion/2020/12/1983_05.jpg" id="apply" alt="" />              
+            <img src="https://static.willbes.net/public/images/promotion/2021/01/1983_06.jpg" id="apply" alt="수강신청" />              
                 <div id="tab1s">
-                    <img src="https://static.willbes.net/public/images/promotion/2020/12/1983_05_01.jpg" title="" />
+                    <img src="https://static.willbes.net/public/images/promotion/2021/01/1983_06_01.jpg" title="신청하기" />
                     <ul class="radio_apply">
                         <li class="one"><input type="radio" id="y_pkg" name="y_pkg" value="176432" onClick=""/><label for="y_pkg"></label></li>   
                         <li class="two"><input type="radio" id="y_pkg" name="y_pkg" value="176415" onClick=""/><label for="y_pkg"></label></li>

d754ce542044a3b4532308c1d194d3ea5a38a7a3
김성일 <jxr880210@willbes.com>
2021-01-15 15:40:08 +0900

    [프론트] > 경찰 온라인_1월 합격 설명회 신청 프로모션 2026

--- views/willbes/pc/promotion/2001/2026.blade.php
+++ views/willbes/pc/promotion/2001/2026.blade.php
@@ -264,6 +264,8 @@
 
     <script>
         function fn_submit() {
+            {!! login_check_inner_script('로그인 후 이용하여 주십시오.','Y') !!}
+
             var $regi_form_register = $('#regi_form_register');
             var _url = '{!! front_url('/event/registerStore') !!}';
 

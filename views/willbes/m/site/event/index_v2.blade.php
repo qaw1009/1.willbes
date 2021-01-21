d487c4bf411890fa536412deb36419289ba60856
김성일 <jxr880210@willbes.com>
2021-01-21 15:03:32 +0900

    [프론트] > 이벤트 게시판 신규 생성

--- views/willbes/m/site/event/index_v2.blade.php
+++ views/willbes/m/site/event/index_v2.blade.php
@@ -16,25 +16,22 @@
             이벤트
         </div>
         <div class="willbes-Lec-Selected NG tx-gray">
+            @if(empty($arr_base['event_type']) === false)
+                <select id="s_event_type" name="s_event_type" title="이벤트종류" class="width49p" onchange="goEventUrl('s_event_type', this.value)">
+                    @foreach($arr_base['event_type'] as $key => $val)
+                        <option value="{{$key}}" @if(element('s_event_type', $arr_input) == $key)selected="selected"@endif>{{$val}}</option>
+                    @endforeach
+                </select>
+            @endif
 
             @if(empty($arr_base['request_type']) === false)
-                <select id="s_request_type" name="s_request_type" title="이벤트종류" class="width49p" onchange="goUrl('s_request_type',this.value)">
+                <select id="s_request_type" name="s_request_type" title="이벤트종류" class="width50p ml1p" onchange="goUrl('s_request_type',this.value)">
                     <option value="">전체 이벤트</option>
                     @foreach($arr_base['request_type'] as $key => $val)
                         <option value="{{$key}}" @if(element('s_request_type', $arr_input) == $key)selected="selected"@endif>{{$val}}</option>
                     @endforeach
                 </select>
             @endif
-
-                <ul class="f_right">
-                    <li class="InfoBtn btn_white">
-                        @if($arr_base['onoff_type'] == 'ongoing')
-                            <a href="{{ front_url('/event/list/'.(empty($__cfg['CateCode']) === false ? 'cate/'.$__cfg['CateCode'].'/pattern/' : '').'end') }}">마감된 이벤트</a>
-                        @elseif($arr_base['onoff_type'] == 'end')
-                            <a href="{{ front_url('/event/list/'.(empty($__cfg['CateCode']) === false ? 'cate/'.$__cfg['CateCode'].'/pattern/' : '').'ongoing') }}">진행중인 이벤트</a>
-                        @endif
-                    </li>
-                </ul>
 
             @if(empty($arr_base['campus']) === false)
                 <select id="s_campus" name="s_campus" title="캠퍼스" class="width50p ml1p" onchange="goUrl('s_campus',this.value)">
@@ -46,7 +43,7 @@
             @endif
 
             <div class="willbes-Lec-Search NG width100p mt1p">
-                <div class="inputBox width90p p_re">
+                <div class="inputBox width88p p_re">
                     <input type="text" id="s_keyword" name="s_keyword" maxlength="30" value="{{ element('s_keyword', $arr_input) }}" class="labelSearch width100p" placeholder="제목 및 내용 검색">
                     <button type="button" onclick="goUrl('s_keyword', document.getElementById('s_keyword').value)" class="search-Btn" class="search-Btn">
                         <span class="hidden">검색</span>
@@ -79,6 +76,12 @@
                             </a>
                         </div>
                         <dl class="w-info tx-gray">
+                            @if($row['IsRegister'] == 'Y' && $row['RegisterEndDate'] > date('Y-m-d H:i:s'))
+                                <dt class="tx-red">진행중<span class="row-line">|</span></dt>
+                            @else
+                                <dt>종료<span class="row-line">|</span></dt>
+                            @endif
+
                             <dt>기간 : {{$row['RegisterStartDay']}}~{{$row['RegisterEndDay']}}<span class="row-line">|</span></dt>
                             <dt>조회수 : <span class="tx-blue">{{$row['ReadCnt']}}</span></dt>
                         </dl>
@@ -100,4 +103,27 @@
         <!-- Topbtn -->
     </div>
     <!-- End Container -->
+
+    <script>
+        function goEventUrl(key, obj_val){
+            var _url = "{{ front_url('/event/list/'.(empty($__cfg['CateCode']) === false ? 'cate/' . $__cfg['CateCode'] . '/pattern/' : '')) }}" + obj_val;
+
+            var $url_form = $('#url_form');
+            var $url_input = $url_form.find('[name="' + key + '"]');
+            var $url_hidden = $url_form.find('input[type="hidden"][name="' + key + '"]');
+
+            if ($url_input.length > 0) {
+                $url_input.val(obj_val);
+                if ($url_input.length > 1 && $url_hidden.length > 0) {
+                    // 동일한 파라미터가 2개 이상일 경우 hidden 파라미터 제거
+                    $url_hidden.remove();
+                }
+            } else {
+                $url_form.append('<input type="hidden" name="' + key + '" value="' + obj_val + '"/>');
+            }
+
+            $url_form.prop('action', _url);
+            $url_form.submit();
+        }
+    </script>
 @stop

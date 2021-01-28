1339917b5eae9b02f5a187374e30683aa792ddb0
김성일 <jxr880210@willbes.com>
2021-01-28 09:27:29 +0900

    [프론트] > 모바일 배너 스크립트 입력 기능 추가

--- views/willbes/m/common/banner.blade.php
+++ views/willbes/m/common/banner.blade.php
@@ -1,13 +1,17 @@
-var html = '', link_url = '#none';
+var html = '', link_url = '#none', link_target = 'self';
 
 @if($disp['DispTypeCcd'] == '664002')
     @if(count($data) == 1)
         // 고정 배너 (등록 배너개수가 1개일 경우)
         @if(empty($data[0]['LinkUrl']) === false && $data[0]['LinkUrl'] != '#')
-            link_url = '{{ front_app_url('/banner/click?banner_idx=' . $data[0]['BIdx'] . '&return_url=' . urlencode($data[0]['LinkUrl']) . '&link_url_type=' . $data[0]['LinkUrlType'], 'www') }}';
+            @if($data[0]['LinkType'] == 'script')
+                link_url = '{{ $data[0]['LinkUrl'] }}';
+            @else
+                link_url = '{{ front_app_url('/banner/click?banner_idx=' . $data[0]['BIdx'] . '&return_url=' . urlencode($data[0]['LinkUrl']) . '&link_url_type=' . $data[0]['LinkUrlType'], 'www') }}';
+            @endif
         @endif
         html = '<div class="{{ $css_class }}">';
-        html += '   <a href="' + link_url + '" target="_{{ $data[0]['LinkType'] }}"><img src="{{ $data[0]['BannerFullPath'] . $data[0]['BannerImgName'] }}" title="{{ $data[0]['BannerName'] }}" style="width: 100%;"/></a>';
+        html += '   <a href="' + link_url + '" @if($data[0]['LinkType'] != 'script') target="_{{ $data[0]['LinkType'] }}" @endif><img src="{{ $data[0]['BannerFullPath'] . $data[0]['BannerImgName'] }}" title="{{ $data[0]['BannerName'] }}" style="width: 100%;"/></a>';
         html += '</div>';
     @else
         // 롤링 배너 (등록 배너개수가 2개 이상일 경우)
@@ -17,10 +21,14 @@ var html = '', link_url = '#none';
             @if(empty($row['LinkUrl']) === false && $row['LinkUrl'] == '#')
                 link_url = '#none';
             @elseif(empty($row['LinkUrl']) === false)
-                link_url = '{{ front_app_url('/banner/click?banner_idx=' . $row['BIdx'] . '&return_url=' . urlencode($row['LinkUrl']) . '&link_url_type=' . $row['LinkUrlType'], 'www') }}';
+                @if($row['LinkType'] == 'script')
+                    link_url = '{{ $row['LinkUrl'] }}';
+                @else
+                    link_url = '{{ front_app_url('/banner/click?banner_idx=' . $row['BIdx'] . '&return_url=' . urlencode($row['LinkUrl']) . '&link_url_type=' . $row['LinkUrlType'], 'www') }}';
+                @endif
             @endif
             html += '   <div class="swiper-slide">';
-            html += '       <a href="' + link_url + '" target="_{{ $row['LinkType'] }}">';
+            html += '       <a href="' + link_url + '" @if($row['LinkType'] != 'script') target="_{{ $row['LinkType'] }}" @endif>';
             html += '           <img src="{{ $row['BannerFullPath'] . $row['BannerImgName'] }}" title="{{ $row['BannerName'] }}"/>';
             @if($set_class == 'bnTit')
                 html += '       <div class="{{ $set_class }}">{{ $row['BannerName'] }}</div>';
@@ -44,6 +52,9 @@ var html = '', link_url = '#none';
         link_url = '//{{ app_to_env_url($data[0]['LinkUrl']) }}/event/popupRegistCreateByBanner?banner_idx=' + {{ $data[0]['BIdx'] }};
         html = '<a href="#none" onclick="event_layer_popup(link_url)" class="{{ $css_class }}"><img src="{{ $data[0]['BannerFullPath'] . $data[0]['BannerImgName'] }}" title="{{ $data[0]['BannerName'] }}"/></a>';
         html += '<div id="APPLYPASS" class="willbes-Layer-Black"></div>';
+    @elseif($data[0]['LinkType'] == 'script')
+        link_url = '{{ $data[0]['LinkUrl'] }}';
+        html = '<a href="' + link_url + '" class="{{ $css_class }}"><img src="{{ $data[0]['BannerFullPath'] . $data[0]['BannerImgName'] }}" title="{{ $data[0]['BannerName'] }}"/></a>';
     @else
         @if(empty($data[0]['LinkUrl']) === false && $data[0]['LinkUrl'] != '#')
             link_url = '{{ front_app_url('/banner/click?banner_idx=' . $data[0]['BIdx'] . '&return_url=' . urlencode($data[0]['LinkUrl']) . '&link_url_type=' . $data[0]['LinkUrlType'], 'www') }}';

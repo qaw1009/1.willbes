7d203462ef7b1fae447d12cd1877ae4148006ce1
김성일 <jxr880210@willbes.com>
2021-02-02 15:40:10 +0900

    [프론트] > 프로모션 썸네일 회원명 수정

--- views/willbes/pc/promotion/register_list_thumbnail_ajax.blade.php
+++ views/willbes/pc/promotion/register_list_thumbnail_ajax.blade.php
@@ -77,7 +77,7 @@
                                     {{ $row['UserName'] }}
                                     <a href="#none" onclick="delRegister('{{ $row['EmIdx'] }}')">X</a>
                                 @else
-                                    {{ str_replace(mb_substr($row['UserName'],1,1,'UTF-8'), "*", $row['UserName']) }}
+                                    {{ str_replace(mb_substr($row['UserName'],1,1,'UTF-8'), "*", mb_substr($row['UserName'],0,4,'UTF-8')) }}
                                 @endif
                             </div>
                             <div class="imgBox">

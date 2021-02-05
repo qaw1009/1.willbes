7f136e949a68b82fedaa443fd997dd09598eb0c0
김창용 <winmir@willbes.com>
2021-02-05 12:01:42 +0900

    공무원학원 소방직 프로모션

--- views/willbes/m/promotion/2004/1780.blade.php
+++ views/willbes/m/promotion/2004/1780.blade.php
@@ -7,10 +7,9 @@
     .evtCtnsBox img {width:100%; max-width:720px;}  
 
     .evt02 a {position: absolute; left: 8.75%; top: 84.07%; width: 83.47%; height: 8.06%; z-index: 2;}
-    .evt04 a {position: absolute; left: 21.39%; width: 56.11%; height: 5.81%; z-index: 2;}
-    .evt04 a.a01 { top: 26.05%;}
-    .evt04 a.a02 { top: 55.46%;}
-    .evt04 a.a03 { top: 85.26%;}
+
+    .evt04 a {position: absolute; left: 22.5%; top: 40.09%; width: 54.86%; height: 8.28%; z-index: 2;}
+    .evt04 a.a02 { top: 84.97%;}
 
     /* 폰 가로, 태블릿 세로*/
     @@media only all and (min-width: 408px)  {
@@ -28,19 +27,18 @@
     </div> 
    
     <div class="evtCtnsBox evt02">
-        <img src="https://static.willbes.net/public/images/promotion/2020/12/1780m_02.jpg" alt="라이브모드">
+        <img src="https://static.willbes.net/public/images/promotion/2021/02/1780m_02.jpg" alt="라이브모드">
         <a href="#evt04" title="소방직 라이브 모드"></a>
     </div>
 
     <div class="evtCtnsBox evt03" >
-        <img src="https://static.willbes.net/public/images/promotion/2020/12/1780m_03.jpg" alt="소방직">
+        <img src="https://static.willbes.net/public/images/promotion/2021/02/1780m_03.jpg" alt="소방직">
     </div>
 
     <div class="evtCtnsBox evt04" id="evt04">
-        <img src="https://static.willbes.net/public/images/promotion/2020/12/1780m_04.jpg" alt="소방직">
+        <img src="https://static.willbes.net/public/images/promotion/2021/02/1780m_04.jpg" alt="소방직">
         <a href="https://pass.willbes.net/m/pass/offPackage/index?cate_code=3050&campus_ccd=605001" title="진도별 문풀 패스" class="a01"></a>
         <a href="https://pass.willbes.net/m/pass/offPackage/index?cate_code=3050&campus_ccd=605001&search_text=UHJvZE5hbWU665287J2067iM" title="라이브 진도별 문풀 패스" class="a02"></a>
-        <a href="https://pass.willbes.net/m/pass/offPackage/index?cate_code=3050&campus_ccd=605001&search_text=UHJvZE5hbWU666y47ZKA" title="3개월 마무리 패스" class="a03"></a>
     </div>
 </div>
 <!-- End Container -->

163ee538e1c89dfabd306a0fbe2ac516ddd095f4
김창용 <winmir@willbes.com>
2021-02-17 10:58:24 +0900

    모바일 통합 게이트

--- views/willbes/m/main.blade.php
+++ views/willbes/m/main.blade.php
@@ -8,18 +8,19 @@
                 <div>
                     <ul class="bigType">
                         <li><a href="{{ front_app_url('/home/index', 'police') }}" target="_blank">신광은경찰</a></li>
-                        <li><a href="{{ front_app_url('/home/index/cate/3094', 'gosi') }}" target="_blank">5급행정</a></li>
                         <li><a href="{{ front_app_url('/home/index', 'pass') }}" target="_blank">공무원</a></li>
+                        <li><a href="{{ front_app_url('/home/index', 'ssam') }}" target="_blank">교원임용 <span>N</span></a></li> 
+                        <li><a href="{{ front_app_url('/home/index/cate/3094', 'gosi') }}" target="_blank">5급행정</a></li>                        
                         <li><a href="{{ front_app_url('/home/index/cate/3095', 'gosi') }}" target="_blank">국립외교원</a></li>
                         <li><a href="{{ front_app_url('/home/index/cate/3035', 'pass') }}" target="_blank">법원직</a></li>
-                        <li><a href="{{ front_app_url('/home/index/cate/3096', 'gosi') }}" target="_blank">5급PSAT</a></li>
-                        <li><a href="{{ front_app_url('/home/index', 'ssam') }}" target="_blank">교원임용 <span>N</span></a></li>
-                        <li><a href="{{ front_app_url('/home/index/cate/309002', 'job') }}" target="_blank">공인노무사</a></li>   
-                        <li><a href="{{ front_app_url('/home/index/cate/3097', 'gosi') }}" target="_blank">5급헌법</a></li>                    
-                        <li><a href="{{ front_app_url('/home/index/cate/309003', 'job') }}" target="_blank">감정평가사</a></li>   
-                        <li><a href="{{ front_app_url('/home/index/cate/3098', 'gosi') }}" target="_blank">법원행시</a></li>                     
-                        <li><a href="{{ front_app_url('/home/index/cate/309004', 'job') }}" target="_blank">변리사</a></li>
+                        <li><a href="{{ front_app_url('/home/index/cate/3096', 'gosi') }}" target="_blank">5급PSAT</a></li>                    
+                        <li><a href="{{ front_app_url('/home/index/cate/3097', 'gosi') }}" target="_blank">5급헌법</a></li> 
+                        <li><a href="{{ front_app_url('/home/index/cate/3103', 'pass') }}" target="_blank">7급PSAT</a></li> 
+                        <li><a href="{{ front_app_url('/home/index/cate/3098', 'gosi') }}" target="_blank">법원행시</a></li> 
                         <li><a href="{{ front_app_url('/home/index/cate/3099', 'gosi') }}" target="_blank">변호사시험</a></li>
+                        <li><a href="{{ front_app_url('/home/index/cate/309002', 'job') }}" target="_blank">공인노무사</a></li>                  
+                        <li><a href="{{ front_app_url('/home/index/cate/309003', 'job') }}" target="_blank">감정평가사</a></li>  
+                        <li><a href="{{ front_app_url('/home/index/cate/309004', 'job') }}" target="_blank">변리사</a></li>
                         <li><a href="{{ front_app_url('/home/index', 'job') }}" target="_blank">세무사</a></li>
                         <li><a href="{{ front_app_url('/home/index/cate/3100', 'spo') }}" target="_blank">경찰간부·일반경찰</a></li>
                         <li><a href="{{ front_app_url('/home/index', 'job') }}" target="_blank">관세사</a></li>
@@ -27,7 +28,7 @@
                         <li><a href="{{ front_app_url('/home/index', 'lang') }}" target="_blank">어학</a></li>              
                         <li><a href="{{ front_app_url('/home/index', 'njob') }}" target="_blank">N잡/e창업 e-커머스</a></li>
                         <li><a href="{{ front_app_url('/home/index', 'willbesedu') }}" target="_blank">인천학원</a></li>
-                        <li class="full"><a href="{{ front_app_url('/home/index', 'book') }}" target="_blank">온라인서점</a></li>
+                        <li><a href="{{ front_app_url('/home/index', 'book') }}" target="_blank">온라인서점</a></li>
                     </ul>
                     <div class="etc">
                         <a href="#none" class="btnMainToggle">기타자격증 <span>+</span></a>

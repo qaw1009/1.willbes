ccf075ee4d2a78b27fc8481098bc8888cb689651
김창용 <winmir@willbes.com>
2021-02-17 15:09:45 +0900

    임용 모바일 수정사항

--- views/html/m/guide_3134_03_inc.blade.php
+++ views/html/m/guide_3134_03_inc.blade.php
@@ -2,7 +2,7 @@
             <table>
                 <tbody>
                     <tr>
-                        <th rowspan="18">2020<BR>
+                        <th rowspan="18">2021<BR>
                         학년도 </th>
                         <th>지역</th>
                         <th>유아 · 초등</th>
@@ -10,88 +10,88 @@
                     </tr>
                     <tr>
                         <td>서울특별시</td>
-                        <td><a href="#none" class="btn01">자료받기 ></a></td>
-                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                        <td><a href="#none" class="btn01">다운하기</a></td>
+                        <td><a href="#none" class="btn01">다운하기</a></td>
                     </tr>
                     <tr>
                         <td>인천광역시</td>
-                        <td><a href="#none" class="btn01">자료받기 ></a></td>
-                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                        <td><a href="#none" class="btn01">다운하기</a></td>
+                        <td><a href="#none" class="btn01">다운하기</a></td>
                     </tr>
                     <tr>
                         <td>대전광역시</td>
-                        <td><a href="#none" class="btn01">자료받기 ></a></td>
-                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                        <td><a href="#none" class="btn01">다운하기</a></td>
+                        <td><a href="#none" class="btn01">다운하기</a></td>
                     </tr>
                     <tr>
                         <td>세종시</td>
-                        <td><a href="#none" class="btn01">자료받기 ></a></td>
-                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                        <td><a href="#none" class="btn01">다운하기</a></td>
+                        <td><a href="#none" class="btn01">다운하기</a></td>
                     </tr>
                     <tr>
                         <td>광주광역시</td>
-                        <td><a href="#none" class="btn01">자료받기 ></a></td>
-                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                        <td><a href="#none" class="btn01">다운하기</a></td>
+                        <td><a href="#none" class="btn01">다운하기</a></td>
                     </tr>
                     <tr>
                         <td>대구광역시</td>
-                        <td><a href="#none" class="btn01">자료받기 ></a></td>
-                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                        <td><a href="#none" class="btn01">다운하기</a></td>
+                        <td><a href="#none" class="btn01">다운하기</a></td>
                     </tr>
                     <tr>
                         <td>울산광역시</td>
-                        <td><a href="#none" class="btn01">자료받기 ></a></td>
-                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                        <td><a href="#none" class="btn01">다운하기</a></td>
+                        <td><a href="#none" class="btn01">다운하기</a></td>
                     </tr>
                     <tr>
                         <td>부산광역시</td>
-                        <td><a href="#none" class="btn01">자료받기 ></a></td>
-                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                        <td><a href="#none" class="btn01">다운하기</a></td>
+                        <td><a href="#none" class="btn01">다운하기</a></td>
                     </tr>
                     <tr>
                       <td>경기도</td>
-                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
-                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                      <td><a href="#none" class="btn01">다운하기</a></td>
+                      <td><a href="#none" class="btn01">다운하기</a></td>
                     </tr>
                     <tr>
                       <td>충청북도</td>
-                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
-                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                      <td><a href="#none" class="btn01">다운하기</a></td>
+                      <td><a href="#none" class="btn01">다운하기</a></td>
                     </tr>
                     <tr>
                       <td>충청남도</td>
-                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
-                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                      <td><a href="#none" class="btn01">다운하기</a></td>
+                      <td><a href="#none" class="btn01">다운하기</a></td>
                     </tr>
                     <tr>
                       <td>전라북도</td>
-                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
-                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                      <td><a href="#none" class="btn01">다운하기</a></td>
+                      <td><a href="#none" class="btn01">다운하기</a></td>
                     </tr>
                     <tr>
                       <td>전라남도</td>
-                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
-                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                      <td><a href="#none" class="btn01">다운하기</a></td>
+                      <td><a href="#none" class="btn01">다운하기</a></td>
                     </tr>
                     <tr>
                       <td>강원도</td>
-                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
-                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                      <td><a href="#none" class="btn01">다운하기</a></td>
+                      <td><a href="#none" class="btn01">다운하기</a></td>
                     </tr>
                     <tr>
                       <td>경상북도</td>
-                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
-                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                      <td><a href="#none" class="btn01">다운하기</a></td>
+                      <td><a href="#none" class="btn01">다운하기</a></td>
                     </tr>
                     <tr>
                       <td>경상남도</td>
-                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
-                      <td><a href="#none" class="btn01">자료받기 &gt;</a></td>
+                      <td><a href="#none" class="btn01">다운하기</a></td>
+                      <td><a href="#none" class="btn01">다운하기</a></td>
                     </tr>
                     <tr>
                         <td>제주도</td>
-                        <td><a href="#none" class="btn01">자료받기 ></a></td>
-                        <td><a href="#none" class="btn01">자료받기 ></a></td>
+                        <td><a href="#none" class="btn01">다운하기</a></td>
+                        <td><a href="#none" class="btn01">다운하기</a></td>
                     </tr>
                 </tbody>
             </table>
--- views/html/m/guide_3134_06.blade.php
+++ views/html/m/guide_3134_06.blade.php
@@ -10,16 +10,13 @@
         임용 교수채용
     </div>
 
-    <div class="w-Guide-Ssam">
-        <div class="request_con NSK">
-            <p class="txt01">RECRUITMENT</p>
-            <p class="txt03">교육에 대한 열정을 가지고 계신 교수님을</p>
-            <p class="txt02 NSK-Black">윌비스 임용고시 학원에서 모시고자 합니다<i>!</i></p>
-            <img src="https://static.willbes.net/public/images/promotion/main/2018/professor_img00.png" alt="">
-            <p class="txt04">교원임용시험은 교사에 대한 처우개선 및 직업의 <br>안정성으로 인하여 가장 선호되고 있는 시험 중 하나이며,<br>
-            임용고시는 순위고사라는 우선 임용제 방식에서 <br>91년도부터 공개경쟁채용시험으로 변경되어 선발하고 있습니다.</p>
+    <div class="w-Guide-Ssam pd-zero">
+        <div class="request_con bg01 NSK">
+            <p class="txt03">윌비스 임용고시 학원은</p>
+            <p class="txt02 NSK-Black">교육에 대한 열정을 바탕으로 명 강의를<br> 진행해주실 교수님을 모시고 있습니다.</p>
+            <img src="https://static.willbes.net/public/images/promotion/m/2017/professor_img00.png" alt="">
+            <p class="txt04">윌비스와 함께 큰 뜻을 이뤄나갈 준비가 되셨다면,<br>아래 연락처로 연락 주시면 감사하겠습니다.</p>
             <div class="request_txtWrap">
-                <p class="txt01">“ 윌비스와 함께 큰 뜻을 이뤄나갈 준비가 되셨다면,<br>아래 연락처로 연락 주시면 감사하겠습니다.”</p>
                 <p class="txt02"><span>과목 :</span> 임용시험 전과목</p>
                 <p class="txt02"><span>담당자 :</span> 교원임용 본부장 최창식 ( 010-7167-2329 / genie001@willbes.com )</p>
                 <p class="txt02"><span>학원문의 :</span> 1544-3169</p>

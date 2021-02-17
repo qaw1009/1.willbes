fa9cef32eb1f59507155a17d9dd8ae8e9bdb01c9
김성일 <jxr880210@willbes.com>
2021-02-17 14:12:39 +0900

    [프론트] > 프로모션 2052 팝업 이미지 css 수정

--- views/willbes/pc/promotion/register_thumbnail_popup.blade.php
+++ views/willbes/pc/promotion/register_thumbnail_popup.blade.php
@@ -1,5 +1,5 @@
 @extends('willbes.pc.layouts.master_popup')
 
 @section('content')
-    <img src="{{ (empty($data[0]['FileFullPath']) === false ? $data[0]['FileFullPath'] : '') }}">
+    <img src="{{ (empty($data[0]['FileFullPath']) === false ? $data[0]['FileFullPath'] : '') }}" style="max-width: 100%">
 @stop

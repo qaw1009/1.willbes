ec8af724a97ca8d6bc1658b15facae00f068f607
이윤구 <yoongukk@willbes.com>
2021-02-18 17:04:56 +0900

    고등고시 특강 페이지 타이머 추가

--- views/willbes/pc/promotion/2005/2084.blade.php
+++ views/willbes/pc/promotion/2005/2084.blade.php
@@ -20,6 +20,28 @@
 
         /************************************************************/ 
 
+        /*타이머*/
+        .timeBox {background:#333}
+        .time {width:920px; margin:0 auto; text-align:center;}
+        .time {text-align:center; padding:20px 0}
+        .time ul {width:100%;}
+        .time ul:after {content:''; display:block; clear:both}
+        .time li {display:inline; float:left; line-height:61px; font-size:24px; margin-right:10px;}
+        .time li:first-child {margin-left:80px}
+        .time li img {width:44px}
+        .time .time_txt {color:#fff; letter-spacing:-1px}
+        .time .time_txt span {color:#d63e4d; animation:upDown 2s infinite;-webkit-animation:upDown 2s infinite;}
+        @@keyframes upDown{
+        from{color:#d63e4d}
+        50%{color:#ff6600}
+        to{color:#d63e4d}
+        }
+        @@-webkit-keyframes upDown{
+        from{color:#d63e4d}
+        50%{color:#ff6600}
+        to{color:#d63e4d}
+        }          
+
         .evt_top {background:url(https://static.willbes.net/public/images/promotion/2021/02/2084_top_bg.jpg) no-repeat center top;}	
 
         .evt_01 {background:#b5e5fc;}
@@ -37,6 +59,27 @@
         /************************************************************/      
     </style> 
 	<div class="evtContent NGR">
+
+        <div class="evtCtnsBox timeBox">
+            <div class="time NGEB" id="newTopDday">
+                <ul>
+                    <li class="time_txt"><span>{{ kw_date('n/j(%)', $arr_promotion_params['edate']) }}</span> 마감!</li>
+                    <li class="time_txt"><span>남은 시간은</span></li>
+                    <li><img id="dd1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="dd2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li class="time_txt">일</li>
+                    <li><img id="hh1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="hh2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li class="time_txt">:</li>
+                    <li><img id="mm1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="mm2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li class="time_txt">:</li>
+                    <li><img id="ss1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="ss2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>             
+                </ul>
+            </div>
+        </div>
+
 		<div class="evtCtnsBox evt_top">
             <img src="https://static.willbes.net/public/images/promotion/2021/02/2084_top.jpg" alt="변호사 시험 대비" />
 		</div>
@@ -73,5 +116,15 @@
 		</div> 
 
 	</div>
-    <!-- End Container -->
+     <!-- End Container -->
+
+     <script>  
+        /*디데이카운트다운*/
+        $(document).ready(function() {
+            dDayCountDown('{{$arr_promotion_params['edate']}}');
+        });   
+    </script>
+
+    {{-- 프로모션용 스크립트 include --}}
+    @include('willbes.pc.promotion.promotion_script')
 @stop

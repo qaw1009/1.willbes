aabe8c714eb0eb562e0cc49b5e68543042a62c19
김성일 <jxr880210@willbes.com>
2021-02-22 16:34:13 +0900

    [프론트] > 임용 모바일 메인 수정

--- views/willbes/m/site/examinfo/ajax_notice.blade.php
+++ views/willbes/m/site/examinfo/ajax_notice.blade.php
@@ -11,8 +11,8 @@
         @foreach($arr_download_list as $area => $row)
             <tr>
                 <td>{{$area}}</td>
-                <td><a href="{{ site_url("/examInfo/download?path=" . urlencode('/public/uploads/willbes/site/2018/' . $row['infant_file_name']) . "&fname=" . urlencode($row['infant_file_name_kr'])) }}" class="btn01">자료받기 ></a></td>
-                <td><a href="{{ site_url("/examInfo/download?path=" . urlencode('/public/uploads/willbes/site/2018/' . $row['secondary_file_name']) . "&fname=" . urlencode($row['secondary_file_name_kr'])) }}" class="btn01">자료받기 ></a></td>
+                <td><a href="{{ site_url("/examInfo/download?path=" . urlencode('/public/uploads/willbes/site/2018/' . $row['infant_file_name']) . "&fname=" . urlencode($row['infant_file_name_kr'])) }}" class="btn01">다운하기</a></td>
+                <td><a href="{{ site_url("/examInfo/download?path=" . urlencode('/public/uploads/willbes/site/2018/' . $row['secondary_file_name']) . "&fname=" . urlencode($row['secondary_file_name_kr'])) }}" class="btn01">다운하기</a></td>
             </tr>
         @endforeach
         </tbody>

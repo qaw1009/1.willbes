86a55b1cbac09f41e25e8d94ec7f9010bbb15477
김창용 <winmir@willbes.com>
2021-02-23 17:19:31 +0900

    경찰 합격예측 풀서비스

--- views/willbes/pc/promotion/2001/2069_cts02.blade.php
+++ views/willbes/pc/promotion/2001/2069_cts02.blade.php
@@ -11,21 +11,21 @@
     <div class="evtCtnsBox cts02_top">    
         <div>         
             <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts02_top.jpg" alt="합격예측 풀서비스">
-            <a href="police.willbes.net/promotion/index/cate/3001/code/1775" target="_blank" style="position: absolute; left: 25.89%; top: 84.07%; width: 48.13%; height: 6.3%; z-index: 2;"></a>
+            <a href="police.willbes.net/promotion/index/cate/3001/code/2080" target="_blank" style="position: absolute; left: 25.89%; top: 84.07%; width: 48.13%; height: 6.3%; z-index: 2;"></a>
         </div>
     </div>
     <div class="evtCtnsBox cts02_02" id="cts02_02">
         <div>
             <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts02_02.jpg" alt="라이브 토크쇼">
-            <a href="#none" onclick="javascript:alert('라이브 토크쇼가 종료되었습니다');" style="position: absolute; left: 30.54%; top: 85.61%; width: 38.13%; height: 4.02%; z-index: 2;"></a>
-            {{--<a href="https://www.youtube.com/channel/UCQ-jvqaobw6E9EvnFO88vwQ" target="_blank" style="position: absolute; left: 30.54%; top: 85.61%; width: 38.13%; height: 4.02%; z-index: 2;"></a>--}}
+            {{--<a href="#none" onclick="javascript:alert('라이브 토크쇼가 종료되었습니다');" style="position: absolute; left: 30.54%; top: 85.61%; width: 38.13%; height: 4.02%; z-index: 2;"></a>--}}
+            <a href="https://www.youtube.com/channel/UCQ-jvqaobw6E9EvnFO88vwQ" target="_blank" style="position: absolute; left: 30.54%; top: 85.61%; width: 38.13%; height: 4.02%; z-index: 2;"></a>
         </div>
     </div>
 
     <div class="evtCtnsBox cts02_03">
         <div>
             <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts02_03.jpg" alt="신광은 경찰팀">
-            <a href="https://police.willbes.net/promotion/index/cate/3002/code/1766" target="_blank" style="position: absolute; left: 32.05%; top: 83.47%; width: 35.63%; height: 5.78%; z-index: 2;"></a>
+            <a href="https://police.willbes.net/promotion/index/cate/3001/code/2070" target="_blank" style="position: absolute; left: 32.05%; top: 83.47%; width: 35.63%; height: 5.78%; z-index: 2;"></a>
         </div>
     </div>
 </div>

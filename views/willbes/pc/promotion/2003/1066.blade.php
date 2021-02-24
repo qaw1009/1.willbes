2768a00e95e0fc9d05546b7fa393aea31cde328a
김창용 <winmir@willbes.com>
2021-02-24 15:43:33 +0900

    공무원 한덕현 개강 프로모션

--- views/willbes/pc/promotion/2003/1066.blade.php
+++ views/willbes/pc/promotion/2003/1066.blade.php
@@ -43,7 +43,7 @@
             <a href="#live">
                 <img src="https://static.willbes.net/public/images/promotion/2021/02/1066_sky2.png" title="라이브로 소통하기" >
             </a>    
-            <a href="#live">
+            <a href="#Map1066_apply">
                 <img src="https://static.willbes.net/public/images/promotion/2021/02/1066_sky3.png" title="고득점 실전패키지" >
             </a>    
         </div>
@@ -62,7 +62,7 @@
         <div class="evtCtnsBox wb_cts02">
             <img src="https://static.willbes.net/public/images/promotion/2020/12/1066_01.jpg" usemap="#Map1066a" title="반반한 모의고사 무료방송" border="0" />
             <map name="Map1066a" id="Map1066a">
-                <area shape="rect" coords="238,1051,883,1158" href="https://pass.willbes.net/promotion/index/cate/3019/code/1676" target="_blnak" alt="방송보러가기" onfocus='this.blur()' />
+                <area shape="rect" coords="238,1051,883,1158" href="https://pass.willbes.net/promotion/index/cate/3019/code/1676" target="_blnak" alt="방송보러가기" />
             </map>
         </div>
 
@@ -99,43 +99,29 @@
         <div class="evtCtnsBox wb_cts05" id="cts05">
             <img src="https://static.willbes.net/public/images/promotion/2021/02/1066_04.gif" usemap="#Map1066b" border="0" alt="커리큘럼"/>
             <map name="Map1066b" id="Map1066b">
-                <area shape="rect" coords="230,694,277,711" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/145823" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="230,752,278,769" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/173562" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="229,828,278,845" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/146100" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="426,456,472,474" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/145623" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="426,572,472,590" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/154900" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="424,648,473,666" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/173563" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="424,735,473,754" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/157579" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="625,520,672,536" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/157581" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="625,692,674,710" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/145826" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="626,826,671,844" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/146969" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="829,468,878,484" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/158678" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="830,825,879,842" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/158682" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="830,846,878,863" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/158683" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="230,493,278,511" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/169567" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="230,534,279,551" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/169816" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="424,512,472,531" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/169820" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="829,559,879,577" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/177803" target="_blank" alt="새벽모의고사" onfocus='this.blur()' />
-                <area shape="rect" coords="230,635,278,653" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/169815" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="831,677,877,695" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/179627" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="832,711,877,729" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/177860" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="832,868,878,886" href="https://pass.willbes.net/lecture/show/cate/3020/pattern/only/prod-code/169814" target="_blank" onfocus='this.blur()' />
+                <area shape="rect" coords="230,694,277,711" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/145823" target="_blank" />
+                <area shape="rect" coords="230,752,278,769" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/173562" target="_blank" />
+                <area shape="rect" coords="229,828,278,845" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/146100" target="_blank" />
+                <area shape="rect" coords="426,456,472,474" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/145623" target="_blank" />
+                <area shape="rect" coords="426,572,472,590" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/154900" target="_blank" />
+                <area shape="rect" coords="424,648,473,666" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/173563" target="_blank" />
+                <area shape="rect" coords="424,735,473,754" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/157579" target="_blank" />
+                <area shape="rect" coords="625,520,672,536" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/157581" target="_blank" />
+                <area shape="rect" coords="625,692,674,710" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/145826" target="_blank" />
+                <area shape="rect" coords="626,826,671,844" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/146969" target="_blank" />
+                <area shape="rect" coords="829,468,878,484" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/158678" target="_blank" />
+                <area shape="rect" coords="830,825,879,842" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/158682" target="_blank" />
+                <area shape="rect" coords="830,846,878,863" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/158683" target="_blank" />
+                <area shape="rect" coords="230,493,278,511" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/169567" target="_blank" />
+                <area shape="rect" coords="230,534,279,551" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/169816" target="_blank" />
+                <area shape="rect" coords="424,512,472,531" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/169820" target="_blank" />
+                <area shape="rect" coords="829,559,879,577" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/177803" target="_blank" alt="새벽모의고사" />
+                <area shape="rect" coords="230,635,278,653" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/169815" target="_blank" />
+                <area shape="rect" coords="831,677,877,695" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/179627" target="_blank" />
+                <area shape="rect" coords="832,711,877,729" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/177860" target="_blank" />
+                <area shape="rect" coords="832,868,878,886" href="https://pass.willbes.net/lecture/show/cate/3020/pattern/only/prod-code/169814" target="_blank" />
             </map >        
         </div>
-
-        {{--
-        <div class="evtCtnsBox wb_cts06" >
-            <img src="https://static.willbes.net/public/images/promotion/2020/05/1066_05.png" usemap="#Map1066c" border="0" />
-            <map name="Map1066c" id="Map1066c">
-                <area shape="rect" coords="64,702,223,734" href="https://pass.willbes.net/package/show/cate/3019/pack/648001/prod-code/150364" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="273,700,432,736" href="https://pass.willbes.net/package/show/cate/3019/pack/648001/prod-code/150363" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="484,702,643,735" href="https://pass.willbes.net/package/show/cate/3019/pack/648001/prod-code/150362" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="690,702,853,735" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/163562" target="_blank" onfocus='this.blur()' />
-                <area shape="rect" coords="899,701,1067,736" href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/158684" target="_blank" onfocus='this.blur()' />
-            </map>
-        </div>
-        --}}
-
     </div>
     <!-- End Container -->
 @stop

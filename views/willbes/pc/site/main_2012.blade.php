88ca39b658d74655869869596b12360053a4816c
김창용 <winmir@willbes.com>
2021-03-04 15:18:29 +0900

    윌스토리 메인 문구 수정

--- views/willbes/pc/site/main_2012.blade.php
+++ views/willbes/pc/site/main_2012.blade.php
@@ -88,12 +88,12 @@
                 <div id="tab03" class="bookContent">
                     <div class="booktitle">
                         <span><img src="{{ img_static_url('promotion/main/2012_main_img03.png') }}" alt="예약판매"></span>
-                        <div class="NGEB">예약 판매</div>
+                        <div class="NGEB">출간 예정</div>
                         <p>곧 나올 책들을 윌스토리에서<br>먼저 만나보세요!</p>
                     </div>
                     <div class="bookList">
                         <ul>
-                            {{-- 예약판매 --}}
+                            {{-- 출간 예정 --}}
                             @foreach($data['resv_product'] as $row)
                                 <li>
                                     <div class="bookImg">

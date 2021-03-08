53a7c4abac2253be499a7063271ee1ffce701552
김창용 <winmir@willbes.com>
2021-03-08 15:03:14 +0900

    고등고시 동영상 티패스 프로모션

--- views/willbes/pc/promotion/2005/2106.blade.php
+++ views/willbes/pc/promotion/2005/2106.blade.php
@@ -49,13 +49,14 @@
         .evt_02 div {font-size:50px; color:#000; margin-top:100px}
         .evt_02 p {font-size:40px; color:#003dcc; margin:15px 0 50px}
         
-        .evtInfo {padding:80px 0; background:#333; color:#fff; font-size:14px; margin-top:100px}
+        .evtInfo {padding:80px 0; background:#666; color:#fff; font-size:14px; margin-top:100px}
 		.evtInfoBox { width:1000px; margin:0 auto; text-align:left; line-height:1.4}
 		.evtInfoBox h4 {font-size:30px; margin-bottom:40px}
 		.evtInfoBox .infoTit {font-size:18px; margin-bottom:20px}
 		.evtInfoBox .infoTit strong {padding:8px 20px; background:#000; border-radius:20px; font-weight:normal !important}
 		.evtInfoBox ul {margin-bottom:30px}
         .evtInfoBox li {margin-bottom:8px; list-style:disc; margin-left:20px}
+        .evtInfoBox li span {color:#6ffede}
         .evtInfoBox span {vertical-align:bottom}  
  
         /************************************************************/      
@@ -109,37 +110,56 @@
         
         <div class="evtCtnsBox evtInfo">
             <div class="evtInfoBox">
-                <h4 class="NSK-Black">2021 예비순환 상품 이용안내</h4>
-                <div class="infoTit"><strong>상품구성</strong></div>
+                <h4 class="NSK-Black">윌비스 한림법학원 동영상 T-PASS반 상품이용안내</h4>
+                <div class="infoTit"><strong>이용안내</strong></div>
                 <ul>
-                    <li>2021년도 5급 행정/국립외교원 예비순환 강의 중 이벤트기간 2과목이상 강의신청시 특별혜택이 적용됩니다.(교재별도)<br>
-                    - 2과목 동시 수강신청시 : 수강료 15% 할인+수강기간 20일 연장<br>
-                    - 3과목이상 동시 수강신청시 : 수강료 25% 할인+수강기간 20일 연장</li>
-                    <li>본 이벤트는 ~3/21(일)까지 이벤트페이지에서 수강신청 및 결제완료시 적용됩니다.</li>
-                    <li>수강시작일 설정관련(필독)<br>
-                    - 개강예정인 과목들의 수강시작일은 해당 과목의 동영상 개강일에 자동시작이 됩니다.<br>
-                    수강시작일을 변경 원하시는 분은 동영상게시판에 원하시는 시작일을 적어주시면 변경해드리겠습니다.(신청한 강의 동영상업로드일 이후 30일 범위내)</li>
+                    <li>본 상품은 2021년 3월에서 2022년 6월까지 진행이 되는 신규 예비순환 ~ GS3순환 동영상 과정입니다.</li>
+                    <li>이벤트 내용 <br>
+                    [PASS 상품 및 해당과정 수강기간]<br>
+                    - 예비순환 + GS1순환 필수과목 PASS반<br> : 수강기간 210일<br>
+                    - 예비순환 + GS1순환 + GS2순환 필수과목 PASS반 : 수강기간 365일<br>
+                    - 예비순환 + GS1순환 + GS3순환 필수과목 PASS반 : 수강기간 450일<br>
+                    <br>
+                    [할인율 및 신청방법 : 등록된 PASS 상품 중에서 자유선택]<br>
+                    - 2개 PASS 과정 신청 : 10%할인 + 동영상강의 15%할인쿠폰<br>
+                    - 3개 PASS 과정 신청 : 20%할인 + 동영상강의 15%할인쿠폰<br>
+                    - 4개 PASS 과정 신청 : 30%할인 + 동영상강의 15%할인쿠폰<br>
+                    <br>
+                    <span>[수강기간-과정별 수강기간 자동 적용] : 강의신청시 다음날부터 바로 수강시작이 되며 일시정지, 강의연장이 적용되지 않습니다.</span></li>
+                    <li>강의배수 제한 : 동영상강의는 1.5배수제한 규정이 적용됩니다.</li>
+                    <li>강의진행 월 또는 회차는 학원 사정 등에 따라 변동될 수 있습니다.</li>
+                    <li>동영상 PASS반은 3월 18일까지 신청하실 수 있으며, 사정에 의해서 신청기간이 변경될 수 있습니다.</li>
+                    <li>동영상강의 할인쿠폰(1아이디당 2개발행) : 15%할인쿠폰은 3월 19일 일괄발행됩니다.(5급, 외교원, PSAT, 5급헌법 동영상강의에 적용, 유효기간 : 2021년 12월 31일까지)</li>
                 </ul>
-                <div class="infoTit"><strong>수강관련</strong></div>
+                <div class="infoTit"><strong>교재</strong></div>
                 <ul>
-                    <li>신청하신 강의는 컴퓨터, 스마트기기(willbes.net/m)를 이용하여 수강할 수 있습니다.</li>
-                    <li>동영상 강의는 배수 수강제한 규정이 있습니다.</li>
+                    <li>각 강의수강에 필요한 교재는 별도로 구매하셔야 하며, 각 강좌별 교재는 해당 강의 개강일에 등록이 될 예정입니다.</li>
                 </ul>
-                <div class="infoTit"><strong>교재관련</strong></div>
+                <div class="infoTit"><strong>환불</strong></div>
                 <ul>
-                    <li>강의교재는 별도로 주문하셔야 합니다.</li>
-                    <li>각 강의별 교재는 동영상 강의개강 후 『내 강의실 바로가기』 → 강의보기를 클릭하셔도 주문하실 수 있습니다.</li>
+                    <li>결제 후 7일 이내 강좌의 맛보기 강의를 제외하고 2강 이하 수강 시에만 전액 환불 가능합니다.(일부강의만의 환불은 불가합니다.)</li>
+                    <li>결제 후 7일 이내 환불 요청 시 수강한 각 강의 정가 기준으로 수강 부분만큼 차감 후 나머지 부분에 대해 환불이 진행됩니다.</li>
+                    <li>강좌 내 학습 자료 및 모바일 다운로드 이용 시에는 수강한 것으로 간주됩니다.</li>
+                    <li>고객 변심으로 인한 환불은 수강시작일 (당일 포함)로부터 7일이 경과되면, PASS반 정가 기준으로 계산하여 사용일수만큼 차감 후 환불됩니다.</li>
+                    <li>기타 약관에 규정에 따릅니다.</li>
                 </ul>
-                <div class="infoTit"><strong>환불관련</strong></div>
+                <div class="infoTit"><strong>PASS 수강</strong></div>
                 <ul>
-                    <li>강의환불 시 원수강료와 수강일수 기준으로 환불이 됩니다.</li>
-                    <li>기타 환불규정은 약관의 규정에 따릅니다.</li>
+                    <li>로그인 후 <span>[내강의실] 에서 [무한PASS존]으로 접속</span>합니다.</li>
+                    <li>구매한 PASS 상품 선택 후 <span>[강좌추가]</span> 를 클릭, 원하시는 강좌를 등록한 후 수강할 수 있습니다.</li>
+                    <li>PASS반은 <span>일시 정지, 수강 연장, 재수강 불가</span>합니다.</li>
+                    <li>PASS반 수강 시 이용 가능한 <span>기기 대수는 다음과 같이 제한</span>됩니다.<br>
+                        [ <span>총 수강 기기 대수 2대</span> : PC 2대 또는 PC 1대 모바일 1대 또는 모바일 2대 ] </li>
+                    <li>PC, 모바일 기기에 대한 초기화가 필요할 경우 내용확인 후 진행이 가능하오니 고객센터로 문의 부탁드립니다.(수강기간동안 3회 신청가능) </li>
+                    <li>일부 강좌의 경우 자료 출력 횟수 제한이 적용될 수 있습니다.</li>
                 </ul>
-                <div class="infoTit"><strong>기타</strong></div>
+                <div class="infoTit"><strong>유의사항</strong></div>
                 <ul>
-                    <li>본 이벤트는 복지할인 등 다른 쿠폰과 중복 적용되지 않습니다.</li>
-                    <li>아이디 공유 및 불법공유 행위 적발 시 회원자격 박탈 및 고발 조치가 진행될 수 있습니다.</li>
+                    <li>본 상품은 특별할인기획 상품으로 쿠폰 할인, 다다익선 할인, 포인트 사용 등 혜택이 적용되지 않으니 양해 및 참고 부탁드립니다.</li>
+                    <li>PASS반 과정 중 불가피한 사정에 의해 부득이 진행되지 않을 경우 대체 강좌로 제공이 될 예정입니다.</li>
+                    <li>아이디 공유, 타인에게 양도 및 판매 등 부정 사용 적발 시 수강 중인 PASS반은 즉시 정지, 회원 자격이 박탈됩니다. 이로 인한 강의 환불은 절대 불가하며, 불법 공유 행위 사안에 따라 민형사상 조치가 발생 할 수 있습니다.</li>
                 </ul>
+                <div class="infoTit">※ 이용 문의 : 윌비스 고객만족센터 1566-4770</div>
             </div>
         </div>
 	</div>

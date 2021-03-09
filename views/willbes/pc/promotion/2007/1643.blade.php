f6288c5cf9af017c2dde76fd710aee38773fbb6d
김창용 <winmir@willbes.com>
2021-03-09 14:42:11 +0900

    강의 dP 프로모션

--- views/willbes/pc/promotion/2007/1643.blade.php
+++ views/willbes/pc/promotion/2007/1643.blade.php
@@ -17,7 +17,7 @@
             background:#fff;
         }
         .evtContent span {vertical-align:auto}
-        .evtCtnsBox {width:100%; text-align:center; min-width:1120px; position:relative;} 
+        .evtCtnsBox {width:100%; text-align:center; min-width:1120px;} 
 
         /************************************************************/
         .evtTop {background:#7795b4 url(https://static.willbes.net/public/images/promotion/2020/05/1643_top_bg.jpg) no-repeat center top;}   

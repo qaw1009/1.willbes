86f65767af305335677d744a59f8f81ab27d01db
김창용 <winmir@willbes.com>
2021-03-17 10:15:46 +0900

    내강의실 >온라인강좌 >수강중강좌 코딩 수정

--- views/html/mypage_online2.blade.php
+++ views/html/mypage_online2.blade.php
@@ -353,7 +353,8 @@
                                                     <span class="NSK ml15 nBox n4">완강</span>
                                                 </dt>
                                             </dl><br/>
-                                            <div class="OTclass"><span>직장인/재학생반</span><span class="red">직강전환</span></div>
+                                            <div class="OTclass"><span>직장인/재학생반</span></div>
+                                            <div class="OTclass"><span class="red">직강전환</span></div>
                                             <div class="w-tit">
                                                 <a href="{{ site_url('/home/html/mypage_pass2') }}">2018 [지방직/서울시] 정채영 국어 필살모고 Ⅲ-Ⅳ 및 국문학 종결자 패키지222</a>
                                             </div>

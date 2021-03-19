adc052ad78a8722a80ad3938da68d5f4aa75827f
김창용 <winmir@willbes.com>
2021-03-19 17:11:21 +0900

    임용 교수홈 탭영역

--- views/willbes/pc/site/professor/show_v2.blade.php
+++ views/willbes/pc/site/professor/show_v2.blade.php
@@ -164,6 +164,7 @@
 
         <!-- willbes-Prof-Tabs -->
         <div class="willbes-Prof-Tabs">
+            <div class="mb10">* 수강신청 시, “온라인강좌”와＂학원강좌”를 먼저 선택하신 후, 강의형태(패키지, 단과 등)를 선택하시면 됩니다. </div>
             <div class="ProfDetailWrap">
                 <a name="tabLink"></a>
                 <ul class="tabWrap tabDepthProf tabDepthProf_{{ count($tab_list) }}">

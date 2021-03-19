05cac11f6a56aada7295a9adee81c05dc597b34e
김창용 <winmir@willbes.com>
2021-03-19 11:21:25 +0900

    임용 교수홈 코딩페이지

--- views/html/profsub_ssam.blade.php
+++ views/html/profsub_ssam.blade.php
@@ -1291,7 +1291,7 @@
                                 <div id="acad1" class="tabContent">
                                     <div class="willbes-Lec NG c_both">
                                         <div class="willbes-Lec-Subject tx-dark-black">
-                                            ㆍ온라인 - 패키지 강의
+                                            ㆍ온라인강좌 - 패키지 강의
                                         </div>
                                         <div class="willbes-Lec-Line">-</div>
                                         <!-- willbes-Lec-Line -->
@@ -1369,7 +1369,7 @@
                                 <div id="acad2" class="tabContent">
                                     <div class="willbes-Lec NG c_both">
                                         <div class="willbes-Lec-Subject tx-dark-black">
-                                            ㆍ온라인 - 단과 강의
+                                            ㆍ온라인강좌 - 단과 강의
                                             <div class="selectBoxForm">
                                                 <span class="MoreBtn"><a href="#none">교재정보 <span>전체보기 ▼</span></a></span>
                                             </div>
@@ -1550,7 +1550,7 @@
                                 <div id="acad3" class="tabContent">
                                     <div class="willbes-Lec NG c_both">
                                         <div class="willbes-Lec-Subject tx-dark-black">
-                                            ㆍ온라인 - 특강
+                                            ㆍ온라인강좌 - 특강
                                             <div class="selectBoxForm">
                                                 <span class="MoreBtn"><a href="#none">교재정보 <span>전체보기 ▼</span></a></span>
                                             </div>
@@ -1731,7 +1731,7 @@
                                 <div id="acad4" class="tabContent">
                                     <div class="willbes-Lec NG c_both">
                                         <div class="willbes-Lec-Subject tx-dark-black">
-                                            ㆍ온라인 - 수강생전용
+                                            ㆍ온라인강좌 - 수강생전용
                                             <div class="selectBoxForm">
                                                 <span class="MoreBtn"><a href="#none">교재정보 <span>전체보기 ▼</span></a></span>
                                             </div>
@@ -1929,7 +1929,7 @@
                                 
                                 <div id="acad5" class="tabContent">
                                     <div class="willbes-Lec NG c_both">
-                                        <div class="willbes-Lec-Subject tx-dark-black">ㆍ학원직강 - 패키지 강의</div>
+                                        <div class="willbes-Lec-Subject tx-dark-black">ㆍ학원강좌 - 패키지 강의</div>
                                         <!-- willbes-Lec-Subject -->
 
                                         <div class="willbes-Lec-Line">-</div>
@@ -2071,7 +2071,7 @@
 
                                 <div id="acad6" class="tabContent">
                                     <div class="willbes-Lec NG c_both">
-                                        <div class="willbes-Lec-Subject tx-dark-black">ㆍ학원직강 - 단과 강의<span class="MoreBtn"><a href="#none">교재정보 <span>전체보기 ▼</span></a></span></div>
+                                        <div class="willbes-Lec-Subject tx-dark-black">ㆍ학원강좌 - 단과 강의<span class="MoreBtn"><a href="#none">교재정보 <span>전체보기 ▼</span></a></span></div>
                                         <!-- willbes-Lec-Subject -->
 
                                         <div class="willbes-Lec-Line">-</div>
@@ -2324,7 +2324,7 @@
 
                                 <div id="acad7" class="tabContent">
                                     <div class="willbes-Lec NG c_both">
-                                        <div class="willbes-Lec-Subject tx-dark-black">ㆍ학원직강 - 특강<span class="MoreBtn"><a href="#none">교재정보 <span>전체보기 ▼</span></a></span></div>
+                                        <div class="willbes-Lec-Subject tx-dark-black">ㆍ학원강좌 - 특강<span class="MoreBtn"><a href="#none">교재정보 <span>전체보기 ▼</span></a></span></div>
                                         <!-- willbes-Lec-Subject -->
 
                                         <div class="willbes-Lec-Line">-</div>
@@ -2577,7 +2577,7 @@
 
                                 <div id="acad8" class="tabContent">
                                     <div class="willbes-Lec NG c_both">
-                                        <div class="willbes-Lec-Subject tx-dark-black">ㆍ학원직강 - 전국 라이브 영상반<span class="MoreBtn"><a href="#none">교재정보 <span>전체보기 ▼</span></a></span></div>
+                                        <div class="willbes-Lec-Subject tx-dark-black">ㆍ학원강좌 - 전국 라이브 영상반<span class="MoreBtn"><a href="#none">교재정보 <span>전체보기 ▼</span></a></span></div>
                                         <!-- willbes-Lec-Subject -->
 
                                         <div class="willbes-Lec-Line">-</div>

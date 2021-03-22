7b6bd374fefe7781f0b2a9db5bb6e4f3c11ff0be
이윤구 <yoongukk@willbes.com>
2021-03-22 11:33:13 +0900

    (경찰) 면접캠프 상세페이지 수정

--- views/willbes/pc/promotion/2002/2088.blade.php
+++ views/willbes/pc/promotion/2002/2088.blade.php
@@ -25,21 +25,7 @@
 
         .evt_top_banner {background: url(https://static.willbes.net/public/images/promotion/2021/03/2088_top_banner_bg.jpg) no-repeat center top;}   
 
-        .evtTop {background: url(https://static.willbes.net/public/images/promotion/2021/03/2088_top_bg.jpg) no-repeat center top;padding-bottom:125px;} 
-        .evtTop .request {width:1000px; margin:0 auto; background:#fff; padding:50px;text-align:left}
-        .evtTop .request h3 {font-size:17px;}
-        .evtTop .request td {padding:10px}
-        .evtTop .request input {height:26px;}
-        .evtTop .requestL {width:48%; float:left}
-        .evtTop .requestR {width:48%; float:right; }
-        .evtTop .requestR ul {margin-top:10px; line-height:1.5; padding:10px; border:1px solid #ccc; height:271px; overflow-y:scroll }
-        .evtTop .requestL li {display:inline-block; margin-right:10px}
-        .evtTop .requestR li {margin-bottom:5px}
-        .evtTop .request:after {content:""; display:block; clear:both}
-        .evtTop .btn {clear:both; width:450px; margin:0 auto;}
-        .evtTop .btn a {display:block; text-align:center; font-size:28px; color:#fff; background:#000; padding:20px 0; margin-top:30px; border-radius:50px}
-        .evtTop .btn a:hover {box-shadow:0 10px 10px rgba(0,0,0,.2);}
-        .evtTop .NGEBS{font-weight:bold;}
+        .evt_top {background: url(https://static.willbes.net/public/images/promotion/2021/03/2088_top_bg.jpg) no-repeat center top;}
 
         .evt01 {position:relative;padding-bottom:100px;background:#F3F3F3}
         .evt01 .circle01{position:absolute;left:50%;top:22%;margin-left:-100px;}
@@ -139,110 +125,8 @@
                 <img src="https://static.willbes.net/public/images/promotion/2021/03/2088_top_banner.jpg" title="신광은 면접캠프">
             </div>
 
-            <div class="evtCtnsBox evtTop">
+            <div class="evtCtnsBox evt_top">
                 <img src="https://static.willbes.net/public/images/promotion/2021/03/2088_top.jpg" title="면접캠프">
-                <div class="request" id="request">
-                    <div class="requestL">
-                        <h3 class="NGEBS">* 면접캠프 설명회  신청접수</h3>
-                        <table width="0" cellspacing="0" cellpadding="0" class="table_type">
-                            <col width="25%" />
-                            <col  />
-                            <tr>
-                                <th>* 이름</th>
-                                <td scope="col">
-                                    <input type="text" id="register_name" name="register_name" value="{{sess_data('mem_name')}}" title="성명" {{(sess_data('is_login') === true) ? 'readonly="readonly"' : ''}}/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th>* 연락처</th>
-                                <td>
-                                    <input type="text" id="register_tel" name="register_tel" value="{{sess_data('mem_phone')}}" maxlength="11">
-                                </td>
-                            </tr>
-                            <tr>
-                                <th>* 참여일</th>
-                                <td>
-                                    <ul>
-                                        @foreach($arr_base['register_list'] as $key => $val)
-                                            @if(empty($val['RegisterExpireStatus']) === false && $val['RegisterExpireStatus'] == 'Y')
-                                                @php
-                                                    // 강의 기간 지나면 자동 disabled 처리
-                                                    /*****************************************************************************************
-                                                        신청강의 날짜 형식. 텍스트를 읽어 날짜체크를 하기 때문에 신청리스트 등록시 예시에 맞게 넣을것.
-                                                            ex)     12.14 프리미엄올공반2차 설명회
-                                                                    2.8(토) 초시생을 위한 합격커리큘럼 설명회
-                                                                    6/5 면접캠프설명회
-                                                    *****************************************************************************************/
-                                                    $reg_year = '2021';
-                                                    $temp_date = explode(' ', $val['Name'])[0];
-                                                    if(strpos($temp_date, '(') !== false) {
-                                                        $temp_date = substr($temp_date, 0, strpos($temp_date, '('));
-                                                    }
-                                                    if(strpos($temp_date, '.') !== false) {
-                                                        $reg_month_day = explode('.', $temp_date);
-                                                    } else if (strpos($temp_date, '/') !== false) {
-                                                        $reg_month_day = explode('/', $temp_date);
-                                                    }
-                                                    $reg_month = mb_strlen($reg_month_day[0], 'utf-8') == 1 ? '0'.$reg_month_day[0] : $reg_month_day[0] ;
-                                                    $reg_day = mb_strlen($reg_month_day[1], 'utf-8') == 1 ? '0'.$reg_month_day[1] : $reg_month_day[1] ;
-                                                    $reg_date = $reg_year.$reg_month.$reg_day.'0000';
-                                                    //echo date('YmdHi', strtotime($reg_date. '+1 days'));
-                                                @endphp
-                                                @if(time() >= strtotime($reg_date. '+1 days'))
-                                                    <li><input type="checkbox" name="register_disable[]" id="campus{{$key}}" value="{{$val['ErIdx']}}" /> <label for="campus{{$key}}">{{$val['Name']}}</label></li>
-                                                @else
-                                                    <li><input type="checkbox" name="register_chk[]" id="campus{{$key}}" value="{{$val['ErIdx']}}" /> <label for="campus{{$key}}">{{$val['Name']}}</label></li>
-                                                @endif
-                                            @endif
-                                        @endforeach
-                                    </ul>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th>* 직렬</th>
-                                <td>
-                                    <ul>
-                                        <li><input type="radio" name="register_data2" id="CT1" value="일반남자" /> <label for="CT1">일반남자</label></li>
-                                        <li><input type="radio" name="register_data2" id="CT2" value="일반여자" /> <label for="CT2">일반여자</label></li>
-                                        <li><input type="radio" name="register_data2" id="CT3" value="101단" /> <label for="CT3">101단</label></li>                                      
-                                        <li><input type="radio" name="register_data2" id="CT5" value="전의경" /> <label for="CT5">전의경</label></li>                                    
-                                        &nbsp;&nbsp;&nbsp;<li><input type="radio" name="register_data2" id="CT7" value="기타" /> <label for="CT7">기타</label></li>
-                                    </ul>
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div class="requestR">
-                        <h3 class="NGEB">* 개인정보 수집 및 이용에 대한 안내</h3>
-                        <ul>
-                            <li>
-                                <strong>1. 개인정보 수집 이용 목적</strong> <br>
-                                - 신청자 본인 확인 및 신청 접수 및 문의사항 응대
-                                - 통계분석 및 마케팅
-                                - 윌비스 신광은경찰학원의 신상품이나 새로운 서비스, 이벤트 등 최신 정보 및 광고성 정보 제공
-                            </li>
-                            <li><strong>2. 개인정보 수집 항목</strong> <br>
-                            - 필수항목 : 성명, 연락처, 직렬항목
-                            </li>
-                            <li><strong>3. 개인정보 이용기간 및 보유기간</strong><br>
-                            - 이용 목적 달성 또는 신청자의 신청 해지 및 삭제 요청 시 파기
-                            </li>
-                            <li><strong>4. 신청자의 개인정보 수집 및 활용 동의 거부 시</strong><br>
-                            - 개인정보 수집에 동의하지 않으시는 경우 설명회 접수 및 서비스 이용에 제한이 있을 수 있습니다.
-                            </li>
-                            <li>5. 입력하신 개인정보는 수집목적 외 신청자의 동의 없이 절대 제3 자에게 제공되지 않으며 개인정보 처리방침에 따라 보호되고 있습니다.
-                            </li>
-                            <li>6. 신이벤트 진행에 따른 단체사진 및 영상 촬영에 대한 귀하의 초상권 사용을 동의하며, 해당 저작물에 대한 저작권은 윌비스에 귀속됩니다.
-                            </li>
-                        </ul>
-                        <div>
-                            <input name="is_chk" id="is_chk" type="checkbox" value="Y"><label for="is_chk"> 윌비스에 개인정보 제공 동의하기(필수)</label>
-                        </div>
-                    </div>
-                </div>
-                <div class="btn NGEB">
-                    <a href="#none" onclick="javascript:fn_submit();">면접캠프 설명회 신청하기 ></a>
-                </div>
             </div>
 
             <div class="evtCtnsBox evt01">
@@ -376,7 +260,7 @@
             <div class="evtCtnsBox evt04" id="sky01">
                 <img src="https://static.willbes.net/public/images/promotion/2021/03/2088_04.jpg" usemap="#Map2088b" title="접수안내" border="0">
                 <map name="Map2088b" id="Map2088b">
-                    <area shape="rect" coords="331,916,790,1027" href="https://police.willbes.net/pass/offLecture/index/type/all?cate_code=3010&campus_ccd=605001&course_idx=1047&subject_idx=1064" target="_blank" />
+                    <area shape="rect" coords="336,979,788,1086" href="https://police.willbes.net/pass/offLecture/index/type/all?cate_code=3010&campus_ccd=605001&course_idx=1047&subject_idx=1064" target="_blank" />
                 </map>
             </div>
 

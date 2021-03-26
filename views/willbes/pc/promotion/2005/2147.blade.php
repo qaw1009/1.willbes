4e0e6cdc7b701b801f2abb3ff2b045e5498cc63a
이윤구 <yoongukk@willbes.com>
2021-03-26 13:28:49 +0900

    (고등고시) 새봄맞이 이벤트 페이지 2개 (5급공채, 법학) 신규

---
+++ views/willbes/pc/promotion/2005/2147.blade.php
@@ -0,0 +1,167 @@
+@extends('willbes.pc.layouts.master')
+
+@section('content')
+    @include('willbes.pc.layouts.partial.site_menu')
+    <!-- Container -->
+    <style type="text/css">
+        .evtContent {
+            width:100% !important;
+            min-width:1120px !important;
+            margin-top:20px !important;
+            padding:0 !important;
+            background:#fff;
+        }
+        .evtContent span {vertical-align:auto}
+        .evtCtnsBox {width:100%; text-align:center; min-width:1120px;}
+
+        /************************************************************/ 
+
+        /*타이머*/
+        .timeBox {background:#333}
+        .time {width:920px; margin:0 auto; text-align:center;}
+        .time {text-align:center; padding:20px 0}
+        .time ul {width:100%;}
+        .time ul:after {content:''; display:block; clear:both}
+        .time li {display:inline; float:left; line-height:61px; font-size:24px; margin-right:10px;}
+        .time li:first-child {margin-left:80px}
+        .time li img {width:44px}
+        .time .time_txt {color:#fff; letter-spacing:-1px}
+        .time .time_txt span {color:#d63e4d; animation:upDown 2s infinite;-webkit-animation:upDown 2s infinite;}
+        @@keyframes upDown{
+        from{color:#d63e4d}
+        50%{color:#ff6600}
+        to{color:#d63e4d}
+        }
+        @@-webkit-keyframes upDown{
+        from{color:#d63e4d}
+        50%{color:#ff6600}
+        to{color:#d63e4d}
+        }          
+
+        .evt_top {background:url(https://static.willbes.net/public/images/promotion/2021/03/2147_top_bg.jpg) no-repeat center top;}	
+
+        .evt_01 {margin-bottom:100px}
+        .evt_01 .title {width:1120px; font-size:25px;  margin:0 auto 20px; text-align:left; color:#464646;padding:25px;}
+        .evt_01 .evt01_box {width:1120px; padding:20px 0; margin:0 auto 50px; background:#fff;} 	
+
+        .evt_02 {margin-bottom:100px}
+        .evt_02 .title {width:1120px; font-size:25px;  margin:0 auto 20px; text-align:left; color:#464646;padding:25px;}
+        .evt_02 .evt02_box {width:1120px; padding:20px 0; margin:0 auto 50px; background:#fff;} 	
+
+        .evt_03 {margin-bottom:100px}
+        .evt_03 .title {width:1120px; font-size:25px;  margin:0 auto 20px; text-align:left; color:#464646;padding:25px;}
+        .evt_03 .evt03_box {width:1120px; padding:20px 0; margin:0 auto 50px; background:#fff;} 	
+        
+        .evtInfo {padding:80px 0; background:#333; color:#fff; font-size:14px; margin-top:100px}
+		.evtInfoBox { width:1000px; margin:0 auto; text-align:left; line-height:1.4}
+		.evtInfoBox h4 {font-size:30px; margin-bottom:40px}
+		.evtInfoBox .infoTit {font-size:18px; margin-bottom:20px}
+		.evtInfoBox .infoTit strong {padding:8px 20px; background:#000; border-radius:20px; font-weight:normal !important}
+		.evtInfoBox ul {margin-bottom:30px}
+        .evtInfoBox li {margin-bottom:8px; list-style:disc; margin-left:20px}
+        .evtInfoBox span {vertical-align:bottom}  
+ 
+        /************************************************************/      
+    </style> 
+	<div class="evtContent NSK">
+        <div class="evtCtnsBox timeBox">
+            <div class="time NGEB" id="newTopDday">
+                <ul>
+                    <li class="time_txt"><span>{{ kw_date('n/j(%)', $arr_promotion_params['edate']) }}</span> 마감!</li>
+                    <li class="time_txt"><span>남은 시간은</span></li>
+                    <li><img id="dd1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="dd2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li class="time_txt">일</li>
+                    <li><img id="hh1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="hh2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li class="time_txt">:</li>
+                    <li><img id="mm1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="mm2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li class="time_txt">:</li>
+                    <li><img id="ss1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="ss2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>             
+                </ul>
+            </div>
+        </div>
+
+		<div class="evtCtnsBox evt_top">
+            <img src="https://static.willbes.net/public/images/promotion/2021/03/2147_top.jpg" alt="새봄맞이 특별이벤트" />
+		</div>
+
+        <div class="evtCtnsBox evt_01">                  
+            <div class="evt01_box">       
+                <div class="title NSK-Black">1. 5급 행정</div>                 
+                    @if(empty($arr_base['display_product_data']) === false)
+                        @include('willbes.pc.promotion.display_product_partial',array('group_num'=>1))
+                    @endif                                   
+            </div>
+        </div>
+
+        <div class="evtCtnsBox evt_02">                  
+            <div class="evt02_box">       
+                <div class="title NSK-Black">2. 국립 외교원</div>                 
+                    @if(empty($arr_base['display_product_data']) === false)
+                        @include('willbes.pc.promotion.display_product_partial',array('group_num'=>2))
+                    @endif                                   
+            </div>
+        </div>
+
+        <div class="evtCtnsBox evt_03">                  
+            <div class="evt03_box">       
+                <div class="title NSK-Black">3. PSAT</div>                 
+                    @if(empty($arr_base['display_product_data']) === false)
+                        @include('willbes.pc.promotion.display_product_partial',array('group_num'=>3))
+                    @endif                                   
+            </div>      
+        </div>
+        
+        <div class="evtCtnsBox evtInfo">
+            <div class="evtInfoBox">
+                <h4 class="NSK-Black">윌비스 한림법학원 동영상 새봄맞이 특별이벤트 안내 상품이용안내</h4>
+                <div class="infoTit"><strong>이용안내</strong></div>
+                <ul>
+                    <li>이벤트기간 해당 이벤트 페이지에서 등록된 강의 자유 선택 결제<br>
+                        [5급행정/국립외교원/PSAT/5급헌법]<br>
+                        - 등록된 강의 모두 10%할인<br>
+                        - 2과목 이상 결제 모두 약 20%할인<br><br>
+                        [변호사시험/법원행시]<br>
+                        - 등록된 강의 모두 10%할인<br> 
+                        - 2과목 이상 결제 모두 약 30%할인
+                    </li>
+                    <li>강의배수 제한 :  동영상강의는 강의배수 제한 규정이 적용됩니다.</li>
+                    <li>해당 이벤트는 3월 31일(수) 24:00 까지 적용됩니다.</li>
+                    <li>본 이벤트는 학원 사정에 의해 사전공지없이 종료 또는 변경될 수 있습니다.</li>
+                </ul>
+                <div class="infoTit"><strong>교재</strong></div>
+                <ul>
+                    <li>각 강의수강에 필요한 교재는 별도로 구매하셔야 하며, 각 강좌별 교재는 강좌소개 및 교재구매 메뉴에서 별도 구매 가능합니다.</li>
+                </ul>
+                <div class="infoTit"><strong>환불</strong></div>
+                <ul>
+                    <li>결제 후 7일 이내 강좌의 맛보기 강의를 제외하고 2강 이하 수강 시에만 전액 환불 가능합니다.(일부강의만의 환불은 불가합니다.)</li>
+                    <li>결제 후 7일 이내 환불 요청 시 수강한 각 강의 정가 기준으로 수강 부분만큼 차감 후 나머지 부분에 대해 환불이 진행됩니다.</li>
+                    <li>강좌 내 학습 자료 및 모바일 다운로드 이용 시에는 수강한 것으로 간주됩니다.</li>
+                    <li>고객 변심으로 인한 환불은 수강시작일 (당일 포함)로부터 7일이 경과되면, 정가 기준으로 계산하여 사용일수 만큼 차감 후 환불됩니다.</li>
+                    <li>기타 약관의 규정에 따릅니다.</li>
+                </ul>                
+                <div class="infoTit"><strong>유의사항</strong></div>
+                <ul>
+                    <li>본 상품은 특별할인기획 상품으로 쿠폰 할인, 다다익선 할인, 포인트 사용 등 혜택이 적용되지 않으니 양해 및 참고 부탁드립니다.</li>
+                    <li>아이디 공유, 타인에게 양도 및 판매 등 부정 사용 적발 시 수강 중인 강의는 즉시 정지, 회원 자격이 박탈됩니다. 이로 인한 강의 환불은 절대 불가하며, 불법 공유 행위 사안에 따라 민형사상 조치가 발생 할 수 있습니다.</li>
+                </ul>               
+            </div>
+        </div>
+
+	</div>
+     <!-- End Container -->
+
+     <script>  
+        /*디데이카운트다운*/
+        $(document).ready(function() {
+            dDayCountDown('{{$arr_promotion_params['edate']}}');
+        });   
+    </script>
+
+    {{-- 프로모션용 스크립트 include --}}
+    @include('willbes.pc.promotion.promotion_script')
+@stop

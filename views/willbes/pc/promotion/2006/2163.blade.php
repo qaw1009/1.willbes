b046cf6bf9bdb01350c505260d75dfedb4fad1e0
이윤구 <yoongukk@willbes.com>
2021-04-06 11:12:04 +0900

    [감정평가사] 2022 얼리버드 SUCCESS T-PASS 프로모션 수정

--- views/willbes/pc/promotion/2006/2163.blade.php
+++ views/willbes/pc/promotion/2006/2163.blade.php
@@ -84,7 +84,7 @@
 			<div id="tab01" class="pb100">
                 <img src="https://static.willbes.net/public/images/promotion/2021/04/2163_03_01.png" alt="감정평가실무" usemap="#2163_03_01" border="0" class="pb30" />
                 <map name="2163_03_01" id="2163_03_01">
-                    <area shape="rect" coords="1,226,300,285" href="javascript:fnPlayerSample('', '', 'HD');">
+                    <area shape="rect" coords="1,226,300,285" href="javascript:fnPlayerSample('161446', '1228538', 'HD');">
                     <area shape="rect" coords="309,226,608,284" href="https://job.willbes.net/support/gosiNotice/show/cate/309003?board_idx=330143&s_cate_code=309003&s_cate_code_disabled=Y" target="_blank" />
                 </map>
                 <table cellspacing="0" cellpadding="0" class="boardD">
@@ -111,7 +111,7 @@
                     </tr>
                     <tr style="background:#e3e3e3;color:#696969">
                         <td>문제풀이 패키지</td>
-                        <td>150일</td>
+                        <td>180일</td>
                         <td style="color:#f72739">25%</td>
                         <td width="137"><span style="text-decoration: line-through;">583,000원</span><br />
                         <span style="color:#f72739">437,250원</span></td>
@@ -192,7 +192,7 @@
                         <td width="111">신청</td>
                     </tr>
                     <tr style="background:#e3e3e3;color:#696969">
-                        <td>기본강의 패키지</td>
+                        <td>기본강의</td>
                         <td>90일</td>
                         <td style="color:#f72739">0%</td>
                         <td width="137"><span style="text-decoration: line-through;">146,000원</span><br />
@@ -250,7 +250,7 @@
                         <td>90일</td>
                         <td style="color:#f72739">0%</td>
                         <td width="137"><span style="text-decoration: line-through;">162,000원</span><br />
-                        <span style="color:#f72739">162,200원</span></td>
+                        <span style="color:#f72739">162,000원</span></td>
                         <td style="color:#2766f7" onclick="location.href='http://job.willbes.net/periodPackage/show/cate/309003/pack/648001/prod-code/180955';" style="cursor:pointer;">신청하기</td>
                     </tr>
                     <tr style="background:#e3e3e3;color:#696969">

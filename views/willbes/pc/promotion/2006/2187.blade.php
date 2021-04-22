e3bb3fa9bc39db55c75398d801c5d1653ee8a748
김창용 <winmir@willbes.com>
2021-04-22 15:35:13 +0900

    노무사 리얼 실전모의고사 프로모션 신규

--- views/willbes/pc/promotion/2006/2187.blade.php
+++ views/willbes/pc/promotion/2006/2187.blade.php
@@ -17,9 +17,13 @@
 		/************************************************************/ 
 		.evt_top {background:url(https://static.willbes.net/public/images/promotion/2021/04/2187_top_bg.jpg) no-repeat center top;}	
         .evt_01 {background:url(https://static.willbes.net/public/images/promotion/2021/04/2187_01_bg.jpg) no-repeat center top;}
-        .evt_02 {background:#f0f1ef; padding-bottom:100px}
+        .evt_02 {background:#f0f1ef; padding-bottom:50px}
         .evt_02 > div {width:1120px; margin:0 auto; position:relative}
-     
+        
+        .check {letter-spacing:3; color:#fff; margin-top:30px}
+        .check label {cursor:pointer; font-size:15px;color:#000;font-weight:bold;}
+        .check input {border:2px solid #000; margin-right:10px; height:24px; width:24px; }
+        .check a {display:inline-block; padding:12px 20px 10px 20px; color:#fff; background:#000; margin-left:50px; border-radius:20px;font-size:14px;font-weight:bold;}
 
 		.evtInfo {padding:80px 0; background:#535353; color:#fff; font-size:14px}
 		.evtInfoBox { width:1000px; margin:0 auto; text-align:left; line-height:1.4}
@@ -47,6 +51,13 @@
                 <a href="https://job.willbes.net/pass/offPackage/index" title="학원참석 모의고사" target="_blank" style="position: absolute; left: 0.18%; top: 42.43%; width: 30.71%; height: 48.85%; z-index: 2;"></a>
                 <a href="https://job.willbes.net/book/index/cate/309002" title="모의고사 문제지주문" target="_blank" style="position: absolute; left: 34.29%; top: 42.43%; width: 30.71%; height: 48.85%; z-index: 2;"></a>
                 <a href="https://job.willbes.net/pass/mockTestNew/apply/cate" title="온라인 모의고사" target="_blank" style="position: absolute; left: 69.29%; top: 42.43%; width: 30.71%; height: 48.85%; z-index: 2;"></a>
+            </div>
+            <div class="check mb100">
+                <label>
+                    <input name="ischk"  type="checkbox" value="Y" />
+                    페이지 하단 이용안내를 모두 확인하였고, 이에 동의합니다.
+                </label>
+                <a href="#careful">이용안내확인하기 ↓</a>
             </div>
 		</div>        
 
@@ -85,4 +96,17 @@
 		</div>
 	</div>
     <!-- End Container -->
+
+    <script type="text/javascript">
+        /*수강신청 동의*/ 
+        function go_PassLecture(code){
+            if($("input[name='ischk']:checked").size() < 1){
+                alert("이용안내에 동의하셔야 합니다.");
+                return;
+            }
+
+            var url = '{{ site_url('/package/show/cate/309003/pack/648002/prod-code/') }}' + code;
+            location.href = url;
+        }    
+    </script>
 @stop

850f34f95752d92847ec03f3856fb6015766cca7
김창용 <winmir@willbes.com>
2021-04-29 17:41:22 +0900

    경찰 온라인 기본완성 기출반 프로모션 신규

--- views/willbes/pc/promotion/2001/2188.blade.php
+++ views/willbes/pc/promotion/2001/2188.blade.php
@@ -31,13 +31,48 @@
 
         .wb_02 {background:#015275} 
         .wb_03 {padding-bottom:150px} 
+
+        /*타이머*/
+        .newTopDday {clear:both;background:#f5f5f5; width:100%; padding:20px 0; font-size:26px;}
+        .newTopDday ul {width:1120px; margin:0 auto}
+        .newTopDday ul li {display:inline; float:left; margin-right:5px; text-align:center; font-weight:600; color:#000}
+        .newTopDday ul li strong {line-height:70px}
+        .newTopDday ul li img {width:50px}
+        .newTopDday ul li:first-child {line-height:none; text-align:right; padding-right:10px; padding-top:10px; width:28%}
+        .newTopDday ul li:last-child {line-height:none; text-align:left; padding-left:10px; padding-top:5px; width:24%; line-height:70px}
+        .newTopDday ul:after {content:""; display:block; clear:both}
     </style>
 
     <div class="evtContent NSK" id="evtContainer">
         <div class="skybanner">
             <a href="#evt02"><img src="https://static.willbes.net/public/images/promotion/2021/04/2188_sky01.jpg" alt="기본완성 기출반 단과"></a>
             <a href="#evt03"><img src="https://static.willbes.net/public/images/promotion/2021/04/2188_sky02.jpg" alt="기본완성 기출반 종합반"></a>
-        </div>      
+        </div>   
+
+        <!-- 타이머 -->
+        <div id="newTopDday" class="newTopDday">
+            <div>
+                <ul>
+                    <li>
+                        기본완성기출반<br>사전접수 EVENT
+                    </li>
+                    <li><img id="dd1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="dd2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><strong>일</strong></li>
+                    <li><img id="hh1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="hh2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><strong>:</strong></li>
+                    <li><img id="mm1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="mm2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><strong>:</strong></li>
+                    <li><img id="ss1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="ss2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li>
+                        남았습니다.
+                    </li>
+                </ul>
+            </div>
+        </div>     
         
         <div class="evtCtnsBox wb_police">
             <img src="https://static.willbes.net/public/images/promotion/2020/10/wb_police.jpg"  alt="신광은 경찰학원" />            
@@ -78,4 +113,13 @@
         </div> 
 	</div>
      <!-- End Container -->
+
+    <script type="text/javascript">    
+        /*디데이카운트다운*/
+        $(document).ready(function() {
+            dDayCountDown('@if(empty($arr_promotion_params['edate'])===false) {{$arr_promotion_params['edate']}} @endif');
+        });   
+    </script>
+    {{-- 프로모션용 스크립트 include --}}
+    @include('willbes.pc.promotion.promotion_script')
 @stop

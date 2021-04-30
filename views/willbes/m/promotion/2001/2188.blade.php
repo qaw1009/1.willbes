59c166afb6490f504c0b3be50c7d7ba88857a306
김창용 <winmir@willbes.com>
2021-04-30 09:34:30 +0900

    경찰 온라인 기본완성 기출반 프로모션 모바일

--- views/willbes/m/promotion/2001/2188.blade.php
+++ views/willbes/m/promotion/2001/2188.blade.php
@@ -11,138 +11,95 @@
     .dday span {color:#3a99f0; box-shadow:inset 0 -15px 0 rgba(0,0,0,0.1);}
     .dday a {display:inline-block; float:right; border-radius:30px; padding:5px 20px; color:#fff; background:#000; font-size:14px !important;}
 
-    .evt04 .boxYoutube {margin:40px auto}
-    .evt04 .tabMenu {margin-right:-10px; margin-bottom:20px}
-    .evt04 .tabMenu li {display:inline; float:left; width:33.33333%; }
-    .evt04 .tabMenu a {display:block; opacity: 0.5; margin-right:10px; background:#000;}
-    .evt04 .tabMenu a.active {box-shadow:0 10px 10px rgba(0,0,0,.2); opacity: 1;}
-    .evt04 .tabMenu a img {width:100%}
-    .evt04 .tabMenu:after {content:''; display:block; clear:both}
-
-    .video-container {position:relative; padding-bottom:56.25%; padding-top:30px; height:0; overflow: hidden;}
+    .video-container {position:relative; padding-top:30px; padding-bottom:56.25%; margin:0 20px; height:0; overflow: hidden;}
     .video-container iframe,
     .video-container object,
     .video-container embed {position:absolute; top:0; left:50%; width:100%; margin-left:-50%; height:100%;}
-
-
-    .btnbuyBox {width:100%; position:fixed; bottom:0; text-align:center; background:rgba(255,255,255,0.5); padding-top:10px;z-index:100;}
-    .btnbuy {max-width:720px; margin:0 auto}
-    .btnbuy a {display:inline-block; width:33%; max-width:700px; margin:0 auto 5px; font-size:1rem; background:#000; color:#fff; padding:15px 0; text-align:center; border-radius:10px; line-height:1.4}
-    .btnbuy a span {font-size:1.2rem;}
-    .btnbuy a:hover {background:#3a99f0;
-        -webkit-animation: shadow-drop-2-center 0.4s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
-        animation: shadow-drop-2-center 0.4s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
-    }
-    .infoCheck {width:100%; max-width:720px; margin:10px auto; font-size:12px;}
-    .infoCheck label {margin-right:30px; cursor: pointer; }
-    .infoCheck input[type=checkbox] {width:20px; height:20px; margin-right:10px}
-    .infoCheck input[type=checkbox]:checked + label {border-bottom:1px dashed #0099ff; font-weight:bold; color:#0099ff} 
-    .infoCheck a {display:inline-block; background:#333; color:#fff; height:30px; line-height:30px; text-align:center; padding:0 20px; border-radius:20px}
-    .infoCheck a:hover {background:#0099ff;}
-
-    .evtInfo {padding:40px 20px; background:#333; color:#fff; font-size:16px;}
-    .guide_box {text-align:left; line-height:1.4}
-    .guide_box li {list-style: decimal; margin-left:20px; font-size:14px; margin-bottom:10px; color:#ccc}
-    .guide_box h2 {font-size:24px; margin-bottom:30px}
-    .guide_box dt {margin:20px 0 10px}
-    .guide_box dd {margin-bottom:5px}
-    
-    .guide_box .only {color:#E80000;vertical-align:top;}
 
     /* 폰 가로, 태블릿 세로*/
     @@media only screen and (max-width: 374px)  {   
         .dday {font-size:18px !important;} 
         .dday a {padding:5px 10px;}
-        .evt06 .slide_con {margin:0 10px; padding-bottom:40px}  
-        .content_guide_wrap .guide_tit{font-size:20px; margin-bottom:30px}
-        .content_guide_wrap .tabs li a {font-size:14px !important; letter-spacing:-1px}
-        .btnbuy a {width:31%;}
     }
 
     /* 태블릿 세로 */
     @@media only screen and (min-width: 375px) and (max-width: 640px) {       
         .dday {font-size:18px !important;} 
         .dday a {padding:5px 10px;}
-        .btnbuy a {width:31%;}
     }
 
     /* 태블릿 가로, PC */
     @@media only screen and (min-width: 641px) {
-        .content_guide_wrap .tabs li a br {display:none}
+        
     }
     </style>
 
-<form id="regi_form" name="regi_form" method="POST" onsubmit="return false;" novalidate>
-    {!! csrf_field() !!}
-    {!! method_field('POST') !!}
-</form>
 
 <div id="Container" class="Container NSK c_both"> 
     <div class="evtCtnsBox dday NSK-Thin">
-        <strong class="NSK-Black">2022 몰입 PASS 마감 <span id="ddayCountText"></span> </strong>
+        <strong class="NSK-Black">기본완성기출반 사전접수 마감까지 <span id="ddayCountText"></span> </strong>
     </div>
 
-    <div class="evtCtnsBox evt00">
+    <div class="evtCtnsBox">
         <img src="https://static.willbes.net/public/images/promotion/2020/07/1556m_00.jpg" alt="경찰학원부분 1위" >        
     </div> 
 
-    <div class="evtCtnsBox evtTop">
+    <div class="evtCtnsBox">
         <img src="https://static.willbes.net/public/images/promotion/2021/04/2188m_01.jpg" alt="몰입 패스" >
     </div> 
 
-    <div class="evtCtnsBox evt01">        
+    <div class="evtCtnsBox">        
         <img src="https://static.willbes.net/public/images/promotion/2021/04/2188m_02.jpg" alt="몰입해야 하는 이유" >
         <div class="youtubeCts video-container">
             <iframe src="https://www.youtube.com/embed/40LDBoOoD_k?rel=0&modestbranding=1&showinfo=0" frameborder="0" allowfullscreen=""></iframe>
         </div>
     </div>
     
-    <div class="evtCtnsBox evt02">        
+    <div class="evtCtnsBox">        
         <img src="https://static.willbes.net/public/images/promotion/2021/04/2188m_03.jpg" alt="교수진" >
         <div class="youtubeCts video-container">
             <iframe src="https://www.youtube.com/embed/VHTrL5w2IF4?rel=0&modestbranding=1&showinfo=0" frameborder="0" allowfullscreen=""></iframe>
         </div>        
     </div>
     
-    <div class="evtCtnsBox evt03">        
+    <div class="evtCtnsBox">        
         <img src="https://static.willbes.net/public/images/promotion/2021/04/2188m_04.jpg" alt="무제한 수강" >
         <div class="youtubeCts video-container">
             <iframe src="https://www.youtube.com/embed/KkESWQLjtq8?rel=0&modestbranding=1&showinfo=0" frameborder="0" allowfullscreen=""></iframe>
         </div>
     </div>   
-
     
-    <div class="evtCtnsBox evt05">        
+    <div class="evtCtnsBox">        
         <img src="https://static.willbes.net/public/images/promotion/2021/04/2188m_05.jpg" alt="쿠폰지급" >
     </div>
     
-    <div class="evtCtnsBox evt06">        
+    <div class="evtCtnsBox">        
         <img src="https://static.willbes.net/public/images/promotion/2021/04/2188m_06.jpg" alt="신광은 몰입 t패스" >
     </div>    
 
-    <div class="evtCtnsBox evt07">        
+    <div class="evtCtnsBox">        
         <img src="https://static.willbes.net/public/images/promotion/2021/04/2188m_07.jpg" alt="장정훈 몰입 t패스" >
-    </div>
-    @if(empty($arr_base['display_product_data']) === false)
-        @include('willbes.m.promotion.display_product_partial',array('group_num'=>1))
-    @endif
+        @if(empty($arr_base['display_product_data']) === false)
+            @include('willbes.m.promotion.display_product_partial',array('group_num'=>1))
+        @endif
+    </div>    
     
-    <div class="evtCtnsBox evt08">        
+    <div class="evtCtnsBox pb50">        
         <img src="https://static.willbes.net/public/images/promotion/2021/04/2188m_08.jpg" alt="김원욱 몰입 t패스" >
-    </div>
-    @if(empty($arr_base['display_product_data']) === false)
-        @include('willbes.m.promotion.display_product_partial',array('group_num'=>2))
-    @endif
+        @if(empty($arr_base['display_product_data']) === false)
+            @include('willbes.m.promotion.display_product_partial',array('group_num'=>2))
+        @endif
+    </div>    
 </div>
 
 <!-- End Container -->
-    <script type="text/javascript"> 
-        /*디데이카운트다운*/
-        $(document).ready(function() {
-            dDayCountDown('{{$arr_promotion_params['edate']}}', '{{$arr_promotion_params['etime'] or "00:00"}}', 'txt');
-        });
-    </script>
+<script type="text/javascript"> 
+    /*디데이카운트다운*/
+    $(document).ready(function() {
+        dDayCountDown('{{$arr_promotion_params['edate']}}', '{{$arr_promotion_params['etime'] or "00:00"}}', 'txt');
+    });
+</script>
 
-    {{-- 프로모션용 스크립트 include --}}
-    @include('willbes.pc.promotion.promotion_script')
+{{-- 프로모션용 스크립트 include --}}
+@include('willbes.pc.promotion.promotion_script')
 @stop

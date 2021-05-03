e1eb6debe1e2ddcd42eff05a095dfa4e2bfb8f64
김성일 <jxr880210@willbes.com>
2021-05-03 17:11:52 +0900

    [프론트] > 경찰 온라인 릴레이 프로모션 수정

--- views/willbes/pc/promotion/2001/2186.blade.php
+++ views/willbes/pc/promotion/2001/2186.blade.php
@@ -89,7 +89,7 @@
         </div>
 
         <div id="entry" class="p_re">
-            @php $num = 3; @endphp
+            @php $num = 3; $end_date = ''; @endphp
             @foreach($arr_base['register_list'] as $row)
 
                 @if(time() < strtotime(min($arr_base['register_date_list']['register_start_date'])))
@@ -108,7 +108,7 @@
                 @endif
 
                 @php $num++; @endphp
-                @if(time() >= strtotime($row['RegisterStartDatm']))
+                @if(time() >= strtotime($row['RegisterStartDatm']) && time() < strtotime($row['RegisterEndDatm']))
                     @php $el_idx = $row['ErIdx']; @endphp
                     <div class="evtCtnsBox evt0{{ $num }}">
                         @if($num === 6)
@@ -120,12 +120,31 @@
                             <img src="https://static.willbes.net/public/images/promotion/2021/04/2186_0{{ $num }}.jpg"  alt="{{ $row['Name'] }}"/>
                         @endif
                     </div>
-
-                    @if(time() > strtotime($row['RegisterEndDatm']))
-                        <div class="evtEnd"><img src="https://static.willbes.net/public/images/promotion/2021/04/2186_end.png" alt="마감"/></div>
-                    @endif
                     @break;
                 @endif
+
+                @if(empty($end_date) === false)
+                    @if(time() > strtotime($end_date) && time() < strtotime($row['RegisterStartDatm']))
+                        @php $num--; @endphp
+                        <div class="evtCtnsBox evt0{{ $num }}">
+                            @if($num === 6)
+                                <div class="link">
+                                    <img src="https://static.willbes.net/public/images/promotion/2021/04/2186_0{{ $num }}.jpg" alt="{{ $row['Name'] }}"/>
+                                    <a href="https://police.willbes.net/promotion/index/cate/3001/code/2172" target="_blank" title="이벤트 바로가기" style="position: absolute; left: 78.3%; top: 82.13%; width: 12.41%; height: 5.6%; z-index: 2;"></a>
+                                </div>
+                            @else
+                                <img src="https://static.willbes.net/public/images/promotion/2021/04/2186_0{{ $num }}.jpg"  alt="{{ $row['Name'] }}"/>
+                            @endif
+                        </div>
+                        <div class="evtEnd"><img src="https://static.willbes.net/public/images/promotion/2021/04/2186_end.png" alt="마감"/></div>
+                        @break;
+                    @endif
+                @endif
+
+                @if(time() > strtotime($row['RegisterEndDatm']))
+                    @php $end_date = $row['RegisterEndDatm']; @endphp
+                @endif
+
             @endforeach
         </div>
 
@@ -141,7 +160,7 @@
                     {!! method_field('POST') !!}
                     <input type="hidden" name="event_idx" value="{{ $data['ElIdx'] }}" readonly/>
                     <input type="hidden" name="register_type" value="promotion" readonly/>
-{{--                    <input type="hidden" name="time_check" value="Y" readonly/> --}}{{-- 이벤트 날짜 체크 여부 --}}
+        {{--                    <input type="hidden" name="time_check" value="Y" readonly/> --}}{{-- 이벤트 날짜 체크 여부 --}}
                     <input type="hidden" name="register_chk[]" value="{{ $el_idx or '' }}">
 
                     <div class="tx-left">
@@ -182,11 +201,11 @@
         </div>
 
         <div class="evtCtnsBox evtInfo" id="careful">
-			<div class="evtInfoBox">
-				<h4 class="NSK-Black">윌비스 릴레이 이벤트 이용안내</h4>
-				<div class="infoTit"><strong>5월 릴레이 이벤트 유의사항</strong></div>
-				<ul>
-					<li>이벤트 대상 : 윌비스신광은경찰 온라인 회원 누구나</li>
+            <div class="evtInfoBox">
+                <h4 class="NSK-Black">윌비스 릴레이 이벤트 이용안내</h4>
+                <div class="infoTit"><strong>5월 릴레이 이벤트 유의사항</strong></div>
+                <ul>
+                    <li>이벤트 대상 : 윌비스신광은경찰 온라인 회원 누구나</li>
                     <li>로그인/회원가입 해야만 이벤트에 참여 가능합니다.</li>
                     <li>회원탈퇴 후 재가입하여 참여하시는 분은 이벤트 당첨에서 제외됩니다.</li>
                     <li>회원정보 오류로 인해 상품 미발송된 경우 재발송 불가하오니, 이벤트 참여 전 정확한 확인 부탁드립니다.</li>
@@ -205,74 +224,74 @@
                     <li>본 이벤트로 당첨된 패스 할인쿠폰 사용기간은 7일 입니다.</li>
                     <li>본 이벤트는 당사의 사정으로 인하여 당첨자 공지가 지연될 수 있습니다.</li>
                     <li>본 이벤트는 당사의 사정으로 인하여 상품 변경 또는 상품 지급일이 변경될 수 있습니다.</li>
-                    <li>유의사항을 읽지 않고 발생한 모든 사항에 대해서 윌비스신광은경찰은 책임지지 않습니다.</li>                
-				</ul>
-				<div class="infoTit"><strong>소문내기 이벤트 유의사항</strong></div>
-				<ul>
-					<li>이벤트 대상 윌비스신광은경찰 온라인 회원 누구나</li>
+                    <li>유의사항을 읽지 않고 발생한 모든 사항에 대해서 윌비스신광은경찰은 책임지지 않습니다.</li>
+                </ul>
+                <div class="infoTit"><strong>소문내기 이벤트 유의사항</strong></div>
+                <ul>
+                    <li>이벤트 대상 윌비스신광은경찰 온라인 회원 누구나</li>
                     <li>해당 이벤트는 로그인 후 참여 가능합니다.</li>
                     <li>동일한 URL은 한 번 참여한 것으로 인정됩니다.</li>
                     <li>동일 게시판에 1일 1게시글 이상 작성 시 1회만 참여 인정됩니다. (참여 카페, 커뮤니티/게시판/날짜를 다르게 한 다중참여는 가능)</li>
                     <li>동일한 게시글은 한 번만 인정됩니다. (최초 작성글)</li>
                     <li>각 커뮤니티의 정책에 의해 게시글이 삭제되어 확인할 수 없는 경우는 인증에 포함되지 않습니다.</li>
-                    <li>유의사항을 읽지 않고 발생한 모든 사항에 대해서 윌비스신광은경찰은 책임지지 않습니다.</li>  
-				</ul>
-           
-				<div class="infoTit"><strong>※ 이용문의 : 고객만족센터 1544-5006</strong></div>			
-			</div>
-		</div> 
+                    <li>유의사항을 읽지 않고 발생한 모든 사항에 대해서 윌비스신광은경찰은 책임지지 않습니다.</li>
+                </ul>
+
+                <div class="infoTit"><strong>※ 이용문의 : 고객만족센터 1544-5006</strong></div>
+            </div>
+        </div>
     </div>
-    <!-- End Container -->
+<!-- End Container -->
 
-    <script>
-        var $regi_form = $('#regi_form');
-        var $regi_form_register = $('#regi_form_register');
+<script>
+var $regi_form = $('#regi_form');
+var $regi_form_register = $('#regi_form_register');
 
-        function fn_register_submit() {
-            {!! login_check_inner_script('로그인 후 이용하여 주십시오.','Y') !!}
+function fn_register_submit() {
+    {!! login_check_inner_script('로그인 후 이용하여 주십시오.','Y') !!}
 
-            if (!$regi_form_register.find('input[name="register_chk[]"]').val()) {
-                alert('이벤트 기간이 아닙니다.'); return;
-            }
+    if (!$regi_form_register.find('input[name="register_chk[]"]').val()) {
+        alert('이벤트 기간이 아닙니다.'); return;
+    }
 
-            if ($regi_form_register.find('input[name="is_chk"]').is(':checked') === false) {
-                alert('개인정보 수집/이용 동의 안내에 동의하셔야 합니다.');
-                return;
-            }
+    if ($regi_form_register.find('input[name="is_chk"]').is(':checked') === false) {
+        alert('개인정보 수집/이용 동의 안내에 동의하셔야 합니다.');
+        return;
+    }
 
-            if (!$regi_form_register.find('input[name="register_tel"]').val()) {
-                alert('전화번호를 입력해주세요.');
-                return;
-            }
+    if (!$regi_form_register.find('input[name="register_tel"]').val()) {
+        alert('전화번호를 입력해주세요.');
+        return;
+    }
 
-            var _url = '{!! front_url('/event/registerStore') !!}';
-            if (!confirm('신청하시겠습니까?')) { return; }
-            ajaxSubmit($regi_form_register, _url, function(ret) {
-                if(ret.ret_cd) {
-                    alert(ret.ret_msg);
-                    location.reload();
-                }
-            }, showValidateError, null, false, 'alert');
+    var _url = '{!! front_url('/event/registerStore') !!}';
+    if (!confirm('신청하시겠습니까?')) { return; }
+    ajaxSubmit($regi_form_register, _url, function(ret) {
+        if(ret.ret_cd) {
+            alert(ret.ret_msg);
+            location.reload();
         }
+    }, showValidateError, null, false, 'alert');
+}
 
-        {{--쿠폰발급--}}
-        function giveCheck() {
-            {!! login_check_inner_script('로그인 후 이용하여 주십시오.','Y') !!}
+{{--쿠폰발급--}}
+function giveCheck() {
+    {!! login_check_inner_script('로그인 후 이용하여 주십시오.','Y') !!}
 
-            @if(empty($arr_promotion_params) === false)
-                var _check_url = '{!! front_url('/promotion/promotionEventCheck/') !!}?give_type={{$arr_promotion_params["give_type"]}}&give_idx={{$arr_promotion_params["give_idx"]}}&event_code={{$data['ElIdx']}}&comment_chk_yn={{$arr_promotion_params["comment_chk_yn"]}}';
-            ajaxSubmit($regi_form, _check_url, function (ret) {
-                if (ret.ret_cd) {
-                    alert('쿠폰이 발급되었습니다. \n\n내강의실에서 확인해 주세요.');
-                }
-            }, showValidateError, null, false, 'alert');
-            @else
-                alert('프로모션 추가 파라미터가 지정되지 않았습니다.');
-            @endif
+    @if(empty($arr_promotion_params) === false)
+        var _check_url = '{!! front_url('/promotion/promotionEventCheck/') !!}?give_type={{$arr_promotion_params["give_type"]}}&give_idx={{$arr_promotion_params["give_idx"]}}&event_code={{$data['ElIdx']}}&comment_chk_yn={{$arr_promotion_params["comment_chk_yn"]}}';
+    ajaxSubmit($regi_form, _check_url, function (ret) {
+        if (ret.ret_cd) {
+            alert('쿠폰이 발급되었습니다. \n\n내강의실에서 확인해 주세요.');
         }
+    }, showValidateError, null, false, 'alert');
+    @else
+        alert('프로모션 추가 파라미터가 지정되지 않았습니다.');
+    @endif
+}
 
-        function loginCheck(){
-            {!! login_check_inner_script('로그인 후 이용하여 주십시오.','Y') !!}
-        }
-    </script>
+function loginCheck(){
+    {!! login_check_inner_script('로그인 후 이용하여 주십시오.','Y') !!}
+}
+</script>
 @stop

8d718260ffd5e1793638395b8f695ec63da77ee5
김성일 <jxr880210@willbes.com>
2021-05-17 14:27:08 +0900

    [프론트] > 임용 실전모의고사 프로모션 2202

--- views/willbes/pc/promotion/frame_comment_list_713002.blade.php
+++ views/willbes/pc/promotion/frame_comment_list_713002.blade.php
@@ -1,101 +1,35 @@
 @extends('willbes.pc.layouts.master_no_sitdbar')
 
 @section('content')
-<style type="text/css">
-    /* 프로모션 홍보 url 댓글 */
-    .urlWrap {
-        width:1000px;
-        margin:30px auto 100px;
-        font-size:12px;
-        line-height: 1.4;
-        color:#555556;
-    }
-    .urlWrap .snslink {
-        width: 90%;
-        margin: 0 auto;
-    }
-    .urlWrap .snslink li {
-        display: inline;
-        float: left;
-        width: 25%;
-        text-align: center;
-    }
-    .urlWrap .snslink:after {
-        content:'';
-        display: block;
-        clear:both;
-    }
-    .urlWrap .textarBox {
-        margin-top:80px;
-        background:#e8e6d9;
-        padding:20px;
-        text-align: center;
-        border-radius: 40px 40px 0 0;
-    }
-    .urlWrap .textarBox span {
-        padding: 0 20px;
-        font-weight: 500;
-        font-size:14px;
-    }
-    .urlWrap .textarBox input {
-        width:65%;
-        height:36px;
-        line-height:36px;
-        border:2px solid #929292;
-        border-radius: 20px 0 0 20px;
-        padding-left:20px;
-        vertical-align: middle;
-    }
-    .urlWrap .textarBox a {
-        display: inline-block;
-        text-align: center;
-        color:#fff;
-        background: #000;
-        margin-left:0;
-        height:36px;
-        line-height: 36px;
-        border-radius: 0 20px 20px 0;
-        vertical-align: middle;
-    }
-    .urlWrap .url-List {
-        margin-top: 1px;
-    }
-    .urlWrap .url-List table {
-        border-top:0;
-    }
-    .urlWrap .url-List tr {
-        border-bottom:1px solid #e8e6d9;
-    }
-    .urlWrap .url-List thead th {
-        background:#e8e6d9;
-        font-weight: bold;
-    }
-    .urlWrap .url-List thead th,
-    .urlWrap .url-List td {
-        padding:10px;
-        text-align: center;
-    }
-    .urlWrap .url-List td:last-child {
-        text-align: left;
-    }
-    .urlWrap .url-List td a {
-        float: right;
-        font-size: 12px;
-        border:1px solid #eee;
-        width: 18px;
-        height: 18px;
-        line-height: 18px;
-        text-align: center;
-        color:#ccc;
-        font-family: "Helvetica", "Apple SD Gothic Neo", "sans-serif";
-    }
-    .urlWrap .url-List td a:hover {
-        border:1px solid #000;
-        color:#000;
-    }
-</style>
 
-<div class="urlWrap" id="url">
+@if($__cfg['SiteGroupCode'] === '1011') {{-- 임용 --}}
+    <style>
+        div { font-family: "Noto Sans KR Regular", "Noto Sans KR", "sans-serif" !important;}
+        .evt_table table{width:100%;border-top:1px solid #e9e9e9}
+        .evt_table table tr {border-bottom:1px solid #e9e9e9}
+        .evt_table table th,
+        .evt_table table td {margin:10px 0; font-size:16px; color:#666}
+        .evt_table table th {background:#f9f9f9; color:#000;}
+        .evt_table table td{text-align:left; padding:15px}
+        .evt_table label {margin-right:10px; line-height:28px;}
+        .evt_table input {vertical-align:middle}
+        .evt_table input[type=text] {height:28px; padding:0 10px; color:#494a4d; border:1px solid #b8b8b8; background:#f5f5f5; vertical-align:middle; width:80%; margin-bottom:5px}
+        .evt_table td input[type=text]:last-child {margin-bottom:0}
+        .evt_table input[type=checkbox] {height:20px; width:20px}
+        .evt_table td li {display:inline-block; float:left; width:50%; margin-bottom:10px}
+        .evt_table td a {height:28px; line-height:28px; display:inline-block; background:#42425b; color:#fff; padding:0 10px; margin-left:5px}
+        .evt_table .btns {margin-top:40px}
+        .evt_table .btns a {display:inline-block; width:260px; text-align:center; height:50px; line-height:50px; font-size:20px; color:#fff; background:#42425b; margin:0 10px; border-radius:40px}
+        .evt_table .btns a:hover {background:#fe544a}
+
+        .urladd {padding:20px; background:#2e2e3c; color:#fff; margin:40px auto 20px; font-size:14px; text-align: center}
+        .urladd input[type=text] {height:28px; padding:0 10px; color:#494a4d; border:1px solid #b8b8b8; background:#f5f5f5; vertical-align:middle; width:70%; margin:0 10px; color:#000}
+        .urladd a {display:inline-block; height:28px; line-height:28px; color:#2e2e3c; background:#ffc943; padding:0 20px; vertical-align:middle}
+        .evt_table {width:100%; background-color:#fff !important; padding:20px 0}
+        .evt_table table td {font-size:14px; text-align:center}
+        .evt_table table td:nth-child(2) {text-align:left}
+    </style>
+
     <form id="regi_form_comment" name="regi_form_comment" method="POST" onsubmit="return false;" novalidate>
         {!! csrf_field() !!}
         {!! method_field($method) !!}
@@ -103,68 +37,210 @@
             <input type="hidden" name="{{$key}}" value="{{$val}}"/>
         @endforeach
 
-        @if((config_app('SiteCode') == '2001' || config_app('SiteCode') == '2002') && $arr_input['bottom_cafe_type'] != 'N')
-            {{-- 경찰 사이트일 경우만 적용 --}}
-            <ul class="snslink">
-                <li><a href="http://cafe.daum.net/policeacademy" target="_blank" ><img src="https://static.willbes.net/public/images/promotion/common/snsline01.png"alt="다음카페 경사모" /></a></li>
-                <li><a href="http://cafe.naver.com/polstudy" target="_blank" ><img src="https://static.willbes.net/public/images/promotion/common/snsline02.png" alt="네이버카페 경꿈사" /></a></li>
-                <li><a href="https://gall.dcinside.com/board/lists/?id=government" target="_blank" ><img src="https://static.willbes.net/public/images/promotion/common/snsline03.png" alt="디시 공무원 갤러리" /></a></li>
-                <li><a href="https://gall.dcinside.com/mgallery/board/lists/?id=policeofficer" target="_blank"><img src="https://static.willbes.net/public/images/promotion/common/snsline04.png" alt="디시 순경마이너 갤러리" /></a></li>
-            </ul>
-        @endif
+        <div class="urladd">
+            홍보 URL 남기기 <input type="text" name="event_comment" id="event_comment">
+            <a href="javascript:void(0);" id="btn_submit_comment">주소입력</a>
+        </div>
+        <div class="evt_table pd20">
+            <table>
+                <col width="8%" />
+                <col  />
+                <col width="10%" />
+                <col width="8%" />
+                <col width="14%" />
+                <tbody>
+                @if(empty($list) === true)
+                    <tr>
+                        <td colspan="5" style="text-align: center;">등록된 내용이 없습니다.</td>
+                    </tr>
+                @else
+                    @foreach($list as $row)
+                        <tr>
+                            <td>{{ $paging['rownum'] }}</td>
+                            <td>{!! $row['Content'] !!}</td>
+                            <td>
+                                @if(sess_data('is_login') === true && sess_data('mem_idx') === $row['MemIdx'])
+                                    <a class="btn-comment-del" data-comment-idx="{{$row['Idx']}}" href="javascript:void(0);">삭제</a>
+                                @endif
+                            </td>
+                            <td>{{ str_replace(mb_substr($row['MemName'],1,1,'UTF-8'), "*", mb_substr($row['MemName'],0,4,'UTF-8')) }}</td>
+                            <td>{{ $row['RegDay'] }}</td>
+                        </tr>
+                        @php $paging['rownum']-- @endphp
+                    @endforeach
+                @endif
+                </tbody>
+            </table>
 
-        @if((config_app('SiteCode') == '2003' || config_app('SiteCode') == '2004') && $arr_input['bottom_cafe_type'] != 'N')
-            {{-- 공무원 사이트일 경우만 적용 --}}
-            <ul class="snslink">
-                <li><a href="http://cafe.daum.net/9glade" target="_blank" ><img src="https://static.willbes.net/public/images/promotion/common/pass_snsline01.jpg"alt="다음카페 구꿈사" /></a></li>
-                <li><a href="https://cafe.naver.com/gugrade" target="_blank" ><img src="https://static.willbes.net/public/images/promotion/common/pass_snsline02.jpg" alt="네이버카페 공드림" /></a></li>
-                <li><a href="https://gall.dcinside.com/board/lists?id=government" target="_blank" ><img src="https://static.willbes.net/public/images/promotion/common/pass_snsline03.jpg" alt="디시 공무원 갤러리" /></a></li>
-            </ul>
-        @endif
-
-        <div class="textarBox NSK">
-            <span>홍보 URL 남기기</span>
-            <input type="text" name="event_comment" id="event_comment" placeholder="홍보 URL 남기기.">
-            <a href="#none" class="btnrwt" id="btn_submit_comment"><span class="ir">이벤트 참여하기</span></a>
+            {!! $paging['pagination'] !!}
         </div>
     </form>
+@else
+    <style type="text/css">
+        /* 프로모션 홍보 url 댓글 */
+        .urlWrap {
+            width:1000px;
+            margin:30px auto 100px;
+            font-size:12px;
+            line-height: 1.4;
+            color:#555556;
+        }
+        .urlWrap .snslink {
+            width: 90%;
+            margin: 0 auto;
+        }
+        .urlWrap .snslink li {
+            display: inline;
+            float: left;
+            width: 25%;
+            text-align: center;
+        }
+        .urlWrap .snslink:after {
+            content:'';
+            display: block;
+            clear:both;
+        }
+        .urlWrap .textarBox {
+            margin-top:80px;
+            background:#e8e6d9;
+            padding:20px;
+            text-align: center;
+            border-radius: 40px 40px 0 0;
+        }
+        .urlWrap .textarBox span {
+            padding: 0 20px;
+            font-weight: 500;
+            font-size:14px;
+        }
+        .urlWrap .textarBox input {
+            width:65%;
+            height:36px;
+            line-height:36px;
+            border:2px solid #929292;
+            border-radius: 20px 0 0 20px;
+            padding-left:20px;
+            vertical-align: middle;
+        }
+        .urlWrap .textarBox a {
+            display: inline-block;
+            text-align: center;
+            color:#fff;
+            background: #000;
+            margin-left:0;
+            height:36px;
+            line-height: 36px;
+            border-radius: 0 20px 20px 0;
+            vertical-align: middle;
+        }
+        .urlWrap .url-List {
+            margin-top: 1px;
+        }
+        .urlWrap .url-List table {
+            border-top:0;
+        }
+        .urlWrap .url-List tr {
+            border-bottom:1px solid #e8e6d9;
+        }
+        .urlWrap .url-List thead th {
+            background:#e8e6d9;
+            font-weight: bold;
+        }
+        .urlWrap .url-List thead th,
+        .urlWrap .url-List td {
+            padding:10px;
+            text-align: center;
+        }
+        .urlWrap .url-List td:last-child {
+            text-align: left;
+        }
+        .urlWrap .url-List td a {
+            float: right;
+            font-size: 12px;
+            border:1px solid #eee;
+            width: 18px;
+            height: 18px;
+            line-height: 18px;
+            text-align: center;
+            color:#ccc;
+            font-family: "Helvetica", "Apple SD Gothic Neo", "sans-serif";
+        }
+        .urlWrap .url-List td a:hover {
+            border:1px solid #000;
+            color:#000;
+        }
+    </style>
 
-    <div class="url-List">
-        <table>
-            <col width="15%"/>
-            <col/>
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>URL</th>
-            </tr>
-            </thead>
-            <tbody>
-            @if(empty($list) === true)
-                <tr>
-                    <td colspan="2" style="text-align: center;">등록된 내용이 없습니다.</td>
-                </tr>
-            @else
-                @foreach($list as $row)
-                    <tr>
-                        <td>{!! hpSubString($row['MemId'],0,2,'*') !!}</td>
-                        <td>
-                            {{--{!! nl2br($row['Content']) !!}--}}
-                            {!! hpSubString($row['Content'], 0, 10, '*********************') !!}
-                            @if(sess_data('is_login') === true && sess_data('mem_idx') === $row['MemIdx'])
-                                <a class="btn-comment-del" data-comment-idx="{{$row['Idx']}}" href="#none">X</a>
-                            @endif
-                        </td>
-                    </tr>
-                @endforeach
+    <div class="urlWrap" id="url">
+        <form id="regi_form_comment" name="regi_form_comment" method="POST" onsubmit="return false;" novalidate>
+            {!! csrf_field() !!}
+            {!! method_field($method) !!}
+            @foreach($arr_base['set_params '] as $key => $val)
+                <input type="hidden" name="{{$key}}" value="{{$val}}"/>
+            @endforeach
+
+            @if((config_app('SiteCode') == '2001' || config_app('SiteCode') == '2002') && $arr_input['bottom_cafe_type'] != 'N')
+                {{-- 경찰 사이트일 경우만 적용 --}}
+                <ul class="snslink">
+                    <li><a href="http://cafe.daum.net/policeacademy" target="_blank" ><img src="https://static.willbes.net/public/images/promotion/common/snsline01.png"alt="다음카페 경사모" /></a></li>
+                    <li><a href="http://cafe.naver.com/polstudy" target="_blank" ><img src="https://static.willbes.net/public/images/promotion/common/snsline02.png" alt="네이버카페 경꿈사" /></a></li>
+                    <li><a href="https://gall.dcinside.com/board/lists/?id=government" target="_blank" ><img src="https://static.willbes.net/public/images/promotion/common/snsline03.png" alt="디시 공무원 갤러리" /></a></li>
+                    <li><a href="https://gall.dcinside.com/mgallery/board/lists/?id=policeofficer" target="_blank"><img src="https://static.willbes.net/public/images/promotion/common/snsline04.png" alt="디시 순경마이너 갤러리" /></a></li>
+                </ul>
             @endif
-            </tbody>
-        </table>
-    </div>  
-    <div class="mt30">
-        {!! $paging['pagination'] !!}
+
+            @if((config_app('SiteCode') == '2003' || config_app('SiteCode') == '2004') && $arr_input['bottom_cafe_type'] != 'N')
+                {{-- 공무원 사이트일 경우만 적용 --}}
+                <ul class="snslink">
+                    <li><a href="http://cafe.daum.net/9glade" target="_blank" ><img src="https://static.willbes.net/public/images/promotion/common/pass_snsline01.jpg"alt="다음카페 구꿈사" /></a></li>
+                    <li><a href="https://cafe.naver.com/gugrade" target="_blank" ><img src="https://static.willbes.net/public/images/promotion/common/pass_snsline02.jpg" alt="네이버카페 공드림" /></a></li>
+                    <li><a href="https://gall.dcinside.com/board/lists?id=government" target="_blank" ><img src="https://static.willbes.net/public/images/promotion/common/pass_snsline03.jpg" alt="디시 공무원 갤러리" /></a></li>
+                </ul>
+            @endif
+
+            <div class="textarBox NSK">
+                <span>홍보 URL 남기기</span>
+                <input type="text" name="event_comment" id="event_comment" placeholder="홍보 URL 남기기.">
+                <a href="#none" class="btnrwt" id="btn_submit_comment"><span class="ir">이벤트 참여하기</span></a>
+            </div>
+        </form>
+
+        <div class="url-List">
+            <table>
+                <col width="15%"/>
+                <col/>
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>URL</th>
+                </tr>
+                </thead>
+                <tbody>
+                @if(empty($list) === true)
+                    <tr>
+                        <td colspan="2" style="text-align: center;">등록된 내용이 없습니다.</td>
+                    </tr>
+                @else
+                    @foreach($list as $row)
+                        <tr>
+                            <td>{!! hpSubString($row['MemId'],0,2,'*') !!}</td>
+                            <td>
+                                {{--{!! nl2br($row['Content']) !!}--}}
+                                {!! hpSubString($row['Content'], 0, 10, '*********************') !!}
+                                @if(sess_data('is_login') === true && sess_data('mem_idx') === $row['MemIdx'])
+                                    <a class="btn-comment-del" data-comment-idx="{{$row['Idx']}}" href="#none">X</a>
+                                @endif
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif
+                </tbody>
+            </table>
+        </div>
+        <div class="mt30">
+            {!! $paging['pagination'] !!}
+        </div>
     </div>
-</div>
+@endif
 
 <script type="text/javascript">
     var $regi_form_comment = $('#regi_form_comment');
--- views/willbes/pc/promotion/show_comment_list_url_partial.blade.php
+++ views/willbes/pc/promotion/show_comment_list_url_partial.blade.php
@@ -6,7 +6,15 @@
     var first_height = '';
     function resizeIframe713002(iframe) {
         if (first_height == '') {
-            iframe.height = (iframe.contentWindow.document.body.scrollHeight + 250) + "px";
+            @if($__cfg['SiteGroupCode'] == '1011') {{-- 임용 --}}
+                iframe.height = (iframe.contentWindow.document.body.scrollHeight + 350) + "px";
+                if(iframe.contentWindow.document.body.scrollHeight > 450){
+                    iframe.height = "544px";    //강제초기화
+                }
+            @else
+                iframe.height = (iframe.contentWindow.document.body.scrollHeight + 250) + "px";
+            @endif
+
             if (iframe.contentWindow.document.body.scrollHeight <= 100) {
                 iframe.height = '633px';    //강제초기화
             }

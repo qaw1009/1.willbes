2e351785a8aca7682325c725be6e03f3d6698009
최현탁 <cht@willbes.com>
2021-05-18 13:43:59 +0900

    프론트 고등고시, 자격증 메인 페이지 수정

--- views/willbes/pc/site/main_partial/map_2011.blade.php
+++ views/willbes/pc/site/main_partial/map_2011.blade.php
@@ -45,48 +45,18 @@
         margin: -36px 0 0;
     }
 </style>
-<script type="text/javascript">
-    {{--
-        카테고리별로 디폴트 탭이 달라지는데 공통스크립트 작동순서를 제어할수 없음.
-        그래서 tabWrap 클래스 이벤트 바인딩이 아닌 tabWrapCustom 추가해서 사용.
-    --}}
-    $(function() {
-        $('ul.tabWrapCustom').each(function () {
-            var $active, $content, $links = $(this).find('a');
-            $active = $($links.filter('[href="' + location.hash + '"]')[0] || $links[0]);
-            $content = $($active[0].hash);
-            $links.not($active).each(function () {
-                $(this.hash).hide().css('display', 'none');
-            });
-            $(this).on('click', 'a', function (e) {
-                $active.removeClass('on');
-                $content.hide().css('display', 'none');
-                $active = $(this);
-                $content = $(this.hash);
-                $active.addClass('on');
-                $content.show().css('display', 'block');
-                e.preventDefault();
-            });
-        });
-        @if(empty($__cfg['CateCode']) === false && $__cfg['CateCode'] == '309004')
-            $('#campus2_btn').click();
-        @else
-            $('#campus1_btn').click();
-        @endif
-    });
-</script>
 <div class="Section Section4_hl mb50">
     <div class="widthAuto">
         <div class="will-acadTit">윌비스 <span class="tx-color">자격증</span> 학원</div>
         <div class="noticeTabs campus c_both">
-            {{-- <ul class="tabWrap noticeWrap_campus"> --}}
             <ul class="tabWrapCustom noticeWrap_campus">
-                <li><a id="campus1_btn" href="#campus1" class="on">신림(본원)</a><span class="row-line">|</span></li>
-                <li><a id="campus2_btn" href="#campus2" class="">강남(분원)</a></li>
+                <li><a id="campus1_btn" href="#campus1" data-map-id="0" class="on">신림(본원)</a><span class="row-line">|</span></li>
+                <li><a id="campus2_btn" href="#campus2" data-map-id="1" class="">강남(분원)</a></li>
             </ul>
             <div class="tabBox noticeBox_campus">
                 <div id="campus1" class="tabContent">
-                    <div class="map_img">
+                    <div class="map_img" id="map_0"></div>
+                    <div class="map_img" id="alterMap_0" style="display: none;">
                         <img src="https://static.willbes.net/public/images/promotion/main/2010_map01.jpg" alt="신림(본원)">
                         <span class="origin">신림(본원)</span>
                     </div>
@@ -117,7 +87,8 @@
                 <!-- 신림동 //-->
 
                 <div id="campus2" class="tabContent">
-                    <div class="map_img">
+                    <div class="map_img" id="map_1"></div>
+                    <div class="map_img" id="alterMap_1" style="display: none;">
                         <img src="https://static.willbes.net/public/images/promotion/main/2010_map02.jpg" alt="강남(분원)">
                         <span>강남(분원)</span>
                     </div>
@@ -150,3 +121,64 @@
         </div>
     </div>
 </div>
+
+<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey={{ config_item('kakao_js_app_key') }}&libraries=services"></script>
+<script type="text/javascript" src="/public/js/map_util.js?ver={{time()}}"></script>
+<script type="text/javascript">
+    const map_info = [[],[]];
+    map_info[0] = {
+        'addr' : '서울 관악구 신림로 23길 16'
+        ,'level' : '3'
+        ,'info_txt' : '<div style="padding:5px; 5px; background:#fff; border: 1px solid midnightblue">욀비스<strong class="tx-color">한림법학원</strong>학원 [신림(본원)]</div>'
+        ,'x_anchor' : '0.5'
+        ,'y_anchor' : '2.7'
+    };
+    map_info[1] = {
+        'addr':'서울 강남구 테헤란로19길 18'
+        ,'level' : '3'
+        ,'info_txt' : '<div style="padding:5px; 5px; background:#fff; border: 1px solid midnightblue">욀비스<strong class="tx-color">한림법학원</strong>학원 [강남(분원)]</div>'
+        ,'x_anchor' : '0.5'
+        ,'y_anchor' : '2.7'
+    };
+
+    {{--
+        카테고리별로 디폴트 탭이 달라지는데 공통스크립트 작동순서를 제어할수 없음.
+        그래서 tabWrap 클래스 이벤트 바인딩이 아닌 tabWrapCustom 추가해서 사용.
+    --}}
+    $(document).ready(function() {
+        var $kakaomap = new kakaoMap();
+
+        $('ul.tabWrapCustom').each(function () {
+            var $active, $content, $links = $(this).find('a');
+            $active = $($links.filter('[href="' + location.hash + '"]')[0] || $links[0]);
+            $content = $($active[0].hash);
+            $links.not($active).each(function () {
+                $(this.hash).hide().css('display', 'none');
+            });
+            $(this).on('click', 'a', function (e) {
+                $active.removeClass('on');
+                $content.hide().css('display', 'none');
+                $active = $(this);
+                $content = $(this.hash);
+                $active.addClass('on');
+                $content.show().css('display', 'block');
+                e.preventDefault();
+
+                var map_id = $(this).data('map-id');
+                $kakaomap.config.ele_id = 'map_'+map_id;
+                $kakaomap.config.alter_id = 'alterMap_'+map_id;
+                $kakaomap.config.level = map_info[map_id]['level'];
+                $kakaomap.config.addr = map_info[map_id]['addr'];
+                $kakaomap.config.info_txt = map_info[map_id]['info_txt'];
+                $kakaomap.config.info_txt_x_anchor = map_info[map_id]['x_anchor'];
+                $kakaomap.config.info_txt_y_anchor = map_info[map_id]['y_anchor'];
+                $kakaomap.run();
+            });
+        });
+        @if(empty($__cfg['CateCode']) === false && ($__cfg['CateCode'] == '3098' || $__cfg['CateCode'] == '3099'))
+            $('#campus2_btn').click();
+        @else
+            $('#campus1_btn').click();
+        @endif
+    });
+</script>

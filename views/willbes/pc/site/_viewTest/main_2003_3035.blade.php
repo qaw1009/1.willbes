5c1fcb880febe599ff30ca199db0739dbe06a9df
최현탁 <cht@willbes.com>
2021-05-25 10:29:17 +0900

    [테스트제외] 프론트 공무원-법원직 메인 테스트 페이지 수정

--- views/willbes/pc/site/_viewTest/main_2003_3035.blade.php
+++ views/willbes/pc/site/_viewTest/main_2003_3035.blade.php
@@ -142,42 +142,4 @@
         </div>
     </div>
     {!! popup('657001', $__cfg['SiteCode'], $__cfg['CateCode']) !!}
-
-    <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey={{ config_item('kakao_js_app_key') }}&libraries=services"></script>
-    <script type="text/javascript" src="/public/js/map_util.js?ver={{time()}}"></script>
-    <script type="text/javascript">
-        $(document).ready(function() {
-            var info_txt = '<div style="padding:5px; 5px; background:#fff; border: 1px solid midnightblue">윌비스 <strong class="tx-color">한림법학원</strong><br>김동진법원팀(7~9층)</div>';
-            var $kakaomap = new kakaoMap();
-            $kakaomap.config.ele_id = 'map';
-            $kakaomap.config.alter_id = 'alterMap1';
-            $kakaomap.config.level = 4;
-            $kakaomap.config.addr = '서울 동작구 노량진로 196';
-            $kakaomap.config.info_txt = info_txt;
-            $kakaomap.config.info_txt_x_anchor = 0.5;
-            $kakaomap.config.info_txt_y_anchor = 2.7;
-            $kakaomap.run();
-
-            $('.PBtab').each(function(){
-                var $active, $content, $links = $(this).find('a');
-                $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
-                $active.addClass('active');
-                $content = $($active[0].hash);
-                $links.not($active).each(function () {
-                    $(this.hash).hide()
-                });
-
-                // Bind the click event handler
-                $(this).on('click', 'a', function(e){
-                    $active.removeClass('active');
-                    $content.hide();
-                    $active = $(this);
-                    $content = $(this.hash);
-                    $active.addClass('active');
-                    $content.show();
-                    e.preventDefault()
-                })
-            });
-        });
-    </script>
 @stop
--- views/willbes/pc/site/_viewTest/main_partial/map_2003.blade.php
+++ views/willbes/pc/site/_viewTest/main_partial/map_2003.blade.php
@@ -1,50 +1,3 @@
-<style>
-    .tabWrapCustom.noticeWrap_campus {
-        height: 60px;
-        border-bottom: none;
-        border-top: 2px solid #000;
-        clear: both;
-    }
-    .tabWrapCustom.noticeWrap_campus li {
-        float: left;
-        width: auto;
-        height: 60px;
-        margin: 0 10px;
-    }
-    .tabWrapCustom.noticeWrap_campus li a {
-        display: block;
-        width: 100%;
-        height: 60px;
-        line-height: 60px;
-        background: none;
-        font-size: 13px;
-        color: #3a3a3a;
-        text-align: center;
-        letter-spacing: 0;
-        border: none;
-        padding-right: 20px;
-    }
-    .tabWrapCustom.noticeWrap_campus li:first-child a {
-        border-left: none;
-    }
-    .tabWrapCustom.noticeWrap_campus li a.on {
-        position: relative;
-        z-index: 2;
-        background: none;
-        height: 60px;
-        line-height: 60px;
-        font-weight: 600;
-        color: #3a3a3a;
-        border: none;
-    }
-    .tabWrapCustom.noticeWrap_campus .row-line {
-        float: right;
-        background: #b7b7b7;
-        width: 1px;
-        height: 12px;
-        margin: -36px 0 0;
-    }
-</style>
 <div class="Section Section4_hl mb50">
     <div class="widthAuto">
         <div class="will-acadTit">윌비스 한림법학원 <span class="tx-color">김동진 법원팀</span></div>
@@ -79,64 +32,19 @@
         </div>
     </div>
 </div>
-
 <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey={{ config_item('kakao_js_app_key') }}&libraries=services"></script>
 <script type="text/javascript" src="/public/js/map_util.js?ver={{time()}}"></script>
 <script type="text/javascript">
-    const map_info = [[],[]];
-    map_info[0] = {
-        'addr' : '서울 관악구 신림로 23길 16'
-        ,'level' : '3'
-        ,'info_txt' : '<div style="padding:5px; 5px; background:#fff; border: 1px solid midnightblue">욀비스<strong class="tx-color">한림법학원</strong> [신림(본원)]</div>'
-        ,'x_anchor' : '0.5'
-        ,'y_anchor' : '2.7'
-    };
-    map_info[1] = {
-        'addr':'서울 강남구 테헤란로19길 18'
-        ,'level' : '3'
-        ,'info_txt' : '<div style="padding:5px; 5px; background:#fff; border: 1px solid midnightblue">욀비스<strong class="tx-color">한림법학원</strong> [강남(분원)]</div>'
-        ,'x_anchor' : '0.5'
-        ,'y_anchor' : '2.7'
-    };
-
-    {{--
-        카테고리별로 디폴트 탭이 달라지는데 공통스크립트 작동순서를 제어할수 없음.
-        그래서 tabWrap 클래스 이벤트 바인딩이 아닌 tabWrapCustom 추가해서 사용.
-    --}}
     $(document).ready(function() {
+        var info_txt = '<div style="padding:5px; 5px; background:#fff; border: 1px solid midnightblue">윌비스 <strong class="tx-color">한림법학원</strong><br>김동진법원팀(7~9층)</div>';
         var $kakaomap = new kakaoMap();
-
-        $('ul.tabWrapCustom').each(function () {
-            var $active, $content, $links = $(this).find('a');
-            $active = $($links.filter('[href="' + location.hash + '"]')[0] || $links[0]);
-            $content = $($active[0].hash);
-            $links.not($active).each(function () {
-                $(this.hash).hide().css('display', 'none');
-            });
-            $(this).on('click', 'a', function (e) {
-                $active.removeClass('on');
-                $content.hide().css('display', 'none');
-                $active = $(this);
-                $content = $(this.hash);
-                $active.addClass('on');
-                $content.show().css('display', 'block');
-                e.preventDefault();
-
-                var map_id = $(this).data('map-id');
-                $kakaomap.config.ele_id = 'map_'+map_id;
-                $kakaomap.config.alter_id = 'alterMap_'+map_id;
-                $kakaomap.config.level = map_info[map_id]['level'];
-                $kakaomap.config.addr = map_info[map_id]['addr'];
-                $kakaomap.config.info_txt = map_info[map_id]['info_txt'];
-                $kakaomap.config.info_txt_x_anchor = map_info[map_id]['x_anchor'];
-                $kakaomap.config.info_txt_y_anchor = map_info[map_id]['y_anchor'];
-                $kakaomap.run();
-            });
-        });
-        @if(empty($__cfg['CateCode']) === false && ($__cfg['CateCode'] == '3098' || $__cfg['CateCode'] == '3099' || $__cfg['CateCode'] == '309004'))
-            $('#campus2_btn').click();
-        @else
-            $('#campus1_btn').click();
-        @endif
+        $kakaomap.config.ele_id = 'map';
+        $kakaomap.config.alter_id = 'alterMap1';
+        $kakaomap.config.level = 4;
+        $kakaomap.config.addr = '서울 동작구 노량진로 196';
+        $kakaomap.config.info_txt = info_txt;
+        $kakaomap.config.info_txt_x_anchor = 0.5;
+        $kakaomap.config.info_txt_y_anchor = 2.2;
+        $kakaomap.run();
     });
 </script>

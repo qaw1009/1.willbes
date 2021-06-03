2a6654fb03f38606622b8a00efb0361eede4606c
김창용 <winmir@willbes.com>
2021-06-03 11:55:38 +0900

    임용 공지용 강의일정 수정

--- views/html/ssam_lecTable.blade.php
+++ views/html/ssam_lecTable.blade.php
@@ -42,8 +42,8 @@
                         <col width="70px" />
                         <col width="60px" />
                         <col />
-                        <col width="150px" />
-                        <col width="235px" />
+                        <col width="140px" />
+                        <col width="245px" />
                         <col width="65px" />
                         <col width="65px" />
                     </colgroup>
@@ -63,8 +63,8 @@
                     </thead>
                     <tbody>
                         <tr>
-                            <td rowspan="3">교육학</td>
-                            <td rowspan="3">이인재</td>
+                            <td rowspan="3" class="strong">교육학</td>
+                            <td rowspan="3" class="strong">이인재</td>
                             <td class="tx-left">(07~08월) 이인재 교육학 기출분석 및 이론반
                                 <br><span class="red">※ 수강시 온/오프 동시 수강 혜택</span></td>
                             <td>07.05.(월)~08.24.(화)<br />
@@ -86,8 +86,8 @@
                             <td><a href="https://ssam.willbes.net/pass/offPackage/show/prod-code/178331" target="_blank" class="btnSt02">신청하기</a></td>
                         </tr>
                         <tr>
-                            <td>유  아</td>
-                            <td>민정선</td>
+                            <td class="strong">유  아</td>
+                            <td class="strong">민정선</td>
                             <td class="tx-left">2020년 (01~02월) 유아교육개론 </td>
                             <td>2020년 진행강의</td>
                             <td>330,000 ▶ <strong> 198,000 <span class="red">(40%↓)</span></strong></td>
@@ -95,8 +95,8 @@
                             <td></td>
                         </tr>
                         <tr>
-                            <td rowspan="3">초  등</td>
-                            <td rowspan="3">배재민</td>
+                            <td rowspan="3" class="strong">초  등</td>
+                            <td rowspan="3" class="strong">배재민</td>
                             <td class="tx-left">(06~08월) 배재민 초등 통합서브노트반</td>
                             <td>06.13.(일)~08.29.(일)<br />
                             매주[일] 13:00~19:00</td>
@@ -117,8 +117,8 @@
                             <td><a href="https://ssam.willbes.net/pass/offPackage/show/prod-code/182497" target="_blank" class="btnSt02">신청하기</a></td>
                         </tr>
                         <tr>
-                            <td rowspan="15">국  어</td>
-                            <td rowspan="6">송원영</td>
+                            <td rowspan="15" class="strong">국  어</td>
+                            <td rowspan="6" class="strong">송원영</td>
                             <td class="tx-left">(07~08월) 송원영 국어교육론 문제풀이를 통한 이론정리반</td>
                             <td>07.01.(목)~08.19.(목)<br />
                             매주[목] 10:00~18:30</td>
@@ -162,7 +162,7 @@
                             <td><a href="https://ssam.willbes.net/pass/offPackage/show/prod-code/182465" target="_blank" class="btnSt02">신청하기</a></td>
                         </tr>
                         <tr>
-                            <td rowspan="9">권보민</td>
+                            <td rowspan="9" class="strong">권보민</td>
                             <td class="tx-left">(07~08월) 권보민 국어문법 미니모의고사+적중예상 300제+핵심 주제문법 1</td>
                             <td>07.03.(토)~08.21.(토)<br />
                             매주[토] 10:30~18:00</td>
@@ -229,8 +229,8 @@
                             <td><a href="https://ssam.willbes.net/pass/offPackage/show/prod-code/182465" target="_blank" class="btnSt02">신청하기</a></td>
                         </tr>
                         <tr>
-                            <td rowspan="6">영  어</td>
-                            <td rowspan="3">김유석</td>
+                            <td rowspan="6" class="strong">영  어</td>
+                            <td rowspan="3" class="strong">김유석</td>
                             <td class="tx-left">(07~08월) 김유석 일반영어/영미문학 상위권 문제풀이</td>
                             <td>07.09.(금)~08.27.(금)<br />
                             매주[금] 09:10~13:00</td>
@@ -251,7 +251,7 @@
                             <td><a href="#none" onclick="javascript:alert('6월18일부터 접수');" class="btnSt02">신청하기</a></td>
                         </tr>
                         <tr>
-                            <td rowspan="3">김영문</td>
+                            <td rowspan="3" class="strong">김영문</td>
                             <td class="tx-left">(07~08월) 김영문 영어학 영역별 모의고사 및 기출문제풀이</td>
                             <td>07.01(목)~08.26.(목)<br />
                             매주[목] 10:00~13:00</td>
@@ -273,8 +273,8 @@
                             <td><a href="https://ssam.willbes.net/pass/offPackage/show/prod-code/182455" target="_blank" class="btnSt02">신청하기</a></td>
                         </tr>
                         <tr>
-                            <td rowspan="10">수  학</td>
-                            <td rowspan="4">김철홍</td>
+                            <td rowspan="10" class="strong">수  학</td>
+                            <td rowspan="4" class="strong">김철홍</td>
                             <td class="tx-left">(07월) 김철홍 선형대수학 완전학습반 (이론 및 문제풀이 통합반) </td>
                             <td>07.04.(일)~07.25.(일)<br />
                             매주[일] 11:00~18:00</td>
@@ -283,7 +283,7 @@
                             <td><a href="https://ssam.willbes.net/pass/offLecture/show/cate/3141/prod-code/176956" target="_blank" class="btnSt02">신청하기</a></td>
                         </tr>
                         <tr>
-                            <td class="tx-left">(08월) 김철홍 복소해석학 완전학습반 (이론    및 문제풀이 통합반)</span></strong></td>
+                            <td class="tx-left">(08월) 김철홍 복소해석학 완전학습반 (이론 및 문제풀이 통합반)</span></strong></td>
                             <td>08.01.(일)~08.29.(일)<br />
                             매주[일] 11:00~18:00</td>
                             <td>220,000 ▶ <strong> 198,000 <span class="red">(10%↓)</span></strong></td>
@@ -303,7 +303,7 @@
                             <td><a href="https://ssam.willbes.net/pass/offPackage/show/prod-code/176972" target="_blank" class="btnSt02">신청하기</a></td>
                         </tr>
                         <tr>
-                            <td rowspan="6">박태영</td>
+                            <td rowspan="6" class="strong">박태영</td>
                             <td class="tx-left">(07~08월) 박태영 수학교육론 주제별 모의고사반 (최종 노트 완성)</span></strong></td>
                             <td>07.07.(수)~08.25.(수)<br />
                             매주[수] 10:00~14:00</td>
@@ -327,13 +327,13 @@
                         </tr>
                         <tr>
                             <td colspan="2" class="tx-left"> 2021년(07~11월) 박태영 수학교육론 하반기  문풀·모고 패키지 </td>
-                            <td>530,000 ▶ <strong> 370,000    <span class="red">(약30%↓)</span></strong></td>
+                            <td>530,000 ▶ <strong> 370,000 <span class="red">(약30%↓)</span></strong></td>
                             <td><a href="https://ssam.willbes.net/package/show/cate/3137/pack/648001/prod-code/176963" target="_blank" class="btnSt01">신청하기</a></td>
                             <td><a href="https://ssam.willbes.net/pass/offPackage/show/prod-code/176975" target="_blank" class="btnSt02">신청하기</a></td>
                         </tr>
                         <tr>
                             <td colspan="2" class="tx-left"> 2021년 수학교육론 정규특강 패키지[상반기 복습특강+하반기특강]</td>
-                            <td>100,000 ▶ <strong> 70,000    <span class="red">(30%↓)</span></strong></td>
+                            <td>100,000 ▶ <strong> 70,000 <span class="red">(30%↓)</span></strong></td>
                             <td><a href=https://ssam.willbes.net/package/show/cate/3137/pack/648001/prod-code/176964none" target="_blank" class="btnSt01">신청하기</a></td>
                             <td><a href="https://ssam.willbes.net/pass/offPackage/show/prod-code/176976" target="_blank" class="btnSt02">신청하기</a></td>
                         </tr>
@@ -344,8 +344,8 @@
                             <td><a href="https://ssam.willbes.net/pass/offPackage/show/prod-code/176972" target="_blank" class="btnSt02">신청하기</a></td>
                         </tr>
                         <tr>
-                            <td rowspan="3">생  물</td>
-                            <td>강치욱</td>
+                            <td rowspan="3" class="strong">생  물</td>
+                            <td class="strong">강치욱</td>
                             <td class="tx-left">(05~08월) 강치욱 생물내용학 기출&amp;예제문제 풀이<br />
                             *온라인 강의는 [강치욱 과학연구소]에서 수강</td>
                             <td>05.08.(토)~08.21.(토)<br />
@@ -355,7 +355,7 @@
                             <td><a href="https://ssam.willbes.net/pass/offLecture/show/cate/3141/prod-code/177925" target="_blank" class="btnSt02">신청하기</a></td>
                         </tr>
                         <tr>
-                            <td rowspan="2">양혜정</td>
+                            <td rowspan="2" class="strong">양혜정</td>
                             <td class="tx-left">(06~08월) 양혜정 생물교육론 기출&amp;예상 문제풀이 </td>
                             <td>06.02.(수)~08.18.(수)<br />
                             매주[수] 10:00~16:00</td>
@@ -370,8 +370,8 @@
                             <td><a href="https://ssam.willbes.net/pass/offPackage/show/prod-code/177776" target="_blank" class="btnSt02">신청하기</a></td>
                         </tr>
                         <tr>
-                            <td rowspan="3">도덕윤리</td>
-                            <td rowspan="3">김병찬</td>
+                            <td rowspan="3" class="strong">도덕윤리</td>
+                            <td rowspan="3" class="strong">김병찬</td>
                             <td class="tx-left">(07~08월) 김병찬 도덕윤리 영역별/주제별 문제풀이 </td>
                             <td>07.01.(목)~08.26.(목)<br />
                             매주[금] 10:00~18:00</td>
@@ -394,8 +394,8 @@
                             <td><a href="https://ssam.willbes.net/pass/offPackage/show/prod-code/176759" target="_blank" class="btnSt02">신청하기</a></td>
                         </tr>
                         <tr>
-                            <td rowspan="3">역  사</td>
-                            <td rowspan="3">최용림</td>
+                            <td rowspan="3" class="strong">역  사</td>
+                            <td rowspan="3" class="strong">최용림</td>
                             <td class="tx-left">(07~08월) 최용림 임용역사 영역별/단원별 문제풀이 </td>
                             <td>07.07.(수)~08.26.(목)<br />
                             매주[수/목] 10:00~18:00</td>
@@ -416,8 +416,8 @@
                             <td><a href="https://ssam.willbes.net/pass/offPackage/show/prod-code/182498" target="_blank" class="btnSt02">신청하기</a></td>
                         </tr>
                         <tr>
-                            <td rowspan="4">음  악</td>
-                            <td rowspan="4">다이애나</td>
+                            <td rowspan="4" class="strong">음  악</td>
+                            <td rowspan="4" class="strong">다이애나</td>
                             <td class="tx-left">(07~08월) 다이애나 전공음악 영역별 문제풀이 </td>
                             <td></td>
                             <td>&nbsp;</td>
@@ -445,10 +445,10 @@
                             <td>준비중</td>
                         </tr>
                         <tr>
-                            <td rowspan="7">전기<br />
+                            <td rowspan="7" class="strong">전기<br />
                             전자<br />
                             통신</td>
-                            <td rowspan="7">최우영</td>
+                            <td rowspan="7" class="strong">최우영</td>
                             <td class="tx-left">(07~09월) [전기·전자·통신 통합] 영역별 문제풀이 </td>
                             <td>07.05.(월)~09.29.(수)<br />
                             매주[월] 14:00~<br />
@@ -494,8 +494,8 @@
                             <td><a href="https://ssam.willbes.net/pass/offPackage/show/prod-code/182656" target="_blank" class="btnSt02">신청하기</a></td>
                         </tr>
                         <tr>
-                            <td rowspan="5">정보컴퓨터</td>
-                            <td rowspan="5">송광진</td>
+                            <td rowspan="5" class="strong">정보컴퓨터</td>
+                            <td rowspan="5" class="strong">송광진</td>
                             <td class="tx-left">(07~09월) 송광진 정보컴퓨터 적중문제풀이반 </td>
                             <td>07.04(일)~09.19.(일)<br />
                             매주[일] 13:00~20:00</td>
@@ -511,7 +511,7 @@
                             <td></td>
                         </tr>
                         <tr>
-                            <td colspan="2" class="tx-left"> 2021년(01~11월) 송광진 정보컴퓨터 내용학 All-In-One    패키지(C언어 포함)</td>
+                            <td colspan="2" class="tx-left"> 2021년(01~11월) 송광진 정보컴퓨터 내용학 All-In-One 패키지(C언어 포함)</td>
                             <td>2,800,000 ▶ <strong> 2,100,000 <span class="red">(25%↓)</span></strong></td>
                             <td><a href="https://ssam.willbes.net/package/show/cate/3137/pack/648001/prod-code/176005" target="_blank" class="btnSt01">신청하기</a></td>
                             <td></td>
@@ -524,14 +524,14 @@
                         </tr>
                         <tr>
                             <td colspan="2" class="tx-left"> 2021년(07~11) 송광진 정보컴퓨터 내용학 하반기 문풀·모고 패키지</td>
-                            <td>1,000,000 ▶ <strong>    800,000 <span class="red">(20%↓)<br />
+                            <td>1,000,000 ▶ <strong> 800,000 <span class="red">(20%↓)
                             <br>※ 06.30.(수) 17시까지 결제완료시, 할인적용</td>
                             <td><a href="https://ssam.willbes.net/package/show/cate/3137/pack/648001/prod-code/182499" target="_blank" class="btnSt01">신청하기</a></td>
                             <td><a href="https://ssam.willbes.net/pass/offPackage/show/prod-code/182500" target="_blank" class="btnSt02">신청하기</a></td>
                         </tr>
                         <tr>
-                            <td rowspan="3">정컴교육론</td>
-                            <td rowspan="3">장순선</td>
+                            <td rowspan="3" class="strong">정컴교육론</td>
+                            <td rowspan="3" class="strong">장순선</td>
                             <td class="tx-left">(06월) 정보컴퓨터 교육론 심화과정</td>
                             <td>06.05.(토)~06.26.(토)<br />
                             매주[토] 13:00~17:00</td>
@@ -554,8 +554,8 @@
                             <td></td>
                         </tr>
                         <tr>
-                            <td rowspan="4">중국어</td>
-                            <td rowspan="4">정경미</td>
+                            <td rowspan="4" class="strong">중국어</td>
+                            <td rowspan="4" class="strong">정경미</td>
                             <td class="tx-left">(07~08월) 정경미 전공중국어 문제풀이+핵심체크반</td>
                             <td>07.03.(토)~08.28.(토)<br />
                             매주[토] 09:30~16:00</td>

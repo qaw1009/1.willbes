710fc949792083c1f2a28c4d7887675d50730932
김창용 <winmir@willbes.com>
2021-06-25 16:53:41 +0900

    공무원 라이브 모드 프로모션 수정

--- views/willbes/m/promotion/2003/1902.blade.php
+++ views/willbes/m/promotion/2003/1902.blade.php
@@ -6,6 +6,8 @@
     .evtCtnsBox {width:100%; max-width:720px; margin:0 auto; text-align:center; position:relative; font-size:14px; line-height:1.5}
     .evtCtnsBox img {width:100%; max-width:720px;}
     .evtTop {position:relative}
+
+    .wrap a:hover {box-shadow:0 0 10px rgba(0,0,0,.5);}
 </style> 
 
 <div id="Container" class="Container NSK c_both">    
@@ -22,32 +24,16 @@
         <img src="https://static.willbes.net/public/images/promotion/2020/11/1902m_02.jpg" alt="" >
     </div> 
 
-    <div class="evtCtnsBox">
-        {{--
-        <img src="https://static.willbes.net/public/images/promotion/2021/04/1902m_03.jpg" alt="" usemap="#Map1902m_apply" border="0" >
-        <map name="Map1902m_apply" id="Map1902m_apply">
-            <area shape="rect" coords="240,782,456,844" href="https://pass.willbes.net/m/pass/offPackage/index?cate_code=3043&campus_ccd=605001&search_text=UHJvZE5hbWU6bGl2ZQ%3D%3D" target="blank" />
-            <area shape="rect" coords="240,1192,456,1254" href="https://pass.willbes.net/m/pass/offPackage/index?cate_code=3050&campus_ccd=605001&search_text=UHJvZE5hbWU6bGl2ZQ%3D%3D" target="blank" />
-            <area shape="rect" coords="240,1538,456,1600" href="https://pass.willbes.net/m/pass/offPackage/index?cate_code=3048&campus_ccd=605001&search_text=UHJvZE5hbWU6bGl2ZQ%3D%3D" target="blank" />
-            <area shape="rect" coords="458,1706,675,1768" href="https://pass.willbes.net/m/pass/offLecture/index?cate_code=3043&campus_ccd=605001&search_text=UHJvZE5hbWU6bGl2ZQ%3D%3D" target="blank" />
-        </map>
-        --}}
+    <div class="evtCtnsBox wrap">
+        <img src="https://static.willbes.net/public/images/promotion/2021/04/1902m_03.jpg" alt="" >
+        <a href="https://pass.willbes.net/m/pass/offPackage/index?cate_code=3043&campus_ccd=605001&search_text=UHJvZE5hbWU6TElWRQ%3D%3D" target="blank" title="9급" style="position: absolute; left: 32.08%; top: 40.46%; width: 32.22%; height: 4.03%; z-index: 2;"></a>
+        <a href="https://pass.willbes.net/m/pass/offPackage/index?cate_code=3050&campus_ccd=605001&search_text=UHJvZE5hbWU6TElWRQ%3D%3D" target="blank" title="불꽃소방" style="position: absolute; left: 32.08%; top: 61.94%; width: 32.22%; height: 4.03%; z-index: 2;"></a>
+        <a href="https://pass.willbes.net/m/pass/offPackage/index?cate_code=3048&campus_ccd=605001&search_text=UHJvZE5hbWU6TElWRQ%3D%3D" target="blank" title="군무원" style="position: absolute; left: 32.08%; top: 80.03%; width: 32.22%; height: 4.03%; z-index: 2;"></a>
+        <a href="https://pass.willbes.net/m/pass/offLecture/index?cate_code=3043&campus_ccd=605001&search_text=UHJvZE5hbWU6bGl2ZQ%3D%3D" target="blank" title="단과" style="position: absolute; left: 62.5%; top: 88.92%; width: 32.22%; height: 4.03%; z-index: 2;"></a>
     </div> 
 
 </div>
 <!-- End Container -->
 
-<script src="/public/vendor/starplayer/js/starplayer_app.js"></script>
-<script type="text/javascript">
-
-</script>
-
-<!-- AceCounter Log Gathering Script V.8.0.AMZ2019080601 -->
-<script language='javascript'>
-    var _AceGID=(function(){var Inf=['gtp14.acecounter.com','8080','AH1A44052179653','AW','0','NaPm,Ncisy','ALL','0']; var _CI=(!_AceGID)?[]:_AceGID.val;var _N=0;var _T=new Image(0,0);if(_CI.join('.').indexOf(Inf[3])<0){ _T.src ="https://"+ Inf[0] +'/?cookie'; _CI.push(Inf);  _N=_CI.length; } return {o: _N,val:_CI}; })();
-    var _AceCounter=(function(){var G=_AceGID;var _sc=document.createElement('script');var _sm=document.getElementsByTagName('script')[0];if(G.o!=0){var _A=G.val[G.o-1];var _G=(_A[0]).substr(0,_A[0].indexOf('.'));var _C=(_A[7]!='0')?(_A[2]):_A[3];var _U=(_A[5]).replace(/\,/g,'_');_sc.src='https:'+'//cr.acecounter.com/Web/AceCounter_'+_C+'.js?gc='+_A[2]+'&py='+_A[4]+'&gd='+_G+'&gp='+_A[1]+'&up='+_U+'&rd='+(new Date().getTime());_sm.parentNode.insertBefore(_sc,_sm);return _sc.src;}})();
-</script>
-<noscript><img src='https://gtp14.acecounter.com:8080/?uid=AH1A44052179653&je=n&' border='0' width='0' height='0' alt=''></noscript>
-<!-- AceCounter Log Gathering Script End -->
 
 @stop

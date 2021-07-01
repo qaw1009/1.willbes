797c1689e7f8d9fe306d57a4eff452820513b2fb
김창용 <winmir@willbes.com>
2021-07-01 17:54:20 +0900

    공무원 한국사 오태진 프로모션 신규

--- views/willbes/pc/promotion/2003/2267.blade.php
+++ views/willbes/pc/promotion/2003/2267.blade.php
@@ -81,13 +81,13 @@
 
         <div class="evtCtnsBox evt_02">
             <div class="wrap"> 
-                <img src="https://static.willbes.net/public/images/promotion/2021/07/2267_02.jpg"  alt="한국사 고득점 완성" />
+                <img src="https://static.willbes.net/public/images/promotion/2021/07/2267_02.jpg"  alt="오태진 한국사 커리큘럼" />
                 <a href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/183420" title="수강신청" target="_blank" style="position: absolute; left: 29.2%; top: 83.41%; width: 30.71%; height: 3.7%; z-index: 2;"></a>
             </div>
         </div>
 
         <div class="evtCtnsBox evt_03 pb100">
-            <img src="https://static.willbes.net/public/images/promotion/2021/06/2264_05.jpg" title="댓글 이벤트"> 
+            <img src="https://static.willbes.net/public/images/promotion/2021/07/2267_03.jpg" title="댓글 이벤트"> 
             {{-- 이모티콘 댓글 --}}
             @if( empty($data['data_option_ccd']) === false && array_key_exists($arr_base['option_ccd']['comment_list'], $data['data_option_ccd']) === true && array_key_exists($arr_base['comment_use_area']['event'], $data['data_comment_use_area']) === true)
                 @include('willbes.pc.promotion.show_comment_list_emoticon2_partial', array('change_emoticon_img' => true))

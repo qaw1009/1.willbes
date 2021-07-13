302d700cee4f175c3e31f7d95e9a5ffd8cd92a11
이윤구 <yoongukk@willbes.com>
2021-07-13 16:39:36 +0900

    (경찰) 21년 경찰2차 풀케어 사전서비스 신규 페이지

---
+++ views/willbes/pc/promotion/2001/2272_cts03.blade.php
@@ -0,0 +1,8 @@
+<style type="text/css">
+     .cts03_top {background:url("https://static.willbes.net/public/images/promotion/2021/07/2272_comingsoon_bg.jpg") no-repeat center top;}
+</style>
+<div>
+    <div class="evtCtnsBox cts03_top">
+        <img src="https://static.willbes.net/public/images/promotion/2021/07/2272_comingsoon.jpg" alt="커밍쑨" />        
+    </div>    
+</div>

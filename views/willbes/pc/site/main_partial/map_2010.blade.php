c7d873e1072d70878ee14869de81556c7fdb330b
김창용 <winmir@willbes.com>
2021-07-21 16:22:03 +0900

    공무원 게이트, 고등고시 메인

--- views/willbes/pc/site/main_partial/map_2010.blade.php
+++ views/willbes/pc/site/main_partial/map_2010.blade.php
@@ -47,7 +47,7 @@
 </style>
 <div class="Section Section4_hl mt50">
     <div class="widthAuto">
-        <div class="will-acadTit">윌비스 <span class="tx-color">고등고시</span> 학원</div>
+        <div class="will-acadTit">윌비스 <span class="tx-color">한림법학원</span></div>
         <div class="noticeTabs campus c_both">
             <ul class="tabWrapCustom noticeWrap_campus">
                 <li><a id="campus1_btn" href="#campus1" data-map-id="0" class="on">신림(본원)</a><span class="row-line">|</span></li>

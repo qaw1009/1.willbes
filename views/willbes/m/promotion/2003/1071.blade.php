a001780a9557786c5829d2606c49060bb7c9be41
김창용 <winmir@willbes.com>
2021-07-29 13:57:11 +0900

    공무원 최우영 프로모션

--- views/willbes/m/promotion/2003/1071.blade.php
+++ views/willbes/m/promotion/2003/1071.blade.php
@@ -7,11 +7,6 @@
     .evtCtnsBox img {width:100%; max-width:720px;}
     .evtCtnsBox a:hover {box-shadow:0 5px 20px rgba(0,0,0,.5); border-radius:6px}
 
-    .evtTop a {position: absolute; width: 31.11%; height: 17.47%; z-index: 2;}
-    .evtTop a.a01 {left: 2.78%; top: 75.6%;}
-    .evtTop a.a02 {left: 34.31%; top: 75.6%;}
-    .evtTop a.a03 {left: 65.97%; top: 75.6%;}
-
     .evt03 a {position: absolute; left: 70.69%; width: 19.03%; height: 2.32%; z-index: 2;}
     .evt03 a.b01 {top: 21.96%;}
     .evt03 a.b02 {top: 25.96%;}
@@ -51,46 +46,35 @@
 
 <div id="Container" class="Container NSK c_both">   
     <div class="evtCtnsBox">
-        <img src="https://static.willbes.net/public/images/promotion/2020/09/1071m_graph.gif" alt="그래프" >
-    </div> 
-
-    <div class="evtCtnsBox evtTop">
-        <img src="https://static.willbes.net/public/images/promotion/2020/09/1071m_top.jpg" alt="통신/전기 최우영" >
-        <a href="https://youtu.be/FYzC6MElEzw?rel=0" target="_blank" class="a01"></a>
-        <a href="https://youtu.be/9dxrpJ6TOZg?rel=0" target="_blank" class="a02"></a>
-        <a href="https://youtu.be/_RDnE7u4k8U?rel=0" target="_blank" class="a03"></a>
+        <img src="https://static.willbes.net/public/images/promotion/2021/07/1071m_top.jpg" alt="통신/전기 최우영" >
     </div> 
 
     <div class="evtCtnsBox">
-        <img src="https://static.willbes.net/public/images/promotion/2020/08/1071m_02.jpg" alt="" >
+        <img src="https://static.willbes.net/public/images/promotion/2021/07/1071m_01.jpg" alt="" >
+    </div> 
+
+    <div class="evtCtnsBox">
+        <img src="https://static.willbes.net/public/images/promotion/2021/07/1071m_02.jpg" alt="" >
+        <a href="https://youtube.com/embed/sC9TJfUNkyc" target="_blank" title="전기이론 기초강의" style="position: absolute; left: 6.53%; top: 25.28%; width: 42.78%; height: 15.32%; z-index: 2;"></a>
+        <a href="https://youtube.com/embed/_crgLD0rmN8" target="_blank" title="전기회로 기본용어" style="position: absolute; left: 50.69%; top: 25.28%; width: 42.78%; height: 15.32%; z-index: 2;"></a>
+        <a href="https://youtube.com/embed/1zATq2Kydwg" target="_blank" title="변조이론" style="position: absolute; left: 6.53%; top: 47.74%; width: 42.78%; height: 15.32%; z-index: 2;"></a>
+        <a href="https://youtube.com/embed/37yjw2mC8wY" target="_blank" title="RLC회로의 특성" style="position: absolute; left: 50.69%; top: 47.74%; width: 42.78%; height: 15.32%; z-index: 2;"></a>
+        <a href="https://youtube.com/embed/eiAKjkFjwtE" target="_blank" title="연산증폭기" style="position: absolute; left: 6.53%; top: 70.13%; width: 42.78%; height: 15.32%; z-index: 2;"></a>
+        <a href="https://youtube.com/embed/wSaPEaVIbbo" target="_blank" title="기출 문풀" style="position: absolute; left: 50.69%; top: 70.13%; width: 42.78%; height: 15.32%; z-index: 2;"></a>
     </div> 
     
-    <div class="evtCtnsBox evt03">
-        <img src="https://static.willbes.net/public/images/promotion/2021/05/1071m_03.jpg" alt="">
-        <a href="https://pass.willbes.net/m/periodPackage/show/cate/3028/pack/648001/prod-code/171526" title="통신기술직" target="_blank" class="b01"></a>
-        <a href="https://pass.willbes.net/m/periodPackage/show/cate/3028/pack/648001/prod-code/171527" title="전송기술직" target="_blank" class="b02"></a>
-        <a href="https://pass.willbes.net/m/package/show/cate/3028/pack/648001/prod-code/171762" title="전기직" target="_blank" class="b03"></a>
-
-        <a href="https://pass.willbes.net/m/package/show/cate/3028/pack/648001/prod-code/182687" title="통신진" target="_blank" class="b11"></a>
-        <a href="https://pass.willbes.net/m/package/show/cate/3028/pack/648001/prod-code/182688" title="전자직" target="_blank" class="b12"></a>
-
-        <a href="https://pass.willbes.net/m/package/show/cate/3028/pack/648001/prod-code/180909" title="지방직 통신기술직" target="_blank" class="b09"></a>
-        <a href="https://pass.willbes.net/m/package/show/cate/3028/pack/648001/prod-code/180908" title="지방직 전기직" target="_blank" class="b10"></a>
-        <a href="https://pass.willbes.net/m/package/show/cate/3028/pack/648001/prod-code/179673" title="국가직" target="_blank" class="b04"></a>
-        <a href="https://pass.willbes.net/m/package/show/cate/3028/pack/648001/prod-code/179678" title="전기직" target="_blank" class="b05"></a>
-        <a href="https://pass.willbes.net/m/package/show/cate/3028/pack/648001/prod-code/177169" title="국가직" target="_blank" class="b06"></a>
-        <a href="https://pass.willbes.net/m/package/show/cate/3028/pack/648001/prod-code/176489" title="전기직" target="_blank" class="b07"></a>
-        <a href="https://pass.willbes.net/m/search/result/?=&cate=&searchfull_text=%EC%B5%9C%EC%9A%B0%EC%98%81" target="_blank" title="단가 수강신청" class="b08"></a>
+    <div class="evtCtnsBox">
+        <img src="https://static.willbes.net/public/images/promotion/2021/07/1071m_03.jpg" alt="">
     </div>   
+
+    <div class="evtCtnsBox">
+        <img src="https://static.willbes.net/public/images/promotion/2021/07/1071m_04.jpg" alt="">
+        <a href="https://pass.willbes.net/m/periodPackage/show/cate/3028/pack/648001/prod-code/184023" target="_blank" title="통신기술직" style="position: absolute; left: 53.19%; top: 41.4%; width: 26.67%; height: 3.33%; z-index: 2;"></a>
+        <a href="https://pass.willbes.net/m/periodPackage/show/cate/3028/pack/648001/prod-code/184024" target="_blank" title="전송기술직" style="position: absolute; left: 53.19%; top: 63.59%; width: 26.67%; height: 3.33%; z-index: 2;"></a>
+        <a href="https://pass.willbes.net/m/package/show/cate/3028/pack/648001/prod-code/184021" target="_blank" title="전기직" style="position: absolute; left: 53.19%; top: 85.78%; width: 26.67%; height: 3.33%; z-index: 2;"></a>
+    </div> 
 </div>
 <!-- End Container -->
 
-<!-- AceCounter Log Gathering Script V.8.0.AMZ2019080601 -->
-<script language='javascript'>
-    var _AceGID=(function(){var Inf=['gtp14.acecounter.com','8080','AH1A44052179653','AW','0','NaPm,Ncisy','ALL','0']; var _CI=(!_AceGID)?[]:_AceGID.val;var _N=0;var _T=new Image(0,0);if(_CI.join('.').indexOf(Inf[3])<0){ _T.src ="https://"+ Inf[0] +'/?cookie'; _CI.push(Inf);  _N=_CI.length; } return {o: _N,val:_CI}; })();
-    var _AceCounter=(function(){var G=_AceGID;var _sc=document.createElement('script');var _sm=document.getElementsByTagName('script')[0];if(G.o!=0){var _A=G.val[G.o-1];var _G=(_A[0]).substr(0,_A[0].indexOf('.'));var _C=(_A[7]!='0')?(_A[2]):_A[3];var _U=(_A[5]).replace(/\,/g,'_');_sc.src='https:'+'//cr.acecounter.com/Web/AceCounter_'+_C+'.js?gc='+_A[2]+'&py='+_A[4]+'&gd='+_G+'&gp='+_A[1]+'&up='+_U+'&rd='+(new Date().getTime());_sm.parentNode.insertBefore(_sc,_sm);return _sc.src;}})();
-</script>
-<noscript><img src='https://gtp14.acecounter.com:8080/?uid=AH1A44052179653&je=n&' border='0' width='0' height='0' alt=''></noscript>
-<!-- AceCounter Log Gathering Script End -->
 
 @stop

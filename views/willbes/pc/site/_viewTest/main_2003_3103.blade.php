7d513da6ddbaa658688f540807d444a16154dc18
최현탁 <cht@willbes.com>
2021-07-29 17:10:17 +0900

    [테스트제외]  공무원 7급 PSAT 메인 테스트 페이지

--- views/willbes/pc/site/_viewTest/main_2003_3103.blade.php
+++ views/willbes/pc/site/_viewTest/main_2003_3103.blade.php
@@ -130,8 +130,67 @@
         <div class="Section Section6 mt80">
             <div class="widthAuto">
                 <div class="nNoticeBox three">
-                    {{-- board include --}}
-                    @include('willbes.pc.site._viewTest.main_partial.board_' . $__cfg['SiteCode'] . '_wide3')
+                    <div class="Section NSK mt90 mb90">
+                        <div class="widthAuto">
+                            <div class="noticeList widthAuto350 f_left">
+                                <div class="will-nlistTit p_re">공지사항 <a href="{{front_url('/support/notice/index/cate/'.$__cfg['CateCode'])}}" target="_blank" class="btn-add">
+                                        <img src="{{ img_url('cop/icon_add_big.png') }}"></a>
+                                </div>
+                                <ul class="List-Table">
+                                    @if(empty($data['notice']) === true)
+                                        <li><span>등록된 내용이 없습니다.</span></li>
+                                    @else
+                                        @foreach($data['notice'] as $row)
+                                            <li>
+                                                <a href="{{front_url('/support/notice/show/cate/'.$__cfg['CateCode'].'?board_idx='.$row['BoardIdx'])}}">
+                                                    <span>{{$row['Title']}}</span> @if(date('Y-m-d') == $row['RegDatm'])<img src="{{ img_url('cop/icon_new.png') }}">@endif
+                                                </a>
+                                                <span class="date">{{$row['RegDatm']}}</span>
+                                            </li>
+                                        @endforeach
+                                    @endif
+                                </ul>
+                            </div>
+                            <div class="noticeList widthAuto350 f_left ml35">
+                                <div class="will-nlistTit p_re">시험공고 <a href="{{front_url('/support/examAnnouncement/index/cate/'.$__cfg['CateCode'])}}" target="_blank" class="btn-add">
+                                        <img src="{{ img_url('cop/icon_add_big.png') }}"></a>
+                                </div>
+                                <ul class="List-Table">
+                                    @if(empty($data['exam_announcement']) === true)
+                                        <li><span>등록된 내용이 없습니다.</span></li>
+                                    @else
+                                        @foreach($data['exam_announcement'] as $row)
+                                            <li>
+                                                <a href="{{front_url('/support/examAnnouncement/show/cate/'.$__cfg['CateCode'].'?board_idx='.$row['BoardIdx'])}}">
+                                                    <span>{{$row['Title']}}</span> @if(date('Y-m-d') == $row['RegDatm'])<img src="{{ img_url('cop/icon_new.png') }}">@endif
+                                                </a>
+                                                <span class="date">{{$row['RegDatm']}}</span>
+                                            </li>
+                                        @endforeach
+                                    @endif
+                                </ul>
+                            </div>
+                            <div class="noticeList widthAuto350 f_left ml35">
+                                <div class="will-nlistTit p_re">수험뉴스 <a href="{{front_url('/support/examNews/index/cate/'.$__cfg['CateCode'])}}" target="_blank" class="btn-add">
+                                        <img src="{{ img_url('cop/icon_add_big.png') }}"></a>
+                                </div>
+                                <ul class="List-Table">
+                                    @if(empty($data['exam_news']) === true)
+                                        <li><span>등록된 내용이 없습니다.</span></li>
+                                    @else
+                                        @foreach($data['exam_news'] as $row)
+                                            <li>
+                                                <a href="{{front_url('/support/examNews/show/cate/'.$__cfg['CateCode'].'?board_idx='.$row['BoardIdx'])}}">
+                                                    <span>{{$row['Title']}}</span> @if(date('Y-m-d') == $row['RegDatm'])<img src="{{ img_url('cop/icon_new.png') }}">@endif
+                                                </a>
+                                                <span class="date">{{$row['RegDatm']}}</span>
+                                            </li>
+                                        @endforeach
+                                    @endif
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <!--willbesNews //-->
             </div>
@@ -140,65 +199,10 @@
         {{--학원 오시는 길--}}
         @include('willbes.pc.site._viewTest.main_partial.map_2003')
 
-        <div class="Section NSK mt90 mb90">
+        <div class="Section mt70 mb90 NSK">
             <div class="widthAuto">
-                <div class="noticeList widthAuto350 f_left">
-                    <div class="will-nlistTit p_re">공지사항 <a href="{{front_url('/support/notice/index/cate/'.$__cfg['CateCode'])}}" target="_blank" class="btn-add">
-                            <img src="{{ img_url('cop/icon_add_big.png') }}"></a>
-                    </div>
-                    <ul class="List-Table">
-                        @if(empty($data['notice']) === true)
-                            <li><span>등록된 내용이 없습니다.</span></li>
-                        @else
-                            @foreach($data['notice'] as $row)
-                                <li>
-                                    <a href="{{front_url('/support/notice/show/cate/'.$__cfg['CateCode'].'?board_idx='.$row['BoardIdx'])}}">
-                                        <span>{{$row['Title']}}</span> @if(date('Y-m-d') == $row['RegDatm'])<img src="{{ img_url('cop/icon_new.png') }}">@endif
-                                    </a>
-                                    <span class="date">{{$row['RegDatm']}}</span>
-                                </li>
-                            @endforeach
-                        @endif
-                    </ul>
-                </div>
-                <div class="noticeList widthAuto350 f_left ml35">
-                    <div class="will-nlistTit p_re">시험공고 <a href="{{front_url('/support/examAnnouncement/index/cate/'.$__cfg['CateCode'])}}" target="_blank" class="btn-add">
-                            <img src="{{ img_url('cop/icon_add_big.png') }}"></a>
-                    </div>
-                    <ul class="List-Table">
-                        @if(empty($data['exam_announcement']) === true)
-                            <li><span>등록된 내용이 없습니다.</span></li>
-                        @else
-                            @foreach($data['exam_announcement'] as $row)
-                                <li>
-                                    <a href="{{front_url('/support/examAnnouncement/show/cate/'.$__cfg['CateCode'].'?board_idx='.$row['BoardIdx'])}}">
-                                        <span>{{$row['Title']}}</span> @if(date('Y-m-d') == $row['RegDatm'])<img src="{{ img_url('cop/icon_new.png') }}">@endif
-                                    </a>
-                                    <span class="date">{{$row['RegDatm']}}</span>
-                                </li>
-                            @endforeach
-                        @endif
-                    </ul>
-                </div>
-                <div class="noticeList widthAuto350 f_left ml35">
-                    <div class="will-nlistTit p_re">수험뉴스 <a href="{{front_url('/support/examNews/index/cate/'.$__cfg['CateCode'])}}" target="_blank" class="btn-add">
-                            <img src="{{ img_url('cop/icon_add_big.png') }}"></a>
-                    </div>
-                    <ul class="List-Table">
-                        @if(empty($data['exam_news']) === true)
-                            <li><span>등록된 내용이 없습니다.</span></li>
-                        @else
-                            @foreach($data['exam_news'] as $row)
-                                <li>
-                                    <a href="{{front_url('/support/examNews/show/cate/'.$__cfg['CateCode'].'?board_idx='.$row['BoardIdx'])}}">
-                                        <span>{{$row['Title']}}</span> @if(date('Y-m-d') == $row['RegDatm'])<img src="{{ img_url('cop/icon_new.png') }}">@endif
-                                    </a>
-                                    <span class="date">{{$row['RegDatm']}}</span>
-                                </li>
-                            @endforeach
-                        @endif
-                    </ul>
-                </div>
+                {{-- cscenter --}}
+                @include('willbes.pc.site.main_partial.cscenter_' . $__cfg['SiteCode'])
             </div>
         </div>
         <!-- CS센터 //-->

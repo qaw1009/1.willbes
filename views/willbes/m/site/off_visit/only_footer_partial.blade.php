72376f326319496f9529b2840931fe4bb3166959
신봉석 <bsshin@willbes.com>
2021-07-30 09:48:20 +0900

    프런트 > 방문결제 유의사항 alert 추가, 모바일 방문결제 장바구니 상시오픈 UI 수정

--- views/willbes/m/site/off_visit/only_footer_partial.blade.php
+++ views/willbes/m/site/off_visit/only_footer_partial.blade.php
@@ -4,8 +4,8 @@
 
     {{-- 장바구니 레이어 --}}
     <div id="basket_box" class="basketBox">
-        <div class="MoreBtn"><a><img src="{{ img_url('m/mypage/icon_arrow_off.png') }}"></a></div>
-        <div class="basketInfo">
+        <div class="MoreBtn"><a><img src="{{ img_url('m/mypage/icon_arrow_on.png') }}"></a></div>
+        <div class="basketInfo" style="display: block;">
             <ul id="basket_list" class="basketList">
             </ul>
             <div class="basketPrice">

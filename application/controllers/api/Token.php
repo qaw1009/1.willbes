754a0b9f701062aba3107cacc7c9ea105995e82c
신봉석 <bsshin@willbes.com>
2021-08-09 09:12:46 +0900

    rest api > 토큰인증방식 수정, 개별인증방식 설정기능 추가, 토큰생성 샘플 api 수정

--- application/controllers/api/Token.php
+++ application/controllers/api/Token.php
@@ -7,7 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Token extends \app\controllers\RestController
 {
     protected $methods = [
-        'index_get' => ['token' => false],
+        'issue_get' => ['auth' => false],
     ];
 
     public function __construct()
@@ -17,56 +17,60 @@ class Token extends \app\controllers\RestController
 
     /**
      * 토큰 발급 샘플 메소드
-     * http://api.local.willbes.net/token/?username=willbes&password=f8a33fbaa737fa97c77bdd4d8f22cedcae01f9915db556dc2812a721970b9b01&method=post&uri=/server
+     * https://api.local.willbes.net/token/issue?username=willbes&password=f8a33fbaa737fa97c77bdd4d8f22cedcae01f9915db556dc2812a721970b9b01&method=get&uri=/sample/server/index&params=acbd18db4cc2f85cedef654fccc4a4d8
+     * password = hash_hmac('sha256', 'password', 'username')
+     * params = md5('파라미터값연결') = md5('foo')
      */
-    public function index_get()
+    public function issue_get()
     {
-        // api 설정
-        $valid_logins = (array) $this->config->item('rest_valid_logins');
+        // 사용자 정보
+        $valid_logins = (array) config_item('rest_valid_logins');
 
-        // 파라미터 validation rule
+        // 파라미터 체크
         $rules = [
             ['field' => 'username', 'label' => '사용자명', 'rules' => 'trim|required'],
             ['field' => 'password', 'label' => '비밀번호', 'rules' => 'trim|required'],
             ['field' => 'method', 'label' => '전송방식', 'rules' => 'trim|required'],
             ['field' => 'uri', 'label' => 'URI', 'rules' => 'trim|required'],
+            ['field' => 'params', 'label' => '파라미터값', 'rules' => 'trim|required'],
         ];
 
-        // 파라미터 체크
         if ($this->validate($rules) === false) {
-            return;
+            return null;
         }
 
         // 수신 파라미터
-        $username = $this->get('username');
-        $password = $this->get('password');
-        $method = $this->get('method');
-        $uri = $this->get('uri');
+        $username = $this->_reqG('username');
+        $password = $this->_reqG('password');
+        $method = $this->_reqG('method');
+        $uri = $this->_reqG('uri');
+        $params = $this->_reqG('params');
 
         // 사용자명 체크
         if (array_key_exists($username, $valid_logins) === false) {
-            $this->api_error($this->lang->line('text_rest_unauthorized'), [], _HTTP_UNAUTHORIZED);
+            $this->api_error(lang('text_rest_unauthorized'), _HTTP_UNAUTHORIZED);
         }
 
         // 비밀번호 체크
-        $_password = hash_hmac('sha256', $valid_logins[$username], $username);
-        if (strcasecmp($_password, $password) !== 0) {
-            $this->api_error($this->lang->line('text_rest_invalid_credentials'), [], _HTTP_UNAUTHORIZED);
+        $valid_password = hash_hmac('sha256', $valid_logins[$username], $username);
+        if (strcasecmp($valid_password, $password) !== 0) {
+            $this->api_error(lang('text_rest_invalid_credentials'), _HTTP_UNAUTHORIZED);
         }
 
-        // token 생성
-        $unique_id = uniqid();
-        $nonce = substr($unique_id, 0, 7) . time() . substr($unique_id, 7);
+        // 토큰 생성
+        $nonce = substr(uniqid(), -6) . time();
+        $secret = hash_hmac('sha256', $valid_logins[$username], $username . $nonce);
+        $md5 = md5(strtoupper($method) . ':' . parse_url($uri, PHP_URL_PATH) . ':' . $params);
 
-        $token = $username . ':' . $nonce . ':' . $password . ':' . strtoupper($method) . ':' . $uri;
-        $token = md5(hash_hmac('sha256', $token, $nonce));
+        $token = $username . ':' . $nonce . ':' . $md5;
+        $token = md5(hash_hmac('sha256', $token, $secret));
 
         $results = [
-            $this->config->item('rest_user_name') => $username,
-            $this->config->item('rest_nonce_name') => $nonce,
-            $this->config->item('rest_token_name') => $token
+            config_item('rest_user_name') => $username,
+            config_item('rest_nonce_name') => $nonce,
+            config_item('rest_token_name') => $token
         ];
 
-        $this->api_success(null, $results);
+        return $this->api_success($results);
     }
 }

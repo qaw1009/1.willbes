39f66fe919593170e4229fe8df9c83e7c472c9e7
김창용 <winmir@willbes.com>
2021-08-31 11:17:32 +0900

    공무원 웰컴팩 프로모션 수정

--- views/willbes/pc/promotion/2003/2225.blade.php
+++ views/willbes/pc/promotion/2003/2225.blade.php
@@ -14,7 +14,8 @@
         }
         .evtContent span {vertical-align:auto}
         .evtCtnsBox {width:100%; text-align:center; min-width:1120px;}
-        .evt_wrap {width:1120px; margin:0 auto; position: relative;}
+        .evtCtnsBox .wrap {width:1120px; margin:0 auto; position: relative;}
+        .evtCtnsBox .wrap a:hover {box-shadow:0 0 10px rgba(0,0,0,.5); border-radius:8px}
 
         /************************************************************/
 
@@ -29,20 +30,20 @@
 
     <div class="p_re evtContent NSK" id="evtContainer">     
 
-        <div class="evtCtnsBox wb_top">
+        <div class="evtCtnsBox wb_top" data-aos="fade-left">
             <img src="https://static.willbes.net/public/images/promotion/2021/08/2225_top.jpg" alt="공무원 웰컴팩" />
         </div>
 
         <div class="evtCtnsBox wb_cts01">
-            <img src="https://static.willbes.net/public/images/promotion/2021/08/2225_01.jpg" alt="아직도 고민만 하고 계세요?" />
+            <img src="https://static.willbes.net/public/images/promotion/2021/08/2225_01.jpg" alt="아직도 고민만 하고 계세요?" data-aos="fade-right"/>
         </div>
 
         <div class="evtCtnsBox wb_cts02">
-            <img src="https://static.willbes.net/public/images/promotion/2021/08/2225_02.jpg" alt="모든 혜택이 0원" />
+            <img src="https://static.willbes.net/public/images/promotion/2021/08/2225_02.jpg" alt="모든 혜택이 0원" data-aos="fade-left"/>
         </div>      
 
         <div class="evtCtnsBox wb_cts03">
-            <div class="evt_wrap">
+            <div class="wrap" data-aos="fade-up">
                 <img src="https://static.willbes.net/public/images/promotion/2021/08/2225_03.jpg" alt="자세히보기"/>
                 <a href="https://pass.willbes.net/home/index/cate/3019" target="_blank" title="9급" style="position: absolute;left: 14.77%;top: 52%;width: 12.27%;height: 3.8%;z-index: 2;"></a>
                 <a href="https://pass.willbes.net/home/index/cate/3020" target="_blank" title="7급" style="position: absolute;left: 43.77%;top: 52%;width: 12.27%;height: 3.8%;z-index: 2;"></a>
@@ -56,8 +57,12 @@
     </div>
     <!-- End Container -->
 
-    <script>    
-    
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+      $( document ).ready( function() {
+        AOS.init();
+      } );
     </script>
 
     {{-- 프로모션용 스크립트 include --}}

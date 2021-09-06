775704c0f57e955bba39531f4b489f4e0081118c
김창용 <winmir@willbes.com>
2021-09-06 15:00:56 +0900

    빅데이타 분석기사 메인

--- views/willbes/m/site/main_2016_3147.blade.php
+++ views/willbes/m/site/main_2016_3147.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <!-- Container -->
-    <div id="Container" class="Container NSK mb40 ic3146">
+    <div id="Container" class="Container NSK mb40 ic3147">
         @if(empty($data['dday']) === false)
             <div id="newTopDday" class="newTopDday">
                 <div class="d_day NSK">
@@ -20,15 +20,15 @@
         @endif
 
         <div class="Section article1">
-            <img src="{{ img_static_url('promotion/main/2006/308906m_top.jpg') }}" alt="빅데이터 분석기사">
+            <img src="{{ img_static_url('promotion/main/2016/3147m_top.jpg') }}" alt="빅데이터 분석기사">
         </div>
 
         <div class="Section article2" data-aos="fade-left">
-            <img src="{{ img_static_url('promotion/main/2006/308906m_01.jpg') }}" alt="향후 5년 내 데이터 산업의 빅데이터 필요 인력">
+            <img src="{{ img_static_url('promotion/main/2016/3147m_01.jpg') }}" alt="향후 5년 내 데이터 산업의 빅데이터 필요 인력">
         </div>
 
         <div class="Section article3">
-            <img src="{{ img_static_url('promotion/main/2006/308906m_02.jpg') }}" alt="빅데이터 분석기사 자격증 왜 훈쌤인가">
+            <img src="{{ img_static_url('promotion/main/2016/3147m_02.jpg') }}" alt="빅데이터 분석기사 자격증 왜 훈쌤인가">
         </div>
 
         <div class="Section article4">
@@ -40,38 +40,38 @@
 
             <div class="btnBox">
                 <div>
-                    <img src="{{ img_static_url('promotion/main/2006/308906m_btn01.jpg') }}" alt="빅데이터 분석기사 필기합격 패키지 6개월">
-                    <a href="{{ front_url('lecture/show/cate/3146/pattern/only/prod-code/185587') }}" title="패키지 6개월 신청하기"></a>
+                    <img src="{{ img_static_url('promotion/main/2016/3147m_btn01.jpg') }}" alt="빅데이터 분석기사 필기합격 패키지 6개월">
+                    <a href="{{ front_url('lecture/show/cate/3147/pattern/only/prod-code/185587') }}" title="패키지 6개월 신청하기"></a>
                 </div>
                 <div>
-                    <img src="{{ img_static_url('promotion/main/2006/308906m_btn02.jpg') }}" alt="빅데이터 분석기사 필기 합격 패키지 3개월">
-                    <a href="{{ front_url('/lecture/show/cate/3146/pattern/only/prod-code/185588') }}" title="패키지 3개월 신청하기"></a>
+                    <img src="{{ img_static_url('promotion/main/2016/3147m_btn02.jpg') }}" alt="빅데이터 분석기사 필기 합격 패키지 3개월">
+                    <a href="{{ front_url('/lecture/show/cate/3147/pattern/only/prod-code/185588') }}" title="패키지 3개월 신청하기"></a>
                 </div>
             </div>
         </div>
     </div>
     <!-- End Container -->
     <style type="text/css">
-        .ic3146 {width:100%; max-width:720px; margin:0 auto; text-align:center; position:relative; font-size:14px; line-height:1.5; clear:both}
-        .ic3146 img {width:100%; max-width:720px;}
+        .ic3147 {width:100%; max-width:720px; margin:0 auto; text-align:center; position:relative; font-size:14px; line-height:1.5; clear:both}
+        .ic3147 img {width:100%; max-width:720px;}
 
         /* 유튜브영상 */
-        .ic3146 .video-container-box {padding:20px}
-        .ic3146 .video-container {position:relative; padding-bottom:56.25%; padding-top:30px; height:0; overflow: hidden;}
-        .ic3146 .video-container iframe,
-        .ic3146 .video-container object,
-        .ic3146 .video-container embed {position:absolute; top:0; left:50%; width:100%; margin-left:-50%; height:100%;}
+        .ic3147 .video-container-box {padding:20px}
+        .ic3147 .video-container {position:relative; padding-bottom:56.25%; padding-top:30px; height:0; overflow: hidden;}
+        .ic3147 .video-container iframe,
+        .ic3147 .video-container object,
+        .ic3147 .video-container embed {position:absolute; top:0; left:50%; width:100%; margin-left:-50%; height:100%;}
 
-        .ic3146 .btnBox{display: flex; justify-content: space-between; align-items: center; padding: 5% 20px;}
-        .ic3146 .btnBox div{ width:48%; position: relative;}
-        .ic3146 .btnBox div a{width:68%; height:30%; position: absolute; top:59%; left:15%; }
+        .ic3147 .btnBox{display: flex; justify-content: space-between; align-items: center; padding: 5% 20px;}
+        .ic3147 .btnBox div{ width:48%; position: relative;}
+        .ic3147 .btnBox div a{width:68%; height:30%; position: absolute; top:59%; left:15%; }
 
         /*타이머*/
-        .ic3146 .newTopDday {clear:both; background:#fff; width:100%; padding:20px 0;}
-        .ic3146 .newTopDday ul { display: flex; justify-content: center; align-items: center;}
-        .ic3146 .newTopDday ul li {color:#000; font-size:20px;}
-        .ic3146 .newTopDday ul li:nth-child(1) {margin-right:5%; }
-        .ic3146 .newTopDday .d_day {line-height:1.2;text-align:center;padding-top:30px;}
-        .ic3146 .newTopDday .d_day p {color:#fffb00; font-size:40px; background:#000; border-radius:40px; padding:5px 15px}
+        .ic3147 .newTopDday {clear:both; background:#fff; width:100%; padding:20px 0;}
+        .ic3147 .newTopDday ul { display: flex; justify-content: center; align-items: center;}
+        .ic3147 .newTopDday ul li {color:#000; font-size:20px;}
+        .ic3147 .newTopDday ul li:nth-child(1) {margin-right:5%; }
+        .ic3147 .newTopDday .d_day {line-height:1.2;text-align:center;padding-top:30px;}
+        .ic3147 .newTopDday .d_day p {color:#fffb00; font-size:40px; background:#000; border-radius:40px; padding:5px 15px}
     </style>
 @stop
--- views/willbes/pc/site/main_2016_3147.blade.php
+++ views/willbes/pc/site/main_2016_3147.blade.php
@@ -3,34 +3,34 @@
 @section('content')
 <style type="text/css">
     /* 빅데이터분석기사 */
-    .ic3146 .article1 {background:url(https://static.willbes.net/public/images/promotion/main/2016/3147_top_bg.jpg) no-repeat center top; margin-top:20px; text-align:center}
+    .ic3147 .article1 {background:url(https://static.willbes.net/public/images/promotion/main/2016/3147_top_bg.jpg) no-repeat center top; margin-top:20px; text-align:center}
     
-    .ic3146 .article2 {width:979px; margin:-87px auto 120px; box-shadow:10px 10px 20px rgba(0,0,0,.3);} 
-    .ic3146 .article3 {background:#c7ecff; text-align:center; padding-bottom:100px}
-    .ic3146 .article3 a {display:block; width:450px; margin:50px auto; padding:20px 0; font-size:24px; border-radius:50px; background:#363636; color:#fff}
-    .ic3146 .article3 a:hover {background:#000}
+    .ic3147 .article2 {width:979px; margin:-87px auto 120px; box-shadow:10px 10px 20px rgba(0,0,0,.3);} 
+    .ic3147 .article3 {background:#c7ecff; text-align:center; padding-bottom:100px}
+    .ic3147 .article3 a {display:block; width:450px; margin:50px auto; padding:20px 0; font-size:24px; border-radius:50px; background:#363636; color:#fff}
+    .ic3147 .article3 a:hover {background:#000}
 
-    .ic3146 .article4 {background:#efefef; padding-bottom:100px}
-    .ic3146 .article4 .wrap {width:1120px; margin:0 auto; position:relative}
-    .ic3146 .article5 .wrap {width:1120px; margin:0 auto; position:relative}
-    .ic3146 .article6 {background:#c7ebff;}
-    .ic3146 .article6 .wrap {width:1120px; margin:0 auto; position:relative}
-    .ic3146 .article6 {position:relative;}
-    .ic3146 .youtube {width:860px; margin:0 auto;}
-    .ic3146 .youtube iframe {width:860px; height:450px; margin-bottom:50px} 
+    .ic3147 .article4 {background:#efefef; padding-bottom:100px}
+    .ic3147 .article4 .wrap {width:1120px; margin:0 auto; position:relative}
+    .ic3147 .article5 .wrap {width:1120px; margin:0 auto; position:relative}
+    .ic3147 .article6 {background:#c7ebff;}
+    .ic3147 .article6 .wrap {width:1120px; margin:0 auto; position:relative}
+    .ic3147 .article6 {position:relative;}
+    .ic3147 .youtube {width:860px; margin:0 auto;}
+    .ic3147 .youtube iframe {width:860px; height:450px; margin-bottom:50px} 
 
     /*타이머*/
-    .ic3146 .newTopDday {clear:both; background:#fff; width:100%; padding:10px 0;}
-    .ic3146 .newTopDday ul {width:1120px; margin:0 auto}
-    .ic3146 .newTopDday ul li {display:inline-block; text-align:center; color:#000; font-size:20px;}
-    .ic3146 .newTopDday ul li:nth-child(1) {margin-right:10px; }
-    .ic3146 .newTopDday .d_day {line-height:1.2;text-align:center;padding-top:30px;}
-    .ic3146 .newTopDday .d_day p {color:#fffb00; font-size:40px; background:#000; border-radius:40px; padding:5px 15px}
+    .ic3147 .newTopDday {clear:both; background:#fff; width:100%; padding:10px 0;}
+    .ic3147 .newTopDday ul {width:1120px; margin:0 auto}
+    .ic3147 .newTopDday ul li {display:inline-block; text-align:center; color:#000; font-size:20px;}
+    .ic3147 .newTopDday ul li:nth-child(1) {margin-right:10px; }
+    .ic3147 .newTopDday .d_day {line-height:1.2;text-align:center;padding-top:30px;}
+    .ic3147 .newTopDday .d_day p {color:#fffb00; font-size:40px; background:#000; border-radius:40px; padding:5px 15px}
 </style>
 
 
     <!-- Container -->
-    <div id="Container" class="Container ic3146 c_both">
+    <div id="Container" class="Container ic3147 c_both">
         <!-- site nav -->
         @include('willbes.pc.layouts.partial.site_menu')
 
@@ -60,7 +60,7 @@
 
         <div class="Section article3">
             <img src="https://static.willbes.net/public/images/promotion/main/2016/3147_02.jpg" title="시험일정">
-            <a href="https://willbesedu.willbes.net/landing/show/lcode/1045/cate/3146/preview/Y" target="_blank" class="NSK-Black">빅데이터 분석기사 더 자세히 알아보기 ></a>
+            <a href="https://willbesedu.willbes.net/landing/show/lcode/1045/cate/3147/preview/Y" target="_blank" class="NSK-Black">빅데이터 분석기사 더 자세히 알아보기 ></a>
         </div>
 
         <div class="Section article4">
@@ -68,8 +68,8 @@
                 <img src="https://static.willbes.net/public/images/promotion/main/2016/3147_03.jpg" title="왜 훈샘인가?"  data-aos="fade-left">
                 <div class="youtube">
                     <iframe src="https://www.youtube.com/embed/efr9iOZ57gM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    <a href="https://willbesedu.willbes.net/lecture/show/cate/3146/pattern/only/prod-code/185587"><img src="https://static.willbes.net/public/images/promotion/main/2016/3147_03_01.jpg" title="훈쌤 전격입성"></a>
-                    <a href="https://willbesedu.willbes.net/lecture/show/cate/3146/pattern/only/prod-code/185588" class="f_right"><img src="https://static.willbes.net/public/images/promotion/main/2016/3147_03_02.jpg" title="훈쌤 전격입성"></a>
+                    <a href="https://willbesedu.willbes.net/lecture/show/cate/3147/pattern/only/prod-code/185587"><img src="https://static.willbes.net/public/images/promotion/main/2016/3147_03_01.jpg" title="훈쌤 전격입성"></a>
+                    <a href="https://willbesedu.willbes.net/lecture/show/cate/3147/pattern/only/prod-code/185588" class="f_right"><img src="https://static.willbes.net/public/images/promotion/main/2016/3147_03_02.jpg" title="훈쌤 전격입성"></a>
                 </div>  
             </div>  
         </div>

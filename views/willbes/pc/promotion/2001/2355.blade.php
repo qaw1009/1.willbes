b3a9fac3d83bdd96b768d9eb9f136e32be4a0cae
김창용 <winmir@willbes.com>
2021-09-09 17:29:17 +0900

    경찰 한가위 드림 이벤트 프로모션

--- views/willbes/pc/promotion/2001/2355.blade.php
+++ views/willbes/pc/promotion/2001/2355.blade.php
@@ -14,9 +14,11 @@
         .evtContent span {vertical-align:auto}
         .evtCtnsBox {width:100%; text-align:center; min-width:1120px;}
         .wrap {width:1120px; margin:0 auto; position:relative}
+        .wrap a:hover {box-shadow:0 10px 10px rgba(0,0,0,.5); border-radius:4px}
 
         /************************************************************/
         .evttop {background:url("https://static.willbes.net/public/images/promotion/2021/09/2355_top_bg.jpg") no-repeat center top;}
+        .evt01 {background:url("https://static.willbes.net/public/images/promotion/2021/09/2355_01_bg.jpg")}
      
 
     </style>
@@ -26,9 +28,11 @@
             <img src="https://static.willbes.net/public/images/promotion/2021/09/2355_top.jpg" title="한가위 드림 이벤트">   
         </div>
 
-        <div class="evtCtnsBox wrap">
-            <img src="https://static.willbes.net/public/images/promotion/2021/09/2355_01.jpg" title="드림 패키지" data-aos="fade-left">
-            <a href="https://police.willbes.net/package/show/cate/3001/pack/648001/prod-code/185708" target="_blank" title="패키지 신청하기" style="position: absolute; left: 22.68%; top: 88.77%; width: 54.82%; height: 5.24%; z-index: 2;"></a>
+        <div class="evtCtnsBox evt01">
+            <div class="wrap">
+                <img src="https://static.willbes.net/public/images/promotion/2021/09/2355_01.jpg" title="드림 패키지" data-aos="fade-left">
+                <a href="https://police.willbes.net/package/show/cate/3001/pack/648001/prod-code/185708" target="_blank" title="패키지 신청하기" style="position: absolute; left: 22.68%; top: 88.77%; width: 54.82%; height: 5.24%; z-index: 2;"></a>
+            </div>
         </div>
 
         <div class="evtCtnsBox wrap">

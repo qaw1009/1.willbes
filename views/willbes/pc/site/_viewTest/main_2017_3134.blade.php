f318342516251d564fefc989696e2901930cd5c8
김창용 <winmir@willbes.com>
2021-09-10 09:36:09 +0900

    임요 메인 테스트

--- views/willbes/pc/site/_viewTest/main_2017_3134.blade.php
+++ views/willbes/pc/site/_viewTest/main_2017_3134.blade.php
@@ -424,29 +424,6 @@
             // }).mouseleave(function (){
             //     subslidesImg.startAuto();
             // });
-
-            /*
-            $('.prof-Tab').each(function() {
-                var $active, $content, $links = $(this).find('a');
-                $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
-                $active.addClass('active');
-                $content = $($active[0].hash);
-                $links.not($active).each(function(){
-                    $(this.hash).hide()
-                });
-
-                // Bind the click event handler
-                $(this).on('click', 'a', function(e){
-                    $active.removeClass('active');
-                    $content.hide();
-                    $active = $(this);
-                    $content = $(this.hash);
-                    $active.addClass('active');
-                    $content.show();
-                    e.preventDefault();
-                });
-            });
-            */
         });
     </script>
 @stop

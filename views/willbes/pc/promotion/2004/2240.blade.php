f669a83bd59d527eafbf868ba01b030f1f4c30ad
김창용 <winmir@willbes.com>
2021-09-23 16:42:29 +0900

    공무원학원 9급 일반행정 프로모션 수정

--- views/willbes/pc/promotion/2004/2240.blade.php
+++ views/willbes/pc/promotion/2004/2240.blade.php
@@ -17,8 +17,8 @@
 
         /************************************************************/   
         
-        .sky {position:fixed;top:200px;right:10px;z-index:11;}
-        .sky a {display:block; margin-bottom:10px}
+        .sky {position:fixed;top:200px;right:5px;z-index:11;}
+        .sky a {display:block; margin-bottom:5px}
 
         .wb_top {background:url(https://static.willbes.net/public/images/promotion/2021/06/2240_top_bg.jpg) no-repeat center top;}
 
@@ -120,7 +120,7 @@
             <a href="https://pass.willbes.net/pass/offinfo/boardInfo/index/78?" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2021/06/2240_sky.png" alt="7월 new"></a>
             <a href="https://pass.willbes.net/pass/promotion/index/cate/3043/code/1297" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2021/07/2240_sky2.png" alt="새벽모의고사"></a>
             <a href="javascript:alert('Coming Soon!')"><img src="https://static.willbes.net/public/images/promotion/2021/08/2241_sky3.png" alt="신기훈 t"></a>
-            <a href="https://pass.willbes.net/pass/offLecture/index?cate_code=3043&campus_ccd=605001&subject_idx=1254&prof_idx=50500" target="_blank" ><img src="https://static.willbes.net/public/images/promotion/2021/08/2240_sky4.png" alt="한덕현 t"></a>
+            <a href="https://pass.willbes.net/pass/offLecture/index?cate_code=3046&campus_ccd=605001" target="_blank" ><img src="https://static.willbes.net/public/images/promotion/2021/09/2240_sky5.png" alt="세무직"></a>
         </div>
 
         <div class="evtCtnsBox wb_top" >            

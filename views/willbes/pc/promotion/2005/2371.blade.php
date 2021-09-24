5a70b9eefb9ac1506ea52d635ea55007c8ad7b0d
이윤구 <yoongukk@willbes.com>
2021-09-24 16:57:14 +0900

    (고등고시) 5급행정 페이지 신규

--- views/willbes/pc/promotion/2005/2371.blade.php
+++ views/willbes/pc/promotion/2005/2371.blade.php
@@ -14,7 +14,29 @@
         .evtContent span {vertical-align:auto}
         .evtCtnsBox {width:100%; text-align:center; min-width:1120px;}
 
-        /************************************************************/     
+        /************************************************************/    
+
+        /*타이머*/
+        .timeBox {background:#333}
+        .time {width:920px; margin:0 auto; text-align:center;}
+        .time {text-align:center; padding:20px 0}
+        .time ul {width:100%;}
+        .time ul:after {content:''; display:block; clear:both}
+        .time li {display:inline; float:left; line-height:61px; font-size:24px; margin-right:10px;}
+        .time li:first-child {margin-left:80px}
+        .time li img {width:44px}
+        .time .time_txt {color:#fff; letter-spacing:-1px}
+        .time .time_txt span {color:#d63e4d; animation:upDown 2s infinite;-webkit-animation:upDown 2s infinite;}
+        @@keyframes upDown{
+        from{color:#d63e4d}
+        50%{color:#ff6600}
+        to{color:#d63e4d}
+        }
+        @@-webkit-keyframes upDown{
+        from{color:#d63e4d}
+        50%{color:#ff6600}
+        to{color:#d63e4d}
+        }        
 
         .evt_top {background:url(https://static.willbes.net/public/images/promotion/2021/09/2371_top_bg.jpg) no-repeat center top;}	
 
@@ -42,7 +64,27 @@
         /************************************************************/      
     </style> 
 
-	<div class="evtContent NSK">     
+	<div class="evtContent NSK">    
+
+        <div class="evtCtnsBox timeBox">
+            <div class="time NGEB" id="newTopDday">
+                <ul>
+                    <li class="time_txt"><span>{{ kw_date('n/j(%)', $arr_promotion_params['edate']) }}</span> 마감!</li>
+                    <li class="time_txt"><span>남은 시간은</span></li>
+                    <li><img id="dd1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="dd2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li class="time_txt">일</li>
+                    <li><img id="hh1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="hh2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li class="time_txt">:</li>
+                    <li><img id="mm1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="mm2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li class="time_txt">:</li>
+                    <li><img id="ss1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="ss2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>             
+                </ul>
+            </div>
+        </div>
 
 		<div class="evtCtnsBox evt_top" data-aos="fade-left">
             <img src="https://static.willbes.net/public/images/promotion/2021/09/2371_top.jpg" alt="온라인 선행 2순환" />
@@ -148,8 +190,13 @@
             $("#imgBannerRight3").click(function (){
                 slidesImg1.goToNextSlide();
             });
-        });
+        });        
 
+        /*디데이카운트다운*/
+        $(document).ready(function() {
+        dDayCountDown('{{$arr_promotion_params['edate']}}','{{$arr_promotion_params['etime'] or "00:00"}}');
+        });   
+                
     </script>
 
 

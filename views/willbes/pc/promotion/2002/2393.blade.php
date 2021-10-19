594b9a15465bf08b5538bab40d05bafc1074183a
김창용 <winmir@willbes.com>
2021-10-19 16:20:13 +0900

    경찰학원 상담자뽑기 프로모션 신규

--- views/willbes/pc/promotion/2002/2393.blade.php
+++ views/willbes/pc/promotion/2002/2393.blade.php
@@ -14,29 +14,44 @@
         .evtContent span {vertical-align:top}
         .evtCtnsBox {width:100%; text-align:center; min-width:1120px;}
         .evtCtnsBox .wrap {width:1120px; margin:0 auto; position:relative}
+        .evtCtnsBox .wrap a {border:1px solid #000}
 
         /************************************************************/
+        .sky {position:fixed;top:225px; width:206px; right:0; z-index:1;} 
+        .sky a {display:block; margin-bottom:10px}
        
         .evt00 {background:#0a0a0a}
 
         .evtTop {background:url(https://static.willbes.net/public/images/promotion/2021/10/2393_top_bg.jpg) no-repeat center top;}
+        
+        .evt01 {padding-bottom:150px}
+        .evt01 div {width:400px; margin:0 auto}
+        .evt01 div a {display:block; background:#383838; color:#fff; border-radius:10px; font-size:30px; padding:20px 0;}
+        .evt01 div a:hover {background:#ff4b4a}
 
-        .evt01 {}
+        .evt02 {background:#eee; padding-bottom:150px}
 
+        .evtCtnsBox .slide_con {width:848px; margin:0 auto; text-align:center; position:relative;}
+        .evtCtnsBox .slide_con p {position:absolute; top:50%; margin-top:-39px; width:42px !important; height:78px !important; z-index:10}
+        .evtCtnsBox .slide_con p.leftBtn {left:-100px}
+        .evtCtnsBox .slide_con p.rightBtn {right:-100px}
 
-        .evt02 {background:#3b3d41}
-
-        .evtCtnsBox .slide_con {width:554px; margin:0 auto; text-align:center; position:relative;}
-        .evtCtnsBox .slide_con p {position:absolute; top:45%; margin-top:-22px; width:44px !important; height:45px !important; z-index:10}
-        .evtCtnsBox .slide_con p.leftBtn {left:-60px}
-        .evtCtnsBox .slide_con p.rightBtn {right:-60px}
-
-
-        .evt03 {background:#e7e4dd; padding-top:100px}
+        .evt03 {padding-top:150px; width:1120px; margin:0 auto}
+        .evt03 .title {font-size:36px; color:#383838; line-height:1.4}
+        .evt03 .title  p {font-size:60px}
+        .evt03 .tabs {display:flex; justify-content: center; margin-top:100px}
+        .evt03 .tabs a {display:inline-block; padding:30px 0; color:#393535; background:#eee; width:calc(25% - 1px); margin-right:1px; font-size:24px; font-weight:bold}
+        .evt03 .tabs a:hover,
+        .evt03 .tabs a.active {color:#fff; background:#ff4b4a}
 
     </style>
     
-    <div class="evtContent NSK" id="evtContainer">      
+    <div class="evtContent NSK" id="evtContainer">     
+        <div class="sky" id="QuickMenu">               
+            <a href="#event01"><img src="https://static.willbes.net/public/images/promotion/2021/10/2393_sky01.png" title="초시생"></a>
+            <a href="#event02"><img src="https://static.willbes.net/public/images/promotion/2021/10/2393_sky02.png" title="유튜브"></a>
+            <a href="#event03"><img src="https://static.willbes.net/public/images/promotion/2021/10/2393_sky03.png" title="합격 설명회"></a>
+        </div> 
 
         <div class="evtCtnsBox evt00">
             <img src="https://static.willbes.net/public/images/promotion/2020/09/1864_first.jpg"  alt="경찰학원부분 1위" />
@@ -46,28 +61,33 @@
             <img src="https://static.willbes.net/public/images/promotion/2021/10/2393_top.jpg" alt="신규가입 이벤트" />
         </div>
 
-        <div class="evtCtnsBox evt01" data-aos="fade-up">  
+        <div class="evtCtnsBox evt01" data-aos="fade-up" id="event01">  
             <img src="https://static.willbes.net/public/images/promotion/2021/10/2393_01.jpg" alt="2022년 경찰시험 개편"/>
+            <div><a href="https://police.willbes.net/pass/consult/visitConsult/index" target="_blank">상담신청하기 ></a></div>
         </div>
 
-        <div class="evtCtnsBox evt02" data-aos="fade-up">
+        <div class="evtCtnsBox evt02" data-aos="fade-up" id="event02">
             <img src="https://static.willbes.net/public/images/promotion/2021/10/2393_02.jpg" alt="혜택" />
             <div class="slide_con">
                 <div id="slidesImg">
-                    <div><img src="https://static.willbes.net/public/images/promotion/2021/10/2393_02_01.png" alt="1" /></div>
-                    <div><img src="https://static.willbes.net/public/images/promotion/2021/10/2393_02_02.png" alt="2" /></div>
-                    <div><img src="https://static.willbes.net/public/images/promotion/2021/10/2393_02_03.png" alt="3" /></div>
-                    <div><img src="https://static.willbes.net/public/images/promotion/2021/10/2393_02_04.png" alt="4" /></div>
-                    <div><img src="https://static.willbes.net/public/images/promotion/2021/10/2393_02_05.png" alt="5" /></div>
-                    <div><img src="https://static.willbes.net/public/images/promotion/2021/10/2393_02_06.png" alt="6" /></div>
+                    <div><img src="https://static.willbes.net/public/images/promotion/2021/10/2393_02_01.jpg" alt="1" /></div>
+                    <div><img src="https://static.willbes.net/public/images/promotion/2021/10/2393_02_02.jpg" alt="2" /></div>
+                    <div><img src="https://static.willbes.net/public/images/promotion/2021/10/2393_02_03.jpg" alt="3" /></div>
+                    <div><img src="https://static.willbes.net/public/images/promotion/2021/10/2393_02_04.jpg" alt="4" /></div>
+                    <div><img src="https://static.willbes.net/public/images/promotion/2021/10/2393_02_05.jpg" alt="5" /></div>
+                    <div><img src="https://static.willbes.net/public/images/promotion/2021/10/2393_02_06.jpg" alt="6" /></div>
                 </div>
                 <p class="leftBtn"><a id="imgBannerLeft"><img src="https://static.willbes.net/public/images/promotion/2021/10/2393_arrL.png"></a></p>
                 <p class="rightBtn"><a id="imgBannerRight"><img src="https://static.willbes.net/public/images/promotion/2021/10/2393_arrR.png"></a></p>
             </div>
         </div>
 
-        <div class="evtCtnsBox evt03" data-aos="fade-up">
-            <div>
+        <div class="evtCtnsBox evt03" data-aos="fade-up" id="event03">
+            <div class="title">
+                2022년 경찰합격 역시,
+                <p class="NSK-Black">신광은경찰팀과 함께^^</p>
+            </div>
+            <div class="tabs">
                 <a href="#tab01">10개월<br>슈퍼PASS</a>
                 <a href="#tab02">4개월<br>슈퍼PASS</a>
                 <a href="#tab03">심화기술<br>패키지</a>
@@ -76,19 +96,23 @@
             <div class="wrap">
                 <div id="tab01">
                     <img src="https://static.willbes.net/public/images/promotion/2021/10/2393_03_01.jpg" alt="10개월 슈퍼패스" />
-                    <a href="https://www.willbes.net/member/join/?ismobile=0&sitecode=2001" target="_blank" title="" style="position: absolute; left: 29.2%; top: 20.83%; width: 41.61%; height: 5.64%; z-index: 2;"></a>
+                    <a href="https://police.willbes.net/pass/offPackage/show/prod-code/186105" target="_blank" title="김원욱 헌법" style="position: absolute; left: 16.52%; top: 51.98%; width: 10.63%; height: 4.14%; z-index: 2;"></a>
+                    <a href="https://police.willbes.net/pass/offPackage/show/prod-code/186107" target="_blank" title="이국령 헌법" style="position: absolute; left: 45.27%; top: 51.98%; width: 10.63%; height: 4.14%; z-index: 2;"></a>
+                    <a href="https://police.willbes.net/pass/offPackage/show/prod-code/186109" target="_blank" title="경행경채" style="position: absolute; left: 72.77%; top: 51.98%; width: 10.63%; height: 4.14%; z-index: 2;"></a>
                 </div>
                 <div id="tab02">
                     <img src="https://static.willbes.net/public/images/promotion/2021/10/2393_03_02.jpg" alt="4개월 슈퍼패스" />
-                    <a href="https://www.willbes.net/member/join/?ismobile=0&sitecode=2001" target="_blank" title="" style="position: absolute; left: 29.2%; top: 20.83%; width: 41.61%; height: 5.64%; z-index: 2;"></a>
+                    <a href="https://police.willbes.net/pass/offPackage/show/prod-code/186101" target="_blank" title="김원욱 헌법" style="position: absolute; left: 16.07%; top: 52.44%; width: 10.63%; height: 4.14%; z-index: 2;"></a>
+                    <a href="https://police.willbes.net/pass/offPackage/show/prod-code/186103" target="_blank" title="이국령 헌법" style="position: absolute; left: 44.73%; top: 52.44%; width: 10.63%; height: 4.14%; z-index: 2;"></a>
+
                 </div>
                 <div id="tab03">
                     <img src="https://static.willbes.net/public/images/promotion/2021/10/2393_03_03.jpg" alt="심화기술 패키지" />
-                    <a href="https://www.willbes.net/member/join/?ismobile=0&sitecode=2001" target="_blank" title="" style="position: absolute; left: 29.2%; top: 20.83%; width: 41.61%; height: 5.64%; z-index: 2;"></a>
+                    <a href="https://police.willbes.net/pass/offPackage/index/type/all?cate_code=3010&campus_ccd=605001&course_idx=1041" target="_blank" title="" style="position: absolute; left: 32.32%; top: 74.52%; width: 35.80%; height: 8%; z-index: 2;"></a>
                 </div>
                 <div id="tab04">
                     <img src="https://static.willbes.net/public/images/promotion/2021/10/2393_03_04.jpg" alt="기본이론 종합반" />
-                    <a href="https://www.willbes.net/member/join/?ismobile=0&sitecode=2001" target="_blank" title="" style="position: absolute; left: 29.2%; top: 20.83%; width: 41.61%; height: 5.64%; z-index: 2;"></a>
+                    <a href="https://police.willbes.net/pass/promotion/index/cate/3010/code/2364" target="_blank" title="" style="position: absolute; left: 29.38%; top: 75.71%; width: 41.96%; height: 8%; z-index: 2;"></a>
                 </div>
             </div>
         </div>
@@ -129,6 +153,30 @@
                 slidesImg.goToNextSlide();
             });
         });       
+
+        /*탭*/
+        $(document).ready(function(){
+            $('.tabs').each(function(){
+                var $active, $content, $links = $(this).find('a');
+                $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+                $active.addClass('active');
+                $content = $($active[0].hash);
+                $links.not($active).each(function () {
+                    $(this.hash).hide()
+                });
+
+                // Bind the click event handler
+                $(this).on('click', 'a', function(e){
+                    $active.removeClass('active');
+                    $content.hide();
+                    $active = $(this);
+                    $content = $(this.hash);
+                    $active.addClass('active');
+                    $content.show();
+                    e.preventDefault();
+                });
+            });
+        });
       
     </script>
 

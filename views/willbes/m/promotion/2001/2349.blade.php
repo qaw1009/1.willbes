807b7b6b15289e53b090602a339266af15407faf
김창용 <winmir@willbes.com>
2021-10-20 16:47:46 +0900

    경찰 11월 가입 이벤트 프로모션 모바일

---
+++ views/willbes/m/promotion/2001/2349.blade.php
@@ -0,0 +1,59 @@
+@extends('willbes.m.layouts.master')
+
+@section('content')
+<!-- Container -->
+
+<style type="text/css">
+    .evtCtnsBox {width:100%; max-width:720px; margin:0 auto; text-align:center; position:relative; font-size:14px; line-height:1.5; clear:both}
+    .evtCtnsBox img {width:100%; max-width:720px;}
+
+    /* 폰 가로, 태블릿 세로*/
+    @@media only screen and (max-width: 374px)  {   
+
+    }
+
+    /* 태블릿 세로 */
+    @@media only screen and (min-width: 375px) and (max-width: 640px) {       
+
+    }
+    
+    /* 태블릿 가로, PC */
+    @@media only screen and (min-width: 641px) {
+        
+    }
+</style>
+
+<div id="Container" class="Container NSK c_both">    
+
+    <div class="evtCtnsBox"  data-aos="fade-down">
+        <img src="https://static.willbes.net/public/images/promotion/2020/07/1556m_00.jpg" alt="경찰학원부분 1위" >        
+    </div> 
+
+    <div class="evtCtnsBox" data-aos="fade-up">
+        <img src="https://static.willbes.net/public/images/promotion/2021/08/2334m_top.jpg" alt="심화이론반" >
+    </div> 
+
+    <div class="evtCtnsBox" data-aos="fade-up">
+        <img src="https://static.willbes.net/public/images/promotion/2021/08/2334m_01.jpg" alt="9월 스타트" >
+    </div> 
+
+    <div class="evtCtnsBox" data-aos="fade-up">
+        <img src="https://static.willbes.net/public/images/promotion/2021/08/2334m_02.jpg" alt="심화이론 포인트" >
+    </div> 
+
+</div>
+
+<!-- End Container -->
+
+<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+      $( document ).ready( function() {
+        AOS.init();
+      } );
+</script>
+
+
+{{-- 프로모션용 스크립트 include --}}
+@include('willbes.pc.promotion.promotion_script')
+@stop

00cb971a1188bd2a559b7f77395402e163fec774
이윤구 <yoongukk@willbes.com>
2021-11-09 14:49:28 +0900

    (공무원) 실전덕후단 서울시1회 강좌전 프로모션 페이지 수정

--- views/willbes/pc/promotion/2003/2408.blade.php
+++ views/willbes/pc/promotion/2003/2408.blade.php
@@ -12,6 +12,7 @@
             margin-top:20px !important;
             padding:0 !important;
             background:#fff;
+            margin:0 auto;
         }
         .evtContent span {vertical-align:auto}
         .evtCtnsBox {width:100%; text-align:center; min-width:1120px; position:relative}
@@ -21,22 +22,13 @@
         /************************************************************/
 
         /*타이머*/
-        .time {width:1120px; margin:0 auto; text-align:center; padding:20px 0}
+        .time {width:1120px; margin:0 auto; text-align:center; padding:25px 0}
         .time ul {width:100%; display:flex; justify-content: center;}
-        .time li {line-height:61px; font-size:24px; margin-right:10px; position: relative;}
-        .time li:first-child,
-        .time li:last-child {line-height:1.3; color:#363635}
-        .time li:first-child {margin-right:20px}
-        .time li:last-child {margin-left:20px}
-        .time li:first-child span {color:#0086AC}      
-        .time li:last-child span {line-height:2.5; color:#363635;font-weight:bold;} 
-        .time li:last-child a {display:block; color:#fff; background:#242424; padding:10px 20px; margin-top:20px}
-        .time li img {width:44px}
-        .time .time_txt {color:#000; letter-spacing:-1px}
-        .time .time_txt span {color:#d63e4d; animation:upDown 2s infinite;-webkit-animation:upDown 2s infinite;}
-        .time .d_day {color:#fff;font-size:30px;} 
-
-        .evt00 {background:#f4f1f3}
+        .time li {font-size:30px;position: relative;}
+        .time li:first-child {margin-right:25px}
+        .time li:last-child {margin-left:25px}
+        .time li:first-child span {color:#fff;}        
+        .time li:last-child span {color:#fdeb00;font-weight:bold;}        
 
         .evtTop {background:url(https://static.willbes.net/public/images/promotion/2021/11/2408_top_bg.jpg) no-repeat center top;}
 
@@ -44,37 +36,26 @@
 
         .evt02 {background:#0186ad}
 
-        .evt03 {padding-bottom:100px;}     
+        .evt03 {padding-bottom:100px;}
+        .evt03 .sTitle {margin:50px 0 30px; font-size:25px; text-align:left; color:#464646}
         
-         /*타이머 스크롤*/
+        /*타이머 스크롤*/
         .jbMenu {display:none}
-        .jbMenu {top:0px; width:100%; background:#f4f1f3; display:block; z-index:100}
+        .jbMenu {top:0px; width:100%; background:#000; display:block; z-index:100}
         .jbFixed {position:fixed; top: 0px}
 
     </style>
 
-    <div class="p_re evtContent NSK" id="evtContainer">   
+    <div class="p_re evtContent NSK" id="evtContainer">
 
-        <div class="evtCtnsBox evt00 jbMenu cf">
+        <div class="evtCtnsBox jbMenu"> 
             <div class="time NSK-Black" id="newTopDday">
                 <ul>
                     <li>
-                        <span>2022.2.26</span><br>
-                              서울시 1회 시험까지
-                    </li>
-                    <li><img id="dd1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
-                    <li><img id="dd2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
-                    <li class="time_txt">일</li>
-                    <li><img id="hh1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
-                    <li><img id="hh2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
-                    <li class="time_txt">:</li>
-                    <li><img id="mm1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
-                    <li><img id="mm2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
-                    <li class="time_txt">:</li>
-                    <li><img id="ss1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
-                    <li><img id="ss2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                        <span>2022.2.26 서울시 1회 시험까지</span>                    
+                    </li>                   
                     <li>
-                        <span class="NSK">남았습니다.</span>                        
+                        <span class="NSK">D-123</span>                        
                     </li>          
                 </ul>
             </div> 
@@ -95,13 +76,14 @@
         <div class="evtCtnsBox evt03" data-aos="fade-up">
             <div class="wrap">
                 <img src="https://static.willbes.net/public/images/promotion/2021/11/2408_03.jpg" alt="패키지" />
-                <a href="https://pass.willbes.net/package/show/cate/3019/pack/648001/prod-code/187287" target="_blank" title="수강신청하기" style="position: absolute;left: 31.98%;top: 66.33%;width: 32.46%;height: 7.77%;z-index: 2;"></a>       
+                <a href="https://pass.willbes.net/package/show/cate/3019/pack/648001/prod-code/187287" target="_blank" title="수강신청하기" style="position: absolute;left: 31.98%;top: 61.33%;width: 32.46%;height: 7.77%;z-index: 2;"></a>       
+                <div class="sTitle NSK-Black">실전덕후단 서울시 1회 단과 수강신청</div>
                 @if(empty($arr_base['display_product_data']) === false)
                     @include('willbes.pc.promotion.display_product_partial',array('group_num'=>1))
-                @endif                
+                @endif      
             </div>
         </div>
-        
+                
     </div>
     <form id="regi_form" name="regi_form" method="POST" onsubmit="return false;" novalidate>
         {!! csrf_field() !!}

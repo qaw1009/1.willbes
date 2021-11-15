0f4a093ed8e260097d33bfaeb2d15f65854bb2f1
신봉석 <bsshin@willbes.com>
2021-11-15 11:52:31 +0900

    wbs > 로그인잠금해제 수정 (btob 관리자 해제가능여부 조건 수정)

--- application/models/wbs/sys/LoginLockModel.php
+++ application/models/wbs/sys/LoginLockModel.php
@@ -32,7 +32,10 @@ class LoginLockModel extends WB_Model
         } else {
             $in_column = 'L.wLockIdx, L.wAdminId, L.wLockSubDomain, L.wLockDatm, L.wLockIp, L.wUnLockDatm, L.wUnLockAdminIdx
                 , ifnull(A.wAdminName, "비운영자") as wAdminName, A.wRoleIdx, A.wIsUse
-                , if(A.wAdminIdx is null, "X", if(L.wUnLockDatm is null, "N", "Y")) as wIsUnLock
+                , (case L.wLockSubDomain 
+                    when "btob" then if(L.wUnLockDatm is null, "N", "Y")
+                    else if(A.wAdminIdx is null, "X", if(L.wUnLockDatm is null, "N", "Y"))
+                    end) as wIsUnLock
                 , R.wRoleName
                 , UA.wAdminId as wUnLockAdminId, UA.wAdminName as wUnLockAdminName';
 

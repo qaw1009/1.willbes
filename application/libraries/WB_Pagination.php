845d04d01cc072ecdaff59cface2a2568b7efd75
최현탁 <cht@willbes.com>
2021-11-22 14:09:35 +0900

    프론트 pagination 라이브러리 수정

--- application/libraries/WB_Pagination.php
+++ application/libraries/WB_Pagination.php
@@ -98,7 +98,7 @@ class WB_Pagination extends CI_Pagination
 			show_error('Your number of links must be a positive number.');
 		}
 
-		if ( ! is_numeric($this->cur_page)) {
+		if ( ! is_numeric($this->cur_page) || ($this->cur_page <= 0)) {
 			$this->cur_page = $base_page;
 		}
 

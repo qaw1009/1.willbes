4d2ab53901414a1eb9ad41c866f24a8d21b04391
최현탁 <cht@willbes.com>
2021-11-30 18:05:57 +0900

    프로모션 2416 개발 : 겨울추천강좌 이벤트

--- views/willbes/pc/promotion/2001/2416.blade.php
+++ views/willbes/pc/promotion/2001/2416.blade.php
@@ -149,11 +149,10 @@
                     <div class="slide_con">
                         <ul id="slidesImg3">
                             @for($i=1;$i<=12;$i++)
-                                <li>
-                                    <img src="https://static.willbes.net/public/images/promotion/2021/11/2416_07_{{(strlen($i) > 1 ? '' : '0').$i}}.png" alt="" />
-                                    <a href="{{ (sess_data('is_login') === false ? 'javascript:void(0);' : (empty($file_yn) === false && $file_yn[$i] == 'Y' ? front_url($file_link[$i]) : $file_link[$i])) }}"
-                                       onclick="{{ (sess_data('is_login') === false ? 'javascript:alert(\'로그인 후 이용해주세요.\'); return false;' : '') }}" title="배경화면 다운로드" style="position: absolute;left: 0;top: 90.55%;width: 100%;height: 11.21%;z-index: 2;"></a>
-                                </li>
+                                <a href="{{ (sess_data('is_login') === false ? 'javascript:void(0);' : (empty($file_yn) === false && $file_yn[$i] == 'Y' ? front_url($file_link[$i]) : $file_link[$i])) }}"
+                                   onclick="{{ (sess_data('is_login') === false ? 'javascript:alert(\'로그인 후 이용해주세요.\'); return false;' : '') }}" title="배경화면 다운로드" style="position: absolute;left: 0;top: 90.55%;width: 100%;height: 11.21%;z-index: 2;" target="_blank">
+                                    <li><img src="https://static.willbes.net/public/images/promotion/2021/11/2416_07_{{(strlen($i) > 1 ? '' : '0').$i}}.png" alt="" /></li>
+                                </a>
                             @endfor
                         </ul>
                         <p class="leftBtn"><a id="imgBannerLeft3"><img src="https://static.willbes.net/public/images/promotion/2021/11/2416_left.png"></a></p>

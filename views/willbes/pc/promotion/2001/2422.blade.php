595268195cd85a4e4f820e560d9e8bd625ae88ca
이윤구 <yoongukk@willbes.com>
2021-12-01 13:57:19 +0900

    (경찰) 김원욱 헌법 200% 활용하 페이지 수정

--- views/willbes/pc/promotion/2001/2422.blade.php
+++ views/willbes/pc/promotion/2001/2422.blade.php
@@ -25,7 +25,7 @@
 
         .evt00 {background:#0a0a0a}
 
-        .wb_top {background:url(https://static.willbes.net/public/images/promotion/2021/11/2422_top_bg.jpg) no-repeat center; position:relative}  
+        .wb_top {background:url(https://static.willbes.net/public/images/promotion/2021/12/2422_top_bg.jpg) no-repeat center; position:relative}  
         .wb_top .youtube {position:absolute; top:946px; left:50%; width:978px; z-index:1;margin-left:-489px; box-shadow:0 10px 20px rgba(0,0,0,.3);}
         .wb_top .youtube iframe {width:978px; height:505px}      
         
@@ -63,14 +63,8 @@
             <img src="https://static.willbes.net/public/images/promotion/2020/07/1009_first.jpg"  alt="경찰학원부분 1위"/>
         </div>    
 
-        <div class="evtCtnsBox wb_top" id="main">
-            <div class="wrap">
-                <img src="https://static.willbes.net/public/images/promotion/2021/11/2422_top.jpg" alt="김원욱 경찰헌법" usemap="#Map1990B" border="0"/>
-                <a href="https://police.willbes.net/professor/show/cate/3001/prof-idx/51146?subject_idx=1049&subject_name=%ED%97%8C%EB%B2%95%2822%EB%85%84%EB%8C%80%EB%B9%84%29" target="_blank" alt="교수홈 바로가기" style="position: absolute; left: 70.89%; top: 30.2%; width: 17.95%; height: 2.69%; z-index: 2;"></a>
-            </div>
-            <div class="youtube" data-aos="fade-up">
-                <iframe src="https://www.youtube.com/embed/B_SpAwQNwio?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>	
+        <div class="evtCtnsBox wb_top" id="main">            
+            <img src="https://static.willbes.net/public/images/promotion/2021/12/2422_top.jpg" alt="김원욱 경찰헌법"/>             
 		</div>
 
         <div class="evtCtnsBox wb_01" >
@@ -101,7 +95,7 @@
 		</div>        
 
         <div class="evtCtnsBox wb_04" id="evt04">
-            <img src="https://static.willbes.net/public/images/promotion/2021/11/2422_04.jpg"  alt="김원욱 헌법 최신강의"/>
+            <img src="https://static.willbes.net/public/images/promotion/2021/12/2422_04.jpg"  alt="김원욱 헌법 최신강의"/>
             @if(empty($arr_base['display_product_data']) === false)
                 @include('willbes.pc.promotion.display_product_partial',array('group_num'=>1))
             @endif	
@@ -110,7 +104,7 @@
         <div class="evtCtnsBox wb_05" >
             <img src="https://static.willbes.net/public/images/promotion/2021/11/2422_05_01.jpg" alt="경찰헌법 기본이론"/>
             <div class="youtube" data-aos="fade-right">
-                <iframe src="https://www.youtube.com/embed/xCaNQU2R_h8?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/UB91DCctYgU?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>	
             <img src="https://static.willbes.net/public/images/promotion/2021/11/2422_05_02.jpg" alt="경찰헌법 심화이론"/>
             <div class="youtube" data-aos="fade-right">
@@ -118,7 +112,7 @@
             </div>
             <img src="https://static.willbes.net/public/images/promotion/2021/11/2422_05_03.jpg" alt="경찰헌법 심화기출"/>
             <div class="youtube" data-aos="fade-right">
-                <iframe src="https://www.youtube.com/embed/RGS2dBNNjss?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/xpxf6BfraTQ?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
             <img src="https://static.willbes.net/public/images/promotion/2021/11/2422_05_04.jpg"  alt="경찰헌법 문제풀이"/><br>
             <div class="comingsoon"><img src="https://static.willbes.net/public/images/promotion/2021/11/2422_05_05.jpg" alt=""/></div>

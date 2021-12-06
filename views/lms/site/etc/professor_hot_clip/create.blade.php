d23ee6a63ee1f9d9ea8ad242c6c4d7ec18e035e3
최현탁 <cht@willbes.com>
2021-12-06 10:34:10 +0900

    lms 임용 핫클립 기능 추가

--- views/lms/site/etc/professor_hot_clip/create.blade.php
+++ views/lms/site/etc/professor_hot_clip/create.blade.php
@@ -5,8 +5,8 @@
     <form class="form-horizontal form-label-left" id="regi_form" name="regi_form" method="POST" enctype="multipart/form-data" onsubmit="return false;" novalidate>
         {!! csrf_field() !!}
         {!! method_field($method) !!}
-        <input type="hidden" name="site_code" value="2017"/>    {{-- 임용온라인전용 --}}
         <input type="hidden" name="cate_code" value="{{ $data['CateCode'] }}" title="카테고리 선택" required="required"/>
+        <input type="hidden" name="temp_view_type"/>
         <input type="hidden" name="idx" value="{{ $idx }}"/>
         <span id="del_thumbnail_inputbox"></span>
 
@@ -20,6 +20,44 @@
                 <div class="clearfix"></div>
             </div>
             <div class="x_content">
+                <div class="form-group">
+                    <label class="control-label col-md-1-1" for="site_code">운영사이트<span class="required">*</span></label>
+                    <div class="col-md-4 form-inline item">
+                        {!! html_site_select('2017', 'site_code', 'site_code', '', '운영 사이트', 'required', '', true) !!}
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-md-1-1">그룹선택 <span class="required">*</span>
+                    </label>
+                    <div class="col-md-9 form-inline item">
+                        <select class="form-control" id="hotclip_group_idx" name="hotclip_group_idx" required="required" title="그룹">
+                            <option value="">그룹선택</option>
+                            <optgroup label="==메인==">
+                                @foreach($group_list as $row)
+                                    @if($row['ViewType'] == 1)
+                                        <option class="{{ $row['SiteCode'] }}"
+                                                data-view-type="{{ $row['ViewType'] }}"
+                                                value="{{ $row['PhcgIdx'] }}" {{ ($row['PhcgIdx'] == $data['PhcgIdx'] ? 'selected="selected"' : '') }}>
+                                            {{ $row['Title'] }} [{{ ($row['IsUse'] == 'Y' ? '사용' : '미사용') }}]
+                                        </option>
+                                    @endif
+                                @endforeach
+                            </optgroup>
+                            <optgroup label="==이벤트==">
+                                @foreach($group_list as $row)
+                                    @if($row['ViewType'] == 2)
+                                        <option class="{{ $row['SiteCode'] }}"
+                                                data-view-type="{{ $row['ViewType'] }}"
+                                                value="{{ $row['PhcgIdx'] }}" {{ ($row['PhcgIdx'] == $data['PhcgIdx'] ? 'selected="selected"' : '') }}>
+                                            {{ $row['Title'] }} [{{ ($row['IsUse'] == 'Y' ? '사용' : '미사용') }}]
+                                        </option>
+                                    @endif
+                                @endforeach
+                            </optgroup>
+                        </select>
+                    </div>
+                </div>
                 <div class="form-group">
                     <label class="control-label col-md-1-1">카테고리정보 <span class="required">*</span>
                     </label>
@@ -38,10 +76,12 @@
                     <div class="col-md-9 form-inline">
                         <button type="button" id="btn_prof_subject_search" class="btn btn-sm btn-primary">과목/교수검색</button>
                         <span id="selected_prof_subject" class="pl-10">
+                            @if($method == 'PUT' && empty($data['ProfSubjectName']) === false)
                             <span class="pr-10">{{ $data['ProfSubjectName'] }}
                                 <a href="javascript:void(0);" data-prof-subject-idx="{{ $data['ProfSubjectIdx'] }}" class="selected-prof-subject-delete"><i class="fa fa-times red"></i></a>
                                 <input type="hidden" name="prof_subject_idx[]" value="{{ $data['ProfSubjectIdx'] }}"/>
                             </span>
+                            @endif
                         </span>
                         <p class="form-control-static"># 단일 선택만 가능합니다.</p>
                     </div>
@@ -87,6 +127,105 @@
                         <div class="radio">
                             <input type="radio" id="studycomment_btn_is_use_y" name="studycomment_btn_is_use" class="flat" value="Y" required="required" title="사용여부" @if($method == 'POST' || $data['StudyCommentBtnIsUse']=='Y')checked="checked"@endif/> <label for="studycomment_btn_is_use_y" class="input-label">사용</label>
                             <input type="radio" id="studycomment_btn_is_use_n" name="studycomment_btn_is_use" class="flat" value="N" @if($data['StudyCommentBtnIsUse']=='N')checked="checked"@endif/> <label for="studycomment_btn_is_use_n" class="input-label">미사용</label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="event">
+                    <div class="form-group">
+                        <label class="control-label col-md-1-1" for="subject_idx">임용 온라인강좌<span class="required">*</span></label>
+                        <div class="col-md-10">
+                            <button type="button" class="btn btn-sm btn-primary btn_product_search" data-site-code="2017" data-product-type="on" data-target-id="online_box">상품검색</button>
+                            <span id="online_box" class="hide"></span>
+                            <table class="table table-striped table-bordered mt-15" id="table_product_on">
+                                <thead>
+                                <tr>
+                                    <th>강좌명</th>
+                                    <th>상품종류</th>
+                                    <th>논술포함여부</th>
+                                    <th>DP순서</th>
+                                    <th>삭제</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @if (empty($list_product['636001']) === false)
+                                    @php $i=1; @endphp
+                                    @foreach($list_product['636001'] as $row)
+                                        <tr>
+                                            <td>
+                                                <input type="hidden" name="pp_idx[]" value="{{$row['PpIdx']}}">
+                                                <input type="hidden" name="prod_code[]" value="{{$row['ProdCode']}}">
+                                                [{{$row['ProdCode']}}] {{$row['ProdName']}}
+                                            </td>
+                                            <td>{{$row['LearnPatternCcdName']}}</td>
+                                            <td class="form-inline">
+                                                <select class="form-control" name="is_essay[]">
+                                                    <option value="Y" {{ ($row['IsEssay'] == 'Y' ? 'selected="selected"' : '') }}>포함</option>
+                                                    <option value="N" {{ ($row['IsEssay'] == 'N' ? 'selected="selected"' : '') }}>미포함</option>
+                                                </select>
+                                            </td>
+                                            <td>
+                                                <input type="text" name="order_num[]" value="{{$row['OrderNum']}}">
+                                            </td>
+                                            <td>
+                                                <a href="#none" class="btn-product-delete" data-pp-idx="{{$row['PpIdx']}}">
+                                                    <i class="fa fa-times fa-lg red"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        @php $i++; @endphp
+                                    @endforeach
+                                @endif
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-md-1-1" for="subject_idx">임용 학원강좌<span class="required">*</span></label>
+                        <div class="col-md-10">
+                            <button type="button" class="btn btn-sm btn-primary btn_product_search" data-site-code="2018" data-product-type="off" data-target-id="off_box">상품검색</button>
+                            <span id="off_box" class="hide"></span>
+                            <table class="table table-striped table-bordered mt-15" id="table_product_off">
+                                <thead>
+                                <tr>
+                                    <th>강좌명</th>
+                                    <th>상품종류</th>
+                                    <th>논술포함여부</th>
+                                    <th>DP순서</th>
+                                    <th>삭제</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @if (empty($list_product['636002']) === false)
+                                    @php $i=1; @endphp
+                                    @foreach($list_product['636002'] as $row)
+                                        <tr>
+                                            <td>
+                                                <input type="hidden" name="pp_idx[]" value="{{$row['PpIdx']}}">
+                                                <input type="hidden" name="prod_code[]" value="{{$row['ProdCode']}}">
+                                                [{{$row['ProdCode']}}] {{$row['ProdName']}}
+                                            </td>
+                                            <td>{{$row['LearnPatternCcdName']}}</td>
+                                            <td class="form-inline">
+                                                <select class="form-control" name="is_essay[]">
+                                                    <option value="Y" {{ ($row['IsEssay'] == 'Y' ? 'selected="selected"' : '') }}>포함</option>
+                                                    <option value="N" {{ ($row['IsEssay'] == 'N' ? 'selected="selected"' : '') }}>미포함</option>
+                                                </select>
+                                            </td>
+                                            <td>
+                                                <input type="text" name="order_num[]" value="{{$row['OrderNum']}}">
+                                            </td>
+                                            <td>
+                                                <a href="#none" class="btn-product-delete" data-pp-idx="{{$row['PpIdx']}}">
+                                                    <i class="fa fa-times fa-lg red"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        @php $i++; @endphp
+                                    @endforeach
+                                @endif
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
@@ -188,7 +327,22 @@
 
     <script type="text/javascript">
         var $regi_form = $('#regi_form');
+
         $(document).ready(function() {
+            $regi_form.find('select[name="hotclip_group_idx"]').chained("#site_code");
+
+            var view_type = $("#hotclip_group_idx").find("option:selected").data("view-type");
+            if (typeof view_type === 'undefined') {
+                $('.event').hide();
+            } else {
+                viewType_Onoff(view_type);
+            }
+
+            $regi_form.on('change','select[name="hotclip_group_idx"]', function () {
+                var view_type = $(this).find("option:selected").data("view-type");
+                viewType_Onoff(view_type);
+            });
+
             // 카테고리 검색
             $('#btn_category_search').on('click', function() {
                 var site_code = $regi_form.find('input[name="site_code"]').val();
@@ -275,10 +429,101 @@
                 }, showError, false, 'POST');
             });
 
+            // 강좌 검색
+            $('.btn_product_search').on('click', function(event) {
+                var site_code = $(this).data("site-code");
+                var prod_type = $(this).data("product-type");
+                var target_id = $(this).data("target-id");
+                var target_field = 'prod_code';
+                $('.btn_product_search').setLayer({
+                    'url' : '{{ site_url('/common/searchLectureAll/') }}?site_code=' + site_code + '&prod_type='+prod_type+'&return_type=inline&target_id='+target_id+'&target_field='+target_field+'&is_event=Y',
+                    'width' : 1200
+                });
+            });
+
+            //강좌개별삭제
+            $regi_form.on('click', '.btn-product-delete', function() {
+                var _url = '{{ site_url("/site/etc/professorHotClip/deleteProduct") }}';
+                var data = {
+                    '{{ csrf_token_name() }}' : $regi_form.find('input[name="{{ csrf_token_name() }}"]').val(),
+                    '_method' : 'DELETE',
+                    'pp_idx' : $(this).data('pp-idx')
+                };
+                if (!confirm('정말로 삭제하시겠습니까?')) {
+                    return;
+                }
+                sendAjax(_url, data, function(ret) {
+                    if (ret.ret_cd) {
+                        notifyAlert('success', '알림', ret.ret_msg);
+                        location.reload();
+                    }
+                }, showError, false, 'POST');
+            });
+
+            $regi_form.on('change', '#online_box', function() {
+                var code, data, html = '';
+                var dp_prod_num = 0;
+                var $tbody = $('#table_product_on tbody');
+                $(this).find('input[name="prod_code[]"]').each(function() {
+                    code = $(this).val();
+                    data = $(this).data();
+                    html += '<tr>';
+                    html += '	<td>';
+                    html += '		<input type="hidden" name="pp_idx[]" value=""> [' + code + '] ' + Base64.decode(data.prodName);
+                    html += '		<input type="hidden" name="prod_code[]" value="' + code + '">';
+                    html += '	</td>';
+                    html += '	<td>' + data.learnPatternCcdName + '</td>';
+                    html += '	<td class="form-inline">';
+                    html += '		<select class="form-control" name="is_essay[]"><option>포함</option><option>미포함</option></select>';
+                    html += '	</td>';
+                    html += '	<td class="form-inline">';
+                    html += '		<input type="text" class="form-control" name="order_num[]" value="999">';
+                    html += '	</td>';
+                    html += '	<td>';
+                    html += '       <a href="javascript:void(0);" class="temp-product-delete"><i class="fa fa-times red"></i></a>';
+                    html += '	</td>';
+                    html += '</tr>';
+                });
+                $(this).html('');    // 임시 데이터 삭제
+                $tbody.append(html);
+            });
+
+            $regi_form.on('change', '#off_box', function() {
+                var code, data, html = '';
+                var dp_prod_num = 0;
+                var $tbody = $('#table_product_off tbody');
+                $(this).find('input[name="prod_code[]"]').each(function() {
+                    code = $(this).val();
+                    data = $(this).data();
+                    html += '<tr>';
+                    html += '	<td>';
+                    html += '		<input type="hidden" name="pp_idx[]" value=""> [' + code + '] ' + Base64.decode(data.prodName);
+                    html += '		<input type="hidden" name="prod_code[]" value="' + code + '">';
+                    html += '	</td>';
+                    html += '	<td>' + data.learnPatternCcdName + '</td>';
+                    html += '	<td class="form-inline">';
+                    html += '		<select class="form-control" name="is_essay[]"><option>포함</option><option>미포함</option></select>';
+                    html += '	</td>';
+                    html += '	<td class="form-inline">';
+                    html += '		<input type="text" class="form-control" name="order_num[]" value="999">';
+                    html += '	</td>';
+                    html += '	<td>';
+                    html += '       <a href="javascript:void(0);" class="temp-product-delete"><i class="fa fa-times red"></i></a>';
+                    html += '	</td>';
+                    html += '</tr>';
+                });
+                $(this).html('');    // 임시 데이터 삭제
+                $tbody.append(html);
+            });
+
+            $regi_form.on('click', '.temp-product-delete', function() {
+                var that = $(this);
+                that.parent().parent().remove();
+            });
+
             // ajax submit
             $regi_form.submit(function() {
                 var _url = '{{ site_url("/site/etc/professorHotClip/store") }}' + getQueryString();
-
                 ajaxSubmit($regi_form, _url, function(ret) {
                     if(ret.ret_cd) {
                         notifyAlert('success', '알림', ret.ret_msg);
@@ -308,6 +553,19 @@
                 location.href='{{ site_url("/site/etc/professorHotClip/index") }}' + getQueryString();
             });
         });
+
+        function viewType_Onoff(view_type) {
+            if (view_type == '1') {
+                $regi_form.find('input[name="temp_view_type"]').val(view_type);
+                $('.event').hide();
+            } else if (view_type == '2'){
+                $regi_form.find('input[name="temp_view_type"]').val(view_type);
+                $('.event').show();
+            } else {
+                $regi_form.find('input[name="temp_view_type"]').val('');
+                $('.event').hide();
+            }
+        }
 
         function addValidate() {
             if ($regi_form.find('input[name="prof_subject_idx[]"]').length >= 2) {

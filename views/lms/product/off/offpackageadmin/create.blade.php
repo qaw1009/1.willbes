ff09f40735153da2b5d0eaf03ecd1184792120b3
조규호 <benoo@willbes.com>
2021-12-16 09:07:19 +0900

    lms-> 종합반 그룹번호 수정

--- views/lms/product/off/offpackageadmin/create.blade.php
+++ views/lms/product/off/offpackageadmin/create.blade.php
@@ -308,7 +308,7 @@
                                         <input type='hidden'  name='IsEssential[]' id='IsEssential{{$loop->index}}' value='Y'>
                                         <td>
                                             <select name='SubGroupName[]' id='SubGroupNamel{{$loop->index}}' class="form-control mr-10">
-                                                @for($i=1;$i<= 30;$i++)
+                                                @for($i=1;$i<= 100;$i++)
                                                     <option value='{{$i}}' @if($i ==$row['SubGroupName']) selected="selected" @endif>{{$i}}</option>
                                                 @endfor
                                             </select>
@@ -380,7 +380,7 @@
                                         <input type='hidden'  name='IsEssential[]' id='IsEssential{{$loop->index}}' value='N'>
                                         <td>
                                             <select name='SubGroupName[]' id='SubGroupNamel{{$loop->index}}' class="form-control mr-10">
-                                                @for($i=1;$i<=30;$i++)
+                                                @for($i=1;$i<=100;$i++)
                                                     <option value='{{$i}}' @if($i ==$row['SubGroupName']) selected="selected" @endif>{{$i}}</option>
                                                 @endfor
                                             </select>

14d07de8369d64d24f820e9b430bb8785493b550
최현탁 <cht@willbes.com>
2021-12-21 16:10:22 +0900

    프론트 자격증 공인노무사 메인 리뉴얼 : 테스트 페이지 반영

---
+++ views/willbes/pc/site/_viewTest/main_partial/content_menu_2006_309003.blade.php
@@ -0,0 +1,9 @@
+<div class="go_MenuBtns">
+    <ul id="go_MenuBtns">
+        <li><a href="{{ front_url('/offLecture/index?cate_code=3112', true) }}"><img src="https://static.willbes.net/public/images/promotion/main/2005/2005_icon02.png" alt=""><span>학원수강신청</span></a></li>
+        <li><a href="{{ front_url('/offinfo/boardInfo/index/82?on_off_link_cate_code='.$__cfg['CateCode'].'&s_cate_code_disabled=Y', true) }}"><img src="https://static.willbes.net/public/images/promotion/main/2005/2005_icon04.png" alt=""><span>강의실배정표</span></a></li>
+        <li><a href="{{ front_url('/support/qna/index?s_cate_code='.$__cfg['CateCode'].'&s_cate_code_disabled=Y') }}"><img src="https://static.willbes.net/public/images/promotion/main/2005/2005_icon05.png" alt=""><span>학원1:1상담</span></a></li>
+        <li><a href="{{ front_url('/support/qna/index?s_cate_code='.$__cfg['CateCode'].'&s_cate_code_disabled=Y') }}"><img src="https://static.willbes.net/public/images/promotion/main/2005/2005_icon06.png" alt=""><span>동영상1:1상담</span></a></li>
+        <li><a href="{{ front_url('/offinfo/boardInfo/index/110?on_off_link_cate_code='.$__cfg['CateCode'].'&s_cate_code_disabled=Y', true) }}"><img src="https://static.willbes.net/public/images/promotion/main/2005/2005_icon07.png" alt=""><span>강의자료실</span></a></li>
+    </ul>
+</div>

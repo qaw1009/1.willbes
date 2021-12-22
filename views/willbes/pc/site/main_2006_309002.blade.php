e96277e685a197b00da3689fbfd69c0cc5a0d263
김창용 <winmir@willbes.com>
2021-12-22 15:41:07 +0900

    자격증 노무, 감평, 변리사 메인

--- views/willbes/pc/site/main_2006_309002.blade.php
+++ views/willbes/pc/site/main_2006_309002.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <!-- Container -->
-    <div id="Container" class="Container nomu NGR c_both">
+    <div id="Container" class="Container job nomu NGR c_both">
         <!-- site nav -->
         @include('willbes.pc.layouts.partial.site_menu')
 

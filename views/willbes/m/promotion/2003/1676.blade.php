29b3b2d64ad43530ee7ccf659d34f4eff8db8fed
이윤구 <yoongukk@willbes.com>
2022-01-11 17:17:57 +0900

    (공무원) 더켠의 반반똑똑영어 1~2월 방송 진행을 위한 페이지 수정

--- views/willbes/m/promotion/2003/1676.blade.php
+++ views/willbes/m/promotion/2003/1676.blade.php
@@ -135,17 +135,17 @@
         <img src="https://static.willbes.net/public/images/promotion/2020/12/1676m_02.jpg" alt="" >
         <ul>
             <li>
-                <a href="https://pass.willbes.net/m/lecture/show/cate/3019/pattern/only/prod-code/185911" target="_blank">
+                <a href="https://pass.willbes.net/m/lecture/show/cate/3019/pattern/only/prod-code/190319" target="_blank">
                     <span>반반모고&똑똑영어</span>
-                    <span>12월 방송</span>
+                    <span>1~2월 방송</span>
                     <span>다시보기</span>
                     <span><strong>6만원으로</strong>방송 다시보기 ></span>
                 </a>
             </li>
             <li>
-                <a href="https://pass.willbes.net/m/package/show/cate/3019/pack/648001/prod-code/185909" target="_blank">
+                <a href="javascript:alert('Coming Soon!')" >
                     <span>반반모고&똑똑영어</span>
-                    <span>7~12월 방송</span>
+                    <span>1~4월 방송</span>
                     <span>다시보기</span>
                     <span><strong>19만원으로</strong>방송 다시보기 ></span>
                 </a>
@@ -260,7 +260,7 @@
 
             <p class="NSK-Black"><span>●</span> 열공 출첵 이벤트 관련</p>
             <ul>
-                <li>본 이벤트는 로그인 후 참여 가능하며, 12/13(월)~1/7(금)까지 총 20회 진행됩니다. <span> (*토/일 제외)</span></li>
+                <li>본 이벤트는 로그인 후 참여 가능하며, 1/17(월)~2/18(금)까지 총 20회 진행됩니다. <span> (*토/일 제외)</span></li>
                 <li>출석체크 가능 시간 반반 21:00~22:00, 똑똑 21:00~21:40, 모의고사 해설 LIVE (12/20(월), 1/3(월) 19:00~20:00 내에 페이지 새로고침 (F5) 후 출석체크 버튼 클릭 시 정상 인정되며, 방송이 종료되지 않더라도 해당 시간 이외에는 출석으로 인정되지 않습니다.</li>
                 <li>당첨자 안내 공지는 1/10(월) 윌비스 공무원 공지사항을 통해 확인하실 수 있습니다.</li>
             </ul> 

6a872981e325e2c893bcae00dc2eefdbfc86e87a
김창용 <winmir@willbes.com>
2022-02-08 17:22:27 +0900

    프로모션 게시판 수정

--- views/willbes/pc/promotion/board_list_ajax.blade.php
+++ views/willbes/pc/promotion/board_list_ajax.blade.php
@@ -1,5 +1,17 @@
+<style>
+    .postWrap {width:1000px; margin:0 auto; line-height:1.5; text-align:left; font-size:12px}
+    .postList > h4 {font-size:14px; padding:10px 50px 0 0; position: relative; color:#216a4f}
+    .postList > h4 strong {background:#216a4f; color:#fff; padding:3px 6px; border-radius:0 4px 4px 0; margin-right:10px}
+    .postList > h4 a {position: absolute; top:10px; right:10px; font-size:12px; background:#333; color:#fff; padding:3px 6px; border-radius:4px}
+    .postList .postSub {padding:10px 0; border-bottom:1px solid #e0e0e0; color:#666; display:flex; justify-content: space-between;}
+    .postList .postSub span {padding:0 10px; border-right:1px solid #ccc}
+    .postList .postSub span:last-child {border:0}
+    .postWrap .postContent {border-bottom:1px solid #999; background:#fafafa; padding:10px; color:#666}
+    .postWrap .postContent .btn-more-text {cursor: pointer; font-weight:bold; color:#000; display:inline-block; box-shadow:0 -10px 0 #e5fcf4 inset }
+</style>
+
 @if(empty($list) === false)
-    <div class="evtCtnsBox evt02" data-aos="fade-up">
+    <div class="evtCtnsBox mt100 mb100 NG" data-aos="fade-up">
         <div class="postWrap">
             @foreach($list as $row)
                 <div class="postList">
@@ -25,7 +37,7 @@
                     </div>
                     <div class="postContent minimum-text">
                         {!! nl2br(mb_substr($row['Content'],0,150,'UTF-8')) !!}
-                        {!! (mb_strlen($row['Content'],'UTF-8') > 150 ? '<span class="btn-more-text" style="cursor: pointer; font-size: 15px; font-weight:bold;">...더보기</span>' : '') !!}
+                        {!! (mb_strlen($row['Content'],'UTF-8') > 150 ? '<span class="btn-more-text">...더보기</span>' : '') !!}
                     </div>
                     <div class="postContent full-text" style="display: none;">
                         {!! nl2br($row['Content']) !!}

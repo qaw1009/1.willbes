2a8d39fa79bbd624214b410fd127cbffe9d96ca9
김창용 <winmir@willbes.com>
2022-02-11 13:37:52 +0900

    변리사 메인 수험정보 링크 수정

--- views/willbes/pc/site/main_partial/content_menu_2006_309004.blade.php
+++ views/willbes/pc/site/main_partial/content_menu_2006_309004.blade.php
@@ -1,7 +1,7 @@
 <div class="go_MenuBtns">
     <ul id="go_MenuBtns">
         <li><a href="{{ front_url('/support/gosiNotice/index/cate/'.$__cfg['CateCode'].'?s_cate_code='.$__cfg['CateCode'].'&s_cate_code_disabled=Y') }}"><img src="https://static.willbes.net/public/images/promotion/main/2006/main_icon01.png" alt=""><span>학원공지사항</span></a></li>
-        <li><a href="https://job.willbes.net/landing/show/lcode/1009/cate/309004"><img src="https://static.willbes.net/public/images/promotion/main/2006/main_icon02.png" alt=""><span>수험정보</span></a></li>
+        <li><a href="{{ front_url('/support/examNews/index/cate/'.$__cfg['CateCode'].'?s_cate_code='.$__cfg['CateCode'].'&s_cate_code_disabled=Y') }}"><img src="https://static.willbes.net/public/images/promotion/main/2006/main_icon02.png" alt=""><span>수험정보</span></a></li>
         <li><a href="{{ front_url('/support/qna/index', true) }}"><img src="https://static.willbes.net/public/images/promotion/main/2006/main_icon03.png" alt=""><span>학원1:1상담</span></a></li>
         <li><a href="{{ front_url('/support/qna/index?s_cate_code='.$__cfg['CateCode'].'&s_cate_code_disabled=Y') }}"><img src="https://static.willbes.net/public/images/promotion/main/2006/main_icon04.png" alt=""><span>동영상1:1상담</span></a></li>
         <li><a href="{{ front_url('/offinfo/boardInfo/index/110?on_off_link_cate_code='.$__cfg['CateCode'].'&s_cate_code_disabled=Y', true) }}"><img src="https://static.willbes.net/public/images/promotion/main/2006/main_icon05.png" alt=""><span>강의자료실</span></a></li>

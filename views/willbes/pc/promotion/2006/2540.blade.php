f5445f18dc5cd1aa0638c913af4f557656de825b
이윤구 <yoongukk@willbes.com>
2022-02-15 16:13:05 +0900

    (자격증) [공인노무사] 2차 GS0 순환 할인 프로모션 페이지(pc/m) 신규

---
+++ views/willbes/pc/promotion/2006/2540.blade.php
@@ -0,0 +1,128 @@
+@extends('willbes.pc.layouts.master')
+
+@section('content')
+    @include('willbes.pc.layouts.partial.site_menu')
+    <!-- Container -->
+    <style type="text/css">
+        .evtContent {
+            width:100%;
+            min-width:1120px !important;
+            max-width:2000px !important;
+            margin:20px auto 0;
+            padding:0 !important;
+            background:#fff;     
+            font-size:14px;       
+        }
+        .evtContent span {vertical-align:top}
+        .evtCtnsBox {width:100%; text-align:center; min-width:1120px;position: relative;}
+        .evtCtnsBox .wrap {width:1120px; margin:0 auto; position:relative}
+        /*.evtCtnsBox .wrap a {border:1px solid #000}*/
+
+        /************************************************************/
+
+        /*타이머*/
+        .time {width:1120px; margin:0 auto; text-align:center; padding:20px 0}
+        .time ul {width:100%; display:flex; justify-content: center;}
+        .time li {line-height:61px; font-size:24px; margin-right:10px; position: relative;}
+        .time li:first-child,
+        .time li:last-child {line-height:1.3; color:#363635}
+        .time li:first-child {margin-right:20px}
+        .time li:last-child {margin-left:20px}
+        .time li:first-child span {color:#C82F25}        
+        .time li:last-child span {line-height:2.5; color:#363635;font-weight:bold;} 
+        .time li:last-child a {display:block; color:#fff; background:#242424; padding:10px 20px; margin-top:20px}
+        .time li img {width:44px}
+        .time .time_txt {color:#000; letter-spacing:-1px}
+        .time .time_txt span {color:#d63e4d; animation:upDown 2s infinite;-webkit-animation:upDown 2s infinite;}
+        .time .d_day {color:#fff;font-size:30px;}
+        
+        .evt_top {background:url(https://static.willbes.net/public/images/promotion/2022/02/2540_top_bg.jpg) repeat-x center top;}
+
+        .evt_01 {background:url(https://static.willbes.net/public/images/promotion/2022/02/2540_01_bg.jpg) repeat-x center top;}
+
+        .evt_02 {padding:100px 0; width:1120px; margin:auto}
+        .evt_02 .NSK-Black {font-size:50px; text-align:left; padding:20px 0; color:#331780 }    
+ 
+        /************************************************************/
+        
+    </style> 
+
+	<div class="evtContent NSK">     
+            
+        <div class="evtCtnsBox" data-aos="fade-down">
+            <div class="time NSK-Black" id="newTopDday">
+                <ul>
+                    <li>
+                        <span>[이벤트 마감]</span><br>
+                              2.28(월)까지
+                    </li>
+                    <li><img id="dd1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="dd2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li class="time_txt">일</li>
+                    <li><img id="hh1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="hh2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li class="time_txt">:</li>
+                    <li><img id="mm1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="mm2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li class="time_txt">:</li>
+                    <li><img id="ss1" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li><img id="ss2" src="https://static.willbes.net/public/images/promotion/common/0.png" /></li>
+                    <li>
+                        <span class="NSK">남았습니다.</span>                        
+                    </li>          
+                </ul>
+            </div> 
+        </div>  
+
+		<div class="evtCtnsBox evt_top" data-aos="fade-up">
+            <img src="https://static.willbes.net/public/images/promotion/2022/02/2540_top.jpg" alt="이벤트 마감까지" />
+		</div>
+
+        <div class="evtCtnsBox evt_01" data-aos="fade-up">
+            <img src="https://static.willbes.net/public/images/promotion/2022/02/2540_01.jpg" alt="특별할인 프로모션" />
+        </div>        
+
+        <div class="evtCtnsBox evt_02" data-aos="fade-up">
+            <div class="NSK-Black">노동법</div>
+            <div>
+                @if(empty($arr_base['display_product_data']) === false)
+                    @include('willbes.pc.promotion.display_product_partial',array('group_num'=>1))
+                @endif
+            </div>
+            <div class="NSK-Black">행정쟁송법</div>
+            <div>
+                @if(empty($arr_base['display_product_data']) === false)
+                    @include('willbes.pc.promotion.display_product_partial',array('group_num'=>2))
+                @endif
+            </div>
+            <div class="NSK-Black">인사노무관리</div>
+            <div>
+                @if(empty($arr_base['display_product_data']) === false)
+                    @include('willbes.pc.promotion.display_product_partial',array('group_num'=>3))
+                @endif
+            </div>
+            <div class="NSK-Black">선택과목(경영조직론/민사소송법)</div>
+            <div>
+                @if(empty($arr_base['display_product_data']) === false)
+                    @include('willbes.pc.promotion.display_product_partial',array('group_num'=>4))
+                @endif
+            </div>
+        </div>       
+
+	</div>
+     <!-- End Container -->
+
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        $(document).ready(function() {
+            AOS.init();
+
+            /*디데이*/
+            dDayCountDown('{{$arr_promotion_params['edate']}}','{{$arr_promotion_params['etime'] or "00:00"}}');
+        });
+    </script>
+    
+    {{-- 프로모션용 스크립트 include --}}
+    @include('willbes.pc.promotion.promotion_script')
+@stop

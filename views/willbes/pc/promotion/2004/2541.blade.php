31a8659d76d89aabb8f8fde043a8b3378d5830b2
김창용 <winmir@willbes.com>
2022-02-18 10:04:03 +0900

    공무원 광주캠퍼스 올패스 프로모션

--- views/willbes/pc/promotion/2004/2541.blade.php
+++ views/willbes/pc/promotion/2004/2541.blade.php
@@ -19,9 +19,59 @@
 
         /************************************************************/
         .evtTop {background:url(https://static.willbes.net/public/images/promotion/2022/02/2541_top_bg.jpg) no-repeat center;}    
-
-        .evt01 {}
+        
         .evt02 {background:url(https://static.willbes.net/public/images/promotion/2022/02/2541_02_bg.jpg) no-repeat center;}
+        .evt03 a {display:block; width:800px; margin:0 auto;
+            color: #fff;
+            font-weight: bold;
+            letter-spacing: 1px;
+            display: block;
+            font-size: 30px;
+            padding: 15px 0;
+            border-radius: 30px;
+            position: relative;
+            overflow: hidden;
+            text-indent: 18px;
+            background-color:#000;
+        }
+        .evt03 a:hover {animation: upup 1s ease-in-out infinite both;}
+        .evt03 a:after{
+            content:'';
+            position: absolute;
+            top:0;
+            left:0;
+            background-color: #fff;
+            width: 100px;
+            height: 100%;
+            z-index: 1;
+            transform: skewY(129deg) skewX(-60deg);
+        }
+        .evt03 a:after{animation:shinyBtn 3s ease-in-out infinite;}
+        @@keyframes shinyBtn {
+            0% { transform: scale(0) rotate(45deg); opacity: 0; }
+            80% { transform: scale(0) rotate(45deg); opacity: 0.3; }
+            81% { transform: scale(4) rotate(45deg); opacity: 0.7; }
+            100% { transform: scale(50) rotate(45deg); opacity: 0; }
+        }
+        @@keyframes upup {
+        from {
+            transform: scale(1);
+            transform-origin: center center;
+            animation-timing-function: ease-out;
+        }
+        17% {
+            transform: scale(1.1);
+            animation-timing-function: ease-out;
+        }
+        33% {
+            transform: scale(0.9);
+            animation-timing-function: ease-in;
+        }
+        45% {
+            transform: scale(1);
+            animation-timing-function: ease-out;
+        }
+    }
 
                       
     </style>
@@ -43,7 +93,7 @@
 
         <div class="evtCtnsBox evt03" data-aos="fade-up">
             <img src="https://static.willbes.net/public/images/promotion/2022/02/2541_03_01.jpg" alt="" />  
-            <a href="https://pass.willbes.net/pass/offLecture/index?cate_code=3059" target="_blank">지금 바로 신규 개강 과정 신청하기 ></a>
+            <a href="https://pass.willbes.net/pass/offPackage/index?cate_code=3043&campus_ccd=605006" target="_blank">지금 바로 신규 개강 과정 신청하기 ></a>
             <img src="https://static.willbes.net/public/images/promotion/2022/02/2541_03_02.jpg" alt="" />  
         </div>
     </div>

a974b40e761049ec0f5c9694cb86772bedcaed43
이윤구 <yoongukk@willbes.com>
2022-03-08 18:14:19 +0900

    (경찰) <22년1차>합격풀케어 사전서비스 신규 페이지(pc/모바일)

---
+++ views/willbes/pc/promotion/2001/2548_cts03.blade.php
@@ -0,0 +1,128 @@
+<style type="text/css">
+    .cts03_comingsoon {background:url("https://static.willbes.net/public/images/promotion/2020/03/1555_comingsoon_bg.jpg") no-repeat center top;}
+    .cts03_top {background:url("https://static.willbes.net/public/images/promotion/2021/03/2069_cts03_top_bg.jpg") no-repeat center top;}
+    .cts03_01 {background:#1E3344}
+    .cts03_02 {background:url("https://static.willbes.net/public/images/promotion/2021/03/2069_cts03_02_bg.jpg") no-repeat center top;}
+    .cts03_03 {background:#e7e7e7}
+    .cts03_03 .tabWrapEvt {width:1210px; margin:0 auto}
+    .cts03_03 .tabWrapEvt li {display:inline; float:left; width:20%}
+    .cts03_03 .tabWrapEvt li a {display:block; text-align:center; margin-right:1px; background:#ababab}
+    .cts03_03 .tabWrapEvt li a img.off {display:block}
+    .cts03_03 .tabWrapEvt li a img.on {display:none}
+    .cts03_03 .tabWrapEvt li a:hover img.off {display:none}
+    .cts03_03 .tabWrapEvt li a:hover img.on {display:block}
+    .cts03_03 .tabWrapEvt li a.active img.off {display:none}
+    .cts03_03 .tabWrapEvt li a.active img.on {display:block}
+    .cts03_03 .tabWrapEvt li a:hover,
+    .cts03_03 .tabWrapEvt li a.active {background:#fff}
+    .cts03_03 .tabWrapEvt li:last-child a {margin-right:0}
+    .cts03_03 .tabWrapEvt:after {content:""; display:block; clear:both}
+    .cts03_03 .tabcts {background:#fff}
+    .cts03_04 {background:url("https://static.willbes.net/public/images/promotion/2021/03/2069_cts03_04_bg.jpg") no-repeat center top;}
+    .cts03_05 {background:#1E3344} 
+</style>
+
+    <div>
+        <div class="evtCtnsBox cts03_top">
+            <img src="https://static.willbes.net/public/images/promotion/2021/03/2069_cts03_top.jpg" alt="아이언 폴리스" />
+        </div>
+        <div class="evtCtnsBox cts03_01">
+            <img src="https://static.willbes.net/public/images/promotion/2021/03/2069_cts03_01.jpg" alt="경찰체력특전" usemap="#Map2069_coupon" border="0" />
+            <map name="Map2069_coupon" id="Map2069_coupon">
+                <area shape="rect" coords="266,1118,854,1173" href="@if(empty($cert_apply) === false){{ $file_yn[2] == 'Y' ? front_url($file_link[2]) : $file_link[2] }}@else{{"javascript:alert('필시합격 인증 후 다운로드 가능합니다.');"}}@endif" title="쿠폰받기" @if(empty($cert_apply) === false && $file_yn[2] == 'Y') target="_blank" @endif/>
+            </map>
+        </div>
+        <div class="evtCtnsBox cts03_02">
+            <img src="https://static.willbes.net/public/images/promotion/2021/03/2069_cts03_02.jpg" alt="체력 q&a" usemap="#Map2069ct03A" border="0" />
+            <map name="Map2069ct03A" id="Map2069ct03A">
+                <area shape="rect" coords="427,442,789,515" href="https://irongym.modoo.at/" target="_blank" alt="체력qna바로가기" />
+                {{--<area shape="rect" coords="427,442,789,515" href="javascript:void(0);" onclick="javascript:alert('Coming soon!');" alt="체력qna바로가기" />--}}
+            </map>
+        </div>
+        <div class="evtCtnsBox cts03_03">
+            <img src="https://static.willbes.net/public/images/promotion/2021/03/2069_cts03_03.jpg" alt="경찰공무원 체력 검정 시험" />
+            <ul class="tabWrapEvt">
+                <li>
+                    <a href="#ct03tab01">
+                        <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts03_03_tab01_off.jpg" alt="" class="off"/>
+                        <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts03_03_tab01_on.jpg" alt="" class="on"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="#ct03tab02">
+                        <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts03_03_tab02_off.jpg" alt="" class="off"/>
+                        <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts03_03_tab02_on.jpg" alt="" class="on"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="#ct03tab03">
+                        <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts03_03_tab03_off.jpg" alt="" class="off"/>
+                        <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts03_03_tab03_on.jpg" alt="" class="on"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="#ct03tab04">
+                        <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts03_03_tab04_off.jpg" alt="" class="off"/>
+                        <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts03_03_tab04_on.jpg" alt="" class="on"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="#ct03tab05">
+                        <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts03_03_tab05_off.jpg" alt="" class="off"/>
+                        <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts03_03_tab05_on.jpg" alt="" class="on"/>
+                    </a>
+                </li>
+            </ul>
+
+            <div id="ct03tab01" class="tabcts">
+                <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts03_03_tab01.jpg" alt=""/>
+            </div>
+            <div id="ct03tab02" class="tabcts">
+                <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts03_03_tab02.jpg" alt=""/>
+            </div>
+            <div id="ct03tab03" class="tabcts">
+                <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts03_03_tab03.jpg" alt=""/>
+            </div>
+            <div id="ct03tab04" class="tabcts">
+                <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts03_03_tab04.jpg" alt=""/>
+            </div>
+            <div id="ct03tab05" class="tabcts">
+                <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts03_03_tab05.jpg" alt=""/>
+            </div>
+        </div>
+        <div class="evtCtnsBox cts03_04">
+            <img src="https://static.willbes.net/public/images/promotion/2021/03/2069_cts03_04.jpg" alt="왜,체력시험을 미리 준비 해야 할까요?" />
+        </div>
+        <div class="evtCtnsBox cts03_05">
+            <img src="https://static.willbes.net/public/images/promotion/2021/03/2069_cts03_05.jpg" alt="서류준비 꿀팁" />
+        </div>
+	</div>
+
+    <script type="text/javascript">
+        /*tab*/
+        $(document).ready(function(){
+            $('.tabWrapEvt').each(function(){
+                var $active, $content, $links = $(this).find('a');
+                $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+                $active.addClass('active');
+
+                $content = $($active[0].hash);
+
+                $links.not($active).each(function () {
+                    $(this.hash).hide()});
+
+                // Bind the click event handler
+                $(this).on('click', 'a', function(e){
+                    $active.removeClass('active');
+                    $content.hide();
+
+                    $active = $(this);
+                    $content = $(this.hash);
+
+                    $active.addClass('active');
+                    $content.show();
+
+                    e.preventDefault()})})}
+        );
+    </script>
+

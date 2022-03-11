8dd2bd9b4b736e25183a22739bf4fe24936849f3
최현탁 <cht@willbes.com>
2022-03-11 15:35:17 +0900

    경찰 합격예측 사전예약 프로모션 개발

--- application/controllers/willbes/share/passpredict/BasePassPredict.php
+++ application/controllers/willbes/share/passpredict/BasePassPredict.php
@@ -225,8 +225,10 @@ class BasePassPredict extends \app\controllers\FrontController
 
         $arr_condition = [
             'EQ' => [
-                'PredictIdx' => $this->_reqP('PredictIdx')
-                ,'TakeMockPart' => $this->_reqP('GroupCcd')
+                'a.PredictIdx' => $this->_reqP('PredictIdx')
+                ,'a.TakeMockPart' => $this->_reqP('GroupCcd')
+                ,'b.GroupCcd' => $this->_reqP('GroupCcd')
+                ,'a.IsStatus' => 'Y'
             ]
         ];
         $list = $this->surveyModel->getSubjectCode($arr_condition);

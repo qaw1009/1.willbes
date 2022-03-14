c8663aa5a05cd575c97088a20c2d4da4aa5d2036
이윤구 <yoongukk@willbes.com>
2022-03-14 10:44:49 +0900

    (경찰) 22년1차> 합격풀케어 합격예측 서비스 및 당일 본서비스 페이지 신규

--- views/willbes/pc/promotion/2001/2548_cts02.blade.php
+++ views/willbes/pc/promotion/2001/2548_cts02.blade.php
@@ -1,26 +1,29 @@
 <style type="text/css">
-
+.evtCtnsBox .wrap {width:1120px; margin:0 auto; position:relative}
+/*.evtCtnsBox .wrap a {border:1px solid #000} */
+.cts02_top {background:url("https://static.willbes.net/public/images/promotion/2022/03/2548_cts02_top_bg.jpg") no-repeat center top;}
+.cts02_01 {background:#eaeaea;}
+.cts02_02 {padding-bottom:50px;}
 </style>
 
 <div>
-    <div class="evtCtnsBox cts02_top">    
-        <div>         
-            <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts02_top.jpg" alt="합격예측 풀서비스">
-            <a href="javascript:alert('서비스가 종료 되었습니다.')" style="position: absolute; left: 25.89%; top: 84.07%; width: 48.13%; height: 6.3%; z-index: 2;"></a>
+    <div class="evtCtnsBox cts02_top">
+        <div class="wrap">         
+            <img src="https://static.willbes.net/public/images/promotion/2022/03/2548_cts02_top.jpg" alt="합격예측 풀서비스">
+            <a href="javascript:alert('3월 26일 12:30 오픈됩니다.')" style="position: absolute;left: 26.01%;top: 84.07%;width: 48.13%;height: 6.3%;z-index: 2;"></a>
         </div>
     </div>
-    <div class="evtCtnsBox cts02_02" id="cts02_02">
-        <div>
-            <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts02_02.jpg" alt="라이브 토크쇼">
-            {{--<a href="#none" onclick="javascript:alert('라이브 토크쇼가 종료되었습니다');" style="position: absolute; left: 30.54%; top: 85.61%; width: 38.13%; height: 4.02%; z-index: 2;"></a>--}}
-            <a href="https://www.youtube.com/channel/UCQ-jvqaobw6E9EvnFO88vwQ" target="_blank" style="position: absolute; left: 30.54%; top: 85.61%; width: 38.13%; height: 4.02%; z-index: 2;"></a>
+    <div class="evtCtnsBox cts02_01" id="cts02_01">
+        <div class="wrap">
+            <img src="https://static.willbes.net/public/images/promotion/2022/03/2548_cts02_01.jpg" alt="라이브 토크쇼">            
+            <a href="https://www.youtube.com/channel/UCQ-jvqaobw6E9EvnFO88vwQ" target="_blank" style="position: absolute;left: 28.04%;top: 87.41%;width: 44.13%;height: 4.02%;z-index: 2;"></a>
         </div>
     </div>
 
-    <div class="evtCtnsBox cts02_03">
-        <div>
-            <img src="https://static.willbes.net/public/images/promotion/2021/02/2069_cts02_03.jpg" alt="신광은 경찰팀">
-            <a href="https://police.willbes.net/promotion/index/cate/3001/code/2070" target="_blank" style="position: absolute; left: 32.05%; top: 83.47%; width: 35.63%; height: 5.78%; z-index: 2;"></a>
+    <div class="evtCtnsBox cts02_02">
+        <div class="wrap">
+            <img src="https://static.willbes.net/public/images/promotion/2022/03/2548_cts02_02.jpg" alt="신광은 경찰팀">
+            <a href="https://police.willbes.net/promotion/index/cate/3001/code/2578" target="_blank" style="position: absolute;left: 27.99%;top: 91.07%;width: 44.13%;height: 5.78%;z-index: 2;"></a>
         </div>
     </div>
 </div>

376eb6bbbf842133ef4b15e2441d21e4420e625a
김창용 <winmir@willbes.com>
2022-03-16 09:08:16 +0900

    경찰 라이브 토크쇼

--- views/willbes/pc/liveTalkShowCaption/index.blade.php
+++ views/willbes/pc/liveTalkShowCaption/index.blade.php
@@ -22,7 +22,7 @@
         .liveTab01 .txt02 {top:70px; left:292px; font-size:30px; font-weight:bold; color:#fdf9c1; width:190px; text-align:center; line-height:1.3}
         .liveTab01 .txt03 {top:62px; left:824px; font-size:50px; font-weight:bold; color:#ff99cc; letter-spacing:-1px; font-family:"Times New Roman", Times, serif}
         .liveTab01 .txt04 {top:33px; left:1040px; width:140px; height:33px; line-height:33px; font-size:28px; text-align:right; font-family:"Times New Roman", Times, serif; font-weight:bold; color:#fff}
-        .liveTab01 .txt05 {top:74px; left:1040px; width:140px; height:33px; line-height:33px; font-size:28px; text-align:right; font-family:"Times New Roman", Times, serif; font-weight:bold;}
+        .liveTab01 .txt05 {top:74px; left:1040px; width:140px; height:33px; line-height:33px; font-size:28px; text-align:right; font-family:"Times New Roman", Times, serif; font-weight:bold; color:#fff}
 
         .liveTab02 {position:absolute; top:0; left:0; width:1210px; height:130px; z-index:10}
         .liveTab02 li {padding-left:260px; height:130px; line-height:130px; color:#fff; font-size:40px; letter-spacing:-3px; text-align:left; width:890px; overflow:hidden; white-space:nowrap; text-overflow:ellipsis;}

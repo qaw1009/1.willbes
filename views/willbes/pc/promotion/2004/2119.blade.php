f18302080a3d39a7a6b509729834899b0071eb1f
김창용 <winmir@willbes.com>
2022-03-17 09:22:47 +0900

    공무원 법원직 온라인관리반 수정

--- views/willbes/pc/promotion/2004/2119.blade.php
+++ views/willbes/pc/promotion/2004/2119.blade.php
@@ -20,18 +20,18 @@
         .wb_top {background:url(https://static.willbes.net/public/images/promotion/2021/03/2119_top_bg.jpg) no-repeat center;}    
 
         .wb_01 {background:#fff}
-        .wb_02 {background:#f4f4f4}
-
-        .evtCtnsBox .wrap {width:1120px; margin:0 auto; position:relative}
+        .wb_02 {padding-bottom:150px}
+        .wb_02 a {display:block; width:600px; margin:0 auto; padding:20px; text-align:center; font-size:30px; color:#fff; background:#000; border-radius:50px;}
+        .wb_02 a:hover {background:#012d98}
                       
     </style>
 
     <div class="evtContent NSK" id="evtContainer">
-        <div class="evtCtnsBox wb_top">             
+        <div class="evtCtnsBox wb_top" data-aos="fade-up">             
             <img src="https://static.willbes.net/public/images/promotion/2021/03/2119_top.jpg" alt="김동진법원팀 동행5기 온라인관리반" />  
         </div>       
 
-        <div class="evtCtnsBox wb_01">
+        <div class="evtCtnsBox wb_01" data-aos="fade-up">
             <img src="https://static.willbes.net/public/images/promotion/2021/03/2119_01_01.jpg" alt="온라인관리반 소개" />
             <div class="wrap">
                 <img src="https://static.willbes.net/public/images/promotion/2021/03/2119_01_02.jpg" alt="만족 후기" />
@@ -39,13 +39,18 @@
             </div>
         </div>  
 
-        <div class="evtCtnsBox wb_02">
-            <div class="wrap">
-                <img src="https://static.willbes.net/public/images/promotion/2021/11/2119_02.jpg" alt="수강신청" />  
-                <a href="https://pass.willbes.net/pass/offLecture/index?cate_code=3059" target="_blank" title="수강신청하기" style="position: absolute; left: 29.55%; top: 84.79%; width: 43.39%; height: 7.16%; z-index: 2;"></a>
-            </div>
+        <div class="evtCtnsBox wb_02 NSK-Black" data-aos="fade-up">
+            <a href="https://pass.willbes.net/pass/offLecture/index?cate_code=3059" target="_blank" title="수강신청하기">온라인관리반 상품 확인하기 ></a>
         </div>
     </div>
     <!-- End Container -->
+
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        $(document).ready(function() {
+            AOS.init();
+        });
+    </script>
 
 @stop

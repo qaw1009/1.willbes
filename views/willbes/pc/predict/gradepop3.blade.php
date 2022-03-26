958826dde79573622129ca08b1a78277cffd6fbe
최현탁 <cht@willbes.com>
2022-03-26 14:25:35 +0900

    프론트 합격예측 성적 직접입력 페이지 수정

--- views/willbes/pc/predict/gradepop3.blade.php
+++ views/willbes/pc/predict/gradepop3.blade.php
@@ -30,7 +30,7 @@
                             <tr>
                                 <th>{{$val['CcdName']}}</th>
                                 <td>
-                                    <input type="number" name="Score[]" maxlength="3" data-max-num="{{$val['TotalScore']}}" oninput="maxLengthCheck(this)" @if(empty($subject_grade)===false) value="{{ $subject_grade[$val['PpIdx']] }}" @endif > 점
+                                    <input type="number" name="Score[]" maxlength="4" data-max-num="{{$val['TotalScore']}}" oninput="maxLengthCheck(this)" @if(empty($subject_grade)===false) value="{{ $subject_grade[$val['PpIdx']] }}" @endif > 점
                                     <input type="hidden" name="PpIdx[]" value="{{ $val['PpIdx'] }}" />
                                 </td>
                             </tr>
@@ -53,7 +53,7 @@
 
         function maxLengthCheck(object){
             if($(object).prop('type') == 'number') {
-                object.value = object.value.replace(/[^0-9.]/g, "");
+                /*object.value = object.value.replace(/[^0-9.]/g, "");*/
                 if($(object).data('max-num') != undefined) {
                     if( Number(object.value) > Number($(object).data('max-num')) ) {
                         object.value = object.value.slice(0, -1);
@@ -68,17 +68,33 @@
 
         function lastSave(){
             var vali_msg = '';
+            var check = true;
+
             $('input[name="Score[]"]').each(function(){
                 var scr_val = $(this).val();
+
                 if($.trim(scr_val) == ''){
                     vali_msg = '점수를 입력하지 않은 과목이 있습니다';
+                    check = false;
                 }else if(scr_val < 0 || scr_val > 100){
                     vali_msg = '점수는 0~100점 사이 이어야 합니다';
-                }else if(scr_val%5 != 0){
-                    vali_msg = '정확한 원점수를 입력해주세요'; //한문제당 5점
+                    check = false;
+                } else {
+                    if ($(this).data("max-num") == 100) {
+                        if(scr_val%5 != 0) {
+                            vali_msg = '정확한 원점수를 입력해주세요'; //한문제당 5점
+                            check = false;
+                        }
+                    } else {
+                        if(scr_val%5 == 0 || scr_val%5 == 0.5) {
+                        } else {
+                            vali_msg = '정확한 원점수를 입력해주세요'; //한문제당 0.5점
+                            check = false;
+                        }
+                    }
                 }
             });
-            if(vali_msg){ alert(vali_msg); return; }
+            if(check == false){ alert(vali_msg); return false; }
 
             if (confirm('정답을 제출하시겠습니까?')) {
                 var _url = '{{ front_url('/predict/examSendAjax3') }}';

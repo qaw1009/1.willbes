22aea72e61806abe77cb92399c01ac013a8e19de
이윤구 <yoongukk@willbes.com>
2022-03-28 14:20:03 +0900

    (공무원) 실전덕후단 관련 교수진 사진 및 GNB 내 교수명 삭제

--- views/willbes/m/layouts/mega_menu.blade.php
+++ views/willbes/m/layouts/mega_menu.blade.php
@@ -30,8 +30,7 @@
                 <li>
                     <span>한국사</span>
                     <a href="{{ front_url('/professor/show/cate/' . $menu_cate_code . '/prof-idx/50619?subject_idx=1109') }}">김상범</a>
-                    <a href="{{ front_url('/professor/show/cate/' . $menu_cate_code . '/prof-idx/51354?subject_idx=1109') }}">김민규</a>
-                    <a href="{{ front_url('/professor/show/cate/' . $menu_cate_code . '/prof-idx/51267?subject_idx=1109') }}">나명재</a>   
+                    <a href="{{ front_url('/professor/show/cate/' . $menu_cate_code . '/prof-idx/51354?subject_idx=1109') }}">김민규</a>               
                     <a href="{{ front_url('/professor/show/cate/' . $menu_cate_code . '/prof-idx/50027?subject_idx=1109') }}">오태진</a>                    
                     <a href="{{ front_url('/professor/show/cate/' . $menu_cate_code . '/prof-idx/50647?subject_idx=1109') }}">조민주</a>
                     <a href="{{ front_url('/professor/show/cate/' . $menu_cate_code . '/prof-idx/50003?subject_idx=1109') }}">원유철</a>
@@ -229,8 +228,7 @@
                 </li>
                 <li>
                     <span>한국사</span>
-                    <a href="{{ front_url('/professor/show/cate/' . $menu_cate_code . '/prof-idx/50619?subject_idx=1109') }}">김상범</a>
-                    <a href="{{ front_url('/professor/show/cate/3028/prof-idx/51267?subject_idx=1109&subject_name=한국사') }}">나명재</a>
+                    <a href="{{ front_url('/professor/show/cate/' . $menu_cate_code . '/prof-idx/50619?subject_idx=1109') }}">김상범</a>                   
                     <a href="{{ front_url('/professor/show/cate/' . $menu_cate_code . '/prof-idx/50027?subject_idx=1109') }}">오태진</a>                    
                     <a href="{{ front_url('/professor/show/cate/' . $menu_cate_code . '/prof-idx/50647?subject_idx=1109') }}">조민주</a>                   
                 </li>                

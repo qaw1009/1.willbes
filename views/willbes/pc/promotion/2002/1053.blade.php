2a4280e75c00a9282f58d68d35ba169a68efa976
김창용 <winmir@willbes.com>
2022-03-28 16:36:00 +0900

    경찰학원 통합생활관리반 수정

--- views/willbes/pc/promotion/2002/1053.blade.php
+++ views/willbes/pc/promotion/2002/1053.blade.php
@@ -5,19 +5,22 @@
     <!-- Container -->
     <style type="text/css">
         .evtContent {
-            position:relative;
-            width:100% !important;
+            width:100%;
             min-width:1120px !important;
+            max-width:2000px !important;
+            margin:0 auto;
             margin-top:20px !important;
             padding:0 !important;
-            background:#fff;
+            background:#fff;            
         }
-        .evtCtnsBox {width:100%; text-align:center; min-width:1120px; position:relative;}
+        .evtContent span {vertical-align:top}
+        .evtCtnsBox {width:100%; text-align:center; min-width:1120px; position:relative}
+        .evtCtnsBox .wrap {width:1120px; margin:0 auto; position:relative}
+        /*.evtCtnsBox .wrap a {border:1px solid #000}*/
 
         /************************************************************/
 
         .skyBanner {position:fixed; bottom:250px;right:0;z-index:10;}
-        .skybannerB{position:fixed; bottom:0; text-align:center; z-index: 101; background:#242424; width:100%}
 
         /*상단 동영상*/
         .layer {width:100%; height:800px; -ms-overflow:hidden;}
@@ -27,19 +30,10 @@
         .wb_mp4 {width:100%; text-align:center; margin:0 auto; background:#000; min-width:1120px;}
         .wb_mp4 ul {width:100%; margin:0 auto; min-width:1120px;}
        
-        /* 슬라이드배너 */
-        .slide_con {position:relative; width:960px; margin:0 auto;}
-        .slide_con ul {width:960px; height:540px}
-        .slide_con img {width:100%;}
-        .slide_con p {position:absolute; top:50%; width:62px; margin-top:-31px; height:62px; opacity:0.9; filter:alpha(opacity=90); z-index:90;}
-        .slide_con p a {cursor:pointer}
-        .slide_con p.leftBtn1 {left:-31px;}
-        .slide_con p.rightBtn1 {right:-31px; opacity:0.9;}
 
         /*탭(이미지)*/
-        .tabs{width:100%; text-align:center; background:#0f44a0; padding-top:30px}
-        .tabs ul {width:1180px;margin:0 auto;}		
-        .tabs li {display:inline; float:left;}	
+        .tabs{background:#0f44a0; padding-top:30px}
+        .tabs ul {display:flex; justify-content: center; }
         .tabs a img.off {display:block}
         .tabs a img.on {display:none}
         .tabs a.active img.off {display:none}
@@ -97,8 +91,8 @@
         .tab04s2{background:#f7f7f7}
 
           /* tip */
-        .wb_cts09 {background:#e9e9e9; text-align:left; padding:100px 0;}
-        .wb_tipBox {border:1px solid #333; padding:100px; width:1120px; margin:0 auto; }
+        .wb_cts09 {text-align:left; padding:100px 0; background:#f4f4f4}
+        .wb_tipBox {width:1000px; margin:0 auto;}
         .wb_tipBox > strong {font-size:16px !important; font-weight:bold; color:#333; display:block; margin-bottom:20px}
         .wb_tipBox p {font-size:24px !important; font-weight:bold;  letter-spacing:-3px; margin-bottom:10px; color:#111}	
         .wb_tipBox ol li {margin:25px 0 10px; line-height:1.5; list-style:decimal; margin-left:15px;font-size:14px;}
@@ -160,13 +154,6 @@
             <img src="https://static.willbes.net/public/images/promotion/2019/08/1053_skybanner.png" title="경찰통합생활관리반">
         </div>
 
-        <div class="skybannerB">
-            <img src="https://static.willbes.net/public/images/promotion/2019/10/1053_footer.gif" usemap="#Map1053_bottom" title="예약 접수" border="0"  />
-            <map name="Map1053_bottom" id="Map1053_bottom">
-                <area shape="rect" coords="807,35,934,90" href="https://police.willbes.net/pass/event/show/ongoing?event_idx=454&" target="_blank" />
-            </map> 
-        </div>
-
         <div class="evtCtnsBox wb_mp4" id="main">
             <div class="layer">
                 <div class="video">
@@ -203,47 +190,22 @@
                             <img src="https://static.willbes.net/public/images/promotion/2019/11/1053_tab3_off.png" class="off"/>
                         </a>
                     </li>
-                    <li>
-                        <a href="#tab04s">
-                            <img src="https://static.willbes.net/public/images/promotion/2019/11/1053_tab4_on.png" class="on"/>
-                            <img src="https://static.willbes.net/public/images/promotion/2019/11/1053_tab4_off.png" class="off"/>
-                        </a>
-                    </li>
                 </ul>
             </div>
 
             <div id="tab01s" class="pb100">            
-                <img src="https://static.willbes.net/public/images/promotion/2020/12/1053_tab1_con.jpg">                
+                <img src="https://static.willbes.net/public/images/promotion/2022/03/1053_tab1_con.jpg">                
                 <div class="btngo"><a href="https://police.willbes.net/pass/offPackage/index/type/life?cate_code=3010&campus_ccd=605001&course_idx=1093" target="_blank" >신청하기 ></a></div>                            
             </div> 
 
             <div id="tab02s" class="pb100">
-                <img src="https://static.willbes.net/public/images/promotion/2021/01/1053_tab2_con.jpg" usemap="#Map1053tab2" border="0" />
-                <map name="Map1053tab2" id="Map1053tab2">
-                    <area shape="rect" coords="226,4529,468,4576" href="https://police.willbes.net/pass/promotion/index/cate/3010/code/1051" target="_blank" />
-                    <area shape="rect" coords="227,4651,467,4700" href="https://police.willbes.net/pass/promotion/index/cate/3010/code/1128" target="_blank" />
-                </map> 
+                <img src="https://static.willbes.net/public/images/promotion/2022/03/1053_tab2_con.jpg" /> 
             </div>
 
             <div id="tab03s" class="pb100">
                 <div class="EventBox">            
                     <div id="evtRollingSlider" class="evtBigimg">
                         <div><img id="result_slide_img" src="https://static.willbes.net/public/images/promotion/2021/01/1053_info1.jpg" alt="배너명"></div>
-                        {{--
-                        <div><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info2.jpg" alt="배너명"></div>
-                        <div><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info3.jpg" alt="배너명"></div>
-                        <div><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info4.jpg" alt="배너명"></div>
-                        <div><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info5.jpg" alt="배너명"></div>
-                        <div><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info6.jpg" alt="배너명"></div>  
-                        <div><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info7.jpg" alt="배너명"></div>
-                        <div><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info8.jpg" alt="배너명"></div>
-                        <div><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info9.jpg" alt="배너명"></div>
-                        <div><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info10.jpg" alt="배너명"></div>
-                        <div><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info13.jpg" alt="배너명"></div>
-                        <div><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info14.jpg" alt="배너명"></div>
-                        <div><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info11.jpg" alt="배너명"></div>
-                        <div><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info12.jpg" alt="배너명"></div>
-                        --}}
                     </div>
 
                     <div class="mt20">                        
@@ -296,64 +258,8 @@
                     <p class="leftBtn" id="imgBannerLeft"><a href="#none">이전</a></p>
                     <p class="rightBtn" id="imgBannerRight"><a href="#none">다음</a></p> 
                 </div>
-                {{--
-                <div class="slide_con mt100 mb100">
-                    <ul id="slidesImg1">
-                        <li><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info1.jpg" alt=""/></li>
-                        <li><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info2.jpg" alt=""/></li>
-                        <li><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info3.jpg" alt=""/></li>
-                        <li><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info4.jpg" alt=""/></li>
-                        <li><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info5.jpg" alt=""/></li>
-                        <li><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info6.jpg" alt=""/></li>
-                        <li><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info7.jpg" alt=""/></li>
-                        <li><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info8.jpg" alt=""/></li>
-                        <li><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info9.jpg" alt=""/></li>
-                        <li><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info10.jpg" alt=""/></li>
-                        <li><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info11.jpg" alt=""/></li>
-                        <li><img src="https://static.willbes.net/public/images/promotion/2021/01/1053_info12.jpg" alt=""/></li>
-                    </ul>
-                    <p class="leftBtn1"><a id="imgBannerLeft1"><img src="https://static.willbes.net/public/images/promotion/sparta/1501_roll_arr_l.png"></a></p>
-                    <p class="rightBtn1"><a id="imgBannerRight1"><img src="https://static.willbes.net/public/images/promotion/sparta/1051_roll_arr_r.png"></a></p>
-                </div>
-                --}}
-                <img src="https://static.willbes.net/public/images/promotion/2021/01/1053_tab3_con.jpg" />            
-            </div>
 
-            <div id="tab04s">
-                <img src="https://static.willbes.net/public/images/promotion/2021/01/1053_tab4_con.jpg" /> 
-                <div class="tab04s2">
-                    <img src="https://static.willbes.net/public/images/promotion/2019/11/1129_02.jpg" alt="여러분을 합격의 지름길로 안내할 3가지의 신의법칙 " />
-                    <div class="evttab">
-                        <ul>
-                            <li><a href="#tab01" class="active">기본과정</a></li>
-                            <li><a href="#tab02">심화과정</a></li>
-                            <li><a href="#tab03">3개월 필합 풀패키지</a></li>
-                        </ul>
-                    </div>
-                    <div id="tab01" class="tabCts">
-                        <img src="https://static.willbes.net/public/images/promotion/2021/01/1053_02_t01.jpg" alt="기본과정" usemap="#Map1126A" border="0">
-                        <map name="Map1126A" id="Map1126A">
-                            <area shape="rect" coords="354,585,438,614" href="{{ site_url('/pass/promotion/index/cate/3010/code/1124') }}" />
-                            <area shape="rect" coords="235,2450,318,2476" href="{{ site_url('/pass/promotion/index/cate/3010/code/1131') }}" />
-                            <area shape="rect" coords="803,2450,884,2476" href="javascript:alert('Coming Soon')" />
-                        </map>
-                    </div>
-                    <div id="tab02" class="tabCts">
-                        <img src="https://static.willbes.net/public/images/promotion/2021/01/1053_02_t02.jpg" alt="심화과정" usemap="#Map1126B" border="0">
-                        <map name="Map1126B" id="Map1126B">
-                            <area shape="rect" coords="235,2208,318,2240" href="{{ site_url('/pass/promotion/index/cate/3010/code/1131') }}" />
-                            <area shape="rect" coords="803,2208,884,2240" href="javascript:alert('Coming Soon')" />
-                        </map>
-                    </div>
-                    <div id="tab03" class="tabCts">
-                        <img src="https://static.willbes.net/public/images/promotion/2021/01/1053_02_t03.jpg" alt="필합 풀패키지">
-                        <img src="https://static.willbes.net/public/images/promotion/2021/01/1053_02_t03s.jpg" alt="필합 풀패키지" usemap="#Map1126C" border="0">
-                        <map name="Map1126C" id="Map1126C">
-                            <area shape="rect" coords="232,425,323,456" href="{{ site_url('/pass/promotion/index/cate/3010/code/1131') }}" />
-                            <area shape="rect" coords="794,421,893,459" href="javascript:alert('Coming Soon')" />
-                        </map>                           
-                    </div>
-                </div>    
+                <img src="https://static.willbes.net/public/images/promotion/2022/03/1053_tab3_con.jpg" />            
             </div>
         </div>
 
@@ -372,7 +278,6 @@
                         <li><strong>[실강 PASS]</strong><br />
                         - <span class="wb_tip_orange">국가재난, 정부 지침 등으로 인한 학원 휴원으로 실강 진행이 어려울 경우 동영상 강의로 대체될 수 있으며, 
                             이로 인한 해당기간 환불은 불가합니다.</span><br />
-                        - 영어집중관리반, 영어아침특강은 슈퍼패스에 포함되어있지 않아 별도의 수강등록이 필요합니다.<br />
                         - 인강패스와 정규과정 외 특강 및 모의고사는 슈퍼패스 등록생에게 제공되는 무료혜택입니다.<br />
                         - 특강의 경우 별도의 신청이 필요합니다.<br />
                         - 일부 특강의 경우 별도의 결제가 필요할 수 있습니다.<br />

860d01f59ab475124adebbb41c2e3bb9c58c90c5
김창용 <winmir@willbes.com>
2022-03-30 17:07:19 +0900

    모바일 경찰 메인 리뉴얼  수정

--- views/willbes/m/site/_viewTest/main_2001.blade.php
+++ views/willbes/m/site/_viewTest/main_2001.blade.php
@@ -244,9 +244,9 @@
         {!! banner('M_메인_01', 'MainBnrSlider', $__cfg['SiteCode'], '0') !!}
 
         <div class="groupLink">
-            <a href="{{ front_url('/home/index/cate/3006') }}" target="_blank">경찰승진<img src="https://static.willbes.net/public/images/promotion/m/icon_link.png" ></a>
+            <a href="https://police.willbes.net/home/index/cate/3006" target="_blank">경찰승진<img src="https://static.willbes.net/public/images/promotion/m/icon_link.png" ></a>
             <a href="{{ front_url('/promotion/index/cate/3007/code/2414') }}" target="_blank">해양경찰<img src="https://static.willbes.net/public/images/promotion/m/icon_link.png" ></a>
-            <a href="{{ front_url('/home/index/cate/3008') }}" target="_blank">해경경채<img src="https://static.willbes.net/public/images/promotion/m/icon_link.png" ></a>
+            <a href="https://police.willbes.net/home/index/cate/3008" target="_blank">해경경채<img src="https://static.willbes.net/public/images/promotion/m/icon_link.png" ></a>
             <a href="{{ front_url('/promotion/index/cate/3001/code/2366') }}" target="_blank">경찰간부<img src="https://static.willbes.net/public/images/promotion/m/icon_link.png" ></a>
         </div>
 

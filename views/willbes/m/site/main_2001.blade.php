983b7ebbbeb2d967579bfeac9d7a5097f009d271
김창용 <winmir@willbes.com>
2022-04-01 09:35:53 +0900

    모바일 경찰 메인 링크 수정

--- views/willbes/m/site/main_2001.blade.php
+++ views/willbes/m/site/main_2001.blade.php
@@ -228,8 +228,9 @@
         <div class="widthAutoFull">
             <div class="subMenuWrap">
                 <a href="{{ front_app_url('/classroom/on/list/ongoing', 'www') }}">내강의실</a>
-                <a href="{{ front_url('/lecture/index/pattern/free') }}">무료특강(보강)</a>
-                <a href="{{ front_url('/support/notice/show?board_idx=259726') }}">신규강의안내</a>
+                <a href="{{ front_url('/lecture/index/pattern/free') }}">무료특강</a>
+                <a href="{{ front_url('/lecture/index/pattern/only') }}">전체강좌</a>
+                <a href="{{ front_url('/book/index') }}">교재구매</a>
             </div>
         </div>
 
@@ -240,9 +241,9 @@
         {!! banner('M_메인_01', 'MainBnrSlider', $__cfg['SiteCode'], '0') !!}
 
         <div class="groupLink">
-            <a href="https://police.willbes.net/home/index/cate/3006" target="_blank">경찰승진<img src="https://static.willbes.net/public/images/promotion/m/icon_link.png" ></a>
+            <a href="https://police.willbes.net/promotion/index/cate/3006/code/2478" target="_blank">경찰승진<img src="https://static.willbes.net/public/images/promotion/m/icon_link.png" ></a>
             <a href="{{ front_url('/promotion/index/cate/3007/code/2414') }}" target="_blank">해양경찰<img src="https://static.willbes.net/public/images/promotion/m/icon_link.png" ></a>
-            <a href="https://police.willbes.net/home/index/cate/3008" target="_blank">해경경채<img src="https://static.willbes.net/public/images/promotion/m/icon_link.png" ></a>
+            <a href="https://police.willbes.net/promotion/index/cate/3008/code/2437" target="_blank">해경경채<img src="https://static.willbes.net/public/images/promotion/m/icon_link.png" ></a>
             <a href="{{ front_url('/promotion/index/cate/3001/code/2366') }}" target="_blank">경찰간부<img src="https://static.willbes.net/public/images/promotion/m/icon_link.png" ></a>
         </div>
 

5a6d7778262d35388167a1162b9e94c7b18c14eb
김창용 <winmir@willbes.com>
2022-04-11 10:24:02 +0900

    임용 이경범 교육학

---
+++ views/willbes/pc/promotion/2017/2622.blade.php
@@ -0,0 +1,104 @@
+@extends('willbes.pc.layouts.master')
+
+@section('content')
+    @include('willbes.pc.layouts.partial.site_menu')
+    <!-- content -->
+    <style type="text/css">
+        .evtContent {
+            width:100% !important;
+            min-width:1120px !important;
+            max-width:2000px !important;
+            margin:0 auto;
+            margin-top:20px !important;
+            padding:0 !important;
+            background:#fff;            
+        }
+        .evtContent span {vertical-align:middle}
+        .evtCtnsBox {width:100%; text-align:center; min-width:1120px; font-size:14px; position: relative;}
+        .evtCtnsBox .wrap {width:1120px; margin:0 auto; position:relative}
+        /*.evtCtnsBox .wrap a:hover {border:1px solid #000}*/
+
+        /************************************************************/
+
+        .sky {position:fixed; top:150px; right:10px; z-index:10;}
+
+        .evtTop  {background:url("https://static.willbes.net/public/images/promotion/2022/03/2580_top_bg.jpg") no-repeat center top;}
+
+        .evt02 {background:#2a387f}
+
+        .youtube {padding:150px 0}
+        .youtube iframe {width:897px; height:507px}
+
+        .evtInfo {color:#646464; font-size:16px; background:#e2e2e2 url("https://static.willbes.net/public/images/promotion/2022/03/2580_03_bg.jpg") no-repeat center top; padding-bottom:100px}
+		.evtInfoBox {width:1000px; margin:0 auto; text-align:left; line-height:1.4}
+		.evtInfoBox h4 {font-size:40px; margin-bottom:40px}
+        .evtInfoBox div {font-size:20px; font-weight:bold; margin-bottom:10px}
+        .evtInfoBox ul {margin-bottom:40px}
+        .evtInfoBox li {list-style-type: decimal;margin-left:20px; margin-bottom:5px}
+    </style>
+
+    <div class="evtContent NSK">
+        <div class="sky" id="QuickMenu">
+            <a href="https://www.youtube.com/channel/UCUbuqBgYMeqiVR4FuEIgkiw" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2022/03/2580_sky.png" title="채널구독"></a>
+        </div>
+
+        <div class="evtCtnsBox evtTop" data-aos="fade-up">
+            <img src="https://static.willbes.net/public/images/promotion/2022/03/2580_top.jpg" alt="신태식 교육학논술"/>
+        </div>
+
+        <div class="evtCtnsBox evt01" data-aos="fade-up">
+            <img src="https://static.willbes.net/public/images/promotion/2022/03/2580_01.jpg" alt="커리큘럼"/>
+        </div>
+
+        <div class="evtCtnsBox evt02" data-aos="fade-up">
+            <img src="https://static.willbes.net/public/images/promotion/2022/03/2580_02.jpg" alt="학습에 도전"/>
+        </div>
+
+        <div class="evtCtnsBox youtube" data-aos="fade-up">
+            <iframe src="https://www.youtube.com/embed/V4B8cUDEXik?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>           
+        </div>
+
+        <div class="evtCtnsBox evtInfo" data-aos="fade-up">
+            <div class="wrap">
+                <img src="https://static.willbes.net/public/images/promotion/2022/03/2580_03.jpg"/>
+                <a href="https://ssam.willbes.net/package/show/cate/3134/pack/648001/prod-code/189065" title="수강하기" style="position: absolute; left: 77.5%; top: 58.23%; width: 18.57%; height: 23.18%; z-index: 2;"></a>
+            </div>
+            <div class="evtInfoBox">
+                <h4 class="NSK-Black">[필독] 유의사항</h4>
+                <div>[7일 무료체험 이벤트 유의사항]</div>
+                <ul>
+                    <li>본 이벤트는 교원임용시험을 도전하는 신규수강생 참여 가능한 이벤트 입니다.(기존 수강생은 참여할 수 있으나 혜택을 받을 수 없습니다)</li>
+                    <li>강의제공방식<br>
+                    - 강의체험기간은 7일이며, 강의시간은 1배수 형태로 제공됩니다. (※ 1배수란? 강의진행 시간만큼만 재생이 가능합니다)</li>
+                    <li>강의체험에 필요한 교재는 별도로 구매하셔야 합니다.</li>
+                    <li>무료체험강의는 양도 및 매매가 불가능하며, 위반시 처벌받을 수 있습니다.</li>
+                </ul>
+                <div>[70%할인  5~6월반 수강신청 이벤트 유의사항]</div>
+                <ul>
+                    <li>본 이벤트는 정가에서 70%할인하여 이벤트 기간에만 선착순 50명만 수강가능한 상품입니다. </li>
+                    <li>해당강의는 실강으로만 진행되는 강좌이며, 인강전환이 불가합니다.</li>
+                    <li>특별할인해서 기간한정으로 일시적으로만 판매되는 강좌이므로 수업시작 후 환불이 불가합니다.</li>
+                    <li>강의에 필요한 교재는 별도로 구매하셔야 합니다.</li>
+                    <li>강의는 양도 및 매매가 불가능하며, 위반시 처벌받을 수 있습니다.</li>
+                </ul>
+                <div>[참여혜택]</div>
+                <ul>
+                    <li>본 혜택은 참여한 모든 분들께 3~4월 논객반 및 하반기패키지 10%할인쿠폰 지급됩니다. 유료강의 신청시 쿠폰 사용하여 결재하시면 됩니다.</li>
+                    <li>본 혜택은 참여한 모든 분들께 추첨을 통하여 20명 교재를 증정해드립니다.<br>
+                    (추첨 발표는 4월 28일 윌비스 홈페이지이 공지사항 게시_개별통보)</li>
+                    <li>본 혜택은 기존 패키지 수강생들은 혜택에서 제외됩니다. </li>
+                </ul>
+            </div>
+        </div>  
+    </div>
+    <!-- End Container -->
+
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            AOS.init();
+            fnReviewList(1);
+        });
+    </script>
+@stop

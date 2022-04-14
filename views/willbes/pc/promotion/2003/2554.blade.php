1f37e4f149f3e785e0db4ca68ed5c1dd9689e9c8
이윤구 <yoongukk@willbes.com>
2022-04-14 10:33:50 +0900

    (공무원) 강치욱 생물학 무료 페이지 내 수강후기 이벤트 추가

--- views/willbes/pc/promotion/2003/2554.blade.php
+++ views/willbes/pc/promotion/2003/2554.blade.php
@@ -13,7 +13,7 @@
             padding:0 !important;
             background:#fff;            
         }
-        .evtContent span {vertical-align:auto}
+        .evtContent span {vertical-align:top;}
         .evtCtnsBox {width:100%; text-align:center; min-width:1120px; position:relative}
         .evtCtnsBox .wrap {width:1120px; margin:0 auto; position:relative}
         /*.evtCtnsBox .wrap a {border:1px solid #000}*/
@@ -29,14 +29,23 @@
         .wb_02 {background:#9831ff}
         .wb_02 a {position: absolute; left: 50%; top: 1390px; width:280px; margin-left:80px; height:50px; line-height:46px; z-index: 2; background:#141414; border:2px solid #000; border-radius:20px; color:#fed541; font-size:20px; font-weight:bold}
         .wb_02 a:hover {color:#000; background:#fed541; }
-        .wb_03 {padding:100px 0;}
-        .wb_03 .sTitle {margin:0 0 30px; font-size:30px; text-align:left; color:#9831ff}
+
+        .wb_03 {background:url(https://static.willbes.net/public/images/promotion/2022/04/2554_03_bg.jpg) no-repeat center top;}
+
+        .evt_apply {width:570px; margin:90px auto 0;}
+        .evt_apply a {display:block; font-size:28px; color:#fff; padding:25px; background:#000; border-radius:50px;}
+        .evt_apply a:hover {background:#533fd1}
+        .evt_apply span {color:#ffec67;}
+
+        .wb_05 {padding:100px 0;}
+        .wb_05 .sTitle {margin:0 0 30px; font-size:30px; text-align:left; color:#9831ff}
 
     </style>
 
     <div class="evtContent NSK" id="evtContainer">
         <div class="sky" id="QuickMenu">          
-            <a href="#lecbuy01"><img src="https://static.willbes.net/public/images/promotion/2022/03/2554_sky.png" alt="조경직 티패스" /></a>       
+            <a href="#lecbuy01"><img src="https://static.willbes.net/public/images/promotion/2022/03/2554_sky.png" alt="조경직 티패스" /></a>
+            <a href="#lecbuy02"><img src="https://static.willbes.net/public/images/promotion/2022/04/2554_sky2.png" alt="" /></a>       
         </div>
 
         <div class="evtCtnsBox wb_top" data-aos="fade-up">
@@ -55,9 +64,21 @@
                 <img src="https://static.willbes.net/public/images/promotion/2022/03/2554_02.jpg" alt="수강신청" />
                 <a href="https://pass.willbes.net/lecture/show/cate/3019/pattern/only/prod-code/191917" target="_blank">지금 바로 신청하기 ></a>
             </div>            
-        </div>      
+        </div>
 
         <div class="evtCtnsBox wb_03" data-aos="fade-up" id="lecbuy02">
+            <img src="https://static.willbes.net/public/images/promotion/2022/04/2554_03.jpg" alt="수강후기 이벤트" />
+        </div>      
+
+        <div class="evtCtnsBox wb_04" data-aos="fade-up">
+            <img src="https://static.willbes.net/public/images/promotion/2022/04/2554_04.jpg" alt="이벤트 기간" />
+        </div>
+
+         <div class="evt_apply NSK-Black" data-aos="fade-up">
+            <a href="https://www.willbes.net/classroom/on/list/ongoing" target="_blank" alt="수강후기 쓰기"><span>지금 바로 무료강좌 수강하고</span> 수강후기 쓰기 ></a>
+        </div>   
+
+        <div class="evtCtnsBox wb_05" data-aos="fade-up" id="lecbuy02">
             <div class="wrap">
                 <div class="sTitle NSK-Black">단과 수강신청</div>           
                 @if(empty($arr_base['display_product_data']) === false)

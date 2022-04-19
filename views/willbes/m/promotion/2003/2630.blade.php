58075e8ac0874f5f561b5e423855430e69d9914e
김창용 <winmir@willbes.com>
2022-04-19 14:27:18 +0900

    공무원 검찰직 하루 하나

---
+++ views/willbes/m/promotion/2003/2630.blade.php
@@ -0,0 +1,63 @@
+@extends('willbes.m.layouts.master')
+
+@section('content')
+<!-- Container -->
+
+<style type="text/css">
+
+    .evtCtnsBox {width:100%; max-width:720px; margin:0 auto; text-align:center; position:relative; line-height:1.5; clear:both}
+    .evtCtnsBox img {max-width:100%;}
+    .evtCtnsBox .wrap { margin:0 auto; position:relative}
+    /*.evtCtnsBox .wrap a {border:1px solid #000}*/
+    
+    /* 폰 가로, 태블릿 세로*/
+    @@media only screen and (max-width: 374px)  {   
+        
+    }
+
+    /* 태블릿 세로 */
+    @@media only screen and (min-width: 375px) and (max-width: 640px) {       
+
+    }
+    
+    /* 태블릿 가로, PC */
+    @@media only screen and (min-width: 641px) {
+        
+    }
+
+</style>
+
+<div id="Container" class="Container NSK c_both">
+
+    <div class="evtCtnsBox" data-aos="fade-down">
+        <img src="https://static.willbes.net/public/images/promotion/2022/04/2630m_top.jpg" alt="검찰직 하루 하나강" >
+    </div> 
+
+    <div class="evtCtnsBox" data-aos="fade-up">
+        <img src="https://static.willbes.net/public/images/promotion/2022/04/2630m_01.jpg" alt="하루 하나란?" >
+    </div>
+
+    <div class="evtCtnsBox" data-aos="fade-up">
+        <a href="https://www.youtube.com/channel/UC2J41ggwL4CTIJfoVBXsWkg/videos" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2022/04/2630m_02.jpg" alt="유튜브" > </a>
+    </div> 
+
+    <div class="evtCtnsBox" data-aos="fade-up">
+        <img src="https://static.willbes.net/public/images/promotion/2022/04/2630m_03.jpg" alt="연간 커리큘럼" >
+    </div> 
+
+</div>
+
+<!-- End Container -->
+
+<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script>
+      $( document ).ready( function() {
+        AOS.init();
+      } );
+</script>
+
+
+{{-- 프로모션용 스크립트 include --}}
+@include('willbes.pc.promotion.promotion_script')
+@stop
---
+++ views/willbes/pc/promotion/2003/2630.blade.php
@@ -0,0 +1,57 @@
+@extends('willbes.pc.layouts.master')
+
+@section('content')
+    @include('willbes.pc.layouts.partial.site_menu')
+    <!-- Container -->
+    <style type="text/css">
+        .evtContent {
+            width:100% !important;
+            min-width:1120px !important;
+            max-width:2000px !important;
+            margin:0 auto;
+            margin-top:20px !important;
+            padding:0 !important;
+            background:#fff url(https://static.willbes.net/public/images/promotion/2022/04/2630_bg.jpg) no-repeat center top;          
+        }
+        .evtContent span {vertical-align:top}
+        .evtCtnsBox {width:100%; text-align:center; min-width:1120px;}
+        .evtCtnsBox .wrap {width:1120px; margin:0 auto; position:relative}
+        /*.evtCtnsBox .wrap a {border:1px solid #fff}*/
+        /************************************************************/
+
+        .wb_02 {background:#130c05; padding-bottom:150px}      
+
+         
+    </style>
+
+    <div class="evtContent NSK" id="evtContainer">
+
+        <div class="evtCtnsBox wb_top" data-aos="fade-down">            
+            <img src="https://static.willbes.net/public/images/promotion/2022/04/2630_top.jpg" alt="9급 검찰직 하루 하나!"/>               
+        </div>
+
+        <div class="evtCtnsBox wb_01" data-aos="fade-up">
+            <img src="https://static.willbes.net/public/images/promotion/2022/04/2630_01.jpg"  alt="하루 하나란?"/>
+		</div>
+
+        <div class="evtCtnsBox wb_02" data-aos="fade-up">            
+            <a href="https://www.youtube.com/channel/UC2J41ggwL4CTIJfoVBXsWkg/videos" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2022/04/2630_02.jpg"  alt="유튜브"/>              
+		</div>
+
+        <div class="evtCtnsBox wb_03" data-aos="fade-up">            
+            <img src="https://static.willbes.net/public/images/promotion/2022/04/2630_03.jpg" alt=""/></a>            
+		</div>
+
+    </div>
+
+    <!-- End Container -->
+
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        $( document ).ready( function() {
+            AOS.init();
+        } );      
+    </script>
+    
+@stop

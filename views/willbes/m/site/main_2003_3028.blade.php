534d03fc0794cb60f9ed930f4acce527538bc73a
신봉석 <bsshin@willbes.com>
2022-05-12 09:17:46 +0900

    모바일 > 공무원 메인 교수진 배너영역 수정

--- views/willbes/m/site/main_2003_3028.blade.php
+++ views/willbes/m/site/main_2003_3028.blade.php
@@ -1,13 +1,34 @@
 @extends('willbes.m.layouts.master')
 
 @section('content')
+    <style>
+        .gosi .swiper-sec04 {position: relative;overflow: hidden;max-height:620px;background:#c0bcb0;margin-top:30px;}
+        .gosi .swiper-sec04 .gosiTitle {text-align:left; margin-left:24px; color:#fff}
+        .gosi .swiper-sec04 .swiper-wrapper {display: flex; justify-content: space-between; padding-left:20px }
+        .gosi .swiper-sec04 .swiper-container-sec04 .swiper-slide {width: 208px; max-height:407px; margin-right:20px; align-items: flex-start; background:none;}
+        .gosi .swiper-sec04 .swiper-slide a {display: block;}
+        .gosi .swiper-sec04 .swiper-slide img {max-width: 100%;}
+
+        /* iPhone 5/SE */
+        @@media only screen and (max-width: 374px) {
+            .gosi .swiper-sec04 {max-height:400px;}
+            .gosi .swiper-sec04 .gosiTitle {margin-left:10px; font-size:18px; padding-top:30px}
+            .gosi .swiper-sec04 .swiper-wrapper {padding-left:10px}
+            .gosi .swiper-sec04 .swiper-container-sec04 .swiper-slide {width: 130px; max-height:auto; margin-right:10px;}
+        }
+    </style>
+
     <div id="Container" class="Container NSK gosi mb40">
         {!! banner('M_메인상단', 'MainSlider mt20', $__cfg['SiteCode'], $__cfg['CateCode']) !!}
         {!! banner('M_메인빅배너', 'MainSlider mt20', $__cfg['SiteCode'], $__cfg['CateCode']) !!}
         {!! banner('M_메인서브', 'MainSlider mt20', $__cfg['SiteCode'], $__cfg['CateCode']) !!}
 
-        <div class="gosiTitle NSK">합격을 책임질 <span class="NSK-Black">윌비스 교수진</span></div>
-        <div class="gosiProf">{!! banner('M_메인_교수진', 'swiper-container-prof', $__cfg['SiteCode'], $__cfg['CateCode']) !!}</div>
+        <div class="swiper-sec04">
+            <div class="gosiTitle NSK">
+                합격을 책임지는 <strong class="NSK-Black">윌비스 교수진</strong>
+            </div>
+            {!! banner('M_메인_교수진', 'swiper-container-sec04', $__cfg['SiteCode'], $__cfg['CateCode']) !!}
+        </div>
 
         {{-- 게시판 --}}
         <div class="noticeTabs c_both mt30">
@@ -26,24 +47,14 @@
     <script type="text/javascript">
         $(function() {
             //교수진
-            var swiper = new Swiper('.swiper-container-prof', {
+            new Swiper('.swiper-container-sec04', {
                 slidesPerView: 'auto',
-                slidesPerColumn: 2,
-                spaceBetween: 30,
+                slidesPerColumn: 1,
+                spaceBetween: 0,
                 autoplay: {
                     delay: 3000,
                     disableOnInteraction: false,
                 }, //3초에 한번씩 자동 넘김
-                // init: false,
-                pagination: {
-                    el: '.swiper-pagination',
-                    clickable: true,
-                },
-                breakpoints: {
-                    640: {
-                        spaceBetween: 10,
-                    },
-                }
             });
         });
     </script>

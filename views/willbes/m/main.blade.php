24bc8dfac803946e7fc25705c708a226850b3fb9
김창용 <winmir@willbes.com>
2022-06-23 09:38:53 +0900

    모바일 게이트 손해평가사 추가

--- views/willbes/m/main.blade.php
+++ views/willbes/m/main.blade.php
@@ -9,7 +9,7 @@
                     <ul class="bigType">
                         <li><a href="{{ front_app_url('/home/index', 'police') }}" target="_blank">신광은경찰</a></li>
                         <li><a href="{{ front_app_url('/intro/index', 'pass') }}" target="_blank">공무원</a></li>
-                        <li><a href="{{ front_app_url('/home/index', 'ssam') }}" target="_blank">교원임용 <span>N</span></a></li> 
+                        <li><a href="{{ front_app_url('/home/index', 'ssam') }}" target="_blank">교원임용</a></li> 
                         <li><a href="{{ front_app_url('/home/index/cate/3094', 'gosi') }}" target="_blank">5급행정</a></li>                        
                         <li><a href="{{ front_app_url('/home/index/cate/3095', 'gosi') }}" target="_blank">국립외교원</a></li>
                         <li><a href="{{ front_app_url('/home/index/cate/3035', 'pass') }}" target="_blank">법원직</a></li>

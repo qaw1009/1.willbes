e56eb16ad36e0e40fe54a29222b8e1ce02bb77a8
김창용 <winmir@willbes.com>
2022-06-23 09:53:15 +0900

    모바일 게이트 손해평가사 추가

--- views/willbes/m/main.blade.php
+++ views/willbes/m/main.blade.php
@@ -29,7 +29,7 @@
                         <li><a href="{{ front_app_url('/home/index', 'willbesedu', true) }}" target="_blank">인천학원</a></li>                        
                         <li><a href="{{ front_app_url('/home/index/cate/308906', 'job') }}" target="_blank">빅데이터분석기사</a></li>
                         <li><a href="{{ front_app_url('/home/index/cate/309007', 'job') }}" target="_blank">손해평가사 <span>N</span></a></li>
-                        <li class="full"><a href="{{ front_app_url('/home/index', 'book') }}" target="_blank">온라인서점</a></li>
+                        <li><a href="{{ front_app_url('/home/index', 'book') }}" target="_blank">온라인서점</a></li>
                     </ul>
                     <div class="etc">
                         <a href="#none" class="btnMainToggle">기타자격증 <span>+</span></a>

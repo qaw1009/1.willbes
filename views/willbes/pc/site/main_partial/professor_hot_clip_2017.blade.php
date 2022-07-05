e43841bbe7e6e2b10d5a41e2f1be3999aade388d
최현탁 <cht@willbes.com>
2022-07-05 14:12:33 +0900

    임용 메인 페이지 핫클립 영역 수정

--- views/willbes/pc/site/main_partial/professor_hot_clip_2017.blade.php
+++ views/willbes/pc/site/main_partial/professor_hot_clip_2017.blade.php
@@ -1,62 +1,4 @@
 <div class="sec-prof-title"><img src="https://static.willbes.net/public/images/promotion/main/2018/title01.jpg" title="교수진"></div>
-{{--
-<div class="widthAuto p_re NSK">
-    <ul class="prof-Tab" id="profRolling">
-        @if(empty($data['prof_hot_clip']) === false)
-            @foreach($data['prof_hot_clip'] as $row)
-                <li><a href="#tab{{$loop->index}}" @if($loop->first)class="active"@endif><span>{{ $row['SubjectName'] }}</span>{{ $row['ProfNickName'] }}</a></li>
-            @endforeach
-        @endif
-    </ul>
-
-    <div class="prof-Tab-Wrap" id="profRollingSlider">
-    @if(empty($data['prof_hot_clip']) === false)
-        @foreach($data['prof_hot_clip'] as $row)
-            <div class="prof-Tab-Cts" id="tab{{$loop->index}}">
-                <div class="btnBox">
-                    <div class="prof-top-btn">
-                        @if($row['ProfBtnIsUse'] == 'Y')
-                            <a href="{{front_url("/professor/show/prof-idx/{$row['ProfIdx']}?cate_code={$row['CateCode']}&subject_idx={$row['SubjectIdx']}")}}">교수님 홈</a>
-                        @endif
-                        @if($row['CurriculumBtnIsUse'] == 'Y')
-                            <a href="javascript:void(0);" onclick="openWin('sec-prof-layer'); fnOpenProfCurriculum('{{$row['ProfIdx']}}');">커리큘럼</a>
-                        @endif
-                        @if($row['StudyCommentBtnIsUse'] == 'Y')
-                            <a href="javascript:void(0);" onclick="fnStudyComentLayer('{{$row['SubjectIdx']}}', '{{$row['ProfIdx']}}', '', '{{$row['CateCode']}}', 'ProfReply');">수강후기</a>
-                        @endif
-                    </div>
-                    @if(empty($row['thumbnail_data']) === false)
-                        <div class="prof-clip-btn">
-                            @php
-                                $thumbnail_data = json_decode($row['thumbnail_data'],true);
-                                foreach ($thumbnail_data as $thumbnail_row) {
-                                    $html = '';
-                                    if ($thumbnail_row['LinkType'] == 'layer') {
-                                        $html .= '<a onclick="fnOpenYoutube(\''.$thumbnail_row['LinkUrl'].'\')">';
-                                    } elseif ($thumbnail_row['LinkType'] == 'self') {
-                                        $html .= '<a href="'.$thumbnail_row['LinkUrl'].'">';
-                                    } elseif ($thumbnail_row['LinkType'] == 'blank') {
-                                        $html .= '<a href="'.$thumbnail_row['LinkUrl'].'" target="_balnk">';
-                                    } else {
-                                        $html .= '<a href="javascript:void(0);">';
-                                    }
-                                    $html .= '<img src="'.$thumbnail_row['ThumbnailPath'].$thumbnail_row['ThumbnailFileName'].'" title="유튜브">';
-                                    $html .= '</a>';
-                                    echo $html;
-                                }
-                            @endphp
-                        </div>
-                    @endif
-                </div>
-                @if(empty($row['thumbnail_data']) === false)<span class="hotclip"><img src="https://static.willbes.net/public/images/promotion/main/2018/hotclip.jpg" title="hot clip"></span>@endif
-                <img src="{{$row['ProfBgImagePath'].$row['ProfBgImageName']}}" title="{{$row['SubjectName']}} {{$row['ProfNickName']}}">
-            </div>
-        @endforeach
-    @endif
-    </div>
-</div>
---}}
-
 <div class="widthAuto p_re NSK">
     <ul class="prof-Tab" id="profRolling">
         @if(empty($data['prof_hot_clip']) === false)
@@ -67,49 +9,51 @@
     </ul>
 
     <div class="prof-Tab-Wrap" id="profRollingSlider">
-    @if(empty($data['prof_hot_clip']) === false)
-        @foreach($data['prof_hot_clip'] as $row)
-            <div class="prof-Tab-Cts" id="tab{{$loop->index}}">
-                <div class="btnBox">
-                    <div class="prof-top-btn">
-                        @if($row['ProfBtnIsUse'] == 'Y')
-                            <a href="{{front_url("/professor/show/prof-idx/{$row['ProfIdx']}?cate_code={$row['CateCode']}&subject_idx={$row['SubjectIdx']}")}}">교수님 홈</a>
-                        @endif
-                        @if($row['CurriculumBtnIsUse'] == 'Y')
-                            <a href="javascript:void(0);" onclick="openWin('sec-prof-layer'); fnOpenProfCurriculum('{{$row['ProfIdx']}}');">커리큘럼</a>
-                        @endif
-                        @if($row['StudyCommentBtnIsUse'] == 'Y')
-                            <a href="javascript:void(0);" onclick="fnStudyComentLayer('{{$row['SubjectIdx']}}', '{{$row['ProfIdx']}}', '', '{{$row['CateCode']}}', 'ProfReply');">수강후기</a>
+        @if(empty($data['prof_hot_clip']) === false)
+            @foreach($data['prof_hot_clip'] as $row)
+                <div class="prof-Tab-Cts" id="tab{{$loop->index}}">
+                    <div class="btnBox">
+                        <div class="prof-top-btn">
+                            @if($row['ProfBtnIsUse'] == 'Y')
+                                <a href="{{front_url("/professor/show/prof-idx/{$row['ProfIdx']}?cate_code={$row['CateCode']}&subject_idx={$row['SubjectIdx']}")}}">교수님 홈</a>
+                            @endif
+                            @if($row['CurriculumBtnIsUse'] == 'Y')
+                                <a href="javascript:void(0);" onclick="openWin('sec-prof-layer'); fnOpenProfCurriculum('{{$row['ProfIdx']}}');">커리큘럼</a>
+                            @endif
+                            @if($row['StudyCommentBtnIsUse'] == 'Y')
+                                <a href="javascript:void(0);" onclick="fnStudyComentLayer('{{$row['SubjectIdx']}}', '{{$row['ProfIdx']}}', '', '{{$row['CateCode']}}', 'ProfReply');">수강후기</a>
+                            @endif
+                        </div>
+                        @if(empty($row['thumbnail_data']) === false)
+                            <div class="prof-clip-btn">
+                                @php
+                                    $thumbnail_data = json_decode($row['thumbnail_data'],true);
+                                    foreach ($thumbnail_data as $thumbnail_row) {
+                                        $html = '';
+                                        if ($thumbnail_row['LinkType'] == 'layer') {
+                                            $html .= '<a onclick="fnOpenYoutube(\''.$thumbnail_row['LinkUrl'].'\')">';
+                                        } elseif ($thumbnail_row['LinkType'] == 'self') {
+                                            $html .= '<a href="'.$thumbnail_row['LinkUrl'].'">';
+                                        } elseif ($thumbnail_row['LinkType'] == 'blank') {
+                                            $html .= '<a href="'.$thumbnail_row['LinkUrl'].'" target="_balnk">';
+                                        } else {
+                                            $html .= '<a href="javascript:void(0);">';
+                                        }
+                                        $html .= '<img src="'.$thumbnail_row['ThumbnailPath'].$thumbnail_row['ThumbnailFileName'].'" title="유튜브">';
+                                        $html .= '</a>';
+                                        echo $html;
+                                    }
+                                @endphp
+                            </div>
                         @endif
                     </div>
-                    @if(empty($row['thumbnail_data']) === false)
-                        <div class="prof-clip-btn">
-                            @php
-                                $thumbnail_data = json_decode($row['thumbnail_data'],true);
-                                foreach ($thumbnail_data as $thumbnail_row) {
-                                    $html = '';
-                                    if ($thumbnail_row['LinkType'] == 'layer') {
-                                        $html .= '<a onclick="fnOpenYoutube(\''.$thumbnail_row['LinkUrl'].'\')">';
-                                    } elseif ($thumbnail_row['LinkType'] == 'self') {
-                                        $html .= '<a href="'.$thumbnail_row['LinkUrl'].'">';
-                                    } elseif ($thumbnail_row['LinkType'] == 'blank') {
-                                        $html .= '<a href="'.$thumbnail_row['LinkUrl'].'" target="_balnk">';
-                                    } else {
-                                        $html .= '<a href="javascript:void(0);">';
-                                    }
-                                    $html .= '<img src="'.$thumbnail_row['ThumbnailPath'].$thumbnail_row['ThumbnailFileName'].'" title="유튜브">';
-                                    $html .= '</a>';
-                                    echo $html;
-                                }
-                            @endphp
-                        </div>
-                    @endif
+                    @if(empty($row['thumbnail_data']) === false)<span class="hotclip"><img src="https://static.willbes.net/public/images/promotion/main/2018/hotclip.jpg" title="hot clip"></span>@endif
+                    <a href="{!! (($row['ProfBgLinkUrlType'] == 'N') ? 'javascript:void(0);' : ($row['ProfBgLinkUrlType'] == 'I' ? '//'.(strpos($row['ProfBgLinkUrl'], config_item('base_domain')) === false ? $row['ProfBgLinkUrl'] : app_to_env_url($row['ProfBgLinkUrl'])) : $row['ProfBgLinkUrl'])) !!}">
+                        <img src="{{$row['ProfBgImagePath'].$row['ProfBgImageName']}}" title="{{$row['SubjectName']}} {{$row['ProfNickName']}}" class="url-type-{{$row['ProfBgLinkUrlType']}}">
+                    </a>
                 </div>
-                @if(empty($row['thumbnail_data']) === false)<span class="hotclip"><img src="https://static.willbes.net/public/images/promotion/main/2018/hotclip.jpg" title="hot clip"></span>@endif
-                <a href="#none"><img src="{{$row['ProfBgImagePath'].$row['ProfBgImageName']}}" title="{{$row['SubjectName']}} {{$row['ProfNickName']}}"></a>
-            </div>
-        @endforeach
-    @endif
+            @endforeach
+        @endif
     </div>
 </div>
 

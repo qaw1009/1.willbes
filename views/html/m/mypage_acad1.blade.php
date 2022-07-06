58c83114ca745bdcecd354018e848c7a95b7cdbc
김창용 <winmir@willbes.com>
2022-07-06 13:14:10 +0900

    임용 모바일 수강증 관련 부분 수정

--- views/html/m/mypage_acad1.blade.php
+++ views/html/m/mypage_acad1.blade.php
@@ -93,7 +93,7 @@
                                             수강기간 : 2018-00-00~2018-00-00<br>
                                             요일/회차 : 월화수목금 10회차<span class="row-line">|</span>2018-00-00 개강
                                         </div>
-                                        <div class="supplementBtn"><a href="mypage_acad1_7">보강/복습동영상 신청 ></a></div>
+                                        <div class="supplementBtn"><a href="mypage_acad1_7">보강/복습동영상 신청 ></a> <a href="mypage_acad1_8" class="type2">수강증 보기 ></a></div> 
                                     </li>
                                     <li>
                                         <div>영어<span class="row-line">|</span>한덕현교수님</div>
@@ -104,7 +104,7 @@
                                             수강기간 : 2018-00-00~2018-00-00<br>
                                             요일/회차 : 월화수목금 10회차<span class="row-line">|</span>2018-00-00 개강
                                         </div>
-                                        <div class="supplementBtn"><a href="mypage_acad1_7">보강/복습동영상 신청 ></a></div>
+                                        <div class="supplementBtn"><a href="mypage_acad1_7">보강/복습동영상 신청 ></a> <a href="mypage_acad1_8" class="type2">수강증 보기 ></a></div>
                                     </li>
                                     <li>
                                         <div>영어<span class="row-line">|</span>한덕현교수님</div>
@@ -115,7 +115,7 @@
                                             수강기간 : 2018-00-00~2018-00-00<br>
                                             요일/회차 : 월화수목금 10회차<span class="row-line">|</span>2018-00-00 개강
                                         </div>
-                                        <div class="supplementBtn"><a href="mypage_acad1_7">보강/복습동영상 신청 ></a></div>
+                                        <div class="supplementBtn"><a href="mypage_acad1_7">보강/복습동영상 신청 ></a> <a href="mypage_acad1_8" class="type2">수강증 보기 ></a></div>
                                     </li>                                   
                                 </ul>                            
                             </div> 
@@ -182,7 +182,7 @@
                             <div class="w-info bg-light-gray pd10 pb0 mt10 bdb-bright-gray">  
                                 <div class="mb10"><a href="#none" class="btnStfull02">온라인첨삭 ></a></div>    
                                 <div class="mb10"><a href="#none" class="btnStfull03">보강/복습동영상 신청 ></a></div>     
-                                <div class="mb10"><a href="#none" class="btnStfull04">수강증 보기 ></a></div>                       
+                                <div class="mb10"><a href="mypage_acad1_8" class="btnStfull04">수강증 보기 ></a></div>                       
                             </div>
                         </td>
                     </tr>
--- views/html/m/mypage_acad1_8.blade.php
+++ views/html/m/mypage_acad1_8.blade.php
@@ -15,16 +15,24 @@
     </div> 
 
     <div class="cardWrap">
-        <ul>
-            <li>한주연(463463)</li>
-            <li>상품명이 출력됩니다. 상품명이출력됩니다. 상품명이 출력됩니다. 상품명이출력됩니다. </li>
-            <li>2022-06-28
-                <div>
-                    <img src="https://static.willbes.net/public/images/promotion/m/card_logo.png" alt="">
-                    <span>임용</span>
-                    <img src="/public/img/willbes/stamp/stamp.png">
-                </div>
+        <ul class="cardBox">
+            <li><span>합격을 부르는 윌비스 임용</span></li>
+            <li>
+                <div><span>수강생명 :</span> 전병식(481387)</div>
+                <div><span>강의명 :</span> 2022(07~08월) 신태식 교육학 단원별 실전 모의고사반)</div>
             </li>
+            <li>2022-06-28</li>
+            <li><img src="https://static.willbes.net/public/images/promotion/m/ssam_card_logo.jpg"></li>
+        </ul>
+        <ul class="info">
+            <li>유의사항</li>
+            <li>본 수강증은 판매 및 양도, 공유할 수 없습니다. (적발시 법적책임이 따릅니다)</li>
+            <li>수강 환불 시, 원 수강료(할인전 수강료)에서 적용됩니다. </li>
+            <li>1/2이 지나면 환불이 되지 않습니다.(한 달 기준)</li>
+            <li>수강환불 시, 학원에 오셔서 카드결제하였다면 카드 영수증을 지참하셔야 합니다.</li>
+            <li>윌비스임용고시학원은 교육청의 수강료 반환기준을 준수합니다.</li>
+            <li>수강 등록 후, 학원의 제반 규정을 준수해야 합니다. </li>
+            <li>학원 관계자의 요구시 본 수강증을 제시하셔야 합니다. (미 지참시 강의실 입실 불가)</li>
         </ul>
     </div> 
 

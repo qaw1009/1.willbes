63db8e2dbc665042ec90ec07b6e5cb88274cd9af
김창용 <winmir@willbes.com>
2022-07-06 10:06:56 +0900

    공무원 7급 PSAT 이나우, 한승아 프로모션

--- views/willbes/pc/promotion/2003/2711.blade.php
+++ views/willbes/pc/promotion/2003/2711.blade.php
@@ -16,29 +16,30 @@
         .evtContent span {vertical-align:top}
         .evtCtnsBox {width:100%; text-align:center; min-width:1120px; position:relative}
         .evtCtnsBox .wrap {width:1120px; margin:0 auto; position:relative}
-        /*.evtCtnsBox .wrap a {border:1px solid #000}*/
+        .evtCtnsBox .wrap a {border:1px solid #000}
 
         /************************************************************/
 
-        .wb_top {background:url(https://static.willbes.net/public/images/promotion/2022/06/2708_top_bg.jpg) no-repeat center top;}
+        .wb_top {background:#ff8aad url(https://static.willbes.net/public/images/promotion/2022/07/2711_top_bg.jpg) no-repeat center top; height: 1304px;}
+        .wb_top div {padding-top:480px}
 
     </style>
 
     <div class="evtContent NSK" id="evtContainer">
 
-        <div class="evtCtnsBox wb_top" data-aos="fade-down">            
-            <img src="https://static.willbes.net/public/images/promotion/2022/06/2708_top.jpg" alt="한승아 언어논리 "/>                         
+        <div class="evtCtnsBox wb_top">            
+            <div><img src="https://static.willbes.net/public/images/promotion/2022/07/2711_top.png" alt="한승아 언어논리" data-aos="fade-down"/></div>                      
         </div>
 
         <div class="evtCtnsBox wb_01" data-aos="fade-up">
             <div class="wrap">
-                <img src="https://static.willbes.net/public/images/promotion/2022/06/2708_01.jpg" alt="">
-                <a href="https://pass.willbes.net/pass/offLecture/show/cate/3143/prod-code/199385" target="_blank" title="수강신청" style="position: absolute;left: 70.45%;top: 93.84%;width: 28.3%;height: 3.46%;z-index: 2;"></a>
+                <img src="https://static.willbes.net/public/images/promotion/2022/07/2711_01.jpg" alt="">
+                <a href="https://pass.willbes.net/pass/offLecture/show/cate/3143/prod-code/199385" target="_blank" title="수강신청" style="position: absolute; left: 70%; top: 92.2%; width: 28.93%; height: 4.48%; z-index: 2;"></a>
             </div>
 		</div>
 
         <div class="evtCtnsBox wb_02" data-aos="fade-up"> 
-            <img src="https://static.willbes.net/public/images/promotion/2022/06/2708_02.jpg" alt="">              
+            <img src="https://static.willbes.net/public/images/promotion/2022/07/2711_02.jpg" alt="">              
 		</div>
     </div>
 

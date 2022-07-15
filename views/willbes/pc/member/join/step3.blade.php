8e3e511e4afbd41623121d3b37bbea685dbbbb89
하준걸 <hereisme@willbes.net>
2022-07-15 10:38:05 +0900

    공무원 회원가입 Meta 코드 삽입

--- views/willbes/pc/member/join/step3.blade.php
+++ views/willbes/pc/member/join/step3.blade.php
@@ -113,6 +113,22 @@
             kakaoPixel('8760977232968567433').pageView();
             kakaoPixel('8760977232968567433').completeRegistration('회원가입');
         </script>
+
+        <!-- Meta Pixel Code -->
+        <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '757464248917693');
+            fbq('track', 'Complete Registration');
+        </script>
+        <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=757464248917693&ev=PageView&noscript=1"/></noscript>
+        <!-- End Meta Pixel Code -->
     @endif
 
     @if(sess_data('mem_interest') == '718001')

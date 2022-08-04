fd4d8cf0413a41a3f4b78afb7ed673909715be13
하준걸 <hereisme@willbes.net>
2022-08-04 15:20:56 +0900

    자격증 (718003) 회원가입완료 ga 추가

--- views/willbes/pc/member/join/step3.blade.php
+++ views/willbes/pc/member/join/step3.blade.php
@@ -107,6 +107,17 @@
         _nasa["cnv"] = wcs.cnv("2","10");
     </script>
 
+    @if(sess_data('mem_interest') == '718003')
+    {{-- 교육사업부 김문정요청 (김상구본부장님) --}}
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NTXFJQH');</script>
+    <!-- End Google Tag Manager -->
+    @endif
+
     @if(sess_data('mem_interest') == '718002')
         <script type="text/javascript" charset="UTF-8" src="//t1.daumcdn.net/kas/static/kp.js"></script>
         <script type="text/javascript">

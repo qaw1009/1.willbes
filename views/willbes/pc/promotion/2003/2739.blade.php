1cca52ea4cee459c942a9d9be0a733e3fcc53122
윤석찬 <onlybright87@willbes.com>
2022-08-16 11:31:45 +0900

    (공무원) 신규 임병주 교수 프로모션 페이지

--- views/willbes/pc/promotion/2003/2739.blade.php
+++ views/willbes/pc/promotion/2003/2739.blade.php
@@ -21,35 +21,52 @@
 
         /************************************************************/
 
-        .wb_top {background:url(https://static.willbes.net/public/images/promotion/2022/07/2725_top_bg.jpg) no-repeat center top; height: 1118px;}
-        .wb_top span {position:absolute; left:50%}
-        .wb_top .img01 {position:absolute; width:963px; top:100px; margin-left:-481px; z-index: 1;}
-        .wb_top .img02 {position:absolute; width:693px; top:450px; margin-left:-346px; z-index: 2;}
+        .wb_top {background:url(https://static.willbes.net/public/images/promotion/2022/08/2739_top_bg.jpg) no-repeat center top; }
+        .wb_02{background-color: #e1e1e1;}
+        .wb_03 .youtube{position: absolute; top:66%; left:50%; transform: translateX(-50%); width:80%;}
+        .wb_03 .embed-container{position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%;}
+        .wb_03 .embed-container iframe, .embed-container object, .embed-container embed{
+            position: absolute;
+            top:0;
+            left:0;
+            width: 100%;
+            height: 100%;
+        }
+        .wb_04{padding-bottom: 100px;}
     </style>
 
 
     <div class="evtContent NSK" id="evtContainer">
-        <div class="evtCtnsBox wb_top">
-            <span class="img02" data-aos="fade-right"><img src="https://static.willbes.net/public/images/promotion/2022/07/2725_top02.png" alt="행정법"/></span>
-            <span class="img01" data-aos="fade-left"><img src="https://static.willbes.net/public/images/promotion/2022/07/2725_top01.png" alt="행정법"/></span>
+        <div class="evtCtnsBox wb_top"  data-aos="fade-up">
+            <img src="https://static.willbes.net/public/images/promotion/2022/08/2739_top.jpg" alt="행정법의 신"/>
         </div>
 
-        <div class="evtCtnsBox wb_01">
-            <img src="https://static.willbes.net/public/images/promotion/2022/07/2725_top02.png" alt="행정법 임병주"/>
+        <div class="evtCtnsBox wb_01"  data-aos="fade-up">
+            <img src="https://static.willbes.net/public/images/promotion/2022/08/2739_01.jpg" alt="수험생들을 멘붕에 빠뜨린 행정법!"/>
         </div>
 
-        <div class="evtCtnsBox wb_02">
-            <img src="https://static.willbes.net/public/images/promotion/2022/07/2725_top02.png" alt="행정법"/>
+        <div class="evtCtnsBox wb_02"  data-aos="fade-up">
+            <img src="https://static.willbes.net/public/images/promotion/2022/08/2739_02.jpg" alt="임병주 교수님과 함께면 가능합니다."/>
+
         </div>
 
-        <div class="evtCtnsBox wb_03">
-            <img src="https://static.willbes.net/public/images/promotion/2022/07/2725_top02.png" alt="행정법"/>
+        <div class="evtCtnsBox wb_03"  data-aos="fade-up">
+            <div class="wrap">
+                <img src="https://static.willbes.net/public/images/promotion/2022/08/2739_03.jpg" alt="임병주 교수님과 만나면 더 쉬워집니다."/>
+                <div class="youtube">
+                    <div class="embed-container">
+                        <iframe src="https://www.youtube.com/embed/Gp4IU7Ouykc?rel=0" frameborder="0" allowfullscreen></iframe>
+                    </div> 
+                </div> 
+            </div>
         </div>
-
-        <div class="evtCtnsBox wb_04">            
-            @if(empty($arr_base['display_product_data']) === false)
+        <div class="evtCtnsBox wb_04"  data-aos="fade-up">
+            <div class="wrap">
+                <img src="https://static.willbes.net/public/images/promotion/2022/08/2739_04.jpg" alt="임병주 교수님만 믿고 따라오세요!"/>
+                @if(empty($arr_base['display_product_data']) === false)
                 @include('willbes.pc.promotion.display_product_partial',array('group_num'=>1))
-            @endif
+                @endif
+            </div>
         </div>
 
     </div>

659ca2676942507d8d94eb3c086aede14cb6344a
하준걸 <hereisme@willbes.com>
2022-08-19 13:12:56 +0900

    스타플레이어 업데이트

--- public/vendor/starplayer/js/starplayer_config.js
+++ public/vendor/starplayer/js/starplayer_config.js
@@ -1,28 +1,20 @@
 //x86
-//var STARPLAYER_URL = "https://cabstarplayer.myskcdn.com/starplayer/starplayer-1.5.24.55.cab";
-//var STARPLAYER_SETUP_URL = "https://cabstarplayer.myskcdn.com/starplayer/starplayer-1.5.24.55.exe";
 var STARPLAYER_URL = "https://cabstarplayer.myskcdn.com/starplayer/starplayer-1.5.27.60.cab";
 var STARPLAYER_SETUP_URL = "https://cabstarplayer.myskcdn.com/starplayer/starplayer-1.5.27.60.exe";
 var STARPLAYER_VERSION = "1,5,22,54";
 
 //x64
-//var STARPLAYER64_URL = "https://cabstarplayer.myskcdn.com/starplayer/starplayer64-1.5.24.55.cab";
-//var STARPLAYER64_SETUP_URL = "https://cabstarplayer.myskcdn.com/starplayer/starplayer64-1.5.24.55.exe";
 var STARPLAYER64_URL = "https://cabstarplayer.myskcdn.com/starplayer/starplayer64-1.5.27.60.cab";
 var STARPLAYER64_SETUP_URL = "https://cabstarplayer.myskcdn.com/starplayer/starplayer64-1.5.27.60.exe";
 var STARPLAYER64_VERSION = "1,5,22,54";
 
 //OSX
-//var STARPLAYER_OSX_SETUP_URL = "https://cabstarplayer.myskcdn.com/starplayer/StarPlayerAgent_1.0.5.17.pkg";
-//var STARPLAYER_OSX_VERSION = "1,0,5,17";
-//var STARPLAYER_OSX_SETUP_URL = "https://cabstarplayer.myskcdn.com/starplayer/StarPlayerAgent_1.0.7.22.pkg";
-var STARPLAYER_OSX_SETUP_URL = "https://cab-starplayer.service.concdn.com/starplayer/StarPlayerAgent_1.1.8.11.pkg";
-var STARPLAYER_OSX_VERSION = "1,0,7,22";
+var STARPLAYER_OSX_SETUP_URL = "https://cab-starplayer.service.concdn.com/starplayer/StarPlayerAgent_1.1.8.16.pkg";
+var STARPLAYER_OSX_VERSION = "1,1,8,16";
 
 //Agent
-//var STARPLAYER_AGENT_SETUP_URL = "https://cabstarplayer.myskcdn.com/starplayer/starplayer_agent_1.2.8.81.exe";
-var STARPLAYER_AGENT_SETUP_URL = "https://cabstarplayer.myskcdn.com/starplayer/starplayer_agent_1.3.12.95.exe";
-var STARPLAYER_AGENT_VERSION = "1,2,8,81";
+var STARPLAYER_AGENT_SETUP_URL = "https://cab-starplayer.service.concdn.com/starplayer/starplayer_agent_1.3.14.3.exe";
+var STARPLAYER_AGENT_VERSION = "1,3,14,3";
 
 //CHROME
 var STARPLAYER_CHROME_URL = "https://cabstarplayer.myskcdn.com/starplayer/chrome.txt";

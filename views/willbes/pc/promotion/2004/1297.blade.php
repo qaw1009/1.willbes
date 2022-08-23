297968650cf71bae7ef5fe3e68d36b6219ee0a18
이윤구 <yoongukk@willbes.com>
2022-08-23 14:17:21 +0900

    (공무원학원) 새벽모의고사 페이지 수정

--- views/willbes/pc/promotion/2004/1297.blade.php
+++ views/willbes/pc/promotion/2004/1297.blade.php
@@ -17,7 +17,8 @@
                 
         /************************************************************/
 
-        .wb_top {background:url(https://static.willbes.net/public/images/promotion/2021/07/1297_top_bg.jpg) no-repeat center top;}
+        .wb_top {background:url(https://static.willbes.net/public/images/promotion/2022/08/1297_top_bg.jpg) no-repeat center top;height:960px;position:relative;}
+        .wb_tops {position:absolute;left:50%;margin-left:-455.5px;top:8.5%;} 
 
         .wb_cts01 {background:url(https://static.willbes.net/public/images/promotion/2021/07/1297_01_bg.jpg) no-repeat center top;}
 
@@ -27,17 +28,19 @@
 
     <div class="p_re evtContent NGR" id="evtContainer">
 
-        <div class="evtCtnsBox wb_top" >            
-            <img src="https://static.willbes.net/public/images/promotion/2021/07/1297_top.jpg" alt="새벽실전 모의고사" />            
+        <div class="evtCtnsBox wb_top" >        
+            <div class="wb_tops" data-aos="fade-down">
+                <img src="https://static.willbes.net/public/images/promotion/2022/08/1297_top.png" alt="새벽실전 모의고사"/>
+            </div>              
         </div>
 
-        <div class="evtCtnsBox wb_cts01">
+        <div class="evtCtnsBox wb_cts01" data-aos="fade-up">
             <div class="wrap">
-                <img src="https://static.willbes.net/public/images/promotion/2021/07/1297_01.jpg" alt="집중과 몰입의 시간" />
-                <a href="https://pass.willbes.net/professor/show/cate/3019/prof-idx/50499/?subject_idx=1108&subject_name=%EC%98%81%EC%96%B4&tab=open_lecture&search_order=regist&series=&campus_ccd=605001&search_text=UHJvZE5hbWU67IOI67K9" title="신청하기" target="_blank" style="position: absolute;left: 29.09%;top: 72.59%;width: 41.93%;height: 6%;z-index: 2;"></a>
+                <img src="https://static.willbes.net/public/images/promotion/2022/08/1297_01.jpg" alt="집중과 몰입의 시간" />
+                <a href="https://pass.willbes.net/pass/offLecture/show/cate/3043/prod-code/200664" title="신청하기" target="_blank" style="position: absolute;left: 29.09%;top: 72.59%;width: 41.93%;height: 6%;z-index: 2;"></a>
             </div>    
         </div> 
-
+        {{--
         <div class="evtCtnsBox wb_cts02">       
             <div class="wrap">     
                 <img src="https://static.willbes.net/public/images/promotion/2021/07/1297_02.jpg" alt="100% 온라인 강의"/>
@@ -45,7 +48,16 @@
                 <a href="https://pass.willbes.net/professor/show/cate/3019/prof-idx/50619?subject_idx=1109&subject_name=%ED%95%9C%EA%B5%AD%EC%82%AC&tab=open_lecture&search_order=regist&series=&campus_ccd=605001&search_text=UHJvZE5hbWU67IOI67K9" title="신청하기" target="_blank" style="position: absolute;left: 58.89%;top: 70.19%;width: 27.93%;height: 8%;z-index: 2;"></a>
             </div>    
         </div>
-
+        --}}
     </div>
     <!-- End Container -->
+    
+    <link href="/public/js/willbes/dist/aos.css" rel="stylesheet">
+    <script src="/public/js/willbes/dist/aos.js"></script>
+    <script>
+        $(document).ready( function() {
+        AOS.init();
+        });
+    </script>
+
 @stop

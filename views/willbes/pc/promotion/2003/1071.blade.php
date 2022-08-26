b99fbabc0ef76067a794faf0be8c93d6e66d7d9c
김창용 <winmir@willbes.com>
2022-08-26 16:50:41 +0900

    공무원 추석맞이 이벤트

--- views/willbes/pc/promotion/2003/1071.blade.php
+++ views/willbes/pc/promotion/2003/1071.blade.php
@@ -954,12 +954,12 @@
     <!-- End Container -->
 
 
-    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <link href="/public/js/willbes/dist/aos.css" rel="stylesheet">
+    <script src="/public/js/willbes/dist/aos.js"></script>
     <script>
-      $( document ).ready( function() {
+      $(document).ready( function() {
         AOS.init();
-      } );
+      });
     </script>
 
     <script type="text/javascript">

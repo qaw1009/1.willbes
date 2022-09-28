f446510dc70299146ffc36c676b8dc4d4fd061f2
김창용 <winmir@willbes.com>
2022-09-28 17:38:00 +0900

    공무원 10월 윌비스 드림팩

--- views/willbes/pc/promotion/2003/2225.blade.php
+++ views/willbes/pc/promotion/2003/2225.blade.php
@@ -21,26 +21,11 @@
 
         /************************************************************/
 
-        .wb_top08 {background:url(https://static.willbes.net/public/images/promotion/2022/08/2225_top_bg.jpg) no-repeat center top; height: 1222px;}
-        .wb_top08 .imgA {position: absolute; top:350px; left:50%; margin-left:-211px; z-index: 2;}
-
         .wb_top09 {background:url(https://static.willbes.net/public/images/promotion/2022/09/2225_top_bg.jpg) no-repeat center top; height: 1200px;}
         .wb_top09 .imgA {position: absolute; top:175px; left:50%; margin-left:-470px; z-index: 2;}
 
-        /*
-        .wb_top2 {background:url(https://static.willbes.net/public/images/promotion/2022/07/2225_top_bg.jpg) no-repeat center top; height: 1154px; overflow: hidden;}
-        .wb_top2 span {position: absolute; z-index: 2; -webkit-animation:swing 3s linear infinite; animation:swing 3s linear infinite}
-        @@keyframes swing{
-            0%{-webkit-transform:rotate3d(0,0,1,0deg);transform:rotate3d(0,0,1,0deg)}
-            25%{-webkit-transform:rotate3d(0,0,1,5deg);transform:rotate3d(0,0,1,5deg)}
-            50%{-webkit-transform:rotate3d(0,0,1,0deg);transform:rotate3d(0,0,1,0deg)}
-            75%{-webkit-transform:rotate3d(0,0,1,-5deg);transform:rotate3d(0,0,1,-5deg)}
-            100%{-webkit-transform:rotate3d(0,0,1,0deg);transform:rotate3d(0,0,1,0deg)}
-        }
-        .wb_top2 span.imgA {left:-50px; top:-80px}
-        .wb_top2 span.imgB {left:50%; margin-left:350px; bottom:-80px}              
-        .topimg {position: absolute; top:930px; left:50%; margin-left:-634px; z-index: 10;}
-        */
+        .wb_top10 {background:url(https://static.willbes.net/public/images/promotion/2022/10/2225_top_bg.jpg) no-repeat center top; height: 1222px;}
+        .wb_top10 .imgA {position: absolute; top:250px; left:50%; margin-left:-372px; z-index: 2;}
 
         .wb_cts03 {background:#303132}
 
@@ -48,34 +33,37 @@
 
     <div class="evtContent NSK" id="evtContainer"> 
 
-        {{--8월--}}
-        @if(time() < strtotime('202208312400'))
-        <!--
-        <div class="evtCtnsBox wb_top2" data-aos="fade-up">
-            <span class="imgA"><img src="https://static.willbes.net/public/images/promotion/2022/07/2225_img_01.png" alt=""/></span>
-            <span class="imgB"><img src="https://static.willbes.net/public/images/promotion/2022/07/2225_img_02.png" alt=""/></span>
-        </div>       
-        <div class="topimg" data-aos="flip-up" data-aos-duration="1000"><img src="https://static.willbes.net/public/images/promotion/2022/07/2225_img_03.png" alt="공무원 웰컴팩"/></div>
-         -->
-         <div class="evtCtnsBox wb_top08">
-           <span class="imgA" data-aos="flip-up" data-aos-duration="1000"><img src="https://static.willbes.net/public/images/promotion/2022/08/2225_top_img.png" alt="윌비스 웰컴팩"/></span>
+        {{--9월--}}
+        @if(time() < strtotime('202209302400'))
+         <div class="evtCtnsBox wb_top09">
+           <span class="imgA" data-aos="flip-up" data-aos-duration="1000"><img src="https://static.willbes.net/public/images/promotion/2022/09/2225_top_img.png" alt="윌비스 웰컴팩"/></span>
         </div>
-        @else
-        {{--9월--}}            
-        <div class="evtCtnsBox wb_top09">
-           <span class="imgA" data-aos="flip-up" data-aos-duration="1000"><img src="https://static.willbes.net/public/images/promotion/2022/09/2225_top_img.png" alt="윌비스 드림팩"/></span>
-        </div>
-        @endif   
 
         <div class="evtCtnsBox wb_cts01" data-aos="fade-up">
             <div class="wrap">
-                <img src="https://static.willbes.net/public/images/promotion/2022/09/2225_member.jpg" alt="윌비스 회원가입"/>
+                <img src="https://static.willbes.net/public/images/promotion/2022/09/2225_01.jpg" alt="윌비스 회원가입"/>
                 <a href="https://www.willbes.net/member/join/?ismobile=0&sitecode=2003" target="_blank" title="회원가입하기" style="position: absolute;left: 28.77%;top: 25.12%;width: 42.27%;height: 15.45%;z-index: 2;"></a>
             </div>
         </div>
         <div class="evtCtnsBox wb_cts02" data-aos="fade-up">
-            <img src="https://static.willbes.net/public/images/promotion/2022/09/2225_dream_gift.jpg" alt="드림 기프트"/>
+            <img src="https://static.willbes.net/public/images/promotion/2022/09/2225_02.jpg" alt="드림 기프트"/>
         </div>
+        @else
+        {{--10월--}}            
+        <div class="evtCtnsBox wb_top10">
+           <span class="imgA" data-aos="flip-up" data-aos-duration="1000"><img src="https://static.willbes.net/public/images/promotion/2022/10/2225_top_img.png" alt="윌비스 드림팩"/></span>
+        </div>          
+
+        <div class="evtCtnsBox wb_cts01" data-aos="fade-up">
+            <div class="wrap">
+                <img src="https://static.willbes.net/public/images/promotion/2022/10/2225_01.jpg" alt="윌비스 회원가입"/>
+                <a href="https://www.willbes.net/member/join/?ismobile=0&sitecode=2003" target="_blank" title="회원가입하기" style="position: absolute;left: 28.77%;top: 25.12%;width: 42.27%;height: 15.45%;z-index: 2;"></a>
+            </div>
+        </div>
+        <div class="evtCtnsBox wb_cts02" data-aos="fade-up">
+            <img src="https://static.willbes.net/public/images/promotion/2022/10/2225_02.jpg" alt="드림 기프트"/>
+        </div>
+        @endif 
 
         <div class="evtCtnsBox wb_cts03" data-aos="fade-up">
             <div class="wrap">

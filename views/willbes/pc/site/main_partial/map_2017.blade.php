e496e7b9250acb33cc175857781721fda6108173
최현탁 <cht@willbes.com>
2022-09-30 13:54:58 +0900

    임용 메인 페이지 지도영역 수정

--- views/willbes/pc/site/main_partial/map_2017.blade.php
+++ views/willbes/pc/site/main_partial/map_2017.blade.php
@@ -4,8 +4,8 @@
             윌비스 <span class="tx-color">교원임용고시 </span> 학원
         </div>
 
-        <div class="mapL" id="map">지도영역</div>
-        <div class="mapL" id="alterMap1" style="display: none;"><img src="https://static.willbes.net/public/images/promotion/main/2018/2017_map.jpg"></div>
+        <div class="mapL" id="map" style="display: none;">지도영역</div>
+        <div class="mapL" id="alterMap1" style="display: block;"><img src="https://static.willbes.net/public/images/promotion/main/2018/2017_map.jpg"></div>
         <div class="mapR">
             <div>
                 <p>윌비스 <span class="tx-color">교원임용고시</span>학원 오시는 길</p>
@@ -61,7 +61,8 @@
 <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey={{ config_item('kakao_js_app_key') }}&libraries=services"></script>
 <script type="text/javascript" src="/public/js/map_util.js?ver={{time()}}"></script>
 <script type="text/javascript">
-    $(document).ready(function() {
+    //카카오맵 주석처리
+    /*$(document).ready(function() {
         var info_txt = '<div style="padding:5px; 5px; background:#fff; border: 1px solid midnightblue"><strong class="tx-color">교원임용고시학원</strong> (남강타워 5층)</div>';
         var $kakaomap = new kakaoMap();
         $kakaomap.config.ele_id = 'map';
@@ -72,5 +73,5 @@
         $kakaomap.config.info_txt_x_anchor = 0.5;
         $kakaomap.config.info_txt_y_anchor = 2.7;
         $kakaomap.run();
-    });
+    });*/
 </script>

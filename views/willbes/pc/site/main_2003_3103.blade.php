913f65dea7166db9a217ba1e16706ac59d04b1d0
김창용 <winmir@willbes.com>
2022-10-04 17:23:18 +0900

    7급 psat PC 메인 수정

--- views/willbes/pc/site/main_2003_3103.blade.php
+++ views/willbes/pc/site/main_2003_3103.blade.php
@@ -2,6 +2,52 @@
 
 @section('content')
     <link href="/public/css/willbes/style_gosi_3103.css?ver={{time()}}" rel="stylesheet">
+    <style type="text/css">            
+        .evtContent {
+            width:100% !important;
+            min-width:1120px !important;
+            margin-top:20px !important;
+            padding:0 !important;
+            background:#fff;
+        }
+        .evtContent span {vertical-align:auto}
+        .evtCtnsBox {width:100%; text-align:center; min-width:1120px;}        
+        .evtCtnsBox .wrap {width:1120px; margin:0 auto; position:relative}
+        
+        /************************************************************/     
+
+        .evtTop {background:#1D318A;}
+
+        .evt01 {background:#222327;}
+        .evt01 {background:url(https://static.willbes.net/public/images/promotion/2022/10/2790_01_bg.jpg) no-repeat center top; height: 1137px;}        
+        .evt01 .imgA {position: absolute; top:1445px; left:50%; margin-left:-460px; z-index: 2;}
+        .evt01 .imgB {position: absolute; top:1540px; left:50%; margin-left:-390px; z-index: 2;}
+
+        .deadline {            
+            animation:upDown 1s infinite;
+            -webkit-animation:upDown 1s infinite;
+            z-index:10;
+        }
+        @@keyframes upDown{
+            from{margin-top:0}
+            60%{margin-top:-10px}
+            to{margin-top:0}
+        }
+        @@-webkit-keyframes upDown{
+            from{margin-top:0}
+            60%{margin-top:-10px}
+            to{margin-top:0}
+        }
+
+        .evt02 {background:#222327;}
+
+        .evt03 {background:url(https://static.willbes.net/public/images/promotion/2022/10/2790_03_bg.jpg) no-repeat center top;}
+
+        .evt05 {background:#D4F0F1;}
+
+        .evt06 {background:#1D318A;}
+
+    </style>
 
     <div id="Container" class="Container gosi NGR c_both">
         <!-- site nav -->
@@ -13,6 +59,7 @@
             </div>
         </div>
 
+        <!--
         <div class="Section gosi-Sec NSK">
             <div class="gosi-bntop">
                 <div id="TechRollingSlider" class="GositabBox">
@@ -126,6 +173,45 @@
                 <p class="rightBtn"><a id="imgBannerRight4"><img src="https://static.willbes.net/public/images/promotion/main/2003/3103_right.png"></a></p>
             </div>
         </div>
+-->
+
+
+        <div class="evtContent NSK" id="evtContainer">         
+
+            <div class="evtCtnsBox evtTop" data-aos="fade-down">
+                <img src="https://static.willbes.net/public/images/promotion/2022/10/2790_top.jpg" title="5일일의 투자">
+            </div>
+
+            <div class="evtCtnsBox evt01">
+                <span class="imgA deadline" data-aos="flip-down" data-aos-duration="1000"><img src="https://static.willbes.net/public/images/promotion/2022/10/2790_img01.png" alt=""/></span>
+                <span class="imgB deadline" data-aos="flip-down" data-aos-duration="1000"><img src="https://static.willbes.net/public/images/promotion/2022/10/2790_img02.png" alt=""/></span>
+            </div>
+
+            <div class="evtCtnsBox evt02" data-aos="fade-up">
+                <img src="https://static.willbes.net/public/images/promotion/2022/10/2790_02.jpg" title="빅4">
+            </div>
+
+            <div class="evtCtnsBox evt03" data-aos="fade-up">
+                <img src="https://static.willbes.net/public/images/promotion/2022/10/2790_03.jpg" title="석치수 자료해석">
+            </div>
+
+            <div class="evtCtnsBox evt04" data-aos="fade-up">
+                <img src="https://static.willbes.net/public/images/promotion/2022/10/2790_04.jpg" title="강의정보">
+            </div>
+
+            <div class="evtCtnsBox evt05" data-aos="fade-up">
+                <div>
+                    <img src="https://static.willbes.net/public/images/promotion/2022/10/2790_05.jpg" title="welcome event">
+                    <a href="https://pass.willbes.net/pass/offLecture/show/cate/3143/prod-code/201562" target="_blank" title="바로가기" style="position: absolute;left: 32.63%;top: 72.95%;width: 34.58%;height: 10.06%;z-index: 2;"></a>
+                </div>
+            </div>
+
+            <div class="evtCtnsBox evt06" data-aos="fade-up">
+                <img src="https://static.willbes.net/public/images/promotion/2022/10/2790_06.jpg" title="psat so easy">
+            </div>
+
+        </div>
+        <!-- End Container -->
 
         <div class="Section mt80">
             <div class="widthAuto">
@@ -192,12 +278,13 @@
                         </div>
                     </div>
                 </div>
-                <!--willbesNews //-->
             </div>
         </div>
+        
 
         {{--학원 오시는 길--}}
         @include('willbes.pc.site.main_partial.map_2003')
+        
 
         <div class="Section mt70 mb90 NSK">
             <div class="widthAuto">
@@ -340,4 +427,12 @@
         });
     </script>
     {!! popup('657001', $__cfg['SiteCode'], $__cfg['CateCode']) !!}
+
+    <link href="/public/js/willbes/dist/aos.css" rel="stylesheet">
+    <script src="/public/js/willbes/dist/aos.js"></script>
+    <script>
+        $(document).ready( function() {
+        AOS.init();
+        });
+    </script>
 @stop

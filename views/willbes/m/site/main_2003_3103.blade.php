c48b30f5da888a89cad92ebd2f35c9b734496691
김창용 <winmir@willbes.com>
2022-10-05 09:28:34 +0900

    모바일 7급 past 메인 수정

--- views/willbes/m/site/main_2003_3103.blade.php
+++ views/willbes/m/site/main_2003_3103.blade.php
@@ -1,8 +1,18 @@
 @extends('willbes.m.layouts.master')
 
 @section('content')
+<style type="text/css">
+    .Container {font-size:62.5%;}
+    .evtCtnsBox {width:100%; max-width:720px; margin:0 auto; text-align:center; position:relative; font-size: calc(1.4rem + (((100vw - 1.4rem) / (90 - 20))) * (2.0 - 1.4)); line-height:1.5; clear:both}
+    .evtCtnsBox img {max-width:100%;}
+    /*.evtCtnsBox a {border:1px solid #000}*/ 
+    }    
+</style>
+
     <div id="Container" class="Container NSK gosi mb40">
         {!! banner('M_메인_빅배너', 'MainSlider mt20', $__cfg['SiteCode'], $__cfg['CateCode']) !!}
+
+        <!--
         {!! banner('M_메인_서브', 'MainSlider mt20', $__cfg['SiteCode'], $__cfg['CateCode']) !!}
 
         <div class="gosiTitle NSK">
@@ -22,6 +32,28 @@
         </div>
         <div class="gosiTip">
             {!! banner('M_메인_cast', 'swiper-container-tip', $__cfg['SiteCode'], $__cfg['CateCode'], 'bnTit') !!}
+        </div>
+        -->
+
+        <div id="Container" class="Container NSK c_both">
+            <div class="evtCtnsBox" data-aos="fade-down">
+                <img src="https://static.willbes.net/public/images/promotion/2022/10/2790m_01.jpg" alt="선착순" >
+            </div>
+
+            <div class="evtCtnsBox" data-aos="fade-up">
+                <img src="https://static.willbes.net/public/images/promotion/2022/10/2790m_02.jpg" alt="빅4" >
+            </div> 
+
+            <div class="evtCtnsBox" data-aos="fade-up">
+                <img src="https://static.willbes.net/public/images/promotion/2022/10/2790m_04.jpg" alt="강의 정보" >
+            </div> 
+
+            <div class="evtCtnsBox" data-aos="fade-up">
+                <div class="wrap">
+                    <img src="https://static.willbes.net/public/images/promotion/2022/10/2790m_05.jpg" alt="수강신청 바로가기" >
+                    <a href="https://pass.willbes.net/m/pass/offLecture/show/cate/3143/prod-code/201562" title="바로가기" target="_blank" style="position: absolute;left: 14.92%;top: 79.12%;width: 70.39%;height: 9.28%;z-index: 2;"></a>
+                </div>
+            </div>  
         </div>
 
         {{-- 게시판 --}}
@@ -81,6 +113,14 @@
             });
 
         });
+    </script>
+
+    <link href="/public/js/willbes/dist/aos.css" rel="stylesheet">
+    <script src="/public/js/willbes/dist/aos.js"></script>
+    <script>
+        $(document).ready( function() {
+            AOS.init();
+        });    
     </script>
     {!! popup('657007', $__cfg['SiteCode'], $__cfg['CateCode']) !!}
 @stop

9ad53043c4879cc1862470053bfdb1ce617cccb6
이윤구 <yoongukk@willbes.com>
2022-11-18 14:15:39 +0900

    (경찰) 12월5일 김재규경찰학 100일완성반 pc/모바일 페이지 신규

---
+++ views/willbes/m/promotion/2002/2826.blade.php
@@ -0,0 +1,147 @@
+@extends('willbes.m.layouts.master')
+
+@section('content')
+<!-- Container -->
+
+<style type="text/css">
+
+    .evtCtnsBox {width:100%; max-width:720px; margin:0 auto; text-align:center; position:relative; font-size:1.4vh; line-height:1.4; clear:both}
+    .evtCtnsBox img {width:100%; max-width:720px;}
+    .evtCtnsBox .wrap {position:relative}
+    /*.evtCtnsBox a {border:1px solid #000}*/
+   
+    .evt01 .youtube {position:relative; padding-top:30px; padding-bottom:56.25%; margin:0; height:0;background: #000;} 
+    .evt01 .youtube iframe {position:absolute; top:0; left:50%; width:100%; margin-left:-50%; height:100%; z-index: 2;}
+
+    .evt05 {background:#F5F5F5; padding-bottom:8vh}
+    .slide_con {padding:0 30px 30px}
+    .slide_con .bx-wrapper {box-shadow:none; border:0; margin:0; padding:0}
+    .slide_con .bx-wrapper .bx-pager {
+        width: auto;
+        bottom: -30px;
+        left:0;
+        right:0;
+        text-align: center;
+        z-index:90;
+    }
+    .slide_con .bx-wrapper .bx-pager.bx-default-pager a {
+        background: #ccc;
+        width: 14px;
+        height: 14px;
+        margin: 0 4px;
+        border-radius:10px;
+    }
+    .slide_con .bx-wrapper .bx-pager.bx-default-pager a:hover,
+    .slide_con .bx-wrapper .bx-pager.bx-default-pager a.active,
+    .slide_con .bx-wrapper .bx-pager.bx-default-pager a:focus {
+        background: #2D57A3;
+    }
+    .slide_con .bx-wrapper .bx-pager.bx-default-pager a.active {
+        width: 30px;
+    }    
+
+    /* 폰 가로, 태블릿 세로*/
+    @@media only screen and (max-width: 374px)  {   
+
+    }
+
+    /* 태블릿 세로 */
+    @@media only screen and (min-width: 375px) and (max-width: 640px) { 
+        
+    }
+    
+    /* 태블릿 가로, PC */
+    @@media only screen and (min-width: 641px) {
+
+    }
+    
+</style>
+
+<div id="Container" class="Container NSK c_both">
+
+    <div class="evtCtnsBox evt_top" data-aos="fade-down">
+        <img src="https://static.willbes.net/public/images/promotion/2022/11/2826m_top.jpg" alt="100일 완성반" >        
+    </div> 
+
+    <div class="evtCtnsBox evt01" data-aos="fade-up">
+        <img src="https://static.willbes.net/public/images/promotion/2022/11/2826m_01.jpg" alt="고득점 형사법"/>
+        <div class="youtube">
+            <iframe src="https://www.youtube.com/embed/ojZcGpQXrcs?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <img src="https://static.willbes.net/public/images/promotion/2022/11/2826m_01_01.jpg" alt=""/>
+    </div>
+
+    <div class="evtCtnsBox" data-aos="fade-up">
+        <img src="https://static.willbes.net/public/images/promotion/2022/11/2826m_02.jpg"  alt="합격률"/>               
+    </div>
+
+    <div class="evtCtnsBox" data-aos="fade-up">
+        <img src="https://static.willbes.net/public/images/promotion/2022/11/2826m_03.jpg"  alt="커리큘럼"/>               
+    </div>
+
+    <div class="evtCtnsBox" data-aos="fade-up">        
+        <img src="https://static.willbes.net/public/images/promotion/2022/11/2826m_04.jpg"  alt="경찰학 교재"/>                
+    </div>
+
+    <div class="evtCtnsBox evt05" data-aos="fade-up">
+        <img src="https://static.willbes.net/public/images/promotion/2022/11/2826m_05.jpg" alt="리얼 수강후기" >
+        <div class="slide_con">
+            <ul id="slidesImg2">
+                <li><img src="https://static.willbes.net/public/images/promotion/2022/11/2825m_comment01.png" alt="" /></li>
+                <li><img src="https://static.willbes.net/public/images/promotion/2022/11/2825m_comment02.png" alt="" /></li>
+                <li><img src="https://static.willbes.net/public/images/promotion/2022/11/2825m_comment03.png" alt="" /></li>
+                <li><img src="https://static.willbes.net/public/images/promotion/2022/11/2825m_comment04.png" alt="" /></li>
+                <li><img src="https://static.willbes.net/public/images/promotion/2022/11/2825m_comment05.png" alt="" /></li>
+                <li><img src="https://static.willbes.net/public/images/promotion/2022/11/2825m_comment06.png" alt="" /></li>
+                <li><img src="https://static.willbes.net/public/images/promotion/2022/11/2825m_comment07.png" alt="" /></li>                
+                <li><img src="https://static.willbes.net/public/images/promotion/2022/11/2825m_comment08.png" alt="" /></li>               
+            </ul>
+        </div>
+    </div>
+
+    <div class="evtCtnsBox">
+        <img src="https://static.willbes.net/public/images/promotion/2022/11/2826m_06.jpg"  alt="그레이스 호퍼 명언"/>               
+    </div>
+
+    <div class="evtCtnsBox">
+        <div class="wrap">
+            <img src="https://static.willbes.net/public/images/promotion/2022/11/2826m_07.jpg"  alt="후회없는 선택"/>
+            <a href="https://police.willbes.net/m/pass/offPackage/index/type/all?cate_code=3010&campus_ccd=605001&course_idx=1040" target="_blank" title="신청하기" style="position: absolute;left: 25.47%;top: 87.65%;width: 49.55%;height: 5.57%;z-index: 2;"></a>
+        </div>      
+    </div>
+
+</div>
+
+<!-- End Container -->
+
+<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+      $( document ).ready( function() {
+        AOS.init();
+      } );
+</script>
+
+<link rel="stylesheet" href="/public/vendor/jquery/bxslider/jquery.bxslider.min.css">
+<script src="/public/vendor/jquery/bxslider/jquery.bxslider.js"></script>
+<script type="text/javascript">
+
+    /*슬라이드*/
+    $(document).ready(function() {
+        var slidesImg1 = $("#slidesImg2").bxSlider({
+            auto: true,
+            speed: 500,
+            pause: 4000,
+            mode:'horizontal',
+            autoControls: false,
+            controls:false,
+            pager:true,
+        });
+    });   
+
+</script>
+
+{{-- 프로모션용 스크립트 include --}}
+@include('willbes.pc.promotion.promotion_script')
+
+@stop
---
+++ views/willbes/pc/promotion/2002/2826.blade.php
@@ -0,0 +1,204 @@
+@extends('willbes.pc.layouts.master')
+
+@section('content')
+    @include('willbes.pc.layouts.partial.site_menu')
+    <!-- Container -->
+    <style type="text/css">
+        .evtContent {
+            width:100% !important;
+            min-width:1120px !important;
+            max-width:2000px !important;
+            margin:0 auto;
+            margin-top:20px !important;
+            padding:0 !important;
+            background:#fff;            
+        }
+        .evtContent span {vertical-align:top}
+        .evtCtnsBox {width:100%; text-align:center; min-width:1120px; position:relative}
+        .evtCtnsBox .wrap {width:1120px; margin:0 auto; position:relative}
+        /*.evtCtnsBox .wrap a {border:1px solid #000}*/
+
+
+        /************************************************************/
+
+        .sky {position:fixed;top:200px;right:10px;z-index:100;}
+        .sky a {display:block; margin-bottom:10px}
+
+        .wb_top {background:url(https://static.willbes.net/public/images/promotion/2022/11/2826_top_bg.jpg) no-repeat center;}
+
+        .evt01 {background:#2D57A3}
+
+        .evt02 {background:#37383A;position:relative;}
+        .youtube {position:absolute; top:582px; left:50%;z-index:1;margin-left:-430px}
+        .youtube iframe {width:860px; height:484px}
+
+        .evt03 {background:#F5F5F5}
+
+        .evt05 {background:#F5F5F5;padding-bottom:250px;}
+        .evt05 .infoText {margin-top:50px; color:#fff; font-size:30px}        
+        /*교수 롤링*/
+        .section_pro {
+        background:url(https://static.willbes.net/public/images/promotion/2022/11/2826_05_bg.jpg) no-repeat center top; 
+                   position:relative;height:1000px;clear:both;}      
+        .section_pro .box_pro {position:absolute; top:700px; left:0; width:100%; z-index:1}
+        .section_pro .box_pro .bx-wrapper{max-width:100% !important;}
+        .section_pro .box_pro li {display:inline; float:left;height:492px;}
+        .section_pro .box_pro li img {
+        width: 100%;
+        height: 100%;        
+        }
+
+        .evt06 {background:#F5F5F5;}
+
+        .evt07 {padding-bottom:100px; width:1120px; margin:0 auto}
+        .evt07 .title {font-size:30px; font-weight:bold; margin-bottom:10px; text-align:left}        
+        .pass_apply {margin:0 auto;width:500px; height:80px; line-height:80px; left:50%; }
+        .pass_apply a {display:block; border-radius:50px; color:#fff; background:#001334; font-size:40px}
+        .pass_apply a:hover {box-shadow: 10px 10px 20px 1px rgba(0,0,0,0.2); color:#fff455}
+
+        /*레이어팝업*/
+        .Pstyle {
+            opacity: 0;
+            display: none;
+            position: relative;
+            width: auto;
+        }
+        .b-close {
+            right: 10px;
+            top: 50px;
+            padding: 5px;
+            display: inline-block;
+            cursor: pointer;
+        }
+        .Pstyle .content {height:auto; width:auto;}
+
+    </style>
+    
+    <div class="evtContent NSK" id="evtContainer">
+
+        <div class="sky" id="QuickMenu">
+            <a href="#lec"><img src="https://static.willbes.net/public/images/promotion/2022/11/2826_sky01.png" alt="경찰학 스페셜 이벤트"></a>
+            <a href="#lec"><img src="https://static.willbes.net/public/images/promotion/2022/11/2826_sky02.png" title="경찰학 완성반 신청하기"></a> 
+        </div>
+
+        <div class="evtCtnsBox wb_top" data-aos="fade-down">           
+            <img src="https://static.willbes.net/public/images/promotion/2022/11/2826_top.jpg"  alt="100일 완성반" />   
+		</div>
+
+        <div class="evtCtnsBox evt01" data-aos="fade-up">
+            <img src="https://static.willbes.net/public/images/promotion/2022/11/2826_01.jpg"  alt="오리지날 경찰학"/>    
+            <div class="youtube">
+                <iframe src="https://www.youtube.com/embed/ojZcGpQXrcs?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>         
+        </div>
+
+        <div class="evtCtnsBox evt02" data-aos="fade-up">
+            <img src="https://static.willbes.net/public/images/promotion/2022/11/2826_02.jpg"  alt="합격률"/>               
+        </div>
+
+        <div class="evtCtnsBox evt03" data-aos="fade-up">      
+            <img src="https://static.willbes.net/public/images/promotion/2022/11/2826_03.jpg"  alt="커리큘럼"/>    
+        </div>
+
+        <div class="evtCtnsBox evt04" data-aos="fade-up">
+            <div class="wrap">
+                <img src="https://static.willbes.net/public/images/promotion/2022/11/2826_04.jpg"  alt="경찰학 교재"/>
+                <a href="javascript:go_popup1()" title="핵심 서브노트" style="position: absolute;left: 40.66%;top: 45.25%;width: 14.69%;height: 2.89%;z-index: 2;"></a>
+                <a href="javascript:go_popup2()" title="21개년 총알기출 ox" style="position: absolute;left: 40.66%;top: 65.65%;width: 14.69%;height: 2.89%;z-index: 2;"></a>
+                <a href="javascript:go_popup3()" title="플러스 100제" style="position: absolute;left: 40.66%;top: 86.05%;width: 14.69%;height: 2.89%;z-index: 2;"></a>
+            </div>
+        </div>
+
+        <div class="evtCtnsBox evt05" data-aos="fade-up">
+            <div class="section_pro">                
+                <div class="box_pro">
+                    <ul class="slide_pro">
+                        <li><img src="https://static.willbes.net/public/images/promotion/2022/11/2825_comment01.png" alt="" /></li>
+                        <li><img src="https://static.willbes.net/public/images/promotion/2022/11/2825_comment02.png" alt="" /></li>
+                        <li><img src="https://static.willbes.net/public/images/promotion/2022/11/2825_comment03.png" alt="" /></li>
+                        <li><img src="https://static.willbes.net/public/images/promotion/2022/11/2825_comment04.png" alt="" /></li>
+                        <li><img src="https://static.willbes.net/public/images/promotion/2022/11/2825_comment05.png" alt="" /></li>
+                        <li><img src="https://static.willbes.net/public/images/promotion/2022/11/2825_comment06.png" alt="" /></li>
+                        <li><img src="https://static.willbes.net/public/images/promotion/2022/11/2825_comment07.png" alt="" /></li>
+                        <li><img src="https://static.willbes.net/public/images/promotion/2022/11/2825_comment08.png" alt="" /></li>                                                           
+                    </ul>
+                </div>  
+            </div>
+        </div>
+
+        <div class="evtCtnsBox evt06">      
+            <img src="https://static.willbes.net/public/images/promotion/2022/11/2826_06.jpg"  alt="그레이스 호퍼 명언"/>    
+        </div>
+
+        <div class="evtCtnsBox evt07" data-aos="fade-up" id="lec">                     
+            <img src="https://static.willbes.net/public/images/promotion/2022/11/2826_07.jpg"  alt="후회없는 선택"/>
+            <div class="pass_apply NSK-Black"><a href="https://police.willbes.net/pass/offPackage/index/type/all?cate_code=3010&campus_ccd=605001&course_idx=1040" target="_blank">신청하기 ></a></div> 
+        </div>
+
+        <!--레이어팝업-->
+        <div id="popup1" class="Pstyle">
+            <span class="b-close"></span>
+            <div class="content1">                  
+                <img src="https://static.willbes.net/public/images/promotion/2022/11/2825_textbook01.png" class="off" alt="" />    
+            </div> 
+        </div>    
+        <div id="popup2" class="Pstyle">
+            <span class="b-close"></span>   
+            <div class="content2">         
+                <img src="https://static.willbes.net/public/images/promotion/2022/11/2825_textbook02.png" class="off" alt="" />  
+            </div> 
+        </div>
+        <div id="popup3" class="Pstyle">    
+            <span class="b-close"></span>
+            <div class="content3">            
+                <img src="https://static.willbes.net/public/images/promotion/2022/11/2825_textbook03.png" class="off" alt="" />  
+            </div>
+        </div>
+
+    </div>
+
+    <!-- End Container -->
+    
+    <link href="/public/js/willbes/dist/aos.css" rel="stylesheet">
+    <script src="/public/js/willbes/dist/aos.js"></script>
+    <script type="text/javascript" src="/public/js/willbes/jquery.bpopup.min.js"></script>
+    <script>
+      $(document).ready( function() {
+        AOS.init();
+      });
+
+      /*롤링*/
+      $(document).ready(function() {
+            var BxBook = $('.slide_pro').bxSlider({
+                slideWidth: 533,
+                slideMargin: 40,
+                maxSlides:10,
+                minSlides:1,
+                moveSlides: 1,
+                ticker:true,
+                speed:40000,
+                onSlideAfter: function() {
+                    BxBook.stopAuto();
+                    BxBook.startAuto();
+                }
+            });
+        });
+
+        /*레이어팝업*/     
+
+        function go_popup1() {
+            $('#popup1').bPopup();
+        }   
+        function go_popup2() {
+            $('#popup2').bPopup();
+        }
+        function go_popup3() {
+            $('#popup3').bPopup();
+        }
+
+    </script>
+      
+    {{-- 프로모션용 스크립트 include --}}
+    @include('willbes.pc.promotion.promotion_script')
+
+@stop

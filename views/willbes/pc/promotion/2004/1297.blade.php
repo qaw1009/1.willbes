7c9831685f69049ec4f63b8a49a2b88ac5e3a270
김창용 <winmir@willbes.com>
2022-11-30 10:24:34 +0900

    공무원 새벽실전 모의고사 수정

--- views/willbes/pc/promotion/2004/1297.blade.php
+++ views/willbes/pc/promotion/2004/1297.blade.php
@@ -5,15 +5,19 @@
     <!-- Container -->
     <style type="text/css">
         .evtContent {
-            width:100% !important;
+            width:100%;
             min-width:1120px !important;
-            margin-top:20px !important;
+            max-width:2000px !important;
+            margin:20px auto 0;
             padding:0 !important;
-            background:#fff;
+            background:#fff;     
+            font-size:14px;       
+            line-height:1.3;
         }
-        .evtContent span {vertical-align:auto}
-        .evtCtnsBox {width:100%; text-align:center; min-width:1120px;}
+        .evtContent span {vertical-align:top}
+        .evtCtnsBox {width:100%; text-align:center; min-width:1120px; position:relative}
         .evtCtnsBox .wrap {width:1120px; margin:0 auto; position:relative}
+        /*.evtCtnsBox .wrap a {border:1px solid #000} */
                 
         /************************************************************/
 
@@ -40,15 +44,6 @@
                 <a href="https://pass.willbes.net/pass/offLecture/index?cate_code=3043&campus_ccd=605001" title="신청하기" target="_blank" style="position: absolute;left: 29.09%;top: 72.59%;width: 41.93%;height: 6%;z-index: 2;"></a>
             </div> 
         </div> 
-        {{--
-        <div class="evtCtnsBox wb_cts02">       
-            <div class="wrap">     
-                <img src="https://static.willbes.net/public/images/promotion/2021/07/1297_02.jpg" alt="100% 온라인 강의"/>
-                <a href="https://pass.willbes.net/professor/show/cate/3019/prof-idx/50157/?subject_idx=1107&subject_name=%EA%B5%AD%EC%96%B4&tab=open_lecture&search_order=regist&series=&campus_ccd=605001&search_text=UHJvZE5hbWU67IOI67K9" title="신청하기" target="_blank" style="position: absolute;left: 13.89%;top: 70.19%;width: 27.93%;height: 8%;z-index: 2;"></a>
-                <a href="https://pass.willbes.net/professor/show/cate/3019/prof-idx/50619?subject_idx=1109&subject_name=%ED%95%9C%EA%B5%AD%EC%82%AC&tab=open_lecture&search_order=regist&series=&campus_ccd=605001&search_text=UHJvZE5hbWU67IOI67K9" title="신청하기" target="_blank" style="position: absolute;left: 58.89%;top: 70.19%;width: 27.93%;height: 8%;z-index: 2;"></a>
-            </div>    
-        </div>
-        --}}
     </div>
     <!-- End Container -->
     

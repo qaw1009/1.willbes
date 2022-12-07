0511452de07a540f95a9675b1211e49762dbae35
김창용 <winmir@willbes.com>
2022-12-07 17:28:22 +0900

    공무원 법원직 3~5순환 패스 온라인

---
+++ views/willbes/m/promotion/2003/2847.blade.php
@@ -0,0 +1,172 @@
+@extends('willbes.m.layouts.master')
+
+@section('content')
+<!-- Container -->
+
+<style type="text/css">
+
+    .evtCtnsBox {width:100%; max-width:720px; margin:0 auto; text-align:center; position:relative; line-height:1.5; clear:both}
+    .evtCtnsBox img {max-width:100%;}
+    .evtCtnsBox.wrap {position:relative}
+    /*.evtCtnsBox.wrap a {border:1px solid #000}*/
+
+    /* 이용안내 */
+    .wb_info {padding:50px 20px; color:#fff; line-height:1.4; background:#333}
+    .guide_box{text-align:left; word-break:keep-all}
+    .guide_box h2 {font-size:3vh; margin-bottom:30px;}
+    .guide_box dt{margin-bottom:10px; color:#fff; background:#000; display:inline-block; padding:5px 10px; font-weight:bold; margin-right:10px; font-size:1.8vh;}        
+    .guide_box dd{margin:0 0 20px 5px;}
+    .guide_box dd li{margin-bottom:3px; list-style:decimal; margin-left:20px;font-size:1.5vh;}
+    
+</style>
+
+<div id="Container" class="Container NSK c_both">
+    <div class="evtCtnsBox wrap" data-aos="fade-up">
+        <img src="https://static.willbes.net/public/images/promotion/2022/12/2847m_top.jpg" alt="법원직 3~5순환패스" >
+        <a href="https://pass.willbes.net/m/support/notice/show/cate/3035?board_idx=442661&s_cate_code=3035&s_keyword=%EC%8B%9C%EA%B0%84%ED%91%9C" title="" style="position: absolute; left: 84.17%; top: 29.46%; width: 15.14%; height: 12.88%; z-index: 2;"></a>
+    </div> 
+
+    <div class="evtCtnsBox " data-aos="fade-left">
+        <img src="https://static.willbes.net/public/images/promotion/2022/12/2847m_01.jpg" alt="합격생 배출 압도적 1위" />
+    </div> 
+
+    <div class="evtCtnsBox" data-aos="fade-right">
+        <img src="https://static.willbes.net/public/images/promotion/2022/12/2847m_02.jpg" alt="체크포인트" >
+    </div> 
+
+    <div class="evtCtnsBox" data-aos="fade-up">
+        <img src="https://static.willbes.net/public/images/promotion/2022/12/2847m_03.jpg" alt="김동진팀"/>
+    </div> 
+
+    <div class="evtCtnsBox wrap" data-aos="fade-up">
+        <img src="https://static.willbes.net/public/images/promotion/2022/12/2847m_04.jpg" alt="커리큘럼"/>
+        <a href="https://pass.willbes.net/m/promotion/index/cate/3035/code/2696" title="" style="position: absolute; left: 69.03%; top: 22.35%; width: 19.03%; height: 2.79%; z-index: 2;"></a>
+    </div>
+
+    <div class="evtCtnsBox wrap" data-aos="fade-up">
+        <img src="https://static.willbes.net/public/images/promotion/2022/12/2847m_05.jpg" alt="면접스터디 "/>
+    </div>
+
+    <div class="evtCtnsBox wrap" data-aos="fade-up">
+        <img src="https://static.willbes.net/public/images/promotion/2022/12/2847m_06.jpg" alt="후기"/>
+        <a href="https://cafe.daum.net/LAW-KDJTEAM/I7Bo" target="_blank" title="" style="position: absolute; left: 20.28%; top: 84.82%; width: 58.75%; height: 6.29%;z-index: 2;"></a>        
+    </div>
+
+    <div class="evtCtnsBox wrap" data-aos="fade-up">
+        <img src="https://static.willbes.net/public/images/promotion/2022/12/2847m_07.jpg" alt="수강신청"/>
+        <a href="https://pass.willbes.net/m/periodPackage/show/cate/3035/pack/648001/prod-code/203422" title="" style="position: absolute; left: 19.44%; top: 69.6%; width: 58.75%; height: 11.75%; z-index: 2;"></a>
+    </div>
+
+    <div class="evtCtnsBox wrap" data-aos="fade-up">
+        <img src="https://static.willbes.net/public/images/promotion/2022/12/2847m_08.jpg" alt="체험 팩 설문조사"/>
+        <a href="javascript:void(0);" onclick="showPopup();" title="설문조사" style="position: absolute; left: 19.44%; top: 66.13%; width: 58.75%; height: 11.73%;  z-index: 2;"></a>
+    </div>
+
+{{--
+    <div class="evtCtnsBox wb_info" id="info">
+        <div class="guide_box">
+            <h2 class="NSK-Black">2023대비 예비순환 온라인관리반 Light 이용안내</h2>
+            <dl>
+                <dt>상품구성</dt>
+                <dd>
+                    <ol>
+                        <li>온라인관리반 상품은 윌비스김동진법원팀의 예비순환 온라인 동영상 강의를 수강하고, 매주 월~토 기간동안
+                            오픈채팅을 통한 출석체크와 학습내역 확인을 통한 학습관리, 그리고 전화 또는 방문상담을 제공해드립니다.</li>
+                        <li>수강기간 내 본인이 신청한 과목의 예비순환 온라인 동영상 강의는 무제한 수강이 가능합니다.</li>
+                        <li>온라인관리반 등록 시 예비순환에 강의용 교재를 저자 구매 제공으로 무료 제공해드립니다.</li>
+                        <li>2023대비 김동진법원팀 전용 W플래너를 무료로 제공해드립니다.</li>
+                    </ol>
+                </dd>
+
+                <dt>수강기간</dt>
+                <dd>
+                    <ol>
+                        <li>2022년 7월 4일(월)부터 1순환 시작 전인 8월 7일(일)까지 무제한 수강하실 수 있습니다.</li>
+                    </ol>
+                </dd>
+
+                <dt>동영상 수강방법 및 제한</dt>
+                <dd>
+                    <ol>
+                        <li>① 홈페이지 [내강의실] 메뉴에서 [온라인강좌] > [수강중강좌] > [관리자부여강좌(복습동영상)]로 접속합니다.<br>
+                            ② 접속 후 구매하신 패키지 상품명 옆의 [강좌추가]를 선택합니다.<br>
+                            ③ 원하는 과목/교수/강좌를 선택하여 등록하신 후 수강이 가능합니다.</li>
+                        <li>수강 시 이용가능한 기기는 ｢PC 2대 or 모바일 2대 or PC&모바일 각 1대｣로 총 2대의 기기로 제한됩니다.</li>
+                        <li>PC/모바일 기기 변경 등 단말기 초기화가 필요한 경우, 계정당 최초 1회에 한해 직접 초기화가 가능하며, 추후 단말기 초기화는 고객센터에서 관련 내용 확인 후 최대 2회까지 추가 진행 가능합니다.</li>
+                    </ol>
+                </dd>
+                
+                <dt>교재 및 자료 관련</dt>
+                <dd>
+                    <ol>
+                        <li>모든 교재는 무료로 제공해드립니다(저자구매제공)</li>
+                        <li>강의자료는 각 강좌별 개별 강의에 첨부파일의 형식으로 제공됩니다. 필요한 자료는 개별 출력하여야 합니다.</li>
+                    </ol>
+                </dd>
+
+                <dt>환불 관련</dt>
+                <dd>
+                    <ol>
+                        <li>환불은 학원설립및과외교습에관한법률의 규정에 따라 진행된 강의에 해당하는 수강료를 공제합니다.</li>
+                        <li>1개월 강의료 기준으로 강의시작 후 1/3 진행 전에는 1/3을 공제하고, 1/3~1/2 진행 시에는 1/2을 공제, 1/2이상 진행 시 전액을 공제합니다.</li>
+                        <li>중도 환불 시 무료로 제공된 교재 및 플래너 구매비용이 추가로 공제될 수 있습니다.</li>
+                    </ol>
+                </dd>
+
+                <dt>유의사항</dt>
+                <dd>
+                    <ol>
+                        <li>학원 사정에 의해 일부 강사의 강의가 부득이하게 진행되지 않거나 교수가 변경될 수 있습니다.</li>
+                        <li>복습동영상 아이디 불법 공유 적발 시 회원자격 박탈 및 환불불가 조치되며, 민/형사상 책임을 질 수 있습니다.</li>
+                    </ol>
+                </dd>
+
+                <dt>※ 이용문의 : 김동진법원팀 노량진학원 1544-0330(2번 연결)</dt>
+            </dl>
+        </div>
+    </div>
+    --}}
+</div>
+
+<!-- End Container -->
+
+<link href="/public/js/willbes/dist/aos.css" rel="stylesheet">
+<script src="/public/js/willbes/dist/aos.js"></script>
+<script>
+      $( document ).ready( function() {
+        AOS.init();
+      } );
+
+      var $regi_form = $('#regi_form');
+
+      function showPopup(){
+          @if(empty($arr_promotion_params['SsIdx']) === true)
+            alert('설문정보가 없습니다.');
+            return;
+          @else
+            var url = "{{front_url('/eventSurvey/index/' . $arr_promotion_params['SsIdx'])}}";
+            window.open(url,'survey_event', 'top=100,scrollbars=yes,toolbar=no,resizable=yes,width=868,height=630');
+          @endif
+      }
+
+      {{--쿠폰발급--}}
+      function giveCheck() {
+          {!! login_check_inner_script('로그인 후 이용해주세요.','Y') !!}
+
+          @if(empty($arr_promotion_params) === false)
+              var _check_url = '{!! front_url('/promotion/promotionEventCheck/') !!}?give_type={{$arr_promotion_params["give_type"]}}&give_idx={{$arr_promotion_params["give_idx"]}}&event_code={{$data['ElIdx']}}&comment_chk_yn={{$arr_promotion_params["comment_chk_yn"]}}';
+              ajaxSubmit($regi_form, _check_url, function (ret) {
+                  if (ret.ret_cd) {
+                      alert('쿠폰이 발급되었습니다. \n\n내강의실에서 확인해 주세요.');
+                  }
+              }, showValidateError, null, false, 'alert');
+          @else
+            alert('프로모션 추가 파라미터가 지정되지 않았습니다.');
+          @endif
+      }
+    </script>
+
+{{-- 프로모션용 스크립트 include --}}
+@include('willbes.pc.promotion.promotion_script')
+
+@stop
--- views/willbes/pc/promotion/2003/2847.blade.php
+++ views/willbes/pc/promotion/2003/2847.blade.php
@@ -5,137 +5,209 @@
     <!-- Container -->
     <style type="text/css">
         .evtContent {
-            width:100% !important;
+            width:100%;
             min-width:1120px !important;
             max-width:2000px !important;
-            margin:0 auto;
-            margin-top:20px !important;
+            margin:20px auto 0;
             padding:0 !important;
-            background:#fff           
+            background:#fff;     
+            font-size:14px;       
         }
         .evtContent span {vertical-align:top}
-        .evtCtnsBox {width:100%; text-align:center; min-width:1120px; position:relative}
+        .evtCtnsBox {width:100%; text-align:center; min-width:1120px;position: relative;}
         .evtCtnsBox .wrap {width:1120px; margin:0 auto; position:relative}
-        /*.evtCtnsBox .wrap a {border:1px solid #fff}*/
+        /*.evtCtnsBox .wrap a {border:1px solid #000}*/
 
-        /************************************************************/        
+        /************************************************************/ 
 
-        .evt_top {background:#3fddde}	
-        .evtCtnsBox .title {color:#11153a; font-size:36px; margin:100px 0 50px;}
 
-        .evt_01 {background:#00375e}
-        .evt_02 {background:#f1f1f1}
-        .evt_03 {background:#3fddde}
+        .evtTop {background:url(https://static.willbes.net/public/images/promotion/2022/12/2847_top_bg.jpg) no-repeat center top;}
 
-        .evt_05 a {display:block; width:250px; margin:30px auto 0; padding:10px; text-align:center; background:#333; color:#fff; font-size:14px; border-radius:30px;}
-        .evt_05 p {font-size:40px; margin:100px auto}
+        .evt02 {background:#ede2dc}
+        .evt03 {background:url(https://static.willbes.net/public/images/promotion/2022/12/2847_03_bg.jpg) no-repeat center top;}
+        .evt05 {background:#dbdbdb}
+
+        .evt07 {background:#ffdede}
+        .evt08 {background:#ebebeb}
 
         /* 이용안내 */
-        .wb_info {padding:100px 0;background:#ededed}
-        .guide_box{width:1000px; margin:0 auto; text-align:left; word-break:keep-all; line-height:1.5; font-size:13px;}
-        .guide_box h2 {font-size:30px; margin-bottom:50px; color:#3a3a3a;}
-        .guide_box dt{margin-bottom:10px; display:inline-block; font-weight:bold; font-size:18px; border-radius:30px; color:#fff; background:#333;padding:5px 15px}        
-        .guide_box dd{color:#777; margin:0 0 40px 20px;}
-        .guide_box dd strong {color:#555}
-        .guide_box dd li {margin-bottom:3px; list-style:decimal; margin-left:20px;color:#3a3a3a;font-size:15px}
-        .guide_box dd span {color:red}
-        .guide_box dd li a {display:inline-block; margin-left:20px; background:#032E5B; color:#fff; padding:3px 10px; border-radius:15px;}
-        .guide_box .inquire{padding-top:25px;font-size:20px;font-weight:bold;color:#000;}
- 
+        .evtInfo {padding:100px 0; background:#555; color:#fff; line-height:1.5}
+        .guide_box{width:1120px; margin:0 auto; padding:0 50px; text-align:left; word-break:keep-all}
+        .guide_box h2 {font-size:30px; margin-bottom:30px;}
+        .guide_box dt{margin-bottom:10px; color:#fff; background:#333; display:inline-block; padding:5px 10px; font-weight:bold; margin-right:10px; font-size:16px;}        
+        .guide_box dd{margin-bottom:50px;}
+        .guide_box dd li{margin-bottom:5px; list-style:decimal; margin-left:20px;font-size:14px;}
+        .guide_box dd li.none {list-style:none; margin-left:0}
+        .guide_box dd:last-child {margin:0}
         /************************************************************/      
     </style> 
-	<div class="evtContent NSK">
-		<div class="evtCtnsBox evt_top" data-aos="fade-up">
-            <img src="https://static.willbes.net/public/images/promotion/2022/12/2848_top.jpg" alt="7급 PSAT 모의고사" />
-		</div> 
 
-        <div class="evtCtnsBox evt_01" data-aos="fade-up">
-            <img src="https://static.willbes.net/public/images/promotion/2022/12/2848_01.jpg" alt="모의고사 + 라이브 동영상" />
-		</div> 
+    <div class="evtContent NSK" id="evtContainer">
+		<div class="evtCtnsBox evtTop" data-aos="fade-up">
+            <div class="wrap">
+                <img src="https://static.willbes.net/public/images/promotion/2022/12/2847_top.jpg" alt="김동진 법원팀" />
+                <a href="https://pass.willbes.net/support/notice/show/cate/3035?board_idx=442661&s_cate_code=3035&s_keyword=%EC%8B%9C%EA%B0%84%ED%91%9C" target="_blank" title="시간표 확인" style="position: absolute; left: 83.93%; top: 29.16%; width: 14.38%; height: 12.29%; z-index: 2;"></a>
+            </div>
+		</div>
 
-        <div class="evtCtnsBox evt_02" data-aos="fade-up">
-            <img src="https://static.willbes.net/public/images/promotion/2022/12/2848_02.jpg" alt="수강특전" />
-		</div>        
+        <div class="evtCtnsBox evt01" data-aos="fade-left">
+            <img src="https://static.willbes.net/public/images/promotion/2022/12/2847_01.jpg" alt="김동진 법원팀" />
+		</div>
 
-        <div class="evtCtnsBox evt_03" data-aos="fade-up">  
-            <img src="https://static.willbes.net/public/images/promotion/2022/12/2848_03.jpg" alt="교수진" />
-        </div> 
-        
-        <div class="evtCtnsBox evt_04" data-aos="fade-up">
-            <img src="https://static.willbes.net/public/images/promotion/2022/12/2848_04.jpg" alt="마스터 클래스" />
-		</div>   
-        
-        @if(empty($arr_base['display_product_data']) === false)
-            @include('willbes.pc.promotion.display_product_partial',array('group_num'=>1))
-        @endif 
+        <div class="evtCtnsBox evt02" data-aos="fade-right">
+            <div class="wrap">
+                <img src="https://static.willbes.net/public/images/promotion/2022/12/2847_02.jpg" alt="절대 만족 후기"/>                
+            </div>
+		</div>
 
-        <div class="evtCtnsBox evt_05">
-            <a href="#tip">이용안내 확인하기 ▼</a>
-            <p class="NSK-Black">“바른 시작이 합격을 위한 가장 빠른 방법입니다.”</p>
-        </div>
+        <div class="evtCtnsBox evt03" data-aos="fade-left">
+            <img src="https://static.willbes.net/public/images/promotion/2022/12/2847_03.jpg" alt="김동진 법원팀" />
+		</div>
 
-        <div class="evtCtnsBox wb_info" id="tip" data-aos="fade-up">
+
+
+        <div class="evtCtnsBox evt04" data-aos="fade-right">
+            <div class="wrap">            
+                <img src="https://static.willbes.net/public/images/promotion/2022/12/2847_04.jpg" alt="연간 커리큘럼">
+                <a href="https://pass.willbes.net/lecture/index/cate/3035/pattern/only?search_order=regist&subject_idx=&search_text=UHJvZE5hbWU67JiI67mE7Iic7ZmY&course_idx=" title="예비순환" target="_blank" style="position: absolute; left: 0.98%; top: 76.24%; width: 15.71%; height: 9.08%; z-index: 2;"></a>
+                <a href="https://pass.willbes.net/lecture/index/cate/3035/pattern/only?search_order=regist&subject_idx=&search_text=UHJvZE5hbWU6MeyInO2ZmA%3D%3D" title="1순환" target="_blank" style="position: absolute; left: 17.41%; top: 76.24%; width: 15.71%; height: 9.08%; z-index: 2;"></a>
+                <a href="https://pass.willbes.net/lecture/index/cate/3035/pattern/only?search_order=regist&subject_idx=&search_text=UHJvZE5hbWU6MuyInO2ZmA%3D%3D" title="2순환" target="_blank" style="position: absolute; left: 33.75%; top: 76.24%; width: 15.71%; height: 9.08%; z-index: 2;"></a>
+                <a href="https://pass.willbes.net/lecture/index/cate/3035/pattern/only?search_order=regist&subject_idx=&search_text=UHJvZE5hbWU6M%2ByInO2ZmA%3D%3D" title="3순환" target="_blank" style="position: absolute; left: 50.36%; top: 76.24%; width: 15.71%; height: 9.08%; z-index: 2;"></a>
+                <a href="https://pass.willbes.net/lecture/index/cate/3035/pattern/only?search_order=regist&subject_idx=&search_text=UHJvZE5hbWU6NOyInO2ZmA%3D%3D" title="4순환" target="_blank" style="position: absolute; left: 66.79%; top: 76.24%; width: 15.71%; height: 9.08%;z-index: 2;"></a>
+                <a href="https://pass.willbes.net/lecture/index/cate/3035/pattern/only?search_order=regist&subject_idx=&search_text=UHJvZE5hbWU6NeyInO2ZmA%3D%3D" title="5순환" target="_blank" style="position: absolute; left: 83.04%; top: 76.24%; width: 15.71%; height: 9.08%; z-index: 2;"></a>
+                <a href="https://pass.willbes.net/promotion/index/cate/3035/code/2696" target="_blank" title="패스 자세히보기" style="position: absolute; left: 16.16%; top: 89.17%; width: 11.52%; height: 3.3%; z-index: 2;"></a>
+            </div>
+		</div>
+
+        <div class="evtCtnsBox evt05" data-aos="fade-up">
+            <img src="https://static.willbes.net/public/images/promotion/2022/12/2847_05.jpg" alt="동행 면접스터디" />
+		</div>
+
+        <div class="evtCtnsBox evt06" data-aos="fade-up">
+            <div class="wrap">
+                <img src="https://static.willbes.net/public/images/promotion/2022/12/2847_06.jpg" alt="설문조사" />
+                <a href="https://cafe.daum.net/LAW-KDJTEAM/I7Bo" target="_blank" title="더 많은 합격수기 보기" style="position: absolute; left: 24.73%; top: 79.85%; width: 50%; height: 9%; z-index: 2;"></a>
+            </div>
+		</div>
+
+        <div class="evtCtnsBox evt07" data-aos="fade-up">
+            <div class="wrap">
+                <img src="https://static.willbes.net/public/images/promotion/2022/12/2847_07.jpg" alt="수강신청" />
+                <a href="https://pass.willbes.net/periodPackage/show/cate/3035/pack/648001/prod-code/203422" target="_blank" style="position: absolute; left: 16.88%; top: 78.05%; width: 64.11%; height: 13.08%; z-index: 2;"></a>
+            </div>
+		</div>
+
+        <div class="evtCtnsBox evt08" data-aos="fade-up">
+            <div class="wrap">
+                <img src="https://static.willbes.net/public/images/promotion/2022/12/2847_08.jpg" alt="설문조사" />
+                <a href="javascript:void(0);" onclick="showPopup();" style="position: absolute; left: 16.88%; top: 77.05%; width: 64.11%; height: 13.08%; z-index: 2;"></a>
+            </div>
+		</div>
+{{--
+        <div class="evtCtnsBox evtInfo" id="info">
             <div class="guide_box">
-                <h2 class="NSK-Black">윌비스 한림법학원 교수님 온라인 T-PASS반 이용안내</h2>
+                <h2 class="NSK-Black">이벤트 유의사항</h2>
                 <dl>
                     <dt>상품구성</dt>
                     <dd>
                         <ol>
-                            <li>본 상품은 2023년 7급공무원 1차시험(PSAT)을 준비하시는 분을 위해 준비되었습니다.<br>
-                            [과목별 교수님]<br> 
-                            자료해석 : 석치수, 상황판단 : 박준범, 언어논리(택1) : 이나우/한승아</li>
-                            <li>수강기간 : 본 상품에 등록된 강의는<span>강의신청일부터 2023년 7급 1차 시험일까지 수강</span>하실 수 있습니다.<br>
-                            ※ 패키지 과정은 특별할인 적용 상품으로, <span>일시중지/연장/재수강 적용되지 않으며 수강시작일 지정 불가</span>합니다.</li>
-                            <li>강의배수  : 본 패키지 과정은 <span>3배수제한 적용 과정</span>입니다.</li>
-                            <li><span>기기대수제한  : pc 2 또는 pc 1 + 모바일 기기 1</span>(스마트폰 또는 테블릿)</li>
-                            <li>교재구매 : 수강중인 강의 > 강의보기 > 구매하지않은 교재 확인 후 결제 진행 하시면 됩니다.</li>
-                            <li>CORE 특강 무료제공 : 신정하신 교수님의 <span>CORE특강이 자동 등록</span>됩니다.(+ 해당 강사님 T-PASS 신청시 추가 강의 무료제공)</li>
-                            <li>진행(업로드) 강좌 순차 업로드 : <br>
-                            MAIN CLASS(기본, 22년 10월), ADVANCED CLASS(심화, 23년 1~2월),   MASTER CLASS(실전모강, 23년 4~5월) 이 순차적으로 업로드 될 예정입니다.</li>
-                            <li>강좌 및 교수는 학원 사정에 따라 변동될 수 있습니다.</li>
+                            <li>온라인PASS 상품은 윌비스김동진법원팀 교수진의 지정된 ‘예비순환+1~5순환’ 또는 ‘1~5순환 과정’을 기간 내 배수 제한 없이 무제한 수강 가능합니다.
+                            <li>‘민법(김동진), 민사소송법(이덕훈), 형법(문형석), 형사소송법(유안석), 헌법(이국령), 영어(박초롱), 한국사(임진석), 국어(박재현)’ 교수별 예비순환 및 1~5순환 과정 중 구매한 상품에 해당되는 강의<br>
+                            ※ 개강 전후 부득이한 학원 사정에 의해 일부 강사진의 변동이 있을 수 있습니다.</li>
                         </ol>
                     </dd>
 
-                    <dt>교재관련</dt>
+                    <dt>수강기간</dt>
                     <dd>
                         <ol>
-                            <li>각 강의수강에 필요한 교재는 별도로 구매하셔야 하며, 각 강좌별 교재는 강좌소개 및 교재구매 메뉴에서 별도 구매 가능합니다.</li>
+                            <li>수강시작일로부터 2023년 시험일까지 제공되며, 수강을 시작하는 즉시 수강 가능한 강의가 제공됩니다.<br>
+                                (예비순환+1~5순환 과정에 한함, 1~5순환 과정은 8월 9일부터 강의가 제공됩니다.)</li>
+                            <li>2023대비 예비순환 강의는 수강신청 즉시, 1순환 강의는 2022년 8월 9일부터 순차적으로 업로드 될 예정입니다.</li>
                         </ol>
                     </dd>
 
-                    <dt>환불정책</dt>
+                    <dt>동영상 수강방법 및 제한</dt>
                     <dd>
                         <ol>
-                            <li>결제 후 7일 이내 강좌의 맛보기 강의를 제외하고 2강 이하 수강 시에만 전액 환불 가능합니다.</li>
-                            <li>결제 후 7일 이내 환불 요청 시 수강한 각 강의 정가 기준으로 수강 부분만큼 차감 후 나머지 부분에 대해 환불이 진행됩니다.</li>
-                            <li>강좌 내 학습 자료 및 모바일 다운로드 이용 시에는 수강한 것으로 간주됩니다.</li>
-                            <li>고객 변심으로 인한 환불은 수강시작일 (당일 포함)로부터 7일이 경과되면, 온라인 T-PASS반 결제가 기준으로 
-                            계산하여 사용일수만큼 차감 후 환불됩니다.(환불시 CORE 특강 수강료도 정가기준 수강부분만큼 차감 후 환불됩니다.)</li>
+                            <li>① 홈페이지 [내강의실] 메뉴에서 [PASS강의] > [수강중강좌]로 접속합니다.<br>
+                                ② 접속 후 구매하신 PASS 상품명 옆의 [강좌추가]를 선택합니다.<br>
+                                ③ 원하는 과목/교수/강좌를 선택하여 추가하신 후 수강이 가능합니다.</li>
+                            <li>수강 시 이용가능한 기기는 ｢PC 2대 or 모바일 2대 or PC&모바일 각 1대｣로 총 2대의 기기로 제한됩니다.</li>
+                            <li>PC/모바일 기기 변경 등 단말기 초기화가 필요한 경우, 계정당 최초 1회에 한해 직접 초기화가 가능하며, 추후 단말기 초기화는 고객센터에서 관련 내용 확인 후 최대 2회까지 추가 진행 가능합니다.</li>
+                        </ol>
+                    </dd>
+                    
+                    <dt>교재 및 자료 관련</dt>
+                    <dd>
+                        <ol>
+                            <li>모든 교재는 별도로 구매하셔야 합니다. 각 강좌별 [수강중강좌] 페이지 내 [교재구매]를 선택하여 구매 가능합니다.</li>
+                            <li>강의자료는 각 강좌별 개별 강의에 첨부파일의 형식으로 제공됩니다. 필요한 자료는 개별 출력하여야 합니다.</li>
+                        </ol>
+                    </dd>
+
+                    <dt>환불 관련</dt>
+                    <dd>
+                        <ol>
+                            <li>결제 후 7일 이내에 전액환불 가능합니다. 단, 맛보기 강의를 제외하고 2강 이하 수강 시에만 전액환불대상이 됩니다.</li>
+                            <li>자료 또는 모바일 강의 다운로드 시에도 이용 및 수강 여부와 무관하게 수강한 것으로 간주됩니다.</li>
+                            <li>본 상품은 할인가가 적용된 특별기획상품이므로 부분환불은 정가 대비 사용일수에 따라 차감 후 환불됩니다. 구체적인 환불기준은 환불 시 고객센터로 문의 시 자세히 안내 받으실 수 있습니다.</li>
                         </ol>
                     </dd>
 
                     <dt>유의사항</dt>
                     <dd>
                         <ol>
-                            <li>본 상품은 특별할인기획 상품으로 쿠폰 할인, 다다익선 할인, 포인트 사용 등 혜택이 적용되지 않으니 양해 및 참고 부탁드립니다.</li>
-                            <li>온라인 T-PASS 반 중 불가피한 사정에 의해 부득이 진행되지 않을 경우 대체 강좌로 제공 예정이며,  이로 인한 환불은 불가합니다.</li>
-                            <li>아이디 공유, 타인에게 양도 및 판매 등 부정 사용 적발 시 수강 중인 온라인 T-PASS 반은 즉시 정지,  회원 자격이 박탈됩니다. 이로 인한 강의 환불은 절대 불가하며, 불법 공유 행위 사안에 따라 민형사상 조치가 발생 할 수 있습니다.</li>
+                            <li>본 상품은 특별기획상품으로 쿠폰 할인, 적립금 사용 등 혜택이 적용되지 않습니다.</li>
+                            <li>학원 사정에 의해 일부 강사의 강의가 부득이하게 진행되지 않거나 교수가 변경될 수 있습니다.</li>
+                            <li>아이디 불법 공유 적발 시 회원자격 박탈 및 환불불가 조치되며, 민/형사상 책임을 질 수 있습니다.</li>
                         </ol>
                     </dd>
 
-                
+                    <dt>※ 이용문의 : 고객만족센터 1544-5006</dt>
                 </dl>
-                
             </div>
         </div>
+        --}}
 	</div>
-     <!-- End Container -->
+    <!-- End Container -->
 
-    <link href="/public/js/willbes/dist/aos.css" rel="stylesheet">    
+    <link href="/public/js/willbes/dist/aos.css" rel="stylesheet">
     <script src="/public/js/willbes/dist/aos.js"></script>
     <script>
         $( document ).ready( function() {
             AOS.init();
-        } );      
+        } );
+
+      var $regi_form = $('#regi_form');
+
+      function showPopup(){
+          @if(empty($arr_promotion_params['SsIdx']) === true)
+            alert('설문정보가 없습니다.');
+            return;
+          @else
+            var url = "{{front_url('/eventSurvey/index/' . $arr_promotion_params['SsIdx'])}}";
+            var windowW = 800; // 창의 가로 길이
+            var windowH = 700; // 창의 세로 길이
+            var left = Math.ceil((window.screen.width - windowW)/2);
+            var top = Math.ceil((window.screen.height - windowH)/2);
+            window.open(url,"survey_event","top="+top+", left="+left+", height="+windowH+", width="+windowW);
+          @endif
+      }
+
+      {{--쿠폰발급--}}
+      function giveCheck() {
+          {!! login_check_inner_script('로그인 후 이용해주세요.','Y') !!}
+
+          @if(empty($arr_promotion_params) === false)
+              var _check_url = '{!! front_url('/promotion/promotionEventCheck/') !!}?give_type={{$arr_promotion_params["give_type"]}}&give_idx={{$arr_promotion_params["give_idx"]}}&event_code={{$data['ElIdx']}}&comment_chk_yn={{$arr_promotion_params["comment_chk_yn"]}}';
+              ajaxSubmit($regi_form, _check_url, function (ret) {
+                  if (ret.ret_cd) {
+                      alert('쿠폰이 발급되었습니다. \n\n내강의실에서 확인해 주세요.');
+                  }
+              }, showValidateError, null, false, 'alert');
+          @else
+            alert('프로모션 추가 파라미터가 지정되지 않았습니다.');
+          @endif
+      }
     </script>
 @stop

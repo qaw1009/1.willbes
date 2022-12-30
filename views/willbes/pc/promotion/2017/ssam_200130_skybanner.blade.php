f1dc436e8b7cce0c06194e3d8fa7405cf47baf17
김창용 <winmir@willbes.com>
2022-12-30 14:18:01 +0900

    임용 교수별 프로모션 우측 공통 메뉴

--- views/willbes/pc/promotion/2017/ssam_200130_skybanner.blade.php
+++ views/willbes/pc/promotion/2017/ssam_200130_skybanner.blade.php
@@ -21,9 +21,6 @@
         <li><a href='1817'>초등 <strong>배재민</strong></a></li>
         <li><a href='2555'>교육학 <strong>이경범</strong></a></li>
         <li><a href='2556'>교육학 <strong>정 현</strong></a></li>
-        <li><a href='2557'>교육학 <strong>신태식</strong></a></li>
-        <li><a href='1814'>교육학 <strong>이인재</strong></a></li>
-        <li><a href='1815'>교육학 <strong>홍의일</strong></a></li>
         <li><a href='1818'>전공국어 <strong>송원영</strong></a></li>
         <li><a href='1820'>전공국어 <strong>권보민</strong></a></li>
         <li><a href='2558'>전공국어 <strong>구동언</strong></a></li>
@@ -33,17 +30,15 @@
         <li><a href='2559'>전공수학 <strong>김현웅</strong></a></li>
         <li><a href='1825'>수학교육론 <strong>박태영</strong></a></li>
         <li><a href='2560'>수학교육론 <strong>박혜향</strong></a></li>
-        <li><a href='1828'>도덕윤리 <strong>김병찬</strong></a></li>
-        <li><a href='2561'>도덕윤리 <strong>김민응</strong></a></li>
-        <li><a href='2562'>일반사회 <strong>허역 팀</strong></a></li>
-        <li><a href='2564'>전공역사 <strong>김종권</strong></a></li>
         <li><a href='1826'>전공생물 <strong>강치욱</strong></a></li>
         <li><a href='1827'>생물교육론 <strong>양혜정</strong></a></li>
         <li><a href='2565'>전공화학 <strong>강 철</strong></a></li>
-        <li><a href='2566'>전공체육 <strong>최규훈</strong></a></li>    
+        <li><a href='1828'>도덕윤리 <strong>김병찬</strong></a></li>
+        <li><a href='2562'>일반사회 <strong>허역 팀</strong></a></li>
+        <li><a href='2564'>전공역사 <strong>김종권</strong></a></li>
         <li><a href='1829'>전공음악 <strong>다이애나</strong></a></li>
+        <li><a href='2566'>전공체육 <strong>최규훈</strong></a></li>   
         <li><a href='1830'>전기전자통신 <strong>최우영</strong></a></li>
-        <li><a href='1831'>정보컴퓨터 <strong>송광진</strong></a></li>
         <li><a href='1832'>정컴교육론 <strong>장순선</strong></a></li>
         <li><a href='2563'>중국어 <strong>장영희</strong></a></li>
     </ul>

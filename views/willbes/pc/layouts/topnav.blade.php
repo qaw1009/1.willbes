1572debf8e29c84df620491dd3c40d44620bad3a
신봉석 <bsshin@willbes.com>
2023-01-06 17:10:34 +0900

    프런트 > 경찰승진 (올라) 임시 상단 타이틀 적용

--- views/willbes/pc/layouts/topnav.blade.php
+++ views/willbes/pc/layouts/topnav.blade.php
@@ -4,8 +4,13 @@
         <div class="topnavTxt {{ SUB_DOMAIN }}">            
             @if($__cfg['SiteCode'] == '2014')
                 경제적 자유, 수익이 늘어나는 <span class="tx-color">자동화 시스템</span> 만들기
-            @else   
-                내 인생 모든 시험, <span class="tx-color">윌비스</span>로 합격하다!
+            @else
+                @if(in_array(ENVIRONMENT, ['local', 'development']) === true && empty($__cfg['CateCode']) === false && $__cfg['CateCode'] == '3006')
+                    {{-- TODO : 경찰승진 임시적용 --}}
+                    경찰승진, <span class="tx-color">이젠 올공에서 합격</span>한다!
+                @else
+                    내 인생 모든 시험, <span class="tx-color">윌비스</span>로 합격하다!
+                @endif
             @endif
         </div>
         <div class="loginDepth p_re">

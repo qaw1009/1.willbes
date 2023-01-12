92b23cb1debe131cb2467a6520906f74e13500eb
최현탁 <cht@willbes.com>
2023-01-12 09:59:50 +0900

    상담예약페이지 수정

--- views/willbes/pc/site/consult/index.blade.php
+++ views/willbes/pc/site/consult/index.blade.php
@@ -18,7 +18,7 @@
        </div>
 
         {{--소방직 배너 2023.01.31까지 노출--}}
-        @if ($__cfg['SiteCode'] == '2004' && element('s_campus',$arr_input) == '605001')
+        @if ($__cfg['SiteCode'] == '2004' && (element('s_campus',$arr_input) == '605001' || element('s_campus',$arr_input) == '605005'))
             <div class="pt50 c_both"><img src="https://static.willbes.net/public/images/promotion/2022/12/3023_940x300.jpg" alt="소방직 1월 구매 이벤트"/></div>
         @endif
 
@@ -46,7 +46,9 @@
 
         sendAjax(_url, data, function(ret) {
             $('#calendar_box').html(ret).show().css('display', 'block').trigger('click');
-            @if ($__cfg['SiteCode'] == '2004' && element('s_campus',$arr_input) == '605001') $(".calendar_week").find("td:eq(3)").html('수 <div class="tx-red">상담투어신청</div>'); @endif
+            @if ($__cfg['SiteCode'] == '2004' && (element('s_campus',$arr_input) == '605001' || element('s_campus',$arr_input) == '605005'))
+                $(".calendar_week").find("td:eq(3)").html('수 <div class="tx-red">상담투어신청</div>');
+            @endif
         }, showAlertError, false, 'GET', 'html');
 
         $('#schedule_list_table').html('');

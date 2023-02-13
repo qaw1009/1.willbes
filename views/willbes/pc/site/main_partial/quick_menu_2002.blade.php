42f287bfd6153f23d4c45ade73c62a10f95e78ab
김창용 <winmir@willbes.com>
2023-02-13 17:51:50 +0900

    경찰학원 메인 퀵메뉴 수정

--- views/willbes/pc/site/main_partial/quick_menu_2002.blade.php
+++ views/willbes/pc/site/main_partial/quick_menu_2002.blade.php
@@ -17,9 +17,14 @@
     <li><a href="{{ front_url('/support/notice/index') }}">공지사항</a></li>
     <li><a href="{{ front_url('/offinfo/boardInfo/index/80') }}">강의 시간표</a></li>
     <li><a href="{{ front_url('/offinfo/boardInfo/index/82') }}">강의실 배정표</a></li>    
-    <li><a href="{{ front_url('/consult/visitConsult/index') }}">1:1 방문상담</a></li>
-    <!-- <li><a href="{{ front_url('/consult/telConsult/index') }}">1:1 전화상담</a></li> -->
+    <li><a href="https://police.willbes.net/pass/promotion/index/cate/3010/code/1789">카카오 상담</a></li> 
+    <!--<li><a href="{{ front_url('/consult/visitConsult/index') }}">1:1 방문상담</a></li>
+     <li><a href="{{ front_url('/consult/telConsult/index') }}">1:1 전화상담</a></li> -->
     <li><a href="{{ site_url('/lecture/index/cate/3001/pattern/free?course_idx=1077') }}" target="_blank">보강동영상</a></li>
 </ul>
+<div class="mb5">
 {!! banner_html(element('메인_우측퀵_01', $data['arr_main_banner'])) !!}
+</div>
+<div>
 {!! banner_html(element('메인_우측퀵_02', $data['arr_main_banner'])) !!}
+</div>

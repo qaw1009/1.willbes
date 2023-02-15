9a1c82350bf41540a595d6824dd3f6226913b0f0
김창용 <winmir@willbes.com>
2023-02-15 15:26:12 +0900

    소방학원 실강파격할인 이벤트

---
+++ views/willbes/m/promotion/2015/2902.blade.php
@@ -0,0 +1,134 @@
+@extends('willbes.m.layouts.master')
+
+@section('content')
+<!-- Container -->
+
+<style type="text/css">
+    .evtCtnsBox {width:100%; max-width:720px; margin:0 auto; text-align:center; position:relative; line-height:1.5; clear:both}
+    .evtCtnsBox img {max-width:100%;}
+    .evtCtnsBox.wrap {position:relative}
+    /*.evtCtnsBox a {border:1px solid #000}*/
+
+    /*유튜브*/
+    .youtube {position:relative; padding-bottom:56.25%; overflow: hidden; margin-top:-20px !important}
+    .youtube object {position:absolute; top:0; left:50%; width:100%; margin-left:-50%; height:100%;}
+
+    .evt_04 .lecbuy {display:flex}
+    .evt_04 .lecbuy a {display:block; background:#5907ca; color:#fff; font-size:3vh; padding:3vh; width:50%; text-align:center; line-height:1.4; position: relative; margin-right:1px}
+    .evt_04 .lecbuy a span {color:#d3b4fe; text-decoration: line-through;}
+    .evt_04 .lecbuy a strong {background:#fff; color:#5907ca; padding:1vh 2vh; border-radius:4vh; display:block; margin:1vh 2vh 0}
+    .evt_04 .lecbuy a:hover {background:#000}
+
+    .evt_05 ul {padding:10px; text-align:left; font-size:2vh; line-height:1.7;}
+    .evt_05 li {list-style-type: disc; margin-left:20px; margin-bottom:20px;}
+    .evt_05 li div {font-size:1.8vh; color:#666}   
+    .evt_05 li div span {padding:2px 5px; font-size:1.4vh; color:#fff; border-radius:4px; vertical-align:middle; display:inline-block}
+    .evt_05 li div span:nth-of-type(1) {background:#3957ac;}
+    .evt_05 li div span:nth-of-type(2) {background:#40a028;}
+    .evt_05 li div span:nth-of-type(3) {background:#c90f25;}
+    .evt_05 li div span:nth-of-type(4) {background:#40a028;}    
+</style>
+
+<div id="Container" class="Container NSK c_both">
+
+    <div class="evtCtnsBox" data-aos="fade-up">        
+        <img src="https://static.willbes.net/public/images/promotion/2023/02/2902m_top.jpg" alt="상위 10%"/>        
+    </div>
+
+    <div class="evtCtnsBox" data-aos="fade-up">
+        <div class="wrap">    
+            <img src="https://static.willbes.net/public/images/promotion/2023/02/2902m_01.jpg" alt=""/>
+            <a href="https://willbesedu.willbes.net/m/pass/offLecture/show/cate/3126/prod-code/205095" title="합격반" style="position: absolute; left: 67.22%; top: 17.59%; width: 29.17%; height: 15.59%; z-index: 2;"></a>
+            <a href="https://willbesedu.willbes.net/m/pass/offLecture/show/cate/3126/prod-code/205027" title="합격보장반" style="position: absolute; left: 67.22%; top: 43.67%; width: 29.17%; height: 15.59%; z-index: 2;"></a>
+        </div>
+    </div>
+
+    <div class="evtCtnsBox" data-aos="fade-up">        
+        <img src="https://static.willbes.net/public/images/promotion/2023/02/2902m_02.jpg" alt=""/>           
+    </div>
+
+    <div class="evtCtnsBox" data-aos="fade-up">
+        <img src="https://static.willbes.net/public/images/promotion/2023/02/2902m_03.jpg" alt=""/>  
+        <div class="wrap">            
+            <a href="https://pass.willbes.net/m/pass/professor/show/prof-idx/51422/?cate_code=3050&subject_idx=1259" target="_blank" title="권나라 홈"><img src="https://static.willbes.net/public/images/promotion/2023/02/2902m_04.jpg"/></a>
+            <a href="https://pass.willbes.net/m/pass/professor/show/prof-idx/51423?cate_code=3050&subject_idx=1284" target="_blank" title="오도희 홈"><img src="https://static.willbes.net/public/images/promotion/2023/02/2902m_05.jpg"/></a>
+            <a href="https://pass.willbes.net/m/pass/professor/show/prof-idx/51215?cate_code=3050&subject_idx=1257" target="_blank" title="신기훈 홈"><img src="https://static.willbes.net/public/images/promotion/2023/02/2902m_06.jpg"/></a>
+        </div>       
+    </div>
+
+    <div class="evtCtnsBox" data-aos="fade-up">        
+        <img src="https://static.willbes.net/public/images/promotion/2023/02/2902m_07.jpg" alt="추가 학습혜택"/>           
+    </div>
+
+    <div class="evtCtnsBox" data-aos="fade-up">   
+        <a href="https://willbesedu.willbes.net/pass/promotion/index/cate/3126/code/2879" title="통합생활 관리반 더 알아보기" target="_blank">
+            <img src="https://static.willbes.net/public/images/promotion/2023/02/2902m_08.jpg" alt="통합생활 관리반"/>
+        </a>      
+    </div>
+
+    <div class="evtCtnsBox" data-aos="fade-up">        
+        <div class="youtube">
+            <object data="https://www.youtube.com/embed/qzgc1l4EtGA?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></object>
+        </div> 
+    </div>
+
+    <div class="evtCtnsBox evt_04" data-aos="fade-up">        
+        <img src="https://static.willbes.net/public/images/promotion/2023/02/2902m_09.jpg" alt=""/> 
+        <div class="lecbuy">
+            <a class="NSK-Black" href="https://willbesedu.willbes.net/m/pass/offLecture/show/cate/3126/prod-code/205095" target="_blank">
+                소방 합격반<br> <span class="NSK">350만원</span> 150만원
+                <strong>신청하기 ></strong>
+            </a>
+            <a class="NSK-Black" href="https://willbesedu.willbes.net/m/pass/offLecture/show/cate/3126/prod-code/205027" target="_blank">
+                소방 합격보장반<br> <span class="NSK">500만원</span> 300만원
+                <strong>신청하기 ></strong>
+            </a>
+        </div>          
+    </div>
+
+    <div class="evtCtnsBox evt_05" data-aos="fade-up">        
+        <img src="https://static.willbes.net/public/images/promotion/2023/02/2902m_10.jpg" alt=""/>
+        <div>
+            <img src="https://static.willbes.net/public/images/willbes/gosi_acad/map/map_so.jpg">
+        </div> 
+        <ul>
+            <li>
+                주소
+                <div>서울시 동작구 만양로 105 한성빌딩 2층</div>
+            </li>
+            <li>
+                관리반 문의
+                <div>1522-6112</div>
+            </li>
+            <li>
+                지하철 이용 시
+                <div>노량진역 1번 출구 도보로 3분 / 3번 출구 도보로 4분</div>
+            </li>
+            <li>
+                버스 이용 시
+                <div>
+                    <span>간선</span> 150, 360, 500, 504, 507, 605, 640, 654, 742<br>
+                    <span>지선</span> 5516, 5517, 5531, 5535, 5536, 6211, 641<br>
+                    <span>광역</span> 9480<br>
+                    <span>마을</span> 동작01, 동작03, 동작08, 동작13
+                </div>
+            </li>
+        </ul>	             
+    </div>
+
+</div>
+
+<!-- End Container -->
+
+<link href="/public/js/willbes/dist/aos.css" rel="stylesheet">
+<script src="/public/js/willbes/dist/aos.js"></script>
+<script>
+    $(document).ready( function() {
+        AOS.init();
+    });    
+</script>
+
+{{-- 프로모션용 스크립트 include --}}
+@include('willbes.pc.promotion.promotion_script')
+
+@stop
---
+++ views/willbes/pc/promotion/2015/2902.blade.php
@@ -0,0 +1,161 @@
+@extends('willbes.pc.layouts.master')
+
+@section('content')
+    @include('willbes.pc.layouts.partial.site_menu')
+    <!-- Container -->
+    <link href="/public/css/willbes/style_2015.css?ver={{time()}}" rel="stylesheet">
+    <style type="text/css">
+        .evtContent {
+            width:100%;
+            min-width:1120px !important;
+            max-width:2000px !important;
+            margin:20px auto 0;
+            padding:0 !important;
+            background:#fff;     
+            font-size:14px;       
+        }
+        .evtContent span {vertical-align:top}
+        .evtCtnsBox {width:100%; text-align:center; min-width:1120px; position: relative;}
+        .evtCtnsBox .wrap {width:1120px; margin:0 auto; position:relative}
+        /*.evtCtnsBox .wrap a {border:1px solid #000}*/
+
+		/************************************************************/ 
+
+        .sky {position:fixed; top:200px; right:10px; z-index:100;}
+        .sky a {display:block; margin-bottom:10px}
+
+		.evt_top {background:url(https://static.willbes.net/public/images/promotion/2023/02/2902_top_bg.jpg) no-repeat center top;}
+
+        .evt_01 {background:#ececec;}
+
+        .evt_02 .tabmenu {width:980px; margin:0 auto 40px; display:flex; justify-content: space-between;}
+        .evt_02 .tabmenu a {padding:20px; width:33.3333%; background:#a5a8ad; color:#fff; font-size:28px}
+        .evt_02 .tabmenu a.active {background:#5907ca}
+
+        .evt_03 {background:url(https://static.willbes.net/public/images/promotion/2023/02/2902_03_bg.jpg) no-repeat center top;}
+        
+        .evt_04 .lecbuy { width:1120px; margin:0 auto 100px; display:flex}
+        .evt_04 .lecbuy a {display:block; background:#5907ca; color:#fff; font-size:30px; padding:30px; width:50%; text-align:left; line-height:1.4; position: relative; margin-right:1px}
+        .evt_04 .lecbuy a span {color:#d3b4fe; text-decoration: line-through;}
+        .evt_04 .lecbuy a strong {position: absolute; top:30px; right:30px; background:#fff; color:#5907ca; padding:20px 30px; border-radius:40px}
+        .evt_04 .lecbuy a:hover {background:#000}
+
+        .youtube {}
+        .youtube iframe {width:1120px; height:550px}    
+
+
+        /************************************************************/
+
+    </style>
+
+	<div class="evtContent NSK">
+
+        <div class="sky" id="QuickMenu">
+            <a href="#evt01"><img src="https://static.willbes.net/public/images/promotion/2023/02/2902_sky01.jpg" alt="파격할인 이벤트"></a>         
+        </div>
+
+		<div class="evtCtnsBox evt_top" data-aos="fade-up">
+            <div class="wrap">
+                <img src="https://static.willbes.net/public/images/promotion/2023/02/2902_top.jpg"  alt="파격할인 이벤트"/>
+                <a href="https://willbesedu.willbes.net/pass/offLecture/show/cate/3126/prod-code/205095" target="_blank" title="합격반" style="position: absolute; left: 65%; top: 65.37%; width: 24.02%; height: 6.19%; z-index: 2;" id="evt01"></a>
+                <a href="https://willbesedu.willbes.net/pass/offLecture/show/cate/3126/prod-code/205027" target="_blank" title="합격보장반" style="position: absolute; left: 65%; top: 76.29%; width: 24.02%; height: 6.19%; z-index: 2;"></a>
+            </div>
+		</div>
+
+		<div class="evtCtnsBox evt_01" data-aos="fade-up">
+            <img src="https://static.willbes.net/public/images/promotion/2023/02/2902_01.jpg" alt="왜 윌비스 소방인가?" />
+		</div>
+
+        <div class="evtCtnsBox evt_02" data-aos="fade-up">
+			<img src="https://static.willbes.net/public/images/promotion/2023/02/2902_02_01.jpg" alt="불꽃 소방팀" />
+            <div class="tabmenu">
+                <a href="#tab01"><strong class="NSK-Black">소방학</strong><br> 권나라</a>        
+                <a href="#tab02"><strong class="NSK-Black">소방관계법규</strong><br> 오도희</a>
+                <a href="#tab03"><strong class="NSK-Black">소방행정법</strong><br> 신기훈</a>
+            </div>
+            <div id="tab01">
+                <a href="https://pass.willbes.net/pass/professor/show/prof-idx/51422/?cate_code=3050&subject_idx=1259" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2023/02/2902_02_t01.jpg" alt="소방학 권나라" /></a>
+            </div>        
+            <div id="tab02">
+                <a href="https://pass.willbes.net/pass/professor/show/prof-idx/51423?cate_code=3050&subject_idx=1284" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2023/02/2902_02_t02.jpg" alt="소방관계법규 오도희" /></a>
+            </div>
+            <div id="tab03">
+                <a href="https://pass.willbes.net/pass/professor/show/prof-idx/51215?cate_code=3050&subject_idx=1257" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2023/02/2902_02_t03.jpg" alt="소방행정법 신기훈" /></a>	
+            </div>           			  
+		</div>
+
+        <div class="evtCtnsBox evt_03" data-aos="fade-up">
+            <img src="https://static.willbes.net/public/images/promotion/2023/02/2902_03.jpg" alt="커리큘럼" />	
+		</div>   
+
+        <div class="evtCtnsBox evt_04" data-aos="fade-up">
+            <img src="https://static.willbes.net/public/images/promotion/2023/02/2902_04_01.jpg" alt="" /><br>
+            <a href="https://willbesedu.willbes.net/pass/promotion/index/cate/3126/code/2879" target="_blank"><img src="https://static.willbes.net/public/images/promotion/2023/02/2902_04_02.jpg" alt="" /></a>
+            <img src="https://static.willbes.net/public/images/promotion/2023/02/2902_04_03.jpg" alt="" />
+            <div class="lecbuy">
+                <a class="NSK-Black" href="https://willbesedu.willbes.net/pass/offLecture/show/cate/3126/prod-code/205095" target="_blank">
+                    소방 합격반<br> <span class="NSK">350만원</span> 150만원
+                    <strong>신청하기 ></strong>
+                </a>
+                <a class="NSK-Black" href="https://willbesedu.willbes.net/pass/offLecture/show/cate/3126/prod-code/205027" target="_blank">
+                    소방 합격보장반<br> <span class="NSK">500만원</span> 300만원
+                    <strong>신청하기 ></strong>
+                </a>
+            </div>
+		</div> 
+
+        <div class="evtCtnsBox evt_04" data-aos="fade-up">
+            <div class="youtube">
+                <iframe src="https://www.youtube.com/embed/qzgc1l4EtGA?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>     
+		</div>
+
+        <div class="evtCtnsBox" data-aos="fade-up">
+            <img src="https://static.willbes.net/public/images/promotion/2023/02/2902_05.jpg" alt="" />	
+		</div>
+
+        <div class="incheon">
+            <div class="Section Section4_ic mt40 mb100">
+                @include('willbes.pc.site.main_partial.campus_' . $__cfg['SiteCode'])
+            </div>
+        </div>
+	</div>
+    <!-- End Container -->
+
+    <link href="/public/js/willbes/dist/aos.css" rel="stylesheet">
+    <script src="/public/js/willbes/dist/aos.js"></script>
+    <script>
+        $(document).ready(function(){
+            $('.tabmenu').each(function(){
+                var $active, $content, $links = $(this).find('a');
+                $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+                $active.addClass('active');
+            
+                $content = $($active[0].hash);
+            
+                $links.not($active).each(function () {
+                $(this.hash).hide()});
+            
+                // Bind the click event handler
+                $(this).on('click', 'a', function(e){
+                $active.removeClass('active');
+                $content.hide();
+            
+                $active = $(this);
+                $content = $(this.hash);
+            
+                $active.addClass('active');
+                $content.show();
+            
+                e.preventDefault()})}
+            )}
+        );
+
+        $(document).ready( function() {
+            AOS.init();
+        });
+
+    </script>
+    
+
+@stop

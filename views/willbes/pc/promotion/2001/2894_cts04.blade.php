2a417fb8166224413f29286c607dcd6cab007c2e
이윤구 <yoongukk@willbes.com>
2023-02-28 15:00:36 +0900

    (경찰) 2023년 1차 합격풀케어 사전서비스 페이지 신규

---
+++ views/willbes/pc/promotion/2001/2894_cts04.blade.php
@@ -0,0 +1,9 @@
+<style type="text/css">
+     .cts04_01 {background:#0c2744;}
+</style>
+
+<div>
+    <div class="evtCtnsBox cts04_01">
+        <img src="https://static.willbes.net/public/images/promotion/2022/04/2548_cts04_top.jpg" alt="" />
+    </div>    
+</div>

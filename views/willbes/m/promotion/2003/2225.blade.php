ebed0b09be1c8f7bc7f59ffffb267de04df02d1f
김창용 <winmir@willbes.com>
2023-02-28 14:49:36 +0900

    공무원 3월 드림팩

--- views/willbes/m/promotion/2003/2225.blade.php
+++ views/willbes/m/promotion/2003/2225.blade.php
@@ -5,34 +5,34 @@
 <style type="text/css">
     .evtCtnsBox {width:100%; max-width:720px; margin:0 auto; text-align:center; position:relative; font-size:14px; line-height:1.5}
     .evtCtnsBox img {width:100%; max-width:720px;}    
-    /*.evtCtnsBox a {border:1px solid #000} */
+    .evtCtnsBox a {border:1px solid #000}
 </style>
 
 <div id="Container" class="Container NSK c_both">    
 
     <div class="evtCtnsBox" data-aos="fade-down">        
-        <img src="https://static.willbes.net/public/images/promotion/2023/01/2225m_top.jpg" alt="윌비스 드림팩" >       
+        <img src="https://static.willbes.net/public/images/promotion/2023/02/2225m_top.jpg" alt="윌비스 드림팩" >       
     </div>
 
     <div class="evtCtnsBox" data-aos="fade-up">
         <div class="wrap">
-            <img src="https://static.willbes.net/public/images/promotion/2022/09/2225m_member.jpg" alt="윌비스 회원가입" >
-            <a href="https://www.willbes.net/member/join/?ismobile=0&sitecode=2003" target="_blank" title="회원가입하기" style="position: absolute;left: 17.77%;top: 27.12%;width: 64.27%;height: 15.15%;z-index: 2;"></a>
+            <img src="https://static.willbes.net/public/images/promotion/2023/02/2225m_01.jpg" alt="윌비스 회원가입" >
+            <a href="https://www.willbes.net/member/join/?ismobile=0&sitecode=2003" target="_blank" title="회원가입하기" style="position: absolute;left: 17.77%;top: 25%;width: 64.27%;height: 15.15%;z-index: 2;"></a>
         </div>
     </div> 
 
     <div class="evtCtnsBox" data-aos="fade-up">
-        <img src="https://static.willbes.net/public/images/promotion/2022/09/2225m_dream_gift.jpg" alt="드림 기프트" >
+        <img src="https://static.willbes.net/public/images/promotion/2023/02/2225m_02.jpg" alt="드림 기프트" >
     </div>
 
     <div class="evtCtnsBox evt03" data-aos="fade-up">
-        <img src="https://static.willbes.net/public/images/promotion/2021/08/2225_m03.jpg" alt="자세히보기">
-        <a href="https://pass.willbes.net/m/home/index/cate/3019" target="_blank" title="9급" style="position: absolute;left: 18%;top: 35%;width: 17.27%;height: 3.8%;z-index: 2;"></a>
-        <a href="https://pass.willbes.net/m/home/index/cate/3020" target="_blank" title="7급" style="position: absolute;left: 65%;top: 35%;width: 17.27%;height: 3.8%;z-index: 2;"></a>
-        <a href="https://pass.willbes.net/m/home/index/cate/3023" target="_blank" title="소방" style="position: absolute;left: 18%;top: 59.5%;width: 17.27%;height: 3.8%;z-index: 2;"></a>
-        <a href="https://pass.willbes.net/m/home/index/cate/3024" target="_blank" title="군무원" style="position: absolute;left: 65%;top: 59.5%;width: 17.27%;height: 3.8%;z-index: 2;"></a>
-        <a href="https://pass.willbes.net/m/home/index/cate/3028" target="_blank" title="기술직" style="position: absolute;left: 18%;top: 84%;width: 17.27%;height: 3.8%;z-index: 2;"></a>
-        <a href="https://pass.willbes.net/m/home/index/cate/3035" target="_blank" title="법원팀" style="position: absolute;left: 65%;top: 84%;width: 17.27%;height: 3.8%;z-index: 2;"></a>
+        <img src="https://static.willbes.net/public/images/promotion/2023/02/2225m_03.jpg" alt="자세히보기">
+        <a href="https://pass.willbes.net/m/home/index/cate/3019" target="_blank" title="9급" style="position: absolute;left: 18%;top: 36%;width: 17.27%;height: 3.8%;z-index: 2;"></a>
+        <a href="https://pass.willbes.net/m/home/index/cate/3020" target="_blank" title="7급" style="position: absolute;left: 65%;top: 36%;width: 17.27%;height: 3.8%;z-index: 2;"></a>
+        <a href="https://pass.willbes.net/m/home/index/cate/3023" target="_blank" title="소방" style="position: absolute;left: 18%;top: 61.5%;width: 17.27%;height: 3.8%;z-index: 2;"></a>
+        <a href="https://pass.willbes.net/m/home/index/cate/3024" target="_blank" title="군무원" style="position: absolute;left: 65%;top: 61.5%;width: 17.27%;height: 3.8%;z-index: 2;"></a>
+        <a href="https://pass.willbes.net/m/home/index/cate/3028" target="_blank" title="기술직" style="position: absolute;left: 18%;top: 87%;width: 17.27%;height: 3.8%;z-index: 2;"></a>
+        <a href="https://pass.willbes.net/m/home/index/cate/3035" target="_blank" title="법원팀" style="position: absolute;left: 65%;top: 87%;width: 17.27%;height: 3.8%;z-index: 2;"></a>
     </div>
     
 </div>
@@ -42,7 +42,7 @@
 <script src="/public/js/willbes/dist/aos.js"></script>
 <script>
     $(document).ready( function() {
-    AOS.init();
+        AOS.init();
     });
 </script>
 
--- views/willbes/pc/promotion/2003/2225.blade.php
+++ views/willbes/pc/promotion/2003/2225.blade.php
@@ -21,8 +21,8 @@
 
         /************************************************************/
 
-        .wb_top10 {background:url(https://static.willbes.net/public/images/promotion/2023/01/2225_top_bg.jpg) no-repeat center top; height: 1170px;}
-        .wb_top10 .imgA {position: absolute; top:225px; left:50%; margin-left:-372px; width:745px; z-index: 2;}
+        .wb_top {}
+        .wb_top .imgA {position: absolute; top:225px; left:50%; margin-left:-372px; width:745px; z-index: 2;}
 
         .wb_cts03 {background:#303132}
 
@@ -30,24 +30,25 @@
 
     <div class="evtContent NSK" id="evtContainer">
 
-        <div class="evtCtnsBox wb_top10">
-           <span class="imgA" data-aos="flip-up" data-aos-duration="1000"><img src="https://static.willbes.net/public/images/promotion/2022/12/2225_top_img.png" alt="윌비스 드림팩"/></span>
+        <div class="evtCtnsBox wb_top">
+            <img src="https://static.willbes.net/public/images/promotion/2023/02/2225_top.jpg" alt="윌비스 회원가입"/>
+           <span class="imgA" data-aos="flip-up" data-aos-duration="1000"><img src="https://static.willbes.net/public/images/promotion/2023/02/2225_top_img.png" alt="윌비스 드림팩"/></span>
         </div>
 
         <div class="evtCtnsBox wb_cts01" data-aos="fade-up">
             <div class="wrap">
-                <img src="https://static.willbes.net/public/images/promotion/2022/10/2225_01.jpg" alt="윌비스 회원가입"/>
-                <a href="https://www.willbes.net/member/join/?ismobile=0&sitecode=2003" target="_blank" title="회원가입하기" style="position: absolute;left: 28.77%;top: 25.12%;width: 42.27%;height: 15.45%;z-index: 2;"></a>
+                <img src="https://static.willbes.net/public/images/promotion/2023/02/2225_01.jpg" alt="윌비스 회원가입"/>
+                <a href="https://www.willbes.net/member/join/?ismobile=0&sitecode=2003" target="_blank" title="회원가입하기" style="position: absolute;left: 28.77%;top: 23%;width: 42.27%;height: 15.45%;z-index: 2;"></a>
             </div>
         </div>
         <div class="evtCtnsBox wb_cts02" data-aos="fade-up">
-            <img src="https://static.willbes.net/public/images/promotion/2022/10/2225_02.jpg" alt="드림 기프트"/>
+            <img src="https://static.willbes.net/public/images/promotion/2023/02/2225_02.jpg" alt="드림 기프트"/>
         </div>
  
 
         <div class="evtCtnsBox wb_cts03" data-aos="fade-up">
             <div class="wrap">
-                <img src="https://static.willbes.net/public/images/promotion/2021/08/2225_03.jpg" alt="자세히보기"/>
+                <img src="https://static.willbes.net/public/images/promotion/2023/02/2225_03.jpg" alt="자세히보기"/>
                 <a href="https://pass.willbes.net/home/index/cate/3019" target="_blank" title="9급" style="position: absolute;left: 14.77%;top: 52%;width: 12.27%;height: 3.8%;z-index: 2;"></a>
                 <a href="https://pass.willbes.net/home/index/cate/3020" target="_blank" title="7급" style="position: absolute;left: 43.77%;top: 52%;width: 12.27%;height: 3.8%;z-index: 2;"></a>
                 <a href="https://pass.willbes.net/home/index/cate/3023" target="_blank" title="소방" style="position: absolute;left: 72.77%;top: 52%;width: 12.27%;height: 3.8%;z-index: 2;"></a>

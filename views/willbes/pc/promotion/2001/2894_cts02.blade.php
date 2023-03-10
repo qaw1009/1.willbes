63357a8e883e64897b99f5f3d7ece63979367339
이윤구 <yoongukk@willbes.com>
2023-03-10 16:26:30 +0900

    (경찰) 23년 1차 당일 합격풀케어 서비스 메인 페이지 및 본서비스 페이지 신규

--- views/willbes/pc/promotion/2001/2894_cts02.blade.php
+++ views/willbes/pc/promotion/2001/2894_cts02.blade.php
@@ -1,15 +1,15 @@
 <style type="text/css">
 .evtCtnsBox .wrap {width:1120px; margin:0 auto; position:relative}
 /*.evtCtnsBox .wrap a {border:1px solid #000} */
-.cts02_top {background:url("https://static.willbes.net/public/images/promotion/2022/03/2548_cts02_top_bg.jpg") no-repeat center top;}
-.cts02_01 {background:#eaeaea;}
+.cts02_top {background:url("https://static.willbes.net/public/images/promotion/2023/03/2894_cts02_top_bg.jpg") no-repeat center top;}
+.cts02_01 {background:url("https://static.willbes.net/public/images/promotion/2023/03/2894_cts02_01_bg.jpg") no-repeat center top;}
 .cts02_02 {padding-bottom:50px;}
 </style>
 
 <div>
     <div class="evtCtnsBox cts02_top">
         <div class="wrap">
-            <img src="https://static.willbes.net/public/images/promotion/2022/03/2548_cts02_top.jpg" alt="합격예측 풀서비스">
+            <img src="https://static.willbes.net/public/images/promotion/2023/03/2894_cts02_top.jpg" alt="합격예측 풀서비스">
             @if($arr_promotion_params['start_active_tab2'] <= date('YmdHi'))
                 <a href="javascript:alert('종료 되었습니다.')"  style="position: absolute;left: 26.01%;top: 84.07%;width: 48.13%;height: 6.3%;z-index: 2;"></a>
             @else
@@ -25,14 +25,14 @@
     </div>
     <div class="evtCtnsBox cts02_01" id="cts02_01">
         <div class="wrap">
-            <img src="https://static.willbes.net/public/images/promotion/2022/03/2548_cts02_01.jpg" alt="라이브 토크쇼">            
+            <img src="https://static.willbes.net/public/images/promotion/2023/03/2894_cts02_01.jpg" alt="라이브 토크쇼">            
             <a href="https://www.youtube.com/channel/UCQ-jvqaobw6E9EvnFO88vwQ" target="_blank" style="position: absolute;left: 28.04%;top: 87.41%;width: 44.13%;height: 4.02%;z-index: 2;"></a>
         </div>
     </div>
 
     <div class="evtCtnsBox cts02_02">
         <div class="wrap">
-            <img src="https://static.willbes.net/public/images/promotion/2022/03/2548_cts02_02.jpg" alt="신광은 경찰팀">
+            <img src="https://static.willbes.net/public/images/promotion/2023/03/2894_cts02_02.jpg" alt="신광은 경찰팀">
             <a href="javascript:alert('종료 되었습니다.')"    style="position: absolute;left: 27.99%;top: 91.07%;width: 44.13%;height: 5.78%;z-index: 2;"></a>
         </div>
     </div>

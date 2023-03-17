8e4cc65961cf22322f9d2937c1a838d7a82961f2
김창용 <winmir@willbes.com>
2023-03-17 16:02:15 +0900

    고등고시 직렬별 메인 배너 영역

--- views/willbes/pc/site/main_2005_3094.blade.php
+++ views/willbes/pc/site/main_2005_3094.blade.php
@@ -39,12 +39,12 @@
             <div class="widthAuto bnSt02">
                 <div class="bSlider bnLeft">
                     <div class="slider">
-                        {!! banner_html(element('메인_서브1_V2', $data['arr_main_banner'])) !!}
+                        {!! banner_html(element('메인_서브1_V2', $data['arr_main_banner']), 'slider') !!}
                     </div>
                 </div>
                 <div class="bSlider bnRight">
                     <div class="slider">
-                        {!! banner_html(element('메인_서브2_V2', $data['arr_main_banner'])) !!}
+                        {!! banner_html(element('메인_서브2_V2', $data['arr_main_banner']), 'slider') !!}
                     </div>
                 </div>
             </div>

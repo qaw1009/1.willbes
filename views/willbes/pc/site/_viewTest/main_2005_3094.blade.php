f35e61d100298b4454aafae53b4e01cd12640710
김창용 <winmir@willbes.com>
2023-03-17 16:34:21 +0900

    고등고시 직렬별 메인 배너 영역

--- views/willbes/pc/site/_viewTest/main_2005_3094.blade.php
+++ views/willbes/pc/site/_viewTest/main_2005_3094.blade.php
@@ -21,7 +21,7 @@
     border: 1px solid #000;
     background:#f4f4f4
 }
-.hanlim .bnLeft .bSlider .bx-viewport {height:360px !important;}
+.hanlim .bnLeft .bSlider .bx-viewport {height:90px !important;}
 </style>
     <!-- Container -->
     <div id="Container" class="Container hanlim hanlim{{$__cfg['CateCode']}} NSK c_both">

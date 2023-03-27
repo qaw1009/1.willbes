0364ec410b738c887bccc6b815fe46472749a0e6
김창용 <winmir@willbes.com>
2023-03-27 14:19:23 +0900

    소방직 종합적성검사 수정

--- views/willbes/pc/promotion/2003/2325.blade.php
+++ views/willbes/pc/promotion/2003/2325.blade.php
@@ -10,8 +10,8 @@
             margin-top:20px !important;
             padding:0 !important;            
         }
-        .evtContent span {vertical-align:auto}
-        .evtCtnsBox {width:100%; text-align:center; min-width:1120px;}
+        .evtContent span {vertical-align:top}
+        .evtCtnsBox {width:100%; text-align:center; min-width:1120px; position:relative}
         .evtCtnsBox .wrap {width:1120px; margin:0 auto; position:relative}
 
         /************************************************************/
@@ -19,14 +19,14 @@
         .sky {position:fixed;top:200px;right:15px;z-index:200;}
         .sky a {display:block;margin-top:10px;}
 
-        .evtTop {background:url(https://static.willbes.net/public/images/promotion/2021/08/2325_top_bg.jpg) no-repeat center top;} 
-        .evtTop span {position:absolute; left:50%; width:227px; top:483px; margin-left:-690px; z-index:1}
+        .evtTop {background:url(https://static.willbes.net/public/images/promotion/2023/03/2325_top_bg.jpg) no-repeat center top;} 
+        .evtTop span {position:absolute; left:50%; width:227px; top:180px; margin-left:230px; z-index:1}
 
         .evt02 {background:#585858}
 
         .evt04 {background:#f5f5f5}
 
-        .evt05 {padding:100px 0; width:1120px; margin:0 auto}
+        .evt05 {width:1120px; margin:0 auto 150px}
         .evt05 .sTitle {margin:100px 0 30px; font-size:20px; text-align:left; color:#464646}
 
 
@@ -44,54 +44,47 @@
         {{--
         <div class="sky" id="QuickMenu">
             <a href="#evt04"> 
-                <img src="https://static.willbes.net/public/images/promotion/2021/08/2325_sky01.jpg" alt="무료수강권">
+                <img src="https://static.willbes.net/public/images/promotion/2023/03/2325_sky01.jpg" alt="무료수강권">
             </a>
         </div>
         --}}
 
         <div class="evtCtnsBox evtTop">
-            <img src="https://static.willbes.net/public/images/promotion/2021/08/2325_top.jpg" alt="인적성검사"/>
+            <img src="https://static.willbes.net/public/images/promotion/2023/03/2325_top.jpg" alt="인적성검사"/>
             <span data-aos="fade-right">
-                <img src="https://static.willbes.net/public/images/promotion/2021/08/2325_top_img.png" alt=""/>
+                <img src="https://static.willbes.net/public/images/promotion/2023/03/2325_top_img.png" alt=""/>
             </span>
         </div>
 
         <div class="evtCtnsBox evt01">
-            <img src="https://static.willbes.net/public/images/promotion/2021/08/2325_01.jpg" alt="인적성검사는 왜 따로 준비해야할까요?"  data-aos="fade-left"/>
+            <img src="https://static.willbes.net/public/images/promotion/2023/03/2325_01.jpg" alt="인적성검사는 왜 따로 준비해야할까요?"  data-aos="fade-left"/>
         </div>
 
         <div class="evtCtnsBox evt02">
-            <img src="https://static.willbes.net/public/images/promotion/2021/08/2325_02.jpg" alt="인적성검사를 선택해야하는 이유" data-aos="fade-right"/>
+            <img src="https://static.willbes.net/public/images/promotion/2023/03/2325_02.jpg" alt="인적성검사를 선택해야하는 이유" data-aos="fade-right"/>
         </div>
 
         <div class="evtCtnsBox evt03">
-            <img src="https://static.willbes.net/public/images/promotion/2021/08/2325_03.jpg" alt="어떤 것을 평가하나요?" data-aos="fade-left"/>
+            <img src="https://static.willbes.net/public/images/promotion/2023/03/2325_03.jpg" alt="어떤 것을 평가하나요?" data-aos="fade-left"/>
         </div>
 
         <div class="evtCtnsBox evt04">
-            <img src="https://static.willbes.net/public/images/promotion/2021/08/2325_04.jpg" alt="세부 결과 제공" data-aos="fade-right"/>   
+            <img src="https://static.willbes.net/public/images/promotion/2023/03/2325_04.jpg" alt="세부 결과 제공" data-aos="fade-right"/>   
         </div> 
+
         
         <div class="evtCtnsBox evt05">
-            <img src="https://static.willbes.net/public/images/promotion/2021/08/2325_05.jpg" alt="온라인 수강 신청"/>
+            <img src="https://static.willbes.net/public/images/promotion/2023/03/2325_05.jpg" alt="온라인 수강 신청"/>
+            {{--
             <div class="sTitle NSK-Black">인·적성역량(PCA)검사</div>
+            --}}
             @if(empty($arr_base['display_product_data']) === false)
                 @include('willbes.pc.promotion.display_product_partial',array('group_num'=>1))
             @endif 
-            <div class="sTitle NSK-Black">인·적성검사</div>
-            @if(empty($arr_base['display_product_data']) === false)
-                @include('willbes.pc.promotion.display_product_partial',array('group_num'=>2))
-            @endif
-            <div class="sTitle NSK-Black">인·적성Lite검사</div>
-            @if(empty($arr_base['display_product_data']) === false)
-                @include('willbes.pc.promotion.display_product_partial',array('group_num'=>3))
-            @endif
-            <div class="sTitle NSK-Black">인성검사</div>
-            @if(empty($arr_base['display_product_data']) === false)
-                @include('willbes.pc.promotion.display_product_partial',array('group_num'=>4))
-            @endif
+            
         </div>
 
+        {{--
         <div class="evtCtnsBox evtInfo" id="careful">
 			<div class="evtInfoBox">
 				<h4 class="NSK-Black">윌비스 소방공무원 인·적성검사 이용안내</h4>
@@ -158,15 +151,16 @@
 				
 			</div>
 		</div>
+        --}}
     </div>
 
 
     <!-- End Container -->
-    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <link href="/public/js/willbes/dist/aos.css" rel="stylesheet">
+    <script src="/public/js/willbes/dist/aos.js"></script>
     <script>
-      $( document ).ready( function() {
+      $(document).ready(function() {
         AOS.init();
-      } );
+      });
     </script>
 @stop

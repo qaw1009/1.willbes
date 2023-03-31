673f3502d8c9d14d5f22cd53bb3a86d292598816
김창용 <winmir@willbes.com>
2023-03-31 11:22:26 +0900

    공무원 4월 드림팩

--- views/willbes/m/promotion/2003/2225.blade.php
+++ views/willbes/m/promotion/2003/2225.blade.php
@@ -17,7 +17,7 @@
     <div class="evtCtnsBox" data-aos="fade-up">
         <div class="wrap">
             <img src="https://static.willbes.net/public/images/promotion/2023/03/2225m_01.jpg" alt="윌비스 회원가입" >
-            <a href="https://www.willbes.net/member/join/?ismobile=0&sitecode=2003" target="_blank" title="회원가입하기" style="position: absolute;left: 17.77%;top: 28%;width: 64.27%;height: 15.15%;z-index: 2;"></a>
+            <a href="https://www.willbes.net/member/join/?ismobile=0&sitecode=2003" target="_blank" title="회원가입하기" style="position: absolute;left: 17.77%; top: 28%; width: 64.27%;height: 15.15%;z-index: 2;"></a>
         </div>
     </div> 
 
@@ -33,8 +33,7 @@
         <a href="https://pass.willbes.net/m/home/index/cate/3024" target="_blank" title="군무원" style="position: absolute;left: 65%;top: 61.5%;width: 17.27%;height: 3.8%;z-index: 2;"></a>
         <a href="https://pass.willbes.net/m/home/index/cate/3028" target="_blank" title="기술직" style="position: absolute;left: 18%;top: 87%;width: 17.27%;height: 3.8%;z-index: 2;"></a>
         <a href="https://pass.willbes.net/m/home/index/cate/3035" target="_blank" title="법원팀" style="position: absolute;left: 65%;top: 87%;width: 17.27%;height: 3.8%;z-index: 2;"></a>
-    </div>
-    
+    </div>    
 </div>
 <!-- End Container -->
 

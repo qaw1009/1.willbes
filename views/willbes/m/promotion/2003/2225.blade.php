330b4bfbcb5f12d651f6a658434f36b8334817b6
김창용 <winmir@willbes.com>
2023-03-31 11:20:48 +0900

    공무원 4월 드림팩

--- views/willbes/m/promotion/2003/2225.blade.php
+++ views/willbes/m/promotion/2003/2225.blade.php
@@ -5,28 +5,28 @@
 <style type="text/css">
     .evtCtnsBox {width:100%; max-width:720px; margin:0 auto; text-align:center; position:relative; font-size:14px; line-height:1.5}
     .evtCtnsBox img {width:100%; max-width:720px;}    
-    .evtCtnsBox a {border:1px solid #000}
+    /*.evtCtnsBox a {border:1px solid #000}*/
 </style>
 
 <div id="Container" class="Container NSK c_both">    
 
     <div class="evtCtnsBox" data-aos="fade-down">        
-        <img src="https://static.willbes.net/public/images/promotion/2023/02/2225m_top.jpg" alt="윌비스 드림팩" >       
+        <img src="https://static.willbes.net/public/images/promotion/2023/03/2225m_top.jpg" alt="윌비스 드림팩" >       
     </div>
 
     <div class="evtCtnsBox" data-aos="fade-up">
         <div class="wrap">
-            <img src="https://static.willbes.net/public/images/promotion/2023/02/2225m_01.jpg" alt="윌비스 회원가입" >
-            <a href="https://www.willbes.net/member/join/?ismobile=0&sitecode=2003" target="_blank" title="회원가입하기" style="position: absolute;left: 17.77%;top: 25%;width: 64.27%;height: 15.15%;z-index: 2;"></a>
+            <img src="https://static.willbes.net/public/images/promotion/2023/03/2225m_01.jpg" alt="윌비스 회원가입" >
+            <a href="https://www.willbes.net/member/join/?ismobile=0&sitecode=2003" target="_blank" title="회원가입하기" style="position: absolute;left: 17.77%;top: 28%;width: 64.27%;height: 15.15%;z-index: 2;"></a>
         </div>
     </div> 
 
     <div class="evtCtnsBox" data-aos="fade-up">
-        <img src="https://static.willbes.net/public/images/promotion/2023/02/2225m_02.jpg" alt="드림 기프트" >
+        <img src="https://static.willbes.net/public/images/promotion/2023/03/2225m_02.jpg" alt="드림 기프트" >
     </div>
 
     <div class="evtCtnsBox evt03" data-aos="fade-up">
-        <img src="https://static.willbes.net/public/images/promotion/2023/02/2225m_03.jpg" alt="자세히보기">
+        <img src="https://static.willbes.net/public/images/promotion/2023/03/2225m_03.jpg" alt="자세히보기">
         <a href="https://pass.willbes.net/m/home/index/cate/3019" target="_blank" title="9급" style="position: absolute;left: 18%;top: 36%;width: 17.27%;height: 3.8%;z-index: 2;"></a>
         <a href="https://pass.willbes.net/m/home/index/cate/3020" target="_blank" title="7급" style="position: absolute;left: 65%;top: 36%;width: 17.27%;height: 3.8%;z-index: 2;"></a>
         <a href="https://pass.willbes.net/m/home/index/cate/3023" target="_blank" title="소방" style="position: absolute;left: 18%;top: 61.5%;width: 17.27%;height: 3.8%;z-index: 2;"></a>

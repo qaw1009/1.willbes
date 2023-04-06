621ae17886879166127b86a1d5fe9c4a29656b15
하준걸 <hereisme@willbes.com>
2023-04-06 14:57:13 +0900

    starplayer config update

--- public/vendor/starplayer/js/starplayer_config_live.js
+++ public/vendor/starplayer/js/starplayer_config_live.js
@@ -9,17 +9,17 @@ var STARPLAYER64_SETUP_URL = "https://cabstarplayer.myskcdn.com/starplayer/starp
 var STARPLAYER64_VERSION = "1,5,24,55";
 
 //OSX
-var STARPLAYER_OSX_SETUP_URL = "https://cab-starplayer.service.concdn.com/starplayer/StarPlayerAgent_1.1.9.6.pkg";
+var STARPLAYER_OSX_SETUP_URL = "https://cab-starplayer.service.concdn.com/starplayer/StarPlayerAgent_1.1.10.5.pkg";
 var STARPLAYER_OSX_VERSION = "1,1,9,6";
 
 //Agent
-var STARPLAYER_AGENT_SETUP_URL = "https://cab-starplayer.service.concdn.com/starplayer/starplayer_agent_1.3.14.3.exe";
+var STARPLAYER_AGENT_SETUP_URL = "https://cab-starplayer.service.concdn.com/starplayer/starplayer_agent_1.3.16.4.exe";
 var STARPLAYER_AGENT_VERSION = "1,2,8,81";
 
 //CHROME
-var STARPLAYER_CHROME_URL = "https://cabstarplayer.myskcdn.com/starplayer/chrome.txt";
+var STARPLAYER_CHROME_URL = "https://cab-starplayer.service.concdn.com/starplayer/chrome.txt";
 
 //CONFIG
 var STARPLAYER_CONFIG_URL = "https://static.willbes.net/public/uploads/starplayer/starplayer.txt";
-var STARPLAYER_UPDATED_DATE = "2020.06.11";
-var STARPLAYER_AGENT_UPDATED_DATE = "2020.09.24";
+var STARPLAYER_UPDATED_DATE = "2023.03.23";
+var STARPLAYER_AGENT_UPDATED_DATE = "2023.03.23";

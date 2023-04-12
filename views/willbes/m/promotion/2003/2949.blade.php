5b7f5c648f623bb06e1592b2733fbb2e45360108
이윤구 <yoongukk@willbes.com>
2023-04-12 16:23:02 +0900

    (공무원 )소방 환급형 0원 인강패스 페이지 수정

--- views/willbes/m/promotion/2003/2949.blade.php
+++ views/willbes/m/promotion/2003/2949.blade.php
@@ -4,10 +4,35 @@
 <!-- Container -->
 
 <style type="text/css">
-    .evtCtnsBox {width:100%; max-width:720px; margin:0 auto; text-align:center; position:relative; line-height:1.5; clear:both}
-    .evtCtnsBox img {max-width:100%;}
-    .evtCtnsBox.wrap {position:relative}
+    .evtCtnsBox {width:100%; max-width:720px; margin:0 auto; text-align:center; position:relative; font-size:1.4vh; line-height:1.4; clear:both}
+    .evtCtnsBox img {width:100%; max-width:720px;}
+    .evtCtnsBox .wrap {position:relative}
+    /*.evtCtnsBox a {border:1px solid #000}*/
     /*.evtCtnsBox.wrap a {border:1px solid #000}*/
+
+    .evt01 .price {text-align:center; font-size:23px; font-weight:bold; color:#000; letter-spacing:-1px; position:absolute; top:92.5%; width:100%; z-index: 10;}
+    .evt01 .price p {margin-bottom:20px}
+
+    .evt01 .ext01txt {padding:20px; text-align:left}
+    .evt01 .ext01txt label {font-size:19px; font-weight:bold;}
+    .evt01 input[type="radio"] {height:30px; width:30px; vertical-align:middle}
+    .evt01 input[type="checkbox"] {height:20px; width:20px; vertical-align:middle; margin-right:5px}    
+    .evt01 .ext01txt input:checked + label {color:blue}
+    .evt01 .ext01txt ul {margin:10px 0 0 25px;}
+
+    .evt01 {background:#FF9C00; padding-bottom:50px}
+    .evt01 .lecbuy {display:flex; justify-content:center; flex-wrap: wrap; margin:0 2vh}
+    .evt01 .lecbuy .p_re {width:50%; margin-bottom:20px; position: relative;}
+    .evt01 .lecbuy .p_re img {max-width:317px;}
+    .evt01 .check {font-size:1.6vh; text-align:center; line-height:1.4;margin-top:40px;font-weight:bold;}
+    .evt01 .check input {border:2px solid #000; margin-right:10px; height:20px; width:20px}
+    .evt01 .check a {display:block; padding:5px 0; color:#fff; background:#000; width:60%; margin:20px auto 0; border-radius:20px; font-size:1.8vh;}
+    .evt01 .check a:hover {color:#333; background:#35fffa;}
+    .evt01 .info {margin:20px auto 50px; padding:10px;line-height:1.4; font-size:1.6vh; font-weight:bold;}
+
+    .totalPrice {width:80%; margin:0 auto;}
+    .totalPrice a {display:block; background:#000; font-size:1.5rem; color:#fff; padding:20px; background:#000; border-radius:5px; box-shadow:10px rgba(0,0,0,.5);}
+    .totalPrice a:hover {background:#533fd1}
 
     .evt_07 {padding:100px 0;}
     .youtubeWrap {position:relative; padding-bottom:56.25%; overflow: hidden; margin-top:-20px !important}
@@ -36,16 +61,31 @@
     /* 폰 가로, 태블릿 세로*/
     @@media only screen and (max-width: 374px)  {   
         .videoBox{width:325px;}
+        .evt01 .price {font-size:12px;}
+        .evt01 .ext01txt label {font-size:14px;}
+        .evt01 input[type="radio"] {height:15px; width:15px; vertical-align:middle}    
+
+        .evt01 .lecbuy a {width: calc(50% - 10px);} 
     }
 
     /* 태블릿 세로 */
     @@media only screen and (min-width: 375px) and (max-width: 640px) { 
         .videoBox{width:500px;}
+        .evt01 .price {font-size:15px;}
+        .evt01 .ext01txt label {font-size:17px;}
+        .evt01 input[type="radio"] {height:20px; width:20px; vertical-align:middle}
+
+        .evt01 .lecbuy a {width: calc(50% - 10px);}    
     }
 
     /* 태블릿 가로, PC */
     @@media only screen and (min-width: 641px) {
         .videoBox{width:600px;}
+        .evt01 .price {font-size:17px;}
+        .evt01 .ext01txt label {font-size:19px;}
+        .evt01 input[type="radio"] {height:25px; width:25px; vertical-align:middle}
+
+        .evt01 .lecbuy a {width: calc(50% - 10px);}
     }
 
 </style>
@@ -56,18 +96,56 @@
         <img src="https://static.willbes.net/public/images/promotion/2023/04/2949m_top.jpg"  alt="윌비스 소방 119패스"/>
     </div>
 
-    <div class="evtCtnsBox evt_01" data-aos="fade-up">
-        <div class="wrap">
-            <img src="https://static.willbes.net/public/images/promotion/2023/04/2949m_01.jpg" title="수강신청">
-            <a href="https://pass.willbes.net/m/periodPackage/show/cate/3023/pack/648001/prod-code/207036" target="_blank" title="24년 공채 0원 119 PASS" style="position: absolute;left: 13.73%;top: 19.16%;width: 35.71%;height: 28.11%;z-index: 2;"></a>
-            <a href="https://pass.willbes.net/m/periodPackage/show/cate/3023/pack/648001/prod-code/207037" target="_blank" title="4년 공채 119 PASS" style="position: absolute;left: 51.03%;top: 19.16%;width: 35.71%;height: 28.11%;z-index: 2;"></a>
-            <a href="https://pass.willbes.net/m/periodPackage/show/cate/3023/pack/648001/prod-code/207038" target="_blank" title="24년 구조/학과 경채 0원 PASS" style="position: absolute;left: 13.73%;top: 48.46%;width: 35.71%;height: 28.11%;z-index: 2;"></a>
-            <a href="https://pass.willbes.net/m/periodPackage/show/cate/3023/pack/648001/prod-code/207039" target="_blank" title="24년 구급 경채 0원 PASS" style="position: absolute;left: 51.03%;top: 48.46%;width: 35.71%;height: 28.11%;z-index: 2;"></a>
-        </div>    
+    <div class="evtCtnsBox evt01" data-aos="fade-up" id="evt01"> 
+        <img src="https://static.willbes.net/public/images/promotion/2023/04/2949m_01_title.jpg" alt="소방공채"/>            
+        <div class="evt01_coupon lecbuy" id="pass">
+            <div class="p_re">
+                <img src="https://static.willbes.net/public/images/promotion/2023/04/2949m_01_01.png" alt="" > 
+                <div class="price NSK-Black">                       
+                    <input type="radio" id="y_pkg1" name="y_pkg" value="207036" onClick=""/>
+                    <label for="y_pkg1">24년 0원 119 PASS</label>
+                </div>
+            </div>
+            <div class="p_re">
+                <img src="https://static.willbes.net/public/images/promotion/2023/04/2949m_01_02.png" alt="" > 
+                <div class="price NSK-Black">                       
+                    <input type="radio" id="y_pkg2" name="y_pkg" value="207037" onClick=""/>
+                    <label for="y_pkg2">24년 119 PASS</label>
+                </div>
+            </div>
+            <img src="https://static.willbes.net/public/images/promotion/2023/04/2949m_01s_title.jpg" alt="소방경채"/> 
+            <div class="p_re">
+                <img src="https://static.willbes.net/public/images/promotion/2023/04/2949m_01_03.png" alt="" > 
+                <div class="price NSK-Black">                       
+                    <input type="radio" id="y_pkg3" name="y_pkg" value="207038" onClick=""/>
+                    <label for="y_pkg3">24년 0원 119 PASS<br> 구조/학과</label>
+                </div>
+            </div>          
+            <div class="p_re">
+                <img src="https://static.willbes.net/public/images/promotion/2023/04/2949m_01_04.png" alt="" > 
+                <div class="price NSK-Black">                       
+                    <input type="radio" id="y_pkg4" name="y_pkg" value="207039" onClick=""/>
+                    <label for="y_pkg4">24년 0원 119 PASS<br> 구급</label>
+                </div>
+            </div>                           
+            <div class="ext01txt">
+                <input type="checkbox" id="is_chk1" name="is_chk" value="Y"/> <label for="is_chk1">PASS 과정 세부사항 확인하였고,이에 동의합니다.</label>              
+            </div>
+        </div>
+        <div class="totalPrice NSK-Black">
+            <a href="javascript:void(0);" onclick="goCartNDirectPay('evt01', 'y_pkg', 'on_lecture', 'periodpack_lecture', 'Y','is_chk1', 'pass');">패스상품 신청하기 ></a>
+        </div>
     </div>
 
     <div class="evtCtnsBox evt_02" data-aos="fade-up">
-        <img src="https://static.willbes.net/public/images/promotion/2023/04/2949m_02.jpg" title="강사진">           
+        <div class"wrap">
+            <img src="https://static.willbes.net/public/images/promotion/2023/04/2949m_02.jpg" title="강사진">
+            <a href="https://pass.willbes.net/m/professor/show/cate/3023/prof-idx/51421?subject_idx=1113" title="권나라" target="_blank" style="position: absolute;left: 4.93%;top: 50.12%;width: 8.71%;height: 4.31%;z-index: 2;"></a>
+            <a href="https://pass.willbes.net/m/professor/show/cate/3023/prof-idx/51420?subject_idx=1138" title="오도희" target="_blank" style="position: absolute;left: 23.93%;top: 50.12%;width: 8.71%;height: 4.31%;z-index: 2;"></a>
+            <a href="https://pass.willbes.net/m/professor/show/cate/3023/prof-idx/51206?subject_idx=1111" title="신기훈" target="_blank" style="position: absolute;left: 44.93%;top: 50.12%;width: 8.71%;height: 4.31%;z-index: 2;"></a>
+            <a href="https://pass.willbes.net/m/professor/show/cate/3023/prof-idx/51449?subject_idx=1140" title="김윤정" target="_blank" style="position: absolute;left: 64.93%;top: 50.12%;width: 8.71%;height: 4.31%;z-index: 2;"></a>
+            <a href="https://pass.willbes.net/m/professor/show/cate/3023/prof-idx/51439?subject_idx=2261" title="이혜영" target="_blank" style="position: absolute;left: 86.93%;top: 50.12%;width: 8.71%;height: 4.31%;z-index: 2;"></a>
+        </div>
     </div>
 
     <div class="evtCtnsBox evt_03" data-aos="fade-up">
@@ -126,7 +204,7 @@
     <div id="vid4" class="yt_f" style="display: none;">
         <span class="b-close">X</span>
         <div class="videoBox">
-            <iframe src="https://www.youtube.com/embed/HEVczcIriqw?rel=0" frameborder="0" allowfullscreen=""></iframe>
+            <iframe src="https://www.youtube.com/embed/e4arGm-tJWE?rel=0" frameborder="0" allowfullscreen=""></iframe>
         </div>
     </div>
     <div id="vid5" class="yt_f" style="display: none;">
@@ -164,7 +242,51 @@
 
   <!-- End evtContainer -->
 
-  <link href="/public/js/willbes/dist/aos.css" rel="stylesheet">
+    <script type="text/javascript">
+        var $regi_form = $('#regi_form');
+       
+        function goCartNDirectPay(ele_id, field_name, cart_type, learn_pattern, is_direct_pay) {
+            {!! login_check_inner_script('로그인 후 이용하여 주십시오.','Y') !!}
+
+            var $regi_form = $('#' + ele_id);
+            var $prod_code = $regi_form.find('input[name="' + field_name + '"]:checked');   // 상품코드
+            var $is_chk = $regi_form.find('input[name="is_chk"]');  // 동의여부
+            var params;
+
+            if ($is_chk.length > 0) {
+                if ($is_chk.is(':checked') === false) {
+                    alert('이용안내에 동의하셔야 합니다.');
+                    $is_chk.focus();
+                    return;
+                }
+            }
+
+            if ($prod_code.length < 1) {
+                alert('강좌를 선택해 주세요.');
+                return;
+            }
+
+            // 장바구니 저장 기본 파라미터
+                params = [
+                { 'name' : '{{ csrf_token_name() }}', 'value' : '{{ csrf_token() }}' },
+                { 'name' : '_method', 'value' : 'POST' },
+                { 'name' : 'cart_type', 'value' : cart_type },
+                { 'name' : 'learn_pattern', 'value' : learn_pattern },
+                { 'name' : 'is_direct_pay', 'value' : is_direct_pay }
+            ];
+
+            // 선택된 상품코드 파라미터
+            $prod_code.each(function() {
+                params.push({ 'name' : 'prod_code[]', 'value' : $(this).val() + ':613001:' + $(this).val() });
+            });
+
+            //장바구니 저장 URL로 전송
+            formCreateSubmit('{{ front_url('/cart/store') }}', params, 'POST');
+        }
+
+    </script>
+
+    <link href="/public/js/willbes/dist/aos.css" rel="stylesheet">
     <script src="/public/js/willbes/dist/aos.js"></script>  
     <script type="text/javascript" src="https://pass.willbes.net/public/js/willbes/jquery.bpopup.min.js"></script>
 
@@ -185,7 +307,7 @@
                     $("#vid1 iframe").attr('src', 'https://www.youtube.com/embed/B11PSWKoBsY?rel=0');
                     $("#vid2 iframe").attr('src', 'https://www.youtube.com/embed/8WuVy15dGw0?rel=0');
                     $("#vid3 iframe").attr('src', 'https://www.youtube.com/embed/Z7PDrEhrY2o?rel=0');
-                    $("#vid4 iframe").attr('src', 'https://www.youtube.com/embed/HEVczcIriqw?rel=0');
+                    $("#vid4 iframe").attr('src', 'https://www.youtube.com/embed/e4arGm-tJWE?rel=0');
                     $("#vid5 iframe").attr('src', 'https://www.youtube.com/embed/W3wcvq26MuM?rel=0');
                     $("#vid6 iframe").attr('src', 'https://www.youtube.com/embed/t6sfD77mE8Y?rel=0');
                     $("#vid7 iframe").attr('src', 'https://www.youtube.com/embed/kiOvGUUzPhM?rel=0');
@@ -193,7 +315,8 @@
                     $("#vid9 iframe").attr('src', 'https://www.youtube.com/embed/rkkN4KuT4cQ?rel=0');
                 }
             });
-        }
+        }        
+        
         </script>
         
     </script>
--- views/willbes/pc/promotion/2003/2949.blade.php
+++ views/willbes/pc/promotion/2003/2949.blade.php
@@ -22,12 +22,34 @@
         .sky a {display:block; margin-bottom:10px}
 
         .evt_top {background:url(https://static.willbes.net/public/images/promotion/2023/04/2949_top_bg.jpg) no-repeat center top; height:1075px;}
-        .evt_top .main_img {position:absolute; top:200px; left:50%; margin-left:-331.5px}    
+        .evt_top .main_img {position:absolute; top:200px; left:50%; margin-left:-334.5px}
 
-        .evt_01 {background:url(https://static.willbes.net/public/images/promotion/2023/04/2949_01_bg.jpg) no-repeat center top;}
-        .apply_area {display:flex; justify-content:space-around; position:absolute; bottom:300px; width:1120px; left:50%; margin-left:-560px;}
-        .apply {background:#7B0000;color:#FF9C00;padding:10px;width:200px;font-size:14px;line-height:1.5;font-weight:bold;}
-        .apply:hover {background:#fff;color:#000;}
+        .evtPass {background:#FF9C00; padding:50px 0}
+        .evtPass .title01 {font-size:30px; color:#000; margin-bottom:100px}
+        .evtPass .wrap {width:1120px; margin:0 auto}
+        .evtPass .passLecBuy {display:flex; justify-content:space-around; position:absolute; bottom:25px; width:1120px; left:50%; margin-left:-560px;}
+        .evtPass .passLecBuy div {width:50%; line-height:30px; font-size:21px; font-weight:bold; text-align:center;} 
+        .evtPass .passLecBuy p {font-size:18px; margin-bottom:20px; text-align:center; margin-left:-30px}
+
+        .evtPass input[type="radio"] {height:22px; width:22px; vertical-align:middle}
+        .evtPass input[type="checkbox"] {height:25px; width:25px; vertical-align:middle}
+        .evtPass input:checked + label {border-bottom:1px dashed #533fd1; color:#533fd1}
+
+        .evtPass .totalPrice {width:860px; margin:90px auto 0;}
+        .evtPass .totalPrice a {display:block; background:#000; font-size:44px; color:#fff; padding:20px; background:#000; border-radius:5px; box-shadow:10px rgba(0,0,0,.5);}
+        .evtPass .totalPrice a:hover {background:#533fd1}
+
+        .evtPass .check {width:800px; margin:0 auto; padding:20px; font-size:25px;}
+        .evtPass .check a {display:inline-block; padding:10px; color:#fff; background:#000; margin-left:40px; border-radius:20px; font-size:12px}
+        .evtPass .check p {font-size:14px; padding:10px 0 0 20px; line-height:1.4}
+        .evtPass .check input:checked + label {border-bottom:1px dashed #533fd1; color:#533fd1}
+        .evtPass .agree{font-weight:bold;}
+
+        .evtPass .title02 {font-size:30px; color:#000; margin:90px auto 30px;font-weight:bold;line-height:1.25;}
+        .evtPass .title02 div {font-size:46px}
+
+        .evtPass .txtinfo {width:1006px; margin:0 auto; padding:40px; border:1px solid #000; border-radius:10px; margin-bottom:50px; font-size:16px}
+        .evtPass .txtinfo p {background:#000; color:#fff; padding:10px; border-radius:30px; margin-top:-60px; margin-bottom:20px; font-size:20px}
 
         .evt_02 {background:#E3E3E3;}    
 
@@ -52,7 +74,7 @@
         position: absolute;
         right: 0;
         top: -60px;
-        display: inline-block;
+        display: inline-block;as 
         cursor: pointer;
         font-size: 40px;
         font-weight: bold;
@@ -75,26 +97,56 @@
             <img src="https://static.willbes.net/public/images/promotion/2023/04/2949_top.png"  alt="윌비스 소방 119패스" data-aos="flip-down" class="main_img"/>
         </div>
 
-        <div class="evtCtnsBox evt_01" data-aos="fade-up" id="apply_go">
-            <img src="https://static.willbes.net/public/images/promotion/2023/04/2949_01.jpg" title="수강신청">
-            <div class="apply_area">
-                <div class="apply">
-                    <a href="https://pass.willbes.net/periodPackage/show/cate/3023/pack/648001/prod-code/207036" target="_blank">24년 공채 0원 119 PASS<br>신청하기 ></a>                
+        <div class="evtCtnsBox evtPass" data-aos="fade-up" id="apply_go">
+            <img src="https://static.willbes.net/public/images/promotion/2023/04/2949_01_title.jpg" alt="구매전 안내"/>           
+            <div class="wrap" id="pass">
+                <img src="https://static.willbes.net/public/images/promotion/2023/04/2949_01.jpg" alt="">
+                <div class="passLecBuy NSK-Black"> 
+                    <div>                                       
+                        <input type="radio" id="y_pkg1" name="y_pkg" value="207036"/>                
+                        <label for="y_pkg1">24년 0원 119 PASS</label>
+                    </div>
+                    <div>                    
+                        <input type="radio" id="y_pkg2" name="y_pkg" value="207037"/>
+                        <label for="y_pkg2">24년 119 PASS</label>
+                    </div>
+                    <div>                                       
+                        <input type="radio" id="y_pkg3" name="y_pkg" value="207038"/>                
+                        <label for="y_pkg3">24년 0원 119 PASS<br> 구조/학과</label>
+                    </div>
+                    <div>                                       
+                        <input type="radio" id="y_pkg4" name="y_pkg" value="207039"/>                
+                        <label for="y_pkg4">24년 0원 119 PASS<br> 구급</label>
+                    </div>                   
                 </div>
-                <div class="apply">
-                    <a href="https://pass.willbes.net/periodPackage/show/cate/3023/pack/648001/prod-code/207037" target="_blank">24년 공채 119 PASS<br>신청하기 ></a>               
-                </div>
-                <div class="apply">
-                    <a href="https://pass.willbes.net/periodPackage/show/cate/3023/pack/648001/prod-code/207038" target="_blank">24년 구조/학과 경채 0원 PASS<br>신청하기 ></a>               
-                </div>
-                <div class="apply">
-                    <a href="https://pass.willbes.net/periodPackage/show/cate/3023/pack/648001/prod-code/207039" target="_blank">24년 구급 경채 0원 PASS<br>신청하기 ></a>                
-                </div>
+            </div>
+
+            <div class="check">
+                <input type="checkbox" id="is_chk1" name="is_chk" value="Y"/>
+                <label for="is_chk1" class="agree">PASS 과정 세부사항 확인하였고,이에 동의합니다.</label>             
+            </div>
+
+            <div class="title02" id="transfer">
+                신규가입 & 재수강 & 환승 하실 수강생은 모두 주목!!
+                <div class="NSK-Black">조건없이 무조건 혜택 받고 구매하세요.</div>
+            </div>
+
+          
+
+            <div class="totalPrice NSK-Black">
+                <a href="javascript:void(0);" onclick="termsCheck('is_chk1', 'pass');">패스상품 신청하기 ></a>
             </div>  
         </div>
   
         <div class="evtCtnsBox evt_02" data-aos="fade-up">
-            <img src="https://static.willbes.net/public/images/promotion/2023/04/2949_02.jpg" title="강사진">           
+            <div class="wrap">
+                <img src="https://static.willbes.net/public/images/promotion/2023/04/2949_02.jpg" title="강사진">
+                <a href="https://pass.willbes.net/professor/show/cate/3023/prof-idx/51421?subject_idx=1113" title="권나라" target="_blank" style="position: absolute;left: 6.93%;top: 61.16%;width: 7.71%;height: 5.31%;z-index: 2;"></a>
+                <a href="https://pass.willbes.net/professor/show/cate/3023/prof-idx/51420?subject_idx=1138" title="오도희" target="_blank" style="position: absolute;left: 25.93%;top: 61.16%;width: 7.71%;height: 5.31%;z-index: 2;"></a>
+                <a href="https://pass.willbes.net/professor/show/cate/3023/prof-idx/51206?subject_idx=1111" title="신기훈" target="_blank" style="position: absolute;left: 46.53%;top: 61.16%;width: 7.71%;height: 5.31%;z-index: 2;"></a>
+                <a href="https://pass.willbes.net/professor/show/cate/3023/prof-idx/51449?subject_idx=1140" title="김윤정" target="_blank" style="position: absolute;left: 65.93%;top: 61.16%;width: 7.71%;height: 5.31%;z-index: 2;"></a>
+                <a href="https://pass.willbes.net/professor/show/cate/3023/prof-idx/51439?subject_idx=2261" title="이혜영" target="_blank" style="position: absolute;left: 87.53%;top: 61.16%;width: 7.71%;height: 5.31%;z-index: 2;"></a>
+            </div>         
         </div>
 
         <div class="evtCtnsBox evt_03" data-aos="fade-up">
@@ -156,7 +208,7 @@
     <div id="vid4" class="yt_f" style="display: none;">
         <span class="b-close">X</span>
         <div class="videoBox">
-            <iframe src="https://www.youtube.com/embed/HEVczcIriqw?rel=0" frameborder="0" allowfullscreen=""></iframe>
+            <iframe src="https://www.youtube.com/embed/e4arGm-tJWE?rel=0" frameborder="0" allowfullscreen=""></iframe>
         </div>
     </div>
     <div id="vid5" class="yt_f" style="display: none;">
@@ -213,7 +265,7 @@
                     $("#vid1 iframe").attr('src', 'https://www.youtube.com/embed/B11PSWKoBsY?rel=0');
                     $("#vid2 iframe").attr('src', 'https://www.youtube.com/embed/8WuVy15dGw0?rel=0');
                     $("#vid3 iframe").attr('src', 'https://www.youtube.com/embed/Z7PDrEhrY2o?rel=0');
-                    $("#vid4 iframe").attr('src', 'https://www.youtube.com/embed/HEVczcIriqw?rel=0');
+                    $("#vid4 iframe").attr('src', 'https://www.youtube.com/embed/e4arGm-tJWE?rel=0');
                     $("#vid5 iframe").attr('src', 'https://www.youtube.com/embed/W3wcvq26MuM?rel=0');
                     $("#vid6 iframe").attr('src', 'https://www.youtube.com/embed/t6sfD77mE8Y?rel=0');
                     $("#vid7 iframe").attr('src', 'https://www.youtube.com/embed/kiOvGUUzPhM?rel=0');
@@ -221,6 +273,16 @@
                     $("#vid9 iframe").attr('src', 'https://www.youtube.com/embed/rkkN4KuT4cQ?rel=0');
                 }
             });
+        }
+
+           /*약관동의*/
+           function termsCheck(terms_id,ele_id){
+            if($("#" + terms_id).is(":checked") === false){
+                $("#" + terms_id).focus();
+                alert('이용안내에 동의하셔야 합니다.');
+                return;
+            }
+            goCartNDirectPay(ele_id, 'y_pkg', 'on_lecture', 'periodpack_lecture', 'Y');
         }
         
         </script>
